d0a77658279d582c60c1f84056438e78bd9e3778
logg <devnull@localhost>
2002-11-08 18:39:14 +0000

    Tailorized "2002-11-08 12:38:07 by logg" Added missing file.

---
+++ src/kernel/main/Problem.C
@@ -0,0 +1,28 @@
+#include <dolfin/Problem.hh>
+#include "dolfin_modules.h"
+
+using namespace dolfin;
+
+//-----------------------------------------------------------------------------
+Problem::Problem(const char *problem)
+{
+  solver = dolfin_module_solver(problem);
+  grid = 0;
+}
+//-----------------------------------------------------------------------------
+Problem::Problem(const char *problem, Grid *grid)
+{
+  solver = dolfin_module_solver(problem,grid);
+  this->grid = grid;
+}
+//-----------------------------------------------------------------------------
+void Problem::set(const char *property, ...)
+{
+  
+}
+//-----------------------------------------------------------------------------
+void Problem::solve()
+{
+  solver->solve();
+}
+//-----------------------------------------------------------------------------
