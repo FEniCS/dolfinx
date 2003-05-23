158b9813e2814fd6e3e8d40d367bcf1e59f507b0
logg <devnull@localhost>
2003-05-23 16:51:58 +0000

    Tailorized "2003-05-23 11:51:54 by logg" Added tools for automatic creation of documentation

---
+++ src/greeting/greeting.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Include variables saved by make
+TMPFILE="var.tmp"
+VERSION=`cat $TMPFILE | grep VERSION | cut -d'"' -f2`
+
+# Display a message
+echo "-------------------------------------------------------------------------------"
+echo "                  - DOLFIN successfully compiled -"
+echo " "
+echo "If you have reached this far, this is a good indication that the compilation"
+echo "was successful. The next thing you want to do is probably to try out some of"
+echo "the demo programs in the sub-directory src/demo/."
+echo " "
+echo "To improve future releases of DOLFIN, any feedback or suggestions are very much"
+echo "appreciated. Send any comments to dolfin@math.chalmers.se."
+echo " "
+echo "If you like, you can try to type the following command:"
+echo " "
+echo "   uname -a | mail -s 'DOLFIN $VERSION installed' dolfin@math.chalmers.se"
+echo " "
+echo "This will send an email with the output of 'uname -a' to the above address. If"
+echo "you are worried about the information you send, type 'uname -a' first to see"
+echo "what it does. The output should be something like"
+echo " "
+echo "   Linux elrond 2.4.19 #4 tis aug 27 09:33:15 CEST 2002 i686 GNU/Linux"
+echo "   SunOS mathtest4 5.7 Generic_106541-16 sun4u sparc SUNW,Ultra-5_1"
+echo " "
+echo "Enjoy!"
+echo "-------------------------------------------------------------------------------"
