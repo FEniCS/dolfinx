e66dea13bad0f15599fa8903561d8bcb32da5879
logg <devnull@localhost>
2003-06-17 12:42:56 +0000

    Tailorized "2003-06-17 07:42:56 by logg" Added missing files

---
+++ src/demo/tanganyika/main.cpp
@@ -0,0 +1,39 @@
+// Copyright (C) 2003 Johan Hoffman and Anders Logg.
+// Licensed under the GNU GPL Version 2.
+
+#include <stdlib.h>
+#include <dolfin.h>
+
+using namespace dolfin;
+
+int main(int argc, char** argv)
+{
+  dolfin_set("output", "plain text");
+
+  if ( argc != 3 ) {
+    dolfin::cout << "Usage: dolfin-tanganyika method q' where method is one of" << dolfin::endl;
+    dolfin::cout << "cgq or dgq, and q is the order" << dolfin::endl;
+    return 1;
+  }
+  
+  int q = atoi(argv[2]);
+
+  if ( strcasecmp(argv[1], "cgq") == 0 ) {
+    
+    cGqMethod cGq(q);
+    cGq.show();
+
+  }
+  else if ( strcasecmp(argv[1], "dgq") == 0 ) {
+    
+    dGqMethod dGq(q);
+    dGq.show();
+    
+  } 
+  else {
+    dolfin::cout << "Unknown method." << dolfin::endl;
+    return 1;
+  }
+  
+  return 0;
+}
