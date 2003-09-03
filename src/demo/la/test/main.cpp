57b0566849a8c46da1d354fb0687ca8fabb2aab9
logg <devnull@localhost>
2003-09-03 17:41:57 +0000

    Tailorized "2003-09-03 12:41:57 by logg" ----

---
+++ src/demo/la/test/main.cpp
@@ -0,0 +1,32 @@
+// Copyright (C) 2003 Johan Hoffman and Anders Logg.
+// Licensed under the GNU GPL Version 2.
+
+#include <dolfin.h>
+
+using namespace dolfin;
+
+int main()
+{
+  dolfin::cout << "Test" << " test" << dolfin::endl;
+
+  Matrix A(3,3, Matrix::dense);
+
+  A(0,0) = 1.0;
+  A(0,2) = 2.0;
+  A(1,0) = 3.0;
+  A(2,1) = 1.0;
+  A(2,2) = 2.0;
+
+  Vector b(3);
+  b = 1.0;
+
+  Vector x;
+  A.solve(x,b);
+
+  x.show();
+
+  File file("matrix.m");
+  file << A;
+
+  return 0;
+}
