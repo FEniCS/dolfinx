1d1130e900038e1f507907c0d5ea66be137753b8
logg <devnull@localhost>
2003-09-16 00:12:47 +0000

    Tailorized "2003-09-15 19:12:47 by logg" Added file

---
+++ src/kernel/grid/EdgeRefData.cpp
@@ -0,0 +1,25 @@
+// Copyright (C) 2003 Johan Hoffman and Anders Logg.
+// Licensed under the GNU GPL Version 2.
+
+#include <dolfin/Cell.h>
+#include <dolfin/EdgeRefData.h>
+
+using namespace dolfin;
+
+//-----------------------------------------------------------------------------
+void EdgeRefData::mark(Cell& cell)
+{
+  if ( !cells.contains(&cell) )
+    cells.add(&cell);
+}
+//-----------------------------------------------------------------------------
+bool EdgeRefData::marked() const
+{
+  return cells.size() > 0;
+}
+//-----------------------------------------------------------------------------
+bool EdgeRefData::marked(Cell& cell)
+{
+  return cells.contains(&cell);
+}
+//-----------------------------------------------------------------------------
