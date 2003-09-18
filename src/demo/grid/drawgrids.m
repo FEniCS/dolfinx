f00ce5aad67250c551563b7b27a6ead8299b9c1c
logg <devnull@localhost>
2003-09-18 13:55:24 +0000

    Tailorized "2003-09-18 08:55:24 by logg" Grid refinement updates

---
+++ src/demo/grid/drawgrids.m
@@ -0,0 +1,24 @@
+% Copyright (C) 2003 Johan Hoffman and Anders Logg.
+% Licensed under the GNU GPL Version 2.
+%
+% Draw grids from the 2D grid refinement example.
+
+grid2D_unrefined
+p0 = points;
+e0 = edges;
+t0 = cells;
+
+grid2D_refined
+p1 = points;
+e1 = edges;
+t1 = cells;
+
+clf
+
+subplot(1,2,1)
+pdemesh(p0, e0, t0)
+axis([0 1 0 1])
+
+subplot(1,2,2)
+pdemesh(p1, e1, t1)
+axis([0 1 0 1])
