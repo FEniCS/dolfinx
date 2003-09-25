b6b75df1a725fed4f4d0257ba026cd9a3f55b546
hoffman <devnull@localhost>
2003-09-25 19:47:54 +0000

    Tailorized "2003-09-25 14:47:54 by hoffman" fixes

--- src/kernel/la/dolfin/dolfin_la.h
+++ src/kernel/la/dolfin/dolfin_la.h
@@ -6,6 +6,7 @@
 #include <dolfin/Vector.h>
 #include <dolfin/DenseMatrix.h>
 #include <dolfin/Matrix.h>
+#include <dolfin/MultigridSolver.h>
 #include <dolfin/DirectSolver.h>
 #include <dolfin/SISolver.h>
 #include <dolfin/KrylovSolver.h>
