411c55bba0566e36e6e70e5dc79b8d03e9440da9
hoffman <devnull@localhost>
2003-09-25 19:02:29 +0000

    Tailorized "2003-09-25 14:02:29 by hoffman" Added MultigridSolver

---
+++ src/kernel/la/dolfin/MultigridSolver.h
@@ -0,0 +1,30 @@
+// Copyright (C) 2002 Johan Hoffman and Anders Logg.
+// Licensed under the GNU GPL Version 2.
+
+#ifndef __MULTIGRID_SOLVER_H
+#define __MULTIGRID_SOLVER_H
+
+namespace dolfin {
+  
+  class GridHierarchy;
+  class Matrix;
+  class Vector;
+  
+  class MultigridSolver{
+  public:
+    
+    MultigridSolver(GridHierarchy& grids);
+    MultigridSolver();
+    ~MultigridSolver();
+    
+    /// Solve Ax = b 
+    void solve(const Matrix& A, Vector& x, const Vector& b);
+
+  private:
+    
+
+  };
+  
+}
+  
+#endif
