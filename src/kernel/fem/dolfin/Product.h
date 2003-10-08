edc6ae06f4a8ddddd1622c660297faf438e6eaad
logg <devnull@localhost>
2003-10-08 01:00:29 +0000

    Tailorized "2003-10-07 20:00:29 by logg" Remove a Makefile

--- src/kernel/fem/dolfin/Product.h
+++ src/kernel/fem/dolfin/Product.h
@@ -18,77 +18,77 @@ namespace dolfin {
   class FunctionSpace::Product {
   public:
 	 
-	 // Empty constructor for v = 1
-	 Product();
-
-	 // Initialisation
-	 Product(const ShapeFunction &v);
-	 Product(const Product       &v);
-	 
-	 // Constructors for v0 * v1
-	 Product(const ShapeFunction &v0, const ShapeFunction &v1);
-	 Product(const Product       &v0, const Product       &v1);
-	 Product(const ShapeFunction &v0, const Product       &v1);
-	 
-	 // Destructor
-	 ~Product();
-	 
-	 // Assignment (v0 * v1)
-	 void set(const ShapeFunction &v0, const ShapeFunction &v1);
-	 void set(const Product       &v0, const Product       &v1);
-	 void set(const ShapeFunction &v0, const Product       &v1);
-
-	 // Get id
-	 int* id() const;
-
-	 // True if equal to zero
-	 bool zero() const;
-
-	 // True if equal to unity
-	 bool one() const;
-	 
-	 // Get number of factors
-	 int size() const;
-
-	 //--- Operators ---
-	 
-	 // Evaluation
-	 real operator() (real x, real y, real z, real t) const;
-	 real operator() (Point p) const;
-	 
-	 // Assignment
-	 Product& operator= (const ShapeFunction &v);
-	 Product& operator= (const Product       &v);
-	 
-	 // Addition
-	 ElementFunction operator+ (const ShapeFunction   &v) const;
-	 ElementFunction operator+ (const Product         &v) const;
-	 ElementFunction operator+ (const ElementFunction &v) const;
-	 
-	 // Subtraction
-	 ElementFunction operator- (const ShapeFunction   &v) const;
-	 ElementFunction operator- (const Product         &v) const;
-	 ElementFunction operator- (const ElementFunction &v) const;
-	 
-	 // Multiplication
-	 ElementFunction operator* (real a)                   const;
-	 Product         operator* (const ShapeFunction   &v) const;
-	 Product         operator* (const Product         &v) const;
-	 ElementFunction operator* (const ElementFunction &v) const;
-	 
-	 // Integration
-	 real operator* (Integral::Measure &dm) const;
-
-	 // Output
-	 friend LogStream& operator<<(LogStream& stream, const Product &v);
-	 
+    // Empty constructor for v = 1
+    Product();
+    
+    // Initialisation
+    Product(const ShapeFunction &v);
+    Product(const Product       &v);
+    
+    // Constructors for v0 * v1
+    Product(const ShapeFunction &v0, const ShapeFunction &v1);
+    Product(const Product       &v0, const Product       &v1);
+    Product(const ShapeFunction &v0, const Product       &v1);
+    
+    // Destructor
+    ~Product();
+    
+    // Assignment (v0 * v1)
+    void set(const ShapeFunction &v0, const ShapeFunction &v1);
+    void set(const Product       &v0, const Product       &v1);
+    void set(const ShapeFunction &v0, const Product       &v1);
+    
+    // Get id
+    int* id() const;
+    
+    // True if equal to zero
+    bool zero() const;
+    
+    // True if equal to unity
+    bool one() const;
+    
+    // Get number of factors
+    int size() const;
+    
+    //--- Operators ---
+    
+    // Evaluation
+    real operator() (real x, real y, real z, real t) const;
+    real operator() (Point p) const;
+    
+    // Assignment
+    Product& operator= (const ShapeFunction &v);
+    Product& operator= (const Product       &v);
+    
+    // Addition
+    ElementFunction operator+ (const ShapeFunction   &v) const;
+    ElementFunction operator+ (const Product         &v) const;
+    ElementFunction operator+ (const ElementFunction &v) const;
+    
+    // Subtraction
+    ElementFunction operator- (const ShapeFunction   &v) const;
+    ElementFunction operator- (const Product         &v) const;
+    ElementFunction operator- (const ElementFunction &v) const;
+    
+    // Multiplication
+    ElementFunction operator* (real a)                   const;
+    Product         operator* (const ShapeFunction   &v) const;
+    Product         operator* (const Product         &v) const;
+    ElementFunction operator* (const ElementFunction &v) const;
+    
+    // Integration
+    real operator* (Integral::Measure &dm) const;
+    
+    // Output
+    friend LogStream& operator<<(LogStream& stream, const Product &v);
+    
   private:
-	 
-	 int n;    // Number of factors
-	 int *_id; // Shape function id:s
-	 
+    
+    int n;    // Number of factors
+    int *_id; // Shape function id:s
+    
   };
-
+  
   FunctionSpace::ElementFunction operator* (real a, const FunctionSpace::Product &v);
   
 }
