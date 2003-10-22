c8febaf612643155f7c15e948eb738b8d2c44698
logg <devnull@localhost>
2003-10-22 03:20:55 +0000

    Tailorized "2003-10-21 22:18:42 by logg" Updates

--- src/post/post.sh
+++ src/post/post.sh
@@ -2,30 +2,38 @@
 
 # Generate configure script dolfin-config
 
+cd ../../
+TOPLEVEL=`pwd`
+
 # Main include file
-MAIN_INCLUDE="../kernel/main/dolfin.h"
+MAIN_INCLUDE="src/kernel/main/dolfin.h"
+
+# Find all dolfin include files
+DOLFIN_INCLUDES=""
+echo "Scanning for DOLFIN include files..."
+for f in `find src -name '*.h*' | grep dolfin | grep -v 'dolfin.h' | grep -v '~'`; do
+	 DOLFIN_INCLUDES="$DOLFIN_INCLUDES $f"
+#	 echo "  Found: $f"
+done
 
 # Find all libraries (.a files)
 DOLFIN_LIBS=""
 echo "Scanning for DOLFIN libraries..."
-for f in `find .. -name '*.a'`; do
+for f in `find src -name '*.a'`; do
 	 lib=`echo $f | xargs basename | cut -d'b' -f2-0 | cut -d'.' -f1`
 	 DOLFIN_LIBS="$DOLFIN_LIBS $f"
 	 echo "  Found: $lib"
 done
 
-# Find all dolfin include files
-DOLFIN_INCLUDES=""
-echo "Scanning for DOLFIN include files..."
-for f in `find .. -name '*.h*' | grep dolfin | grep -v 'dolfin.h' | grep -v '~'`; do
-	 DOLFIN_INCLUDES="$DOLFIN_INCLUDES $f"
-	 echo "  Found: $f"
+# Create symbolic link for main include file
+ln -s -f $TOPLEVEL/$MAIN_INCLUDE include
+
+# Create symbolic links for include files
+for f in $DOLFIN_INCLUDES; do
+    ln -s -f $TOPLEVEL/$f include/dolfin
 done
 
-# Copy include files and libraries
-echo Copying main include file to ../../include
-cp $MAIN_INCLUDE ../../include
-echo Copying include files to ../../include/dolfin
-cp $DOLFIN_INCLUDES ../../include/dolfin
-echo Copying libraries to ../../lib
-cp $DOLFIN_LIBS ../../lib
+# Create symbolic links for libraries
+for f in $DOLFIN_LIBS; do
+    ln -s -f $TOPLEVEL/$f lib
+done
