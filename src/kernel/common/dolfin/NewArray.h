94b0ef5e631e7d89497d8b72d9bf0c02047404fc
logg <devnull@localhost>
2003-11-07 13:35:46 +0000

    Tailorized "2003-11-07 07:35:46 by logg" Added missing NewArray and NewList

---
+++ src/kernel/common/dolfin/NewArray.h
@@ -0,0 +1,30 @@
+// Copyright (C) 2003 Johan Jansson.
+// Licensed under the GNU GPL Version 2.
+
+#ifndef __NEWARRAY_H
+#define __NEWARRAY_H
+
+#include <vector>
+
+namespace dolfin {
+
+  /// A NewArray is a container that provides O(1) access time to elements
+  /// and O(1) memory overhead.  
+  ///
+  /// It is a wrapper for std::vector, so see the STL manual for further
+  /// details.
+  
+  template <class T>
+  class NewArray : public std::vector<T>
+  {
+  public:
+    
+    NewArray() : std::vector<T>() {}
+    
+    NewArray(int n) : std::vector<T>(n) {}
+    
+  };
+  
+}
+
+#endif
---
+++ src/kernel/common/dolfin/NewList.h
@@ -0,0 +1,32 @@
+// Copyright (C) 2003 Johan Jansson.
+// Licensed under the GNU GPL Version 2.
+
+#ifndef __NEWLIST_H
+#define __NEWLIST_H
+
+#include <list>
+#include <iterator>
+
+namespace dolfin {
+
+  /// A NewList is a container that provides O(n) access time to elements
+  /// and O(n) memory overhead. However, a NewList can be grown/shrunk without
+  /// reallocation and spliced together with other lists, etc.
+  ///
+  /// It is a wrapper for std::list (doubly-linked list), so see the STL
+  /// manual for further details.
+
+  template <class T>
+  class NewList : public std::list<T>
+  {
+  public:
+    
+    NewList() : std::list<T>() {}
+
+    NewList(NewList<T> &x) : std::list<T>(x) {}
+  
+  };
+  
+}
+
+#endif
