ba059c10ed0f95f8bec84ef81adbb97252ff2ffd
logg <devnull@localhost>
2004-01-23 21:10:04 +0000

    Tailorized "2004-01-23 15:10:04 by logg" Automatically create symbolic links for new octave functions.

---
+++ src/post/links.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cd ../../
+TOPLEVEL=`pwd`
+
+# Create symbolic links
+
+ln -s $TOPLEVEL/src/utils/octave/pdeplot.m src/demo/solvers/convdiff/
+ln -s $TOPLEVEL/src/utils/octave/pdemesh.m src/demo/solvers/convdiff/
+ln -s $TOPLEVEL/src/utils/octave/pdesurf.m src/demo/solvers/convdiff/
+
+ls -s $TOPLEVEL/src/utils/matlab/plotslab.m src/demo/solvers/ode/test/
