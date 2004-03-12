117c4b161924d59467a135b04fd3d429f20713e1
hoffman <devnull@localhost>
2004-03-12 01:46:20 +0000

    Tailorized "2004-03-11 19:46:20 by hoffman" navier-stokes module compiles

--- src/demo/solvers/navierstokes/benchmark/main.cpp
+++ src/demo/solvers/navierstokes/benchmark/main.cpp
@@ -1,53 +1,36 @@
-// Copyright (C) 2002 Johan Hoffman and Anders Logg.
+// Copyright (C) 2002 Johan Hoffman.
 // Licensed under the GNU GPL Version 2.
 
 #include <dolfin.h>
 
 using namespace dolfin;
 
-dolfin_bc my_bc(real x, real y, real z, int node, int component);
+//dolfin_bc mybc(real x, real y, real z, int node, int component);
 
-real fx(real x, real y, real z, real t);
+real f(real x, real y, real z, real t);
 
-real Re = 100.0;
-
-int main(int argc, char **argv)
+// Boundary conditions
+void mybc(BoundaryCondition& bc)
 {
-  // Set problem
-  dolfin_set_problem("navier-stokes");
+  bc.set(BoundaryCondition::DIRICHLET, 0.0);
+}
+
+int main()
+{
+  Mesh mesh("mesh.xml.gz");
+  Problem navierstokes("poisson", mesh);
   
-  // Set parameters
-  dolfin_set_parameter("problem description", "Navier-Stokes equations");
-  dolfin_set_parameter("grid file",           "../../../../data/grids/tetgrid_8_8_8.inp");
-  dolfin_set_parameter("output file prefix",  "navier_stokes");
-  dolfin_set_parameter("output file type",  "opendx");
-  dolfin_set_parameter("output samples",10);
-  dolfin_set_parameter("reynolds number", Re);
-  dolfin_set_parameter("start time", 0.0);
-  dolfin_set_parameter("final time", 0.1);
-  dolfin_set_parameter("debug level", 0);
-
-  dolfin_set_function("fx",fx);
-
-  // Set boundary conditions
-  dolfin_set_boundary_conditions(my_bc);
-  
-  // Initialize dolfin
-  dolfin_init(argc,argv);
-
-  // Save parameters
-  dolfin_save_parameters();
-
-  // Solve the problem
-  dolfin_solve();
-  
-  // Finished
-  dolfin_end();
+  navierstokes.set("source", f);
+  navierstokes.set("boundary condition", mybc);
+ 
+  navierstokes.solve();
   
   return 0;
 }
 
-dolfin_bc my_bc(real x, real y, real z, int node, int component)
+
+/*
+dolfin_bc mybc(real x, real y, real z, int node, int component)
 {
   dolfin_bc bc;
 
@@ -58,12 +41,10 @@ dolfin_bc my_bc(real x, real y, real z, int node, int component)
       bc.type = dirichlet;
       bc.val  = 1.0;
     }
-    /*
-    if ( x == 1.0 ){
-      bc.type = dirichlet;
-      bc.val  = 1.0;
-    }
-    */
+//    if ( x == 1.0 ){
+//      bc.type = dirichlet;
+//      bc.val  = 1.0;
+//    }
     if ( y == 0.0 ){
       bc.type = dirichlet;
       bc.val  = 0.0;
@@ -84,16 +65,14 @@ dolfin_bc my_bc(real x, real y, real z, int node, int component)
     break;
   case 1:
 
-    /*
-    if ( x == 0.0 ){
-      bc.type = dirichlet;
-      bc.val  = 0.0;
-    }
-    if ( x == 1.0 ){
-      bc.type = dirichlet;
-      bc.val  = 0.0;
-    }
-    */
+  //    if ( x == 0.0 ){
+  //      bc.type = dirichlet;
+  //      bc.val  = 0.0;
+  //    }
+  //    if ( x == 1.0 ){
+  //      bc.type = dirichlet;
+  //      bc.val  = 0.0;
+  //    }
     if ( y == 0.0 ){
       bc.type = dirichlet;
       bc.val  = 0.0;
@@ -115,16 +94,14 @@ dolfin_bc my_bc(real x, real y, real z, int node, int component)
 
   case 2:
 
-    /* 
-    if ( x == 0.0 ){
-      bc.type = dirichlet;
-      bc.val  = 0.0;
-    }
-    if ( x == 1.0 ){
-      bc.type = dirichlet;
-      bc.val  = 0.0;
-    }
-    */
+  //    if ( x == 0.0 ){
+  //      bc.type = dirichlet;
+  //      bc.val  = 0.0;
+  //    }
+  //    if ( x == 1.0 ){
+  //      bc.type = dirichlet;
+  //      bc.val  = 0.0;
+  //    }
     if ( y == 0.0 ){
       bc.type = dirichlet;
       bc.val  = 0.0;
@@ -146,12 +123,10 @@ dolfin_bc my_bc(real x, real y, real z, int node, int component)
 
   case 3:
 
-    /*
-    if ( x == 0.0 ){
-      bc.type = dirichlet;
-      bc.val  = 0.0;
-    }
-    */
+  //    if ( x == 0.0 ){
+  //      bc.type = dirichlet;
+  //      bc.val  = 0.0;
+  //    }
     if ( x == 1.0 ){
       bc.type = dirichlet;
       bc.val  = 0.0;
@@ -163,8 +138,9 @@ dolfin_bc my_bc(real x, real y, real z, int node, int component)
     
   return bc;
 }
+*/
 
-real fx(real x, real y, real z, real t)
+real f(real x, real y, real z, real t)
 {
   //return (32.0/Re) * (y*(1.0-y) + z*(1.0-z)); 
   return 0.0; 
