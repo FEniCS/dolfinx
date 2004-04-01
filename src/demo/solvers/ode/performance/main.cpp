75ec9d339c746262480babc412b9f94c35508c43
logg <devnull@localhost>
2004-04-01 16:15:56 +0000

    Tailorized "2004-04-01 10:15:56 by logg" Cleanup performance test

--- src/demo/solvers/ode/performance/main.cpp
+++ src/demo/solvers/ode/performance/main.cpp
@@ -13,69 +13,31 @@
 
 using namespace dolfin;
 
-class Benchmarkcg : public ParticleSystem
+class Benchmark : public ParticleSystem
 {
 public:
   
-  void computeSparsity()
-  {
-    Matrix S(2 * n, 2 * n);
-    int deps = 0;
-
-    for(unsigned int i = 0; i < n; i++)
-    {
-      S(i, n + i) = 1;
-      S(n + i, i) = 1;
-      S(n + i, n + i) = 1;
-      deps += 2;
-
-      if(i != 0)
-      {
-	S(n + i, i - 1) = 1;
-	S(n + i, n + i - 1) = 1;
-	deps++;
-      }
-
-      if(i != n - 1)
-      {
-	S(n + i, i + 1) = 1;
-	S(n + i, n + i + 1) = 1;
-	deps++;
-      }
-    }
-
-    sparsity.set(S);
-
-    cout << "deps: " << deps << endl;
-  }
-
-  Benchmarkcg(unsigned int n, unsigned int m, real b) :
-    ParticleSystem(n, 1), b(b), m(m)
+  Benchmark(unsigned int n, unsigned int M, real b) :
+    ParticleSystem(n, 1), b(b), M(M)
   {
     if ( n < 2 )
       dolfin_error("System must have at least 2 particles.");
-
+    
     cout << "b: " << b << endl;
-    cout << "m: " << m << endl;
+    cout << "M: " << M << endl;
 
     // Final time
     T = 1.0;
-
+    
     // Spring constant
     k = 1.0;
-
-    // Damping constant
-    //b = 100.0;
     
     // Grid size
     //h = 1.0 / static_cast<real>(n - 1);
     h = 0.1;
 
     // Compute sparsity
-    //sparse();
     computeSparsity();
-
-    //sparsity.show();
   }
 
   real x0(unsigned int i)
@@ -109,47 +71,56 @@ public:
   real timestep(unsigned int i)
   {
     if(i == 0)
-      return 0.1 / m;
+      return 0.1 / static_cast<real>(M);
     if(i == n)
-      return 0.1 / m;
+      return 0.1 / static_cast<real>(M);
     return 0.1;
   }
 
 protected:
 
+  void computeSparsity()
+  {
+    Matrix S(2*n, 2*n);
+
+    for (unsigned int i = 0; i < n; i++)
+    {
+      S(i, n + i) = 1;
+      S(n + i, i) = 1;
+      S(n + i, n + i) = 1;
+
+      if (i != 0)
+      {
+	S(n + i, i - 1) = 1;
+	S(n + i, n + i - 1) = 1;
+      }
+
+      if (i != n - 1)
+      {
+	S(n + i, i + 1) = 1;
+	S(n + i, n + i + 1) = 1;
+      }
+    }
+
+    sparsity.set(S);
+  }
+
   real k;
   real b;
   real h;
-  real m;
+  real M;
   
-};
-
-class Benchmarkdg : public Benchmarkcg
-{
-public:
-
-  Benchmarkdg(unsigned int n, unsigned int m, real b) : Benchmarkcg(n, m, b)
-  {
-  }
-
-  real x0(unsigned int i)
-  {
-    if ( i == 0 )
-      return h/2;
-
-    return h * static_cast<real>(i);
-  }
 };
 
 int main(int argC, char* argV[])
 {
-  int n = 100;
-  int m = 100;
+  int n  = 100;
+  int M  = 100;
   real b = 100.0;
 
   std::string method = "mcg";
 
-  if(argC > 1)
+  if (argC > 1)
   {
     std::string arg = argV[1];
     std::istringstream argstream(arg);
@@ -157,15 +128,15 @@ int main(int argC, char* argV[])
     argstream >> n;
   }
 
-  if(argC > 2)
+  if (argC > 2)
   {
     std::string arg = argV[2];
     std::istringstream argstream(arg);
     
-    argstream >> m;
+    argstream >> M;
   }
 
-  if(argC > 3)
+  if (argC > 3)
   {
     std::string arg = argV[3];
     std::istringstream argstream(arg);
@@ -173,66 +144,65 @@ int main(int argC, char* argV[])
     argstream >> b;
   }
 
-  if(argC > 4)
+  if (argC > 4)
   {
     std::string arg = argV[4];
     if(arg == "mcg")
       method = "mcg";
-    if(arg == "mdg")
+    else if(arg == "mdg")
       method = "mdg";
-    if(arg == "cg")
+    else if(arg == "cg")
       method = "cg";
-    if(arg == "dg")
+    else if(arg == "dg")
       method = "dg";
+    else
+      dolfin_error("Unknown method.");
   }
 
   dolfin_set("output", "plain text");
   dolfin_set("tolerance", 0.1);
-  dolfin_set("number of samples", 111);
   dolfin_set("solve dual problem", false);
   dolfin_set("save solution", false);
-  //dolfin_set("save solution", true);
   dolfin_set("partitioning threshold", 0.99);
-  //dolfin::dolfin_set("partitioning threshold", 1e-7);
-  dolfin::dolfin_set("fixed time step", true);
-
+  dolfin_set("fixed time step", true);  
+  dolfin_set("maximum iterations", 20000);
   //dolfin_set("initial time step", 0.01);
   //dolfin_set("fixed time step", true);
-  dolfin_set("maximum iterations", 20000);
 
   cout << "Creating problem with " << n << " masses" << endl;
-
+  
   if (method == "mcg")
   {
-    Benchmarkcg bench(n, m, b);
+    dolfin_set("method", "cg");
+    dolfin_set("order", 1);
+
+    Benchmark bench(n, M, b);
     bench.solve();
   }
-
-  if (method == "mdg")
+  else if (method == "mdg")
   {
     dolfin_set("method", "dg");
     dolfin_set("order", 0);
-
-    Benchmarkdg bench(n, m, b);
+    
+    Benchmark bench(n, M, b);
     bench.solve();
   }
-
-  if (method == "cg")
+  else if (method == "cg")
   {
-    dolfin::dolfin_set("partitioning threshold", 1e-7);
-
-    Benchmarkcg bench(n, m, b);
+    dolfin_set("method", "cg");
+    dolfin_set("order", 1);
+    dolfin_set("partitioning threshold", 1e-7);
+    
+    Benchmark bench(n, M, b);
     bench.solve();
   }
-
-  if (method == "dg")
+  else if (method == "dg")
   {
-    dolfin::dolfin_set("partitioning threshold", 1e-7);
-
     dolfin_set("method", "dg");
     dolfin_set("order", 0);
+    dolfin_set("partitioning threshold", 1e-7);
 
-    Benchmarkdg bench(n, m, b);
+    Benchmark bench(n, M, b);
     bench.solve();
   }
 
--- src/demo/solvers/ode/performance/run-perf-test.sh
+++ src/demo/solvers/ode/performance/run-perf-test.sh
@@ -4,12 +4,16 @@
 # Licensed under the GNU GPL Version 2.
 #
 # Modified by Anders Logg, 2004.
+#
+# Note: /usr/bin/time (Debian package time) is needed
+# to avoid using the bash built-in time, which is
+# difficult to redirect.
 
 function runsingle()
 {
-    time ./dolfin-ode-perf-test $1 $2 $3 $4 1> log1 2> tlog
+    /usr/bin/time -f"$TIMEFORMAT" -otlog ./dolfin-ode-perf-test $1 $2 $3 $4 1> log1 2> log2
     TIME=$( cat tlog )
-    echo "Elapsed time:   " $TIME
+    echo "Elapsed time: "$TIME
     echo -n $( echo $TIME | awk '{ print $4 }' ) " + " $( echo $TIME | awk '{ print $6 }' ) >> timings.m
     echo -n " " >> timings.m
     echo
@@ -17,72 +21,79 @@ function runsingle()
 
 function run()
 {
-    VERSION=`../../../../config/dolfin-config --version`
-    DATE=`date`
-    CFLAGS=`../../../../config/dolfin-config --cflags`
-    TIMEFORMAT='real: %3R  user: %3U  sys: %3S  cpu: %P%%'
-    
-    echo "DOLFIN version: $VERSION ($DATE)"
-    echo "Compiler flags: $CFLAGS"
-    
-    uname -a
-    `../../../../config/dolfin-config --compiler` --version
-    echo "---------------------------------------------------------------------------"
+    #n="10 20 40 100 200 400 1000 2000 4000 10000 20000 50000 100000"
+    #n="10 20 40 100 200 400 1000"
+    n="10 20"
 
-    #N="10 20 40 100 200 400 1000 2000 4000 10000 20000 50000 100000"
-    N="10 20 40 100 200 400 1000"
-    M="100"
-    B="100"
-    
     if [ "$1" = "" ]; then
 	M=100
     else
-	M=$1	
+	M=$1
     fi
     
     if [ "$2" = "" ]; then
-	B=100
+	b=100
     else
-	B=$2
+	b=$2
     fi
-    
+
+    echo "---------------------------------------------------------------------------"    
     echo "M = "$M
-    echo "B = "    
+    echo "b = "$b
     echo
-    #N="100 200 400"
-    #N="4 8 16"    
-    echo "n = [ " $N "];" > timings.m
+
+    echo "n = [ " $n "];" > timings.m
     echo -n "t_mdg0 = [" >> timings.m
 
-    for i in $N; do
-	
+    # Run multi-adaptive test
+    echo "Running multi-adaptive performance test"
+    echo "---------------------------------------"
+    echo
+
+    for i in $n; do
 	echo "mdG(0) n = " $i
-	runsingle $i $M $B mdg
-	
+	runsingle $i $M $b mdg	
     done
-    
+
     echo "];" >> timings.m
     echo "k_mdg0 = " $( echo "timings; lsquares(n, t_mdg0)(1)" | octave -q | awk '{ print $3 }' ) ";" >> timings.m
     echo "m_mdg0 = " $( echo "timings; lsquares(n, t_mdg0)(2)" | octave -q | awk '{ print $3 }' ) ";" >> timings.m
     echo
     echo -n "t_dg0 = [" >> timings.m
-    
-    for i in $N; do
-	
+
+    # Run mono-adaptive test
+    echo "Running mono-adaptive performance test"
+    echo "--------------------------------------"
+    echo
+
+    for i in $n; do
 	echo "dG(0) n = " $i
 	runsingle $i $M mono
-	
     done
-    
+
     echo "];" >> timings.m
     echo "k_dg0 = " $( echo "timings; lsquares(n, t_mdg0)(1)" | octave -q | awk '{ print $3 }' ) ";" >> timings.m
     echo "m_dg0 = " $( echo "timings; lsquares(n, t_mdg0)(2)" | octave -q | awk '{ print $3 }' ) ";" >> timings.m
-    echo
-    echo "---------------------------------------------------------------------------"
 
 }
 
-run 100 100
+VERSION=`../../../../config/dolfin-config --version`
+DATE=`date`
+CFLAGS=`../../../../config/dolfin-config --cflags`
+TIMEFORMAT='real: %e  user: %U  sys: %S  cpu: %P%%'
+echo "DOLFIN version: $VERSION ($DATE)"
+echo "Compiler flags: $CFLAGS"
+uname -a
+`../../../../config/dolfin-config --compiler` --version
+
+# Non-stiff
+run 100 0
+mv timings.m timings_m100_b0.m
+run 200 0
+mv timings.m timings_m200_b0.m
+
+# Non-stiff
+run 100 0
 mv timings.m timings_m100_b100.m
-run 200 100
+run 200 0
 mv timings.m timings_m200_b100.m
