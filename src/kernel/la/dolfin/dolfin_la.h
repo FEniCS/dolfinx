1d0f27e85784a880a7a5b7531de0ad9915856449
logg <devnull@localhost>
2004-04-22 20:13:07 +0000

    Tailorized "2004-04-22 15:13:07 by logg" Remove template multigrid solver. The new multigrid solver will arrive shortly...

--- src/kernel/la/dolfin/dolfin_la.h
+++ src/kernel/la/dolfin/dolfin_la.h
@@ -6,7 +6,6 @@
 #include <dolfin/Vector.h>
 #include <dolfin/DenseMatrix.h>
 #include <dolfin/Matrix.h>
-#include <dolfin/MultigridSolver.h>
 #include <dolfin/DirectSolver.h>
 #include <dolfin/SISolver.h>
 #include <dolfin/KrylovSolver.h>
