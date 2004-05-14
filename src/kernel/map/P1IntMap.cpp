80aa6e3fb4928ac814e7ef6ce3f36d06e9b0a673
hoffman <devnull@localhost>
2004-05-14 20:17:35 +0000

    Tailorized "2004-05-14 15:17:34 by hoffman" New 1d mappings

---
+++ src/kernel/map/P1IntMap.cpp
@@ -0,0 +1,71 @@
+// Copyright (C) 2003 Johan Hoffman and Anders Logg.
+// Licensed under the GNU GPL Version 2.
+
+#include <dolfin/dolfin_log.h>
+#include <dolfin/Cell.h>
+#include <dolfin/Point.h>
+#include <dolfin/Node.h>
+#include <dolfin/P1IntMap.h>
+#include <cmath>
+
+using namespace dolfin;
+
+//-----------------------------------------------------------------------------
+P1IntMap::P1IntMap() : Map()
+{
+  // Set dimension
+  dim = 1;
+}
+//-----------------------------------------------------------------------------
+const FunctionSpace::ElementFunction P1IntMap::ddx
+(const FunctionSpace::ShapeFunction& v) const
+{
+  return g11*v.ddX();
+}
+//-----------------------------------------------------------------------------
+const FunctionSpace::ElementFunction P1IntMap::ddy
+(const FunctionSpace::ShapeFunction& v) const
+{
+  return v.ddY();
+}
+//-----------------------------------------------------------------------------
+const FunctionSpace::ElementFunction P1IntMap::ddz
+(const FunctionSpace::ShapeFunction& v) const
+{
+  return v.ddZ();
+}
+//-----------------------------------------------------------------------------
+const FunctionSpace::ElementFunction P1IntMap::ddt
+(const FunctionSpace::ShapeFunction& v) const
+{
+  return v.ddT();
+}
+//-----------------------------------------------------------------------------
+void P1IntMap::update(const Cell& cell)
+{
+  // Check that cell type is correct
+  if ( cell.type() != Cell::interval )
+    dolfin_error("Wrong cell type for map (must be an interval).");
+  
+  // Reset values
+  reset();
+  
+  // Get coordinates
+  NodeIterator n(cell);
+  Point p0 = n->coord(); ++n;
+  Point p1 = n->coord(); 
+
+  // Set values for Jacobian
+  f11 = p1.x - p0.x; 
+  
+  // Compute determinant
+  d = f11;
+  
+  // Check determinant
+  if ( fabs(d) < DOLFIN_EPS )
+    dolfin_error("Map from reference element is singular.");
+  
+  // Compute inverse
+  g11 = 1 / d;
+}
+//-----------------------------------------------------------------------------
---
+++ src/kernel/map/dolfin/P1IntMap.h
@@ -0,0 +1,30 @@
+// Copyright (C) 2003 Johan Hoffman and Anders Logg.
+// Licensed under the GNU GPL Version 2.
+
+// Map from reference element (0,0) - (1,0) - (0,1)
+// to a given triangle in R^2 (i.e. z = 0).
+
+#ifndef __P1_INT_MAP_H
+#define __P1_INT_MAP_H
+
+#include <dolfin/Map.h>
+
+namespace dolfin {
+  
+  class P1IntMap : public Map {
+  public:
+    
+    P1IntMap();
+    
+    const FunctionSpace::ElementFunction ddx(const FunctionSpace::ShapeFunction &v) const;
+    const FunctionSpace::ElementFunction ddy(const FunctionSpace::ShapeFunction &v) const;
+    const FunctionSpace::ElementFunction ddz(const FunctionSpace::ShapeFunction &v) const;
+    const FunctionSpace::ElementFunction ddt(const FunctionSpace::ShapeFunction &v) const;
+    
+    void update(const Cell &cell);
+    
+  };
+
+}
+
+#endif
