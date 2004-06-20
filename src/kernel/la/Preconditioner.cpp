6e2ceeb632acf7f93fd48dbba89ec2dcb9431004
logg <devnull@localhost>
2004-06-20 00:49:05 +0000

    Tailorized "2004-06-19 19:49:05 by logg" Added missing file

---
+++ src/kernel/la/Preconditioner.cpp
@@ -0,0 +1,18 @@
+// Copyright (C) 2004 Johan Hoffman and Anders Logg.
+// Licensed under the GNU GPL Version 2.
+
+#include <dolfin/Preconditioner.h>
+
+using namespace dolfin;
+
+//-----------------------------------------------------------------------------
+Preconditioner::Preconditioner()
+{
+  // Do nothing
+}
+//-----------------------------------------------------------------------------
+Preconditioner::~Preconditioner()
+{
+  // Do nothing
+}
+//-----------------------------------------------------------------------------
