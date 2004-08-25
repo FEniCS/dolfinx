e3a56f37e4798557f3a5bad8769a10cc8348024e
johanjan <devnull@localhost>
2004-08-25 04:31:36 +0000

    Tailorized "2004-08-24 23:31:36 by johanjan" Added skeleton for VectorFunction (really this time).

---
+++ src/kernel/function/VectorFunction.cpp
@@ -0,0 +1,15 @@
+// Copyright (C) 2003 Johan Hoffman, Johan Jansson and Anders Logg.
+// Licensed under the GNU GPL Version 2.
+
+#include <dolfin/VectorFunction.h>
+
+using namespace dolfin;
+
+//-----------------------------------------------------------------------------
+NewArray<real> VectorFunction::operator() (const Point& p, real t)
+{
+  NewArray<real> v;
+
+  return v;
+}
+//-----------------------------------------------------------------------------
