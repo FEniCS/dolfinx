0f7706cd38c57e92b3fdefe3cdf6a29630078ef9
hoffman <devnull@localhost>
2005-02-14 18:39:38 +0000

    Tailorized "2005-02-14 12:39:38 by hoffman" fixes

---
+++ src/modules/convdiff/dolfin/ConvDiff.h
@@ -0,0 +1,44 @@
+// Copyright (C) 2002 Johan Hoffman and Anders Logg.
+// Licensed under the GNU GPL Version 2.
+
+#ifndef __CONV_DIFF_H
+#define __CONV_DIFF_H
+
+#include <dolfin/PDE.h>
+
+namespace dolfin {
+  
+  class ConvDiff : public PDE {
+  public:
+    
+    ConvDiff(Function& source,
+	     Function::Vector& previous,
+	     Function& diffusion,
+	     Function::Vector& convection) : PDE(2), up(1), b(3)
+    {
+      add(f,  source);
+      add(up, previous);
+      add(a,  diffusion);
+      add(b,  convection);
+    }
+    
+    real lhs(const ShapeFunction& u, const ShapeFunction& v)
+    {
+      return (u*v + k*((b,grad(u))*v + a*(grad(u),grad(v)))) * dx;
+    }
+    
+    real rhs(const ShapeFunction& v)
+    {
+      return (up(0)*v + k*f*v) * dx;
+    }
+    
+  private:    
+    ElementFunction f;          // Source term
+    ElementFunction::Vector up; // Function value at left end-point
+    ElementFunction a;          // Diffusivity
+    ElementFunction::Vector b;  // Convection
+  };
+
+}
+
+#endif
