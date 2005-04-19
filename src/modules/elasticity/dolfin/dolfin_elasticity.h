5598c118077d1d1d3d5da8f18b82dfc8e204b14e
johanjan <devnull@localhost>
2005-04-19 19:28:26 +0000

    Tailorized "2005-04-19 14:28:26 by johanjan" Added missing file.

---
+++ src/modules/elasticity/dolfin/dolfin_elasticity.h
@@ -0,0 +1,10 @@
+// Copyright (C) 2005 Johan Hoffman and Anders Logg.
+// Licensed under the GNU GPL Version 2.
+
+#ifndef __DOLFIN_ELASTICITY_H
+#define __DOLFIN_ELASTICITY_H
+
+#include <dolfin/Elasticity.h>
+#include <dolfin/ElasticitySolver.h>
+
+#endif
