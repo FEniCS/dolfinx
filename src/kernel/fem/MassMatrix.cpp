01833c1c346e4746ca4e37b48f380458e2b2a5e7
logg <devnull@localhost>
2005-06-05 22:06:37 +0000

    Tailorized "2005-06-05 17:06:36 by logg" Updated MassMatrix and StiffnessMatrix for new system.
    MassMatrix and StiffnessMatrix are easy-to-use shortcuts for linear
    elements. Normally, assembly is through FFC, but sometimes it may
    be convenient to just declare a mass or stiffness matrix for linear
    elements.
    
    For example, to create the standard stiffness matrix on the unit
    square with homogeneous Neumann boundary conditions, just do
    
      UnitSquare mesh(16, 16); // Total number of triangles is 2*16*16
      StiffnessMatrix A(mesh); // A is a (PETSc) matrix

--- src/kernel/fem/MassMatrix.cpp
+++ src/kernel/fem/MassMatrix.cpp
@@ -1,7 +1,10 @@
 // Copyright (C) 2004 Johan Hoffman and Anders Logg.
 // Licensed under the GNU GPL Version 2.
 
+#include <dolfin/FEM.h>
 #include <dolfin/Mesh.h>
+#include <dolfin/MassMatrix2D.h>
+#include <dolfin/MassMatrix3D.h>
 #include <dolfin/MassMatrix.h>
 
 using namespace dolfin;
@@ -9,7 +12,19 @@ using namespace dolfin;
 //-----------------------------------------------------------------------------
 MassMatrix::MassMatrix(Mesh& mesh) : Matrix(mesh.noNodes(), mesh.noNodes())
 {
-  // FIXME: BROKEN
-  dolfin_error("Not implemented for new system.");
+  if ( mesh.type() == Mesh::triangles )
+  {
+    MassMatrix2D::BilinearForm a;
+    FEM::assemble(a, *this, mesh);
+  }
+  else if ( mesh.type() == Mesh::tetrahedrons )
+  {
+    MassMatrix3D::BilinearForm a;
+    FEM::assemble(a, *this, mesh);
+  }
+  else
+  {
+    dolfin_error("Unknown mesh type.");
+  }
 }
 //-----------------------------------------------------------------------------
--- src/kernel/fem/StiffnessMatrix.cpp
+++ src/kernel/fem/StiffnessMatrix.cpp
@@ -1,7 +1,10 @@
 // Copyright (C) 2004 Johan Hoffman and Anders Logg.
 // Licensed under the GNU GPL Version 2.
 
+#include <dolfin/FEM.h>
 #include <dolfin/Mesh.h>
+#include <dolfin/StiffnessMatrix2D.h>
+#include <dolfin/StiffnessMatrix3D.h>
 #include <dolfin/StiffnessMatrix.h>
 
 using namespace dolfin;
@@ -10,7 +13,19 @@ using namespace dolfin;
 StiffnessMatrix::StiffnessMatrix(Mesh& mesh, real epsilon)
   : Matrix(mesh.noNodes(), mesh.noNodes())
 {
-  // FIXME: BROKEN
-  dolfin_error("Not implemented for new system.");
+  if ( mesh.type() == Mesh::triangles )
+  {
+    StiffnessMatrix2D::BilinearForm a;
+    FEM::assemble(a, *this, mesh);
+  }
+  else if ( mesh.type() == Mesh::tetrahedrons )
+  {
+    StiffnessMatrix3D::BilinearForm a;
+    FEM::assemble(a, *this, mesh);
+  }
+  else
+  {
+    dolfin_error("Unknown mesh type.");
+  }
 }
 //-----------------------------------------------------------------------------
