4d9e8e1e4e6db3ff45c01bdb0afe2bd97e1ea5d3
logg <devnull@localhost>
2005-10-23 10:38:06 +0000

    Tailorized "2005-10-23 05:38:05 by logg" Add directory and template for test programs.
    This file is to used for testing out new features implemented
    in the library, which means that the contents of this file
    might be (should be) constantly changing. Anything can be
    thrown into this file at any time and may also be removed
    at any time. Use this for temporary test programs that are
    not suitable to be implemented as demos in src/demo.

---
+++ src/test/main.cpp
@@ -0,0 +1,20 @@
+// Copyright (C) 2005 Anders Logg.
+// Licensed under the GNU GPL Version 2.
+
+// This file is used for testing out new features implemented
+// in the library, which means that the contents of this file
+// might be (should be) constantly changing. Anything can be
+// thrown into this file at any time and may also be removed
+// at any time. Use this for temporary test programs that are
+// not suitable to be implemented as demos in src/demo.
+
+#include <dolfin.h>
+
+using namespace dolfin;
+
+int main(int argc, char* argv[])
+{
+  dolfin_info("Testing DOLFIN...");
+
+  return 0;
+}
