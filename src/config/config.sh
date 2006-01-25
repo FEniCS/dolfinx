70172103107260ae0521867ba4ca449849809bb5
johanjan <devnull@localhost>
2006-01-25 19:34:26 +0000

    Tailorized "2006-01-25 13:34:25 by johanjan" config:
    Added "dolfin-swig" tool which automatically generates a Python module
    from a form file (in DOLFIN format).
    
    Modified "dolfin-config" tool to also provide Python include flags for SWIG.

--- src/config/config.sh
+++ src/config/config.sh
@@ -7,6 +7,7 @@ TMPFILE="var.tmp"
 COMPILER=`cat $TMPFILE | grep COMPILER | cut -d'"' -f2`
 CFLAGS=`cat $TMPFILE | grep CFLAGS | cut -d'"' -f2`
 LIBS=`cat $TMPFILE | grep LIBS | cut -d'"' -f2`
+PYTHON_CPPFLAGS=`cat $TMPFILE | grep PYTHON_CPPFLAGS | cut -d'"' -f2`
 PACKAGE=`cat $TMPFILE | grep PACKAGE | cut -d'"' -f2`
 VERSION=`cat $TMPFILE | grep VERSION | cut -d'"' -f2`
 PREFIX=`cat $TMPFILE | grep PREFIX | cut -d'"' -f2`
@@ -31,6 +32,7 @@ echo >> $FILE
 echo COMPILER=\"$COMPILER\" >> $FILE
 echo CFLAGS=\"$CFLAGS_SYSTEM\" >> $FILE
 echo LIBS=\"$LIBS_SYSTEM\" >> $FILE
+echo SWIGCFLAGS=\"$PYTHON_CPPFLAGS\" >> $FILE
 echo PACKAGE=\"$PACKAGE\" >> $FILE
 echo VERSION=\"$VERSION\" >> $FILE
 echo >> $FILE
---
+++ src/config/swig.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+
+# Don't need to do anything, just be part of build system for installation
+
+echo "Building dolfin-swig tool"
+
