5423c54130d24f6475fe580f7d180ce9f68881ec
Anders Logg <logg@tti-c.org>
2006-02-10 12:35:52 -0500

    Initial work on PDE class for automating the solution of variational problems.

--- src/kernel/form/PDE.cpp
+++ src/kernel/form/PDE.cpp
@@ -1,23 +1,28 @@
-// Copyright (C) 2004-2005 Anders Logg.
+// Copyright (C) 2004-2006 Anders Logg.
 // Licensed under the GNU GPL Version 2.
 //
 // First added:  2004
-// Last changed: 2005
+// Last changed: 2006-02-10
 
 #include <dolfin/dolfin_log.h>
+#include <dolfin/Function.h>
 #include <dolfin/PDE.h>
 
 using namespace dolfin;
 
 //-----------------------------------------------------------------------------
-PDE::PDE() : bilinear(0), linear(0)
+PDE::PDE(BilinearForm& a, LinearForm& L, Mesh& mesh)
+  : Parametrized(), _a(&a), _L(&L), _mesh(&mesh), _bc(0)
 {
-  // Do nothing
+  // Add parameters
+  add("solver", "direct");
 }
 //-----------------------------------------------------------------------------
-PDE::PDE(BilinearForm& a, LinearForm& L) : bilinear(&a), linear(&L)
+PDE::PDE(BilinearForm& a, LinearForm& L, Mesh& mesh, BoundaryCondition& bc)
+  : Parametrized(), _a(&a), _L(&L), _mesh(&mesh), _bc(&bc)
 {
-  // Do nothing
+  // Add parameters
+  add("solver", "direct");
 }
 //-----------------------------------------------------------------------------
 PDE::~PDE()
@@ -25,17 +30,33 @@ PDE::~PDE()
   // Do nothing
 }
 //-----------------------------------------------------------------------------
+Function PDE::solve()
+{
+  Function u;
+  return u;
+}
+//-----------------------------------------------------------------------------
+void PDE::solve(Function& u)
+{
+  
+
+}
+//-----------------------------------------------------------------------------
 BilinearForm& PDE::a()
 {
-  if ( !bilinear )
-    dolfin_error("Bilinear form is not specified.");
-  return *bilinear;
+  dolfin_assert(_a);
+  return *_a;
 }
 //-----------------------------------------------------------------------------
 LinearForm& PDE::L()
 {
-  if ( !linear )
-    dolfin_error("Linear form is not specified.");
-  return *linear;
+  dolfin_assert(_L);
+  return *_L;
+}
+//-----------------------------------------------------------------------------
+Mesh& PDE::mesh()
+{
+  dolfin_assert(_mesh);
+  return *_mesh;
 }
 //-----------------------------------------------------------------------------
--- src/kernel/form/dolfin/PDE.h
+++ src/kernel/form/dolfin/PDE.h
@@ -1,48 +1,63 @@
-// Copyright (C) 2004-2005 Anders Logg.
+// Copyright (C) 2004-2006 Anders Logg.
 // Licensed under the GNU GPL Version 2.
 //
 // First added:  2004
-// Last changed: 2005
+// Last changed: 2006-02-10
 
 #ifndef __PDE_H
 #define __PDE_H
+
+#include <dolfin/Parametrized.h>
 
 namespace dolfin
 {
 
   class BilinearForm;
   class LinearForm;
+  class Mesh;
+  class BoundaryCondition;
 
   /// A PDE represents a (linearized) partial differential equation,
   /// given by a variation problem of the form: Find u in V such that
   ///
-  ///     a(u,v) = L(v) for all v in V,
+  ///     a(v, u) = L(v) for all v in V,
   ///
   /// where a(.,.) is a given bilinear form and L(.) is a given linear form.
 
-  class PDE
+  class PDE : public Parametrized
   {
   public:
 
-    /// Constructor
-    PDE();
+    /// Define a static linear PDE with natural boundary conditions
+    PDE(BilinearForm& a, LinearForm& L, Mesh& mesh);
 
-    /// Constructor
-    PDE(BilinearForm& a, LinearForm& L);
+    /// Define a static linear PDE with Dirichlet boundary conditions
+    PDE(BilinearForm& a, LinearForm& L, Mesh& mesh, BoundaryCondition& bc);
 
     /// Destructor
     ~PDE();
 
+    /// Solve PDE
+    Function solve();
+
+    /// Solve PDE
+    void solve(Function& u);
+    
     /// Return the bilinear form a(.,.)
     BilinearForm& a();
 
     /// Return the linear form L(.,.)
     LinearForm& L();
 
+    /// Return the mesh
+    Mesh& mesh();
+
   protected:
 
-    BilinearForm* bilinear;
-    LinearForm* linear;
+    BilinearForm* _a;
+    LinearForm* _L;
+    Mesh* _mesh;
+    BoundaryCondition* _bc;
 
   };
 
