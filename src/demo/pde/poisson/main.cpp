3c43500d6118ed923040251e9516524a1285d3ab
Anders Logg <logg@tti-c.org>
2006-02-10 17:53:33 -0500

    Make dolfin-convert install during make install

--- src/demo/pde/poisson/main.cpp
+++ src/demo/pde/poisson/main.cpp
@@ -1,8 +1,8 @@
-// Copyright (C) 2005 Anders Logg.
+// Copyright (C) 2005-2006 Anders Logg.
 // Licensed under the GNU GPL Version 2.
 //
 // First added:  2005
-// Last changed: 2005-12-28
+// Last changed: 2006-02-10
 //
 // This demo program solves Poisson's equation
 //
