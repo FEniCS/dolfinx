d1b0ab045279c985ef917e4951d1ca1c4717707f
Garth N. Wells <g.n.wells@tudelft.nl>
2006-02-17 13:44:23 +0100

    Add post-proceessing step to compute and output strain in /src/demo/pde/elasticty.
    The computation of the strain is very slow because in the constructor of PDE, the linear solver set to direct, and it's not possible for the user to chanage this before the solve is peformed.  
    
    The VTK format can handle tensors, so when Function supports tensors, the processing will become simpler.

--- src/demo/pde/elasticity/main.cpp
+++ src/demo/pde/elasticity/main.cpp
@@ -10,6 +10,7 @@
 
 #include <dolfin.h>
 #include "Elasticity.h"
+#include "ElasticityStrain.h"
 
 using namespace dolfin;
 
@@ -59,9 +60,24 @@ int main()
   // Compute solution
   Function u = pde.solve();
 
-  // Save solution to file
+  // Save solution (displacement) to file
   File file("elasticity.pvd");
   file << u;
+
+  // Set up post-processing problem to compute strain
+  ElasticityStrain::BilinearForm a_strain;
+  ElasticityStrain::LinearForm L_strain(u);
+  PDE pde_strain(a_strain, L_strain, mesh);
+  Function normal_strain, shear_strain;
+
+  // Compute solution
+   pde_strain.solve(normal_strain, shear_strain);
+
+  // Save solution (strain) to files
+  File file_normal_strain("normal_strain.pvd");
+  File file_shear_strain("shear_strain.pvd");
+  file_normal_strain << normal_strain;
+  file_shear_strain  << shear_strain;
 
   return 0;
 }
