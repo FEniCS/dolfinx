64566b0f6d2b5cbbaab149fdd3d9c64bf0d5e2fe
Anders Logg <logg@tti-c.org>
2006-03-13 15:29:42 -0500

    Update mesh demo: c.__deref()__ not needed

--- src/demo/scripting/pydolfin/meshdemo.py
+++ src/demo/scripting/pydolfin/meshdemo.py
@@ -1,18 +1,17 @@
 import dolfin
-mesh1 = dolfin.Mesh("cow05b.xml.gz")
-vi = dolfin.VertexIterator(mesh1)
-while not vi.end():
-    vertex = vi.__deref__()
+
+mesh = dolfin.Mesh("cow05b.xml.gz")
+
+vertex = dolfin.VertexIterator(mesh)
+while not vertex.end():
     print "vertex(" + str(vertex.id()) + "): ",
     print "x: " + str(vertex.coord().x),
     print "y: " + str(vertex.coord().y),
     print "z: " + str(vertex.coord().z)
-    vi.increment()
+    vertex.increment()
 
-ci = dolfin.CellIterator(mesh1)
-while not ci.end():
-    cell = ci.__deref__()
-    print "cell(" + str(cell.id()) + "): ",
+cell = dolfin.CellIterator(mesh)
+while not cell.end():
+    print "cell(" + str(cell) + "): ",
     print "h: " + str(cell.diameter())
-    ci.increment()
-
+    cell.increment()
