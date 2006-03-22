f6a361bb226ea22870aa85543d4f9880561d56c0
Anders Logg <logg@tti-c.org>
2006-03-22 11:35:00 -0500

    Update convection-diffusion demo:
     - Fix bug in boundary conditions for pressure (same as in Stokes demos)
     - Use direct solver to speed up computation

--- src/demo/pde/convection-diffusion/main.cpp
+++ src/demo/pde/convection-diffusion/main.cpp
@@ -2,7 +2,7 @@
 // Licensed under the GNU GPL Version 2.
 //
 // First added:  2006-02-09
-// Last changed: 2006-03-01
+// Last changed: 2006-03-22
 //
 // This demo first computes the flow around a dolphin by solving
 // the Stokes equations using a mixed formulation with Taylor-Hood
@@ -95,25 +95,28 @@ int main()
     void eval(BoundaryValue& value, const Point& p, unsigned int i)
     {
       // Pressure boundary condition, zero pressure at one point
-      if ( i == 2 && p.x < DOLFIN_EPS && p.y < DOLFIN_EPS )
+      if ( i == 2 )
       {
-	value = 0.0;
-	return;
+        if ( p.x < DOLFIN_EPS && p.y < DOLFIN_EPS )
+        {
+          value = 0.0;
+        }
+        return;
       }
       
       // Velocity boundary condition at inflow
       if ( p.x > (1.0 - DOLFIN_EPS) )
       {
-	if ( i == 0 )
-	  value = -1.0;
-	else
-	  value = 0.0;
-	return;
+        if ( i == 0 )
+          value = -1.0;
+        else
+          value = 0.0;
+        return;
       }
       
       // Velocity boundary condition at remaining boundary (excluding outflow)
       if ( p.x > DOLFIN_EPS )
-	value = 0.0;
+        value = 0.0;
     }
   } bc;
 
@@ -127,6 +130,7 @@ int main()
   // Compute solution
   Function U;
   Function P;
+  pde.set("solver", "direct");
   pde.solve(U, P);
 
   // Save solution to file
