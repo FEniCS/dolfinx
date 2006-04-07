0b206b2caa04e463312e41f82caaa2be86337ce1
Johan Jansson <johanjan@math.chalmers.se>
2006-04-07 17:57:39 +0200

    PyDOLFIN:
    Add option "--includes" to dolfin-config for only returning the
    include paths.
    
    Workaround for uBlas/PETSc header conflict by reordering includes.

--- src/config/config.sh
+++ src/config/config.sh
@@ -7,6 +7,7 @@ TMPFILE="var.tmp"
 COMPILER=`cat $TMPFILE | grep COMPILER | cut -d'"' -f2`
 LINKER=`cat $TMPFILE | grep LINKER | cut -d'"' -f2`
 CFLAGS=`cat $TMPFILE | grep CFLAGS | cut -d'"' -f2`
+INCLUDES=`cat $TMPFILE | grep INCLUDES | cut -d'"' -f2`
 LIBS=`cat $TMPFILE | grep LIBS | cut -d'"' -f2`
 PYTHON_CPPFLAGS=`cat $TMPFILE | grep PYTHON_CPPFLAGS | cut -d'"' -f2`
 PACKAGE=`cat $TMPFILE | grep PACKAGE | cut -d'"' -f2`
@@ -21,6 +22,7 @@ cd src/config
 # Set variables
 CFLAGS_SYSTEM="-I$PREFIX/include $CFLAGS"
 LIBS_SYSTEM="-L$PREFIX/lib $LIBS"
+INCLUDES_SYSTEM="-I$PREFIX/include $INCLUDES"
 FILE="./dolfin-config"
 TEMPLATE="./dolfin-config.template"
 
@@ -33,6 +35,7 @@ echo >> $FILE
 echo COMPILER=\"$COMPILER\" >> $FILE
 echo LINKER=\"$LINKER\" >> $FILE
 echo CFLAGS=\"$CFLAGS_SYSTEM\" >> $FILE
+echo INCLUDES=\"$INCLUDES_SYSTEM\" >> $FILE
 echo LIBS=\"$LIBS_SYSTEM\" >> $FILE
 echo SWIGCFLAGS=\"$PYTHON_CPPFLAGS\" >> $FILE
 echo PACKAGE=\"$PACKAGE\" >> $FILE
