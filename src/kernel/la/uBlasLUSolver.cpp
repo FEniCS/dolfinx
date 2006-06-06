e8cb34475884e8c65482477bbe8a0a97925d9515
Garth N. Wells <g.n.wells@tudelft.nl>
2006-06-06 20:04:48 +0200

    Add uBlasLUSolver.

---
+++ src/kernel/la/uBlasLUSolver.cpp
@@ -0,0 +1,22 @@
+// Copyright (C) 2006 Garth N. Wells.
+// Licensed under the GNU GPL Version 2.
+//
+// First added:  2006-06-01
+// Last changed:
+
+#include <dolfin/dolfin_log.h>
+#include <dolfin/uBlasLUSolver.h>
+
+using namespace dolfin;
+
+//-----------------------------------------------------------------------------
+uBlasLUSolver::uBlasLUSolver() : LinearSolver()
+{
+  // Do nothing
+}
+//-----------------------------------------------------------------------------
+uBlasLUSolver::~uBlasLUSolver()
+{
+  // Do nothing
+}
+//-----------------------------------------------------------------------------
