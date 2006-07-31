4782f6ee321f5c4f80a3975cb5299b336c2b21b3
Johan Jansson <johanjan@math.chalmers.se>
2006-07-31 09:39:49 +0200

    Updated elasticity:
    Clean up of demo.

--- src/demo/solvers/elasticity-updated/main.cpp
+++ src/demo/solvers/elasticity-updated/main.cpp
@@ -28,7 +28,7 @@ class Source : public Function
   real eval(const Point& p, unsigned int i)
   {
     if(i == 1)
-      return 0.0;
+      return -10.0 * 1e3;
     else
       return 0.0;
   }
@@ -41,30 +41,6 @@ class InitialVelocity : public Function
   {
     real result = 0.0;
 
-    Point w, r, v, center;
-
-    center.x = 1.0;
-    center.y = 1.0;
-    center.z = 0.0;
-
-    r = p - center;
-
-    w.x = 0.0;
-    w.y = 0.0;
-    w.z = 2.0;
-
-    v = w.cross(r);
-
-    if(i == 0)
-    {
-      result += v.x;
-    }
-    if(i == 1)
-    {
-      result += v.y;
-    }
-
-
     return result;
   }
 };
@@ -72,26 +48,24 @@ class InitialVelocity : public Function
 // Boundary condition
 class MyBC : public BoundaryCondition
 {
-  //  void eval(BoundaryValue& value, const Point& p, unsigned int i)
-  //   {
-  //     if(p.x == 0.0 && p.y == 0.0)
-  //       value = 0.0;
-  //   }
-
-  void eval(BoundaryValue& value, const Point& p, unsigned int i) {}
+  void eval(BoundaryValue& value, const Point& p, unsigned int i)
+  {
+    if(p.x == 0.0)
+      value = 0.0;
+  }
 };
 
 int main(int argc, char **argv)
 {
   dolfin_output("plain text");
 
-  real T = 10.0;  // final time
-  real k = 1.0e-2; // time step
+  real T = 5.0;  // final time
+  real k = 1.0e-3; // time step
 
   set("ODE method", "cg");
   set("ODE order", 1);
 
-  set("ODE save solution", true);
+  set("ODE save solution", false);
   set("ODE solution file name", "primal.py");
   set("ODE number of samples", 400);
 
@@ -99,53 +73,17 @@ int main(int argc, char **argv)
   set("ODE initial time step", k);
   set("ODE maximum time step", k);
   set("ODE maximum iterations", 100);
-//   set("solver", "newton");
 
+  Mesh mesh("tetmesh-1c.xml.gz");
 
-
-  //Mesh mesh("cow01.xml.gz");
-  //Mesh mesh("cow07.xml.gz");
-  //Mesh mesh("mymesh01.xml.gz");
-//   Mesh mesh("minimal2.xml.gz");
-
-  Mesh mesh("roterror01.xml.gz");
-//   UnitCube mesh(7, 7, 7);
-
-//   for (VertexIterator n(&mesh); !n.end();)
-//   {
-//     Vertex &cvertex = *n;
-//     ++n;
-
-//     if(cvertex.coord().y > 10.0)
-//     {
-//       mesh.remove(cvertex);
-//     }
-//   }
-
-//   mesh.init();
-
-//   File outfile("unitcube-07.xml.gz");
-//   outfile << mesh;
-  
   Source f;
   Density rho;
   InitialVelocity v0;
   MyBC bc;
 
-//   real T = 0.1;  // final time
-//   real k = 0.001; // time step
-
-//   real E = 1.0e4; // Young's modulus
-//   real nu = 0.3; // Poisson's ratio
-//   real nuv = 1.0e4; // viscosity
-
-//   real E = 1.0e5; // Young's modulus
-//   real nu = 0.3; // Poisson's ratio
-//   real nuv = 1.0e3; // viscosity
-
-  real E = 5.0e4; // Young's modulus
+  real E = 1.5 * 5.0e5; // Young's modulus
   real nu = 0.3; // Poisson's ratio
-  real nuv = 1.0e2; // viscosity
+  real nuv = 0.0 * 1.0e2; // viscosity
   real nuplast = 0.0; // plastic viscosity
 
   ElasticityUpdatedSolver::solve(mesh, f, v0, rho, E, nu, nuv,
