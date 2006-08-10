c88315734e38c758a7aa62daf7e7416435288f33
Garth N. Wells <g.n.wells@tudelft.nl>
2006-08-10 14:44:42 +0200

    Add function to SettingsGlue.cpp to allow linking with libraries that contain FORTRAN code.

--- src/pydolfin/SettingsGlue.cpp
+++ src/pydolfin/SettingsGlue.cpp
@@ -3,6 +3,14 @@
 #include <dolfin.h>
 #include "SettingsGlue.h"
 
+
+// This function is required when linking to libraries which contain 
+// FORTRAN code
+extern "C"
+{
+  int MAIN__()  { return(0); }
+}
+
 void glueset(std::string name, dolfin::real val)
 {
   dolfin::set(name, val);
