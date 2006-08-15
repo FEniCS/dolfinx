e542be9a4013540a264d43ab14ecc8074aa02043
Garth N. Wells <g.n.wells@tudelft.nl>
2006-08-15 21:41:42 +0200

    Add unit tests for set() and get() for uBlas Vectors.

--- src/test/la/algebraUBlas.py
+++ src/test/la/algebraUBlas.py
@@ -8,11 +8,34 @@ __license__  = "GNU GPL Version 2"
 import unittest
 from dolfin import *
 
+class SetGetVectors(unittest.TestCase):
+
+    def testUBlasVectorSet(self):
+        """Set values of uBlas"""
+        x = uBlasVector(3)
+        x.set(0,1)
+        x.set(1,1)
+        x.set(2,1)
+#       Can we get DOLFIN_EPS?
+        self.assertTrue( abs(x.norm()-3.0**(0.5) ) < 1.0e-8 )
+
+    def testUBlasVectorGet(self):
+        """Set values of uBlas"""
+        x = uBlasVector(3)
+        x.set(0, 1)
+        x.set(1, 1)
+        x.set(2, 1)
+        sumx = x.get(0) + x.get(1) + x.get(2)
+        self.assertTrue( abs(sumx-3.0 ) < 1.0e-8 )
+
 #class AddVectors(unittest.TestCase):
 #
 #    def testUBlasVector(self):
 #        """Add uBlas vectors"""
 #        x = uBlasVector(10)
+#        x.disp()
+#        x.set(1, 1)
+#        x.disp()
 #        y = uBlasVector(10)
 #        z = uBlasVector(10)
 #        x(0) = 1.0
@@ -20,6 +43,7 @@ from dolfin import *
 #        z(0) = 1.0
 #        z(1) = 1.0
 #        self.assertEqual(x+y, z)
+
 
 #class AddMatrices(unittest.TestCase):
 #
