ae94ed5ee0bc08047897a1f5bab7a5269b069716
Garth N. Wells <g.n.wells@tudelft.nl>
2006-08-20 18:24:46 +0200

    -Fix compile error when PETSC_DIR is not specified.
    -Remove fix functions in dolfin_glue.cpp which were needed to avoid some linking errors.

--- src/pydolfin/dolfin_glue.cpp
+++ src/pydolfin/dolfin_glue.cpp
@@ -3,21 +3,6 @@
 #include <dolfin.h>
 #include "dolfin_glue.h"
 
-// This function is required when linking to libraries which contain 
-// FORTRAN code
-extern "C"
-{
-  int MAIN__()  { return(0); }
-}
-
-// These functions are a fix to avoid undefined symbol errors
-extern "C"
-{
-  int umfpack_di_transpose()  { return(0); }
-  int umfpack_di_solve()  { return(0); }
-}
-
-
 void glueset(std::string name, dolfin::real val)
 {
   dolfin::set(name, val);
