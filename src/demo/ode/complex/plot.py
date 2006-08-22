c3ff2f5c3fc9a974fd025a6ce0cd328238a00756
Anders Logg <logg@simula.no>
2006-08-22 10:32:38 +0200

    Add plot.py script for Lorenz demo.

--- src/demo/ode/complex/plot.py
+++ src/demo/ode/complex/plot.py
@@ -1,14 +1,12 @@
-# Import solution
-from solution import *
-
 # Import matplotlib
 from pylab import *
+
+# Import solution
+from solution import *
 
 # Plot solution
 plot(t, u[:,0], t, u[:,1])
 xlabel('t')
 ylabel('U(t)')
 title('Complex exponential')
-
-# Show plot
 show()
