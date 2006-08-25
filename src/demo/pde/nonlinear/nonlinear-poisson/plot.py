e74c57f990cd7c68e1b542d97c0b50c99c1e98c5
Garth N. Wells <g.n.wells@tudelft.nl>
2006-08-25 10:24:15 +0200

    Add plot scripts for nonlinear demos.

--- src/demo/pde/nonlinear/nonlinear-poisson/plot.py
+++ src/demo/pde/nonlinear/nonlinear-poisson/plot.py
@@ -3,8 +3,8 @@ from mayavi import *
 
 # Plot solution
 v = mayavi()
-d = v.open_vtk_xml("nonlinear_poisson000000.vtu")
-m = v.load_module("BandedSurfaceMap")
+d = v.open_vtk_xml("nonlinear_poisson000000.vtu", 0)
+m = v.load_module("BandedSurfaceMap", 0)
 
 # Wait until window is closed
 v.master.wait_window()
