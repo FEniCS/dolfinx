6c5b2eef2ed6626fb595a667185d5c5f3532b491
Anders Logg <logg@simula.no>
2006-10-09 13:28:12 +0200

    Add notice that operator++ and number() should be removed from class Variable.

--- src/kernel/common/Variable.cpp
+++ src/kernel/common/Variable.cpp
@@ -1,8 +1,8 @@
-// Copyright (C) 2003-2005 Anders Logg.
+// Copyright (C) 2003-2006 Anders Logg.
 // Licensed under the GNU GPL Version 2.
 //
 // First added:  2003-02-26
-// Last changed: 2005
+// Last changed: 2006-10-09
 
 #include <dolfin/Variable.h>
 
@@ -10,21 +10,21 @@ using namespace dolfin;
 
 //-----------------------------------------------------------------------------
 Variable::Variable() :
-  _name("x"), _label("data with no label")
+  _number(0), _name("x"), _label("data with no label")
 {
-  _number = 0;
+  // Do nothing
 }
 //-----------------------------------------------------------------------------
 Variable::Variable(const std::string name, const std::string label) :
-  _name(name), _label(label)
+  _number(0), _name(name), _label(label)
 {
-  _number = 0;
+  // Do nothing
 }
 //-----------------------------------------------------------------------------
 Variable::Variable(const Variable& variable) :
-  _name(variable._name), _label(variable._label)
+  _number(0), _name(variable._name), _label(variable._label)
 {
-  _number = 0;
+  // Do nothing
 }
 //-----------------------------------------------------------------------------
 void Variable::rename(const std::string name, const std::string label)
@@ -41,15 +41,5 @@ const std::string& Variable::name() const
 const std::string& Variable::label() const
 {
   return _label;
-}
-//-----------------------------------------------------------------------------
-void Variable::operator++()
-{
-  _number++;
-}
-//-----------------------------------------------------------------------------
-int Variable::number() const
-{
-  return _number;
 }
 //-----------------------------------------------------------------------------
--- src/kernel/common/dolfin/Variable.h
+++ src/kernel/common/dolfin/Variable.h
@@ -1,8 +1,8 @@
-// Copyright (C) 2003-2005 Anders Logg.
+// Copyright (C) 2003-2006 Anders Logg.
 // Licensed under the GNU GPL Version 2.
 //
 // First added:  2003-02-26
-// Last changed: 2005
+// Last changed: 2006-10-09
 
 #ifndef __VARIABLE_H
 #define __VARIABLE_H
@@ -25,16 +25,15 @@ namespace dolfin
     const std::string& name()  const;
     const std::string& label() const;
 
-    void operator++();
-    int number() const;
-    
+    // FIXME: Remove these
+    void operator++() { _number++; }
+    int number() const { return _number; }
+    int _number;
+
   private:
     
     std::string _name;
     std::string _label;
-
-    // Number of times variable has been saved to file
-    int _number;
     
   };
   
