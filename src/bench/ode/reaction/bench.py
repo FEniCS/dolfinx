cfa026ad59b09f38a0f06cb12d3611c7f3d8c1a2
Anders Logg <logg@simula.no>
2006-10-26 14:12:44 +0200

    Fixes for benchmarks. All tests run ok now except for the fem benchmark in src/bench/fem/convergence which gives a segmentation fault. Need to track this down before we make the new release.

--- src/bench/ode/reaction/bench.py
+++ src/bench/ode/reaction/bench.py
@@ -17,7 +17,7 @@ system("./dolfin-ode-reaction mcg fixed-point 1e-3 2000 1.0 parameters-bench.xml
 toc = time() - tic
 
 # Save timing to log file
-system("dolfin-config --version >> " + logfile)
+system("pkg-config --modversion dolfin >> " + logfile)
 system("date +'%Y-%m-%d %H:%M:%S' >> " + logfile)
 system("uname -snm >> " + logfile)
 file = open(logfile, "a")
