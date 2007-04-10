0b77822c5a158285e386809427c4263a5a095fa2
Anders Logg <logg@simula.no>
2007-04-10 23:41:19 +0200

    Implement boundary conditions:  - Application implemented in NewBoundaryCondition::apply()  - New class SubDomain makes definition of sub domains easy  - Solution vector looks correct for Poisson demo!
    Next we need to fix the export to vertex values to plot
    the solution. Also need to update the PDE classes.

---
+++ src/kernel/mesh/dolfin/SubDomain.h
@@ -0,0 +1,36 @@
+// Copyright (C) 2007 Anders Logg.
+// Licensed under the GNU GPL Version 2.
+//
+// First added:  2007-04-10
+// Last changed: 2007-04-10
+
+#ifndef __SUB_DOMAIN_H
+#define __SUB_DOMAIN_H
+
+#include <dolfin/constants.h>
+
+namespace dolfin
+{
+
+  /// This class defines the interface for definition of sub domains.
+  /// Alternatively, sub domains may be defined by a Mesh and a
+  /// MeshFunction<uint> over the mesh.
+
+  class SubDomain
+  {
+  public:
+
+    /// Constructor
+    SubDomain() {}
+
+    /// Destructor
+    virtual ~SubDomain() {}
+
+    /// Return true for points inside the sub domain
+    virtual bool inside(const real* x, bool on_boundary) = 0;
+
+  };
+
+}
+
+#endif
