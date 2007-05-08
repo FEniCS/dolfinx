5b09a73e652b339885ca571cae631bd8d5cbe13d
Anders Logg <logg@simula.no>
2007-05-08 22:10:10 +0200

    Add mesh partitioning demo

---
+++ src/demo/mesh/partitioning/main.cpp
@@ -0,0 +1,27 @@
+// Copyright (C) 2007 Anders Logg and Magnus Vikström.
+// Licensed under the GNU GPL Version 2.
+//
+// First added:  2007-05-08
+// Last changed: 2007-05-08
+
+#include <dolfin.h>
+
+using namespace dolfin;
+
+int main()
+{
+  // Create mesh
+  UnitSquare mesh(8, 8);
+
+  // FIXME: Should work just like this, need to call partitions.init(...)
+  // MeshFunction<unsigned int> partitions;
+
+  // Partition mesh
+  MeshFunction<unsigned int> partitions(mesh, 2);
+  mesh.partition(10, partitions);
+
+  // Plot mesh partition
+  plot(partitions);
+
+  return 0;
+}
