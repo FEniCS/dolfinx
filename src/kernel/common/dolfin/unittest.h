7b5135438d2f536a911495a57a206360358f8656
Anders Logg <logg@simula.no>
2007-05-14 12:43:22 +0200

    Add framework for C++ unit tests based on cppunit. See separate post.

---
+++ src/kernel/common/dolfin/unittest.h
@@ -0,0 +1,25 @@
+// Copyright (C) 2007 Anders Logg.
+// Licensed under the GNU LGPL Version 2.1.
+//
+// First added:  2007-05-14
+// Last changed: 2007-05-14
+//
+// Simple macro for DOLFIN unit tests (using cppunit). The macro
+// DOLFIN_TEST removes the need of repeating the same 8 lines of
+// code in every single unit test.
+
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/TestRunner.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/CompilerOutputter.h>
+
+#define DOLFIN_TEST \
+CppUnit::TestResult result; \
+CppUnit::TestResultCollector collected_results; \
+result.addListener(&collected_results); \
+CppUnit::TestRunner runner; \
+runner.addTest(CppUnit::TestFactoryRegistry::getRegistry().makeTest()); \
+runner.run(result); \
+CppUnit::CompilerOutputter outputter(&collected_results, std::cerr); \
+outputter.write ();
---
+++ src/test/mesh/cpp/test.cpp
@@ -0,0 +1,77 @@
+// Copyright (C) 2007 Anders Logg.
+// Licensed under the GNU LGPL Version 2.1.
+//
+// First added:  2007-05-14
+// Last changed: 2007-05-14
+//
+// Unit tests for the mesh library
+
+#include <dolfin.h>
+#include <dolfin/unittest.h>
+
+using namespace dolfin;
+
+class SimpleShapes : public CppUnit::TestFixture
+{
+  CPPUNIT_TEST_SUITE(SimpleShapes);
+  CPPUNIT_TEST(testUnitSquare);
+  CPPUNIT_TEST(testUnitCube);
+  CPPUNIT_TEST_SUITE_END();
+
+public: 
+
+  void testUnitSquare()
+  {
+    // Create mesh of unit square
+    UnitSquare mesh(5, 7);
+    CPPUNIT_ASSERT(mesh.numVertices() == 48);
+    CPPUNIT_ASSERT(mesh.numCells() == 70);
+  }
+   
+  void testUnitCube()
+  {
+    // Create mesh of unit cube
+    UnitCube mesh(5, 7, 9);
+    CPPUNIT_ASSERT(mesh.numVertices() == 480);
+    CPPUNIT_ASSERT(mesh.numCells() == 1890);
+  }
+
+};
+
+class MeshRefinement : public CppUnit::TestFixture
+{
+  CPPUNIT_TEST_SUITE(MeshRefinement);
+  CPPUNIT_TEST(testRefineUnitSquare);
+  CPPUNIT_TEST(testRefineUnitCube);
+  CPPUNIT_TEST_SUITE_END();
+
+public:
+
+  void testRefineUnitSquare()
+  {
+    // Refine mesh of unit square
+    UnitSquare mesh(5, 7);
+    mesh.refine();
+    CPPUNIT_ASSERT(mesh.numVertices() == 165);
+    CPPUNIT_ASSERT(mesh.numCells() == 280);
+  }
+  
+  void testRefineUnitCube()
+  {
+    // Refine mesh of unit cube
+    UnitCube mesh(5, 7, 9);
+    mesh.refine();
+    CPPUNIT_ASSERT(mesh.numVertices() == 3135);
+    CPPUNIT_ASSERT(mesh.numCells() == 15120);
+  }
+
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(SimpleShapes);
+CPPUNIT_TEST_SUITE_REGISTRATION(MeshRefinement);
+
+int main()
+{
+  DOLFIN_TEST
+  return 0;
+}
