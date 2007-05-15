0b0f94000738b60e8d1040c6ba3ba59cfbc750f1
Anders Logg <logg@simula.no>
2007-05-15 13:07:44 +0200

    Change back DOLFIN_LINELENGTH. Should not be necessary to increase it. Using vsnprintf in the log system should take care of this. (The long signatures should now be cropped at the maximum length.)

--- src/kernel/main/dolfin/constants.h
+++ src/kernel/main/dolfin/constants.h
@@ -14,8 +14,7 @@
 #define DOLFIN_EPS           3.0e-16
 #define DOLFIN_SQRT_EPS      1.0e-8
 #define DOLFIN_PI            3.141592653589793238462
-#define DOLFIN_LINELENGTH    4096
-#define DOLFIN_WORDLENGTH    128
+#define DOLFIN_LINELENGTH    256
 #define DOLFIN_TERM_WIDTH    80
 
 // DOLFIN typedefs
