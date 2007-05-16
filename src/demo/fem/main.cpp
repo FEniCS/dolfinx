ae8f4aa21d0bef5065c8057e603959a0617a5e71
Garth N. Wells <g.n.wells@tudelft.nl>
2007-05-16 14:18:17 +0200

    Get src/demo/fem working again.

--- src/demo/fem/main.cpp
+++ src/demo/fem/main.cpp
@@ -1,6 +1,8 @@
 // Copyright (C) 2003-2005 Johan Hoffman and Anders Logg.
 // Licensed under the GNU LGPL Version 2.1.
 //
+// Modified by Garth N. Wells, 2007.
+//
 // Thanks to David Heintz for the reference matrices.
 //
 // This demo program dmonstrates how to create simple finite
@@ -22,18 +24,25 @@ int main()
   MassMatrix M(mesh);
 
   // Create reference matrices
+  real A0_array[4][4];
+  real M0_array[4][4];
+
+  A0_array[0][0] = 1.0/2.0;   A0_array[0][1] =-1.0/6.0;   A0_array[0][2] =-1.0/6.0;   A0_array[0][3] =-1.0/6.0;
+  A0_array[1][0] =-1.0/6.0;   A0_array[1][1] = 1.0/6.0;   A0_array[1][2] = 0.0;       A0_array[1][3] = 0.0;
+  A0_array[2][0] =-1.0/6.0;   A0_array[2][1] = 0.0;       A0_array[2][2] = 1.0/6.0;   A0_array[2][3] = 0.0;
+  A0_array[3][0] =-1.0/6.0;   A0_array[3][1] = 0.0;       A0_array[3][2] = 0.0;       A0_array[3][3] = 1.0/6.0;
+
+  M0_array[0][0] = 1.0/60.0;  M0_array[0][1] = 1.0/120.0; M0_array[0][2] = 1.0/120.0; M0_array[0][3] = 1.0/120.0;
+  M0_array[1][0] = 1.0/120.0; M0_array[1][1] = 1.0/60.0;  M0_array[1][2] = 1.0/120.0; M0_array[1][3] = 1.0/120.0;
+  M0_array[2][0] = 1.0/120.0; M0_array[2][1] = 1.0/120.0; M0_array[2][2] = 1.0/60.0;  M0_array[2][3] = 1.0/120.0;
+  M0_array[3][0] = 1.0/120.0; M0_array[3][1] = 1.0/120.0; M0_array[3][2] = 1.0/120.0; M0_array[3][3] = 1.0/60.0;
+
+  unsigned int position[4] = {0, 1, 2, 3};
+
   Matrix A0(4,4);
   Matrix M0(4,4);
-
-  A0(0,0) =  1.0/2.0;   A0(0,1) = -1.0/6.0;   A0(0,2) = -1.0/6.0;   A0(0,3) = -1.0/6.0;
-  A0(1,0) = -1.0/6.0;   A0(1,1) =  1.0/6.0;   A0(1,2) =  0.0;       A0(1,3) =  0.0;
-  A0(2,0) = -1.0/6.0;   A0(2,1) =  0.0;       A0(2,2) =  1.0/6.0;   A0(2,3) =  0.0;
-  A0(3,0) = -1.0/6.0;   A0(3,1) =  0.0;       A0(3,2) =  0.0;       A0(3,3) =  1.0/6.0;
-
-  M0(0,0) =  1.0/60.0;  M0(0,1) =  1.0/120.0; M0(0,2) =  1.0/120.0; M0(0,3) =  1.0/120.0;
-  M0(1,0) =  1.0/120.0; M0(1,1) =  1.0/60.0;  M0(1,2) =  1.0/120.0; M0(1,3) =  1.0/120.0;
-  M0(2,0) =  1.0/120.0; M0(2,1) =  1.0/120.0; M0(2,2) =  1.0/60.0;  M0(2,3) =  1.0/120.0;
-  M0(3,0) =  1.0/120.0; M0(3,1) =  1.0/120.0; M0(3,2) =  1.0/120.0; M0(3,3) =  1.0/60.0;
+  A0.set(*A0_array, 4, position, 4, position);
+  M0.set(*M0_array, 4, position, 4, position);
 
   // Display matrices
   cout << endl;
