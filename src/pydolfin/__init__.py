ab4a0dc793a64ef60ee10d343f710501820b9d59
Anders Logg <logg@simula.no>
2007-05-29 23:57:15 +0200

    PyDOLFIN cleanups, unmaintained code removed

--- src/pydolfin/__init__.py
+++ src/pydolfin/__init__.py
@@ -1,7 +1,4 @@
 # Import DOLFIN wrapper module as well as additional utility entities
 
 from dolfin import *
-from import_form import *
-from projection import *
-from assemble import *
 from plot import *
