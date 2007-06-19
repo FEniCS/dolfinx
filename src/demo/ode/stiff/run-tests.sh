ee5ea9faad9292da3da0b0f9f152893faef4b92a
Anders Logg <logg@simula.no>
2007-06-20 01:49:20 +0200

    Fix bug in stiff ODE demo

--- src/demo/ode/stiff/run-tests.sh
+++ src/demo/ode/stiff/run-tests.sh
@@ -3,28 +3,28 @@
 # Run all stiff test problems.
 
 echo Running test problem 1
-./dolfin-ode-stiff 1 > log-1
+./demo 1 > log-1
 
 echo Running test problem 2
-./dolfin-ode-stiff 2 > log-2
+./demo 2 > log-2
 
 echo Running test problem 3
-./dolfin-ode-stiff 3 > log-3
+./demo 3 > log-3
 
 echo Running test problem 4
-./dolfin-ode-stiff 4 > log-4
+./demo 4 > log-4
 
 echo Running test problem 5
-./dolfin-ode-stiff 5 > log-5
+./demo 5 > log-5
 
 echo Running test problem 6
-./dolfin-ode-stiff 6 > log-6
+./demo 6 > log-6
 
 echo Running test problem 7
-./dolfin-ode-stiff 7 > log-7
+./demo 7 > log-7
 
 echo Running test problem 8
-./dolfin-ode-stiff 8 > log-8
+./demo 8 > log-8
 
 echo Running test problem 9
-./dolfin-ode-stiff 9 > log-9
+./demo 9 > log-9
