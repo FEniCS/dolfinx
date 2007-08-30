fbc2abbbbb8d6ec63fc5cbc1574d7012a2bd3e7c
Anders Logg <logg@simula.no>
2007-08-30 23:33:22 +0200

    Uncomment plotting in demo

--- src/demo/pde/poisson/python/demo.py
+++ src/demo/pde/poisson/python/demo.py
@@ -64,7 +64,7 @@ bc = DirichletBC(u0, mesh, boundary)
 # Solve PDE and plot solution
 pde = LinearPDE(a, L, mesh, bc)
 u = pde.solve()
-#plot(u)
+plot(u)
 
 # Save solution to file
 file = File("poisson.pvd")
