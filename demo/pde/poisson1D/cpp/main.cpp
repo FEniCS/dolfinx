89538ae48d45a40611f185aeeaee49069c046988
Anders Logg <logg@simula.no>
2008-04-28 13:18:08 +0200

    Activate plotting in Poisson 1D demo and fix demo so both C++ and Python return the same solution.

--- demo/pde/poisson1D/cpp/main.cpp
+++ demo/pde/poisson1D/cpp/main.cpp
@@ -2,19 +2,20 @@
 // Licensed under the GNU LGPL Version 2.1.
 //
 // First added:  2007-11-23
-// Last changed: 2007-11-23
+// Last changed: 2008-04-28
 //
 // This demo program solves Poisson's equation
 //
 //     - div grad u(x) = f(x)
 //
-// on the unit square with source f given by
+// on the unit interval with source f given by
 //
-//     f(x) = 9.0*DOLFIN_PI * DOLFIN_PI * sin(3.0*DOLFIN_PI*x[0]);
+//     f(x) = 9.0*DOLFIN_PI*DOLFIN_PI*sin(3.0*DOLFIN_PI*x[0]);
 //
 // and boundary conditions given by
 //
-//     u(x) = 0  for x = 0 and x = 1
+//     u(x) = 0 for x = 0
+//    du/dx = 0 for x = 1
 
 #include <dolfin.h>
 #include "Poisson.h"
@@ -39,21 +40,9 @@ public:
 
   real eval(const real* x) const
   {
-      return 9.0*DOLFIN_PI * DOLFIN_PI * sin(3.0*DOLFIN_PI*x[0]);
+      return 9.0*DOLFIN_PI*DOLFIN_PI*sin(3.0*DOLFIN_PI*x[0]);
   }
-};
 
-// Neumann boundary condition
-class Flux : public Function
-{
-public:
-
-  Flux(Mesh& mesh) : Function(mesh) {}
-
-  real eval(const real* x) const
-  {
-      return 0.0;
-  }
 };
 
 int main()
@@ -66,25 +55,24 @@ int main()
   DirichletBoundary boundary;
   DirichletBC bc(zero, mesh, boundary);
 
-  // Create source and flux
+  // Create source
   Source f(mesh);
-  Flux g(mesh);
 
   // Define PDE
   PoissonBilinearForm a;
-  PoissonLinearForm L(f, g);
+  PoissonLinearForm L(f);
   LinearPDE pde(a, L, mesh, bc);
 
   // Solve PDE
   Function u;
   pde.solve(u);
 
-  // Plot solution (Viper can't plot in 1D yet)
-  //plot(u);
-
   // Save solution to file
   File file_u("poisson.pvd");
   file_u << u;
+
+  // Plot solution
+  plot(u);
 
   return 0;
 }
