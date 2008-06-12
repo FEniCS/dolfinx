72b7d417f7c97c9e30ea6d795ec7ce277f592d57
Ilmar Wilbers <ilmarw@simula.no>
2008-06-12 19:12:17 +0200

    Minor fixes assembly bench.

--- bench/fem/assembly/run_benchmark.py
+++ bench/fem/assembly/run_benchmark.py
@@ -3,6 +3,10 @@ __date__ = "2008-06-04"
 __copyright__ = "Copyright (C) 2007 Ilmar Wilbers"
 __license__  = "GNU LGPL Version 2.1"
 
+# Be careful to first run the benchmark once in order for
+# the Python extensions to be created, or else the timings
+# will not be correct.
+
 from dolfin import *
 from time import time
 import sys
