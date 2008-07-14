f73c2c826609705ac2f1078680439de1920fd6f9
Anders Logg <logg@simula.no>
2008-07-14 23:08:34 +0200

    Speed up Lorenz demo

--- demo/ode/lorenz/cpp/main.cpp
+++ demo/ode/lorenz/cpp/main.cpp
@@ -1,10 +1,10 @@
-// Copyright (C) 2003-2007 Anders Logg.
+// Copyright (C) 2003-2008 Anders Logg.
 // Licensed under the GNU LGPL Version 2.1.
 //
-// Modified by Benjamin Kehlet 2008
+// Modified by Benjamin Kehlet, 2008.
 //
 // First added:  2003-07-02
-// Last changed: 2008-06-17
+// Last changed: 2008-07-14
 
 #include <stdio.h>
 #include <dolfin.h>
@@ -65,12 +65,10 @@ int main()
   dolfin_set("ODE number of samples", 500);
   dolfin_set("ODE initial time step", 0.01);
   dolfin_set("ODE fixed time step", true);
-  //dolfin_set("ODE nonlinear solver", "newton");
   dolfin_set("ODE method", "cg");
   dolfin_set("ODE order", 5);
   dolfin_set("ODE discrete tolerance", 1e-10);
   dolfin_set("ODE save solution", true);
-  dolfin_set("ODE solve dual problem", true);
  
   Lorenz lorenz;
   lorenz.solve();
