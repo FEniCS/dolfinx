29f23fe538693231dfcbf62f9f7d850e271c2490
Anders Logg <logg@simula.no>
2008-07-20 23:58:32 +0200

    Add class Table for pretty-printing of tables (for example assembly benchmark results). Example usage:
      Table table;
    
      table("uBLAS",  "Assemble") = 0.010;
      table("uBLAS",  "Solve")    = 0.020;
      table("PETSc",  "Assemble") = 0.011;
      table("PETSc",  "Solve")    = 0.019;
      table("Epetra", "Assemble") = 0.012;
      table("Epetra", "Solve")    = 0.018;
    
      table.disp();
    
    Gives the following output:
    
              |   Init  Assemble
      --------------------------
      uBLAS   |   0.01      0.02
      PETSc   |  0.011     0.019
      Epetra  |  0.012     0.018

---
+++ dolfin/log/Table.h
@@ -0,0 +1,105 @@
+// Copyright (C) 2008 Anders Logg.
+// Licensed under the GNU LGPL Version 2.1.
+//
+// First added:  2008-07-19
+// Last changed: 2008-07-19
+
+#ifndef __TABLE_H
+#define __TABLE_H
+
+#include <vector>
+#include <set>
+#include <map>
+
+#include <dolfin/common/types.h>
+
+namespace dolfin
+{
+
+  class TableEntry;
+
+  /// This class provides storage and pretty-printing for tables.
+  /// Example usage:
+  ///
+  ///   Table table;
+  ///
+  ///   table("uBLAS",  "Assemble") = 0.010;
+  ///   table("uBLAS",  "Solve")    = 0.020;
+  ///   table("PETSc",  "Assemble") = 0.011;
+  ///   table("PETSc",  "Solve")    = 0.019;
+  ///   table("Epetra", "Assemble") = 0.012;
+  ///   table("Epetra", "Solve")    = 0.018;
+  ///
+  ///   table.disp();
+
+  class Table
+  {
+  public:
+
+    /// Create empty table
+    Table();
+
+    /// Destructor
+    ~Table();
+
+    /// Return table entry
+    TableEntry operator() (std::string row, std::string col);
+
+    /// Get value of table entry
+    real get(std::string row, std::string col) const;
+
+    /// Set value of table entry
+    void set(std::string row, std::string col, real value);
+
+    /// Display table
+    void disp() const;
+
+  private:
+
+    // Rows
+    std::vector<std::string> rows;
+    std::set<std::string> row_set;
+
+    // Columns
+    std::vector<std::string> cols;
+    std::set<std::string> col_set;
+
+    // Table values
+    std::map<std::pair<std::string, std::string>, real> values;
+
+  };
+
+  /// This class represents an entry in a Table
+
+  class TableEntry
+  {
+  public:
+
+    /// Create table entry
+    TableEntry(std::string row, std::string col, Table& table);
+
+    /// Destructor
+    ~TableEntry();
+
+    /// Assign value to table entry
+    const TableEntry& operator= (real value);
+
+    /// Cast to entry value
+    operator real() const;
+
+  private:
+
+    // Row
+    std::string row;
+
+    // Column
+    std::string col;
+
+    // Table
+    Table& table;
+
+  };
+
+}
+
+#endif
