3e0eb4a5c60322ca87ea01f7b99dd44741065e7a
Anders Logg <logg@simula.no>
2008-08-12 17:47:05 +0200

    Initial work on building parallel dof map

--- dolfin/fem/DofMap.h
+++ dolfin/fem/DofMap.h
@@ -19,7 +19,8 @@
 
 namespace dolfin
 {
-  class SubSytem;
+
+  class SubSystem;
   class UFC;
 
   /// This class handles the mapping of degrees of freedom.
@@ -61,7 +62,7 @@ namespace dolfin
     
     /// Return the dimension of the global finite element function space
     unsigned int global_dimension() const
-      { return ufc_dof_map->global_dimension(); }
+    { return ufc_dof_map->global_dimension(); }
 
     /// Return the dimension of the local finite element function space
     unsigned int local_dimension() const
@@ -85,37 +86,40 @@ namespace dolfin
     // FIXME: Can this function eventually be removed?
     /// Tabulate the local-to-global mapping of dofs on a ufc cell
     void tabulate_dofs(uint* dofs, const ufc::cell& cell) const 
-      { ufc_dof_map->tabulate_dofs(dofs, ufc_mesh, cell); }
+    { ufc_dof_map->tabulate_dofs(dofs, ufc_mesh, cell); }
 
     void tabulate_coordinates(real** coordinates, const ufc::cell& ufc_cell) const
-      { ufc_dof_map->tabulate_coordinates(coordinates, ufc_cell); }
+    { ufc_dof_map->tabulate_coordinates(coordinates, ufc_cell); }
 
     /// Extract sub dof map
     DofMap* extractDofMap(const Array<uint>& sub_system, uint& offset) const;
 
     /// Return mesh associated with map
     Mesh& mesh() const
-      { return dolfin_mesh; }
+    { return dolfin_mesh; }
 
     /// Build parallel dof map
     void build(UFC& ufc);
-
+    
     /// Return renumbering (used for testing)
     std::map<uint, uint> getMap() const;
-
+    
     /// Display mapping
     void disp() const;
-
+   
   private:
+
+    /// Friends
+    friend class DofMapBuilder;
 
     /// Initialise DofMap
     void init();
-
+    
     /// Extract sub DofMap
     ufc::dof_map* extractDofMap(const ufc::dof_map& dof_map, uint& offset, const Array<uint>& sub_system) const;
 
-    // Parallel dof map
-    uint** dof_map;
+    // Precomputed dof map
+    uint* dof_map;
 
     // UFC dof map
     ufc::dof_map* ufc_dof_map;
---
+++ dolfin/fem/DofMapBuilder.cpp
@@ -0,0 +1,85 @@
+// Copyright (C) 2008 Anders Logg and Ola Skavhaug.
+// Licensed under the GNU LGPL Version 2.1.
+//
+// First added:  2008-08-12
+// Last changed: 2008-08-12
+
+#include <dolfin/log/log.h>
+#include <dolfin/mesh/Mesh.h>
+#include <dolfin/mesh/MeshData.h>
+#include <dolfin/main/MPI.h>
+#include "UFC.h"
+#include "DofMap.h"
+#include "DofMapBuilder.h"
+
+using namespace dolfin;
+
+//-----------------------------------------------------------------------------
+void DofMapBuilder::build(DofMap& dof_map, UFC& ufc, Mesh& mesh)
+{
+  // Work in progress, to be based on Algorithm 5 in the paper
+  // http://home.simula.no/~logg/pub/papers/submitted-Log2008a.pdf
+
+  message("Building parallel dof map (in parallel)");
+
+  // Check that dof map has not been built
+  if (dof_map.dof_map)
+    error("Local-to-global mapping has already been computed.");
+
+  // Allocate dof map
+  const uint n = dof_map.local_dimension();
+  dof_map.dof_map = new uint[n*mesh.numCells()];
+  
+  error("Not implemented.");
+
+  // Get mesh functions
+  MeshFunction<uint>* S = mesh.data().meshFunction("subdomain overlap");
+  MeshFunction<uint>* F = mesh.data().meshFunction("facet overlap");
+  dolfin_assert(S);
+  dolfin_assert(F);
+
+  // Get number of this process
+  const uint this_process = MPI::processNumber();
+  message("Building dof map on processor %d.", this_process);
+
+  // Build stage 0: Compute offsets
+  computeOffsets(this_process);
+  
+  // Build stage 0.5: Communicate offsets
+  communicateOffsets();
+  
+  // Build stage 1: Compute mapping on shared facets
+  computeShared();
+  
+  // Build stage 2: Communicate mapping on shared facets
+  communicateShared();
+  
+  // Build stage 3: Compute mapping for interior degrees of freedom
+  computeInterior();
+}
+//-----------------------------------------------------------------------------
+void DofMapBuilder::computeOffsets(uint this_process)
+{
+
+}
+//-----------------------------------------------------------------------------
+void DofMapBuilder::communicateOffsets()
+{
+
+}
+//-----------------------------------------------------------------------------
+void DofMapBuilder::computeShared()
+{
+
+}
+//-----------------------------------------------------------------------------
+void DofMapBuilder::communicateShared()
+{
+
+}
+//-----------------------------------------------------------------------------
+void DofMapBuilder::computeInterior()
+{
+
+}
+//-----------------------------------------------------------------------------
