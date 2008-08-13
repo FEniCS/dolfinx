92f307c64f8bbd06b6a1ce45ed609f2cab5f29ba
Ola Skavhaug <skavhaug@simula.no>
2008-08-13 16:21:02 +0200

    Added tentusscher ode example.

---
+++ demo/ode/tentusscher/cpp/main.cpp
@@ -0,0 +1,29 @@
+// Copyright (C) Glenn Terje Lines, Ola Skavhaug and Simula Research Laboratory.
+// Licensed under the GNU LGPL Version 2.1.
+//
+// Original code copied from PyCC.
+// Modified by Anders Logg 2006.
+//
+// First added:  2006-05-24
+// Last changed: 2006-08-21
+//
+// This demo solves the Courtemanche model for cardiac excitation.
+
+#include <dolfin.h>
+#include "tentusscher.h"
+
+using namespace dolfin;
+
+int main()
+{
+  dolfin_set("ODE tolerance", 1.0e-5);
+  dolfin_set("ODE maximum time step", 100.0);
+  dolfin_set("ODE nonlinear solver", "newton");
+  dolfin_set("ODE linear solver", "iterative");
+  dolfin_set("ODE initial time step", 0.25);
+
+  tentusscher ode(500);
+  ode.solve();
+
+  return 0;
+}
