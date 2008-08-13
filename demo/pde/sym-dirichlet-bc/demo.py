5d73ca7142660d591048839c5c0b96a3797b8501
Kent-Andre Mardal <kent-and@simula.no>
2008-08-13 11:41:23 +0200

    update text

--- demo/pde/sym-dirichlet-bc/demo.py
+++ demo/pde/sym-dirichlet-bc/demo.py
@@ -1,22 +1,14 @@
-"""This demo program solves Poisson's equation
-
-    - div grad u(x, y) = f(x, y)
-
-on the unit square with source f given by
-
-    f(x, y) = 500*exp(-((x-0.5)^2 + (y-0.5)^2)/0.02)
-
-and boundary conditions given by
-
-    u(x, y)     = 0               for x = 0
-    du/dn(x, y) = 25 sin(5 pi y)  for x = 1
-    du/dn(x, y) = 0               otherwise
+"""This demo demonstrate how to assemble the matrix, the right-hands side 
+   as well as enforcing boundary conditions (in a symmetric way)
+   on the standard Poisson demo
 """
 
-__author__ = "Anders Logg (logg@simula.no)"
-__date__ = "2007-08-16 -- 2008-04-03"
-__copyright__ = "Copyright (C) 2007-2008 Anders Logg"
+__author__ = "Kent-Andre Mardal (kent-and@simula.no)"
+__date__ = "2008-08-13"
+__copyright__ = "Copyright (C) 2008 Kent-Andre Mardal"
 __license__  = "GNU LGPL Version 2.1"
+
+
 
 from dolfin import *
 
