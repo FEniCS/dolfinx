210c798144e6986009648c509c859e6c268b6937
Ola Skavhaug <skavhaug@simula.no>
2008-08-13 11:25:48 +0200

    Initial work on partitioning a stored mesh into several stored meshes with facet meshfunctions data.

--- dolfin/mesh/MeshPartition.h
+++ dolfin/mesh/MeshPartition.h
@@ -2,9 +2,10 @@
 // Licensed under the GNU LGPL Version 2.1.
 //
 // Modified by Anders Logg, 2008.
+// Modified by Ola Skavhaug, 2008.
 //
 // First added:  2007-04-24
-// Last changed: 2008-02-04
+// Last changed: 2008-08-13
 
 #ifndef __MESH_PARTITIONING_H
 #define __MESH_PARTITIONING_H
@@ -21,6 +22,10 @@ namespace dolfin
 
     /// Partition a mesh into num_part partitions
     static void partition(Mesh& mesh, MeshFunction<uint>& partitions, uint num_partitions);
+
+    /// Partition mesh in meshfile into num_partitions meshes with subdomain and facet overlap data attached 
+    /// Outfile names are given running numbering <meshfile>_i.tar.gz for i in [0, num_partitions).
+    static void partition(std::string meshfile, uint num_partitions);
 
   };
 
