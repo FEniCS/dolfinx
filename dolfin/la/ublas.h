30a9b893aee17f028a0dd0e35e00773ea49c3c05
Benjamin Kehlet <benjamik@ifi.uio.no>
2008-09-30 00:56:18 +0200

    Change double to real

--- dolfin/la/ublas.h
+++ dolfin/la/ublas.h
@@ -7,6 +7,8 @@
 #ifndef __UBLAS_H
 #define __UBLAS_H
 
+#include <dolfin/common/types.h>
+
 #include <boost/numeric/ublas/matrix.hpp>
 #include <boost/numeric/ublas/vector.hpp>
 #include <boost/numeric/ublas/vector_of_vector.hpp>
@@ -28,33 +30,33 @@ namespace dolfin
   namespace ublas = boost::numeric::ublas;
 
   // uBLAS vector
-  typedef ublas::vector<double> ublas_vector;
+  typedef ublas::vector<real> ublas_vector;
   typedef ublas::vector_range<ublas_vector> ublas_vector_range;
 
   // uBLAS dense matrix
-  typedef ublas::matrix< double > ublas_dense_matrix;
+  typedef ublas::matrix<real> ublas_dense_matrix;
   typedef ublas::matrix_range<ublas_dense_matrix> ublas_matrix_range;
 
   // uBLAS dense matrix (column major format)
-  typedef ublas::matrix<double, ublas::column_major> ublas_matrix_cmajor;
+  typedef ublas::matrix<real, ublas::column_major> ublas_matrix_cmajor;
   typedef ublas::matrix_range<ublas_matrix_cmajor> ublas_matrix_cmajor_range;
 
   // uBLAS sparse matrix
-  typedef ublas::compressed_matrix<double, ublas::row_major> ublas_sparse_matrix;
+  typedef ublas::compressed_matrix<real, ublas::row_major> ublas_sparse_matrix;
 
   // uBLAS sparse matrix (column major format) 
-  typedef ublas::compressed_matrix<double, ublas::column_major> ublas_sparse_matrix_cmajor;
+  typedef ublas::compressed_matrix<real, ublas::column_major> ublas_sparse_matrix_cmajor;
 
   // uBLAS sparse matrix for temporary assembly
-  typedef ublas::generalized_vector_of_vector< double, ublas::row_major, 
-            ublas::vector<ublas::compressed_vector<double> > > ublas_assembly_matrix;
+  typedef ublas::generalized_vector_of_vector< real, ublas::row_major, 
+            ublas::vector<ublas::compressed_vector<real> > > ublas_assembly_matrix;
 
   // uBLAS sparse matrix for temporary assembly (column major format)
-  typedef ublas::generalized_vector_of_vector< double, ublas::column_major, 
-            ublas::vector<ublas::compressed_vector<double> > > ublas_assembly_matrix_cmajor;
+  typedef ublas::generalized_vector_of_vector< real, ublas::column_major, 
+            ublas::vector<ublas::compressed_vector<real> > > ublas_assembly_matrix_cmajor;
 
   // uBLAS upper triangular matrix (column major format)
-  typedef ublas::triangular_matrix<double, ublas::upper, ublas::column_major> ublas_matrix_cmajor_tri;
+  typedef ublas::triangular_matrix<real, ublas::upper, ublas::column_major> ublas_matrix_cmajor_tri;
   typedef ublas::matrix_range<ublas_matrix_cmajor_tri> ublas_matrix_cmajor_tri_range;
   typedef ublas::matrix_column<ublas_matrix_cmajor_tri> ublas_matrix_cmajor_tri_column;
 
