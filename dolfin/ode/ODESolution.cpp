9f585768958550345fae20fec089b1aecc8a8672
Benjamin Kehlet <benjamik@ifi.uio.no>
2008-10-02 14:43:24 +0200

    Clean up, added comments in ODESolution

--- dolfin/ode/ODESolution.cpp
+++ dolfin/ode/ODESolution.cpp
@@ -15,20 +15,20 @@ using namespace dolfin;
 ODESolution::ODESolution(ODE& ode) : 
   ode(ode),
   filename(tmpnam(0)),
-  file(filename, std::ios::out | std::ios::binary),
-  count(0),
+  file(filename.c_str(), std::ios::out | std::ios::binary),
   bintree(std::vector<real>()),
   step(sizeof(real)*ode.size()),
+  buffercount(0),
   dataondisk(false)
 {
-
   //initalize the cache
   cachesize = ode.get("ODE order");
   std::string m = ode.get("ODE method") ;
   if (m == "dg") ++cachesize;
 
   cache = new std::pair<real, uBLASVector>[cachesize];
-  for (uint i = 0; i < cachesize; ++i) {
+  for (uint i = 0; i < cachesize; ++i) 
+  {
     cache[i].first = -1;
     cache[i].second.resize(ode.size());
   }
@@ -41,74 +41,75 @@ ODESolution::ODESolution(ODE& ode) :
 
 }
 //-----------------------------------------------------------------------------
-ODESolution::~ODESolution() {
+ODESolution::~ODESolution() 
+{
   delete[] cache;
   file.close();
-  remove(filename);
+  remove(filename.c_str());
   free(buffer);
 }
 
-void ODESolution::addSample(Sample& sample) {
-  
+void ODESolution::addSample(Sample& sample) 
+{  
+  cout << "Addsample t = " << sample.t() << endl;
+
   bintree.push_back(sample.t());
   
   //check if there is allocated memory for another entry in the buffer
-  if (step*(count+1) > buffersize) {
-    //memory buffer not big enough
-
+  if (step*(buffercount+1) > buffersize) 
+  {
     //check if we can just extend the allocated memory
-    if (buffersize*2 <= ODESOLUTION_MAX_ALLOC) {
+    if (buffersize*2 <= ODESOLUTION_MAX_ALLOC) 
+    {
       //extend the memory
       buffersize *= 2;
       buffer = (real *) realloc(buffer, buffersize);
-      cout << "ODESolution: Reallocating memory. New size: " << buffersize << endl;
-    } else {
+    } else 
+    {
       // No more memory available. Dump to disk
       cout << "ODESolution: Writing to disk" << endl;
-      file.write((char *) buffer, step*count);
-      bufferoffset += count;
-      count = 0;
+      file.write((char *) buffer, step*buffercount);
+      bufferoffset += buffercount;
+      buffercount = 0;
       dataondisk = true;
     }
   }
 
-  for (uint i = 0; i < sample.size(); ++i) {
-    buffer[count*ode.size()+i] = sample.u(i);
+  for (uint i = 0; i < sample.size(); ++i) 
+  {
+    buffer[buffercount*ode.size()+i] = sample.u(i);
   }
 
-  ++count;
+  ++buffercount;
 }
 //-----------------------------------------------------------------------------
-void ODESolution::makeIndex() {
-
-  if (dataondisk && count > 0) {
+void ODESolution::makeIndex() 
+{
+  if (dataondisk && buffercount > 0) 
+  {
     cout << "ODESolution: Writing last chunk to disk" << endl;
-    file.write((char *) buffer, count*step);
+    file.write((char *) buffer, buffercount*step);
   }
-
-  //cout << "Switching, bufferoffset: " << bufferoffset << ", count: " << count << ", size: " << size << endl;
 
   file.close();
-  file.open(filename, std::ios::in | std::ios::binary);  
+  file.open(filename.c_str(), std::ios::in | std::ios::binary);  
 }
 //-----------------------------------------------------------------------------
-void ODESolution::eval(const real t, uBLASVector& y) {
-
+void ODESolution::eval(const real t, uBLASVector& y) 
+{
   //scan the cache
-  for (uint i = 0; i < cachesize; ++i) {
-    if (cache[i].first < 0) {
-      continue;
-    }
+  for (uint i = 0; i < cachesize; ++i) 
+  {
+    if (cache[i].first < 0) continue;
     
-    if (cache[i].first == t) {
+    if (cache[i].first == t) 
+    {
       //found return cache[i]
-      for (uint j = 0; j < ode.size(); j++) {
+      for (uint j = 0; j < ode.size(); j++) 
+      {
         uBLASVector& c = cache[i].second;
         y[j] = c[j];
       }
-      //std::cout << "t=" << t << " " << std::flush;
-      //printVector(y);
-
       return;
     }
   }
@@ -120,40 +121,30 @@ void ODESolution::eval(const real t, uBLASVector& y) {
   uint b = uint(low-bintree.begin());
   uint a = b-1;
 
-  if (b >= bintree.size()) 
+  if ( b >= bintree.size() || b < 1 ) 
   {
-    b = bintree.size() - 1;
-    a = bintree.size() - 2;
+    error("ODESolution, eval(%g) out of range", t);
   } 
-  else if (b < 1) 
-  {
-    b = 1;
-    a = 0;
-  }
 
   real t_a = bintree[a];
   real t_b = bintree[b];
 
   //check if we need to read from disk
-  if (a < bufferoffset || b > bufferoffset + count) {
-    
+  if (a < bufferoffset || b > bufferoffset + buffercount) 
+  {  
     cout << "ODESolution: Fetching from disk" << endl;
-
-    //printf("t=%f, a=%d \n", t, a);
 
     //put a in the middle of the buffer
     bufferoffset = (uint) std::max((int) (a - buffersize/(step*2)), 0);
-    count = std::min(buffersize/step, bintree.size()-bufferoffset); 
+    buffercount = std::min(buffersize/step, bintree.size()-bufferoffset); 
     file.seekg(bufferoffset*step);
-    file.read( (char *) buffer, count*step);
-
-    //cout << "Bufferoffset: " << bufferoffset << ", count: " << count << endl;
-    //exit(1);
+    file.read( (char *) buffer, buffercount*step);
   }
 
   uBLASVector tmp(ode.size());
   
-  for (unsigned int i = 0; i < ode.size(); i++) {
+  for (unsigned int i = 0; i < ode.size(); i++) 
+  {
     y[i]   = buffer[(a - bufferoffset)*ode.size() + i];
     tmp[i] = buffer[(b - bufferoffset)*ode.size() + i];
   }
@@ -162,32 +153,26 @@ void ODESolution::eval(const real t, uBLASVector& y) {
 
   //cache y
   cache[ringbufcounter].first = t;
-  for (uint i = 0; i < ode.size(); i++) {
+  for (uint i = 0; i < ode.size(); i++) 
+  {
     cache[ringbufcounter].second[i] = y[i];
   }
+
   ringbufcounter = (ringbufcounter+1)%cachesize;
   
-  //std::cout << "t="<<t<<" " << std::flush;
-  //printVector(y);
-
 }
 //-----------------------------------------------------------------------------
 void ODESolution::lerp(const uBLASVector& v1, 
-			      const real t1, 
-			      const uBLASVector& v2, 
-			      const real t2, 
-			      const real t, 
-			      uBLASVector& result) {
+		       const real t1, 
+		       const uBLASVector& v2, 
+		       const real t2, 
+		       const real t, 
+		       uBLASVector& result) 
+{
   real h = t2-t1;
-  for (uint i = 0; i < ode.size(); i++) {
+  for (uint i = 0; i < ode.size(); i++) 
+  {
     result[i] = v1[i] + (t-t1)*((v2[i]-v1[i])/h);
   }
 }
 //-----------------------------------------------------------------------------
-//REMOVE AFTER TESTING
-void ODESolution::printVector(const uBLASVector& u) {
-  for (unsigned int i=0; i < u.size(); i++) {
-    printf("%.15f ", u[i]);
-  }
-  printf("\n");
-}
--- dolfin/ode/ODESolution.h
+++ dolfin/ode/ODESolution.h
@@ -2,7 +2,7 @@
 // Licensed under the GNU LGPL Version 2.1.
 //
 // First added:  2008-06-11
-// Last changed: 2008-06-11
+// Last changed: 2008-10-02
 
 
 #ifndef __ODESOLUTION_H
@@ -10,17 +10,27 @@
 
 #include <iostream>
 #include <fstream>
+#include <vector>
 #include <dolfin/common/types.h>
-#include <dolfin/la/uBLASVector.h>
 
-#define ODESOLUTION_INITIAL_ALLOC 5000
-#define ODESOLUTION_MAX_ALLOC     10000
+#define ODESOLUTION_INITIAL_ALLOC 1000000
+#define ODESOLUTION_MAX_ALLOC     16000000
 
 namespace dolfin
 {
   //forward declarations
   class ODE;
   class Sample;
+  class uBLASVector;
+
+  /// ODESolution stores the samples from the ODE solver, primarily to be able
+  /// to solve the dual problem. To be ble to evaluate the solution in an
+  /// arbitrary point ODESolution makes a simple linear interpolation between the
+  /// the closest samples. A number of interpolated values is cached, since 
+  /// the ODE solver repeatedly requests evaluation of the same t.
+  /// 
+  /// The samples is stored in memory if possible, otherwise stored in a
+  /// temporary file and fetched from disk when needed.
 
   class ODESolution
   {
@@ -29,38 +39,35 @@ namespace dolfin
     ODESolution(ODE& ode);
     ~ODESolution();
 
-    /// Evaluate (interpolate) value og solution at given time    
+    /// Evaluate (interpolate) value of solution at given time    
     void eval(const real t, uBLASVector& y);
     
-    /// for testing
-    void printVector(const uBLASVector& u);
-    
-  private :
-    
+  private :    
     ODE& ode;
-    char *filename;
+    std::string filename;
     std::fstream file;
     std::pair<real, uBLASVector> *cache;
-    uint count; // TODO rename to 'index' or so
     
     uint cachesize;
     uint ringbufcounter;
     
-    //binary tree with mapping from t value to index in file/buffer
+    //sorted vector with pair. Each entry representents a
+    // mapping from t value to index in file/buffer
     std::vector<real> bintree;
     uint step;
     
     real* buffer;
     uint buffersize;
     uint bufferoffset;
+    uint buffercount; //actual number of entries in buffer
     bool dataondisk;
 
     void lerp(const uBLASVector& v1,
-                     const real t1, 
-                     const uBLASVector& v2, 
-                     const real t2, 
-                     const real t, 
-                     uBLASVector& result);
+	      const real t1, 
+	      const uBLASVector& v2, 
+	      const real t2, 
+	      const real t, 
+	      uBLASVector& result);
     
     void addSample(Sample& sample);
     
