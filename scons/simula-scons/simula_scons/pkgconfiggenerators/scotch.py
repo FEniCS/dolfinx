e84eeb5c18885c085378ae9a2cd23bf352c57059
Johannes Ring <johannr@simula.no>
2008-10-15 10:34:57 +0200

    Fixed and restructured SCOTCH pkg-config generator (should now pick up SCOTCH from Intrepid/unstable).

--- scons/simula-scons/simula_scons/pkgconfiggenerators/scotch.py
+++ scons/simula-scons/simula_scons/pkgconfiggenerators/scotch.py
@@ -7,32 +7,59 @@ import commands
 from commonPkgConfigUtils import *
 
 def getScotchDir(sconsEnv=None):
-    default = os.path.join(os.path.sep,"usr","local")
+    default = os.path.join(os.path.sep, "usr")
     scotch_dir = getPackageDir("scotch", sconsEnv=sconsEnv, default=default)
     return scotch_dir
 
-def pkgTests(forceCompiler=None, sconsEnv=None, **kwargs):
-  """Run the tests for this package
-  
-  If Ok, return various variables, if not we will end with an exception.
-  forceCompiler, if set, should be a tuple containing (compiler, linker)
-  or just a string, which in that case will be used as both
-  """
+def pkgVersion(sconsEnv=None):
+    # Try to figure out the SCOTCH version from scotch.h
+    # Fource places to look for scotch.h:
+    # 1. SCOTCH_DIR/scotch.h
+    # 2. SCOTCH_DIR/bin/scotch.h
+    # 3. SCOTCH_DIR/include/scotch.h
+    # 4. SCOTCH_DIR/include/scotch/scotch.h
+    
+    scotch_dir = getScotchDir(sconsEnv=sconsEnv)
 
-  # There are two different ways of installing scotch, as done by scotch just
-  # in one directory, or in subdirs lib, include, etc...
-  # If usealternate is True, the latter is assumed. Default is the former.
-  usealternate = False
+    failure = True
+    include_dirs = ['', 'bin', 'include', os.path.join('include', 'scotch')]
+    for inc_dir in include_dirs:
+        scotch_h_filename = os.path.join(scotch_dir, inc_dir, 'scotch.h')
+        try:
+            scotch_h_file = open(scotch_h_filename, 'r')
+        except Exception:
+            continue
+        failure = False
+        break
 
-  # Don't know of a way to figure out the scotch version, have to investigate 
-  # that later. From what I can tell, scotch itself does not support this.
-  # We will just assume scotch v.4.0
+    if failure:
+        msg = "Unable to locate scotch.h. Is SCOTCH_DIR set correctly?"
+        raise UnableToXXXException(msg)
+    
+    scotch_version = '4.0'  # assume 4.0 as default
+    for line in scotch_h_file:
+        if "Version" in line:
+            # the lines with the version number is something like this:
+            # /**                # Version 4.0  : from : 11 dec 2001     **/
+            tmp = line.split()
+            scotch_version = tmp[tmp.index("Version")+1]
 
-  # It seems that the version number is present in scotch.h (and ptscotch.h).
-  # Maybe we can use that?
-  
-  # Simple test-program that try to include the main scotch header
-  cpp_test_include_str = r"""
+    return scotch_version
+
+def pkgCflags(compiler=None, sconsEnv=None):
+    # Four places to look for scotch.h:
+    # 1. SCOTCH_DIR/scotch.h
+    # 2. SCOTCH_DIR/bin/scotch.h
+    # 3. SCOTCH_DIR/include/scotch.h
+    # 4. SCOTCH_DIR/include/scotch/scotch.h
+
+    if not compiler:
+        compiler = get_compiler(sconsEnv)
+
+    scotch_dir = getScotchDir(sconsEnv=sconsEnv)
+    
+    # Simple test-program that try to include the main scotch header
+    cpp_test_include_str = r"""
 extern "C" {
 #include <stdio.h>
 #include <scotch.h>
@@ -41,9 +68,46 @@ int main() {
   return 0;
 }
 """
+    cpp_file = "scotch_config_test_include.cpp"
+    write_cppfile(cpp_test_include_str, cpp_file)
 
-  # Test that I can call a scotch function
-  cpp_test_lib_str = r"""
+    include_dirs = ['', 'bin', 'include', os.path.join('include', 'scotch')]
+    for inc_dir in include_dirs:
+        scotch_inc_dir = os.path.join(scotch_dir, inc_dir)
+
+        cmdstr = "%s -I%s -c %s" % (compiler, scotch_inc_dir, cpp_file)
+        compileFailed, cmdoutput = getstatusoutput(cmdstr)
+        if not compileFailed:
+            break
+
+    if compileFailed:
+        remove_cppfile(cpp_file)
+        raise UnableToCompileException("SCOTCH", cmd=cmdstr,
+                                       program=cpp_test_include_str,
+                                       errormsg=cmdoutput)
+
+    remove_cppfile(cpp_file, ofile=True)
+
+    return "-I%s" % scotch_inc_dir
+
+def pkgLibs(compiler=None, linker=None, cflags=None, sconsEnv=None):
+    # Three cases for libscotch.a:
+    # 1. SCOTCH_DIR/libscotch.a
+    # 2. SCOTCH_DIR/bin/libscotch.a
+    # 3. SCOTCH_DIR/lib/libscotch.a
+    # FIXME: should we look for libscotch.so too?
+    
+    if not compiler:
+        compiler = get_compiler(sconsEnv)
+    if not linker:
+        linker = get_linker(sconsEnv)
+    if not cflags:
+        cflags = pkgCflags(compiler=compiler, sconsEnv=sconsEnv)
+
+    scotch_dir = getScotchDir(sconsEnv=sconsEnv)
+    
+    # Test that we can call a SCOTCH function
+    cpp_test_lib_str = r"""
 extern "C" {
 #include <stdio.h>
 #include <scotch.h>
@@ -53,109 +117,95 @@ int main() {
   SCOTCH_Mesh mesh;
 
   if (SCOTCH_meshInit (&mesh) !=0) {
-    std::cout<<"failure";
+    std::cout << "failure";
   } else {
-    std::cout<<"success";
+    std::cout << "success";
   }
   return 0;
 }
 """
+    cpp_file = "scotch_config_test_lib.cpp"
+    write_cppfile(cpp_test_lib_str, cpp_file)
 
-  write_cppfile(cpp_test_include_str,"scotch_config_test_include.cpp")
-  write_cppfile(cpp_test_lib_str, "scotch_config_test_lib.cpp")
- 
-  if not forceCompiler:
-    compiler = get_compiler(sconsEnv)
-    linker = get_linker(sconsEnv)
-  else:
-    compiler, linker = set_forced_compiler(forceCompiler)
-
-  scotch_dir = getScotchDir(sconsEnv=sconsEnv)
-  scotch_libs = scotch_dir
-  scotch_includes = scotch_dir
-
-  # test that we can include scotch:
-  cmdstr = "%s -I%s -c scotch_config_test_include.cpp" % (compiler, scotch_dir)
-  alt_cmdstr = "%s -I%s/include -c scotch_config_test_include.cpp" % (compiler, scotch_dir)
-  # first we try to look in the traditional subdirs lib and include
-  compileFailed, cmdoutput = commands.getstatusoutput(alt_cmdstr)
-  if compileFailed:
-    # then we look under scotch_dir
-    compileFailed, cmdoutput = commands.getstatusoutput(cmdstr)
+    # test that we can compile a program using SCOTCH:
+    cmdstr = "%s %s -c %s" % (compiler, cflags, cpp_file)
+    compileFailed, cmdoutput = getstatusoutput(cmdstr)
     if compileFailed:
-      # Nope, still does not work, exit with Exception, print orig. cmdstr
-      remove_cppfile("scotch_config_test_include.cpp")
-      raise UnableToCompileException("SCOTCH", cmd=cmdstr,
-                                     program=cpp_test_include_str, errormsg=cmdoutput)
-  else:
-    usealternate = True
-    scotch_libs = os.path.join(scotch_libs,'lib')
-    scotch_includes = os.path.join(scotch_includes,'include')
+        remove_cppfile(cpp_file)
+        raise UnableToCompileException("SCOTCH", cmd=cmdstr,
+                                       program=cpp_test_lib_str,
+                                       errormsg=cmdoutput)
 
-  remove_cppfile("scotch_config_test_include.cpp", ofile=True)
+    app = os.path.join(os.getcwd(), "a.out")
+    lib_dirs = ['', 'bin', 'lib']
+    for lib_dir in lib_dirs:
+        scotch_lib_dir = os.path.join(scotch_dir, lib_dir)
+        libscotch_static = os.path.join(scotch_lib_dir, 'libscotch.a')
+        libscotcherr_static = os.path.join(scotch_lib_dir, 'libscotcherr.a')
 
-  # test that we can compile a program using scotch:
-  cmdstr = "%s -I%s -c scotch_config_test_lib.cpp" % (compiler, scotch_dir)
-  alt_cmdstr = "%s -I%s/include -c scotch_config_test_lib.cpp" % (compiler, scotch_dir)
-  if usealternate:
-    cmdstr = alt_cmdstr
-  compileFailed, cmdoutput = commands.getstatusoutput(cmdstr)
-  if compileFailed:
-    remove_cppfile("scotch_config_test_lib.cpp")
-    raise UnableToCompileException("SCOTCH", cmd=cmdstr,
-                                   program=cpp_test_lib_str, errormsg=cmdoutput)
+        # test that we can link a binary using scotch static libs:
+        cmdstr = "%s -o %s %s %s %s" % \
+                 (linker, app, cpp_file.replace('.cpp', '.o'),
+                  libscotch_static, libscotcherr_static)
+        linkFailed, cmdoutput = getstatusoutput(cmdstr)
+        if not linkFailed:
+            break
 
-  # test that we can link a binary using scotch:
-  cmdstr = "%s -L%s scotch_config_test_lib.o -lscotch -lscotcherr" % (linker, scotch_dir)
-  alt_cmdstr = "%s -L%s/lib scotch_config_test_lib.o -lscotch -lscotcherr" % (linker, scotch_dir)
-  if usealternate:
-    cmdstr = alt_cmdstr
-  linkFailed, cmdoutput = commands.getstatusoutput(cmdstr)
-  if linkFailed:
-    remove_cppfile("scotch_config_test_lib.cpp", ofile=True)
-    raise UnableToLinkException("SCOTCH", cmd=cmdstr,
-                                program=cpp_test_lib_str, errormsg=cmdoutput)
+    if linkFailed:
+        remove_cppfile(cpp_file, ofile=True)
+        raise UnableToLinkException("SCOTCH", cmd=cmdstr,
+                                    program=cpp_test_lib_str,
+                                    errormsg=cmdoutput)
    
-  # run the binary:
-  runFailed, cmdoutput = commands.getstatusoutput("./a.out")
-  if runFailed or not cmdoutput == "success":
-    remove_cppfile("scotch_config_test_lib.cpp", ofile=True, execfile=True)
-    raise UnableToRunException("SCOTCH", errormsg=cmdoutput)
+    # test that we can run the binary:
+    runFailed, cmdoutput = commands.getstatusoutput(app)
+    remove_cppfile(cpp_file, ofile=True, execfile=True)
+    if runFailed or not cmdoutput == "success":
+        raise UnableToRunException("SCOTCH", errormsg=cmdoutput)
 
-  remove_cppfile("scotch_config_test_lib.cpp", ofile=True, execfile=True)
+    return "-L%s -lscotch -lscotcherr" % scotch_lib_dir
 
-  # Trying to figure out the version number from scotch.h
-  if usealternate:
-    scotch_h_filename = "%s/include/scotch.h" % scotch_dir
-  else:
-    scotch_h_filename = "%s/scotch.h" % scotch_dir
-  try:
-    scotch_h_file = open(scotch_h_filename, 'r')
-  except Exception, err:
-    msg = "Unable to open %s. Is SCOTCH_DIR set correctly?" % scotch_h_filename
-    raise UnableToXXXException(msg, errormsg=err)
-  scotch_version = '4.0'  # assume 4.0 as default
-  for line in scotch_h_file:
-    if "Version" in line:
-      # the lines with the version number is something like this:
-      # /**                # Version 4.0  : from : 11 dec 2001     **/
-      tmp = line.split()
-      scotch_version = tmp[tmp.index("Version")+1]
+def pkgTests(forceCompiler=None, sconsEnv=None,
+             cflags=None, libs=None, version=None, **kwargs):
+    """Run the tests for this package
+     
+    If Ok, return various variables, if not we will end with an exception.
+    forceCompiler, if set, should be a tuple containing (compiler, linker)
+    or just a string, which in that case will be used as both
+    """
 
-  return scotch_version, scotch_libs, scotch_includes
+    if not forceCompiler:
+        compiler = get_compiler(sconsEnv)
+        linker = get_linker(sconsEnv)
+    else:
+        compiler, linker = set_forced_compiler(forceCompiler)
+
+    if not cflags:
+        cflags = pkgCflags(sconsEnv=sconsEnv)
+    if not libs:
+        libs = pkgLibs(compiler=compiler, linker=linker,
+                       cflags=cflags, sconsEnv=sconsEnv)
+    else:
+        # run pkgLibs as this is the current SCOTCH test
+        pkgLibs(compiler=compiler, linker=linker,
+                cflags=cflags, sconsEnv=sconsEnv) 
+    if not version:
+        version = pkgVersion(sconsEnv=sconsEnv)
+        
+    return version, libs, cflags
 
 def generatePkgConf(directory=suitablePkgConfDir(), sconsEnv=None, **kwargs):
 
-  scotch_version, scotch_libs, scotch_includes = pkgTests(sconsEnv=sconsEnv)
+  scotch_version, scotch_libs, scotch_cflags = pkgTests(sconsEnv=sconsEnv)
 
   # Ready to create a pkg-config file:
   pkg_file_str = r"""Name: SCOTCH
 Version: %s
 Description: SCOTCH mesh and graph partitioning, http://www.labri.fr/perso/pelegrin/scotch/
-Libs: -L%s -lscotch -lscotcherr
-Cflags: -I%s 
-""" % (scotch_version, scotch_libs, scotch_includes)
-  pkg_file = open("%s/scotch.pc" % (directory), 'w')
+Libs: %s
+Cflags: %s 
+""" % (scotch_version, scotch_libs, scotch_cflags)
+  pkg_file = open(os.path.join(directory, "scotch.pc"), 'w')
   pkg_file.write(pkg_file_str)
   pkg_file.close()
   print "done\n Found SCOTCH and generated pkg-config file in\n '%s'" % directory
