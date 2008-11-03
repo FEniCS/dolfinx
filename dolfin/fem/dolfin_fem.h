e2eea41afdae3fa9b017ec18806cb5d4bedf9cd3
Garth N. Wells <gnw20@cam.ac.uk>
2008-11-03 22:32:46 +0000

    Remove old SubSystem include.

--- dolfin/fem/dolfin_fem.h
+++ dolfin/fem/dolfin_fem.h
@@ -5,7 +5,6 @@
 
 #include <dolfin/fem/DofMap.h>
 #include <dolfin/fem/FiniteElement.h>
-#include <dolfin/fem/SubSystem.h>
 #include <dolfin/fem/BoundaryCondition.h>
 #include <dolfin/fem/DirichletBC.h>
 #include <dolfin/fem/PeriodicBC.h>
