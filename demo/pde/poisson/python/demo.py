f1cb6fda94a4d64a1db951ae70bfff365a8494ed
Anders Logg <logg@simula.no>
2008-12-05 14:46:00 +0100

    Some simplifications in Poisson Python demo

--- demo/pde/poisson/python/demo.py
+++ demo/pde/poisson/python/demo.py
@@ -14,50 +14,36 @@ and boundary conditions given by
 """
 
 __author__ = "Anders Logg (logg@simula.no)"
-__date__ = "2007-08-16 -- 2008-11-25"
+__date__ = "2007-08-16 -- 2008-12-05"
 __copyright__ = "Copyright (C) 2007-2008 Anders Logg"
 __license__  = "GNU LGPL Version 2.1"
 
-from   dolfin import *
-import dolfin
+from dolfin import *
 
 # Create mesh and FunctionSpace
 mesh = UnitSquare(32, 32)
 V = FunctionSpace(mesh, "Lagrange", 1)
 
-# Just testing
-W = VectorFunctionSpace(mesh, "Lagrange", 2)
-
-# Source term, using compiled c++ expression
-class Source(Function):
-    cppexpr = ("A*exp(-(pow(x[0]-0.5,2) + pow(x[1]-0.5,2))/B)")
-    defaults = {"A":500.0,"B":0.02}
-
 # Neumann boundary condition, using python Functor
 class Flux(Function):
     def eval(self, values, x):
-        if x[0] > DOLFIN_EPS:
-            values[0] = 25.0*sin(5.0*DOLFIN_PI*x[1])
+        if x[0] > (1.0 - DOLFIN_EPS):
+            values[0] = 25.0*cos(5.0*DOLFIN_PI*x[1])
         else:
             values[0] = 0.0
 
-# Sub domain for Dirichlet boundary condition
+# Subdomain for Dirichlet boundary condition
 class DirichletBoundary(SubDomain):
     def inside(self, x, on_boundary):
-        return bool(x[0] < DOLFIN_EPS)
+        return x[0] < DOLFIN_EPS
 
 # Define variational problem
 v = TestFunction(V)
 u = TrialFunction(V)
 f = Function(V, cppexpr="500.0 * exp(-(pow(x[0] - 0.5, 2) + pow(x[1] - 0.5, 2)) / 0.02)")
-
-f2 = Source(V)
 g = Flux(V)
 a = dot(grad(v), grad(u))*dx
 L = v*f*dx + v*g*ds
-
-A = assemble(a)
-b = assemble(L)
 
 # Define boundary condition
 u0 = Constant("triangle", 0.0)
