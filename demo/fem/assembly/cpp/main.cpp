7a7e01467b2ca31296175d474c6fdf9d5aee0bfd
Anders Logg <logg@simula.no>
2008-12-12 14:21:29 +0100

    Demo fix, still not working, problem with numpy array type

--- demo/fem/assembly/cpp/main.cpp
+++ demo/fem/assembly/cpp/main.cpp
@@ -4,19 +4,7 @@
 // First added:  2008-02-04
 // Last changed: 2008-04-12
 //
-// This demonstrates parallel assembly in DOLFIN. Parallel
-// assembly is currently experimental and exists in the form
-// of the alternate assembler pAssembler. This will be merged
-// with the main DOLFIN assembler.
-//
-// To run this demo in parallel, type
-//
-//     mpirun -n <num_processes> demo
-//
-// Note that currently one needs to edit the code generated
-// by FFC and replace dolfin::Form with dolfin::pForm. This
-// extra step will be removed once Assembler and pAssembler
-// have merged.
+// This demonstrates parallel assembly in DOLFIN.
 
 #include <dolfin.h>
 #include "ReactionDiffusion.h"
@@ -26,27 +14,5 @@ using namespace dolfin;
 int main()
 {
   message("Sorry, this demo is currently broken.");
-  return 0;
-
-//#ifndef HAS_SCOTCH
-//  message("Sorry, this demo requires SCOTCH.");
-//  return 0;
-//#endif
-
-  // Create mesh
-  UnitCube mesh(20, 20, 20);
-
-  // Create function space and form
-  ReactionDiffusionFunctionSpace V(mesh); 
-  ReactionDiffusionBilinearForm a(V, V); 
-
-  // Partition mesh
-  MeshFunction<dolfin::uint> partitions(mesh);
-  mesh.partition(partitions);
-
-  // Assemble matrix using parallel assembler
-  //Matrix A;
-  //pAssembler::assemble(mesh, partitions);
- 
   return 0;
 }
--- demo/fem/simple/python/demo.py
+++ demo/fem/simple/python/demo.py
@@ -1,39 +1,39 @@
-# This demo program dmonstrates how to create simple finite
-# element matrices like the stiffness matrix and mass matrix.
-#
-# Modified by Anders Logg, 2008
+"""This demo program illustrates how to create simple finite element
+matrices like the stiffness matrix and mass matrix."""
 
 __author__ = "Kristian B. Oelgaard (k.b.oelgaard@tudelft.nl)"
-__date__ = "2007-11-15 -- 2008-01-15"
+__date__ = "2007-11-15 -- 2008-12-12"
 __copyright__ = "Copyright (C) 2007 Kristian B. Oelgaard"
 __license__  = "GNU LGPL Version 2.1"
 
+# Modified by Anders Logg, 2008
+
 from dolfin import *
-import numpy
+from numpy import array
 
 # Load reference mesh (just a simple tetrahedron)
 mesh = Mesh("../tetrahedron.xml.gz");
 
 # Assemble stiffness and mass matrices
-element = FiniteElement("Lagrange", "tetrahedron", 1)
-v = TestFunction(element)
-u = TrialFunction(element)
-A = assemble(dot(grad(v), grad(u))*dx, mesh)
-M = assemble(v*u*dx, mesh)
+V = FunctionSpace(mesh, "CG", 1)
+v = TestFunction(V)
+u = TrialFunction(V)
+A = assemble(dot(grad(v), grad(u))*dx)
+M = assemble(v*u*dx)
 
 # Create reference matrices and set entries
 A0 = Matrix(4, 4)
 M0 = Matrix(4, 4)
-pos = numpy.array([0, 1, 2, 3], dtype='I')
-A0.set(numpy.array([[1.0/2.0, -1.0/6.0, -1.0/6.0, -1.0/6.0],
-                    [-1.0/6.0, 1.0/6.0, 0.0, 0.0],
-                    [-1.0/6.0, 0.0, 1.0/6.0, 0.0],
-                    [-1.0/6.0, 0.0, 0.0, 1.0/6.0]]), pos, pos)
+pos = array([0, 1, 2, 3], dtype='uint')
+A0.set(array([[1.0/2.0, -1.0/6.0, -1.0/6.0, -1.0/6.0],
+              [-1.0/6.0, 1.0/6.0, 0.0, 0.0],
+              [-1.0/6.0, 0.0, 1.0/6.0, 0.0],
+              [-1.0/6.0, 0.0, 0.0, 1.0/6.0]]), pos, pos)
 
-M0.set(numpy.array([[1.0/60.0, 1.0/120.0, 1.0/120.0, 1.0/120.0],
-                    [1.0/120.0, 1.0/60.0, 1.0/120.0, 1.0/120.0],
-                    [1.0/120.0, 1.0/120.0, 1.0/60.0, 1.0/120.0],
-                    [1.0/120.0, 1.0/120.0, 1.0/120.0, 1.0/60.0]]), pos, pos)
+M0.set(array([[1.0/60.0, 1.0/120.0, 1.0/120.0, 1.0/120.0],
+              [1.0/120.0, 1.0/60.0, 1.0/120.0, 1.0/120.0],
+              [1.0/120.0, 1.0/120.0, 1.0/60.0, 1.0/120.0],
+              [1.0/120.0, 1.0/120.0, 1.0/120.0, 1.0/60.0]]), pos, pos)
 A0.apply()
 M0.apply()
 
