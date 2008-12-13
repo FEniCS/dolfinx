b58667ce2fad27bed7a624ef0ec0ebd02405fca2
Kristian Oelgaard <k.b.oelgaard@tudelft.nl>
2008-12-13 15:44:16 +0000

    Fix pde/sym-dirichlet-bc demo

--- demo/pde/sym-dirichlet-bc/python/demo.py
+++ demo/pde/sym-dirichlet-bc/python/demo.py
@@ -3,23 +3,21 @@
    on the standard Poisson demo
 """
 
+# Modified by Kristian Oelgaard, 2008
+
 __author__ = "Kent-Andre Mardal (kent-and@simula.no)"
-__date__ = "2008-08-13"
+__date__ = "2008-08-13 -- 2008-12-13"
 __copyright__ = "Copyright (C) 2008 Kent-Andre Mardal"
 __license__  = "GNU LGPL Version 2.1"
-
-
 
 from dolfin import *
 
 # Create mesh and finite element
 mesh = UnitSquare(32, 32)
-element = FiniteElement("Lagrange", "triangle", 1)
+V = FunctionSpace(mesh, "CG", 1)
 
 # Source term
 class Source(Function):
-    def __init__(self, element, mesh):
-        Function.__init__(self, element, mesh)
     def eval(self, values, x):
         dx = x[0] - 0.5
         dy = x[1] - 0.5
@@ -27,8 +25,6 @@ class Source(Function):
 
 # Neumann boundary condition
 class Flux(Function):
-    def __init__(self, element, mesh):
-        Function.__init__(self, element, mesh)
     def eval(self, values, x):
         if x[0] > DOLFIN_EPS:
             values[0] = 25.0*sin(5.0*DOLFIN_PI*x[1])
@@ -41,21 +37,20 @@ class DirichletBoundary(SubDomain):
         return bool(on_boundary and x[0] < DOLFIN_EPS)
 
 # Define variational problem
-v = TestFunction(element)
-u = TrialFunction(element)
-f = Source(element, mesh)
-g = Flux(element, mesh)
+v = TestFunction(V)
+u = TrialFunction(V)
+f = Source(V)
+g = Flux(V)
 
 a = dot(grad(v), grad(u))*dx
 L = v*f*dx + v*g*ds
 
 # Define boundary condition
-u0 = Function(mesh, 0.0)
-boundary = DirichletBoundary()
-bc = DirichletBC(u0, mesh, boundary)
+u0 = Constant(mesh, 0.0)
+bc = DirichletBC(V, u0, DirichletBoundary())
 
 # Solve PDE and plot solution
-pde = LinearPDE(a, L, mesh, bc, symmetric)
+pde = LinearPDE(a, L, bc, symmetric)
 U = pde.solve()
 
 plot(U)
