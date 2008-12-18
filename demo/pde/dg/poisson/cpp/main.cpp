8423d6ca503aebfe87721b0ed5959cddd9e30c16
Anders Logg <logg@simula.no>
2008-12-18 23:00:10 +0100

    Attempt at general detection of discontinuous fields in VTK output without relying on signatures.

--- demo/pde/dg/poisson/cpp/main.cpp
+++ demo/pde/dg/poisson/cpp/main.cpp
@@ -1,8 +1,8 @@
-// Copyright (C) 2006-2007 Anders Logg and Kristian Oelgaard.
+// Copyright (C) 2006-2008 Anders Logg and Kristian Oelgaard.
 // Licensed under the GNU LGPL Version 2.1.
 //
 // First added:  2006-12-05
-// Last changed: 2008-11-19
+// Last changed: 2008-12-18
 //
 // This demo program solves Poisson's equation,
 //
@@ -63,17 +63,8 @@ int main()
   Function u;
   pde.solve(u);
 
-  // Project solution onto continuous basis for post-processing
-  P1ProjectionFunctionSpace U(mesh);
-  P1ProjectionBilinearForm a_p(U, U);
-  P1ProjectionLinearForm L_p(U);
-  L_p.u = u;
-  LinearPDE pde_proj(a_p, L_p);
-  Function u_p(U);
-  pde_proj.solve(u_p);
-
   // Plot solution projected
-  plot(u_p);
+  plot(u);
 
   // Save solution in VTK format
   File file("poisson.pvd");
