4afd267216144edb58c5dd60cec80faef8b6ba5a
Garth N. Wells <gnw20@cam.ac.uk>
2008-12-23 10:59:10 +0000

    Use new IsOutflowFacet in C++ DG advection-diffusion demo.
    Results verified to be the same as the old version.

--- demo/pde/dg/advection-diffusion/cpp/main.cpp
+++ demo/pde/dg/advection-diffusion/cpp/main.cpp
@@ -2,7 +2,7 @@
 // Licensed under the GNU LGPL Version 2.1.
 //
 // First added:  2007-06-29
-// Last changed: 2008-12-12
+// Last changed: 2008-12-23
 //
 // Steady state advection-diffusion equation, discontinuous
 // formulation using full upwinding.
@@ -10,7 +10,6 @@
 #include <dolfin.h>
 
 #include "AdvectionDiffusion.h"
-#include "OutflowFacet.h"
 #include "Projection.h"
 
 using namespace dolfin;
@@ -33,65 +32,6 @@ class DirichletBoundary : public SubDomain
   }
 };
 
-class OutflowFacet : public Function
-{
-public:
-
-  // Constructor
-  OutflowFacet(const Form& form): form(form), 
-				  V(form.function_spaces()), ufc(form)                            
-  {
-    // Some simple sanity checks on form
-    if (!(form.rank() == 0 && form.ufc_form().num_coefficients() == 2))
-      error("Invalid form: rank = %d, number of coefficients = %d. Must be rank 0 form with 2 coefficients.", 
-	    form.rank(), form.ufc_form().num_coefficients());
-    
-    if (!(form.ufc_form().num_cell_integrals() == 0 && form.ufc_form().num_exterior_facet_integrals() == 1 
-	  && form.ufc_form().num_interior_facet_integrals() == 0))
-      error("Invalid form: Must have exactly 1 exterior facet integral");
-  }
-
-  ~OutflowFacet(){}
-
-  void eval(double* values, const Data& data) const
-  {
-    // If there is no facet (assembling on interior), return 0.0
-    if (!data.on_facet())
-    {
-      values[0] = 0.0;
-      return;
-    }
-    else
-    {
-      ufc.update( data.cell() );
-      
-      // Interpolate coefficients on cell and current facet
-      for (dolfin::uint i = 0; i < form.coefficients().size(); i++)
-        form.coefficient(i).interpolate(ufc.w[i], ufc.cell, data.facet());
-  
-      // Get exterior facet integral (we need to be able to tabulate ALL facets 
-      // of a given cell)
-      ufc::exterior_facet_integral* integral = ufc.exterior_facet_integrals[0];
-  
-      // Call tabulate_tensor on exterior facet integral, 
-      // dot(velocity, facet_normal)
-      integral->tabulate_tensor(ufc.A, ufc.w, ufc.cell, data.facet());
-    }
-    
-    // If dot product is positive, the current facet is an outflow facet
-    if (ufc.A[0] > DOLFIN_EPS)
-      values[0] = 1.0;
-    else
-      values[0] = 0.0;
-  }
-  
-private:
-
-  const Form& form;
-  std::vector<const FunctionSpace*> V;
-  mutable UFC ufc;
-};
-
 int main(int argc, char *argv[])
 {
   // Read simple velocity field (-1.0, -0.4) defined on a 64x64 unit square 
@@ -109,16 +49,12 @@ int main(int argc, char *argv[])
   FacetNormal N;
   AvgMeshSize h;
 
-  // Definitions for outflow facet function (use to define flux upwinding)
-  OutflowFacetFunctional M_of;
-  M_of.velocity = velocity;
-  M_of.n = N;
-
-  // Outflow facet function From SpecialFunctions.h
-  OutflowFacet of(M_of); 
-
   // Penalty parameter
   Constant alpha(5.0);
+
+  // Create outflow facet function
+  AdvectionDiffusionBilinearFormCoefficientSpace3 V_dg(mesh);
+  IsOutflowFacet of(V_dg, velocity);
 
   // Create function space
   AdvectionDiffusionFunctionSpace V(mesh);
