e189d36c5aff67a93583bc69f5fba3f124514346
Anders Logg <logg@simula.no>
2008-12-27 15:08:14 +0100

    Bug fix in Newton solver: x += dx --> x -= dx

--- dolfin/nls/NewtonSolver.cpp
+++ dolfin/nls/NewtonSolver.cpp
@@ -5,7 +5,7 @@
 // Modified by Martin Alnes, 2008.
 //
 // First added:  2005-10-23
-// Last changed: 2008-12-04
+// Last changed: 2008-12-27
 
 #include "NewtonSolver.h"
 #include "NonlinearProblem.h"
@@ -46,7 +46,7 @@ dolfin::uint NewtonSolver::solve(NonlinearProblem& nonlinear_problem, GenericVec
   dolfin_assert(b);
   dolfin_assert(dx);
 
-  const uint maxit= get("Newton maximum iterations");
+  const uint maxiter = get("Newton maximum iterations");
 
   begin("Starting Newton solve.");
 
@@ -61,40 +61,39 @@ dolfin::uint NewtonSolver::solve(NonlinearProblem& nonlinear_problem, GenericVec
   bool newton_converged = false;
 
   // Start iterations
-  while( !newton_converged && newton_iteration < maxit )
+  while (!newton_converged && newton_iteration < maxiter)
   {
+    // Perform linear solve and update total number of Krylov iterations
+    if (dx->size() > 0)
+      dx->zero();
+    set("output destination", "silent");
+    krylov_iterations += solver->solve(*A, *dx, *b);
+    set("output destination", "terminal");
 
-      set("output destination", "silent");
-      // Perform linear solve and update total number of Krylov iterations
-      if(dx->size() > 0)
-        dx->zero();
-      krylov_iterations += solver->solve(*A, *dx, *b);
-      set("output destination", "terminal");
-
-      // Compute initial residual
-      if(newton_iteration == 0)
-        newton_converged = converged(*b, *dx, nonlinear_problem);
-
-      // Update solution
-      x += (*dx);
-
-      ++newton_iteration;
-
-      //FIXME: this step is not needed if residual is based on dx and this has converged.
-      // Compute F(u) and J
-      nonlinear_problem.F(*b, x);
-      nonlinear_problem.J(*A, x);
-
-      // Test for convergence 
+    // Compute initial residual
+    if (newton_iteration == 0)
       newton_converged = converged(*b, *dx, nonlinear_problem);
+
+    // Update solution
+    x -= (*dx);
+
+    ++newton_iteration;
+    
+    //FIXME: this step is not needed if residual is based on dx and this has converged.
+    // Compute F(u) and J
+    nonlinear_problem.F(*b, x);
+    nonlinear_problem.J(*A, x);
+
+    // Test for convergence 
+    newton_converged = converged(*b, *dx, nonlinear_problem);
   }
 
-  if(newton_converged)
+  if (newton_converged)
     message("Newton solver finished in %d iterations and %d linear solver iterations.", 
-        newton_iteration, krylov_iterations);
+            newton_iteration, krylov_iterations);
   else
     warning("Newton solver did not converge.");
-
+  
   end();
 
   return newton_iteration;
@@ -146,4 +145,3 @@ bool NewtonSolver::converged(const GenericVector& b, const GenericVector& dx,
   return false;
 }
 //-----------------------------------------------------------------------------
-
