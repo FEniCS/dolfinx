fe5c3b67d4c093343f2bf713fd7e172bc82f5d8e
Garth N. Wells <gnw20@cam.ac.uk>
2009-01-27 16:52:18 +0000

    Make elastodynamics example more interesting.
    Add viscous damping term.

--- demo/pde/elastodynamics/cpp/main.cpp
+++ demo/pde/elastodynamics/cpp/main.cpp
@@ -2,11 +2,12 @@
 // Licensed under the GNU LGPL Version 2.1.
 //
 // First added:  2009-01-22
-// Last changed: 2009
+// Last changed: 2009-01-27
 //
 
 #include <dolfin.h>
 #include "ElastoDynamics.h"
+#include "DG0_eps_xx.h"
 
 using namespace dolfin;
 
@@ -23,7 +24,7 @@ class Pressure : public Function
       if(old && time > 0.0)
         time -= dt;
 
-      const double cutoff_time = 4.0*1.0/64.0;
+      const double cutoff_time = 10.0*1.0/32.0;
       if (time < cutoff_time)
       {
   		  values[0] = 1.0*time/cutoff_time;
@@ -96,15 +97,15 @@ int main(int argc, char* argv[])
   dolfin_init(argc, argv);
 
   // Create Mesh
-  UnitSquare mesh(64, 64);
+  Mesh mesh("../../../../data/meshes/dolfin-2.xml.gz");
 
   // Create function space
   ElastoDynamicsFunctionSpace V(mesh);
 
 	// Material parameters
   Constant rho(1.0);                           // mass density
-  Constant eta(0.0);                           // damping coefficient
-  double E  = 1.0;                             // Young modulus
+  Constant eta(0.25);                          // damping coefficient
+  double E  = 1.0;                             // Youngs modulus
   double nu = 0.0;                             // Poisson ratio 
   Constant lambda((nu*E)/((1.0+nu)*(1.0-nu))); // Lame coefficient
   Constant mu(E/(2.0*(1.0+nu)));               // Lame coefficient
@@ -114,12 +115,12 @@ int main(int argc, char* argv[])
   Constant alpha_f(0.4);
   double Beta = 0.36;                   
   double Gamma = 0.7;      
-  double delta_t = 0.5*1.0/64.0;               // time step
+  double delta_t = 1.0/32.0;               // time step
   double t = 0.0;                              // initial time
-  double T = 2*64.0*delta_t;                   // final time
+  double T = 200*delta_t;                      // final time
   Constant beta(Beta), gamma(Gamma), dt(delta_t);
 
-  // Body forces
+  // Body force
   Constant f(2, 0.0);
 
   // External load
@@ -145,12 +146,12 @@ int main(int argc, char* argv[])
   v0.vector().zero();
   a0.vector().zero();
 
-  // Create forms
+  // Create forms and attach functions
   ElastoDynamicsBilinearForm a_form(V, V);
   a_form.lmbda = lambda; a_form.mu = mu; a_form.rho = rho; 
-//a_form.eta = eta;
+  a_form.eta = eta;
   a_form.beta = beta; 
-// a_form.gamma = gamma;
+  a_form.gamma = gamma;
   a_form.dt = dt; 
   a_form.alpha_m = alpha_m; 
   a_form.alpha_f = alpha_f;
@@ -160,9 +161,9 @@ int main(int argc, char* argv[])
   L.lmbda = lambda; 
   L.mu = mu; 
   L.rho = rho; 
-  //L.eta = eta;
+  L.eta = eta;
   L.beta = beta; 
-  //L.gamma = gamma;
+  L.gamma = gamma;
   L.dt = dt; 
   L.alpha_m = alpha_m; 
   L.alpha_f = alpha_f;
@@ -173,10 +174,17 @@ int main(int argc, char* argv[])
   // Create variational problem
   VariationalProblem pde(a_form, L, bc, 0, &right_boundary_function, 0);
 
+  // Create projection to compute the normal strain eps_xx
+  DG0_eps_xxFunctionSpace Vdg(mesh);
+  DG0_eps_xxBilinearForm a_eps(Vdg, Vdg);
+  DG0_eps_xxLinearForm L_eps(Vdg);
+  L_eps.u = u;
+  VariationalProblem eps(a_eps, L_eps);
+  Function eps_xx(Vdg);
+
   // Create output files
-	File file_u  ("u.pvd");
-	File file_v  ("v.pvd");
-  File file_a  ("a.pvd");
+	File file_u("u.pvd");
+	File file_eps("eps_xx.pvd");
 
   // Start time stepping
   dolfin::uint step = 0;
@@ -188,6 +196,7 @@ int main(int argc, char* argv[])
 
     // Solve
     pde.solve(u);
+    eps.solve(eps_xx);
 
     // Update velocity and acceleration
     update_a(a, u, a0, v0, u0, Beta, delta_t);
@@ -195,11 +204,10 @@ int main(int argc, char* argv[])
     u0 = u; v0 = v; a0 = a;
 
     // Save solutions to file
-    if( step % 1 == 0)
+    if( step % 2 == 0)
     {
       file_u << u;
-      file_v << v;
-      file_a << a;
+      file_eps << eps_xx;
     }
     ++step;
 	}
