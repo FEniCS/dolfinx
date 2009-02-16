f61bb063b17f1b3a6279eccf13368b3528f0db75
Benjamin Kehlet <benjamik@ifi.uio.no>
2009-02-16 18:45:26 +0100

    Rewrite eval()

--- dolfin/math/Legendre.cpp
+++ dolfin/math/Legendre.cpp
@@ -1,8 +1,10 @@
 // Copyright (C) 2003-2008 Anders Logg.
 // Licensed under the GNU LGPL Version 2.1.
 //
+// Modified by Benjamin Kehlet
+//
 // First added:  2003-06-03
-// Last changed: 2008-04-22
+// Last changed: 2009-02-16
 
 #include <cmath>
 #include <dolfin/common/constants.h>
@@ -38,17 +40,30 @@ real Legendre::d2dx(real x)
 //-----------------------------------------------------------------------------
 real Legendre::eval(int n, real x)
 {
-  // Special case n = 0
-  if ( n == 0 )
-    return 1.0;
+  //recursive formula, BETA page 254
+  //return ( (2.0*nn-1.0)*x*eval(n-1, x) - (nn-1.0)*eval(n-2, x) ) / nn;
 
-  // Special case n = 1
-  if ( n == 1 )
-    return x;
-  
-  // Recurrence, BETA page 254
+
+  //The special cases
+  if ( n == 0 ) return 1.0;
+  if ( n == 1 ) return x;
+
   real nn = real(n);
-  return ( (2.0*nn-1.0)*x*eval(n-1, x) - (nn-1.0)*eval(n-2, x) ) / nn;
+
+  //previous computed values
+  real prevprev = 1.0;
+  real prev     = x;
+  real current;
+
+  for (uint i = 2; i <= n; ++i) 
+  {
+    current = ( (2.0*nn-1.0)*x*prev - (nn-1.0)*prevprev ) / nn;
+
+    prevprev = prev;
+    prev = current;
+  }
+
+  return current;
 }
 //-----------------------------------------------------------------------------
 real Legendre::ddx(int n, real x)
