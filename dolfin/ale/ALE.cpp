7d2c994cfd5f1022e97e647cd634e6d419eee9dc
Anders Logg <logg@simula.no>
2009-02-26 00:02:08 +0100

    Add function for simple displacement of meshes. If u is displacement function, the mesh coordinates can now be updated simply by
      mesh.move(u)
    
    See elasticity demo. Useful in FSI simulations.

--- dolfin/ale/ALE.cpp
+++ dolfin/ale/ALE.cpp
@@ -1,9 +1,12 @@
-// Copyright (C) 2008 Solveig Bruvoll and Anders Logg.
+// Copyright (C) 2008-2009 Solveig Bruvoll and Anders Logg.
 // Licensed under the GNU LGPL Version 2.1.
 //
 // First added:  2008-05-02
-// Last changed: 2008-09-11
+// Last changed: 2009-02-25
 
+#include <dolfin/function/Function.h>
+#include <dolfin/function/FunctionSpace.h>
+#include <dolfin/fem/FiniteElement.h>
 #include "TransfiniteInterpolation.h"
 #include "HarmonicSmoothing.h"
 #include "ALE.h"
@@ -32,5 +35,31 @@ void ALE::move(Mesh& mesh, Mesh& new_boundary, ALEType method)
   default:
     error("Unknown method for ALE mesh smoothing.");
   }
+}
+//-----------------------------------------------------------------------------
+void ALE::move(Mesh& mesh, const Function& displacement)
+{
+  // Check dimensions
+  const FiniteElement& element = displacement.function_space().element();
+  const uint gdim = mesh.geometry().dim();
+  if (!((element.value_rank() == 0 && gdim == 0) ||
+        (element.value_rank() == 1 && gdim == element.value_dimension(0))))
+    error("Unable to move mesh, illegal value dimension of displacement function.");
+
+  // Interpolate at vertices
+  const uint N = mesh.numVertices();
+  double* vertex_values = new real[N*gdim];
+  displacement.interpolate(vertex_values);
+
+  // Move vertex coordinates
+  double* x = mesh.geometry().x();
+  for (uint d = 0; d < gdim; d++)
+  {
+    for (uint i = 0; i < N; i++)
+      x[i*gdim + d] += vertex_values[d*N + i];
+  }
+
+  // Clean up
+  delete [] vertex_values;
 }
 //-----------------------------------------------------------------------------
