f586a4d0a7aab7887d5d632e25fcba496b78d506
Ola Skavhaug <skavhaug@simula.no>
2009-03-29 12:08:35 +0200

    Add XMLDolfin files taking care of the outer dolfin xml namespace.

---
+++ dolfin/io/XMLDolfin.cpp
@@ -0,0 +1,71 @@
+// Copyright (C) 2009 Ola Skavhaug
+// Licensed under the GNU LGPL Version 2.1.
+//
+// First added:  2009-03-02
+// Last changed: 2009-03-17
+
+#include <dolfin/log/dolfin_log.h>
+#include "NewXMLFile.h"
+#include "XMLIndent.h"
+#include "XMLDolfin.h"
+
+using namespace dolfin;
+
+//-----------------------------------------------------------------------------
+XMLDolfin::XMLDolfin(XMLHandler& dispatch, NewXMLFile& parser)
+  : XMLHandler(parser), state(OUTSIDE_DOLFIN), dispatch(dispatch)
+{
+  // Do nothing
+}
+
+//-----------------------------------------------------------------------------
+void XMLDolfin::start_element(const xmlChar *name, const xmlChar **attrs)
+{
+  switch ( state )
+  {
+  case OUTSIDE_DOLFIN:
+    
+    if ( xmlStrcasecmp(name, (xmlChar *) "dolfin") == 0 )
+    {
+      state = INSIDE_DOLFIN;
+      dispatch.handle();
+    }
+    
+    break;
+    
+  default:
+    ;
+  }
+}
+//-----------------------------------------------------------------------------
+void XMLDolfin::end_element(const xmlChar *name)
+{
+  switch ( state )
+  {
+  case INSIDE_DOLFIN:
+    
+    if ( xmlStrcasecmp(name, (xmlChar *) "dolfin") == 0 )
+    {
+      state = OUTSIDE_DOLFIN;
+      release();
+    }
+    
+    break;
+    
+  default:
+    ;
+  }
+}
+//-----------------------------------------------------------------------------
+void XMLDolfin::write_start(std::ostream& outfile, uint indentation_level)
+{
+  XMLIndent indent(indentation_level);
+  outfile << indent() << "<dolfin>" << std::endl;
+}
+//-----------------------------------------------------------------------------
+void XMLDolfin::write_end(std::ostream& outfile, uint indentation_level)
+{
+  XMLIndent indent(indentation_level);
+  outfile << indent() << "</dolfin>" << std::endl;
+}
+//-----------------------------------------------------------------------------
