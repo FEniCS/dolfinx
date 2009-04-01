ab7c35418afd7cd22f84c1fb544c443fc0ffded6
Ola Skavhaug <skavhaug@simula.no>
2009-04-01 15:29:26 +0200

    Initial work on the parallel dof map builder. Edge information and local offsets calculated. Some minor changes to the new xml system and sandbox tests as well.

--- dolfin/fem/DofMapBuilder.cpp
+++ dolfin/fem/DofMapBuilder.cpp
@@ -2,9 +2,12 @@
 // Licensed under the GNU LGPL Version 2.1.
 //
 // First added:  2008-08-12
-// Last changed: 2008-08-12
+// Last changed: 2009-04-01
 
+#include <algorithm>
 #include <dolfin/log/log.h>
+#include <dolfin/mesh/Edge.h>
+#include <dolfin/mesh/Vertex.h>
 #include <dolfin/mesh/Mesh.h>
 #include <dolfin/mesh/MeshData.h>
 #include <dolfin/main/MPI.h>
@@ -14,6 +17,7 @@
 
 using namespace dolfin;
 
+#if defined HAS_MPI
 //-----------------------------------------------------------------------------
 void DofMapBuilder::build(DofMap& dof_map, UFC& ufc, const Mesh& mesh)
 {
@@ -30,56 +34,161 @@ void DofMapBuilder::build(DofMap& dof_map, UFC& ufc, const Mesh& mesh)
   const uint n = dof_map.local_dimension();
   dof_map.dof_map = new int[n*mesh.numCells()];
   
-  dolfin_not_implemented();
-
-  // Get mesh functions
-  //MeshFunction<uint>* S = mesh.data().meshFunction("subdomain overlap");
-  //MeshFunction<uint>* F = mesh.data().meshFunction("facet overlap");
-  //dolfin_assert(S);
-  //dolfin_assert(F);
+  //dolfin_not_implemented();
 
   // Get number of this process
   const uint this_process = MPI::process_number();
   message("Building dof map on processor %d.", this_process);
 
-  // Build stage 0: Compute offsets
-  computeOffsets(this_process);
+  // Build stage 0: Initialize data structures
+  initialize_data_structure(dof_map, mesh);
+
+  // Build stage 1: Compute offsets
+  compute_offsets();
   
-  // Build stage 0.5: Communicate offsets
-  communicateOffsets();
+  // Build stage 2: Communicate offsets
+  communicate_offsets();
   
-  // Build stage 1: Compute mapping on shared facets
-  computeShared();
+  // Build stage 3: Compute dofs that this process is resposible for
+  number_dofs();
   
-  // Build stage 2: Communicate mapping on shared facets
-  communicateShared();
-  
-  // Build stage 3: Compute mapping for interior degrees of freedom
-  computeInterior();
+  // Build stage 4: Communicate mapping on shared facets
+  communicate_shared();
 }
 //-----------------------------------------------------------------------------
-void DofMapBuilder::computeOffsets(uint this_process)
+void DofMapBuilder::initialize_data_structure(DofMap& dof_map, const Mesh& mesh)
+{
+  const uint process_number = MPI::process_number();
+  // Initialize mesh entities used by the dof map
+  for (uint d = 0; d <= mesh.topology().dim(); d++)
+    if (dof_map.ufc_dof_map->needs_mesh_entities(d))
+      mesh.init(d);
+
+  // Build edge-global-vertex-number information
+  MeshFunction<uint>* global_vertex_indices = mesh.data().mesh_function("global vertex indices");
+  if (global_vertex_indices != NULL)
+  {
+    std::vector<std::pair<uint, uint> > edges(mesh.topology().size(1));
+
+    std::vector<uint> edge_vertices;
+    std::pair<uint, uint> edge;
+    for (EdgeIterator e(mesh); !e.end(); ++e)
+    {
+      std::cout << "P" << process_number << ". Edge index is " << e->index() << std::endl;
+      edge_vertices.clear();
+      for (VertexIterator vertex(*e); !vertex.end(); ++vertex)
+      {
+        std::cout << "P" << process_number << ". Vertex index is " << vertex->index() << std::endl;
+        std::cout << "P" << process_number << ". Global vertex index is " << global_vertex_indices->get(vertex->index()) << std::endl;
+        edge_vertices.push_back(global_vertex_indices->get(vertex->index()));
+      }
+      edge.first = edge_vertices[0];
+      edge.second= edge_vertices[1];
+      edges[e->index()] = edge;
+    }
+
+    std::cout  << "P" << process_number << ". Total number of edges is " << edges.size() << std::endl;
+
+    // Find out which edges to ignore (belonging to a lower ranked process)
+    std::map<uint, std::vector<uint> >* mapping = mesh.data().vector_mapping("overlap");
+
+    uint num_ignored_edges = 0;
+    for (uint e = 0; e < edges.size(); ++e)
+    {
+      uint v0 = edges[e].first;
+      uint v1 = edges[e].second;
+      if (mapping->count(v0) and mapping->count(v1))
+      {
+        std::vector<uint> intersection(1);
+        std::vector<uint>::iterator intersection_end = std::set_intersection(
+            (*mapping)[v0].begin(), (*mapping)[v0].end(), 
+            (*mapping)[v1].begin(), (*mapping)[v1].end(), intersection.begin());
+
+        if (intersection_end != intersection.begin() and intersection[0] < process_number)
+            ++num_ignored_edges;
+      }
+    }
+
+    // Compute local offset
+    std::cout  << "P" << process_number << ". Number of ignored edges is " << num_ignored_edges << std::endl;
+    uint local_offset = mesh.size(1) - num_ignored_edges;
+    std::cout  << "P" << process_number << ". Offset is " << local_offset << std::endl;
+
+    uint max_global_number = *std::max_element(global_vertex_indices->values(), global_vertex_indices->values() + global_vertex_indices->size());
+    uint global_num_vertices = MPI::global_maximum(max_global_number);
+
+    std::cout  << "P" << process_number << ". Max global number is " << global_num_vertices << std::endl;
+
+
+    
+  /*
+    std::map<uint, std::vector<uint> >::iterator map_iter;
+
+    BoundaryMesh bmesh(mesh);
+
+    for (CellIterator cell(mesh); !cell.end(); ++cell)
+      for (VertexIterator vertex(*cell); !vertex.end(); ++vertex)
+      {
+
+      }
+    
+  */
+  }
+
+}
+//-----------------------------------------------------------------------------
+void DofMapBuilder::compute_offsets()
 {
 
 }
 //-----------------------------------------------------------------------------
-void DofMapBuilder::communicateOffsets()
+void DofMapBuilder::communicate_offsets()
 {
 
 }
 //-----------------------------------------------------------------------------
-void DofMapBuilder::computeShared()
+void DofMapBuilder::number_dofs()
 {
 
 }
 //-----------------------------------------------------------------------------
-void DofMapBuilder::communicateShared()
+void DofMapBuilder::communicate_shared()
 {
 
 }
 //-----------------------------------------------------------------------------
-void DofMapBuilder::computeInterior()
-{
 
+#else
+
+//-----------------------------------------------------------------------------
+void DofMapBuilder::build(DofMap& dof_map, UFC& ufc, const Mesh& mesh)
+{
+  // Do nothing
+}
+
+//-----------------------------------------------------------------------------
+void DofMapBuilder::initialize_data_structure(DofMap& dof_map, const Mesh& mesh)
+{
+  // Do nothing
 }
 //-----------------------------------------------------------------------------
+void DofMapBuilder::compute_offsets()
+{
+  // Do nothing
+}
+//-----------------------------------------------------------------------------
+void DofMapBuilder::communicate_offsets()
+{
+  // Do nothing
+}
+//-----------------------------------------------------------------------------
+void DofMapBuilder::number_dofs()
+{
+  // Do nothing
+}
+//-----------------------------------------------------------------------------
+void DofMapBuilder::communicate_shared()
+{
+  // Do nothing
+}
+#endif
--- dolfin/fem/DofMapBuilder.h
+++ dolfin/fem/DofMapBuilder.h
@@ -2,7 +2,7 @@
 // Licensed under the GNU LGPL Version 2.1.
 //
 // First added:  2008-08-12
-// Last changed: 2008-10-26
+// Last changed: 2009-04-01
 
 #ifndef __DOF_MAP_BUILDER_H
 #define __DOF_MAP_BUILDER_H
@@ -24,22 +24,22 @@ namespace dolfin
     static void build(DofMap& dof_map, UFC& ufc, const Mesh& mesh);
 
   private:
-
-    // Build stage 0: Compute offsets
-    static void computeOffsets(uint this_process);
     
-    // Build stage 0.5: Communicate offsets
-    static void communicateOffsets();
+    // Build stage 0: Initialize data structures
+    static void initialize_data_structure(DofMap& dof_map, const Mesh& mesh);
 
-    // Build stage 1: Compute mapping on shared facets
-    static void computeShared();
+    // Build stage 1: Compute offsets
+    static void compute_offsets();
     
-    // Build stage 2: Communicate mapping on shared facets
-    static void communicateShared();
-    
-    // Build stage 3: Compute mapping for interior degrees of freedom
-    static void computeInterior();
+    // Build stage 2: Communicate offsets
+    static void communicate_offsets();
 
+    // Build stage 3: Compute dofs that this process is resposible for
+    static void number_dofs();
+    
+    // Build stage 4: Communicate mapping on shared facets
+    static void communicate_shared();
+    
   };
 
 }
