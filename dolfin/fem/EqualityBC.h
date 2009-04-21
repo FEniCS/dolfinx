14cf968890ff34f2d7dd7f7c2eab591f84fc4990
Anders Logg <logg@simula.no>
2009-04-21 09:09:55 +0200

    Import EqualityBC patch from BArtosz

---
+++ dolfin/fem/EqualityBC.h
@@ -0,0 +1,91 @@
+// Copyright (C) 2009 Bartosz Sawicki.
+// Licensed under the GNU LGPL Version 2.1.
+//
+// First added:  2009-04-03
+// Last changed: 2009-04-15
+
+#ifndef __EQUALITY_BC_H
+#define __EQUALITY_BC_H
+
+#include <boost/shared_ptr.hpp>
+#include <dolfin/common/types.h>
+#include "BoundaryCondition.h"
+
+namespace dolfin
+{
+
+  class DofMap;
+  class Mesh;
+  class SubDomain;
+  class Form;
+  class GenericMatrix;
+  class GenericVector;
+
+  /// This class specifies the interface for setting equality boundary
+  /// conditions for partial differential equations,
+  ///
+  ///    u(x) = u(y),    for any x and y on G,
+  /// 
+  ///  where G is subdomain of the mesh.
+  ///
+  /// The sub domain G may be specified in a two different ways. Both of 
+  /// them produce set of unknowns (dofs) with should be equal. 
+  ///
+  /// The simplest approach is to specify a SubDomain object, using 
+  /// the inside() function to specify on which facets the boundary 
+  /// condition should be applied.
+  ///
+  /// Alternatively, the boundary may be specified by the boundary 
+  /// indicators included in the mesh. 
+  ///
+  /// Current implementation assume that the problem is scalar,
+  /// so in case of mixed systems (vector-valued and mixed elements)
+  /// all compoments will be equalize.
+
+
+  class EqualityBC : public BoundaryCondition
+  {
+  public:
+
+    // Create equality boundary condition for sub domain
+    EqualityBC(const FunctionSpace& V,
+               const SubDomain& sub_domain);
+
+    // Create boundary condition for boundary data included in the mesh
+    EqualityBC(const FunctionSpace& V,
+               uint sub_domain);
+
+    // Destructor
+    ~EqualityBC();
+
+    // Apply boundary condition to a matrix
+    void apply(GenericMatrix& A) const;
+
+    // Apply boundary condition to a vector
+    void apply(GenericVector& b) const;
+
+    // Apply boundary condition to a linear system
+    void apply(GenericMatrix& A, GenericVector& b) const;
+
+    // Apply boundary condition to a vector for a nonlinear problem
+    void apply(GenericVector& b, const GenericVector& x) const;
+
+    // Apply boundary condition to a linear system for a nonlinear problem
+    void apply(GenericMatrix& A, GenericVector& b, const GenericVector& x) const;
+
+    // Initialize sub domain markers from sub domain
+    void init_from_sub_domain(const SubDomain& sub_domain);
+
+    // Initialize sub domain markers from mesh
+    void init_from_mesh(uint sub_domain);
+
+  private:
+
+    // Vector of equal dofs 
+    std::vector< uint > equal_dofs;
+
+  };
+
+}
+
+#endif
