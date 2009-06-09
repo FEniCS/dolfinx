5e8339178838fc71edad43da1f6a06db88bba2cc
Johannes Ring <johannr@simula.no>
2009-06-09 14:53:48 +0200

    Removed debug output (with wrong indentation) in MTL4 pkg-config generator.

--- scons/simula-scons/simula_scons/pkgconfiggenerators/mtl4.py
+++ scons/simula-scons/simula_scons/pkgconfiggenerators/mtl4.py
@@ -98,8 +98,6 @@ int main() {
 
 def generatePkgConf(directory=suitablePkgConfDir(), sconsEnv=None, **kwargs):
 
-    print "CHECK: MTL4"
-
   name = "MTL4"
   version, libs, cflags = pkgTests(sconsEnv=sconsEnv)
 
