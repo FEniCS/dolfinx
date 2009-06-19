19f8a2533a53e8713cd422788dd433356f8fa1e2
Benjamin Kehlet <benjamik@ifi.uio.no>
2009-06-19 12:49:51 +0200

    Minor optimization

--- dolfin/ode/MonoAdaptiveFixedPointSolver.cpp
+++ dolfin/ode/MonoAdaptiveFixedPointSolver.cpp
@@ -87,24 +87,43 @@ real MonoAdaptiveFixedPointSolver::iteration(real tol, uint iter,
   for (uint m = 0; m < method.qsize(); m++)
     ts.feval(m);
 
+  // Reset values to initial data
+  for (uint n = 0; n < method.nsize(); n++)
+  {
+    const uint noffset = n*ts.N;
+    for (uint i = 0; i < ts.N; i++)
+    {
+      ts.x[noffset + i] /= alpha;      
+      ts.x[noffset + i] += ts.u0[i] - ts.x[noffset+i];
+      ts.x[noffset + i] /= k;
+    }
+  }
+
   // Update the values at each stage
   for (uint n = 0; n < method.nsize(); n++)
   {
-    const uint noffset = n * ts.N;
+    const uint noffset = n*ts.N;
 
-    // Reset values to initial data
-    for (uint i = 0; i < ts.N; i++)
-      ts.x[noffset + i] += alpha*(ts.u0[i] - ts.x[noffset+i]);
-
-    // Add weights of right-hand side
-    for (uint m = 0; m < method.qsize(); m++)
+    for (uint i = 0; i < ts.N; i++) 
     {
-      const real tmp = k * method.nweight(n, m);
-      const uint moffset = m * ts.N;
-      for (uint i = 0; i < ts.N; i++)
-	ts.x[noffset + i] += alpha*tmp*ts.fq[moffset + i];
+      // Add weights of right-hand side
+      for (uint m = 0; m < method.qsize(); m++)
+      {
+	ts.x[noffset + i] += method.nweight(n,m) * ts.fq[m*ts.N + i];
+      }
     }
   }
+
+  // Reset values to initial data
+  for (uint n = 0; n < method.nsize(); n++)
+  {
+    const uint noffset = n*ts.N;
+    for (uint i = 0; i < ts.N; i++)
+    {
+      ts.x[noffset + i] *= alpha*k;
+    }
+  }
+  
   
   // Compute size of increment
   real max_increment = 0.0;
