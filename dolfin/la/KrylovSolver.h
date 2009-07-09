ce4532d453bf60976945db0aba197e57ed768f00
Garth N. Wells <gnw20@cam.ac.uk>
2009-07-09 12:53:06 +0100

    Add KrylovSolver.cpp

--- dolfin/la/KrylovSolver.h
+++ dolfin/la/KrylovSolver.h
@@ -10,133 +10,38 @@
 #ifndef __KRYLOV_SOLVER_H
 #define __KRYLOV_SOLVER_H
 
-#include <dolfin/common/Timer.h>
-#include <dolfin/parameter/Parameters.h>
-#include "GenericMatrix.h"
-#include "GenericVector.h"
-#include "uBLASKrylovSolver.h"
-#include "uBLASSparseMatrix.h"
-#include "uBLASDenseMatrix.h"
-#include "EpetraKrylovSolver.h"
-#include "ITLKrylovSolver.h"
-#include "MTL4Matrix.h"
-#include "MTL4Vector.h"
-#include "PETScKrylovSolver.h"
-#include "PETScMatrix.h"
-#include "PETScVector.h"
-#include "EpetraMatrix.h"
-#include "EpetraVector.h"
 #include "GenericLinearSolver.h"
 
 namespace dolfin
 {
 
-  /// This class defines an interface for a Krylov solver. The underlying
-  /// Krylov solver type is defined in default_type.h.
+  class GenericMatrix;
+  class GenericVector;
+  class Parameters;
+  class uBLASKrylovSolver;
+  class PETScKrylovSolver;
+  class EpetraKrylovSolver;
+  class ITLKrylovSolver;
+
+  /// This class defines an interface for a Krylov solver. The approproiate solver
+  /// is chosen on the basis of the matrix/vector type.
 
   class KrylovSolver : public GenericLinearSolver
   {
   public:
 
     /// Create Krylov solver
-    KrylovSolver(std::string solver_type = "default", std::string pc_type="default")
-      : solver_type(solver_type), pc_type(pc_type), ublas_solver(0), petsc_solver(0),
-        epetra_solver(0), itl_solver(0)
-    {
-      // Set default parameters
-      parameters = default_parameters();
-    }
+    KrylovSolver(std::string solver_type = "default", 
+                 std::string pc_type = "default");
 
     /// Destructor
-    ~KrylovSolver()
-    {
-      delete ublas_solver;
-      delete petsc_solver;
-      delete epetra_solver;
-      delete itl_solver;
-    }
+    ~KrylovSolver();
 
     /// Solve linear system Ax = b
-    uint solve(const GenericMatrix& A, GenericVector& x, const GenericVector& b)
-    {
-      Timer timer("Krylov solver");
-
-      if (A.has_type<uBLASSparseMatrix>())
-      {
-        if (!ublas_solver)
-        {
-          ublas_solver = new uBLASKrylovSolver(solver_type, pc_type);
-          ublas_solver->parameters.update(parameters);
-        }
-        return ublas_solver->solve(A, x, b);
-      }
-
-      if (A.has_type<uBLASDenseMatrix>())
-      {
-        if (!ublas_solver)
-        {
-          ublas_solver = new uBLASKrylovSolver(solver_type, pc_type);
-          ublas_solver->parameters.update(parameters);
-        }
-        return ublas_solver->solve(A.down_cast<uBLASDenseMatrix>(), 
-                                   x.down_cast<uBLASVector>(), 
-                                   b.down_cast<uBLASVector>());
-      }
-
-#ifdef HAS_PETSC
-      if (A.has_type<PETScMatrix>())
-      {
-        if (!petsc_solver)
-        {
-          petsc_solver = new PETScKrylovSolver(solver_type, pc_type);
-          petsc_solver->parameters.update(parameters);
-        }
-        return petsc_solver->solve(A, x, b);
-      }
-#endif
-#ifdef HAS_TRILINOS
-      if (A.has_type<EpetraMatrix>())
-      {
-        if (!epetra_solver)
-        {
-          epetra_solver = new EpetraKrylovSolver(solver_type, pc_type);
-          epetra_solver->parameters.update(parameters);
-        }
-        return epetra_solver->solve(A, x, b);
-      }
-#endif
-#ifdef HAS_MTL4
-      if (A.has_type<MTL4Matrix>())
-      {
-        if (!itl_solver)
-        {
-          itl_solver = new ITLKrylovSolver(solver_type, pc_type);
-          itl_solver->parameters.update(parameters);
-        }
-        return itl_solver->solve(A, x, b);
-      }
-#endif
-
-      error("No default Krylov solver for given backend");
-      return 0;
-    }
+    uint solve(const GenericMatrix& A, GenericVector& x, const GenericVector& b);
 
     /// Default parameter values
-    static Parameters default_parameters()
-    {
-      Parameters p("krylov_solver");
-
-      p.add("relative_tolerance",  1e-15);
-      p.add("absolute_tolerance",  1e-15);
-      p.add("divergence_limit",    1e4);
-      p.add("maximum_iterations",  10000);
-      p.add("gmres_restart",       30);
-      p.add("shift_nonzero",       0.0);
-      p.add("report",              true);
-      p.add("monitor_convergence", false);
-
-      return p;
-    }
+    static Parameters default_parameters();
 
   private:
 
@@ -146,25 +51,12 @@ namespace dolfin
     // Preconditioner type
     std::string pc_type;
 
-    // uBLAS solver
+    // Solvers
     uBLASKrylovSolver* ublas_solver;
-
-    // PETSc solver
-#ifdef HAS_PETSC
     PETScKrylovSolver* petsc_solver;
-#else
-    int* petsc_solver;
-#endif
-#ifdef HAS_TRILINOS
     EpetraKrylovSolver* epetra_solver;
-#else
-    int* epetra_solver;
-#endif
-#ifdef HAS_MTL4
     ITLKrylovSolver* itl_solver;
-#else
-    int* itl_solver;
-#endif
+
   };
 }
 
