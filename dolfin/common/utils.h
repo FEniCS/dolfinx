e7e32471efcac1c360e0b362503ba6ef33f5ed78
Anders Logg <logg@simula.no>
2009-08-09 22:46:45 +0200

    Add function for indenting block of text.

---
+++ dolfin/common/utils.h
@@ -0,0 +1,18 @@
+// Copyright (C) 2009 Anders Logg.
+// Licensed under the GNU LGPL Version 2.1.
+//
+// First added:  2009-08-09
+// Last changed: 2009-08-09
+
+#ifndef __UTILS_H
+#define __UTILS_H
+
+namespace dolfin
+{
+
+  // Indent string block
+  std::string indent(std::string block);
+
+}
+
+#endif
