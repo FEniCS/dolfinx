54dfaa58ff1739a013e3ef802af09659ec1ede71
Johan Hake <hake@simula.no>
2009-09-04 15:34:09 +0200

    Added __call__ operator to Function in PyDOLFIN
      We can now do stuff like:
    
      >>> result = f(x)
    
      where result is, well, the result. If scalar a scalar is returned. x can be
      any of tuple, list or numpy.array.
    
      >>> result = f(0.2,0.2)
    
      will also work
    
      The function can be evaluated into an already excisting numpy.array too.
    
      >>> values = zeros(3)
      >>> f(values,0.2,0.2,0.2)
    
      Added checks for when the call operator is used in ufl forms. The use of
      __call__ PyDOLFIN and ufl should be orthogonal on each others.

--- demo/function/eval/python/demo.py
+++ demo/function/eval/python/demo.py
@@ -1,9 +1,11 @@
 "Demonstrating function evaluation at arbitrary points."
 
 __author__ = "Anders Logg (logg@simula.no)"
-__date__ = "2008-03-17 -- 2008-12-12"
+__date__ = "2008-03-17 -- 2009-09-04"
 __copyright__ = "Copyright (C) 2008 Anders Logg"
 __license__  = "GNU LGPL Version 2.1"
+
+# Modified by Johan Hake, 2009
 
 from dolfin import *
 from numpy import array
@@ -14,20 +16,22 @@ if not has_gts():
 
 # Create mesh and a point in the mesh
 mesh = UnitCube(8, 8, 8);
-x = array((0.31, 0.32, 0.33))
-values = array((0.0,))
+x = (0.31, 0.32, 0.33)
 
 # A user-defined function
-V = FunctionSpace(mesh, "CG", 2)
-f = Function(V, "sin(3.0*x[0])*sin(3.0*x[1])*sin(3.0*x[2])")
+Vs = FunctionSpace(mesh, "CG", 2)
+Vv = VectorFunctionSpace(mesh, "CG", 2)
+fs = Function(Vs, "sin(3.0*x[0])*sin(3.0*x[1])*sin(3.0*x[2])")
+fv = Function(Vv, ("sin(3.0*x[0])*sin(3.0*x[1])*sin(3.0*x[2])",
+                   "1.0 + 3.0*x[0] + 4.0*x[1] + 0.5*x[2]","2"))
 
 # Project to a discrete function
-g = project(f, V)
+g = project(fs, Vs)
 
-# Evaluate user-defined function f
-f.eval(values, x)
-print "f(x) =", values[0]
-
-# Evaluate discrete function g (projection of f)
-g.eval(values, x)
-print "g(x) =", values[0]
+print """
+Evaluate user-defined scalar function fs
+fs(x) = %f
+Evaluate discrete function g (projection of fs)
+g(x) = %f
+Evaluate user-defined vector valued function fv
+fs(x) = %s"""%(fs(x),g(x),str(fv(x)))
