cc9c5814cc654e81e403de15b6d84e2f52c8dce9
Anders Logg <logg@simula.no>
2009-10-03 12:19:59 +0200

    Add missing file (to be renamed GenericFunction.cpp)

---
+++ dolfin/function/Coefficient.cpp
@@ -0,0 +1,59 @@
+// Copyright (C) 2009 Anders Logg.
+// Licensed under the GNU LGPL Version 2.1.
+//
+// First added:  2009-09-28
+// Last changed: 2009-10-03
+
+#include <dolfin/fem/FiniteElement.h>
+#include "Coefficient.h"
+
+using namespace dolfin;
+
+//-----------------------------------------------------------------------------
+Coefficient::Coefficient()
+{
+  // Do nothing
+}
+//-----------------------------------------------------------------------------
+Coefficient::~Coefficient()
+{
+  // Do nothing
+}
+//-----------------------------------------------------------------------------
+void Coefficient::evaluate(double* values,
+                           const double* coordinates,
+                           const ufc::cell& cell) const
+{
+  assert(values);
+  assert(coordinates);
+  assert(data.is_valid());
+
+  // Update coordinates
+  data.x = coordinates;
+
+  // Redirect to eval
+  eval(values, data);
+
+  // Reset coordinates
+  data.x = 0;
+}
+//-----------------------------------------------------------------------------
+void Coefficient::restrict_as_ufc_function(double* w,
+                                           const FiniteElement& element,
+                                           const Cell& dolfin_cell,
+                                           const ufc::cell& ufc_cell,
+                                           int local_facet) const
+{
+  assert(w);
+
+  // Update cell data
+  data.update(dolfin_cell, ufc_cell, local_facet);
+
+  // Evaluate each dof to get the expansion coefficients
+  for (uint i = 0; i < element.space_dimension(); ++i)
+    w[i] = element.evaluate_dof(i, *this, ufc_cell);
+
+  // Invalidate cell data
+  data.invalidate();
+}
+//-----------------------------------------------------------------------------
