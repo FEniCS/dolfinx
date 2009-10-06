7988ca70cc9a3ae1e4473485d6290ebaaf6f3808
Garth N. Wells <gnw20@cam.ac.uk>
2009-10-06 23:25:06 +0100

    Update a unit test.

--- test/unit/fem/python/test.py
+++ test/unit/fem/python/test.py
@@ -23,7 +23,7 @@ class Assembly(unittest.TestCase):
         n = V.cell().n
         h = CellSize(mesh)
         h_avg = (h('+') + h('-'))/2
-        f = Function(V, "500.0*exp(-(pow(x[0] - 0.5, 2) + pow(x[1] - 0.5, 2)) / 0.02)")
+        f = Expression("500.0*exp(-(pow(x[0] - 0.5, 2) + pow(x[1] - 0.5, 2)) / 0.02)", V = V)
 
         # Define bilinear form
         a = dot(grad(v), grad(u))*dx \
