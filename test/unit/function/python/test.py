694f8baaf703c0f9e8265443221bccab3edc3207
Johan Hake <hake@simula.no>
2009-10-31 23:29:25 +0100

    Fix unit test for Function

--- test/unit/function/python/test.py
+++ test/unit/function/python/test.py
@@ -1,7 +1,7 @@
 """Unit tests for the function library"""
 
 __author__ = "Anders Logg (logg@simula.no)"
-__date__ = "2007-05-24 -- 2009-10-06"
+__date__ = "2007-05-24 -- 2009-10-31"
 __copyright__ = "Copyright (C) 2007 Anders Logg"
 __license__  = "GNU LGPL Version 2.1"
 
@@ -96,14 +96,12 @@ class Eval(unittest.TestCase):
           
 class Instantiation(unittest.TestCase):
      def testSameBases(self):
-          c = Constant(mesh,2)
           f0 = Expression("2", V = V)
           class MyConstant(Expression):
                cpparg = "2"
           
           f1 = MyConstant(V = V)
 
-          self.assertEqual(type(f0).__bases__,type(c).__bases__)
           self.assertNotEqual(type(f0).__bases__,type(f1).__bases__)
 
           f2, f3 = Expressions("2", V, "3", V)
