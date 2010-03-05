7ac18494e824b48719b23d2f10fe2a96115039b1
Anders Logg <logg@simula.no>
2010-03-05 11:25:26 +0100

    Formatting fixes in PrimitiveIntersection class. Andre, please look at the changes and go over your other classes and make the corresponding changes: line length, placement of &, copyright notice, spaces etc.

--- dolfin/mesh/PrimitiveIntersector.h
+++ dolfin/mesh/PrimitiveIntersector.h
@@ -1,58 +1,62 @@
-// =====================================================================================
-//
-// Copyright (C) 2010-02-09  André Massing
+// Copyright (C) 2010 André Massing.
 // Licensed under the GNU LGPL Version 2.1.
-//
-// Modified by André Massing, 2010
 //
 // First added:  2010-02-09
 // Last changed: 2010-03-05
-//
-//Author:  André Massing (am), massing@simula.no
-//Company:  Simula Research Laboratory, Fornebu, Norway
-//
-// =====================================================================================
 
-#ifndef  __PRIMITIVEINTERSECTOR_H
-#define  __PRIMITIVEINTERSECTOR_H
+#ifndef  __PRIMITIVE_INTERSECTOR_H
+#define  __PRIMITIVE_INTERSECTOR_H
 
 namespace dolfin
 {
   class MeshEntity;
   class Point;
 
-  /// This class implements an intersection detection, detecting whether two given (arbitrary) meshentities intersect.
+  /// This class implements an intersection detection, detecting
+  /// whether two given (arbitrary) meshentities intersect.
+
   class PrimitiveIntersector
   {
   public:
 
-      ///Computes whether two mesh entities intersect. Uses an inexact geometry
-      ///kernel which is faster but may suffer from floating point precision
-      //issues.
-      static bool do_intersect(const MeshEntity & entity_1, const MeshEntity & entity_2);
-      static bool do_intersect(const MeshEntity  & entity_1, const Point & point);
+    /// Computes whether two mesh entities intersect using an inexact
+    /// geometry kernel which is fast but may suffer from floating
+    /// point precision
+    static bool do_intersect(const MeshEntity& entity_1,
+                             const MeshEntity& entity_2);
 
-      ///Computes whether two mesh entities intersect. Uses an exact geometry
-      ///kernel which is slower but detects intersection always correctly.
-      static bool do_intersect_exact(const MeshEntity & entity_1, const MeshEntity & entity_2);
-      static bool do_intersect_exact(const MeshEntity & entity_1, const Point & point);
-    private:
+    /// Computes whether a mesh entity and point intersect using an
+    /// inexact geometry kernel which is fast but may suffer from
+    /// floating point precision
+    static bool do_intersect(const MeshEntity& entity_1,
+                             const Point& point);
 
-      //@{
-      ///Helper classes to deal with all combination in a N and not N*N way.
-      ///Just declaration, definition and instantation takes place in the corresponding cpp file, where
-      ///this helper function are actually needed.
+    /// Computes whether two mesh entities intersect using an exact
+    /// geometry kernel which is slow but always correct
+    static bool do_intersect_exact(const MeshEntity& entity_1,
+                                   const MeshEntity& entity_2);
 
-      template <typename K, typename T, typename U >
-      static bool do_intersect_with_kernel(const T & entity_1, const U & entity_2);
+    /// Computes whether a mesh entity and point intersect using an
+    /// exact geometry kernel which is slow but always correct
+    static bool do_intersect_exact(const MeshEntity& entity_1,
+                                   const Point& point);
 
-      template<typename K, typename T>
-      static bool do_intersect_with_kernel(const T & entity_1, const MeshEntity & entity_2);
+  private:
 
-      //@}
+    // Helper classes to deal with all combination in a N and not N*N way.
+    // Just declaration, definition and instantation takes place in the corresponding cpp file, where
+    // this helper function are actually needed.
+
+    template <typename K, typename T, typename U >
+    static bool do_intersect_with_kernel(const T& entity_1,
+                                         const U& entity_2);
+
+    template<typename K, typename T>
+    static bool do_intersect_with_kernel(const T& entity_1,
+                                         const MeshEntity& entity_2);
 
   };
 
 }
 
-#endif   // ----- #ifndef __PRIMITIVEINTERSECTOR_H  -----
+#endif
