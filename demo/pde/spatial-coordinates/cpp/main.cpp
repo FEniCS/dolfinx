3b59e79ad0e8bef79231492ddc4d0341c7d69d3e
Kristian B. Ølgaard <k.b.oelgaard@gmail.com>
2010-03-05 16:54:31 +0100

    Added a demo that solves Poisson's equation using SpatialCoordinates to express the source and flux terms.

---
+++ demo/pde/spatial-coordinates/cpp/main.cpp
@@ -0,0 +1,67 @@
+// Copyright (C) 2010 Kristian B. Oelgaard.
+// Licensed under the GNU LGPL Version 2.1.
+//
+// First added:  2010-03-05
+// Last changed: 2010-03-05
+//
+// This demo program solves Poisson's equation
+//
+//     - div grad u(x, y) = f(x, y)
+//
+// on the unit square with source f given by
+//
+//     f(x, y) = 10*exp(-((x - 0.5)^2 + (y - 0.5)^2) / 0.02)
+//
+// and boundary conditions given by
+//
+//     u(x, y) = 0        for x = 0 or x = 1
+// du/dn(x, y) = sin(5*x) for y = 0 or y = 1
+//
+// This demo is identical to the Poisson demo with the only difference that
+// the source and flux term is expressed using SpatialCoordinates in the
+// variational formulation.
+
+#include <dolfin.h>
+#include "SpatialCoordinates.h"
+
+using namespace dolfin;
+
+// Sub domain for Dirichlet boundary condition
+class DirichletBoundary : public SubDomain
+{
+  bool inside(const double* x, bool on_boundary) const
+  {
+    return x[0] < DOLFIN_EPS or x[0] > 1.0 - DOLFIN_EPS;
+  }
+};
+
+int main()
+{
+  // Create mesh and function space
+  UnitSquare mesh(32, 32);
+  SpatialCoordinates::FunctionSpace V(mesh);
+
+  // Define boundary condition
+  Constant u0(0.0);
+  DirichletBoundary boundary;
+  DirichletBC bc(V, u0, boundary);
+
+  // Define variational problem
+  SpatialCoordinates::BilinearForm a(V, V);
+  SpatialCoordinates::LinearForm L(V);
+
+  // Compute solution
+  VariationalProblem problem(a, L, bc);
+  problem.parameters["linear_solver"] = "iterative";
+  Function u(V);
+  problem.solve(u);
+
+  // Save solution in VTK format
+  File file("spatial-coordinates.pvd");
+  file << u;
+
+  // Plot solution
+  plot(u);
+
+  return 0;
+}
---
+++ demo/pde/spatial-coordinates/python/demo.py
@@ -0,0 +1,57 @@
+"""This demo program solves Poisson's equation
+
+    - div grad u(x, y) = f(x, y)
+
+on the unit square with source f given by
+
+    f(x, y) = 10*exp(-((x - 0.5)^2 + (y - 0.5)^2) / 0.02)
+
+and boundary conditions given by
+
+    u(x, y) = 0        for x = 0 or x = 1
+du/dn(x, y) = sin(5*x) for y = 0 or y = 1
+
+This demo is identical to the Poisson demo with the only difference that
+the source and flux term is expressed using SpatialCoordinates in the
+variational formulation.
+"""
+
+__author__ = "Kristian B. Oelgaard (k.b.oelgaard@gmail.com)"
+__date__ = "2010-03-05 -- 2010-03-05"
+__copyright__ = "Copyright (C) 2010 Kristian B. Oelgaard"
+__license__  = "GNU LGPL Version 2.1"
+
+from dolfin import *
+
+# Create mesh and define function space
+mesh = UnitSquare(32, 32)
+V = FunctionSpace(mesh, "CG", 1)
+
+# Define Dirichlet boundary (x = 0 or x = 1)
+def boundary(x):
+    return x[0] < DOLFIN_EPS or x[0] > 1.0 - DOLFIN_EPS
+
+# Define boundary condition
+u0 = Constant(0.0)
+bc = DirichletBC(V, u0, boundary)
+
+# Define variational problem
+v = TestFunction(V)
+u = TrialFunction(V)
+d_x = triangle.x[0] - 0.5
+d_y = triangle.x[1] - 0.5
+f = 10.0*exp(-(d_x*d_x + d_y*d_y) / 0.02)
+g = sin(5.0*triangle.x[0])
+a = inner(grad(v), grad(u))*dx
+L = v*f*dx + v*g*ds
+
+# Compute solution
+problem = VariationalProblem(a, L, bc)
+u = problem.solve()
+
+# Save solution in VTK format
+file = File("spatial-coordinates.pvd")
+file << u
+
+# Plot solution
+plot(u, interactive=True)
