b174d55e4bfc4be53b661d6afce410e6c37443c7
Anders Logg <logg@simula.no>
2010-03-05 11:15:17 +0100

    Add MeshEntity::intersects_exactly and remap old intersects

--- dolfin/mesh/PrimitiveIntersector.h
+++ dolfin/mesh/PrimitiveIntersector.h
@@ -6,8 +6,8 @@
 // Modified by André Massing, 2010
 //
 // First added:  2010-02-09
-// Last changed: 2010-02-19
-// 
+// Last changed: 2010-03-05
+//
 //Author:  André Massing (am), massing@simula.no
 //Company:  Simula Research Laboratory, Fornebu, Norway
 //
@@ -24,7 +24,8 @@ namespace dolfin
   /// This class implements an intersection detection, detecting whether two given (arbitrary) meshentities intersect.
   class PrimitiveIntersector
   {
-    public:
+  public:
+
       ///Computes whether two mesh entities intersect. Uses an inexact geometry
       ///kernel which is faster but may suffer from floating point precision
       //issues.
@@ -36,16 +37,16 @@ namespace dolfin
       static bool do_intersect_exact(const MeshEntity & entity_1, const MeshEntity & entity_2);
       static bool do_intersect_exact(const MeshEntity & entity_1, const Point & point);
     private:
-     
+
       //@{
-      ///Helper classes to deal with all combination in a N and not N*N way. 
+      ///Helper classes to deal with all combination in a N and not N*N way.
       ///Just declaration, definition and instantation takes place in the corresponding cpp file, where
       ///this helper function are actually needed.
 
       template <typename K, typename T, typename U >
       static bool do_intersect_with_kernel(const T & entity_1, const U & entity_2);
-       
-      template<typename K, typename T> 
+
+      template<typename K, typename T>
       static bool do_intersect_with_kernel(const T & entity_1, const MeshEntity & entity_2);
 
       //@}
