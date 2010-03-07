b5e9b87ee3b89ca9a22491337240ce2bc958d775
Johan Hake <hake.dev@gmail.com>
2010-03-07 10:10:56 -0800

    Added greedy (argument name independent) typemaps for all const Array<{uint,int,double}> & input arguments    - All const Arrays of the above primitives can now receive a NumPy array      as input in PyDOLFIN.    - Typemaps for non const arguments still needs to be instantiate explicitly      per argument name.

--- dolfin/la/GenericVector.h
+++ dolfin/la/GenericVector.h
@@ -5,9 +5,10 @@
 // Modified by Kent-Andre Mardal, 2008.
 // Modified by Ola Skavhaug, 2008.
 // Modified by Martin Sandve Alnes, 2009.
+// Modified by Johan Hake, 2009-2010.
 //
 // First added:  2006-04-25
-// Last changed: 2010-02-08
+// Last changed: 2010-03-07
 
 #ifndef __GENERIC_VECTOR_H
 #define __GENERIC_VECTOR_H
@@ -16,6 +17,8 @@
 #include <utility>
 #include <vector>
 #include <boost/lambda/lambda.hpp>
+#include <dolfin/common/Array.h>
+
 #include "GenericSparsityPattern.h"
 #include "GenericTensor.h"
 
@@ -133,8 +136,8 @@ namespace dolfin
 
     /// Return sum of selected rows in vector. Each process sums its local
     //// entries. Off process entries are ignored.
-    virtual double sum(const std::vector<uint>& rows) const
-    { error("GenericVector::sum(const std::vector<uint>& rows) not implemented by backend"); return 0.0; }
+    virtual double sum(const Array<uint>& rows) const
+    { error("GenericVector::sum(const Array<uint>& rows) not implemented by backend"); return 0.0; }
 
     /// Multiply vector by given number
     virtual const GenericVector& operator*= (double a) = 0;
