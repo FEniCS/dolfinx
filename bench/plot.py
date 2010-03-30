8ec26d40cde34a891bcd204917e3caed9b9210c1
Anders Logg <logg@simula.no>
2010-03-30 11:19:32 +0200

    Add empty plot script (with some instructions for Johannes). This is the script that should be running on the benchbot to present the results computed by bench.py.
      bench.py --> logs/bench.log --> plot.py

---
+++ bench/plot.py
@@ -0,0 +1,5 @@
+print """
+This script should parse logs/bench.log and create one plot for each
+case with the timings function of time (dateplot). The description of
+the benchmark should also be included in the plot. (FIXME --> Johannes)
+"""
