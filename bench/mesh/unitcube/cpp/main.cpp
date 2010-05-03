5e9a73b35b1ddb59b046bedb2c8962723eaf1c64
Anders Logg <logg@simula.no>
2010-05-03 10:32:12 +0200

    More parameter adjustments for mesh portion of bencmarks. Look ok now. Started moving all C++ demos to cpp subdirectories. Python versions can be added later.

--- bench/mesh/unitcube/cpp/main.cpp
+++ bench/mesh/unitcube/cpp/main.cpp
@@ -2,14 +2,14 @@
 // Licensed under the GNU LGPL Version 2.1.
 //
 // First added:  2006-11-01
-// Last changed: 2010-03-30
+// Last changed: 2010-05-03
 
 #include <dolfin.h>
 
 using namespace dolfin;
 
-#define NUM_REPS 20
-#define SIZE 32
+#define NUM_REPS 10
+#define SIZE 128
 
 int main(int argc, char* argv[])
 {
