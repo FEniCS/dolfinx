0ec9cf7c896fde794d0f2e481ce015171582dac9
Anders Logg <logg@simula.no>
2010-05-03 10:26:50 +0200

    Adjust parameters for bench-mesh-refinement

--- bench/mesh/refinement/main.cpp
+++ bench/mesh/refinement/main.cpp
@@ -2,13 +2,13 @@
 // Licensed under the GNU LGPL Version 2.1.
 //
 // First added:  2006-11-01
-// Last changed: 2010-04-05
+// Last changed: 2010-05-03
 
 #include <dolfin.h>
 
 using namespace dolfin;
 
-#define NUM_REPS 3
+#define NUM_REPS 5
 #define SIZE 4
 
 int main(int argc, char* argv[])
@@ -21,11 +21,13 @@ int main(int argc, char* argv[])
   UnitCube unitcube_mesh(SIZE, SIZE, SIZE);
   Mesh mesh(unitcube_mesh);
 
+  tic();
   for (int i = 0; i < NUM_REPS; i++)
   {
     mesh = refine(mesh);
     dolfin::cout << "Refined mesh: " << mesh << dolfin::endl;
   }
+  info("BENCH %g", toc());
 
   return 0;
 }
