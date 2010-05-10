2979a24f765cfb4b56e4e00bd0dc82340b91269a
Marie E. Rognes <meg@simula.no>
2010-05-10 13:48:26 +0200

    Added python and cpp(!) demos for Poisson problem with pure Neumann boundary conditions.

---
+++ demo/pde/neumann-poisson/cpp/main.cpp
@@ -0,0 +1,79 @@
+// Copyright (C) 2010 Marie E. Rognes
+// Licensed under the GNU LGPL Version 2.1.
+//
+// Last changed: 2010-05-10
+//
+// This demo program illustrates how to solve Poisson's equation
+//
+//     - div grad u(x, y) = f(x, y)
+//
+// on the unit square with pure Neumann boundary conditions:
+//
+//     du/dn(x, y) = -sin(5*x)
+//
+// and source f given by
+//
+//     f(x, y) = 10*exp(-((x - 0.5)^2 + (y - 0.5)^2) / 0.02)
+//
+// Since only Neumann conditions are applied, u is only determined up to
+// a constant c by the above equations. An addition constraint is thus
+// required, for instance
+//
+//   \int u = 0
+//
+// This can be accomplished by introducing the constant c as an
+// additional unknown (to be sought in the space of real numbers)
+// and the above constraint.
+
+#include <dolfin.h>
+#include "Poisson.h"
+
+using namespace dolfin;
+
+// Source term (right-hand side)
+class Source : public Expression
+{
+  void eval(Array<double>& values, const Array<double>& x) const
+  {
+    double dx = x[0] - 0.5;
+    double dy = x[1] - 0.5;
+    values[0] = 10*exp(-(dx*dx + dy*dy) / 0.02);
+  }
+};
+
+// Boundary flux (Neumann boundary condition)
+class Flux : public Expression
+{
+  void eval(Array<double>& values, const Array<double>& x) const
+  {
+    values[0] = -sin(5*x[0]);
+  }
+};
+
+int main()
+{
+  // Create mesh and function space
+  UnitSquare mesh(64, 64);
+  Poisson::FunctionSpace V(mesh);
+
+  // Define variational problem
+  Poisson::BilinearForm a(V, V);
+  Poisson::LinearForm L(V);
+  Source f;
+  Flux g;
+  L.f = f;
+  L.g = g;
+
+  // Compute solution
+  VariationalProblem problem(a, L);
+  problem.parameters["linear_solver"] = "iterative";
+  Function w(V);
+  problem.solve(w);
+
+  Function u = w[0];
+
+  // Plot solution
+  plot(u);
+
+  return 0;
+}
---
+++ demo/pde/neumann-poisson/python/demo.py
@@ -0,0 +1,51 @@
+"""
+This demo program illustrates how to solve Poisson's equation
+
+    - div grad u(x, y) = f(x, y)
+
+on the unit square with pure Neumann boundary conditions:
+
+    du/dn(x, y) = -sin(5*x)
+
+and source f given by
+
+    f(x, y) = 10*exp(-((x - 0.5)^2 + (y - 0.5)^2) / 0.02)
+
+Since only Neumann conditions are applied, u is only determined up to
+a constant c by the above equations. An addition constraint is thus
+required, for instance
+
+  \int u = 0
+
+This can be accomplished by introducing the constant c as an
+additional unknown (to be sought in the space of real numbers)
+and the above constraint.
+"""
+
+__author__ = "Marie E. Rognes (meg@simula.no)"
+__date__ = "2010-05-10"
+__copyright__ = "Copyright (C) 2010 Marie E. Rognes"
+__license__  = "GNU LGPL Version 2.1"
+
+from dolfin import *
+
+# Create mesh and define function space
+mesh = UnitSquare(64, 64)
+V = FunctionSpace(mesh, "CG", 1)
+R = FunctionSpace(mesh, "R", 0)
+W = V * R
+
+# Define variational problem
+(v, d) = TestFunctions(W)
+(u, c) = TrialFunction(W)
+f = Expression("10*exp(-(pow(x[0] - 0.5, 2) + pow(x[1] - 0.5, 2)) / 0.02)")
+g = Expression("-sin(5*x[0])")
+a = (inner(grad(v), grad(u)) + v*c + d*u)*dx
+L = v*f*dx + v*g*ds
+
+# Compute solution
+problem = VariationalProblem(a, L)
+(u, c) = problem.solve()
+
+# Plot solution
+plot(u, interactive=True)
