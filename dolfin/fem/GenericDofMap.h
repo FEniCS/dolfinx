51f88626dd998e39d6453993ad623fcc6fc5dc44
Garth N. Wells <gnw20@cam.ac.uk>
2010-05-26 10:57:22 +0100

    Remove uncessary function from GenericDofMap.
    I'll split the dolfin::DofMap code in two soon - it's messy have UFC-based dofmaps and std::vector-based dof maps in the same class.

--- dolfin/fem/GenericDofMap.h
+++ dolfin/fem/GenericDofMap.h
@@ -1,7 +1,7 @@
 // Copyright (C) 2010 Anders Logg and Garth N. Wells.
 // Licensed under the GNU LGPL Version 2.1.
 //
-// First added:  2010-05-24
+// First added:  2010-05-26
 // Last changed:
 
 #ifndef __GENERIC_DOF_MAP_H
@@ -39,7 +39,8 @@ namespace dolfin
     /// Return the dimension of the global finite element function space
     virtual unsigned int global_dimension() const = 0;
 
-    /// Return the dimension of the local finite element function space on a cell
+    /// Return the dimension of the local finite element function space on a
+    /// cell
     virtual unsigned int local_dimension(const ufc::cell& cell) const = 0;
 
     /// Return the maximum dimension of the local finite element function space
@@ -51,32 +52,37 @@ namespace dolfin
     /// Return number of facet dofs
     virtual unsigned int num_facet_dofs() const = 0;
 
-    /// Tabulate the local-to-global mapping of dofs on a cell (UFC cell version)
-    virtual void tabulate_dofs(uint* dofs, const ufc::cell& ufc_cell, uint cell_index) const = 0;
+    /// Tabulate the local-to-global mapping of dofs on a cell
+    /// (UFC cell version)
+    virtual void tabulate_dofs(uint* dofs, const ufc::cell& ufc_cell,
+                               uint cell_index) const = 0;
 
-    /// Tabulate the local-to-global mapping of dofs on a cell (DOLFIN cell version)
+    /// Tabulate the local-to-global mapping of dofs on a cell
+    /// (DOLFIN cell version)
     virtual void tabulate_dofs(uint* dofs, const Cell& cell) const = 0;
 
     /// Tabulate local-local facet dofs
     virtual void tabulate_facet_dofs(uint* dofs, uint local_facet) const = 0;
 
     /// Tabulate the coordinates of all dofs on a cell (UFC cell version)
-    virtual void tabulate_coordinates(double** coordinates, const ufc::cell& ufc_cell) const = 0;
+    virtual void tabulate_coordinates(double** coordinates,
+                                      const ufc::cell& ufc_cell) const = 0;
 
     /// Tabulate the coordinates of all dofs on a cell (DOLFIN cell version)
-    virtual void tabulate_coordinates(double** coordinates, const Cell& cell) const = 0;
-
-    /// Test whether dof map has been renumbered
-    virtual bool renumbered() const = 0;
+    virtual void tabulate_coordinates(double** coordinates,
+                                      const Cell& cell) const = 0;
 
     /// Extract sub dofmap component
-    virtual GenericDofMap* extract_sub_dofmap(const std::vector<uint>& component, const Mesh& dolfin_mesh) const = 0;
+    virtual GenericDofMap* extract_sub_dofmap(const std::vector<uint>& component,
+                                              const Mesh& dolfin_mesh) const = 0;
 
     /// "Collapse" a sub dofmap
-    virtual GenericDofMap* collapse(std::map<uint, uint>& collapsed_map, const Mesh& dolfin_mesh) const = 0;
+    virtual GenericDofMap* collapse(std::map<uint, uint>& collapsed_map,
+                                    const Mesh& dolfin_mesh) const = 0;
 
     /// Return the set of dof indices
-    virtual Set<dolfin::uint> dofs(const Mesh& mesh, bool sort = false) const = 0;
+    virtual Set<dolfin::uint> dofs(const Mesh& mesh,
+                                   bool sort = false) const = 0;
 
     /// Return informal string representation (pretty-print)
     virtual std::string str(bool verbose) const = 0;
