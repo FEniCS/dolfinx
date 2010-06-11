966dc809c1f2155fa609c456e35bd049bb7f3e41
Andre Massing <massing@simula.no>
2010-06-11 14:00:11 +0200

    Small modification in eval benchmark.

--- bench/function/evaluation/cpp/main.cpp
+++ bench/function/evaluation/cpp/main.cpp
@@ -6,7 +6,7 @@
 // Modified by André Massing, 2010
 //
 // First added:  2010-06-10
-// Last changed: 2010-06-10
+// Last changed: 2010-06-11
 // 
 //Author:  André Massing (am), massing@simula.no
 //Company:  Simula Research Laboratory, Fornebu, Norway
@@ -15,7 +15,6 @@
 
 #include <dolfin.h>
 #include "P1.h"
-#include "P3.h"
 
 using namespace dolfin;
 using dolfin::uint;
@@ -37,28 +36,25 @@ int main(int argc, char* argv[])
   not_working_in_parallel("Function evalutation benchmark");
   
   const uint mesh_max_size = 32;
-  const uint num_points  = 100000;
+  const uint num_points  = 10000000;
   
+  //starting timing
+  tic();  
   for (uint N = 10; N < mesh_max_size; N += 2)
   {
     UnitCube mesh(N, N, N);
 
-    // Create function spaces
-    P3::FunctionSpace V0(mesh);
-
-    // Create functions
+    P1::FunctionSpace V0(mesh);
     Function f0(V0);
-
     F f;
     f0.interpolate(f);
 
     Array<double> X(3);
     Array<double> value(1);
 
+    //Initialize random generator
     srand(1);
-    
-    //starting timing
-    tic();  
+
     for (uint i = 1; i <= num_points; ++i)
     {
       X[0] = std::rand()/static_cast<double>(RAND_MAX);
@@ -66,12 +62,12 @@ int main(int argc, char* argv[])
       X[2] = std::rand()/static_cast<double>(RAND_MAX);
 
       f.eval(value, X);
-      //use X variable.
     }
-    info("x = %.12e\ty = %.12e\tz = %.12e\tf(x) = %.12e",X[0],X[1],X[2],value[0]);
-    info("BENCH function_evalution %g",toc()); 
-  }
 
+    //use X variable.
+    info("x = %.12e\ty = %.12e\tz = %.12e\tf(x) = %.12e",X[0],X[1],X[2],value[0]);
+  }
+  info("BENCH  %g",toc()); 
 
 return 0;
 
