0d32e7cb5c0033e4ca181fa205403898c7148925
Garth N. Wells <gnw20@cam.ac.uk>
2010-07-23 11:47:32 +0100

    Small change on Hypre precon settings.

--- dolfin/la/PETScPreconditioner.cpp
+++ dolfin/la/PETScPreconditioner.cpp
@@ -80,14 +80,14 @@ void PETScPreconditioner::set(PETScKrylovSolver& solver) const
       PCHYPRESetType(pc, "euclid");
     else
       error("Requested Hypre preconditioner unknown (Note: pilut is not supported).");
+
+    PCFactorSetLevels(pc, parameters["ilu_fill_level"]);
     PCSetFromOptions(pc);
     #else
     warning("PETSc has not been compiled with the HYPRE library for "
             "algebraic multigrid. Default PETSc solver will be used. "
             "For performance, installation of HYPRE is recommended.");
     #endif
-
-    PCFactorSetLevels(pc, parameters["ilu_fill_level"]);
 
     return;
   }
