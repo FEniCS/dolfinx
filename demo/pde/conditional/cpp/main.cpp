c1c698bd6670d39d4b47101135134834ad7cc41a
Kristian B. Ølgaard <k.b.oelgaard@gmail.com>
2010-07-24 11:37:49 +0100

    Add source term to a triangular domain in conditional demo.

--- demo/pde/conditional/cpp/main.cpp
+++ demo/pde/conditional/cpp/main.cpp
@@ -2,7 +2,7 @@
 // Licensed under the GNU GPL version 3 or any later version
 //
 // First added:  2010-07-23
-// Last changed: 2010-07-23
+// Last changed: 2010-07-22
 //
 // This demo program solves Poisson's equation
 //
@@ -12,6 +12,7 @@
 //
 //    f(x, y) =    -1.0 if (x - 0.33)^2 + (y - 0.67)^2 < 0.015
 //                  5.0 if 0.015 < (x - 0.33)^2 + (y - 0.67)^2 < 0.025
+//                 -1.0 if (x,y) in triangle( (0.55, 0.05), (0.95, 0.45), (0.55, 0.45) )
 //                  0.0 otherwise
 //
 // and homogeneous Dirichlet boundary conditions.
--- demo/pde/conditional/python/demo.py
+++ demo/pde/conditional/python/demo.py
@@ -6,13 +6,14 @@ on the unit square with source f given by
 
     f(x, y) =    -1.0 if (x - 0.33)^2 + (y - 0.67)^2 < 0.015
                   5.0 if 0.015 < (x - 0.33)^2 + (y - 0.67)^2 < 0.025
+                 -1.0 if (x,y) in triangle( (0.55, 0.05), (0.95, 0.45), (0.55, 0.45) )
                   0.0 otherwise
 
 and homogeneous Dirichlet boundary conditions.
 """
 
 __author__ = "Kristian B. Oelgaard (k.b.oelgaard@gmail.com)"
-__date__ = "2010-07-23 -- 2010-07-23"
+__date__ = "2010-07-23 -- 2010-07-24"
 __copyright__ = "Copyright (C) 2010 Kristian B. Oelgaard"
 __license__  = "GNU GPL Version 3.0 or later"
 
@@ -34,8 +35,16 @@ bc = DirichletBC(V, u0, boundary)
 v = TestFunction(V)
 u = TrialFunction(V)
 x = V.cell().x
+
 c0 = conditional(le( (x[0]-0.33)**2 + (x[1]-0.67)**2,  0.015), -1.0, 5.0)
-f = conditional( le( (x[0]-0.33)**2 + (x[1]-0.67)**2,  0.025), c0, 0.0 )
+c = conditional( le( (x[0]-0.33)**2 + (x[1]-0.67)**2,  0.025), c0, 0.0 )
+
+t0 = conditional(ge( x[0],  0.55), -1.0, 0.0)
+t1 = conditional(le( x[0],  0.95), t0, 0.0)
+t2 = conditional(ge( x[1],  0.05), t1, 0.0)
+t3 = conditional(le( x[1],  0.45), t2, 0.0)
+t = conditional(ge( x[1] - x[0] - 0.05 + 0.55,  0.0), t3, 0.0)
+f = c + t
 a = inner(grad(v), grad(u))*dx
 L = v*f*dx
 
