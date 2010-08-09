4fc63e7c1bc6b8df1d2e1a74ae327082d8bfa35a
Garth N. Wells <gnw20@cam.ac.uk>
2010-08-09 22:35:58 +0100

    Compile MTL4 test program with CMake.

--- cmake/FindMTL4.dolfin.cmake
+++ cmake/FindMTL4.dolfin.cmake
@@ -10,13 +10,32 @@ find_path(MTL4_INCLUDE_DIR boost/numeric/mtl/mtl.hpp
   )
 
 if(MTL4_INCLUDE_DIR)
-  set(MTL4_FOUND 1)
+  include(CheckCXXSourceRuns)
+  set(CMAKE_REQUIRED_INCLUDES ${MTL4_INCLUDE_DIR})
+  check_cxx_source_runs("
+#include <iostream>
+#include <boost/numeric/mtl/mtl.hpp>
+int main()
+{
+  mtl::dense_vector<double> x(10);
+  int size = mtl::num_rows(x);
+  return 0;
+}
+"
+  MTL4_TEST_RUNS)
+
+  if(NOT MTL4_TEST_RUNS)
+    message("MTL4 was found but a test program could not be run.")
+  endif(NOT MTL4_TEST_RUNS)
+
 endif(MTL4_INCLUDE_DIR)
 
-if(MTL4_FOUND)
+if(MTL4_TEST_RUNS)
   message(STATUS "  found package MTL4")
-  set(CMAKE_REQUIRED_INCLUDES ${MTL4_INCLUDE_DIR})
+  set(MTL4_FOUND 1)
+  include_directories(${MTL4_INCLUDE_DIR})
   add_definitions(-DHAS_MTL4)
-else(MTL4_FOUND)
-  message(STATUS "  package 'MTL4' not found")
-endif(MTL4_FOUND)
+else(MTL4_TEST_RUNS)
+  message(STATUS "  package 'MTL4' could not be configured.")
+endif(MTL4_TEST_RUNS)
+
