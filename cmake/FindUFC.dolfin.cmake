cbf1c2d5fef6fd71bd404826f413e2c1564700d5
Anders Logg <logg@simula.no>
2010-08-16 10:36:06 +0200

    Fatal error when required package UFC is not found. Is this how we should do it? It seems strange to continue the configuration when a required package is not found.

--- cmake/FindUFC.dolfin.cmake
+++ cmake/FindUFC.dolfin.cmake
@@ -1,9 +1,15 @@
 # Try to find UFC - the unified framework for finite element assembly
 #
-# This module defines
-# UFC_FOUND - system has UFC
-# UFC_INCLUDE_DIRS - the UFC include directory
-# UFC_VERSION - the UFC version string (MAJOR.MINOR.MICRO)
+# This module defines the following variables:
+#
+#   UFC_FOUND        - system has UFC
+#   UFC_INCLUDE_DIRS - UFC include directory
+#   UFC_VERSION      - UFC version string (MAJOR.MINOR.MICRO)
 
 include(FindPkgConfig)
 pkg_check_modules(UFC REQUIRED ufc-1>=1.4.1)
+
+# UFC is a required package
+if(NOT $UFC_FOUND)
+  message(FATAL_ERROR "*** Unable to configure package 'UFC'.")
+endif(NOT $UFC_FOUND)
