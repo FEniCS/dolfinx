c0f702cc1bd38cb49eeef5d4f6f53af7a1bbf5c9
Garth N. Wells <gnw20@cam.ac.uk>
2010-08-19 14:48:42 +0100

    Move finding Boost back into top level CMakeLists.txt.

--- cmake/FindBoost.dolfin.cmake
+++
@@ -1,6 +0,0 @@
-# - Try to find Boost
-
-set(Boost_ADDITIONAL_VERSIONS 1.43 1.43.0)
-set(Boost_USE_STATIC_LIBS OFF)
-set(BOOST_ROOT $ENV{BOOST_DIR})
-find_package(Boost 1.36 COMPONENTS filesystem program_options system REQUIRED)
