500d243285ed9a33cb7416e0b72f36f5607f9261
Anders Logg <logg@simula.no>
2010-08-23 23:33:52 +0200

    Add Boost stuff to MTL4 test, now runs on Snow Leopard

--- cmake/FindMTL4.cmake
+++ cmake/FindMTL4.cmake
@@ -1,8 +1,8 @@
 # - Try to find MTL4
 # Once done this will define
 #
-#  MTL4_FOUND       - system has found MTL4
-#  MTL4_INCLUDE_DIR - include directories for MTL4
+#  MTL4_FOUND       - system has MTL4
+#  MTL4_INCLUDE_DIRS - include directories for MTL4
 
 message(STATUS "Checking for package 'MTL4'")
 
@@ -12,11 +12,14 @@ find_path(MTL4_INCLUDE_DIR boost/numeric/mtl/mtl.hpp
   DOC "Directory where the MTL4 header is located"
   )
 
+# Add Boost, needed by MTL4
+set(MTL4_INCLUDE_DIRS "${MTL4_INCLUDE_DIR};${Boost_INCLUDE_DIR}")
+
 # Try compiling and running test program
-if (MTL4_INCLUDE_DIR)
+if (MTL4_INCLUDE_DIRS)
 
   # Set flags for building test program
-  set(CMAKE_REQUIRED_INCLUDES ${MTL4_INCLUDE_DIR})
+  set(CMAKE_REQUIRED_INCLUDES ${MTL4_INCLUDE_DIRS})
 
   # Build and run test program
   include(CheckCXXSourceRuns)
@@ -31,15 +34,10 @@ int main()
 }
 " MTL4_TEST_RUNS)
 
-  if(NOT MTL4_TEST_RUNS)
-    message("   Unable to run test program for package 'MTL4'")
-    set(MTL4_INCLUDE_DIR FALSE)
-  endif(NOT MTL4_TEST_RUNS)
-
-endif (MTL4_INCLUDE_DIR)
+endif()
 
 # Standard package handling
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(MTL4
   "MTL4 could not be found. Be sure to set MTL4_DIR"
-  MTL4_INCLUDE_DIR)
+  MTL4_INCLUDE_DIRS MTL4_TEST_RUNS)
