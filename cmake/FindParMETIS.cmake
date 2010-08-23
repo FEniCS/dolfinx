87c4b1ac9b7c8ac53983d5298c6a9393416246b4
Anders Logg <logg@simula.no>
2010-08-23 23:36:15 +0200

    Correct INCLUDE_DIRS name for ParMETIS

--- cmake/FindParMETIS.cmake
+++ cmake/FindParMETIS.cmake
@@ -1,12 +1,12 @@
 # - Try to find ParMETIS
 # Once done this wil define
 #
-#  PARMETIS_FOUND       - system has ParMETIS
-#  PARMETIS_INCLUDE_DIR - include directories for ParMETIS
-#  PARMETIS_LIBRARIES   - libraries for ParMETIS
+#  PARMETIS_FOUND        - system has ParMETIS
+#  PARMETIS_INCLUDE_DIRS - include directories for ParMETIS
+#  PARMETIS_LIBRARIES    - libraries for ParMETIS
 
-find_path(PARMETIS_INCLUDE_DIR parmetis.h
-  PATHS  $ENV{PARMETIS_DIR}/include
+find_path(PARMETIS_INCLUDE_DIRS parmetis.h
+  PATHS $ENV{PARMETIS_DIR}/include
   DOC "Directory where the ParMETIS header files are located"
 )
 
@@ -23,4 +23,4 @@ set(PARMETIS_LIBRARIES ${PARMETIS_LIBRARY} ${METIS_LIBRARY})
 # Standard package handling
 find_package_handle_standard_args(ParMETIS
                                   "ParMETIS could not be found."
-                                  PARMETIS_INCLUDE_DIR PARMETIS_LIBRARIES)
+                                  PARMETIS_INCLUDE_DIRS PARMETIS_LIBRARIES)
