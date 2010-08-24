cd21fdc4a3095e41587f6307ea151453df163e83
Johannes Ring <johannr@simula.no>
2010-08-24 10:58:52 +0200

    Add test program and link with AMD, CAMD, COLAMD, and CCOLAMD in CHOLMOD find module.

--- cmake/FindCHOLMOD.cmake
+++ cmake/FindCHOLMOD.cmake
@@ -7,26 +7,128 @@
 
 message(STATUS "Checking for package 'CHOLMOD'")
 
+find_package(AMD)
+# FIXME: Should we have separate FindXX modules for CAMD, COLAMD, and CCOLAMD?
+# FIXME: find_package(CAMD)
+# FIXME: find_package(COLAMD)
+# FIXME: find_package(CCOLAMD)
+
+# FIXME: It may be necessary to link to LAPACK and BLAS (or the vecLib
+# FIXME: framework on Darwin).
+
 # Check for header file
 find_path(CHOLMOD_INCLUDE_DIRS cholmod.h
- PATHS $ENV{CHOLMOD_DIR}/include
- PATH_SUFFIXES suitesparse ufsparse
- DOC "Directory where the CHOLMOD header is located"
+  HINTS ${CHOLMOD_DIR}
+  PATHS $ENV{CHOLMOD_DIR}
+  PATH_SUFFIXES include include/suitesparse include/ufsparse
+  DOC "Directory where the CHOLMOD header is located"
  )
-mark_as_advanced(CHOLMOD_INCLUDE_DIRS)
 
 # Check for CHOLMOD library
 find_library(CHOLMOD_LIBRARY cholmod
-  PATHS $ENV{CHOLMOD_DIR}/lib
+  HINTS ${CHOLMOD_DIR}
+  PATHS $ENV{CHOLMOD_DIR}
+  PATH_SUFFIXES lib
   DOC "The CHOLMOD library"
   )
-mark_as_advanced(CHOLMOD_LIBRARY)
+
+# Check for CAMD library
+find_library(CAMD_LIBRARY camd
+  HINTS ${CHOLMOD_DIR} ${CAMD_DIR}
+  PATHS $ENV{CHOLMOD_DIR} $ENV{CAMD_DIR}
+  PATH_SUFFIXES lib
+  DOC "The CAMD library"
+  )
+
+# Check for COLAMD library
+find_library(COLAMD_LIBRARY colamd
+  HINTS ${CHOLMOD_DIR} ${COLAMD_DIR}
+  PATHS $ENV{CHOLMOD_DIR} $ENV{COLAMD_DIR}
+  PATH_SUFFIXES lib
+  DOC "The COLAMD library"
+  )
+
+# Check for CCOLAMD library
+find_library(CCOLAMD_LIBRARY ccolamd
+  HINTS ${CHOLMOD_DIR} ${CCOLAMD_DIR}
+  PATHS $ENV{CHOLMOD_DIR} $ENV{CCOLAMD_DIR}
+  PATH_SUFFIXES lib
+  DOC "The CCOLAMD library"
+  )
 
 # Collect libraries
-set(CHOLMOD_LIBRARIES "${CHOLMOD_LIBRARY};${AMD_LIBRARIES}")
+set(CHOLMOD_LIBRARIES "${CHOLMOD_LIBRARY};${AMD_LIBRARIES};${CAMD_LIBRARY};${COLAMD_LIBRARY};${CCOLAMD_LIBRARY}")
+
+mark_as_advanced(
+  CHOLMOD_INCLUDE_DIRS
+  CHOLMOD_LIBRARY
+  CHOLMOD_LIBRARIES
+  CAMD_LIBRARY
+  COLAMD_LIBRARY
+  CCOLAMD_LIBRARY
+  )
+
+# Try to run a test program that uses CHOLMOD
+if (CHOLMOD_INCLUDE_DIRS AND CHOLMOD_LIBRARIES)
+
+  set(CMAKE_REQUIRED_INCLUDES "${CHOLMOD_INCLUDE_DIRS};${AMD_INCLUDE_DIRS}")
+  set(CMAKE_REQUIRED_LIBRARIES ${CHOLMOD_LIBRARIES})
+
+  set(cholmod_test_libs_str "
+#include <stdio.h>
+#include <cholmod.h>
+
+int main()
+{
+  cholmod_dense *D;
+  cholmod_sparse *S;
+  cholmod_dense *x, *b, *r;
+  cholmod_factor *L;
+  double one[2] = {1,0}, m1[2] = {-1,0};
+  double *dx;
+  cholmod_common c;
+  int n = 5;
+  double K[5][5] = {{1.0, 0.0, 0.0, 0.0, 0.0},
+                    {0.0, 2.0,-1.0, 0.0, 0.0},
+                    {0.0,-1.0, 2.0,-1.0, 0.0},
+                    {0.0, 0.0,-1.0, 2.0, 0.0},
+                    {0.0, 0.0, 0.0, 0.0, 1.0}};
+  cholmod_start (&c);
+  D = cholmod_allocate_dense(n, n, n, CHOLMOD_REAL, &c);
+  dx = (double*)D->x;
+  for (int i=0; i < n; i++)
+    for (int j=0; j < n; j++)
+      dx[i+j*n] = K[i][j];
+  S = cholmod_dense_to_sparse(D, 1, &c);
+  S->stype = 1;
+  cholmod_reallocate_sparse(cholmod_nnz(S, &c), S, &c);
+  b = cholmod_ones(S->nrow, 1, S->xtype, &c);
+  L = cholmod_analyze(S, &c);
+  cholmod_factorize(S, L, &c);
+  x = cholmod_solve(CHOLMOD_A, L, b, &c);
+  r = cholmod_copy_dense(b, &c);
+  cholmod_sdmult(S, 0, m1, one, x, r, &c);
+  printf(\"norm(b-Ax)=%g\n\", cholmod_norm_dense(r, 0, &c));
+  cholmod_free_factor(&L, &c);
+  cholmod_free_dense(&D, &c);
+  cholmod_free_sparse(&S, &c);
+  cholmod_free_dense(&r, &c);
+  cholmod_free_dense(&x, &c);
+  cholmod_free_dense(&b, &c);
+  cholmod_finish(&c);
+  return 0;
+}
+")
+
+  check_cxx_source_runs(cholmod_test_libs_str CHOLMOD_TEST_RUNS)
+
+endif()
 
 # Standard package handling
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(CHOLMOD
   "CHOLMOD could not be found. Be sure to set CHOLMOD_DIR."
-  CHOLMOD_INCLUDE_DIRS CHOLMOD_LIBRARIES AMD_LIBRARIES)
+  CHOLMOD_INCLUDE_DIRS CHOLMOD_LIBRARIES CHOLMOD_TEST_RUNS)
+
+find_package_message(CHOLMOD "Found CHOLMOD: ${CHOLMOD_LIBRARIES}"
+  "[${CHOLMOD_LIBRARIES}][${CHOLMOD_INCLUDE_DIRS}]")
