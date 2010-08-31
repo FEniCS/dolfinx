01c8a141b5c310ee50df5afb89c5a54ca88fdc5e
Marie E. Rognes <meg@simula.no>
2010-08-31 23:09:03 +0200

    Replace la/eigensolver and pde/mixed-poisson demo with updated ones from documentation instead of old ones.

--- demo/pde/mixed-poisson/cpp/main.cpp
+++ demo/pde/mixed-poisson/cpp/main.cpp
@@ -1,85 +1,85 @@
-// Copyright (C) 2007 Anders Logg and Marie Rognes.
+// Copyright (C) 2007-2010 Anders Logg and Marie Rognes.
 // Licensed under the GNU LGPL Version 2.1.
 //
 // Modified by Garth N. Wells, 2008.
 //
 // First added:  2007-04-20
-// Last changed: 2010-01-27
-//
-// This demo program solves the mixed formulation of
-// Poisson's equation,
-//
-//     sigma + grad(u) = 0,
-//          div(sigma) = f.
-//
-// The corresponding weak (variational problem),
-//
-//     <tau, sigma> - <div(tau), u> = 0       for all tau,
-//                  <w, div(sigma)> = <w, f>  for all w,
-//
-// is solved using BDM (Brezzi-Douglas-Marini) elements of degree q
-// for (tau, sigma) and DG (discontinuous Galerkin) elements of degree
-// q - 1 for (w, u).
+// Last changed: 2010-08-31
 
 #include <dolfin.h>
 #include "MixedPoisson.h"
-#include "P1Projection.h"
 
 using namespace dolfin;
 
+// Source term (right-hand side)
+class Source : public Expression
+{
+  void eval(Array<double>& values, const Array<double>& x) const
+  {
+    double dx = x[0] - 0.5;
+    double dy = x[1] - 0.5;
+    values[0] = 10*exp(-(dx*dx + dy*dy) / 0.02);
+  }
+};
+
+// Boundary source for flux boundary condition
+class BoundarySource : public Expression
+{
+public:
+
+  BoundarySource() : Expression(2) {}
+
+  void eval(Array<double>& values, const Data& data) const
+  {
+    double g = sin(5*data.x[0]);
+    values[0] = g*data.normal()[0];
+    values[1] = g*data.normal()[1];
+  }
+};
+
+// Sub domain for essential boundary condition
+class EssentialBoundary : public SubDomain
+{
+  bool inside(const Array<double>& x, bool on_boundary) const
+  {
+    return x[1] < DOLFIN_EPS or x[1] > 1.0 - DOLFIN_EPS;
+  }
+};
+
 int main()
 {
-  // Source term
-  class Source : public Expression
-  {
-  public:
-
-    void eval(Array<double>& values, const Array<double>& x) const
-    {
-      double dx = x[0] - 0.5;
-      double dy = x[1] - 0.5;
-      values[0] = 500.0*exp(-(dx*dx + dy*dy)/0.02);
-    }
-
-  };
-
-  // Create mesh and source term
+  // Create mesh
   UnitSquare mesh(32, 32);
+
+  // Construct function space
+  MixedPoisson::FunctionSpace W(mesh);
+  MixedPoisson::BilinearForm a(W, W);
+  MixedPoisson::LinearForm L(W);
+
+  // Create source and assign to L
   Source f;
-
-  // Define PDE
-  MixedPoisson::FunctionSpace V(mesh);
-  MixedPoisson::BilinearForm a(V, V);
-  MixedPoisson::LinearForm L(V);
   L.f = f;
-  VariationalProblem pde(a, L);
 
-  // Solve PDE
-  Function w(V);
-  pde.solve(w);
+  // Define boundary condition
+  SubSpace W0(W, 0);
+  BoundarySource G;
+  EssentialBoundary boundary;
+  DirichletBC bc(W0, G, boundary);
+
+  // Define variational problem
+  VariationalProblem problem(a, L, bc);
+
+  // Compute (full) solution
+  Function w(W);
+  problem.solve(w);
 
   // Extract sub functions (function views)
   Function& sigma = w[0];
   Function& u = w[1];
 
-  // Project sigma onto P1 continuous Lagrange for post-processing
-  P1Projection::FunctionSpace Vp(mesh);
-  P1Projection::BilinearForm a_p(Vp, Vp);
-  P1Projection::LinearForm L_p(Vp);
-  L_p.f = sigma;
-  VariationalProblem pde_project(a_p, L_p);
-  Function sigma_p(Vp);
-  pde_project.solve(sigma_p);
-
-  // Plot solution
-  plot(sigma_p);
+  // Plot solutions
   plot(u);
-
-  // Save solution in VTK format
-  File f3("sigma.pvd");
-  File f4("u.pvd");
-  f3 << sigma_p;
-  f4 << u;
+  plot(sigma);
 
   return 0;
 }
--- demo/pde/mixed-poisson/cpp/plot.py
+++
@@ -1,7 +0,0 @@
-from dolfin import *
-
-sigma = Function("sigma.xml")
-plot(sigma)
-
-u = Function("u.xml")
-plot(u)
