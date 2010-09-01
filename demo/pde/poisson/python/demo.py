d1d412bb334e386b49ada9a2e6fffc2cd50921af
Marie E. Rognes <meg@simula.no>
2010-09-01 21:09:26 +0200

    Change from a(v, u) to a(u, v) in all documented pde demos. Have not regenerated .h/.cpp files.
    If someone copies these to the fenics-doc, I will make sure that the
    documentation matches.

--- demo/pde/poisson/python/demo.py
+++ demo/pde/poisson/python/demo.py
@@ -34,12 +34,12 @@ u0 = Constant(0.0)
 bc = DirichletBC(V, u0, boundary)
 
 # Define variational problem
-v = TestFunction(V)
 u = TrialFunction(V)
+v = TestFunction(V)
 f = Expression("10*exp(-(pow(x[0] - 0.5, 2) + pow(x[1] - 0.5, 2)) / 0.02)")
 g = Expression("sin(5*x[0])")
-a = inner(grad(v), grad(u))*dx
-L = v*f*dx + v*g*ds
+a = inner(grad(u), grad(v))*dx
+L = f*v*dx + g*v*ds
 
 # Compute solution
 problem = VariationalProblem(a, L, bc)
