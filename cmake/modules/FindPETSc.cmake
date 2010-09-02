82df3cf2f8e4b214e74d2b292d0ed0d7f8a6760b
Johannes Ring <johannr@simula.no>
2010-09-02 15:02:29 +0200

    Minor fix in test program for PETSc CMake module allowing it to pass when run in pbuilder.

--- cmake/modules/FindPETSc.cmake
+++ cmake/modules/FindPETSc.cmake
@@ -145,7 +145,9 @@ int main()
 {
   PetscErrorCode ierr;
   TS ts;
-  ierr = PetscInitializeNoArguments();CHKERRQ(ierr);
+  int argc = 0;
+  char** argv = NULL;
+  ierr = PetscInitialize(&argc, &argv, PETSC_NULL, PETSC_NULL);CHKERRQ(ierr);CHKERRQ(ierr);
   ierr = TSCreate(PETSC_COMM_WORLD,&ts);CHKERRQ(ierr);
   ierr = TSSetFromOptions(ts);CHKERRQ(ierr);
   ierr = TSDestroy(ts);CHKERRQ(ierr);
