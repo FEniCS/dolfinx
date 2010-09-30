3cf52062db7eda3ef003ecd60a84435e1ab59267
Marie E. Rognes <meg@simula.no>
2010-09-30 20:57:09 +0200

    Update doc-string in SLEPcEigenSolver.

--- dolfin/la/SLEPcEigenSolver.h
+++ dolfin/la/SLEPcEigenSolver.h
@@ -28,7 +28,7 @@ namespace dolfin
   ///
   /// The following parameters may be specified to control the solver.
   ///
-  /// 1. "eigenvalue spectrum"
+  /// 1. "spectrum"
   ///
   /// This parameter controls which part of the spectrum to compute.
   /// Possible values are
@@ -39,9 +39,16 @@ namespace dolfin
   ///   "smallest real"       (eigenvalues with smallest double part)
   ///   "largest imaginary"   (eigenvalues with largest imaginary part)
   ///   "smallest imaginary"  (eigenvalues with smallest imaginary part)
-  ///   "default spectrum"    (default spectrum)
   ///
-  /// 2. "eigenvalue solver"
+  /// For SLEPc versions >= 3.1 , the following values are also possible
+  ///
+  ///   "target magnitude"    (eigenvalues closest to target in magnitude)
+  ///   "target real"         (eigenvalues closest to target in real part)
+  ///   "target imaginary"    (eigenvalues closest to target in imaginary part)
+  ///
+  /// The default is "largest magnitude"
+  ///
+  /// 2. "solver"
   ///
   /// This parameter controls which algorithm is used by SLEPc.
   /// Possible values are
@@ -51,21 +58,59 @@ namespace dolfin
   ///   "arnoldi"             (Arnoldi)
   ///   "lanczos"             (Lanczos)
   ///   "krylov-schur"        (Krylov-Schur)
-  ///   "lapack"              (LAPACK, all values, direct, only for small systems)
-  ///   "default"             (default algorithm)
+  ///   "lapack"              (LAPACK, all values, direct, small systems only)
   ///
-  /// 3. "eigenvalue tolerance"
+  /// The default is "krylov-schur"
+  ///
+  /// 3. "tolerance"
   ///
   /// This parameter controls the tolerance used by SLEPc.
   /// Possible values are positive double numbers.
   ///
-  /// 4. "eigenvalue iterations"
+  /// The default is 1e-15;
+  ///
+  /// 4. "maximum_iterations"
   ///
   /// This parameter controls the maximum number of iterations used by SLEPc.
   /// Possible values are positive integers.
   ///
   /// Note that both the tolerance and the number of iterations must be
   /// specified if either one is specified.
+  ///
+  /// 5. "problem_type"
+  ///
+  /// This parameter can be used to give extra information about the
+  /// type of the eigenvalue problem. Some solver types require this
+  /// extra piece of information. Possible values are:
+  ///
+  ///   "hermitian"               (Hermitian)
+  ///   "non_hermitian"           (Non-Hermitian)
+  ///   "gen_hermitian"           (Generalized Hermitian)
+  ///   "gen_non_hermitian"       (Generalized Non-Hermitian)
+  ///
+  /// 6. "spectral_transform"
+  ///
+  /// This parameter controls the application of a spectral transform. A
+  /// spectral transform can be used to enhance the convergence of the
+  /// eigensolver and in particular to only compute eigenvalues in the
+  /// interior of the spectrum. Possible values are:
+  ///
+  ///   "shift-and-invert"      (A shift-and-invert transform)
+  ///
+  /// Note that if a spectral transform is given, then also a non-zero
+  /// spectral shift parameter has to be provided.
+  ///
+  /// The default is no spectral transform.
+  ///
+  /// 7. "spectral_shift"
+  ///
+  /// This parameter controls the spectral shift used by the spectral
+  /// transform and must be provided if a spectral transform is given. The
+  /// possible values are real numbers.
+  ///
+
+
+
 
   class SLEPcEigenSolver : public Variable, public PETScObject
   {
