9c97a9c126f2ee13ed5a50228be1efed8bea3e7d
Johannes Ring <johannr@simula.no>
2011-01-03 14:13:20 +0100

    Remove SCOTCH build test. Needs more testing.

--- cmake/modules/FindScotchPT.cmake
+++ cmake/modules/FindScotchPT.cmake
@@ -45,43 +45,7 @@ find_library(PTSCOTCHERR_LIBRARY
 
 set(SCOTCH_LIBRARIES ${PTSCOTCH_LIBRARY} ${PTSCOTCHERR_LIBRARY})
 
-# Try compiling and running test program
-if (SCOTCH_INCLUDE_DIRS AND SCOTCH_LIBRARIES)
-
-  # Set flags for building test program
-  set(CMAKE_REQUIRED_INCLUDES ${SCOTCH_INCLUDE_DIRS})
-  set(CMAKE_REQUIRED_LIBRARIES ${SCOTCH_LIBRARIES})
-
-  # Add MPI variables if MPI has been found
-  if (MPI_FOUND)
-    set(CMAKE_REQUIRED_INCLUDES ${CMAKE_REQUIRED_INCLUDES} ${MPI_INCLUDE_PATH})
-    set(CMAKE_REQUIRED_LIBRARIES ${CMAKE_REQUIRED_LIBRARIES} ${MPI_LIBRARIES})
-    set(CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS} ${MPI_COMPILE_FLAGS}")
-  endif()
-  message(STATUS "CMAKE_REQUIRED_INCLUDES = ${CMAKE_REQUIRED_INCLUDES}")
-
-  # Build and run test program
-  include(CheckCXXSourceRuns)
-  check_cxx_source_runs("
-#include <stdio.h>
-#include <scotch.h>
-#include <iostream>
-int main() {
-  SCOTCH_Dgraph dgrafdat;
-
-  if (SCOTCH_dgraphInit(&dgrafdat, MPI_COMM_WORLD) != 0) {
-    std::cout << \"failure\";
-  } else {
-    std::cout << \"success\";
-  }
-  return 0;
-}
-" SCOTCH_TEST_RUNS)
-
-endif()
-
 # Standard package handling
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(SCOTCH
-  "SCOTCH could not be found. Be sure to set SCOTCH_DIR."
-  SCOTCH_LIBRARIES SCOTCH_INCLUDE_DIRS SCOTCH_TEST_RUNS)
+find_package_handle_standard_args(SCOTCH DEFAULT_MSG
+                                  SCOTCH_LIBRARIES SCOTCH_INCLUDE_DIRS)
