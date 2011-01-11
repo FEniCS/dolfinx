a9962ac9690b2edce94143475a7b937e01e864b1
Anders Logg <logg@simula.no>
2011-01-11 15:26:47 +0100

    Add some diagnostic output from local mesh refinement:
    670 cells out of 1038 marked for refinement (64.5%).
    Number of cells increased from 1038 to 2198 (111.8% increase).

--- dolfin/mesh/refine.cpp
+++ dolfin/mesh/refine.cpp
@@ -39,8 +39,23 @@ void dolfin::refine(Mesh& refined_mesh,
                     const Mesh& mesh,
                     const MeshFunction<bool>& cell_markers)
 {
+  // Count the number of marked cells
+  uint n0 = mesh.num_cells();
+  uint n = 0;
+  for (uint i = 0; i < cell_markers.size(); i++)
+    if (cell_markers[i])
+      n++;
+  info("%d cells out of %d marked for refinement (%.1f%%).",
+       n, n0, 100.0 * static_cast<double>(n) / static_cast<double>(n0));
+
+  // Call refinement algorithm
   LocalMeshRefinement::refineRecursivelyByEdgeBisection(refined_mesh,
                                                         mesh,
                                                         cell_markers);
+
+  // Report the number of refined cells
+  uint n1 = refined_mesh.num_cells();
+  info("Number of cells increased from %d to %d (%.1f%% increase).",
+       n0, n1, 100.0 * (static_cast<double>(n1) / static_cast<double>(n0) - 1.0));
 }
 //-----------------------------------------------------------------------------
