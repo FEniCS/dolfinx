d3ad0836148a6b71d9e134c9bb73c4b19d2967f1
Anders Logg <logg@simula.no>
2011-01-11 14:32:19 +0100

    Clean up code in refine.cpp

--- dolfin/mesh/refine.cpp
+++ dolfin/mesh/refine.cpp
@@ -1,10 +1,10 @@
 // Copyright (C) 2010 Garth N. Wells.
 // Licensed under the GNU LGPL Version 2.1.
 //
-// Modified by Anders Logg, 2010.
+// Modified by Anders Logg, 2010-2011.
 //
 // First added:  2010-02-10
-// Last changed: 2010-10-19
+// Last changed: 2011-01-11
 
 #include "LocalMeshRefinement.h"
 #include "Mesh.h"
@@ -31,9 +31,7 @@ dolfin::Mesh dolfin::refine(const Mesh& mesh,
                             const MeshFunction<bool>& cell_markers)
 {
   Mesh refined_mesh;
-  LocalMeshRefinement::refineRecursivelyByEdgeBisection(refined_mesh,
-                                                        mesh,
-                                                        cell_markers);
+  refine(refined_mesh, mesh, cell_markers);
   return refined_mesh;
 }
 //-----------------------------------------------------------------------------
