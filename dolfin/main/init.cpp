edb2c0cb44fb9072f9e369c1dd30b16b6a7ac66c
Anders Logg <logg@simula.no>
2011-01-24 14:54:45 +0100

    dolfin::dolfin_init --> dolfin::init

--- dolfin/main/init.cpp
+++ dolfin/main/init.cpp
@@ -1,8 +1,8 @@
-// Copyright (C) 2005-2006 Anders Logg.
+// Copyright (C) 2005-2011 Anders Logg.
 // Licensed under the GNU LGPL Version 2.1.
 //
 // First added:  2005-02-13
-// Last changed: 2006-05-07
+// Last changed: 2011-01-24
 
 #include <dolfin/common/constants.h>
 #include <dolfin/log/dolfin_log.h>
@@ -10,7 +10,7 @@
 #include "init.h"
 
 //-----------------------------------------------------------------------------
-void dolfin::dolfin_init(int argc, char* argv[])
+void dolfin::init(int argc, char* argv[])
 {
   info(PROGRESS, "Initializing DOLFIN version %s.", DOLFIN_VERSION);
 
--- dolfin/main/init.h
+++ dolfin/main/init.h
@@ -1,8 +1,8 @@
-// Copyright (C) 2005 Anders Logg.
+// Copyright (C) 2005-2011 Anders Logg.
 // Licensed under the GNU LGPL Version 2.1.
 //
 // First added:  2005-02-13
-// Last changed: 2005
+// Last changed: 2011-01-24
 
 #ifndef __INIT_H
 #define __INIT_H
@@ -13,7 +13,7 @@ namespace dolfin
   /// Initialize DOLFIN (and PETSc) with command-line arguments. This
   /// should not be needed in most cases since the initialization is
   /// otherwise handled automatically.
-  void dolfin_init(int argc, char* argv[]);
+  void init(int argc, char* argv[]);
 
 }
 
