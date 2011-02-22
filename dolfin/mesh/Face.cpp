91f10b3a890a2df89bc37b716f58b72c9b175882
Garth N. Wells <gnw20@cam.ac.uk>
2011-02-22 15:23:54 +0000

    Remove area computation code from Face.
    Face should be independent of the cell type.

--- dolfin/mesh/Face.cpp
+++ dolfin/mesh/Face.cpp
@@ -15,22 +15,13 @@ using namespace dolfin;
 //-----------------------------------------------------------------------------
 double Face::area() const
 {
-  // Get mesh geometry
-  const MeshGeometry& geometry = mesh().geometry();
+  // Get cell to which face belong (first cell when there is more than one)
+  const Cell cell(*_mesh, this->entities(3)[0]);
 
-  // Get the coordinates of the three vertices
-  const uint* vertices = entities(0);
-  const double* x0 = geometry.x(vertices[0]);
-  const double* x1 = geometry.x(vertices[1]);
-  const double* x2 = geometry.x(vertices[2]);
+  // Get local index of facet with respect to the cell
+  const uint local_facet = cell.index(*this);
 
-  // Compute area of triangle embedded in R^3
-  double v0 = (x0[1]*x1[2] + x0[2]*x2[1] + x1[1]*x2[2]) - (x2[1]*x1[2] + x2[2]*x0[1] + x1[1]*x0[2]);
-  double v1 = (x0[2]*x1[0] + x0[0]*x2[2] + x1[2]*x2[0]) - (x2[2]*x1[0] + x2[0]*x0[2] + x1[2]*x0[0]);
-  double v2 = (x0[0]*x1[1] + x0[1]*x2[0] + x1[0]*x2[1]) - (x2[0]*x1[1] + x2[1]*x0[0] + x1[0]*x0[1]);
-
-  // Formula for area from http://mathworld.wolfram.com
-  return 0.5*sqrt(v0*v0 + v1*v1 + v2*v2);
+  return cell.facet_area(local_facet);
 }
 //-----------------------------------------------------------------------------
 double Face::normal(uint i) const
