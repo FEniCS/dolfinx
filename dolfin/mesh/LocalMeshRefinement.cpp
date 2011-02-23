4af2bde5d01aa116598401cd19e0378def486d8d
Garth N. Wells <gnw20@cam.ac.uk>
2011-02-23 09:31:13 +0000

    Remove broken refinement code.

--- dolfin/mesh/LocalMeshRefinement.cpp
+++ dolfin/mesh/LocalMeshRefinement.cpp
@@ -23,25 +23,21 @@ void LocalMeshRefinement::refine(Mesh& refined_mesh,
   const uint n0 = mesh.num_cells();
   uint n = 0;
   for (uint i = 0; i < cell_markers.size(); i++)
+  {
     if (cell_markers[i])
       n++;
+  }
   info("%d cells out of %d marked for refinement (%.1f%%).",
        n, n0, 100.0 * static_cast<double>(n) / static_cast<double>(n0));
 
   // Call refinement algorithm
   const std::string refinement_algorithm = parameters["refinement_algorithm"];
-  if (refinement_algorithm == "bisection")
-    BisectionRefinement::refine_by_bisection(refined_mesh, mesh, cell_markers);
-  else if (refinement_algorithm == "iterative_bisection")
-    BisectionRefinement::refine_by_iterative_bisection(refined_mesh, mesh, cell_markers);
-  else if (refinement_algorithm == "recursive_bisection")
+  if (refinement_algorithm == "recursive_bisection")
     BisectionRefinement::refine_by_recursive_bisection(refined_mesh, mesh, cell_markers);
   else if (refinement_algorithm == "regular_cut")
     RegularCutRefinement::refine(refined_mesh, mesh, cell_markers);
   else
-  {
     error("Unknown local mesh refinement algorithm: %s", refinement_algorithm.c_str());
-  }
 
   // Report the number of refined cells
   if (refined_mesh.topology().dim() > 0)
