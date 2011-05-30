4dff9296fdbda79073a75a0ac77b4ee8398ff5b2
Marie E. Rognes <meg@simula.no>
2011-05-30 15:29:05 +0200

    Various fixes (Thanks to Johannes!) for configuration/build of documentation, including splitting doc CMake stuff into separate doc/CMakeLists.txt and add target
            doc_install
    
    which installs generated sphinx projects to
    
            share/dolfin/doc/sphinx-[cpp|python]

--- cmake/modules/FindSphinx.cmake
+++ cmake/modules/FindSphinx.cmake
@@ -8,29 +8,29 @@
 
 message(STATUS "Checking for package 'Sphinx'")
 
+# Try to find sphinx-build
 find_program(SPHINX_EXECUTABLE sphinx-build
   HINTS
   $ENV{SPHINX_DIR}
   PATH_SUFFIXES bin
 )
 
+# Try to check Sphinx version by importing Sphinx
+execute_process(
+  COMMAND ${PYTHON_EXECUTABLE} -c "import sphinx; print sphinx.__version__"
+  OUTPUT_VARIABLE SPHINX_VERSION
+  OUTPUT_STRIP_TRAILING_WHITESPACE
+  RESULT_VARIABLE SPHINX_VERSION_NOT_FOUND)
+
+mark_as_advanced(
+  SPHINX_EXECUTABLE
+  SPHINX_VERSION
+)
+
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Sphinx
   "The program sphinx-build could not be found."
   SPHINX_EXECUTABLE
-)
+  SPHINX_VERSION
+  )
 
-if (SPHINX_FOUND)
-  execute_process(
-    COMMAND ${PYTHON_EXECUTABLE} -c "import sphinx; print sphinx.__version__"
-    OUTPUT_VARIABLE SPHINX_VERSION
-    RESULT_VARIABLE SPHINX_VERSION_NOT_FOUND)
-endif()
-
-if (SPHINX_VERSION)
-  set (SPHINX_VERSION ${SPHINX_VERSION} CACHE STRING "Sphinx version")
-endif()
-
-mark_as_advanced(
-  SPHINX_EXECUTABLE
-)
