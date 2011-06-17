10eb5c7786fa1bcb5f94b79cd71a515a621fa007
Johannes Ring <johannr@simula.no>
2011-06-17 14:53:25 +0200

    Try to improve Sphinx build test and only look for Sphinx if Python is found (fixes bug #798308).

--- cmake/modules/FindSphinx.cmake
+++ cmake/modules/FindSphinx.cmake
@@ -2,35 +2,48 @@
 
 # Once done this will define
 #
-#   SPHINX_FOUND      - system has Spinx
-#   SPHINX_EXECUTABLE - sphinx-build
-#   SPHINX_VERSION    - Major.Minor.Micro version of Sphinx
+#   SPHINX_FOUND      - system has Sphinx
+#   SPHINX_EXECUTABLE - full path to the Sphinx documentation generator tool
+#   SPHINX_VERSION    - the version of Sphinx which was found, e.g. "1.0.7"
 
 message(STATUS "Checking for package 'Sphinx'")
 
+# Make sure Python is available
+if (NOT PYTHON_EXECUTABLE)
+  find_package(PythonInterp)
+endif()
+
 # Try to find sphinx-build
 find_program(SPHINX_EXECUTABLE sphinx-build
-  HINTS
-  $ENV{SPHINX_DIR}
+  HINTS ${SPHINX_DIR} $ENV{SPHINX_DIR}
   PATH_SUFFIXES bin
+  DOC "Sphinx documentation generator tool"
 )
 
-# Try to check Sphinx version by importing Sphinx
-execute_process(
-  COMMAND ${PYTHON_EXECUTABLE} -c "import sphinx; print sphinx.__version__"
-  OUTPUT_VARIABLE SPHINX_VERSION
-  OUTPUT_STRIP_TRAILING_WHITESPACE
-  RESULT_VARIABLE SPHINX_VERSION_NOT_FOUND)
+if (SPHINX_EXECUTABLE)
+  # Try to check Sphinx version by importing Sphinx
+  execute_process(
+    COMMAND ${PYTHON_EXECUTABLE} -c "import sphinx; print sphinx.__version__"
+    OUTPUT_VARIABLE SPHINX_VERSION
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+  if (Sphinx_FIND_VERSION)
+    # Check if version found is >= required version
+    if (NOT "${SPHINX_VERSION}" VERSION_LESS "${Sphinx_FIND_VERSION}")
+      set(SPHINX_VERSION_OK TRUE)
+    endif()
+  else()
+    # No specific version of Sphinx is requested
+    set(SPHINX_VERSION_OK TRUE)
+  endif()
+endif()
 
 mark_as_advanced(
   SPHINX_EXECUTABLE
   SPHINX_VERSION
+  SPHINX_VERSION_OK
 )
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Sphinx
-  "The program sphinx-build could not be found."
-  SPHINX_EXECUTABLE
-  SPHINX_VERSION
-  )
-
+find_package_handle_standard_args(Sphinx DEFAULT_MSG
+  SPHINX_EXECUTABLE SPHINX_VERSION_OK)
