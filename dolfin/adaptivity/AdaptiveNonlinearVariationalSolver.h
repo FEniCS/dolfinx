2c9e9517f45dc960899d35d53450ac094be11591
Marie E. Rognes <meg@simula.no>
2011-06-27 20:59:01 +0200

    Get AdaptiveNonlinearVariationalSolver up and running.

---
+++ dolfin/adaptivity/AdaptiveNonlinearVariationalSolver.h
@@ -0,0 +1,68 @@
+// Copyright (C) 2010 Marie E. Rognes
+//
+// This file is part of DOLFIN.
+//
+// DOLFIN is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// DOLFIN is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with DOLFIN. If not, see <http://www.gnu.org/licenses/>.
+//
+// Modified by Anders Logg, 2010-2011.
+//
+// First added:  2010-08-19
+// Last changed: 2011-03-31
+
+#ifndef __ADAPTIVE_NONLINEAR_VARIATIONAL_SOLVER_H
+#define __ADAPTIVE_NONLINEAR_VARIATIONAL_SOLVER_H
+
+#include <boost/shared_ptr.hpp>
+#include <dolfin/fem/BoundaryCondition.h>
+
+#include "GenericAdaptiveVariationalSolver.h"
+
+namespace dolfin
+{
+  // Forward declarations
+  class Function;
+  class NonlinearVariationalProblem;
+  class GoalFunctional;
+  class Mesh;
+
+  class AdaptiveNonlinearVariationalSolver
+    : public GenericAdaptiveVariationalSolver
+  {
+  public:
+
+    /// Create adaptive variational solver for given linear variaional
+    /// problem
+    AdaptiveNonlinearVariationalSolver(NonlinearVariationalProblem& problem);
+
+    void solve(const double tol, GoalFunctional& M);
+
+    boost::shared_ptr<const Function> solve_primal();
+
+    std::vector<boost::shared_ptr<const BoundaryCondition> > extract_bcs() const;
+
+    const double evaluate_goal(Form& M, const Function& u) const;
+
+    void adapt_problem(boost::shared_ptr<const Mesh> mesh);
+
+
+  private:
+
+    // The problem
+    boost::shared_ptr<NonlinearVariationalProblem> problem;
+
+  };
+
+}
+
+#endif
--- dolfin/adaptivity/dolfin_adaptivity.h
+++ dolfin/adaptivity/dolfin_adaptivity.h
@@ -3,9 +3,17 @@
 
 // DOLFIN adaptivity interface
 
+#include <dolfin/adaptivity/AdaptiveDatum.h>
 #include <dolfin/adaptivity/AdaptiveLinearVariationalSolver.h>
+#include <dolfin/adaptivity/AdaptiveNonlinearVariationalSolver.h>
+#include <dolfin/adaptivity/GoalFunctional.h>
 #include <dolfin/adaptivity/ErrorControl.h>
+#include <dolfin/adaptivity/Extrapolation.h>
+#include <dolfin/adaptivity/LocalAssembler.h>
+#include <dolfin/adaptivity/SpecialFacetFunction.h>
 #include <dolfin/adaptivity/TimeSeries.h>
+
 #include <dolfin/adaptivity/adapt.h>
+#include <dolfin/adaptivity/marking.h>
 
 #endif
