f108a204465f76db75275e6772556859cc12743e
Garth N. Wells <gnw20@cam.ac.uk>
2011-07-08 08:50:08 +0100

    Relocate tests.

--- test/unit/fem/python/DirichletBC.py
+++ test/unit/fem/python/DirichletBC.py
@@ -24,8 +24,7 @@ import unittest
 import numpy
 from dolfin import *
 
-# FIXME: No complete
-
+# FIXME: Not complete
 
 class DirichletBCTest(unittest.TestCase):
 
@@ -44,38 +43,6 @@ class DirichletBCTest(unittest.TestCase):
         A = assemble(v*u*dx)
         bc = DirichletBC(V, BoundaryFunction(), Boundary())
         bc.apply(A)
-
-    def test_near_and_between(self):
-        "Test that near and between handle rounding errors correctly."
-        # TODO: Move this test to a test module 'basic' or something?
-        eps = DOLFIN_EPS
-        # Loop over magnitudes
-        for i in range(100):
-            # Loop over base values
-            for j in range(1, 10):
-                # Compute a value v and some values close to it
-                v = j*10**(i-15)
-                vm = v - eps
-                vp = v + eps
-
-                # Check that we return True when we should by definition:
-                self.assertTrue(near(v, vm))
-                self.assertTrue(near(v, vp))
-                self.assertTrue(between(vm, v, vp))
-
-                # vm and vp can round off to v, make some small values != v
-                v2m = v * (1.0 - 2*eps) - 2*eps
-                v2p = v * (1.0 + 2*eps) + 2*eps
-
-                # Close to 1 except for some of the smallest v's:
-                self.assertTrue(v/v2m > 1.0)
-                self.assertTrue(v/v2p < 1.0)
-                self.assertTrue(between(v2m, v, v2p))
-
-                # Check that we can fail for fairly close values
-                self.assertFalse(near(v, v2m))
-                self.assertFalse(near(v, v2p))
-                self.assertFalse(between(v2p, v, v2m))
 
 
 if __name__ == "__main__":
