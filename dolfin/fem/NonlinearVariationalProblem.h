4378d40370a59a5800d5e0cc5f40f25636547697
Anders Logg <logg@simula.no>
2011-07-21 00:10:08 +0200

    Replace VariationalProblem Python class with empty class giving a warning.

--- dolfin/fem/NonlinearVariationalProblem.h
+++ dolfin/fem/NonlinearVariationalProblem.h
@@ -16,7 +16,7 @@
 // along with DOLFIN. If not, see <http://www.gnu.org/licenses/>.
 //
 // First added:  2011-06-22
-// Last changed: 2011-07-17
+// Last changed: 2011-07-18
 
 #ifndef __NONLINEAR_VARIATIONAL_PROBLEM_H
 #define __NONLINEAR_VARIATIONAL_PROBLEM_H
