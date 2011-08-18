1d23f5ead3d04f06a1a3a60f84ae518a7e062428
Anders Logg <logg@simula.no>
2011-08-18 09:55:13 +0200

    Remove (old) sphinx theme 'fenics' and use a builtin sphinx theme for docs. Are replaced with current theme when imported into fenics-web anyway.

--- doc/sphinx-common/source/conf.py
+++ doc/sphinx-common/source/conf.py
@@ -90,7 +90,7 @@ modindex_common_prefix = ["dolfin.", "dolfin.cpp."]
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'fenics'
+html_theme = 'nature'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
