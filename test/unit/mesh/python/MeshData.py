fd29273865df7e4b3246abfaeb5ed5bfe03db945
Anders Logg <logg@simula.no>
2011-08-22 19:50:48 +0200

    Add (incomplete) unit test for I/O for MeshData class

---
+++ test/unit/mesh/python/MeshData.py
@@ -0,0 +1,41 @@
+"Unit tests for the mesh library"
+
+# Copyright (C) 2011 Anders Logg
+#
+# This file is part of DOLFIN.
+#
+# DOLFIN is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# DOLFIN is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with DOLFIN. If not, see <http://www.gnu.org/licenses/>.
+#
+# First added:  2011-08-22
+# Last changed: 2011-08-22
+
+import unittest
+from dolfin import *
+
+class MeshData(unittest.TestCase):
+
+    def test_io(self):
+        "Test input/output"
+
+        # Read mesh with boundary indicators
+        mesh = Mesh()
+        f = File("../../../../data/meshes/aneurysm.xml.gz")
+        f >> mesh
+
+        # Write mesh with boundary indicators
+        g = File("MeshData_test_io.xml")
+        g << mesh
+
+if __name__ == "__main__":
+    unittest.main()
