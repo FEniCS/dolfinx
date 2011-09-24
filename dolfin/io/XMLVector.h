08e08a32915258cb73418fa5751493eef348d32f
Garth N. Wells <gnw20@cam.ac.uk>
2011-09-24 21:56:57 +0100

    Update a subdomain file.

--- dolfin/io/XMLVector.h
+++ dolfin/io/XMLVector.h
@@ -40,6 +40,10 @@ namespace dolfin
     // Read XML vector. Vector must have correct size.
     static void read(GenericVector& x, const pugi::xml_node xml_dolfin);
 
+    // Read XML vector in Array. Vector must have correct size.
+    static void read(Array<double>& x, Array<uint>& indices,
+                     const pugi::xml_node xml_dolfin);
+
     // Read XML vector size
     static uint read_size(const pugi::xml_node xml_dolfin);
 
