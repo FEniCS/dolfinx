c3c25c018c0256d085ed124fc6b7c9a8080c7a5e
Johannes Ring <johannr@simula.no>
2012-01-18 10:56:47 +0100

    PETSc and SLEPc 3.2 are now in Debian unstable. Add automatic detection.

--- cmake/modules/FindPETSc.cmake
+++ cmake/modules/FindPETSc.cmake
@@ -26,6 +26,7 @@ endforeach()
 
 # List of possible locations for PETSC_DIR
 set(petsc_dir_locations "")
+list(APPEND petsc_dir_locations "/usr/lib/petscdir/3.2")    # Debian location
 list(APPEND petsc_dir_locations "/usr/lib/petscdir/3.1")    # Debian location
 list(APPEND petsc_dir_locations "/usr/lib/petscdir/3.0.0")  # Debian location
 list(APPEND petsc_dir_locations "/usr/local/lib/petsc")     # User location
