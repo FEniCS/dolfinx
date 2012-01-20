cdafa3ba71467899fbd350cbfe583e578aa8b5c1
Johan Hake <hake.dev@gmail.com>
2012-01-20 15:45:59 +0100

    Added some global interface files and several changes in the generate script

---
+++ dolfin/swig/forwarddeclarations.i
@@ -0,0 +1,54 @@
+/* -*- C -*- */
+// Copyright (C) 2012 Johan Hake
+//
+// This file is part of DOLFIN.
+//
+// DOLFIN is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// DOLFIN is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with DOLFIN. If not, see <http://www.gnu.org/licenses/>.
+//
+// First added:  2012-01-20
+// Last changed: 2012-01-20
+
+//=============================================================================
+// Forward declarations of common types in DOLFIN
+//=============================================================================
+
+namespace dolfin
+{
+
+  // parameter
+  class Parameters;
+  
+  // common
+  template<typename T>
+    class Hierarchical;
+
+  // mesh
+  class Mesh;
+  template<typename T> class MeshFunction;
+
+  // adaptivity
+  class ErrorControl;
+
+  // fem
+  class Form;
+  class LinearVariationalProblem;
+  class NonlinearVariationalProblem;
+  class DirichletBC;
+  class FiniteElement;
+
+  // function
+  class FunctionSpace;
+  class Function;
+}
+
--- dolfin/swig/generateswigcode.py
+++ dolfin/swig/generateswigcode.py
@@ -30,7 +30,10 @@ import os
 import re
 import glob
 import time
+import sys
+
 from codesnippets import *
+from documentation import generate_docstrings
 
 # Create time info for labeling generated code
 _local_time = time.localtime()
@@ -39,20 +42,66 @@ _date_form = dict(year = _local_time.tm_year,
                   day = _local_time.tm_yday)
 
 # Create form for copyright statement to a SWIG interface file
-_copyright_form_swig = dict(comment = r"//")
-_copyright_form_swig.update(_date_form)
+copyright_form_swig = dict(comment = r"//", holder="Johan Hake")
+copyright_form_swig.update(_date_form)
+
+# Extract original modules from dolfin.h
+# NOTE: We need these, in particular the order
+original_modules = []
+f = open("../dolfin.h")
+for line in f:
+    if "#include <dolfin/" in line and line[:17] == "#include <dolfin/":
+        module = line.split("/")[1]
+        original_modules += [module]
+f.close()
 
 # Combined modules with sub modules
-combined_modules = dict(common = ["common", "parameter", "log", "io"],
+# NOTE: Changes in combined modules should be done here.
+combined_modules = dict(common = ["common", "parameter", "log"],
                         la = ["la", "nls"],
-                        mesh = ["mesh", "intersection", "refinement", "ale"],
-                        function = ["function", "plot", "math"],
-                        fem = ["fem", "adaptivity", "quadrature"])
+                        mesh = ["mesh", "intersection", "refinement", "ale",\
+                                "graph"],
+                        function = ["function", "math"],
+                        fem = ["fem", "quadrature", "adaptivity"],
+                        io = ["io", "plot"])
+
+# Check that the directory structure of the combined modules
+# corresponds to the above dict
+module_dirs = []
+for module_dir in glob.glob("modules/*"):
+    module_dirs.append(module_dir.split(os.path.sep)[-1])
+
+# Some sanity checks 
+for module_dir in module_dirs:
+    if module_dir not in combined_modules:
+        raise RuntimeError("Found a subdirectory: '%s' under the 'modules' "\
+                           "directory, which is not listed as a combined "\
+                           "module." % module_dir)
+
+for combined_module, modules in combined_modules.items():
+    if combined_module not in module_dirs:
+        raise RuntimeError("Found a combined module: '%s' which is not a "\
+                           "subdirectory under the 'modules' directory." % \
+                           combined_module)
+    for module in modules:
+        if module not in original_modules:
+            raise RuntimeError("Found a module: '%s' listed in the '%s' "\
+                               "combined module, which is not part of the "\
+                               "original DOLFIN modules." % \
+                               (module, combined_module))
+
+# Create a map from original modules to the combined
+original_to_combined = {}
+for module in original_modules:
+    for combined_module, modules in combined_modules.items():
+        if module in modules:
+            original_to_combined[module] = combined_module
+            break
 
 # List of headers to exclude (add more here)
 excludes = ["plot.h", "IntersectionOperatorImplementation.h" ]
 
-def create_combined_module_file(combined_module):
+def create_combined_module_file(combined_module, modules):
     """
     Create and initiate the main SWIG interface file for each
     comined module file
@@ -60,39 +109,52 @@ def create_combined_module_file(combined_module):
 
     # Open file
     combined_module_file = open(os.path.join("modules", \
-                                             combined_module+".i"), "w")
+                                             combined_module, "module.i"), "w")
 
-    combined_module_file.write(copyright_statement%(_copyright_form_swig))
+    combined_module_file.write(copyright_statement%(copyright_form_swig))
 
-    combined_module_file.write()
+    combined_module_form = dict(
+        module=combined_module,
+        MODULE=combined_module.upper(),
+        )
+
+    # Create includes for header files and docstrings
+    for module_include in ["docstrings", "includes"]:
+        includes = []
+        for module in original_modules:
+            # Only include files from modules of the same combined module
+            if module not in modules:
+                continue
+            includes.append("%%include \"dolfin/swig/%s/%s.i\""%\
+                            (module, module_include))
+        combined_module_form[module_include] = "\n".join(includes)
+
+    # Create includes for import of types from other modules
+    # NOTE: Special case for common module. This should not be dependent
+    # NOTE: on other modules. It also breaks as The base class Variable
+    # NOTE: is needed by other modules and issues warnigs when these types
+    # NOTE: are imported
+    if combined_module != "common":
+        module_imports = []
+        for module in original_modules:
+            # Do not import files from modules of the same combined module
+            if module in modules:
+                continue
+            module_imports.append("%%include \"dolfin/swig/%s/imports.i\""%module)
+
+        combined_module_form["module_imports"] = "\n".join(module_imports)
+    else:
+        combined_module_form["module_imports"] = ""
     
-    # FIXME: Continue here
-    return combined_module_file
-
-def generate_typemap_includes():
-    """
-    Generate an include file which includes all typemap files under
-
-       dolfin/swig/typemaps
-       
-    """
-
-    include_file = open(os.path.join("typemaps", "includes.i"), "w")
-    include_file.write(copyright_statement%(_copyright_form_swig))
-    include_file.write("""
-//-----------------------------------------------------------------------------
-// Include all global typemap files
-//-----------------------------------------------------------------------------
-""")
-    for typemap_file in glob.glob("typemaps/*.i"):
-        include_file.write("%%include dolfin/swig/typemaps/%s\n"%typemap_file)
+    # Write the generated code
+    combined_module_file.write(combined_module_template % combined_module_form)
 
 def extract_module_header_files(module):
     """
     Extract header files for a module
     """
     module_headers = []
-    print "Processing dolfin_%s.h..." % module
+    print("Processing dolfin_%s.h..." % module)
     f = open("../%s/dolfin_%s.h" % (module, module))
     for line in f:
         if re.search("^#include ",line):
@@ -118,8 +180,8 @@ def write_module_code(module, combinedmodule):
         Write an include statements for pre or post modifier
         """
         if os.path.isfile(os.path.join(module, modifier + ".i")):
-            files[file_type].write("%%include \"dolfin/swig/%s/%s.i\"\n" % \
-                                   (module, modifier))
+            interface_file = "dolfin/swig/%s/%s.i" % (module, modifier)
+            files[file_type].write("%%include \"%s\"\n" % interface_file)
 
     # Get all headers in module
     headers = extract_module_header_files(module)
@@ -129,20 +191,22 @@ def write_module_code(module, combinedmodule):
                         imports="%%%%import(module=\"dolfin.cpp.%s\") \"%%s\"\n" %\
                         combinedmodule)
     
-    # Generate files
+    # Generate include and import files
     files = {}
+    interface_files = []
+    
     for file_type, header_form in header_forms.items():
 
         # Create the file
         files[file_type] = open(os.path.join(module, file_type + ".i"), "w")
-        files[file_type].write(copyright_statement%(_copyright_form_swig))
+        files[file_type].write(copyright_statement%(copyright_form_swig))
     
         files[file_type].write("// Auto generated %s statements for the "\
                                "module: %s\n\n"% (file_type[:-1], module))
 
         # Check if there is a foo/pre.i file
-        if file_type == "includes":
-            write_include_modifier(module, "pre")
+        #if file_type == "includes":
+        write_include_modifier(module, "pre")
 
         # Write include or import statement for each individual file
         for header in headers:
@@ -154,63 +218,70 @@ def write_module_code(module, combinedmodule):
 
         files[file_type].close()
 
-def generate_swig_include_files():
+        not_include = [os.path.join(module, "includes.i"),
+                       os.path.join(module, "modules.i")]
 
-    # Scan typemap directory and generate an include file
-    generate_typemap_includes()
+        # Add interface files
+        interface_files = ["../../%s/"%module + \
+                           interface_file.split(os.path.sep)[-1] \
+                           for interface_file in \
+                           glob.glob(os.path.join(module, "*.i")) \
+                           if interface_file not in not_include]
+
+    # Make the header files relative to where the combined module lives
+    headers = [header.replace("dolfin", "../../..") for header in headers]
+
+    return headers, interface_files
+
+def generate_swig_include_files():
+    """
+    Generate all autogenerated SWIG files for PyDOLFIN
+    """
+
+    # Generate all docstrings
+    # FIXME: Might be integratable into write_module_code
+    #generate_docstrings()
+
+    global_interface_files =  ["../../typemaps/" + \
+                               interface_file.split(os.path.sep)[-1] \
+                               for interface_file in \
+                               glob.glob(os.path.join("typemaps","*.i"))]
+
+    global_interface_files.extend(["../../shared_ptr_classes.i",
+                                   "../../exceptions.i",
+                                   "../../version.i",
+                                   "../../forwarddeclarations.i"])
 
     # Iterate over all combined modules
     for combined_module, modules in combined_modules.items():
 
         # Create a file being the root of the combined module
-        combined_module_file = create_combined_module_file(combined_module)
+        create_combined_module_file(combined_module, modules)
     
+        all_headers = []
+        all_interface_files = []
+
         # Iterate over modules in each combined module and extract headers
         for module in modules:
-            write_module_code(module, combined_module)
+            headers, interface_files = write_module_code(module, combined_module)
+            all_headers.extend(headers)
+            all_interface_files.extend(interface_files)
 
-## Generate list of header files
-#print "Generating file %s" % interface_file
-#f = open(interface_file, "w")
-#f.write("// Generated list of include files for PyDOLFIN\n")
-#for (module, module_headers) in headers:
-#    # Generate module imports
-#    f_import = open(os.path.join("import", module + ".i"), "w")
-#    f_import.write("// Auto generated import statements for the SWIG kernel module: '%s'\n\n"% module)
-#    f.write("\n// DOLFIN headers included from %s\n" % module)
-#    if os.path.isfile(module+"_pre.i"):
-#        f.write("%%include \"dolfin/swig/%s_pre.i\"\n" % module)
-#    for header in module_headers:
-#        f.write("%%include \"%s\"\n" % header)
-#        f_import.write('%%import(module="dolfin.cpp") "%s"\n'%header)
-#    if os.path.isfile(module+"_post.i"):
-#        f.write("%%include \"dolfin/swig/%s_post.i\"\n" % module)
-#    f_import.close()
-#f.close()
-#
-## Create docstrings.i file from docstrings module (only for dolfin.cpp)
-#from documentation import generate_docstrings
-#generate_docstrings()
-#
-## Extract all shared_ptr stored classes and store them in a pyton module
-## and place that under dolfin.compilemodeuls.sharedptrclasses.py
-#shared_ptr_classes = re.findall("%shared_ptr\(dolfin::(.+)\)", \
-#                                open("shared_ptr_classes.i").read())
-#
-#shared_ptr_classes = filter(lambda x: "NAME" not in x, shared_ptr_classes)
-#template = """
-#'''
-#This module contains the names of the classes in DOLFIN that is
-#stored using shared_ptrs. The file is automatically generated by the
-#generate.py script in the dolfin/swig directory.
-#'''
-#
-#__all__ = ['shared_ptr_classes']
-#
-#shared_ptr_classes = %s
-#"""
-#
-#par = os.path.pardir
-#open(os.path.join(par, par, "site-packages", "dolfin", \
-#                  "compilemodules", "sharedptrclasses.py"), "w").write(\
-#    template%(repr(shared_ptr_classes)))
+        # Add global interface files
+        all_interface_files.extend(global_interface_files)
+
+        # Add modules file
+        all_interface_files.append("../../modules/%s/module.i" % combined_module)
+
+        # Generate CMake help file
+        header_files_file = open(os.path.join("modules", combined_module,
+                                              "headers.txt"), "w")
+        header_files_file.write(";".join(all_headers))
+
+        interface_files_file = open(os.path.join("modules", combined_module,
+                                                 "interface_files.txt"), "w")
+        interface_files_file.write(";".join(all_interface_files))
+        
+
+if __name__ == "__main__":
+    generate_swig_include_files()
