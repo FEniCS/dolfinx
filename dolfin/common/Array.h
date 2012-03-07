f0bda8ff0a09e0bdcc9c342c2ba7cd3185f3f479
Joachim Berdal Haga <jobh@simula.no>
2012-03-07 10:32:57 +0100

    Disable Array copy constructor, remove utility methods that are not used

--- dolfin/common/Array.h
+++ dolfin/common/Array.h
@@ -27,6 +27,7 @@
 #include <sstream>
 #include <string>
 #include <utility>
+#include <vector>
 
 #include <dolfin/common/constants.h>
 #include <dolfin/common/types.h>
@@ -43,23 +44,20 @@ namespace dolfin
 
   template <typename T> class Array
   {
+
   public:
-    /// Create an empty array. Must be resized or assigned to to be of any use.
+
+    /// Create an empty array, with ownership. Must be resized or assigned to to be of any use.
     explicit Array() : _size(0), _x(NULL), _owner(true) {}
 
     /// Create array of size N. Array has ownership.
     explicit Array(uint N) : _size(N), _x(new T[N]), _owner(true) {}
 
-    /// Copy constructor. The constructed Array has ownership of its copy of
-    /// the data.
-    Array(const Array& other) : _size(0), _x(NULL), _owner(true)
-    {
-      *this = other;
-    }
+    /// Create an array wrapping a std::vector. Array does not take ownership.
+    explicit Array(std::vector<T>& vec) : _size(vec.size()), _x(&vec[0]), _owner(false) {}
 
-    /// Construct array from a pointer. Array may optionally take ownership of
-    /// the data (default false).
-    Array(uint N, T* x, bool owner=false) : _size(N), _x(x), _owner(owner) {}
+    /// Construct array from a pointer. Array does not take ownership.
+    Array(uint N, T* x) : _size(N), _x(x), _owner(false) {}
 
     /// Destructor.
     ~Array()
@@ -112,20 +110,8 @@ namespace dolfin
     uint size() const
     { return _size; }
 
-    /// Zero array
-    void zero()
-    { dolfin_assert(_x); std::fill(&_x[0], &_x[_size], 0.0); }
-
     /// Set entries which meet (abs(x[i]) < eps) to zero
     void zero_eps(double eps=DOLFIN_EPS);
-
-    /// Return minimum value of array
-    T min() const
-    { dolfin_assert(_x); return *std::min_element(&_x[0], &_x[_size]); }
-
-    /// Return maximum value of array
-    T max() const
-    { dolfin_assert(_x); return *std::max_element(&_x[0], &_x[_size]); }
 
     /// Access value of given entry (const version)
     const T& operator[] (uint i) const
@@ -135,13 +121,23 @@ namespace dolfin
     T& operator[] (uint i)
     { dolfin_assert(i < _size); return _x[i]; }
 
-    /// Assignment operator
-    const Array& operator= (const Array& other)
+    /// Assignment operator. If resize is required, the Array must be owner.
+    Array& operator= (const Array& other)
     {
       if (_size != other._size)
         resize(other._size);
       if (_size > 0)
         std::copy(&other._x[0], &other._x[_size], &_x[0]);
+      return *this;
+    }
+
+    /// Assignment operator from std::vector. If resize is required, the Array must be owner.
+    Array& operator= (const std::vector<T>& other)
+    {
+      if (_size != other.size())
+        resize(other.size());
+      if (_size > 0)
+        std::copy(&other[0], &other[_size], &_x[0]);
       return *this;
     }
 
@@ -153,7 +149,15 @@ namespace dolfin
     T* data()
     { return _x; }
 
+   private:
+
+    /// Disable copy construction, to avoid unanticipated sharing or
+    /// copying of data. This means that an Array must always be passed as
+    /// reference, or as a (possibly shared) pointer.
+    Array(const Array& other) /* leave body undefined */;
+
   private:
+
     /// Length of array
     uint _size;
 
