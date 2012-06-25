5205e85c6d0b181fe6819e2e1d1ef019dcf4a99e
Benjamin Kehlet <benjamik@simula.no>
2012-06-25 23:17:46 +0200

    Port the plot demo to cpp.

--- demo/undocumented/plot/cpp/main.cpp
+++ demo/undocumented/plot/cpp/main.cpp
@@ -15,17 +15,49 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with DOLFIN. If not, see <http://www.gnu.org/licenses/>.
 //
+// Modified by Benjamin Kehlet, 2012
+//
 // First added:  2007-05-29
-// Last changed: 2012-06-14
+// Last changed: 2012-06-25
 
 #include <dolfin.h>
 
 using namespace dolfin;
 
+
+class ScalarExpression : public Expression
+{
+ public:
+  void eval(Array<double>& values, const Array<double>& x) const
+  {
+    values[0] = t * 100 * exp(-10.0 * (pow(x[0] - t, 2) + pow(x[1] - t, 2)));
+  }
+
+  double t;
+};
+
+
+class VectorValuedExpression : public Expression
+{
+ public:
+  VectorValuedExpression() : Expression(2) {}
+  void eval(Array<double>& values, const Array<double>& x) const
+  {
+    values[0] = -(x[1] - t)*exp(-10.0*(pow(x[0] - t, 2) + pow(x[1] - t, 2)));
+    values[1] =  (x[0] - t)*exp(-10.0*(pow(x[0] - t, 2) + pow(x[1] - t, 2)));
+  }
+
+  double t;
+};
+
+
+
 int main()
 {
   // Read and plot mesh from file
-  Mesh mesh("dolfin-2.xml.gz");
+  Mesh mesh("../dolfin-2.xml.gz");
+
+  
 
   double R = 0.15;
   double H = 0.025;
@@ -38,31 +70,67 @@ int main()
   memcpy(original, coordinates, sizeof(original));
 
   double x, y, r;
-
-  for(int i = 0; i < 100; i++) {
-    if (X < H || X > 1.0 - H) {
+    
+  for(int i = 0; i < 100; i++) 
+  {
+    if (X < H || X > 1.0 - H) 
+    {
       dX = -dX;
     }
-    if (Y < H || Y > 1.0 - H) {
+    
+    if (Y < H || Y > 1.0 - H) 
+    {
       dY = -dY;
     }
+    
     X += dX;
     Y += dY;
-
-    for (int j = 0; j < mesh.num_vertices(); ++j) {
+    
+    for (int j = 0; j < mesh.num_vertices(); ++j) 
+    {
       x = coordinates[2*j];
       y = coordinates[2*j+1];
       r = sqrt(pow((x - X),2) + pow((y - Y),2));
-      if (r < R) {
-        coordinates[2*j]   = X + pow((r/R),2)*(x - X);
-        coordinates[2*j+1] = Y + pow((r/R),2)*(y - Y);
+      if (r < R) 
+      {
+  	coordinates[2*j]   = X + pow((r/R),2)*(x - X);
+  	coordinates[2*j+1] = Y + pow((r/R),2)*(y - Y);
       }
     }
     plot(mesh, "Animated mesh plot");
-
-    for (int j = 0; j < mesh.num_vertices()*2; ++j) {
+    
+    for (int j = 0; j < mesh.num_vertices()*2; ++j) 
+    {
       coordinates[j] = original[j];
     }
   }
+
+  
+
+  // Plot scalar function
+  Parameters p;
+  p.add("rescale", true);
+
+  ScalarExpression f_scalar;
+
+  // FIXME: VTK sets the center and zoom incorrectly
+  // if the loop starts at 0.0
+  for(double t = 0.01; t < 1.0; t += 0.01)
+  {
+    f_scalar.t = t;
+    plot(f_scalar, mesh, p);
+  }
+
+  
+  // Plot vector function
+  UnitSquare unit_square(16, 16);
+  VectorValuedExpression f_vectorvalued;
+  for (double t = 0.0; t < 1.0; t += 0.005)
+  {
+    f_vectorvalued.t = t;
+    plot(f_vectorvalued, unit_square);
+  }
+  
+
   interactive();
 }
