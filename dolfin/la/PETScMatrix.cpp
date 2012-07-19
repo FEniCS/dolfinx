a7023a660f9842d81d4f039168197d4c6e30b8fd
Garth N. Wells <gnw20@cam.ac.uk>
2012-07-19 09:02:43 +0100

    Throw error when adding entries to unallocated positions in PETScMatrix.
    This is compatible with the default setting in PETSc 3.3 and will provide consistncy across backends which do not allow post-initialisation modification of non-zeor pattern, e.g. Epetra.

--- dolfin/la/PETScMatrix.cpp
+++ dolfin/la/PETScMatrix.cpp
@@ -210,7 +210,7 @@ void PETScMatrix::init(const TensorLayout& tensor_layout)
   // Set some options
 
   // Do not allow new nonzero entries
-  //MatSetOption(*A, MAT_NEW_NONZERO_LOCATION_ERR, PETSC_FALSE);
+  MatSetOption(*A, MAT_NEW_NONZERO_LOCATION_ERR, PETSC_TRUE);
 
   #if PETSC_VERSION_MAJOR == 3 && PETSC_VERSION_MINOR >= 1
   MatSetOption(*A, MAT_KEEP_NONZERO_PATTERN, PETSC_TRUE);
