d3128674aa8be9fc698b50477d0c03dfc3d1fcab
Anders Logg <logg@glaurung>
2012-08-17 17:18:41 +0200

    Add initial value for LinearTimeDependentProblem class. Note new and shorter name in place of LinearTimeDependentVariationalProblem...

--- dolfin/fem/LinearTimeDependentProblem.cpp
+++ dolfin/fem/LinearTimeDependentProblem.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2011 Anders Logg
+// Copyright (C) 2012 Anders Logg
 //
 // This file is part of DOLFIN.
 //
@@ -15,22 +15,22 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with DOLFIN. If not, see <http://www.gnu.org/licenses/>.
 //
-// First added:  2011-06-22
+// First added:  2012-08-17
 // Last changed: 2012-08-17
 
 #include <dolfin/common/NoDeleter.h>
 #include <dolfin/function/Function.h>
-#include "Form.h"
-#include "LinearVariationalProblem.h"
+#include "TensorProductForm.h"
+#include "LinearTimeDependentProblem.h"
 
 using namespace dolfin;
 
 //-----------------------------------------------------------------------------
-LinearVariationalProblem::
-LinearVariationalProblem(const Form& a,
-                         const Form& L,
-                         Function& u)
-  : Hierarchical<LinearVariationalProblem>(*this),
+LinearTimeDependentProblem::
+LinearTimeDependentProblem(const TensorProductForm& a,
+                           const TensorProductForm& L,
+                           Function& u)
+  : Hierarchical<LinearTimeDependentProblem>(*this),
     _a(reference_to_no_delete_pointer(a)),
     _L(reference_to_no_delete_pointer(L)),
     _u(reference_to_no_delete_pointer(u))
@@ -39,12 +39,12 @@ LinearVariationalProblem(const Form& a,
   check_forms();
 }
 //-----------------------------------------------------------------------------
-LinearVariationalProblem::
-LinearVariationalProblem(const Form& a,
-                         const Form& L,
-                         Function& u,
-                         const BoundaryCondition& bc)
-  : Hierarchical<LinearVariationalProblem>(*this),
+LinearTimeDependentProblem::
+LinearTimeDependentProblem(const TensorProductForm& a,
+                           const TensorProductForm& L,
+                           Function& u,
+                           const BoundaryCondition& bc)
+  : Hierarchical<LinearTimeDependentProblem>(*this),
     _a(reference_to_no_delete_pointer(a)),
     _L(reference_to_no_delete_pointer(L)),
     _u(reference_to_no_delete_pointer(u))
@@ -56,12 +56,12 @@ LinearVariationalProblem(const Form& a,
   check_forms();
 }
 //-----------------------------------------------------------------------------
-LinearVariationalProblem::
-LinearVariationalProblem(const Form& a,
-                         const Form& L,
-                         Function& u,
-                         std::vector<const BoundaryCondition*> bcs)
-  : Hierarchical<LinearVariationalProblem>(*this),
+LinearTimeDependentProblem::
+LinearTimeDependentProblem(const TensorProductForm& a,
+                           const TensorProductForm& L,
+                           Function& u,
+                           std::vector<const BoundaryCondition*> bcs)
+  : Hierarchical<LinearTimeDependentProblem>(*this),
     _a(reference_to_no_delete_pointer(a)),
     _L(reference_to_no_delete_pointer(L)),
     _u(reference_to_no_delete_pointer(u))
@@ -74,12 +74,12 @@ LinearVariationalProblem(const Form& a,
   check_forms();
 }
 //-----------------------------------------------------------------------------
-LinearVariationalProblem::
-LinearVariationalProblem(boost::shared_ptr<const Form> a,
-                         boost::shared_ptr<const Form> L,
-                         boost::shared_ptr<Function> u,
-                         std::vector<boost::shared_ptr<const BoundaryCondition> > bcs)
-  : Hierarchical<LinearVariationalProblem>(*this),
+LinearTimeDependentProblem::
+LinearTimeDependentProblem(boost::shared_ptr<const TensorProductForm> a,
+                           boost::shared_ptr<const TensorProductForm> L,
+                           boost::shared_ptr<Function> u,
+                           std::vector<boost::shared_ptr<const BoundaryCondition> > bcs)
+  : Hierarchical<LinearTimeDependentProblem>(*this),
     _a(a), _L(L), _u(u)
 {
   // Store boundary conditions
@@ -90,53 +90,55 @@ LinearVariationalProblem(boost::shared_ptr<const Form> a,
   check_forms();
 }
 //-----------------------------------------------------------------------------
-boost::shared_ptr<const Form> LinearVariationalProblem::bilinear_form() const
+boost::shared_ptr<const TensorProductForm>
+LinearTimeDependentProblem::bilinear_form() const
 {
   return _a;
 }
 //-----------------------------------------------------------------------------
-boost::shared_ptr<const Form> LinearVariationalProblem::linear_form() const
+boost::shared_ptr<const TensorProductForm>
+LinearTimeDependentProblem::linear_form() const
 {
   return _L;
 }
 //-----------------------------------------------------------------------------
-boost::shared_ptr<Function> LinearVariationalProblem::solution()
+boost::shared_ptr<Function> LinearTimeDependentProblem::solution()
 {
   return _u;
 }
 //-----------------------------------------------------------------------------
-boost::shared_ptr<const Function> LinearVariationalProblem::solution() const
+boost::shared_ptr<const Function> LinearTimeDependentProblem::solution() const
 {
   return _u;
 }
 //-----------------------------------------------------------------------------
 std::vector<boost::shared_ptr<const BoundaryCondition> >
-LinearVariationalProblem::bcs() const
+LinearTimeDependentProblem::bcs() const
 {
   return _bcs;
 }
 //-----------------------------------------------------------------------------
 boost::shared_ptr<const FunctionSpace>
-LinearVariationalProblem::trial_space() const
+LinearTimeDependentProblem::trial_space() const
 {
   dolfin_assert(_u);
   return _u->function_space();
 }
 //-----------------------------------------------------------------------------
 boost::shared_ptr<const FunctionSpace>
-LinearVariationalProblem::test_space() const
+LinearTimeDependentProblem::test_space() const
 {
   dolfin_assert(_a);
   return _a->function_space(0);
 }
 //-----------------------------------------------------------------------------
-void LinearVariationalProblem::check_forms() const
+void LinearTimeDependentProblem::check_forms() const
 {
   // Check rank of bilinear form a
   dolfin_assert(_a);
   if (_a->rank() != 2)
   {
-    dolfin_error("LinearVariationalProblem.cpp",
+    dolfin_error("LinearTimeDependentProblem.cpp",
                  "define linear variational problem a(u, v) == L(v) for all v",
                  "Expecting the left-hand side to be a bilinear form (not rank %d)",
                  _a->rank());
@@ -146,7 +148,7 @@ void LinearVariationalProblem::check_forms() const
   dolfin_assert(_L);
   if (_L->rank() != 1)
   {
-    dolfin_error("LinearVariationalProblem.cpp",
+    dolfin_error("LinearTimeDependentProblem.cpp",
                  "define linear variational problem a(u, v) = L(v) for all v",
                  "Expecting the right-hand side to be a linear form (not rank %d)",
                  _L->rank());
@@ -157,7 +159,7 @@ void LinearVariationalProblem::check_forms() const
   dolfin_assert(_u);
   if (!_u->in(*_a->function_space(1)))
   {
-    dolfin_error("LinearVariationalProblem.cpp",
+    dolfin_error("LinearTimeDependentProblem.cpp",
                  "define linear variational problem a(u, v) = L(v) for all v",
                  "Expecting the solution variable u to be a member of the trial space");
   }
