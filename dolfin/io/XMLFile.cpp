28623df561e92ad394a8fe8bf989c44cb0179bbb
Garth N. Wells <gnw20@cam.ac.uk>
2012-09-25 08:57:24 +0900

    MeshFunction XML fix.

--- dolfin/io/XMLFile.cpp
+++ dolfin/io/XMLFile.cpp
@@ -242,8 +242,9 @@ template<typename T> void XMLFile::read_mesh_function(MeshFunction<T>& t,
   // Open file on process 0 only
   pugi::xml_node dolfin_node;
   if (MPI::process_number() == 0)
+  {
     load_xml_doc(xml_doc);
-    pugi::xml_node dolfin_node = get_dolfin_xml_node(xml_doc);
+    dolfin_node = get_dolfin_xml_node(xml_doc);
   }
   XMLMeshFunction::read(t, type, dolfin_node);
 }
