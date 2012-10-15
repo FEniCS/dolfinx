e46fe0db286100311ab8a80a84b283ac165163b9
Patrick Farrell <patrick.farrell06@imperial.ac.uk>
2012-10-15 19:31:49 +0100

    Revert the change to DirichletBC, it breaks the Newton iteration.

--- dolfin/fem/DirichletBC.cpp
+++ dolfin/fem/DirichletBC.cpp
@@ -516,13 +516,11 @@ void DirichletBC::apply(GenericMatrix* A,
     // Get values (these must reside in local portion (including ghost
     // values) of the vector
     std::vector<double> x_values(size);
-    x->get_local(x_values);
+    x->get_local(&x_values[0], dofs.size(), &dofs[0]);
 
     // Modify RHS entries
     for (uint i = 0; i < size; i++)
-    {
-      values[i] = x_values[dofs[i]] - values[i];
-    }
+      values[i] = x_values[i] - values[i];
   }
 
   log(PROGRESS, "Applying boundary conditions to linear system.");
