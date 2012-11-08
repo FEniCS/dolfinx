0b1065ee96b9e9541be7d0d5f315bcf15a7b62a1
Martin Sandve Alnæs <martinal@simula.no>
2012-11-08 10:37:20 +0100

    Work on figuring out float conversion of Function(R) in parallell, not working but checking in for the record.

--- test/unit/function/python/Function.py
+++ test/unit/function/python/Function.py
@@ -52,20 +52,47 @@ class Interface(unittest.TestCase):
 
         self.assertTrue(all(u_values==1))
 
-    def xtest_float_conversion1(self): # TODO: Check if this works in parallell
+    def test_constant_float_conversion(self):
+        c = Constant(3.45)
+        self.assertTrue(float(c) == 3.45)
+
+    def xtest_real_function_vector_gather(self): # FIXME: Fails because of swig wrapping issues?
+        import numpy
+        c = Function(R)
+        c.assign(Constant(2.34))
+
+        values = numpy.zeros(1, dtype='d')
+        vec = c.vector()
+
+        # Gather value directly from vector in a parallell safe way
+        indices = numpy.zeros(1, dtype='i')
+        vec.gather(values, indices)
+
+        self.assertEqual(float(values[0]), 2.34)
+
+    def xtest_real_function_vector_getlocal(self): # FIXME: Fails because of swig wrapping issues?
+        import numpy
+        c = Function(R)
+        c.assign(Constant(2.34))
+
+        values = numpy.zeros(1, dtype='d')
+        vec = c.vector()
+
+        # Get value directly from local vector
+        vec.get_local(values)
+
+        self.assertEqual(float(values[0]), 2.34)
+
+    def xtest_real_function_float_conversion1(self): # FIXME: Does not work in parallell
         c = Function(R)
         self.assertTrue(float(c) == 0.0)
 
-    def xtest_float_conversion2(self): # TODO: Check if this works in parallell
+    def xtest_real_function_float_conversion2(self): # FIXME: Does not work in parallell
         c = Function(R)
         c.assign(Constant(2.34))
         self.assertTrue(float(c) == 2.34)
 
-    def xtest_float_conversion3(self): # TODO: Check if this works in parallell
-        c = Constant(3.45)
-        self.assertTrue(float(c) == 3.45)
-
-    def xtest_float_conversion4(self): # TODO: Check if this works in parallell
+    def xtest_real_function_float_conversion3(self): # FIXME: Does not work in parallell
         c = Function(R)
         c.vector()[:] = 1.23
         self.assertTrue(float(c) == 1.23)
