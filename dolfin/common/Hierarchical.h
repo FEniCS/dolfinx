aaeca5d5346cdc2c64189f256e32491b2653f9b2
Marie E. Rognes <meg@simula.no>
2012-11-29 15:32:03 +0100

    Add function 'clear_child' for Hierarchical resetting the child pointer, thus enabling hierarchies to be reset/cleared.

--- dolfin/common/Hierarchical.h
+++ dolfin/common/Hierarchical.h
@@ -16,7 +16,7 @@
 // along with DOLFIN. If not, see <http://www.gnu.org/licenses/>.
 //
 // First added:  2011-01-30
-// Last changed: 2011-10-04
+// Last changed: 2012-11-29
 
 #ifndef __HIERARCHICAL_H
 #define __HIERARCHICAL_H
@@ -232,6 +232,12 @@ namespace dolfin
     /// Set parent
     void set_parent(boost::shared_ptr<T> parent)
     { _parent = parent; }
+
+    /// Clear child
+    void clear_child()
+    {
+      _child.reset();
+    }
 
     /// Set child
     void set_child(boost::shared_ptr<T> child)
