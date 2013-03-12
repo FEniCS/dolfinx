9913219cabce46ecea1f01504b25c38a8dc872b5
Johannes Ring <johannr@simula.no>
2013-03-12 13:25:01 +0100

    Another tiny bug fix in ExodusFile.cpp.

--- dolfin/io/ExodusFile.cpp
+++ dolfin/io/ExodusFile.cpp
@@ -238,7 +238,6 @@ void ExodusFile::write_function(const Function& u, double time) const
   if (dofmap.max_cell_dimension() == cell_based_dim)
   {
     // Extract DOFs from u
-    const uint num_cells = mesh.num_cells();
     std::vector<int> dof_set;
     for (CellIterator cell(mesh); !cell.end(); ++cell)
     {
