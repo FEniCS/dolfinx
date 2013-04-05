8a1963feba4acb521d3f31266640a7648019a6c5
Johan Hake <hake.dev@gmail.com>
2013-04-05 10:13:25 +0200

    Added missing file

---
+++ dolfin/multistage/dolfin_multistage.h
@@ -0,0 +1,10 @@
+#ifndef __DOLFIN_MULTISTAGE_H
+#define __DOLFIN_MULTISTAGE_H
+
+// DOLFIN multistage interface
+
+#include <dolfin/multistage/MultiStageScheme.h>
+#include <dolfin/multistage/RKSolver.h>
+#include <dolfin/multistage/PointIntegralSolver.h>
+
+#endif
