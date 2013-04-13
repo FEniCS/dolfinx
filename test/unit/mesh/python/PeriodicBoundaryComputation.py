d880864e90d24de18ff14ad9d9a656add9f49599
Mikael Mortensen <mikaem@math.uio.no>
2013-04-13 19:34:05 +0200

    Moved periodic tests from unit/fem/python/PeriodicBC.py to unit/function/python/ConstrainedFunctionSpace.py

--- test/unit/mesh/python/PeriodicBoundaryComputation.py
+++ test/unit/mesh/python/PeriodicBoundaryComputation.py
@@ -17,30 +17,30 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with DOLFIN. If not, see <http://www.gnu.org/licenses/>.
 #
-# First added:  
-# Last changed: 
+# First added:  2013-04-12 
+# Last changed: 2013-04-12 
 
 import unittest
 import numpy as np
 from dolfin import *
 
+class PeriodicBoundary(SubDomain):
+    def inside(self, x, on_boundary):
+        return x[0] < DOLFIN_EPS
+    def map(self, x, y):
+        y[0] = x[0] - 1.0
+        y[1] = x[1]
+
 if MPI.num_processes() == 1:
+    # Create instance of PeriodicBoundaryComputation
+    periodic_boundary = PeriodicBoundary()
+    pbc = PeriodicBoundaryComputation()
+    mesh = UnitSquareMesh(4, 4)
+
     class PeriodicBoundaryComputations(unittest.TestCase):
 
         def testComputePeriodicPairs(self):
-            
-            class PeriodicBoundary2(SubDomain):
-                def inside(self, x, on_boundary):
-                    return x[0] < DOLFIN_EPS
-                def map(self, x, y):
-                    y[0] = x[0] - 1.0
-                    y[1] = x[1]
-
-            # Create periodic boundary condition
-            periodic_boundary = PeriodicBoundary2()
-            pbc = PeriodicBoundaryComputation()
-            mesh = UnitSquareMesh(4, 4)
-            
+                        
             # Verify that correct number of periodic pairs are computed
             vertices = pbc.compute_periodic_pairs(mesh, periodic_boundary, 0)
             edges    = pbc.compute_periodic_pairs(mesh, periodic_boundary, 1)
@@ -48,19 +48,7 @@ if MPI.num_processes() == 1:
             self.assertEqual(len(edges), 4)
             
         def testMastersSlaves(self):
-            
-            class PeriodicBoundary2(SubDomain):
-                def inside(self, x, on_boundary):
-                    return x[0] < DOLFIN_EPS
-                def map(self, x, y):
-                    y[0] = x[0] - 1.0
-                    y[1] = x[1]
-
-            # Create periodic boundary condition
-            periodic_boundary = PeriodicBoundary2()
-            pbc = PeriodicBoundaryComputation()
-            mesh = UnitSquareMesh(4, 4)
-            
+                        
             # Verify that correct number of masters and slaves are marked
             mf = pbc.masters_slaves(mesh, periodic_boundary, 0)
             self.assertEqual(len(np.where(mf.array() == 1)[0]), 5)
