d1083bbdde511ec90baaadb415fc99cb633a57fd
Anders Logg <logg@simula.no>
2013-04-18 17:15:35 +0200

    Add class MeshPointIntersection for computing intersection between mesh and point. Seems to work fine.

---
+++ dolfin/geometry/MeshPointIntersection.cpp
@@ -0,0 +1,55 @@
+// Copyright (C) 2013 Anders Logg
+//
+// This file is part of DOLFIN.
+//
+// DOLFIN is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// DOLFIN is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with DOLFIN. If not, see <http://www.gnu.org/licenses/>.
+//
+// First added:  2013-04-18
+// Last changed: 2013-04-18
+
+#include <dolfin/log/LogStream.h>
+#include <dolfin/mesh/Cell.h>
+#include "BoundingBoxTree.h"
+#include "MeshPointIntersection.h"
+
+using namespace dolfin;
+
+//-----------------------------------------------------------------------------
+MeshPointIntersection::MeshPointIntersection(const Mesh& mesh,
+                                             const Point& point)
+{
+  compute_intersection(mesh, point);
+}
+//-----------------------------------------------------------------------------
+void MeshPointIntersection::compute_intersection(const Mesh& mesh,
+                                                 const Point& point)
+{
+  dolfin_assert(_intersected_cells.size() == 0);
+
+  // Compute bounding box tree for mesh
+  BoundingBoxTree tree(mesh);
+
+  // Compute list of candidates for intersection
+  std::vector<unsigned int> cell_candidates = tree.find(point);
+
+  // Extract subset of intersecting cells
+  for (unsigned int i = 0; i < cell_candidates.size(); ++i)
+  {
+    const unsigned int cell_index = cell_candidates[i];
+    Cell cell(mesh, cell_index);
+    if (cell.contains(point))
+      _intersected_cells.push_back(cell_index);
+  }
+}
+//-----------------------------------------------------------------------------
---
+++ dolfin/geometry/MeshPointIntersection.h
@@ -0,0 +1,56 @@
+// Copyright (C) 2013 Anders Logg
+//
+// This file is part of DOLFIN.
+//
+// DOLFIN is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// DOLFIN is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with DOLFIN. If not, see <http://www.gnu.org/licenses/>.
+//
+// First added:  2013-04-18
+// Last changed: 2013-04-18
+
+#ifndef __MESH_POINT_INTERSECTION_H
+#define __MESH_POINT_INTERSECTION_H
+
+#include <vector>
+
+namespace dolfin
+{
+
+  /// This class represents an intersection between a _Mesh_ and a
+  /// _Point_. The resulting intersection is stored as a list of zero
+  /// or more cells.
+
+  class MeshPointIntersection
+  {
+  public:
+
+    /// Create (and compute) intersection between mesh and point
+    MeshPointIntersection(const Mesh& mesh, const Point& point);
+
+    /// Return the list of (local) indices for intersected cells
+    const std::vector<unsigned int>& intersected_cells() const
+    { return _intersected_cells; }
+
+  private:
+
+    /// The list of (local) indices for intersected cells
+    std::vector<unsigned int> _intersected_cells;
+
+    // Compute intersection
+    void compute_intersection(const Mesh& mesh, const Point& point);
+
+  };
+
+}
+
+#endif
