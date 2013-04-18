9299a9245c0d2468712b9282cdeb175becd4a050
Anders Logg <logg@simula.no>
2013-04-18 17:16:36 +0200

    Add missing file

---
+++ dolfin/geometry/dolfin_geometry.h
@@ -0,0 +1,9 @@
+#ifndef __DOLFIN_GEOMETRY_H
+#define __DOLFIN_GEOMETRY_H
+
+// DOLFIN geometry interface
+
+#include <dolfin/geometry/BoundingBoxTree.h>
+#include <dolfin/geometry/MeshPointIntersection.h>
+
+#endif
