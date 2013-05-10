f947a4f6f3d48100d28ca1c29e27f7097892104f
Garth N. Wells <gnw20@cam.ac.uk>
2013-05-10 18:00:58 +0100

    Use MathJax via Sphinx for scalable maths in docs.
    Requires Sphinx >= 1.1

--- doc/sphinx-common/source/conf.py
+++ doc/sphinx-common/source/conf.py
@@ -25,7 +25,12 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx', 'sphinx.ext.pngmath']
+#extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx',
+#              'sphinx.ext.pngmath']
+
+# Use mahjax maths
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx',
+              'sphinx.ext.mathjax']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
