e87339eed4b95716fc914b3615dcb634ab304e28
Chris Richardson <chris@bpi.cam.ac.uk>
2014-01-21 13:15:46 +0000

    Add restore partitioning parameter

--- dolfin/io/HDF5File.cpp
+++ dolfin/io/HDF5File.cpp
@@ -18,7 +18,7 @@
 // Modified by Garth N. Wells, 2012
 //
 // First added:  2012-06-01
-// Last changed: 2014-01-17
+// Last changed: 2014-01-21
 
 #ifdef HAS_HDF5
 
@@ -64,6 +64,9 @@ HDF5File::HDF5File(MPI_Comm comm, const std::string filename,
 {
   // HDF5 chunking
   parameters.add("chunking", false);
+
+  // Restore partitioning when reading
+  parameters.add("restore_partitioning", true);
 
   // Open HDF5 file
   const bool mpi_io = MPI::size(_mpi_comm) > 1 ? true : false;
@@ -1235,10 +1238,11 @@ void HDF5File::read(Mesh& input_mesh, const std::string mesh_name) const
     const std::size_t proc = MPI::rank(_mpi_comm);
     cell_range = std::make_pair(partitions[proc], partitions[proc + 1]);
 
-    // Prevent repartitioning
-    mesh_data.cell_partition = 
-      std::vector<std::size_t>(cell_range.second - cell_range.first,
-                               proc);
+    // Restore partitioning unless requested otherwise
+    if(parameters["restore_partitioning"])
+      mesh_data.cell_partition = 
+        std::vector<std::size_t>(cell_range.second - cell_range.first,
+                                 proc);
   }
   else
   {
