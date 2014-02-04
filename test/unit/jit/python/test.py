dcfcaeda100ea1124b47045346c10234b4a25868
mikael_mortensen <mikael.mortensen@gmail.com>
2014-02-04 04:00:33 +0100

    Added note to jit compiler unit test to remind us to modify docstring of compile_extension_module if test is changed.

--- test/unit/jit/python/test.py
+++ test/unit/jit/python/test.py
@@ -42,6 +42,10 @@ class JIT(unittest.TestCase):
             self.assertAlmostEqual(M2, 1.0)
 
     def test_compile_extension_module(self):
+      
+        # This test should do basically the same as the docstring of
+        # the compile_extension_module function in compilemodule.py.
+        # Remember to update the docstring if the test is modified!
 
         if not has_linear_algebra_backend("PETSc"):
             return
