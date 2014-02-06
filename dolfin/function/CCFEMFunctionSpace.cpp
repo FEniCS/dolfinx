20f763a28e9416ddb9d23c394486ec761aff7884
Anders Logg <logg@simula.no>
2014-02-06 23:13:56 +0100

    Compute uncut and covered cells from tree-tree collisions. Remains to compute collision maps for cut cells.

--- dolfin/function/CCFEMFunctionSpace.cpp
+++ dolfin/function/CCFEMFunctionSpace.cpp
@@ -176,21 +176,67 @@ void CCFEMFunctionSpace::_build_collision_maps()
 {
   begin(PROGRESS, "Building collision maps.");
 
+  // Clear collision maps
+  _uncut_cells.clear();
+  _cut_cells.clear();
+  _covered_cells.clear();
+
   // Iterate over all parts
   for (std::size_t i = 0; i < num_parts(); i++)
   {
-
+    // Create markers for collisions with domain and boundary
+    std::vector<bool> markers_domain(_meshes[i]->num_cells());
+    std::vector<bool> markers_boundary(_meshes[i]->num_cells());
+    std::fill(markers_domain.begin(), markers_domain.end(), false);
+    std::fill(markers_boundary.begin(), markers_boundary.end(), false);
 
     // Iterate over covering parts (with higher part number)
     for (std::size_t j = i + 1; j < num_parts(); j++)
     {
       log(PROGRESS, "Computing collisions for mesh %d overlapped by mesh %d.", i, j);
 
-      log(PROGRESS, "  Computing domain collisions.");
+      // Compute both domain collisions and boundary collisions
       auto domain_collisions = _trees[i]->compute_collisions(*_trees[j]);
-      log(PROGRESS, "  Computing boundary collisions.");
       auto boundary_collisions = _trees[i]->compute_collisions(*_boundary_trees[j]);
+
+      // Mark cell collisions for domain
+      for (auto it = domain_collisions.first.begin();
+           it != domain_collisions.first.end(); ++it)
+      {
+        markers_domain[*it] = true;
+      }
+
+      // Mark cell collisions for boundary
+      for (auto it = boundary_collisions.first.begin();
+           it != boundary_collisions.first.end(); ++it)
+      {
+        markers_boundary[*it] = true;
+      }
     }
+
+    // Extract uncut, cut and covered cells:
+    //
+    // uncut   = cell not colliding with any other mesh domain
+    // cut     = cell colliding with any other mesh boundary
+    // covered = cell colliding some other mesh domain but not any boundary
+
+    // Iterate over cells and check markers
+    std::vector<unsigned int> uncut_cells;
+    std::vector<unsigned int> cut_cells; // FIXME: More data needed
+    std::vector<unsigned int> covered_cells;
+    for (unsigned int c = 0; c < _meshes[i]->num_cells(); c++)
+    {
+      if (!markers_domain[c])
+        uncut_cells.push_back(c);
+      else if (markers_boundary[c])
+        cut_cells.push_back(c);
+      else
+        covered_cells.push_back(c);
+    }
+
+    // Store data for this mesh
+    _uncut_cells.push_back(cut_cells);
+    _covered_cells.push_back(covered_cells);
   }
 
   end();
