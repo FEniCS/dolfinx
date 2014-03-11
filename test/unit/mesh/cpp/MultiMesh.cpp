dad11e2e281712d78d5a7826c1a845996342a126
August Johansson <august@simula.no>
2014-03-11 14:02:52 +0100

    MultiMesh unit test with two meshes ok

--- test/unit/mesh/cpp/MultiMesh.cpp
+++ test/unit/mesh/cpp/MultiMesh.cpp
@@ -16,7 +16,7 @@
 // along with DOLFIN. If not, see <http://www.gnu.org/licenses/>.
 //
 // First added:  2014-03-10
-// Last changed: 2014-03-10
+// Last changed: 2014-03-11
 //
 // Unit tests for MultiMesh
 
@@ -40,8 +40,6 @@ public:
 
     // Translate some random distance
     Point pt(0.632350, 0.278498);
-    //Point pt(0.50001, 0);
-    //Point pt(0.1,0.1);
     mesh_1.translate(pt);
 
     // Exact volume is known
@@ -58,26 +56,12 @@ public:
 
     for (std::size_t part = 0; part < multimesh.num_parts(); ++part)
     {
-      std::cout<<"\n\n part "<<part<<"\n\n";
-
       // Uncut cell volume given by function volume
       auto uncut_cells = multimesh.uncut_cells(part);
       for (auto it = uncut_cells.begin(); it != uncut_cells.end(); ++it)
       {
         const Cell cell(*multimesh.part(part), *it);
         volume += cell.volume();
-
-        const MeshGeometry& geometry = cell.mesh().geometry();
-        const unsigned int* vertices = cell.entities(0);
-        const Point a = geometry.point(vertices[0]);
-        const Point b = geometry.point(vertices[1]);
-        const Point c = geometry.point(vertices[2]);
-        Point av = (a+b+c)/3.;
-        //std::cout << av[0]<<' '<<av[1]<<' '<<av[2]<<std::endl;
-        std::cout << "drawtriangle("
-                  << "["<<a[0]<<' '<<a[1]<<"],"
-                  << "["<<b[0]<<' '<<b[1]<<"],"
-                  << "["<<c[0]<<' '<<c[1]<<"]);\n";
       }
 
       // Cut cell volume given by quadrature rule
@@ -86,77 +70,19 @@ public:
       for (auto it = cut_cells.begin(); it != cut_cells.end(); ++it)
       {
         const Cell cell(*multimesh.part(part), *it);
-        const MeshGeometry& geometry = cell.mesh().geometry();
-        const unsigned int* vertices = cell.entities(0);
-        const Point a = geometry.point(vertices[0]);
-        const Point b = geometry.point(vertices[1]);
-        const Point c = geometry.point(vertices[2]);
-        Point av = (a+b+c)/3.;
-        std::cout << av[0]<<' '<<av[1]<<' '<<av[2]<<std::endl;
-
         volume += cell.volume();
 
         // Loop over weights
         for (std::size_t i = 0; i < qr[*it].first.size(); ++i)
-        {
           volume += qr[*it].first[i];
-          //std::cout<<qr[*it].first[i]<<' ';
-        }
       }
-
-      // // Other part's cut cells (none)
-      // {
-      //   const int part = 1;
-      //   auto cut_cells = multimesh.cut_cells(part);
-      //   auto qr = multimesh.quadrature_rule_cut_cells(part);
-      //   for (auto it = cut_cells.begin(); it != cut_cells.end(); ++it)
-      //   {
-      //     // const Cell cell(*multimesh.part(part), *it);
-      //     // const MeshGeometry& geometry = cell.mesh().geometry();
-      //     // const unsigned int* vertices = cell.entities(0);
-      //     // const Point a = geometry.point(vertices[0]);
-      //     // const Point b = geometry.point(vertices[1]);
-      //     // const Point c = geometry.point(vertices[2]);
-      //     // Point av = (a+b+c)/3.;
-      //     // std::cout << av[0]<<' '<<av[1]<<' '<<av[2]<<std::endl;
-
-      //     // Loop over weights
-      //     for (std::size_t i = 0; i < qr[*it].first.size(); ++i)
-      //       volume += qr[*it].first[i];
-      //   }
-      // }
-
-
-
-
-      // // Covered cells
-      // std::cout<<"\n\n covered\n\n";
-      // auto covered_cells = multimesh.covered_cells(part);
-      // for (auto it = covered_cells.begin(); it != covered_cells.end(); ++it)
-      // {
-      //   const Cell cell(*multimesh.part(part), *it);
-      //   volume += cell.volume();
-
-
-      //   const MeshGeometry& geometry = cell.mesh().geometry();
-      //   const unsigned int* vertices = cell.entities(0);
-      //   const Point a = geometry.point(vertices[0]);
-      //   const Point b = geometry.point(vertices[1]);
-      //   const Point c = geometry.point(vertices[2]);
-      //   Point av = (a+b+c)/3.;
-      //   //std::cout << av[0]<<' '<<av[1]<<' '<<av[2]<<std::endl;
-      //   std::cout << "drawtriangle("
-      //             << "["<<a[0]<<' '<<a[1]<<"],"
-      //             << "["<<b[0]<<' '<<b[1]<<"],"
-      //             << "["<<c[0]<<' '<<c[1]<<"]);\n";
-
-      // }
-
     }
 
     CPPUNIT_ASSERT_DOUBLES_EQUAL(exact_volume, volume, DOLFIN_EPS_LARGE);
   }
 };
+
+
 
 
 
@@ -166,75 +92,3 @@ int main()
   DOLFIN_TEST;
 }
 
-
-
-// class MeshFunctions : public CppUnit::TestFixture
-// {
-//   CPPUNIT_TEST_SUITE(MeshFunctions);
-//   CPPUNIT_TEST(test_create_from_domains);
-//   CPPUNIT_TEST_SUITE_END();
-
-// public:
-
-//   void test_create_from_domains()
-//   {
-//     // Create mesh
-//     std::shared_ptr<Mesh> mesh(new UnitSquareMesh(3, 3));
-//     dolfin_assert(mesh);
-
-//     const std::size_t D = mesh->topology().dim();
-
-//     // Test setting all values
-//     for (std::size_t d = 0; d <= D; ++d)
-//     {
-//       // Create MeshDomains object
-//       MeshDomains mesh_domains;
-//       mesh_domains.init(D);
-
-//       mesh->init(d);
-
-//       // Build mesh domain
-//       std::map<std::size_t, std::size_t>& domain = mesh_domains.markers(d);
-//       for (std::size_t i = 0; i < mesh->num_entities(d); ++i)
-//         domain.insert(std::make_pair(i, i));
-
-//       // Create MeshFunction and test values
-//       MeshFunction<std::size_t> mf(mesh, d, mesh_domains);
-//       for (std::size_t i = 0; i < mf.size(); ++i)
-//         CPPUNIT_ASSERT(mf[i] == i);
-//     }
-
-//     // Test setting some values only
-//     for (std::size_t d = 0; d <= D; ++d)
-//     {
-//       // Create MeshDomains object
-//       MeshDomains mesh_domains;
-//       mesh_domains.init(D);
-
-//       mesh->init(d);
-
-//       // Build mesh domain
-//       std::map<std::size_t, std::size_t>& domain = mesh_domains.markers(d);
-//       const std::size_t num_entities = mesh->num_entities(d);
-//       for (std::size_t i = num_entities/2; i < num_entities; ++i)
-//         domain.insert(std::make_pair(i, i));
-
-//       // Create MeshFunction and test values
-//       MeshFunction<std::size_t> mf(mesh, d, mesh_domains);
-//       for (std::size_t i = 0; i < num_entities/2; ++i)
-//         CPPUNIT_ASSERT(mf[i] == std::numeric_limits<std::size_t>::max());
-//       for (std::size_t i = num_entities/2; i < mf.size(); ++i)
-//         CPPUNIT_ASSERT(mf[i] == i);
-//     }
-
-
-//   }
-
-// };
-
-
-// int main()
-// {
-//   CPPUNIT_TEST_SUITE_REGISTRATION(MeshFunctions);
-//   DOLFIN_TEST;
-// }
