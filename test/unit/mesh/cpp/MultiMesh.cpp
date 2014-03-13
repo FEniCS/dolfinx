725ae596805824be762ad2e2e35a7bf645e493db
August Johansson <august@simula.no>
2014-03-13 14:59:55 +0100

    Unit test for three meshes done

--- test/unit/mesh/cpp/MultiMesh.cpp
+++ test/unit/mesh/cpp/MultiMesh.cpp
@@ -16,7 +16,7 @@
 // along with DOLFIN. If not, see <http://www.gnu.org/licenses/>.
 //
 // First added:  2014-03-10
-// Last changed: 2014-03-12
+// Last changed: 2014-03-13
 //
 // Unit tests for MultiMesh
 
@@ -28,62 +28,11 @@ using namespace dolfin;
 class MultiMeshes : public CppUnit::TestFixture
 {
   CPPUNIT_TEST_SUITE(MultiMeshes);
-  //CPPUNIT_TEST(test_integrate_triangles);
+  CPPUNIT_TEST(test_integrate_triangles);
   CPPUNIT_TEST(test_integrate_triangles_three_meshes);
   CPPUNIT_TEST_SUITE_END();
 
 public:
-
-#include <string>
-#include <sstream>
-#include <fstream>
-
-#define Pause { std::cout<<__FUNCTION__<<' '<<__LINE__; char fdajfds; std::cin>>fdajfds; }
-
-  void dolfin_write_medit_triangles(const std::string &filename,
-                                    const Mesh& mesh,
-                                    const int t = 0)
-  {
-    std::stringstream ss;
-    ss<<filename<<"."<<t<<".mesh";
-    std::ofstream file(ss.str().c_str());
-    if (!file.good()) { std::cout << ss.str()<<'\n'; Pause; }
-    file.precision(13);
-    // write vertices
-    const std::size_t nno = mesh.num_vertices();
-    file << "MeshVersionFormatted 1\nDimension\n2\nVertices\n"
-         << nno<<'\n';
-    const std::vector<double>& coords = mesh.coordinates();
-    for (std::size_t i = 0; i < nno; ++i)
-      file << coords[2*i]<<' '<<coords[2*i+1]<<" 1\n";
-    // write connectivity
-    const std::size_t nel = mesh.num_cells();
-    file << "Triangles\n"
-         << nel <<'\n';
-    const std::vector<unsigned int>& cells = mesh.cells();
-    for (std::size_t e = 0; e < nel; ++e)
-      file << cells[3*e]+1<<' '<<cells[3*e+1]+1<<' '<<cells[3*e+2]+1<<" 1\n";
-    file.close();
-  }
-
-  std::string drawtriangle(const Cell &cell)
-  {
-    const MeshGeometry& geometry = cell.mesh().geometry();
-    const unsigned int* vertices = cell.entities(0);
-    const Point a = geometry.point(vertices[0]);
-    const Point b = geometry.point(vertices[1]);
-    const Point c = geometry.point(vertices[2]);
-    //Point av = (a+b+c)/3.;
-    //std::cout << av[0]<<' '<<av[1]<<' '<<av[2]<<std::endl;
-    std::stringstream ss;
-    ss << "drawtriangle("
-       << "["<<a[0]<<' '<<a[1]<<"],"
-       << "["<<b[0]<<' '<<b[1]<<"],"
-       << "["<<c[0]<<' '<<c[1]<<"]);";
-    return ss.str();
-  }
-
-
 
   void test_integrate_triangles()
   {
@@ -137,10 +86,6 @@ public:
 
   void test_integrate_triangles_three_meshes()
   {
-    std::cout<<"\n\n\n"<<__FUNCTION__<<"\n\n";
-
-    set_log_level(PROGRESS);
-
     // Create MultiMesh from three triangle meshes of the unit square
     UnitSquareMesh mesh_0(3, 3), mesh_1(4, 4), mesh_2(5, 5);
 
@@ -153,8 +98,6 @@ public:
     // Exact volume is known
     const double exact_volume = 2.15;
 
-
-
     // Build the multimesh
     MultiMesh multimesh;
     multimesh.add(mesh_0);
@@ -162,32 +105,20 @@ public:
     multimesh.add(mesh_2);
     multimesh.build();
 
-
-    std::cout << std::abs(a[0]-b[0])*std::abs(a[1]-b[1])<<'\n';
-
     // For part 0, compute area of uncut, cut and covered cells
     double volume = 0;
 
     for (std::size_t part = 0; part < multimesh.num_parts(); ++part)
     {
-      std::cout<<"\npart "<<part<<"\n\n";
-
-
       // Uncut cell volume given by function volume
-      std::cout<<"\nuncut\n";
       auto uncut_cells = multimesh.uncut_cells(part);
       for (auto it = uncut_cells.begin(); it != uncut_cells.end(); ++it)
       {
         const Cell cell(*multimesh.part(part), *it);
         volume += cell.volume();
-
-        //std::cout << (*it)<<' ';
-        //std::cout<<drawtriangle(cell);
-        //std::cout<<(*it)<<' '<<drawtriangle(cell)<<'\n';
-       }
+     }
 
       // Cut cell volume given by quadrature rule
-      std::cout<<"\ncut\n";
       auto cut_cells = multimesh.cut_cells(part);
       auto qr = multimesh.quadrature_rule_cut_cells(part);
       for (auto it = cut_cells.begin(); it != cut_cells.end(); ++it)
@@ -195,19 +126,10 @@ public:
         const Cell cell(*multimesh.part(part), *it);
         volume += cell.volume();
 
-        //std::cout << (*it)<<' ';
-        //std::cout<<drawtriangle(cell);
-        std::cout<<(*it)<<' '<<drawtriangle(cell)<<'\n';
-
-
         // Loop over weights
         for (std::size_t i = 0; i < qr[*it].first.size(); ++i)
           volume += qr[*it].first[i];
       }
-
-      //auto covered_cells = multimesh.covered_cells(part);
-
-
     }
 
     CPPUNIT_ASSERT_DOUBLES_EQUAL(exact_volume, volume, DOLFIN_EPS_LARGE);
