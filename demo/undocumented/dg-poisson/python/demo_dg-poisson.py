7708dc1611d23efc66fcf6efba469abc6a953538
mirok <miroslav.kuchta@gmail.com>
2014-03-17 20:20:52 +0100

    add Neumann and Dirichlet bcs to demo-dg-poisson

--- demo/undocumented/dg-poisson/python/demo_dg-poisson.py
+++ demo/undocumented/dg-poisson/python/demo_dg-poisson.py
@@ -4,12 +4,17 @@
 
 on the unit square with source f given by
 
-    f(x, y) = 500*exp(-((x-0.5)^2 + (y-0.5)^2)/0.02)
+    f(x, y) = -100*exp(-((x - 0.5)^2 + (y - 0.5)^2)/0.02)
 
 and boundary conditions given by
 
-    u(x, y)     = 0
-    du/dn(x, y) = 0
+    u(x, y)     = u0 on x = 0 and x = 1
+    du/dn(x, y) = g  on y = 0 and y = 1
+
+where
+
+    u0 = x + 0.25*sin(2*pi*x)
+    g = (y - 0.5)**2 
 
 using a discontinuous Galerkin formulation (interior penalty method).
 """
@@ -32,25 +37,47 @@ using a discontinuous Galerkin formulation (interior penalty method).
 # along with DOLFIN. If not, see <http://www.gnu.org/licenses/>.
 #
 # Modified by Anders Logg 2008-2011
-#
+#             Miroslav Kuchta 2014
 # First added:  2007-10-02
-# Last changed: 2012-11-12
+# Last changed: 2014-03-16
 
 from dolfin import *
 
+# Define class marking Dirichlet boundary (x = 0 or x = 1)
+class DirichletBoundary(SubDomain):
+  def inside(self, x, on_boundary):
+    return on_boundary and near(x[0]*(1 - x[0]), 0)
+
+# Define class marking Neumann boundary (y = 0 or y = 1)
+class NeumanBoundary(SubDomain):
+  def inside(self, x, on_boundary):
+    return on_boundary and near(x[1]*(1 - x[1]), 0)
+
 # Create mesh and define function space
 mesh = UnitSquareMesh(24, 24)
-V = FunctionSpace(mesh, "DG", 1)
+V = FunctionSpace(mesh, 'DG', 1)
 
 # Define test and trial functions
-v = TestFunction(V)
 u = TrialFunction(V)
+v = TestFunction(V)
 
-# Define normal component, mesh size and right-hand side
+# Define normal vector and mesh size
 n = FacetNormal(mesh)
 h = CellSize(mesh)
 h_avg = (h('+') + h('-'))/2
-f = Expression("500.0*exp(-(pow(x[0] - 0.5, 2) + pow(x[1] - 0.5, 2)) / 0.02)")
+
+# Define the source term f, Dirichlet term u0 and Neumann term g
+f = Expression('-100*exp(-(pow(x[0] - 0.5, 2) + pow(x[1] - 0.5, 2)) / 0.02)')
+u0 = Expression('x[0] + 0.25*sin(2*pi*x[1])')
+g = Expression('(x[1] - 0.5)*(x[1] - 0.5)')
+
+# Mark facets of the mesh
+boundaries = FacetFunction('size_t', mesh, 0)
+NeumanBoundary().mark(boundaries, 2)
+DirichletBoundary().mark(boundaries, 1)
+
+# Define outer surface measure aware of Dirichlet and Neumann boundaries
+ds = Measure('ds')[boundaries]
 
 # Define parameters
 alpha = 4.0
@@ -61,15 +88,14 @@ a = dot(grad(v), grad(u))*dx \
    - dot(avg(grad(v)), jump(u, n))*dS \
    - dot(jump(v, n), avg(grad(u)))*dS \
    + alpha/h_avg*dot(jump(v, n), jump(u, n))*dS \
-   - dot(grad(v), u*n)*ds \
-   - dot(v*n, grad(u))*ds \
-   + gamma/h*v*u*ds
-L = v*f*dx
+   - dot(grad(v), u*n)*ds(1) \
+   - dot(v*n, grad(u))*ds(1) \
+   + gamma/h*v*u*ds(1)
+L = v*f*dx - u0*dot(grad(v), n)*ds(1) + gamma/h*u0*v*ds(1) + g*v*ds(2)
 
 # Compute solution
 u = Function(V)
 solve(a == L, u)
-print "norm:", u.vector().norm("l2")
 
 # Project solution to piecewise linears
 u_proj = project(u)
