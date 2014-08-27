53023ae6e70bec6fbd58c22fad3025d94384fbc9
Aslak Bergersen <aslak.bergersen@gmail.com>
2014-08-27 19:17:30 +0200

    Add pytest for cpp. Should be reviewed.

---
+++ test/unit/parameter/cpp/test_cpp_parameter.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env py.test
+
+# Copyright (C) Aslak Wigdahl Bergersen
+#
+# This file is part of DOLFIN.
+#
+# DOLFIN is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# DOLFIN is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with DOLFIN. If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import pytest
+import platform
+from instant import get_status_output
+
+def pytest_generate_tests(metafunc):
+    # Set non-interactive
+    os.putenv('DOLFIN_NOPLOT', '1')
+
+    curdir = os.getcwd()
+    filepath = os.path.dirname(os.path.abspath(__file__))
+    os.chdir(filepath)
+    out_cmake = os.system('cmake .')
+    out_make = os.system('make')
+
+    #Find all tests
+    list_of_tests = []
+    for file in os.listdir('.'):
+        if file.startswith("test_") and not '.' in file:
+            if platform.system() == 'Windows':
+                file += '.exe'
+            list_of_tests.append(file)
+
+    # Create tests
+    os.chdir(curdir)
+    funcargs = {}
+    funcargs['curdir'] = curdir
+    funcargs['filepath'] = filepath
+    for test in list_of_tests:
+        funcargs['test'] = test
+        metafunc.addcall(funcargs=funcargs)
+
+@pytest.mark.cpp
+def test_cpp_mesh(test, curdir, filepath):
+    os.chdir(filepath)
+    status, output = get_status_output(os.path.join('.', test))
+    assert 'OK' in output
+    assert status == 0
+    os.chdir(curdir)
