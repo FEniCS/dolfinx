b26fcbbe07aa5f4f2e90e9a8e25be96e741b3121
Matthias Liertzer <matthias@liertzer.at>
2014-10-08 14:01:28 +0200

    Fix DirichletBC::apply in parallel when use_ident=false

--- dolfin/fem/DirichletBC.cpp
+++ dolfin/fem/DirichletBC.cpp
@@ -614,10 +614,12 @@ void DirichletBC::apply(GenericMatrix* A,
       A->ident_local(size, dofs.data());
     else
     {
-      A->zero(size, dofs.data());
+      A->zero_local(size, dofs.data());
+
+      const std::size_t offset = _function_space->dofmap()->ownership_range().first;
       for (std::size_t i = 0; i < size; i++)
       {
-        std::pair<std::size_t, std::size_t> ij(dofs[i], dofs[i]);
+        std::pair<std::size_t, std::size_t> ij(offset + dofs[i], offset + dofs[i]);
         A->setitem(ij, 1.0);
       }
     }
