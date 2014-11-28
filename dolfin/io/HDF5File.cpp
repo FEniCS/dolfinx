c1c6f319b5f7b0028759f08dbc4a77d4346414ab
Simone Pezzuto <simonep@simula.no>
2014-11-28 23:39:15 +0100

    Optimisation for read_mesh_value_collection.

--- dolfin/io/HDF5File.cpp
+++ dolfin/io/HDF5File.cpp
@@ -1139,6 +1139,7 @@ template <typename T>
 void HDF5File::read_mesh_value_collection(MeshValueCollection<T>& mesh_vc,
                                           const std::string name) const
 {
+  Timer t1("HDF5: read mesh value collection '"+name+"'");
   dolfin_assert(hdf5_file_open);
   mesh_vc.clear();
   if (!HDF5Interface::has_group(hdf5_file_id, name))
@@ -1217,20 +1218,29 @@ void HDF5File::read_mesh_value_collection(MeshValueCollection<T>& mesh_vc,
     std::map<std::pair<std::size_t, std::size_t>, T>& mvc_map
       = mesh_vc.values();
 
-    // Find cells which are on this process
-    for (std::size_t i = 0; i < cells_data.size(); ++i)
+    // Find cells which are on this process,
+    // under the assumption that both array are ordered,
+    dolfin_assert(std::is_sorted(cells_data.begin(), cells_data.end()));
+    dolfin_assert(std::is_sorted(global_cell_index.begin(),
+                                 global_cell_index.end()));
+
+    // The implementation follows std::set_intersection, which
+    // we are not able to use it here since we need the indices
+    // of the intersection, not just the values.
+    std::vector<std::size_t>::const_iterator i = global_cell_index.begin();
+    std::vector<std::size_t>::const_iterator j = cells_data.begin();
+    while (i!=global_cell_index.end() && j!=cells_data.end())
     {
-      const std::vector<std::size_t>::const_iterator lidx
-        = std::find(global_cell_index.begin(), global_cell_index.end(),
-                    cells_data[i]);
-      if (lidx != global_cell_index.end())
+      if (*i < *j) ++i;
+      else if (*i > *j) ++j;
+      else
       {
-        const std::size_t local_index = lidx - global_cell_index.begin();
-        mvc_map[std::make_pair(local_index, entities_data[i])]
-          = values_data[i];
+        std::size_t ii = i - global_cell_index.begin();
+        std::size_t jj = j - cells_data.begin();
+        mvc_map[std::make_pair(ii, entities_data[jj])] = values_data[jj];
+        ++i; ++j;
       }
     }
-
   }
   else
   {
@@ -1444,8 +1454,6 @@ void HDF5File::read(Mesh& input_mesh, const std::string mesh_name,
     if (!has_dataset(marker_dataset))
       continue;
 
-    input_mesh.init(d);
-
     MeshValueCollection<std::size_t> mvc(input_mesh, d);
     read_mesh_value_collection(mvc, marker_dataset);
 
@@ -1459,6 +1467,7 @@ void HDF5File::read(Mesh& input_mesh, const std::string mesh_name,
              std::size_t>::const_iterator entry;
     if (d != input_mesh.topology().dim())
     {
+      input_mesh.init(d);
       for (entry = values.begin(); entry != values.end(); ++entry)
       {
         const Cell cell(input_mesh, entry->first.first);
