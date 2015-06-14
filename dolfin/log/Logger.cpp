6bd034847cf87273404602d79c1f88dc4c7ba7d2
Jan Blechta <blechta@karlin.mff.cuni.cz>
2015-06-14 14:03:53 +0200

    Attempt to fix docstrings of timing routines.

--- dolfin/log/Logger.cpp
+++ dolfin/log/Logger.cpp
@@ -294,7 +294,8 @@ void Logger::register_timing(std::string task,
 void Logger::list_timings(bool reset)
 {
   deprecation("dolfin::list_timings(bool)", "1.6.0", "1.7.0",
-              "The method is replaced by another Logger::list_timings(...).");
+              "The method is replaced by another Logger::list_timings(...), "
+              "e.g, 'list_timings(TimingClear_keep, [TimingType_wall])'");
   list_timings(static_cast<TimingClear>(reset),
                std::set<TimingType>({ TimingType::wall }));
 }
