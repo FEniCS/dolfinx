fa77598bcd7605cb2752788506d1a34463b57fde
Benjamin Kehlet <benjamik@simula.no>
2015-07-31 12:49:15 +0200

    Hardcode failing case (avoids rand()).

--- demo/undocumented/multimesh-cgal/cpp/main.cpp
+++ demo/undocumented/multimesh-cgal/cpp/main.cpp
@@ -282,58 +282,85 @@ void test_multiple_meshes_with_rotation()
 {
   set_log_level(DBG);
 
-  dolfin::seed(0);
-
   const double h = 0.5;
-  UnitSquareMesh background_mesh((int)std::round(1./h),
-                                 (int)std::round(1./h));
 
   MultiMesh multimesh;
-  multimesh.add(background_mesh);
-
-  const std::size_t Nmeshes = 8;
-
-  /* ---------------- Create multimesh ------------------------- */
-  std::size_t i = 0;
-  while (i < Nmeshes)
   {
-    double x0 = dolfin::rand();
-    double x1 = dolfin::rand();
-    if (x0 > x1) std::swap(x0, x1);
-    double y0 = dolfin::rand();
-    double y1 = dolfin::rand();
-    if (y0 > y1) std::swap(y0, y1);
-    const double v = dolfin::rand()*90; // initial rotation
-    const double speed = dolfin::rand()-0.5; // initial speed
-       
-    const double cx = (x0+x1) / 2;
-    const double cy = (y0+y1) / 2;
-    double xr, yr;
-    rotate(x0, y0, cx, cy, v, xr, yr);
-    if (xr > 0 and xr < 1 and yr > 0 and yr < 1)
-    {
-      rotate(x0, y1, cx, cy, v, xr, yr);
-      if (xr > 0 and xr < 1 and yr > 0 and yr < 1)
-      {
-        rotate(x1, y0, cx, cy, v, xr, yr);
-        if (xr > 0 and xr < 1 and yr > 0 and yr < 1)
-        {
-          rotate(x1, y1, cx, cy, v, xr, yr);
-          if (xr > 0 and xr < 1 and yr > 0 and yr < 1)
-          {
-            std::shared_ptr<Mesh> mesh(new RectangleMesh(x0, y0, x1, y1,
-                                                         std::max((int)std::round((x1-x0)/h), 1),
-                                                         std::max((int)std::round((y1-y0)/h), 1)));
-            mesh->rotate(v);
+    std::shared_ptr<Mesh> background_mesh(new UnitSquareMesh((int)std::round(1./h),
+                                                             (int)std::round(1./h)));
+    multimesh.add(background_mesh);
+  }
 
-            // The error happends in mesh from mesh 2 and up
-            if (i > 1)
-              multimesh.add(mesh);
-            i++;
-          }
-        }
-      }
-    }
+  {
+    const double x0 = 0.40022862209017789903;
+    const double y0 = 0.28331474600514150453;
+    const double x1 = 0.89152945200518218805;
+    const double y1 = 0.35245834726489072564;
+    std::shared_ptr<Mesh> mesh(new RectangleMesh(x0, y0, x1, y1,
+                                                 std::max((int)std::round((x1-x0)/h), 1),
+                                                 std::max((int)std::round((y1-y0)/h), 1)));
+    mesh->rotate(72.695206800799439861);
+    multimesh.add(mesh);
+  }
+
+  {
+    const double x0 = 0.37520697637237931943;
+    const double y0 = 0.51253536414007438982;
+    const double x1 = 0.76024873636674539235;
+    const double y1 = 0.66772376078540629507;
+    std::shared_ptr<Mesh> mesh(new RectangleMesh(x0, y0, x1, y1,
+                                                 std::max((int)std::round((x1-x0)/h), 1),
+                                                 std::max((int)std::round((y1-y0)/h), 1)));
+    mesh->rotate(47.844579074459417711);
+    multimesh.add(mesh);
+  }
+
+  {
+    const double x0 = 0.35404867974764142602;
+    const double y0 = 0.16597416632155614913;
+    const double x1 = 0.63997881656511634851;
+    const double y1 = 0.68786139026650294781;
+    std::shared_ptr<Mesh> mesh(new RectangleMesh(x0, y0, x1, y1,
+                                                 std::max((int)std::round((x1-x0)/h), 1),
+                                                 std::max((int)std::round((y1-y0)/h), 1)));
+    mesh->rotate(39.609407484349517858);
+    multimesh.add(mesh);
+  }
+
+  {
+    const double x0 = 0.33033712968711609337;
+    const double y0 = 0.22896817104377231722;
+    const double x1 = 0.82920109332967595339;
+    const double y1 = 0.89337241458397931293;
+    std::shared_ptr<Mesh> mesh(new RectangleMesh(x0, y0, x1, y1,
+                                                 std::max((int)std::round((x1-x0)/h), 1),
+                                                 std::max((int)std::round((y1-y0)/h), 1)));
+    mesh->rotate(31.532416069662392744);
+    multimesh.add(mesh);
+  }
+
+  {
+    const double x0 = 0.58864013319306085492;
+    const double y0 = 0.65730403953106331105;
+    const double x1 = 0.95646825291051917883;
+    const double y1 = 0.85867632592966613991;
+    std::shared_ptr<Mesh> mesh(new RectangleMesh(x0, y0, x1, y1,
+                                                 std::max((int)std::round((x1-x0)/h), 1),
+                                                 std::max((int)std::round((y1-y0)/h), 1)));
+    mesh->rotate(39.560392754879032395);
+    multimesh.add(mesh);
+  }
+
+  {
+    const double x0 = 0.28105941241656401397;
+    const double y0 = 0.30745787374091237965;
+    const double x1 = 0.61959648394007071914;
+    const double y1 = 0.78600209801737319637;
+    std::shared_ptr<Mesh> mesh(new RectangleMesh(x0, y0, x1, y1,
+                                                 std::max((int)std::round((x1-x0)/h), 1),
+                                                 std::max((int)std::round((y1-y0)/h), 1)));
+    mesh->rotate(40.233022128340330426);
+    multimesh.add(mesh);
   }
 
   multimesh.build();
