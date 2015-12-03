FEniCS/dolfinx

commit 2cded0c82c4a640c8b68f2988a3c3b3edeb417a1

Don't waste memory for off-diagonal pattern in sequential
Not tested yet how it interacts with backends.

dolfin/la/SparsityPattern.cpp
@@ -73,15 +73,28 @@ void SparsityPattern::init(
                  "Primary dimension must be less than 2 (0=row major, 1=column major");
   }
 
-  const std::size_t local_size
+
+  const std::size_t local_size0
     = index_maps[_primary_dim]->size(IndexMap::MapSize::OWNED);
 
+  const std::size_t primary_codim = _primary_dim == 0 ? 1 : 0;
+  const std::size_t nonlocal_size1
+    = index_maps[primary_codim]->size(IndexMap::MapSize::UNOWNED);
+
   // Resize diagonal block
-  diagonal.resize(local_size);
+  diagonal.resize(local_size0);
 
   // Resize off-diagonal block (only needed when local range != global
   // range)
-  off_diagonal.resize(local_size);
+  if (nonlocal_size1 > 0)
+  {
+    dolfin_assert(MPI::size(_mpi_comm) > 1);
+    off_diagonal.resize(local_size0);
+  }
+  else
+  {
+    dolfin_assert(MPI::size(_mpi_comm) == 1);
+  }
 }
 //-----------------------------------------------------------------------------
 void SparsityPattern::insert_global(dolfin::la_index i, dolfin::la_index j)

dolfin/la/SparsityPattern.h
@@ -91,7 +91,9 @@ namespace dolfin
 
     /// Fill array with number of nonzeros for off-diagonal block in
     /// local_range for dimension 0. For matrices, fill array with
-    /// number of nonzeros per local row for off-diagonal block
+    /// number of nonzeros per local row for off-diagonal block. If
+    /// there is no off-diagonal pattern, the vector is resized to
+    /// zero-length
     void
       num_nonzeros_off_diagonal(std::vector<std::size_t>& num_nonzeros) const;
 

commit 07e5e6fc3155015c35f432ac21a0a3fe7f697530

Fix bug in off diagonal pattern allocation
Introduced in 2cded0c82c4a640c8b68f2988a3c3b3edeb417a1.

dolfin/la/SparsityPattern.cpp
@@ -73,26 +73,28 @@ void SparsityPattern::init(
                  "Primary dimension must be less than 2 (0=row major, 1=column major");
   }
 
-
   const std::size_t local_size0
     = index_maps[_primary_dim]->size(IndexMap::MapSize::OWNED);
 
   const std::size_t primary_codim = _primary_dim == 0 ? 1 : 0;
-  const std::size_t nonlocal_size1
-    = index_maps[primary_codim]->size(IndexMap::MapSize::UNOWNED);
+  const std::size_t local_size1
+    = index_maps[primary_codim]->size(IndexMap::MapSize::OWNED);
+  const std::size_t global_size1
+    = index_maps[primary_codim]->size(IndexMap::MapSize::GLOBAL);
 
   // Resize diagonal block
   diagonal.resize(local_size0);
 
   // Resize off-diagonal block (only needed when local range != global
   // range)
-  if (nonlocal_size1 > 0)
+  if (global_size1 > local_size1)
   {
     dolfin_assert(MPI::size(_mpi_comm) > 1);
     off_diagonal.resize(local_size0);
   }
   else
   {
+    dolfin_assert(global_size1 == local_size1);
     dolfin_assert(MPI::size(_mpi_comm) == 1);
   }
 }