57fda3e382ddbf4d59847eabf9e6702f7336a1d4
Garth N. Wells <gnw20@cam.ac.uk>
2015-12-15 12:48:56 +0000

    PEP8 fixes for mesh tests.

--- test/unit/python/mesh/test_periodic_boundary_computation.py
+++ test/unit/python/mesh/test_periodic_boundary_computation.py
@@ -28,20 +28,24 @@ from dolfin import *
 
 from dolfin_utils.test import *
 
+
 @fixture
 def periodic_boundary():
     class PeriodicBoundary(SubDomain):
         def inside(self, x, on_boundary):
             return x[0] < DOLFIN_EPS
+
         def map(self, x, y):
             y[0] = x[0] - 1.0
             y[1] = x[1]
 
     return PeriodicBoundary()
 
+
 @fixture
 def pbc():
     return PeriodicBoundaryComputation()
+
 
 @fixture
 def mesh():
@@ -53,7 +57,7 @@ def test_ComputePeriodicPairs(pbc, periodic_boundary, mesh):
 
     # Verify that correct number of periodic pairs are computed
     vertices = pbc.compute_periodic_pairs(mesh, periodic_boundary, 0)
-    edges    = pbc.compute_periodic_pairs(mesh, periodic_boundary, 1)
+    edges = pbc.compute_periodic_pairs(mesh, periodic_boundary, 1)
     assert len(vertices) == 5
     assert len(edges) == 4
 
