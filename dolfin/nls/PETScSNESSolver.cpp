9367b120718747fe2456ab8e6d1fe615488bbbda
Garth N. Wells <gnw20@cam.ac.uk>
2016-04-15 18:59:53 +0100

    Start cleaning up SNES wrappers.

--- dolfin/nls/PETScSNESSolver.cpp
+++ dolfin/nls/PETScSNESSolver.cpp
@@ -82,7 +82,6 @@ Parameters PETScSNESSolver::default_parameters()
   p.rename("snes_solver");
   p.add("solution_tolerance", 1.0e-16);
   p.add("maximum_residual_evaluations", 2000);
-  p.add("options_prefix", "default");
   p.remove("convergence_criterion");
   p.remove("relaxation_parameter");
   p.remove("method");
@@ -93,12 +92,33 @@ Parameters PETScSNESSolver::default_parameters()
   return p;
 }
 //-----------------------------------------------------------------------------
-PETScSNESSolver::PETScSNESSolver(std::string nls_type) :
-  _snes(NULL)
+PETScSNESSolver::PETScSNESSolver(MPI_Comm comm) : _snes(nullptr),
+                                                  _has_explicit_bounds(false)
 {
+  // Create SNES object
+  SNESCreate(comm, &_snes);
+
+  // Set parameter values
+  parameters = default_parameters();
+}
+//-----------------------------------------------------------------------------
+PETScSNESSolver::PETScSNESSolver(std::string nls_type) : _snes(nullptr),
+                                                         _has_explicit_bounds(false)
+{
+  // Create SNES object
+  SNESCreate(MPI_COMM_WORLD, &_snes);
+
+  const std::map<std::string, std::pair<std::string, const SNESType>>::const_iterator
+    method = _methods.find(nls_type);
 
   // Check that the requested method is known
-  if (_methods.count(nls_type) == 0)
+  if (method != _methods.end())
+  {
+    // Set solver type
+    if (method->first != "default")
+      SNESSetType(_snes, method->second.second);
+  }
+  else
   {
     dolfin_error("PETScSNESSolver.cpp",
                  "create PETSc SNES solver",
@@ -107,34 +127,12 @@ PETScSNESSolver::PETScSNESSolver(std::string nls_type) :
 
   // Set parameter values
   parameters = default_parameters();
-
-  init(nls_type);
 }
 //-----------------------------------------------------------------------------
 PETScSNESSolver::~PETScSNESSolver()
 {
   if (_snes)
     SNESDestroy(&_snes);
-}
-//-----------------------------------------------------------------------------
-void PETScSNESSolver::init(const std::string& method)
-{
-  if (_snes)
-    SNESDestroy(&_snes);
-
-  // Create SNES object
-  SNESCreate(PETSC_COMM_WORLD, &_snes);
-
-  // Set solver type
-  if (method != "default")
-  {
-    auto it = _methods.find(method);
-    dolfin_assert(it != _methods.end());
-    SNESSetType(_snes, it->second.second);
-  }
-
-  // Set to default to not having explicit bounds
-  has_explicit_bounds = false;
 }
 //-----------------------------------------------------------------------------
 std::pair<std::size_t, bool>
@@ -167,14 +165,13 @@ PETScSNESSolver::solve(NonlinearProblem& nonlinear_problem,
     _lb(&lb.down_cast<PETScVector>(), NoDeleter());
   this->lb = _lb;
   this->ub = _ub;
-  has_explicit_bounds = true;
+  _has_explicit_bounds = true;
 
   return this->solve(nonlinear_problem, x);
 }
 //-----------------------------------------------------------------------------
-void
-PETScSNESSolver::init(NonlinearProblem& nonlinear_problem,
-                       GenericVector& x)
+void PETScSNESSolver::init(NonlinearProblem& nonlinear_problem,
+                           GenericVector& x)
 {
   Timer timer("SNES solver init");
   PETScMatrix A;
@@ -197,18 +194,6 @@ PETScSNESSolver::init(NonlinearProblem& nonlinear_problem,
   SNESSetJacobian(_snes, A.mat(), A.mat(), PETScSNESSolver::FormJacobian,
                   &_snes_ctx);
   SNESSetObjective(_snes, PETScSNESSolver::FormObjective, &_snes_ctx);
-
-  std::string prefix = std::string(parameters["options_prefix"]);
-  if (prefix != "default")
-  {
-    // Make sure that the prefix has a '_' at the end if the user
-    // didn't provide it
-    char lastchar = *prefix.rbegin();
-    if (lastchar != '_')
-      prefix += "_";
-
-    SNESSetOptionsPrefix(_snes, prefix.c_str());
-  }
 
   // Set some options from the parameters
   if (parameters["report"].is_set())
@@ -297,6 +282,31 @@ PETScSNESSolver::init(NonlinearProblem& nonlinear_problem,
   SNESSetFromOptions(_snes);
 }
 //-----------------------------------------------------------------------------
+void PETScSNESSolver::set_options_prefix(std::string options_prefix)
+{
+  // Set options prefix
+  dolfin_assert(_snes);
+  PetscErrorCode ierr = SNESSetOptionsPrefix(_snes, options_prefix.c_str());
+  if (ierr != 0) petsc_error(ierr, __FILE__, "SNESSetOptionsPrefix");
+}
+//-----------------------------------------------------------------------------
+std::string PETScSNESSolver::get_options_prefix() const
+{
+  dolfin_assert(_snes);
+  const char* prefix = NULL;
+  PetscErrorCode ierr = SNESGetOptionsPrefix(_snes, &prefix);
+  if (ierr != 0) petsc_error(ierr, __FILE__, "SNESGetOptionsPrefix");
+  return std::string(prefix);
+}
+//-----------------------------------------------------------------------------
+MPI_Comm PETScSNESSolver::mpi_comm() const
+{
+  dolfin_assert(_snes);
+  MPI_Comm mpi_comm = MPI_COMM_NULL;
+  PetscObjectGetComm((PetscObject)_snes, &mpi_comm);
+  return mpi_comm;
+}
+//-----------------------------------------------------------------------------
 std::pair<std::size_t, bool>
 PETScSNESSolver::solve(NonlinearProblem& nonlinear_problem,
                        GenericVector& x)
@@ -326,8 +336,7 @@ PETScSNESSolver::solve(NonlinearProblem& nonlinear_problem,
 
   const bool report = parameters["report"];
 
-  MPI_Comm comm = MPI_COMM_NULL;
-  PetscObjectGetComm((PetscObject)_snes, &comm);
+  MPI_Comm comm = mpi_comm();
   if (reason > 0 && report && dolfin::MPI::rank(comm) == 0)
   {
     info("PETSc SNES solver converged in %d iterations with convergence reason %s.",
@@ -433,20 +442,17 @@ void PETScSNESSolver::set_linear_solver_parameters()
   ierr = KSPGetPC(ksp, &pc);
   if (ierr != 0) petsc_error(ierr, __FILE__, "KSPGetPC");
 
-  MPI_Comm comm = MPI_COMM_NULL;
-  PetscObjectGetComm((PetscObject)_snes, &comm);
+  // Get MPI communicator
+  MPI_Comm comm = mpi_comm();
 
   if (parameters["report"].is_set())
   {
     if (parameters["report"])
     {
       #if PETSC_VERSION_MAJOR == 3 && PETSC_VERSION_MINOR <= 6 && PETSC_VERSION_RELEASE == 1
-      KSPMonitorSet(ksp, KSPMonitorDefault,
-                    PETSC_VIEWER_STDOUT_(PetscObjectComm((PetscObject)ksp)),
-                    NULL);
-
+      KSPMonitorSet(ksp, KSPMonitorDefault, PETSC_VIEWER_STDOUT_(comm), NULL);
       #else
-      PetscViewer viewer = PETSC_VIEWER_STDOUT_(PetscObjectComm((PetscObject)ksp));
+      PetscViewer viewer = PETSC_VIEWER_STDOUT_(comm);
       PetscViewerFormat format = PETSC_VIEWER_DEFAULT;
       PetscViewerAndFormat *vf;
       ierr = PetscViewerAndFormatCreate(viewer,format,&vf);
@@ -490,8 +496,8 @@ void PETScSNESSolver::set_linear_solver_parameters()
     {
       #if PETSC_VERSION_MAJOR == 3 && PETSC_VERSION_MINOR <= 6 && PETSC_VERSION_RELEASE == 1
       KSPMonitorSet(ksp, KSPMonitorTrueResidualNorm,
-                       PETSC_VIEWER_STDOUT_(PetscObjectComm((PetscObject)ksp)),
-                       NULL);
+                    PETSC_VIEWER_STDOUT_(PetscObjectComm((PetscObject)ksp)),
+                    NULL);
 
       #else
       PetscViewer viewer = PETSC_VIEWER_STDOUT_(PetscObjectComm((PetscObject)ksp));
@@ -613,7 +619,7 @@ void PETScSNESSolver::set_bounds(GenericVector& x)
       VecDestroy(&ub);
       VecDestroy(&lb);
     }
-    else if (has_explicit_bounds == true)
+    else if (_has_explicit_bounds)
     {
       const PETScVector* lb = this->lb.get();
       const PETScVector* ub = this->ub.get();
@@ -625,14 +631,14 @@ void PETScSNESSolver::set_bounds(GenericVector& x)
 bool PETScSNESSolver::is_vi() const
 {
   const std::string sign = parameters["sign"];
-  if (sign != "default" && this->has_explicit_bounds == true)
+  if (sign != "default" && this->_has_explicit_bounds)
   {
     dolfin_error("PETScSNESSolver.cpp",
                  "set variational inequality bounds",
                  "Both the sign parameter and the explicit bounds are set");
     return false;
   }
-  else if (sign != "default" || this->has_explicit_bounds == true)
+  else if (sign != "default" || this->_has_explicit_bounds)
     return true;
   else
     return false;
