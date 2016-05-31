62b39c84bc2fa8a202e82330668d9f92630d62c1
August Johansson <august@simula.no>
2016-05-31 23:52:43 +0200

    another test -- that worked

--- demo/undocumented/multimesh-volume/cpp/main.cpp
+++ demo/undocumented/multimesh-volume/cpp/main.cpp
@@ -30,10 +30,10 @@ int main(int argc, char* argv[])
   // exact_area += 4*L2 - 0.5*L1 - 0.75*L1;
 
 
-  double exact_area = 0;
-  auto mesh_1 = std::make_shared<RectangleMesh>(Point(0.1, 0.1), Point(0.9, 0.9), N, N);
-  mesh_1->translate(Point(-0.05, 0.05));
-  exact_area += 4*0.8;
+  // double exact_area = 0;
+  // auto mesh_1 = std::make_shared<RectangleMesh>(Point(0.1, 0.1), Point(0.9, 0.9), N, N);
+  // mesh_1->translate(Point(-0.05, 0.05));
+  // exact_area += 4*0.8;
   // auto mesh_2 = std::make_shared<RectangleMesh>(Point(0.2, 0.2), Point(0.8, 0.8), N, N);
   // mesh_2->translate(Point(-0.025, 0.025));
   // exact_area += 4*0.6;
@@ -43,6 +43,15 @@ int main(int argc, char* argv[])
   // auto mesh_4 = std::make_shared<RectangleMesh>(Point(0.4, 0.4), Point(0.6, 0.6), N, N);
   // mesh_4->translate(Point(-0.0125/2, 0.0125/2));
   // exact_area += 4*0.2;
+
+
+  const std::size_t Nx=1, Ny=1;
+  std::vector<std::shared_ptr<const Mesh>> meshes(11);
+  //auto mesh_0 = meshes[0] = std::make_shared<UnitSquareMesh>(Nx, Ny);
+  auto mesh_1 = meshes[1] = std::make_shared<RectangleMesh>(Point(0.489594, 0.353142), Point(0.503781, 0.877049), 1, 1);
+  const double exact_area = std::abs(0.489594-0.503781)*2 + std::abs(0.353142- 0.877049)*2;
+
+
 
   // tools::dolfin_write_medit_triangles("mesh0",*mesh_0);
   // tools::dolfin_write_medit_triangles("mesh1",*mesh_1);
@@ -60,12 +69,13 @@ int main(int argc, char* argv[])
   multimesh->build(1);
   tools::dolfin_write_medit_triangles("multimesh",*multimesh);
 
-  const double volume = tools::compute_volume(*multimesh);
-  std::cout << "volume " << volume << ' ' << exact_volume <<" error="<< std::abs(volume-exact_volume) << std::endl;
+//   const double volume = tools::compute_volume(*multimesh);
+//   std::cout << "volume " << volume << ' ' << exact_volume <<" error="<< std::abs(volume-exact_volume) << std::endl;
+
+//   const double volume_overlap = tools::compute_volume_overlap(*multimesh);
+//   std::cout << "volume_overlap = " << volume_overlap << std::endl;
 
   const double area = tools::compute_interface_area(*multimesh);
   std::cout << "area " << area << ' ' << exact_area << " error="<<std::abs(area-exact_area) << std::endl;
 
-  const double volume_overlap = tools::compute_volume_overlap(*multimesh);
-  std::cout << "volume_overlap = " << volume_overlap << std::endl;
 }
