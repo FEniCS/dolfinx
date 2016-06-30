46d629d79705974cb857004b5a106f0135ef6079
Chris Richardson <chris@bpi.cam.ac.uk>
2016-06-30 20:32:23 +0100

    Update sphinx to use napoleon

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -34,6 +34,8 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
     'breathe' ]
 
 # Add any paths that contain templates here, relative to this directory.
