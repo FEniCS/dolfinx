477d2df01518b392824b134066b78ea73b392d76
August Johansson <august@simula.no>
2016-07-04 16:24:34 +0200

    Computing the volume in test_volume using DG(0) instead of Constant(1.0)*dX(multimesh), since I'm unsure of the syntax.

--- test/unit/python/multimesh/test_volume.py
+++ test/unit/python/multimesh/test_volume.py
@@ -39,8 +39,8 @@ def test_volume_2d():
 
     # Create background mesh so we can easily compute volume...
     mesh_0 = UnitSquareMesh(1, 1)
-    mesh_0.translate(Point(-0.5, -0.5))
-    mesh_0.scale(5.0)
+    mesh_0.scale(10.0)
+    mesh_0.translate(Point(-5, -5))
     exact_volume = 100.0
 
     # Create meshes with centres distributed around the unit circle.
@@ -67,11 +67,38 @@ def test_volume_2d():
         multimesh.add(mesh)
     multimesh.build()
 
-    # Create multimesh function space
-    #V = MultiMeshFunctionSpace(multimesh, 'P', 1)
-    #M = Constant(1)*dX(multimesh)
+    # Create function space
+    V = MultiMeshFunctionSpace(multimesh, "DG", 0)
 
-    assert 1 == 1
+    # Create and evaluate volume functional
+    v = TestFunction(V)
+    M = v*dX
+    approximative_volume = sum(assemble(M).array())
+
+    print("approximative volume ", approximative_volume)
+    print("exact volume ", exact_volume)
+
+    assert approximative_volume == exact_volume
+
+# @skip_in_parallel
+# def test_volume_7_meshes():
+#     "Integrate volume of 7 2D meshes"
+
+#     # Background mesh
+#     mesh_0 = UnitSquareMesh(1, 1)
+
+#     # List of points for generating the 6 meshes on top
+#     points = [ 0.747427, 0.186781, 0.849659, 0.417130,
+#                0.152716, 0.471681, 0.455943, 0.741585,
+#                0.464473, 0.251876, 0.585051, 0.533569,
+#                0.230112, 0.511897, 0.646974, 0.892193,
+#                0.080362, 0.422675, 0.580151, 0.454286,
+#                0.054755, 0.534186, 0.444096, 0.743028 ]
+
+#     # std::vector<double> angles =
+#     # 88.339755, 94.547259, 144.366564, 172.579922, 95.439692, 106.697958
+
+#     assert 1 == 1
 
 # FIXME: Temporary testing
 if __name__ == "__main__":
