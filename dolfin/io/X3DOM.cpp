9c095df57e5b9d8c639a7e5667c58e1e099a5145
Peter Scott <peter_scott@brown.edu>
2016-09-01 23:26:20 -0400

    style changes to comply to dolfin style practices

--- dolfin/io/X3DOM.cpp
+++ dolfin/io/X3DOM.cpp
@@ -178,11 +178,13 @@ bool X3DOMParameters::get_x3d_stats() const
   return _show_x3d_stats;
 }
 //-----------------------------------------------------------------------------
-void X3DOMParameters::set_menu_display(bool show) {
+void X3DOMParameters::set_menu_display(bool show)
+{
   _menu_display = show;
 }
 //-----------------------------------------------------------------------------
-bool X3DOMParameters::get_menu_display() const {
+bool X3DOMParameters::get_menu_display() const
+{
   return _menu_display;
 }
 //-----------------------------------------------------------------------------
