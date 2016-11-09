01b5ce20dc2c3d46a49e83ed8bcf1c9df682563e
August Johansson <august@simula.no>
2016-11-09 13:04:24 +0100

    Move testing of geometry issues to the correct file. [skip ci]

--- test/unit/python/geometry/test_geometry_issues.py
+++ test/unit/python/geometry/test_geometry_issues.py
@@ -20,13 +20,14 @@
 # along with DOLFIN. If not, see <http://www.gnu.org/licenses/>.
 #
 # First added:  2013-12-09
-# Last changed: 2014-05-30
+# Last changed: 2016-11-09
 
 from __future__ import print_function
 import pytest
 
 from dolfin import *
 from dolfin_utils.test import skip_in_parallel
+import numpy as np
 
 @skip_in_parallel
 def test_issue_97():
@@ -50,14 +51,65 @@ def test_issue_168():
     x = (0.75, 0.25, 0.125)
     vx = v(x)
 
+
+@pytest.mark.skipif(True, reason="Not implemented in 3D")
+def test_segment_collides_point_3D_2():
+    """Test case by Oyvind from https://bitbucket.org/fenics-project/dolfin/issue/296 for segment point collision in 3D"""
+    mesh = Mesh()
+    editor = MeshEditor()
+    editor.open(mesh, 1, 3)
+    editor.init_vertices(2)
+    editor.init_cells(1)
+    editor.add_vertex(0, 41.06309891, 63.74219894, 68.10320282)
+    editor.add_vertex(1, 41.45830154, 62.61560059, 66.43019867)
+    editor.add_cell(0,0,1)
+    editor.close()
+    cell = Cell(mesh, 0)
+    assert cell.contains(cell.midpoint())
+
+
+def _test_collision_robustness_2d(aspect, y, step):
+    nx = 10
+    ny = int(aspect*nx)
+    mesh = UnitSquareMesh(nx, ny, 'crossed')
+    bb = mesh.bounding_box_tree()
+
+    x = 0.0
+    p = Point(x, y)
+    while x <= 1.0:
+        c = bb.compute_first_entity_collision(Point(x, y))
+        assert c < np.uintc(-1)
+        x += step
+
+@pytest.mark.skipif(True, reason="Not implemented in 3D")
+def _test_collision_robustness_3d(aspect, y, z, step):
+    nx = nz = 10
+    ny = int(aspect*nx)
+    mesh = UnitCubeMesh(nx, ny, nz)
+    bb = mesh.bounding_box_tree()
+
+    x = 0.0
+    while x <= 1.0:
+        c = bb.compute_first_entity_collision(Point(x, y, z))
+        assert c < np.uintc(-1)
+        x += step
+
 @skip_in_parallel
-def test_triangle_triangle_collision() :
-    "Test that has been failing"
-    assert CollisionPredicates.collides_triangle_triangle_2d(Point(0.177432070718943, 0.5),
-                                                             Point(0.176638957524249, 0.509972290857582),
-                                                             Point(0.217189283468892, 0.550522616802225),
-                                                             Point(0.333333333333333, 0.52399308981973),
-                                                             Point(0.333333333333333, 0.666666666666667),
-                                                             Point(0.211774439087554, 0.545107772420888))
+@pytest.mark.slow
+def test_collision_robustness_slow():
+    """Test cases from https://bitbucket.org/fenics-project/dolfin/issue/296"""
+    _test_collision_robustness_2d( 100, 1e-14,       1e-5)
+    _test_collision_robustness_2d(  40, 1e-03,       1e-5)
+    _test_collision_robustness_2d( 100, 0.5 + 1e-14, 1e-5)
+    _test_collision_robustness_2d(4.43, 0.5,      4.03e-6)
+    #_test_collision_robustness_3d( 100, 1e-14, 1e-14, 1e-5)
 
-
+@skip_in_parallel
+@pytest.mark.slow
+@pytest.mark.skipif(True, reason='Very slow test cases')
+def test_collision_robustness_very_slow():
+    """Test cases from https://bitbucket.org/fenics-project/dolfin/issue/296"""
+    _test_collision_robustness_2d(  10, 1e-16,       1e-7)
+    _test_collision_robustness_2d(4.43, 1e-17,    4.03e-6)
+    _test_collision_robustness_2d(  40, 0.5,         1e-6)
+    _test_collision_robustness_2d(  10, 0.5 + 1e-16, 1e-7)
