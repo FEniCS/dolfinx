85948c7c2b8d19f4996f8f40bdf0eafe4e81aef6
Jack S. Hale <mail@jackhale.co.uk>
2016-11-09 16:28:58 +0000

    Possible new inline param style for comment.

--- dolfin/fem/LocalAssembler.h
+++ dolfin/fem/LocalAssembler.h
@@ -44,7 +44,7 @@ namespace dolfin
   /// functionality provided here is also wrapped as a free function
   /// assemble_local(form_a, cell) in Python for easier usage. Use
   /// from the C++ interface defined below will be faster than the
-  /// free function as less objects need to be created and destroyed.
+  /// free function as fewer objects need to be created and destroyed.
 
   class LocalAssembler
   {
@@ -54,98 +54,59 @@ namespace dolfin
     /// Assemble a local tensor on a cell. Internally calls
     /// assemble_cell(), assemble_exterior_facet(),
     /// assemble_interior_facet().
-    ///
-    /// @param[out] A
-    ///         The tensor to assemble.
-    /// @param[in] ufc 
-    /// @param[in] coordinate_dofs
-    /// @param[in] ufc_cell
-    /// @param[in] cell
-    /// @param[in] cell_domains
-    /// @param[in] exterior_facet_domains
-    /// @param[in] interior_facet_domains
     static void
       assemble(Eigen::Matrix<double, Eigen::Dynamic, 
                              Eigen::Dynamic,
-                             Eigen::RowMajor>& A,
-               UFC& ufc,
-               const std::vector<double>& coordinate_dofs,
-               ufc::cell& ufc_cell,
-               const Cell& cell,
-               const MeshFunction<std::size_t>* cell_domains,
-               const MeshFunction<std::size_t>* exterior_facet_domains,
-               const MeshFunction<std::size_t>* interior_facet_domains);
+                             Eigen::RowMajor>& A, ///< [out] The tensor to assemble. 
+               UFC& ufc, ///< [in]
+               const std::vector<double>& coordinate_dofs, ///< [in]
+               ufc::cell& ufc_cell, ///< [in]
+               const Cell& cell, ///< [in]
+               const MeshFunction<std::size_t>* cell_domains, ///< [in]
+               const MeshFunction<std::size_t>* exterior_facet_domains, ///< [in]
+               const MeshFunction<std::size_t>* interior_facet_domains /*!< [in] */);
 
     /// Worker method called by assemble() to perform assembly of
     /// volume integrals (UFL measure dx).
-    ///
-    /// @param[out] A 
-    ///         The tensor to assemble.
-    /// @param[in] ufc
-    /// @param[in] coordinate_dofs 
-    /// @param[in] ufc_cell
-    /// @param[in] cell
-    /// @param[in] cell_domains
     static void assemble_cell(Eigen::Matrix<double, Eigen::Dynamic,
                                             Eigen::Dynamic,
-                                            Eigen::RowMajor>& A,
-                              UFC& ufc,
-                              const std::vector<double>& coordinate_dofs,
-                              const ufc::cell& ufc_cell,
-                              const Cell& cell,
-                              const MeshFunction<std::size_t>* cell_domains);
+                                            Eigen::RowMajor>& A, ///< [out] The tensor to assemble. 
+                              UFC& ufc, ///< [in]
+                              const std::vector<double>& coordinate_dofs, ///< [in]
+                              const ufc::cell& ufc_cell, ///< [in]
+                              const Cell& cell, ///< [in]
+                              const MeshFunction<std::size_t>* cell_domains /*!< [in] */);
 
     /// Worker method called by assemble() for each of the cell's
     /// external facets to perform assembly of external facet
     /// integrals (UFL measure ds).
-    ///
-    /// @param[out] A
-    ///         The tensor to assemble.
-    /// @param[in] ufc
-    /// @param[in] coordinate_dofs
-    /// @param[in] ufc_cell
-    /// @param[in] cell
-    /// @param[in] facet
-    /// @param[in] local_facet
-    /// @param[in] exterior_facet_domains
     static void
       assemble_exterior_facet(Eigen::Matrix<double, Eigen::Dynamic,
                                             Eigen::Dynamic,
-                                            Eigen::RowMajor>& A,
-                              UFC& ufc,
-                              const std::vector<double>& coordinate_dofs,
-                              const ufc::cell& ufc_cell,
-                              const Cell& cell,
-                              const Facet& facet,
-                              const std::size_t local_facet,
-                              const MeshFunction<std::size_t>* exterior_facet_domains);
+                                            Eigen::RowMajor>& A, ///< [out] The tensor to assemble.
+                              UFC& ufc, ///< [in]
+                              const std::vector<double>& coordinate_dofs, ///< [in]
+                              const ufc::cell& ufc_cell, ///< [in]
+                              const Cell& cell, ///< [in]
+                              const Facet& facet, ///< [in]
+                              const std::size_t local_facet, ///< [in]
+                              const MeshFunction<std::size_t>* exterior_facet_domains /*!< [in] */);
 
     /// Worker method called by assemble() for each of the cell's
     /// internal facets to perform assembly of internal facet
     /// integrals (UFL measure dS)
-    ///
-    /// @param[out] A
-    ///         The tensor to assemble.
-    /// @param[in] ufc
-    /// @param[in] coordinate_dofs
-    /// @param[in] ufc_cell
-    /// @param[in] cell
-    /// @param[in] facet
-    /// @param[in] local_facet
-    /// @param[in] interior_facet_domains
-    /// @param[in] cell_domains
     static void
       assemble_interior_facet(Eigen::Matrix<double, Eigen::Dynamic,
                                             Eigen::Dynamic,
-                                            Eigen::RowMajor>& A,
-                              UFC& ufc,
-                              const std::vector<double>& coordinate_dofs,
-                              const ufc::cell& ufc_cell,
-                              const Cell& cell,
-                              const Facet& facet,
-                              const std::size_t local_facet,
-                              const MeshFunction<std::size_t>* interior_facet_domains,
-                              const MeshFunction<std::size_t>* cell_domains);
+                                            Eigen::RowMajor>& A, ///< [out] 
+                              UFC& ufc, ///< [in]
+                              const std::vector<double>& coordinate_dofs, ///< [in]
+                              const ufc::cell& ufc_cell, ///< [in]
+                              const Cell& cell, ///< [in]
+                              const Facet& facet, ///< [in] 
+                              const std::size_t local_facet, ///< [in]
+                              const MeshFunction<std::size_t>* interior_facet_domains, ///< [in]
+                              const MeshFunction<std::size_t>* cell_domains /*!< [in] */ );
   };
 
 }
