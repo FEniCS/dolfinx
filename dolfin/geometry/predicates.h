dc0831af3719c1f28f163847e840fb755c7eba21
August Johansson <august@simula.no>
2016-11-15 15:44:27 +0100

    Add some comments on how orientXd works [skip ci]

--- dolfin/geometry/predicates.h
+++ dolfin/geometry/predicates.h
@@ -17,10 +17,14 @@
 // Initialize tolerances for exact arithmetic
 void exactinit();
 
-// Compute relative orientation of points pa, pb, pc
+// Compute relative orientation of points pa, pb, pc. The orientation
+// is such that orient2d(pa, pb, pc) > 0 if pa, pb, pc are ordered
+// counter-clockwise.
 double orient2d(double* pa, double* pb, double* pc);
 
-// Compute relative orientation of points pa, pb, pc, pd
+// Compute relative orientation of points pa, pb, pc, pd. The
+// orientation is such that orient3d(pa, pb, pc, pd) > 0 if pa, pb,
+// pc, pd are oriented according to the left hand rule.
 double orient3d(double* pa, double* pb, double* pc, double* pd);
 
 namespace dolfin
