3f94c46fda1dd4d13d9b6849d188be8fd0a6b6db
August Johansson <august@simula.no>
2017-02-18 00:08:53 +0100

    Update demo to allow for rotated meshes.

--- demo/undocumented/multimesh-area/python/demo_multimesh-random-meshes.py
+++ demo/undocumented/multimesh-area/python/demo_multimesh-random-meshes.py
@@ -15,8 +15,10 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with DOLFIN. If not, see <http://www.gnu.org/licenses/>.
 #
+# Modified by August Johansson 2017
+#
 # First added:  2017-02-09
-# Last changed: 2017-02-09
+# Last changed: 2017-02-18
 #
 # This demo program creates a layer of meshes over a unit square mesh and checks
 # that the volume is 1.0.
@@ -28,16 +30,18 @@ from dolfin import *
 parser = argparse.ArgumentParser()
 parser.add_argument('num_parts', type=int, help='number of meshes', default=10)
 parser.add_argument('--N_x', type=int, help='number of mesh divisions (mesh size)', default=2, required=False)
-parser.add_argument('--random_seed', type=float, help='seed for random number generator for creating multimesh', default=1, required=False)
+parser.add_argument('--random_seed', help='seed for random number generator for creating multimesh', default=1, required=False)
+parser.add_argument('--no_random_rotation', action='store_true', help='Do not rotate each mesh with a random angle', default=False, required=False)
 args = parser.parse_args()
 
 print("Number of meshes: {}.".format(args.num_parts))
 print("Number of mesh devisions: {}.".format(args.N_x))
 print("Seed for random number generator for creating multimesh: {}.".format(args.random_seed))
+print("Do not rotate each mesh with a random angle: {}.".format(args.no_random_rotation))
 numpy.random.seed(args.random_seed)
 
 
-def build_multimesh(num_parts, N_x):
+def build_multimesh(num_parts, N_x, no_random_rotation):
 
     # Create multimesh
     multimesh = MultiMesh()
@@ -46,8 +50,8 @@ def build_multimesh(num_parts, N_x):
     mesh = UnitSquareMesh(N_x, N_x)
     multimesh.add(mesh)
 
-    # Add N-1 random sized and rotated rectangular meshes
-    for _ in range(num_parts-1):
+    # Add num_parts-1 random sized and rotated rectangular meshes
+    while (multimesh.num_parts() < num_parts):
 
         x0, x1 = numpy.sort(numpy.random.rand(2))
         y0, y1 = numpy.sort(numpy.random.rand(2))
@@ -56,12 +60,22 @@ def build_multimesh(num_parts, N_x):
         if abs(y1 - y0) < DOLFIN_EPS:
             y1 += DOLFIN_EPS
 
-        print "Add new rectangle mesh ({:.3f}, {:.3f}) x ({:.3f}, {:.3f}).".format(x0, y0, x1, y1)
         mesh = RectangleMesh(Point(x0, y0), Point(x1, y1),
                              int(max(abs(x1-x0)*N_x, 1)), int(max(abs(y1-y0)*N_x, 1)))
 
-        #mesh.rotate(numpy.random.rand()*180)
-        multimesh.add(mesh)
+        s =  "Add new rectangle mesh ({:.3f}, {:.3f}) x ({:.3f}, {:.3f}).".format(x0, y0, x1, y1)
+        is_interior = True
+
+        if not no_random_rotation:
+            phi = numpy.random.rand()*180
+            mesh.rotate(phi)
+            coords = mesh.coordinates()
+            is_interior = not numpy.any(coords < 0) and not numpy.any(coords > 1.)
+            s += " Rotation {:.1f} degrees.".format(phi)
+
+        if is_interior:
+            print s
+            multimesh.add(mesh)
 
     multimesh.build()
     return multimesh
@@ -73,7 +87,7 @@ if __name__ == "__main__":
         exit(0)
 
     # Build multimesh
-    multimesh = build_multimesh(args.num_parts, args.N_x)
+    multimesh = build_multimesh(args.num_parts, args.N_x, args.no_random_rotation)
 
     # Assemble linear system
     # FIXME: This does not work yet
