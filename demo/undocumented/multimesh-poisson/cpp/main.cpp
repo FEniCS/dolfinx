f6d4686734e090d9ab654d9abe43cf5d3ca7e588
Anders Logg <logg@chalmers.se>
2017-05-17 12:33:07 +0200

    Cleanup C++ version of demo multimesh-poisson, now same as Python version and produces same results (some spikes that shouldn't be there).

--- demo/undocumented/multimesh-poisson/cpp/main.cpp
+++ demo/undocumented/multimesh-poisson/cpp/main.cpp
@@ -23,25 +23,12 @@
 // on a sequence of rotating meshes to test the multimesh
 // functionality.
 
-#include "MultiMeshPoisson.h"
-#include <dolfin.h>
-
 #include <cmath>
-#include <fstream>
-
-
+#include <dolfin.h>
+#include "MultiMeshPoisson.h"
 
 using namespace dolfin;
 using std::make_shared;
-
-// Source term (right-hand side)
-class Source : public Expression
-{
-  void eval(Array<double>& values, const Array<double>& x) const
-  {
-    values[0] = 1.0;
-  }
-};
 
 // Sub domain for Dirichlet boundary condition
 class DirichletBoundary : public SubDomain
@@ -53,14 +40,9 @@ class DirichletBoundary : public SubDomain
 };
 
 // Compute solution for given mesh configuration
-std::pair<std::shared_ptr<MultiMesh>,
-          std::shared_ptr<MultiMeshFunction>> solve_poisson(double t,
+std::shared_ptr<MultiMeshFunction> solve_poisson(double t,
                                                  double x1, double y1,
-                                                 double x2, double y2,
-                                                 bool plot_solution,
-                                                 File& u0_file,
-                                                 File& u1_file,
-                                                 File& u2_file)
+                                                 double x2, double y2)
 {
   // Create meshes
   double r = 0.5;
@@ -85,7 +67,7 @@ std::pair<std::shared_ptr<MultiMesh>,
   auto L = make_shared<MultiMeshPoisson::MultiMeshLinearForm>(V);
 
   // Attach coefficients
-  auto f = make_shared<Source>();
+  auto f = make_shared<Constant>(1);
   L->f = f;
 
   // Assemble linear system
@@ -104,22 +86,7 @@ std::pair<std::shared_ptr<MultiMesh>,
   auto u = make_shared<MultiMeshFunction>(V);
   solve(*A, *u->vector(), *b);
 
-  // Save to file
-  // u0_file << *u->part(0);
-  // u1_file << *u->part(1);
-  // u2_file << *u->part(2);
-
-  // Plot solution (last time)
-  if (plot_solution)
-  {
-    plot(V->multimesh());
-    plot(u->part(0), "u_0");
-    plot(u->part(1), "u_1");
-    plot(u->part(2), "u_2");
-    interactive();
-  }
-
-  return std::make_pair(multimesh, u);
+  return u;
 }
 
 int main(int argc, char* argv[])
@@ -131,14 +98,14 @@ int main(int argc, char* argv[])
   }
 
   // Parameters
-  const double T = 40.0;
-  const std::size_t N = 400;
+  const double T = 10.0;
+  const std::size_t N = 100;
   const double dt = T / N;
 
-  // Files for storing solution
-  File u0_file("u0.pvd");
-  File u1_file("u1.pvd");
-  File u2_file("u2.pvd");
+  // Create files for output
+  XDMFFile f0("output/u0.xdmf");
+  XDMFFile f1("output/u1.xdmf");
+  XDMFFile f2("output/u2.xdmf");
 
   // Iterate over configurations
   for (std::size_t n = 0; n < N; n++)
@@ -153,41 +120,18 @@ int main(int argc, char* argv[])
     const double y2 = sin(t)*cos(2*t);
 
     // Compute solution
-    std::pair<std::shared_ptr<MultiMesh>,
-              std::shared_ptr<MultiMeshFunction>> u = solve_poisson(t, x1, y1, x2, y2, n == N - 1,
-                                                                    u0_file, u1_file, u2_file);
+    std::shared_ptr<MultiMeshFunction> u = solve_poisson(t, x1, y1, x2, y2);
 
-    for (int i = 0; i < 3; i++)
-    {
-      std::stringstream filenamestream;
-      filenamestream << "ref_" << n << "_" << i << ".txt";
-      const std::string filename = filenamestream.str();
-
-      std::vector<double> values;
-      u.second->part(i)->compute_vertex_values(values, *u.first->part(i));
-
-      //   std::cout << "Writing reference file " << filename << std::endl;
-      //   std::ofstream outfile(filename);
-      //   outfile << std::setprecision(16);
-      //   for (double d : values)
-      //     outfile << d << std::endl;
-
-      // Compare with reference value
-      std::ifstream infile(filename);
-      double ref_value;
-      std::size_t j = 0;
-      while (infile >> ref_value)
-      {
-        if (!near(ref_value, values[j], DOLFIN_EPS_LARGE))
-        {
-          std::cout << "Comparison with reference value failed: "
-                    << ref_value << ", " << values[j] << " : "
-                    << ref_value-values[j] << std::endl;
-        }
-        j++;
-      }
-    }
+    // Save to file
+    f0.write(*u->part(0), t);
+    f1.write(*u->part(1), t);
+    f2.write(*u->part(2), t);
   }
+
+  // Close files
+  f0.close();
+  f1.close();
+  f2.close();
 
   return 0;
 }
