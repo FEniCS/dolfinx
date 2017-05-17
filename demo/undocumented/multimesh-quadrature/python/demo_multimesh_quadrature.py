d9789e2812864de4c094cfe288ef551361ac589b
Anders Logg <logg@chalmers.se>
2017-05-17 14:34:12 +0200

    Remove demo multimesh-compression Rename demo multimesh --> multimesh-quadrature Illustrate both compression on/off in demo multimesh-quadrature

--- demo/undocumented/multimesh-quadrature/python/demo_multimesh_quadrature.py
+++ demo/undocumented/multimesh-quadrature/python/demo_multimesh_quadrature.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2014-2015 Anders Logg
+# Copyright (C) 2014-2017 Anders Logg
 #
 # This file is part of DOLFIN.
 #
@@ -16,9 +16,18 @@
 # along with DOLFIN. If not, see <http://www.gnu.org/licenses/>.
 #
 # First added:  2014-04-07
-# Last changed: 2015-06-15
+# Last changed: 2017-05-17
+#
+# This demo program illustrates multimesh quadrature on a pair
+# of overlapping meshes using red dots to mark positive quadature
+# weights and black dots to mark negative quadature weights.
+#
+# To create movies from the generated PNG files, enter the output
+# directory and run the following commmands:
+# 
+# ffmpeg -i multimesh_quadrature_%04d.png multimesh_quadrature.mp4
+# ffmpeg -i multimesh_quadrature_%04d.png multimesh_quadrature_compressed.mp4
 
-from __future__ import print_function
 from dolfin import *
 import os
 
@@ -56,80 +65,83 @@ def clear_plot():
     if not plot: return
     pl.clf()
 
-def save_plot(frame):
+def save_plot(frame, compress):
     if not plot: return
+    if not os.path.isdir("output"): os.makedirs("output")
     pl.axis("equal")
     pl.axis("off")
-    pl.savefig("multimesh_propeller_%.4d.png" % frame, dpi=300)
+    c = "_compressed" if compress else ""
+    pl.savefig("output/multimesh_quadrature%s_%.4d.png" % (c, frame), dpi=300)
 
 def show_plot():
     if not plot: return
     pl.show()
 
 # Parameters
-N  = 20
+N = 8
 dv = 5
-R  = 0.6
+R = 0.6
 num_frames = 3
 
 # Uncomment to generate a longer movie
 #num_frames = 360
 
-# Create and load meshes
-mesh = RectangleMesh(Point(-R, -R), Point(R, R), N, N)
-propeller = Mesh("../propeller_2d_coarse.xml.gz")
+# Iterate over quadrature compression on/off
+for compress in [True, False]:
+	print("Compression:", compress)
 
-# Iterate over rotations
-for frame in range(num_frames):
+	# Create and load meshes
+	mesh = RectangleMesh(Point(-R, -R), Point(R, R), N, N)
+	propeller = Mesh("../propeller_2d_coarse.xml.gz")
 
-    print("Frame %d out of %d..." % (frame + 1, num_frames))
+	# Iterate over frames (rotations of the propeller)
+	for frame in range(num_frames):
+	    print("Frame %d out of %d..." % (frame + 1, num_frames))
 
-    # Rotate propeller
-    propeller.rotate(dv)
+	    # Rotate propeller
+	    propeller.rotate(dv)
 
-    # Build multimesh
-    multimesh = MultiMesh()
-    multimesh.add(mesh)
-    multimesh.add(propeller)
-    multimesh.build()
+	    # Build multimesh
+	    multimesh = MultiMesh()
+	    multimesh.parameters.compress_volume_quadrature = compress
+	    multimesh.add(mesh)
+	    multimesh.add(propeller)
+	    multimesh.build()
 
-    # Extract data
-    cut_cells = multimesh.cut_cells(0)
-    uncut_cells = multimesh.uncut_cells(0)
-    covered_cells = multimesh.covered_cells(0)
+	    # Extract data
+	    cut_cells = multimesh.cut_cells(0)
+	    uncut_cells = multimesh.uncut_cells(0)
+	    covered_cells = multimesh.covered_cells(0)
 
-    # Clear plot
-    clear_plot()
+	    # Clear plot
+	    clear_plot()
 
-    # Plot cells in background mesh
-    for c in cut_cells:
-        plot_triangle(c, mesh, yellow)
-    for c in uncut_cells:
-        plot_triangle(c, mesh, blue)
-    for c in covered_cells:
-        plot_triangle(c, mesh, white, alpha_line=0.1)
+	    # Plot cells in background mesh
+	    for c in cut_cells:
+	        plot_triangle(c, mesh, yellow)
+	    for c in uncut_cells:
+	        plot_triangle(c, mesh, blue)
+	    for c in covered_cells:
+	        plot_triangle(c, mesh, white, alpha_line=0.1)
 
-    # Plot propeller mesh
-    for c in range(propeller.num_cells()):
-        plot_triangle(c, propeller, color=white, alpha_fill=0.25)
+	    # Plot propeller mesh
+	    for c in range(propeller.num_cells()):
+	        plot_triangle(c, propeller, color=white, alpha_fill=0.25)
 
-    # Plot quadrature points
-    for c in cut_cells:
-        points, weights = multimesh.quadrature_rule_cut_cell(0, c)
-        for i in range(len(weights)):
-            w = weights[i]
-            x = points[2*i]
-            y = points[2*i + 1]
-            if w > 0:
-                plot_point_red(x, y)
-            else:
-                plot_point_black(x, y)
+	    # Plot quadrature points
+	    for c in cut_cells:
+	        points, weights = multimesh.quadrature_rules_cut_cells(0, c)
+	        for i in range(len(weights)):
+	            w = weights[i]
+	            x = points[2*i]
+	            y = points[2*i + 1]
+	            if w > 0:
+	                plot_point_red(x, y)
+	            else:
+	                plot_point_black(x, y)
 
-    # Save plot
-    save_plot(frame)
+	    # Save plot
+	    save_plot(frame, compress)
 
 # Show last frame
 show_plot()
-
-# Generate movie using
-#ffmpeg -r 25 -b 1800 -i multimesh_propeller_%04d.png multimesh_propeller.mp4
