ee5511a5451a7c9a39c65d5271845d3c65028f04
Tormod Landet <tormod@landet.net>
2017-09-08 15:04:26 +0200

    Add numpy float array to C++ Array<double> unit test
    According to the CI the Array<int> test currently fails (a regression).
    On my computer the float version works, lets see what the CI thinks of
    it ...

--- test/unit/python/common/test_array.py
+++ test/unit/python/common/test_array.py
@@ -24,7 +24,7 @@ import numpy
 
 def test_array_int():
     code = """
-    int test_arrays(const Array<int>& int_arr)
+    int test_int_array(const Array<int>& int_arr)
     {
         int ret = 0;
         for (int i = 0; i < int_arr.size(); i++)
@@ -39,5 +39,35 @@ def test_array_int():
                                              sources=[],
                                              include_dirs=["."])
     arr = numpy.array([1, 2, 4, 8], dtype=numpy.intc)
-    ans = module.test_arrays(arr)
+    ans = module.test_int_array(arr)
     assert ans == arr.sum() == 15
+
+
+def test_array_double():
+    code = """
+    double test_double_array(const Array<double>& arr)
+    {
+        double ret = 0;
+        for (int i = 0; i < arr.size(); i++)
+        {
+            ret += arr[i];
+        }
+        return ret;
+    }
+    """
+    module = dolfin.compile_extension_module(code=code,
+                                             source_directory='.',
+                                             sources=[],
+                                             include_dirs=["."])
+    arr = numpy.array([1, 2, 4, 8], dtype=float)
+    ans = module.test_double_array(arr)
+    assert abs(arr.sum() - 15) < 1e-15
+    assert abs(ans - 15) < 1e-15
+
+
+if __name__ == '__main__':
+    print 'test_array_double'
+    test_array_double()
+    
+    print 'test_array_int'  
+    test_array_int()
