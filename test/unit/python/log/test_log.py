a62e5a0b70abbd6cdda8d9e4f04a9091316b1097
Tormod Landet <tormod@landet.net>
2017-09-20 16:33:12 +0200

    Import fixes

--- test/unit/python/log/test_log.py
+++ test/unit/python/log/test_log.py
@@ -19,8 +19,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with DOLFIN. If not, see <http://www.gnu.org/licenses/>.
 
-import pytest
-from dolfin import *
+from dolfin import LogLevel
 from dolfin_utils.test import skip_if_not_pybind11
 
 
