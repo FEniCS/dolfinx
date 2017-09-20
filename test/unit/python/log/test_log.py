07ac663112fac4d27428993af9593bf4108d5264
Tormod Landet <tormod@landet.net>
2017-09-20 14:07:47 +0200

    Make LogLevel comparable with pybind11

---
+++ test/unit/python/log/test_log.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env py.test
+
+"""Unit tests for the log"""
+
+# Copyright (C) 2017 Tormod Landet
+#
+# This file is part of DOLFIN.
+#
+# DOLFIN is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# DOLFIN is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with DOLFIN. If not, see <http://www.gnu.org/licenses/>.
+
+import pytest
+from dolfin import *
+from dolfin_utils.test import skip_if_not_pybind11
+
+
+@skip_if_not_pybind11
+def test_log_level_comparable():
+    info = LogLevel.INFO
+    warning = LogLevel.WARNING
+    assert info < warning
+    assert warning < 1000
+
