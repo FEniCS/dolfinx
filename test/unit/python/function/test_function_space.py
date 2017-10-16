243a18d7477ff0d2fe825aa7841b1a7e03107b20
Garth N. Wells <gnw20@cam.ac.uk>
2017-10-16 08:50:08 +0100

    Run new test with pybind11 only.

--- test/unit/python/function/test_function_space.py
+++ test/unit/python/function/test_function_space.py
@@ -170,8 +170,9 @@ def test_in_operator(f, g, V, V2, W, W2):
     assert f in V2
     assert g in W
     assert g in W2
-    with pytest.raises(RuntimeError):
-        mesh() in V
+    if has_pybind11():
+        with pytest.raises(RuntimeError):
+            mesh() in V
 
 
 def test_collapse(W, V):
