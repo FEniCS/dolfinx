f4b2298d4f2ae5d8677e4555743521d6f07cbb25
Garth N. Wells <gnw20@cam.ac.uk>
2017-12-11 13:14:25 +0000

    Try pytest to run regression tests.

--- python/demo/test.py
+++ python/demo/test.py
@@ -2,9 +2,21 @@ import subprocess
 import sys
 import pytest
 
-def test_demos():
-    ret = subprocess.run([sys.executable, "./documented/poisson/demo_poisson.py"],
+# Build list of demos
+
+@pytest.mark.parametrize("name,path",
+                         [
+                             ("demo_biharmonic.py", "./documented/biharmonic/"),
+                             ("demo_poisson.py", "./documented/poisson/"),
+                         ])
+def test_demos(name, path):
+    #ret = subprocess.run([sys.executable, "demo_poisson.py"],
+    #                     shell=True, check=True, cwd="./documented/poisson/")
+    ret = subprocess.run([sys.executable, name],
+                         cwd=path,
+                         #env={"DOLFIN_NOPLOT": "0",
+                         #     "PYTHONPATH": "/home/garth/code/fenics/dev/dolfin.d/dolfin/python/build/lib.linux-x86_64-3.6/"},
                          check=True)
     #assert ret
-    print("---------------------")
-    print(ret.returncode)
+    #print("---------------------")
+    #print(ret.returncode)
