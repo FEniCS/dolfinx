336fef8c1799f06f1160ba822792fb184b706392
Garth N. Wells <gnw20@cam.ac.uk>
2017-12-12 16:17:17 +0000

    Add missing pytest config file.

---
+++ python/demo/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+
+def pytest_addoption(parser):
+    parser.addoption("--mpiexec", action="store", default=None,
+                     help="Name of program to run MPI, e.g. mpiexex")
+    parser.addoption("--num-proc", action="store", default=1,
+                     help="Number of MPI processes to use")
+
+@pytest.fixture
+def mpiexec(request):
+    return request.config.getoption("--mpiexec")
+
+
+@pytest.fixture
+def num_proc(request):
+    return request.config.getoption("--num-proc")
