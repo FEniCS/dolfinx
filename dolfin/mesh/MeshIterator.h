4971450af092c6f049bb16752024bcfff3e89125
Garth N. Wells <gnw20@cam.ac.uk>
2018-01-04 19:24:10 +0000

    Some performance debugging of iterators.

--- dolfin/mesh/MeshIterator.h
+++ dolfin/mesh/MeshIterator.h
@@ -25,11 +25,14 @@ namespace dolfin
 
     /// Copy constructor
     MeshIterator(const MeshIterator& it) : _entity(it._entity),  _pos(it._pos), _index(it._index)
-    {}
+    {
+      std::cout << "EMPTY COPY" << std::endl;
+    }
 
     // Copy assignment
     const MeshIterator& operator= (const MeshIterator& m)
     {
+      //std::cout << "ASSIGN" << std::endl;
       _entity = m._entity;
       _pos = m._pos;
       _index = m._index;
@@ -37,16 +40,21 @@ namespace dolfin
     }
 
     // Constructor with Mesh
-    MeshIterator(const Mesh& mesh, std::size_t pos=0) : _entity(mesh, 0), _pos(pos), _index(nullptr)
+    MeshIterator(const Mesh& mesh, std::size_t pos=0)
+      : _entity(mesh, 0), _pos(pos), _index(nullptr)
     {
+      std::cout << "Mesh constructor" << std::endl;
       // Check if mesh is empty
-      if (mesh.num_vertices() == 0)
-        return;
+      //if (mesh.num_vertices() == 0)
+      //  return;
     }
 
     // Constructor with MeshEntity
-    MeshIterator(const MeshEntity& e, std::size_t pos=0) : _entity(e.mesh(), 0), _pos(pos)
+    MeshIterator(const MeshEntity& e, std::size_t pos=0)
+      : _entity(e.mesh(), 0), _pos(pos)
     {
+      std::cout << "AAAA" << std::endl;
+
       // Get connectivity
       const MeshConnectivity& c = e.mesh().topology()(e.dim(), _entity.dim());
 
@@ -65,22 +73,26 @@ namespace dolfin
 
     bool operator==(const MeshIterator& other) const
     {
+      std::cout << "BBBB" << std::endl;
       return (_pos == other._pos and _index == other._index);
     }
 
     bool operator!=(const MeshIterator& other) const
     {
-      return (_pos != other._pos or _index != other._index);
+      //return (_pos != other._pos or _index != other._index);
+      return (_pos != other._pos);
     }
 
     T* operator->()
     {
+      std::cout << "CCC" << std::endl;
       _entity._local_index = (_index ? _index[_pos] : _pos);
       return &_entity;
     }
 
     T& operator*()
     {
+      std::cout << "DDDD" << std::endl;
       _entity._local_index = (_index ? _index[_pos] : _pos);
       return _entity;
     }
@@ -100,9 +112,10 @@ namespace dolfin
 
   // Class defining begin() and end() methods for a given entity
   template<class T>
-  class entities
+    class entities
   {
   public:
+
   entities(const Mesh& mesh) : _it_begin(mesh, 0), _it_end(mesh, 0)
     {
       const std::size_t dim = _it_begin._entity.dim();
--- iterator-debug/main.cpp
+++ iterator-debug/main.cpp
@@ -5,22 +5,48 @@ using namespace dolfin;
 
 int main()
 {
-  std::array<Point, 2> pt = {Point(0.,0.), Point(1.,1.)};
-  auto mesh = std::make_shared<Mesh>(RectangleMesh::create(MPI_COMM_WORLD, pt, {{320, 320}}, CellType::Type::triangle));
+  //std::array<Point, 2> pt = {Point(0.,0.), Point(1.,1.)};
+  //auto mesh = std::make_shared<Mesh>(RectangleMesh::create(MPI_COMM_WORLD, pt, {{320, 320}}, CellType::Type::triangle));
+  std::array<Point, 2> pt = {Point(0.,0.,0.), Point(1.,1.,1.0)};
+  auto mesh = std::make_shared<Mesh>(BoxMesh::create(MPI_COMM_WORLD, pt,
+                                                     {{200, 200, 200}}, CellType::Type::tetrahedron));
 
   {
-    boost::timer::cpu_timer t;
+
     Timer t0("old");
     std::size_t p = 0;
+    boost::timer::cpu_timer t;
     for (CellIterator c(*mesh); !c.end(); ++c)
     {
-      for (VertexIterator v(*c); !v.end(); ++v)
-        p += v->index();
+      ++p;
+      //for (VertexIterator v(*c); !v.end(); ++v)
+      //  p += v->index();
     }
+    auto tend = t.elapsed();
     std::cout << p << std::endl;
-    std::cout << boost::timer::format(t.elapsed()) << "\n";
+    std::cout << boost::timer::format(tend) << "\n";
   }
 
+  {
+    std::size_t p = 0;
+
+    auto u = cells(*mesh);
+    MeshIterator<Cell> c0 = u.begin();
+    MeshIterator<Cell> c1 = u.end();
+
+    std::cout << "Start timer" << std::endl;
+    Timer t0("new3");
+    boost::timer::cpu_timer t;
+    for (MeshIterator<Cell> c = c0; c != c1; ++c)
+    {
+      ++p;
+    }
+    auto tend = t.elapsed();
+    std::cout << p << std::endl;
+    std::cout << boost::timer::format(tend) << "\n";
+  }
+
+  /*
   {
     boost::timer::cpu_timer t;
     Timer t0("new+old");
@@ -70,7 +96,7 @@ int main()
     std::cout << p << std::endl;
     std::cout << boost::timer::format(t.elapsed()) << "\n";
   }
-
+  */
 
 
   list_timings(TimingClear::clear, {{TimingType::wall}});
