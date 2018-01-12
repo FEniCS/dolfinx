81fcdf1700344698714a9775dae5ce9412b76b6d
Chris Richardson <chris@bpi.cam.ac.uk>
2018-01-12 22:03:28 +0000

    example SPDX [skip ci]

--- dolfin/common/ArrayView.h
+++ dolfin/common/ArrayView.h
@@ -1,22 +1,10 @@
 // Copyright (C) 2015 Garth N. Wells
 //
-// This file is part of DOLFIN.
+// This file is part of DOLFIN (www.fenicsproject.org)
 //
-// DOLFIN is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// DOLFIN is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with DOLFIN. If not, see <http://www.gnu.org/licenses/>.
+// SPDX-License-Identifier: LGPL-3.0-or-later
 
-#ifndef __DOLFIN_ARRAYVIEW_H
-#define __DOLFIN_ARRAYVIEW_H
+#pragma once
 
 #include <cstddef>
 #include <dolfin/log/log.h>
@@ -112,5 +100,3 @@ private:
   T* _x;
 };
 }
-
-#endif
