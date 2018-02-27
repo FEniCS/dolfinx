09a66c9500987a6b0ae2dc0168ac452e18818f7d
Garth N. Wells <gnw20@cam.ac.uk>
2018-02-27 11:19:11 +0000

    Add missing include ti jit. Should really be left to user.

--- python/dolfin/function/jit.py
+++ python/dolfin/function/jit.py
@@ -26,6 +26,7 @@ def jit_generate(class_data, module_name, signature, parameters):
     #define DLL_EXPORT __attribute__ ((visibility ("default")))
 #endif
 
+#include <dolfin/common/constants.h>
 #include <dolfin/function/Expression.h>
 #include <Eigen/Dense>
 
