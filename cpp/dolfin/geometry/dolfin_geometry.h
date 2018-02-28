5dfb9d93aefa02928ee41044d972fb7b2623f909
Chris Richardson <chris@bpi.cam.ac.uk>
2018-02-28 17:47:20 +0000

    Fix c++ unittests

--- cpp/dolfin/geometry/dolfin_geometry.h
+++ cpp/dolfin/geometry/dolfin_geometry.h
@@ -13,6 +13,4 @@ namespace dolfin
 
 #include <dolfin/geometry/BoundingBoxTree.h>
 #include <dolfin/geometry/CollisionPredicates.h>
-#include <dolfin/geometry/MeshPointIntersection.h>
 #include <dolfin/geometry/Point.h>
-#include <dolfin/geometry/intersect.h>
