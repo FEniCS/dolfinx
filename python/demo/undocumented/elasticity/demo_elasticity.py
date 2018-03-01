0eff780cd743cbcedd167f18aa6c8ea5034b8be5
Garth N. Wells <gnw20@cam.ac.uk>
2018-03-01 19:55:48 +0000

    Add some demos to CircleCI.

--- python/demo/undocumented/elasticity/demo_elasticity.py
+++ python/demo/undocumented/elasticity/demo_elasticity.py
@@ -19,12 +19,14 @@ smoothed aggregation algerbaric multigrid."""
 # You should have received a copy of the GNU Lesser General Public License
 # along with DOLFIN. If not, see <http://www.gnu.org/licenses/>.
 
-
+import numpy as np
 from dolfin import *
 from dolfin.la import (VectorSpaceBasis, PETScVector, PETScOptions,
                        PETScKrylovSolver)
+from dolfin.fem.assembling import assemble_system
 
 import matplotlib.pyplot as plt
+
 
 def build_nullspace(V, x):
     """Function to build null space for 3D elasticity"""
@@ -33,17 +35,17 @@ def build_nullspace(V, x):
     nullspace_basis = [PETScVector(x) for i in range(6)]
 
     # Build translational null space basis
-    V.sub(0).dofmap().set(nullspace_basis[0], 1.0);
-    V.sub(1).dofmap().set(nullspace_basis[1], 1.0);
-    V.sub(2).dofmap().set(nullspace_basis[2], 1.0);
+    V.sub(0).dofmap().set(nullspace_basis[0], 1.0)
+    V.sub(1).dofmap().set(nullspace_basis[1], 1.0)
+    V.sub(2).dofmap().set(nullspace_basis[2], 1.0)
 
     # Build rotational null space basis
-    V.sub(0).set_x(nullspace_basis[3], -1.0, 1);
-    V.sub(1).set_x(nullspace_basis[3],  1.0, 0);
-    V.sub(0).set_x(nullspace_basis[4],  1.0, 2);
-    V.sub(2).set_x(nullspace_basis[4], -1.0, 0);
-    V.sub(2).set_x(nullspace_basis[5],  1.0, 1);
-    V.sub(1).set_x(nullspace_basis[5], -1.0, 2);
+    V.sub(0).set_x(nullspace_basis[3], -1.0, 1)
+    V.sub(1).set_x(nullspace_basis[3],  1.0, 0)
+    V.sub(0).set_x(nullspace_basis[4],  1.0, 2)
+    V.sub(2).set_x(nullspace_basis[4], -1.0, 0)
+    V.sub(2).set_x(nullspace_basis[5],  1.0, 1)
+    V.sub(1).set_x(nullspace_basis[5], -1.0, 2)
 
     for x in nullspace_basis:
         x.apply()
@@ -56,34 +58,44 @@ def build_nullspace(V, x):
 
 
 # Load mesh from file
-mesh = Mesh(MPI.comm_world)
-XDMFFile(MPI.comm_world, "../pulley.xdmf").read(mesh)
+# mesh = Mesh(MPI.comm_world)
+# XDMFFile(MPI.comm_world, "../pulley.xdmf").read(mesh)
 
-#mesh = UnitCubeMesh(1, 1, 1)
-#mesh = BoxMesh.create(MPI.comm_world, [Point(0,0,0), Point(1,1,1)], [1, 1, 1], CellType.Type.tetrahedron)
+# mesh = UnitCubeMesh(2, 2, 2)
+mesh = BoxMesh.create(MPI.comm_world, [Point(0, 0, 0), Point(1, 1, 1)], [
+                      12, 12, 12], CellType.Type.tetrahedron)
 
 # Function to mark inner surface of pulley
-def inner_surface(x, on_boundary):
-    r = 3.75 - x[2]*0.17
-    return (x[0]*x[0] + x[1]*x[1]) < r*r and on_boundary
+# def inner_surface(x, on_boundary):
+#    r = 3.75 - x[2]*0.17
+#    return (x[0]*x[0] + x[1]*x[1]) < r*r and on_boundary
+
+
+def boundary(x, on_boundary):
+    return np.logical_or(x[:, 0] < DOLFIN_EPS, x[:, 0] > 1.0 - DOLFIN_EPS)
+
 
 # Rotation rate and mass density
 omega = 300.0
 rho = 10.0
 
 # Loading due to centripetal acceleration (rho*omega^2*x_i)
-f = Expression(("rho*omega*omega*x[0]", "rho*omega*omega*x[1]", "0.0"),
-               omega=omega, rho=rho, degree=2)
+# f = Expression(("rho*omega*omega*x[0]", "rho*omega*omega*x[1]", "0.0"),
+#               omega=omega, rho=rho, degree=2)
+f = Expression(("0.0", "1.0e8", "0.0"), degree=2)
 
 # Elasticity parameters
 E = 1.0e9
-nu = 0.3
+nu = 0.0
 mu = E/(2.0*(1.0 + nu))
 lmbda = E*nu/((1.0 + nu)*(1.0 - 2.0*nu))
 
 # Stress computation
+
+
 def sigma(v):
     return 2.0*mu*sym(grad(v)) + lmbda*tr(sym(grad(v)))*Identity(len(v))
+
 
 # Create function space
 V = VectorFunctionSpace(mesh, "Lagrange", 1)
@@ -96,7 +108,7 @@ L = inner(f, v)*dx
 
 # Set up boundary condition on inner surface
 c = Constant((0.0, 0.0, 0.0))
-bc = DirichletBC(V, c, inner_surface)
+bc = DirichletBC(V, c, boundary)
 
 # Assemble system, applying boundary conditions and preserving
 # symmetry)
@@ -112,11 +124,11 @@ null_space = build_nullspace(V, u.vector())
 
 # Attach near nullspace to matrix
 A.set_near_nullspace(null_space)
-#as_backend_type(A).set_near_nullspace(null_space)
+# as_backend_type(A).set_near_nullspace(null_space)
 
 # Set solver options
 PETScOptions.set("ksp_type", "cg")
-PETScOptions.set("ksp_rtol", 1.0e-9)
+PETScOptions.set("ksp_rtol", 1.0e-12)
 PETScOptions.set("pc_type", "gamg")
 
 # Use Chebyshev smoothing for multigrid
@@ -128,18 +140,18 @@ PETScOptions.set("mg_levels_esteig_ksp_type", "cg")
 PETScOptions.set("mg_levels_ksp_chebyshev_esteig_steps", 20)
 
 # Monitor solver
-PETScOptions.set("ksp_monitor");
+PETScOptions.set("ksp_monitor")
 
 # Create CG Krylov solver and turn convergence monitoring on
 solver = PETScKrylovSolver(MPI.comm_world)
-#solver = PETScKrylovSolver()
+# solver = PETScKrylovSolver()
 solver.set_from_options()
 
 # Set matrix operator
-solver.set_operator(A);
+solver.set_operator(A)
 
 # Compute solution
-solver.solve(u.vector(), b);
+solver.solve(u.vector(), b)
 
 # Save solution to XDMF format
 file = XDMFFile(MPI.comm_world, "elasticity.xdmf")
@@ -151,15 +163,17 @@ if MPI.rank(mesh.mpi_comm()) == 0:
 
 
 # Save colored mesh partitions in VTK format if running in parallel
-#if MPI.size(mesh.mpi_comm()) > 1:
+# if MPI.size(mesh.mpi_comm()) > 1:
 #    File("partitions.pvd") << MeshFunction("size_t", mesh, mesh.topology().dim(), \
 #                                           MPI.rank(mesh.mpi_comm()))
 
 # Project and write stress field to post-processing file
-#W = TensorFunctionSpace(mesh, "Discontinuous Lagrange", 0)
-#stress = project(sigma(u), V=W)
-#File("stress.pvd") << stress
+# W = TensorFunctionSpace(mesh, "Discontinuous Lagrange", 0)
+# stress = project(sigma(u), V=W)
+# File("stress.pvd") << stress
 
 # Plot solution
-#plot(u)
-#plt.show()
+import matplotlib.pyplot as plt
+import dolfin.plotting
+dolfin.plotting.plot(u)
+plt.show()
