4733bed22090169b9c80762d0b1ce28b91662410
Garth N. Wells <gnw20@cam.ac.uk>
2018-03-06 10:47:41 +0000

    Consolidate some files.

--- python/dolfin/__init__.py
+++ python/dolfin/__init__.py
@@ -102,8 +102,8 @@ from .generation import UnitIntervalMesh, UnitSquareMesh, UnitCubeMesh
 from .jit.pybind11jit import compile_cpp_code
 
 #from .la import la_index_dtype
-from .mesh.meshfunction import (MeshFunction)
-from .mesh.meshvaluecollection import MeshValueCollection
+from .mesh import MeshFunction
+from .mesh import MeshValueCollection
 
 # Import from ufl
 from ufl import (FiniteElement, TensorElement, VectorElement,
