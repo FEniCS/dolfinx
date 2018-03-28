a6e2b20e06d047b41e51182ed2d1933bb908d215
Garth N. Wells <gnw20@cam.ac.uk>
2018-03-28 15:41:09 +0100

    Add test code to Poisson Python demo for coordinate mapping.

--- cpp/dolfin/function/Function.cpp
+++ cpp/dolfin/function/Function.cpp
@@ -294,11 +294,11 @@ void Function::eval(Eigen::Ref<EigenRowArrayXXd> values,
   // Get coordinate mapping
   auto cmap = _function_space->mesh()->geometry().ufc_coord_mapping;
   // assert(cmap);
-  // if (!cmap)
-  // {
-  //   throw std::runtime_error(
-  //       "ufc::coordinate_mapping has not been attached to mesh.");
-  // }
+  if (!cmap)
+  {
+    throw std::runtime_error(
+        "ufc::coordinate_mapping has not been attached to mesh.");
+  }
 
   if (cmap)
   {
