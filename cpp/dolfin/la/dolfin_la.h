4b1edc83ee114bfd7bfd0cb31cc0df5efd80dba8
nate-sime <nsime@carnegiescience.edu>
2018-05-11 12:43:10 -0400

    Add PETScOptions to dolfin.h

--- cpp/dolfin/la/dolfin_la.h
+++ cpp/dolfin/la/dolfin_la.h
@@ -14,6 +14,7 @@ namespace dolfin
 #include <dolfin/la/PETScBaseMatrix.h>
 #include <dolfin/la/PETScKrylovSolver.h>
 #include <dolfin/la/PETScMatrix.h>
+#include <dolfin/la/PETScOptions.h>
 #include <dolfin/la/PETScVector.h>
 #include <dolfin/la/SLEPcEigenSolver.h>
 #include <dolfin/la/Scalar.h>
