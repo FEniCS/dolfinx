f4cf6f2cadf82ce8ed66ccf2a4140244494a11a1
Chris Richardson <chris@bpi.cam.ac.uk>
2018-05-16 21:42:29 +0100

    More updates for flake8 in mesh tests

--- python/test/unit/mesh/test_mesh_editor.py
+++ python/test/unit/mesh/test_mesh_editor.py
@@ -4,9 +4,12 @@
 #
 # SPDX-License-Identifier:    LGPL-3.0-or-later
 
-from dolfin import *
+from dolfin import Mesh, MPI, CellType, cpp
+from dolfin_utils.test import skip_in_parallel
 import numpy
 
+
+@skip_in_parallel
 def test_triangle_mesh():
 
     # Create mesh object
@@ -14,5 +17,7 @@ def test_triangle_mesh():
                 numpy.array([[0.0, 0.0],
                              [1.0, 0.0],
                             [0.0, 1.0]], dtype=numpy.float64),
-                numpy.array([[0,1,2]], dtype=numpy.int32), [],
+                numpy.array([[0, 1, 2]], dtype=numpy.int32), [],
                 cpp.mesh.GhostMode.none)
+    assert mesh.num_entities_global(0) == 3
+    assert mesh.num_entities_global(2) == 1
