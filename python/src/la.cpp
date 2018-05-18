c855de35e2744fc244fa169f2c279095450e651a
igorbaratta <igorbaratta@gmail.com>
2018-05-18 17:50:34 -0300

    simplify python wrapper

--- python/src/la.cpp
+++ python/src/la.cpp
@@ -812,10 +812,7 @@ void la(py::module& m)
                         & dolfin::la::SLEPcEigenSolver::solve)
       .def("solve", (void (dolfin::la::SLEPcEigenSolver::*)(std::int64_t))
                         & dolfin::la::SLEPcEigenSolver::solve)
-      .def("get_eigenvalue",
-           [](dolfin::la::SLEPcEigenSolver& self, std::size_t i) {
-             return self.get_eigenvalue(i);
-           })
+      .def("get_eigenvalue", &dolfin::la::SLEPcEigenSolver::get_eigenpair)
       .def("get_eigenpair",
            [](dolfin::la::SLEPcEigenSolver& self, std::size_t i) {
              double lr, lc;
