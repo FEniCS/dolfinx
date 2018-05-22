6d45fcdeb61d9548d1b8460fc8ba948ed73e2efa
Garth N. Wells <gnw20@cam.ac.uk>
2018-05-22 11:11:39 +0100

    Clean out a bunch of old code. (#90)
    * Remove some dubious code.
    
    Some functions that shouldn't be used, and some with memory management problems. Latter could come back in improved form.
    
    * Remove deleted header from include list.
    
    * Clean up some constants.

--- python/dolfin/__init__.py
+++ python/dolfin/__init__.py
@@ -4,7 +4,6 @@
 # This file is part of DOLFIN (https://www.fenicsproject.org)
 #
 # SPDX-License-Identifier:    LGPL-3.0-or-later
-
 """Main module for DOLFIN"""
 
 # flake8: noqa
@@ -33,19 +32,17 @@ del sys
 from .cpp import __version__
 
 from .cpp.common import (Variable, has_debug, has_hdf5, has_scotch,
-                         has_hdf5_parallel, has_mpi, has_mpi4py,
-                         has_petsc, has_petsc4py, has_parmetis,
-                         has_slepc, has_slepc4py, git_commit_hash,
-                         DOLFIN_EPS, DOLFIN_PI, TimingType,
+                         has_hdf5_parallel, has_mpi, has_mpi4py, has_petsc,
+                         has_petsc4py, has_parmetis, has_slepc, has_slepc4py,
+                         git_commit_hash, DOLFIN_EPS, DOLFIN_PI, TimingType,
                          timing, timings, list_timings)
 
 if has_hdf5():
     from .cpp.io import HDF5File
 
 from .cpp import MPI
-from .cpp.function import (Expression, Constant, FunctionAXPY)
-from .cpp.fem import (FiniteElement, DofMap,
-                      vertex_to_dof_map, dof_to_vertex_map)
+from .cpp.function import (Expression, Constant)
+from .cpp.fem import (FiniteElement, DofMap)
 
 from .cpp.geometry import BoundingBoxTree, Point
 from .cpp.generation import IntervalMesh, BoxMesh, RectangleMesh
@@ -54,13 +51,11 @@ from .cpp.io import XDMFFile, VTKFile
 if has_slepc():
     from .cpp.la import SLEPcEigenSolver
 
-from .cpp.mesh import (Mesh, MeshTopology, MeshGeometry,
-                       CellType,
-                       Cell, Facet, Face, Edge, Vertex, MeshEntity,
-                       Cells, Facets, Faces, Edges, Vertices, MeshEntities,
-                       CellRange, FacetRange, FaceRange, EdgeRange, VertexRange, EntityRange,
-                       SubDomain, MeshQuality,
-                       PeriodicBoundaryComputation)
+from .cpp.mesh import (Mesh, MeshTopology, MeshGeometry, CellType, Cell, Facet,
+                       Face, Edge, Vertex, MeshEntity, Cells, Facets, Faces,
+                       Edges, Vertices, MeshEntities, CellRange, FacetRange,
+                       FaceRange, EdgeRange, VertexRange, EntityRange,
+                       SubDomain, MeshQuality, PeriodicBoundaryComputation)
 
 from .cpp.nls import (NonlinearProblem, NewtonSolver, OptimisationProblem)
 
@@ -71,17 +66,16 @@ from .fem.projection import project
 from .fem.solving import solve
 from .fem.formmanipulations import (derivative, adjoint, increase_order, tear)
 
-from .function.functionspace import (FunctionSpace,
-                                     VectorFunctionSpace, TensorFunctionSpace)
+from .function.functionspace import (FunctionSpace, VectorFunctionSpace,
+                                     TensorFunctionSpace)
 from .function.function import Function
-from .function.argument import (TestFunction, TrialFunction,
-                                TestFunctions, TrialFunctions)
+from .function.argument import (TestFunction, TrialFunction, TestFunctions,
+                                TrialFunctions)
 from .function.constant import Constant
-from .function.specialfunctions import (MeshCoordinates, FacetArea, FacetNormal,
-                                        CellVolume, SpatialCoordinate, CellNormal,
-                                        CellDiameter, Circumradius,
-                                        MinCellEdgeLength, MaxCellEdgeLength,
-                                        MinFacetEdgeLength, MaxFacetEdgeLength)
+from .function.specialfunctions import (
+    MeshCoordinates, FacetArea, FacetNormal, CellVolume, SpatialCoordinate,
+    CellNormal, CellDiameter, Circumradius, MinCellEdgeLength,
+    MaxCellEdgeLength, MinFacetEdgeLength, MaxFacetEdgeLength)
 from .function.expression import Expression, UserExpression, CompiledExpression
 
 from .generation import UnitIntervalMesh, UnitSquareMesh, UnitCubeMesh
@@ -94,18 +88,15 @@ from .mesh import MeshFunction
 from .mesh import MeshValueCollection
 
 # Import from ufl
-from ufl import (FiniteElement, TensorElement, VectorElement,
-                 MixedElement, rhs, lhs, conditional, le, lt, ge, gt,
-                 split, cross, inner, dot, grad, curl, dx, div,
-                 Measure, det, pi, sin, cos, tan, acos, asin, atan,
-                 ln, exp, sqrt, bessel_I, bessel_J, bessel_K,
-                 bessel_Y, Dx, ds, dS, dP, dX, dC, interval, triangle,
-                 tetrahedron, quadrilateral, hexahedron, avg, jump,
-                 sym, tr, Identity, variable, diff, as_vector,
-                 as_tensor, as_matrix, system, outer, dev, skew,
-                 elem_mult, elem_div, elem_pow, elem_op, erf)
+from ufl import (
+    FiniteElement, TensorElement, VectorElement, MixedElement, rhs, lhs,
+    conditional, le, lt, ge, gt, split, cross, inner, dot, grad, curl, dx, div,
+    Measure, det, pi, sin, cos, tan, acos, asin, atan, ln, exp, sqrt, bessel_I,
+    bessel_J, bessel_K, bessel_Y, Dx, ds, dS, dP, dX, dC, interval, triangle,
+    tetrahedron, quadrilateral, hexahedron, avg, jump, sym, tr, Identity,
+    variable, diff, as_vector, as_tensor, as_matrix, system, outer, dev, skew,
+    elem_mult, elem_div, elem_pow, elem_op, erf)
 from ufl.formoperators import action
-
 
 # Initialise PETSc
 cpp.common.SubSystemsManager.init_petsc()
