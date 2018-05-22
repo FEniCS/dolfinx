f6192783835809b31e727e377d46916f83ff55da
Garth N. Wells <gnw20@cam.ac.uk>
2018-05-22 08:53:55 +0100

    Clean up some constants.

--- cpp/dolfin/common/constants.h
+++ cpp/dolfin/common/constants.h
@@ -7,11 +7,7 @@
 #pragma once
 
 #define DOLFIN_EPS 3.0e-16
-#define DOLFIN_EPS_LARGE 1e-14
-#define DOLFIN_SQRT_EPS 1e-8
 #define DOLFIN_PI 3.141592653589793238462
 
 #define DOLFIN_LINELENGTH 256
 #define DOLFIN_TERM_WIDTH 80
-
-
