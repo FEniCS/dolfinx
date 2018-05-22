e68d09b845504d5ad6c4aeee5c9ba405c1cc4269
Fabian Loeschner <fabian.loeschner@rwth-aachen.de>
2018-05-22 12:00:52 +0200

    Pass form compiler parameters to Form constructor in variational problem

--- python/dolfin/fem/solving.py
+++ python/dolfin/fem/solving.py
@@ -187,8 +187,8 @@ def _solve_varproblem(*args, **kwargs):
     # Solve linear variational problem
     if isinstance(eq.lhs, ufl.Form) and isinstance(eq.rhs, ufl.Form):
 
-        a = Form(eq.lhs)
-        L = Form(eq.rhs)
+        a = Form(eq.lhs, form_compiler_parameters=form_compiler_parameters)
+        L = Form(eq.rhs, form_compiler_parameters=form_compiler_parameters)
 
         comm = L.mesh().mpi_comm()
         A = PETScMatrix(comm)
