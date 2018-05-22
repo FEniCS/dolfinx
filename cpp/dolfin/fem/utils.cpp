f084eae76ead91630a094e70ca2194d36afa2c99
Garth N. Wells <gnw20@cam.ac.uk>
2018-05-22 11:51:57 +0100

    Remove more ArrayView use (#91)
    * Remove some dubious code.
    
    Some functions that shouldn't be used, and some with memory management problems. Latter could come back in improved form.
    
    * Remove deleted header from include list.
    
    * Clean up some constants.
    
    * Remove some use of ArrayView.

--- cpp/dolfin/fem/utils.cpp
+++ cpp/dolfin/fem/utils.cpp
@@ -5,7 +5,6 @@
 // SPDX-License-Identifier:    LGPL-3.0-or-later
 
 #include "utils.h"
-#include <dolfin/common/ArrayView.h>
 #include <dolfin/common/IndexMap.h>
 #include <dolfin/common/Timer.h>
 #include <dolfin/common/types.h>
