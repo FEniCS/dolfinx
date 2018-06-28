3b2fd8aa36fa41b85cbf754fc46a259b6eb2f2bf
Jan Blechta <blechta@karlin.mff.cuni.cz>
2018-06-28 11:27:10 +0200

    Add dolfin version to JIT hash (#102)
    This is a backport of DOLFIN old commit 1211d17cbf1d35727a57b01c61127bc904becaf1
    where it fixed problems on Bamboo

--- python/dolfin/jit/pybind11jit.py
+++ python/dolfin/jit/pybind11jit.py
@@ -11,7 +11,7 @@ import re
 
 from dolfin.cpp.log import log, LogLevel
 from . import get_pybind_include
-
+import dolfin.cpp as cpp
 from dolfin.jit.jit import dijitso_jit, dolfin_pc
 
 
@@ -66,7 +66,8 @@ def compile_cpp_code(cpp_code):
 
     params['build']['cxxflags'] += tuple(dmacros)
 
-    module_hash = hashlib.md5(cpp_code.encode('utf-8')).hexdigest()
+    hash_str = cpp_code + cpp.__version__
+    module_hash = hashlib.md5(hash_str.encode('utf-8')).hexdigest()
     module_name = "dolfin_cpp_module_" + module_hash
 
     module, signature = dijitso_jit(cpp_code, module_name, params,
