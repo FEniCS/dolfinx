b5ccddff99b4599044cd86bd15d1f780f5a5d962
Garth N. Wells <gnw20@cam.ac.uk>
2018-06-28 13:21:33 +0100

    Re-enable null space tests (#109)
    * Add nullspace test to VectorSpaceBasis.
    
    * Enable nullspace tests.
    
    * flake8 fix for nullspace tests.

--- cpp/dolfin/la/VectorSpaceBasis.cpp
+++ cpp/dolfin/la/VectorSpaceBasis.cpp
@@ -5,6 +5,7 @@
 // SPDX-License-Identifier:    LGPL-3.0-or-later
 
 #include "VectorSpaceBasis.h"
+#include "PETScMatrix.h"
 #include "PETScVector.h"
 #include <cmath>
 #include <dolfin/common/constants.h>
@@ -34,8 +35,8 @@ void VectorSpaceBasis::orthonormalize(double tol)
     }
 
     // Normalise basis function
-   const PetscReal norm = _basis[i]->normalize();
-    if (norm  < tol)
+    const PetscReal norm = _basis[i]->normalize();
+    if (norm < tol)
     {
       throw std::runtime_error(
           "VectorSpaceBasis has linear dependency. Cannot orthogonalize");
@@ -81,6 +82,20 @@ bool VectorSpaceBasis::is_orthogonal(double tol) const
   return true;
 }
 //-----------------------------------------------------------------------------
+bool VectorSpaceBasis::in_nullspace(const PETScMatrix& A, double tol) const
+{
+  PETScVector y = A.init_vector(0);
+  for (auto x : _basis)
+  {
+    A.mult(*x, y);
+    const double norm = y.norm(la::Norm::l2);
+    if (norm > tol)
+      return false;
+  }
+
+  return true;
+}
+//-----------------------------------------------------------------------------
 void VectorSpaceBasis::orthogonalize(PETScVector& x) const
 {
   for (std::size_t i = 0; i < _basis.size(); i++)
@@ -93,7 +108,7 @@ void VectorSpaceBasis::orthogonalize(PETScVector& x) const
 //-----------------------------------------------------------------------------
 std::size_t VectorSpaceBasis::dim() const { return _basis.size(); }
 //-----------------------------------------------------------------------------
-std::shared_ptr<const PETScVector> VectorSpaceBasis::
+std::shared_ptr<const la::PETScVector> VectorSpaceBasis::
 operator[](std::size_t i) const
 {
   assert(i < _basis.size());
