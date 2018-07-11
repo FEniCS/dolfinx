10147fd7603758aa1171a8cad28d949545001a4a
igorbaratta <igorbaratta@gmail.com>
2018-07-10 23:35:07 -0300

    change name of variables

--- python/test/unit/fem/test_complex_assembler.py
+++ python/test/unit/fem/test_complex_assembler.py
@@ -15,7 +15,7 @@ import pytest
 @pytest.mark.skipif(not dolfin.has_petsc_complex(),
                     reason="This test only works in complex mode.")
 def test_complex_vector_assembly():
-    """Test assembly of complex scalars"""
+    """Test assembly of complex vectors"""
 
     mesh = dolfin.generation.UnitSquareMesh(dolfin.MPI.comm_world, 32, 31)
     P2 = ufl.FiniteElement("Lagrange", mesh.ufl_cell(), 2)
@@ -28,23 +28,23 @@ def test_complex_vector_assembly():
     zero = dolfin.function.constant.Constant(0.0j)
 
     a = zero * inner(u, v) * dx
-    L1 = inner(g, v) * dx
-    assembler = dolfin.fem.assembling.Assembler(a, L1)
-    A0, b0 = assembler.assemble()
+    L0 = inner(g, v) * dx
+    assembler = dolfin.fem.assembling.Assembler(a, L0)
+    A, b0 = assembler.assemble()
     bnorm = b0.norm(dolfin.cpp.la.Norm.l1)
     b_norm_ref = abs(-2 + 3.0j)
     assert (np.isclose(bnorm, b_norm_ref))
 
     f = dolfin.Expression("j*sin(2*pi*x[0])", degree=2)
-    L0 = inner(f, v) * dx
-    assembler = dolfin.fem.assembling.Assembler(a, L0)
-    A1, b1 = assembler.assemble()
+    L1 = inner(f, v) * dx
+    assembler = dolfin.fem.assembling.Assembler(a, L1)
+    A, b1 = assembler.assemble()
     b1_norm = b1.norm(dolfin.cpp.la.Norm.l2)
 
     f = dolfin.Expression("sin(2*pi*x[0])", degree=2)
     L2 = inner(f, v) * dx
     assembler = dolfin.fem.assembling.Assembler(a, L2)
-    A2, b2 = assembler.assemble()
+    A, b2 = assembler.assemble()
     b2_norm = b1.norm(dolfin.cpp.la.Norm.l2)
 
     b1_inf = b1.norm(dolfin.cpp.la.Norm.linf)
