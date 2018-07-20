b8ad349fa38b559d609560dbe1ecae113678a167
Michal Habera <michal.habera@gmail.com>
2018-07-20 17:15:22 +0200

    Fix mesh test

--- python/test/unit/mesh/test_mesh.py
+++ python/test/unit/mesh/test_mesh.py
@@ -260,8 +260,7 @@ def test_Write(cd_tempdir, f):
     f = f
     f[0] = 1
     f[1] = 2
-    file = XDMFFile(f.mesh().mpi_comm(), "saved_mesh_function.xdmf",
-                    encoding=encoding)
+    file = XDMFFile(f.mesh().mpi_comm(), "saved_mesh_function.xdmf")
     file.write(f)
 
 
