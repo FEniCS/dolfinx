fab3196b827adf0e8bb5520e5a7a2a7945fb90c9
Fabian Löschner <fabian.loeschner@rwth-aachen.de>
2018-07-27 23:40:22 +0200

    Simplified batch assembly test

--- python/test/unit/fem/test_batch_assembly.py
+++ python/test/unit/fem/test_batch_assembly.py
@@ -9,59 +9,36 @@ import numpy
 
 import dolfin
 from dolfin import DOLFIN_EPS, MPI
-from dolfin.jit.jit import ffc_jit
-from dolfin.la import PETScMatrix, PETScVector
 
 import ufl
-from ufl import dot, grad, dx
+from ufl import inner, grad, dx
 
 
 def test_batch_assembly():
     def assemble_test(cell_batch_size: int):
         mesh = dolfin.UnitCubeMesh(MPI.comm_world, 2, 3, 4)
-        Q = dolfin.FunctionSpace(mesh, "Lagrange", 1)
+        element = ufl.FiniteElement("Lagrange", mesh.ufl_cell(), 1)
 
-        u = ufl.TrialFunction(Q)
-        v = ufl.TestFunction(Q)
+        Q = dolfin.function.functionspace.FunctionSpace(mesh, element)
 
-        # Define the boundary: vertices where any component is in machine precision accuracy 0 or 1
+        u = dolfin.function.argument.TrialFunction(Q)
+        v = dolfin.function.argument.TestFunction(Q)
+
         def boundary(x):
             return numpy.sum(numpy.logical_or(x < DOLFIN_EPS, x > 1.0 - DOLFIN_EPS), axis=1) > 0
 
-        u0 = dolfin.Constant(0.0)
-        bc = dolfin.DirichletBC(Q, u0, boundary)
+        u_bc = dolfin.function.constant.Constant(50.0)
+        bc = dolfin.fem.dirichletbc.DirichletBC(Q, u_bc, boundary)
 
-        # Initialize bilinear form and rhs
-        a = dolfin.cpp.fem.Form([Q._cpp_object, Q._cpp_object])
-        L = dolfin.cpp.fem.Form([Q._cpp_object])
+        f = dolfin.function.constant.Constant(1.0)
 
-        print("Form compilation...")
+        a = inner(grad(u), grad(v)) * dx
+        L = f * v * dx
 
-        # Bilinear form
-        jit_result = ffc_jit(dot(grad(u), grad(v)) * dx,
-                             form_compiler_parameters={"cell_batch_size": cell_batch_size,
-                                                       "enable_cross_element_gcc_ext": True})
-        ufc_form = dolfin.cpp.fem.make_ufc_form(jit_result[0])
-        a = dolfin.cpp.fem.Form(ufc_form, [Q._cpp_object, Q._cpp_object])
+        # Create assembler
+        assembler = dolfin.fem.assembling.Assembler([[a]], [L], [bc])
 
-        # Rhs
-        f = dolfin.Expression("2.0", element=Q.ufl_element())
-        jit_result = ffc_jit(f * v * dx,
-                             form_compiler_parameters={"cell_batch_size": cell_batch_size,
-                                                       "enable_cross_element_gcc_ext": True})
-        ufc_form = dolfin.cpp.fem.make_ufc_form(jit_result[0])
-        L = dolfin.cpp.fem.Form(ufc_form, [Q._cpp_object])
-        # Attach rhs expression as coefficient
-        L.set_coefficient(0, f._cpp_object)
-
-        assembler = dolfin.cpp.fem.Assembler([[a]], [L], [bc])
-
-        A = PETScMatrix()
-        b = PETScVector()
-
-        print("Running assembly...")
-        assembler.assemble(A, dolfin.cpp.fem.Assembler.BlockType.monolithic)
-        assembler.assemble(b, dolfin.cpp.fem.Assembler.BlockType.monolithic)
+        A, b = assembler.assemble(mat_type=dolfin.cpp.fem.Assembler.BlockType.monolithic)
 
         return A, b
 
@@ -76,3 +53,5 @@ def test_batch_assembly():
 
     assert(numpy.isclose(A1norm, A4norm))
     assert(numpy.isclose(b1norm, b4norm))
+
+    print(A1norm)
