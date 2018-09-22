16581fb347340053efe4894a44efef6b00bff745
Michal Habera <michal.habera@gmail.com>
2018-09-22 22:53:24 +0200

    Fix leftover cpp_object

--- python/dolfin/fem/solving.py
+++ python/dolfin/fem/solving.py
@@ -198,7 +198,7 @@ def _solve_varproblem(*args, **kwargs):
         assembler = SystemAssembler(a._cpp_object, L._cpp_object, bcs)
         assembler.assemble(A, b)
 
-        comm = L.mesh().mpi_comm()
+        comm = L._cpp_object.mesh().mpi_comm()
         solver = PETScKrylovSolver(comm)
 
         solver.set_options_prefix("dolfin_solve_")
