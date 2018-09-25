e3e63e5725eb9465ae1d98e4e32e4e8d470d521a
Garth N. Wells <gnw20@cam.ac.uk>
2018-09-25 20:10:11 +0100

    Remove some old scripts.

--- python/test/unit/scripts/clean.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#rm -rf `find -name test_*_tempdir`
-#rm -rf `find -name __pycache__`
-git clean -fdx .
--- python/test/unit/scripts/mpirun.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# NB! Run as e.g.
-#  cd test/unit/python
-#  ./scripts/mpirun.sh <pytest args>
-#
-./scripts/clean.sh
-mpirun -n 3 python -B -m pytest -v $@
