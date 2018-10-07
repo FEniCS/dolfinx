8150a56723e87d316085300ccdea8cf788a5557a
Garth N. Wells <gnw20@cam.ac.uk>
2018-10-07 15:52:09 +0100

    Clean up some docstrings.

--- python/dolfin/la/solver.py
+++ python/dolfin/la/solver.py
@@ -27,31 +27,6 @@ def solve(A, x, b, method="default", preconditioner="default"):
     on which linear algebra backend is used and how that has been
     configured.
 
-    To list all available LU methods, run the following command:
-
-    .. code-block:: python
-
-        list_lu_solver_methods()
-
-    To list all available Krylov methods, run the following command:
-
-    .. code-block:: python
-
-        list_krylov_solver_methods()
-
-    To list all available preconditioners, run the following command:
-
-    .. code-block:: python
-
-        list_krylov_solver_preconditioners()
-
-    To list all available solver methods, including LU methods, Krylov
-    methods and, possibly, other methods, run the following command:
-
-    .. code-block:: python
-
-        list_linear_solver_methods()
-
     """
 
     solver = cpp.la.PETScKrylovSolver(cpp.MPI.comm_world)
