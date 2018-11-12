e9cbba013d0685b5cdaa645abfd53352275ddf9b
Chris Richardson <chris@bpi.cam.ac.uk>
2018-11-12 16:56:52 +0000

    Simplify and update test script

--- python/demo/test.py
+++ python/demo/test.py
@@ -16,57 +16,22 @@ for subdir in ['documented', 'undocumented']:
     for f in demo_files:
         demos.append((f.parent, f.name))
 
+@pytest.mark.parametrize("path,name", demos)
+def test_demos(path, name):
 
-# FIXME: fix cases that break pattern
-# Remove 'tensor-weighted-poisson'
-demos = [d for d in demos if d[0].stem != 'tensor-weighted-poisson']
-
-# Testing
-#demos = [d for d in demos if d[0].stem == 'stokes-taylor-hood']
-#print("------------------------------")
-#print(demos)
+    # Run in serial
+    ret = subprocess.run([sys.executable, name],
+                         cwd=str(path),
+                         env={**os.environ, 'MPLBACKEND': 'agg', 'DOLFIN_TEST': '1'},
+                         check=True)
 
 
 @pytest.mark.parametrize("path,name", demos)
-def test_demos(mpiexec, num_proc, path, name):
+def test_demos_mpi(path, name):
 
-    if mpiexec is None:
-        # Run in serial
-        ret = subprocess.run([sys.executable, name],
-                             cwd=str(path),
-                             env={**os.environ, 'MPLBACKEND': 'agg'},
-                             check=True)
-    else:
-        # Run with MPI
-
-        # FIXME: non-MPI demos should exit gracefully
-        # Demos that don't work in parallel
-        broken = ["demo_subdomains.py",
-                  "demo_auto-adaptive-poisson.py",
-                  "demo_nonmatching-interpolation.py"
-                  'demo_adaptive-poisson,py',
-                  'demo_auto-adaptive-navier-stokes.py',
-                  'demo_eval.py',
-                  'demo_extrapolation.py',
-                  'demo_nonmatching-interpolation.py',
-                  'demo_nonmatching-projection.py',
-                  'demo_poisson-disc.py',
-                  'demo_smoothing.py',
-                  'demo_subdomains.py',
-                  'demo_submesh.py',
-                  'demo_time-series.py',
-                  'demo_poisson1D-in-2D.py',
-                  'demo_coordinates.py',
-        ]
-        if name in broken:
-            return
-
-        assert int(num_proc) > 0 and int(num_proc) < 32
-        cmd = mpiexec + " -np " + str(num_proc) + " " + sys.executable + " " + name
-        ret = subprocess.run(cmd,
-                             cwd=str(path),
-                             shell=True,
-                             #stdin=subprocess.DEVNULL,
-                             env={**os.environ, 'MPLBACKEND': 'agg'},
-                             check=True
-        )
+    # Run in parallel
+    cmd = ["mpirun", "-np", "3", sys.executable, name]
+    ret = subprocess.run(cmd,
+                         cwd=str(path),
+                         env={**os.environ, 'MPLBACKEND': 'agg', 'DOLFIN_TEST': '1'},
+                         check=True)
