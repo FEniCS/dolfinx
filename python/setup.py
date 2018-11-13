91e8686251ac0c5592d7ecc02bbc1225e73cbcb5
Jack S. Hale <mail@jackhale.co.uk>
2018-11-13 15:07:56 +0100

    The MPI casters seem optional, remove previous commit.

--- python/setup.py
+++ python/setup.py
@@ -19,7 +19,6 @@ RESTRICT_REQUIREMENTS = ">=2018.2.0.dev0,<2018.3"
 REQUIREMENTS = [
     "numpy",
     "numba",
-    "mpi4py",
     "fenics-ffc{}".format(RESTRICT_REQUIREMENTS),
     "fenics-ufl{}".format(RESTRICT_REQUIREMENTS),
     "fenics-dijitso{}".format(RESTRICT_REQUIREMENTS),
