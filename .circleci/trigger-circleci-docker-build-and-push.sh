80fc69eabbe798b55eadf38b2a68ca283010c085
Jack S. Hale <mail@jackhale.co.uk>
2018-11-16 14:54:12 +0100

    Wrong branch.

--- .circleci/trigger-circleci-docker-build-and-push.sh
+++ .circleci/trigger-circleci-docker-build-and-push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 curl -u ${CIRCLE_API_USER_TOKEN} \
      -d build_parameters[CIRCLE_JOB]=build-and-push-docker-images \
-     https://circleci.com/api/v1.1/project/github/FEniCS/dolfinx/tree/jhale/build-docker-images-on-circle-ci
+     https://circleci.com/api/v1.1/project/github/FEniCS/dolfinx/tree/master
