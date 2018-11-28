cefb54eee10d3a1f5a4f57f9a6bdab557324908e
Garth N. Wells <gnw20@cam.ac.uk>
2018-11-28 15:17:04 +0000

    Clean-up nls test.

--- python/test/unit/nls/test_newton.py
+++ python/test/unit/nls/test_newton.py
@@ -14,55 +14,61 @@ import ufl
 from ufl import derivative, dx, grad, inner
 
 
+class NonlinearPDEProblem(dolfin.cpp.nls.NonlinearProblem):
+    """Nonlinear problem class for a PDE problem."""
+
+    def __init__(self, F, u, bc):
+        super().__init__()
+        V = u.function_space()
+        du = function.TrialFunction(V)
+        self.L = F
+        self.a = derivative(F, u, du)
+        self.bc = bc
+        self._F, self._J = None, None
+
+    def form(self, x):
+        pass
+
+    def F(self, x):
+        """Assemble residual vector."""
+        if self._F is None:
+            self._F = fem.assemble_vector([self.L], [[self.a]], [self.bc],
+                                          dolfin.cpp.fem.BlockType.monolithic,
+                                          x)
+        else:
+            self._F = fem.assemble(self._F, self.L, [self.a], [self.bc], x)
+        return self._F
+
+    def J(self, x):
+        """Assemble Jacobian matrix."""
+        if self._J is None:
+            self._J = fem.assemble_matrix([[self.a]], [self.bc],
+                                          dolfin.cpp.fem.BlockType.monolithic)
+        else:
+            self._J = fem.assemble(self._J, self.a, [self.bc])
+        return self._J
+
+
 def test_linear_pde():
-    class PoissonEquation(dolfin.cpp.nls.NonlinearProblem):
-        """Nonlinear problem class for the linear Poisson problem."""
-
-        def __init__(self, u):
-            super().__init__()
-            V = u.function_space()
-            du = function.TrialFunction(V)
-            v = function.TestFunction(V)
-            self.L = inner(10.0, v) * dx - inner(grad(u), grad(v)) * dx
-            self.a = derivative(self.L, u, du)
-
-            def boundary(x):
-                """Define Dirichlet boundary (x = 0 or x = 1)."""
-                return np.logical_or(x[:, 0] < 1.0e-8, x[:, 0] > 1.0 - 1.0e-8)
-
-            u_bc = function.Function(V)
-            u_bc.vector().set(1.0)
-            u_bc.vector().update_ghosts()
-            self.bc = fem.DirichletBC(V, u_bc, boundary)
-
-            self._F, self._J = None, None
-
-        def F(self, x):
-            """Assemble residual vector."""
-            if self._F is None:
-                self._F = fem.assemble_vector(
-                    [self.L], [[self.a]], [self.bc],
-                    dolfin.cpp.fem.BlockType.monolithic, x)
-            else:
-                self._F = fem.assemble(self._F, self.L, [self.a], [self.bc], x)
-            return self._F
-
-        def J(self, x):
-            """Assemble Jacobian matrix."""
-            if self._J is None:
-                self._J = fem.assemble_matrix(
-                    [[self.a]], [self.bc], dolfin.cpp.fem.BlockType.monolithic)
-            else:
-                self._J = fem.assemble(self._J, self.a, [self.bc])
-            return self._J
-
+    """Test Newton solver for a linear PDE"""
     # Create mesh and function space
     mesh = dolfin.generation.UnitSquareMesh(dolfin.MPI.comm_world, 12, 12)
     V = dolfin.function.FunctionSpace(mesh, ("Lagrange", 1))
-
-    # Create solution function and nonlinear problem
     u = dolfin.function.Function(V)
-    problem = PoissonEquation(u)
+    v = function.TestFunction(V)
+    F = inner(10.0, v) * dx - inner(grad(u), grad(v)) * dx
+
+    def boundary(x):
+        """Define Dirichlet boundary (x = 0 or x = 1)."""
+        return np.logical_or(x[:, 0] < 1.0e-8, x[:, 0] > 1.0 - 1.0e-8)
+
+    u_bc = function.Function(V)
+    u_bc.vector().set(1.0)
+    u_bc.vector().update_ghosts()
+    bc = fem.DirichletBC(V, u_bc, boundary)
+
+    # Create nonlinear problem
+    problem = NonlinearPDEProblem(F, u, bc)
 
     # Create Newton solver and solve
     solver = dolfin.cpp.nls.NewtonSolver(dolfin.MPI.comm_world)
@@ -72,61 +78,31 @@ def test_linear_pde():
 
 
 def test_nonlinear_pde():
-    class PoissonEquation(dolfin.cpp.nls.NonlinearProblem):
-        """Nonlinear problem class for the linear Poisson problem."""
-
-        def __init__(self, u):
-            super().__init__()
-            V = u.function_space()
-            du = function.TrialFunction(V)
-            v = function.TestFunction(V)
-            self.L = inner(2.0, v) * dx - ufl.sqrt(
-                u * u) * inner(grad(u), grad(v)) * dx - inner(u, v) * dx
-            self.a = derivative(self.L, u, du)
-
-            def boundary(x):
-                """Define Dirichlet boundary (x = 0 or x = 1)."""
-                return np.logical_or(x[:, 0] < 1.0e-8, x[:, 0] > 1.0 - 1.0e-8)
-
-            u_bc = function.Function(V)
-            u_bc.vector().set(1.0)
-            u_bc.vector().update_ghosts()
-            self.bc = fem.DirichletBC(V, u_bc, boundary)
-
-            self._F, self._J = None, None
-
-        def form(self, x):
-            pass
-
-        def F(self, x):
-            """Assemble residual vector."""
-            if self._F is None:
-                self._F = fem.assemble_vector(
-                    [self.L], [[self.a]], [self.bc],
-                    dolfin.cpp.fem.BlockType.monolithic, x)
-            else:
-                self._F = fem.assemble(self._F, self.L, [self.a], [self.bc], x)
-            return self._F
-
-        def J(self, x):
-            """Assemble Jacobian matrix."""
-            if self._J is None:
-                self._J = fem.assemble_matrix(
-                    [[self.a]], [self.bc], dolfin.cpp.fem.BlockType.monolithic)
-            else:
-                self._J = fem.assemble(self._J, self.a, [self.bc])
-            return self._J
-
+    """Test Newton solver for a simple nonlinear PDE"""
     # Create mesh and function space
-    mesh = dolfin.generation.UnitSquareMesh(dolfin.MPI.comm_world, 12, 12)
+    mesh = dolfin.generation.UnitSquareMesh(dolfin.MPI.comm_world, 12, 15)
     V = dolfin.function.FunctionSpace(mesh, ("Lagrange", 1))
-
-    # Create solution function and nonlinear problem
     u = dolfin.function.Function(V)
-    u.vector().set(0.9)
-    problem = PoissonEquation(u)
+    v = function.TestFunction(V)
+    F = inner(2.0, v) * dx - ufl.sqrt(u * u) * inner(
+        grad(u), grad(v)) * dx - inner(u, v) * dx
+
+    def boundary(x):
+        """Define Dirichlet boundary (x = 0 or x = 1)."""
+        return np.logical_or(x[:, 0] < 1.0e-8, x[:, 0] > 1.0 - 1.0e-8)
+
+    u_bc = function.Function(V)
+    u_bc.vector().set(1.0)
+    u_bc.vector().update_ghosts()
+    bc = fem.DirichletBC(V, u_bc, boundary)
+
+    # Create nonlinear problem
+    problem = NonlinearPDEProblem(F, u, bc)
+
 
     # Create Newton solver and solve
+    u.vector().set(0.9)
+    u.vector().update_ghosts()
     solver = dolfin.cpp.nls.NewtonSolver(dolfin.MPI.comm_world)
     n, converged = solver.solve(problem, u.vector())
     assert converged
