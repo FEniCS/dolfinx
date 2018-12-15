eddbb0064db605500906599681d58e3e1ff16022
Garth N. Wells <gnw20@cam.ac.uk>
2018-12-15 17:22:24 +0000

    Update for pybind11 interface.

--- cpp/dolfin/nls/NonlinearProblem.h
+++ cpp/dolfin/nls/NonlinearProblem.h
@@ -32,6 +32,8 @@ public:
 
   /// Function called by Newton solver before requesting F, J or J_pc.
   /// This can be used to compute F, J and J_pc together.
+  /// Note: the vector x is not const as this function is commonly used
+  /// to update ghost entries before assembly.
   virtual void form(la::PETScVector& x)
   {
     // Do nothing if not supplied by the user
