2a8e373766084b12fa4f5d7c3453020993f18738
Garth N. Wells <gnw20@cam.ac.uk>
2018-12-20 12:16:15 +0000

    Enable more assembly tests.

--- python/test/unit/fem/test_assembler.py
+++ python/test/unit/fem/test_assembler.py
@@ -116,28 +116,14 @@ def test_matrix_assembly_block():
     bnorm1 = math.sqrt(sum([x.norm()**2 for x in b1.vec().getNestSubVecs()]))
     assert bnorm0 == pytest.approx(bnorm1, 1.0e-12)
 
-    try:
-        Anorm1 = 0.0
-        nrows, ncols = A1.mat().getNestSize()
-        for row in range(nrows):
-            for col in range(ncols):
-                A_sub = A1.mat().getNestSubMatrix(row, col)
-                norm = A_sub.norm()
-                Anorm1 += norm * norm
-                # A_sub.view()
-
-        # is_rows, is_cols = A1.mat().getNestLocalISs()
-        # for is0 in is_rows:
-        #     for is1 in is_cols:
-        #         A_sub = A1.mat().getLocalSubMatrix(is0, is1)
-        #         norm = A_sub.norm()
-        #         Anorm1 += norm * norm
-
-        Anorm1 = math.sqrt(Anorm1)
-        assert Anorm0 == pytest.approx(Anorm1, 1.0e-12)
-
-    except AttributeError:
-        print("Recent petsc4py(-dev) required to get MatNest sub-matrix.")
+    Anorm1 = 0.0
+    nrows, ncols = A1.mat().getNestSize()
+    for row in range(nrows):
+        for col in range(ncols):
+            A_sub = A1.mat().getNestSubMatrix(row, col)
+            norm = A_sub.norm()
+            Anorm1 += norm * norm
+    Anorm1 = math.sqrt(Anorm1)
 
     # Monolithic version
     E = P0 * P1
@@ -179,12 +165,14 @@ def test_assembly_solve_block():
 
     u_bc0 = dolfin.function.Function(V0)
     u_bc0.vector().set(50.0)
-
+    u_bc0.vector().update_ghosts()
     u_bc1 = dolfin.function.Function(V1)
     u_bc1.vector().set(20.0)
-
-    bc0 = dolfin.fem.dirichletbc.DirichletBC(V0, u_bc0, boundary)
-    bc1 = dolfin.fem.dirichletbc.DirichletBC(V1, u_bc1, boundary)
+    u_bc1.vector().update_ghosts()
+    bcs = [
+        dolfin.fem.dirichletbc.DirichletBC(V0, u_bc0, boundary),
+        dolfin.fem.dirichletbc.DirichletBC(V1, u_bc1, boundary)
+    ]
 
     # Variational problem
     u, p = dolfin.function.argument.TrialFunction(
@@ -206,10 +194,9 @@ def test_assembly_solve_block():
         pass
         # print("Norm:", its, rnorm)
 
-    A0 = dolfin.fem.assemble_matrix([[a00, a01], [a10, a11]], [bc0, bc1],
+    A0 = dolfin.fem.assemble_matrix([[a00, a01], [a10, a11]], bcs,
                                     dolfin.cpp.fem.BlockType.monolithic)
-    b0 = dolfin.fem.assemble_vector([L0, L1], [[a00, a01], [a10, a11]],
-                                    [bc0, bc1],
+    b0 = dolfin.fem.assemble_vector([L0, L1], [[a00, a01], [a10, a11]], bcs,
                                     dolfin.cpp.fem.BlockType.monolithic)
     A0norm = A0.mat().norm()
     b0norm = b0.vec().norm()
@@ -219,17 +206,15 @@ def test_assembly_solve_block():
     ksp.setOperators(A0.mat())
     ksp.setMonitor(monitor)
     ksp.setType('cg')
-    #ksp.getPC().setType('jacobi')
     ksp.setTolerances(rtol=1.0e-14)
     ksp.setFromOptions()
     ksp.solve(b0.vec(), x0)
     x0norm = x0.norm()
 
     # Nested (MatNest)
-    A1 = dolfin.fem.assemble_matrix([[a00, a01], [a10, a11]], [bc0, bc1],
+    A1 = dolfin.fem.assemble_matrix([[a00, a01], [a10, a11]], bcs,
                                     dolfin.cpp.fem.BlockType.nested)
-    b1 = dolfin.fem.assemble_vector([L0, L1], [[a00, a01], [a10, a11]],
-                                    [bc0, bc1],
+    b1 = dolfin.fem.assemble_vector([L0, L1], [[a00, a01], [a10, a11]], bcs,
                                     dolfin.cpp.fem.BlockType.nested)
     b1norm = b1.vec().norm()
     assert b1norm == pytest.approx(b0norm, 1.0e-12)
@@ -249,10 +234,8 @@ def test_assembly_solve_block():
     ksp.setMonitor(monitor)
     ksp.setOperators(A1.mat())
     ksp.setType('cg')
-    # ksp.getPC().setType('jacobi')
-    ksp.setTolerances(rtol=1.0e-14)
+    ksp.setTolerances(rtol=1.0e-12)
     ksp.setFromOptions()
-    # ksp.view()
     ksp.solve(b1.vec(), x1.vec())
     x1norm = x1.vec().norm()
     assert x1norm == pytest.approx(x0norm, rel=1.0e-12)
@@ -288,8 +271,8 @@ def test_assembly_solve_block():
 
     A2norm = A2.mat().norm()
     b2norm = b2.vec().norm()
-    assert A2norm == pytest.approx(A0norm, 1.0e-10)
-    assert b2norm == pytest.approx(b0norm, 1.0e-10)
+    assert A2norm == pytest.approx(A0norm, 1.0e-12)
+    assert b2norm == pytest.approx(b0norm, 1.0e-12)
 
     x2 = dolfin.cpp.la.PETScVector(b2)
     ksp = PETSc.KSP()
@@ -302,13 +285,9 @@ def test_assembly_solve_block():
     ksp.setFromOptions()
     ksp.solve(b2.vec(), x2.vec())
     x2norm = x2.vec().norm()
-    #print(x0norm)
-    # print(x2norm)
-    #assert x2norm == pytest.approx(x0norm, 1.0e-6)
-    # 459.3351568043951
+    assert x2norm == pytest.approx(x0norm, 1.0e-10)
 
-
-    # # Old assembler (reference)
+    # # Old assembler (reference solution)
     A3, b3 = dolfin.fem.assembling.assemble_system(a, L, bcs)
     x3 = dolfin.cpp.la.PETScVector(b3)
     ksp = PETSc.KSP()
@@ -320,10 +299,4 @@ def test_assembly_solve_block():
     ksp.setFromOptions()
     ksp.solve(b3.vec(), x3.vec())
     x3norm = x3.vec().norm()
-    # assert x3norm == pytest.approx(x0norm, 1.0e-8)
-
-    if dolfin.MPI.rank(dolfin.MPI.comm_world) == 0:
-        print(x0norm)
-        print(x1norm)
-        print(x2norm)
-        print(x3norm)
+    assert x3norm == pytest.approx(x0norm, 1.0e-10)
