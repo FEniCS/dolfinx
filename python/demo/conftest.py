9ec3c98e39179313b3c682cfd72f053149d82bf7
Garth N. Wells <gnw20@cam.ac.uk>
2019-01-05 22:00:26 +0000

    Changes for passing MPI options.

--- python/demo/conftest.py
+++ python/demo/conftest.py
@@ -9,11 +9,18 @@ def pytest_addoption(parser):
                      help="Name of program to run MPI, e.g. mpiexex")
     parser.addoption("--num-proc", action="store", default=1,
                      help="Number of MPI processes to use")
+    parser.addoption("--mpi-options", action="store",
+                     help="Additional MPI options to use")
 
 
 @pytest.fixture
 def mpiexec(request):
     return request.config.getoption("--mpiexec")
+
+
+@pytest.fixture
+def mpioptions(request):
+    return request.config.getoption("--mpi-options")
 
 
 @pytest.fixture
