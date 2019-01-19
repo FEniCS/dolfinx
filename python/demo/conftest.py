3bce7877ccb462e1bac6f3dd3e8eb36540b16191
Garth N. Wells <gnw20@cam.ac.uk>
2019-01-19 21:11:56 +0000

    Remove MPI options for pyetst demos.

--- python/demo/conftest.py
+++ python/demo/conftest.py
@@ -9,18 +9,11 @@ def pytest_addoption(parser):
                      help="Name of program to run MPI, e.g. mpiexex")
     parser.addoption("--num-proc", action="store", default=1,
                      help="Number of MPI processes to use")
-    parser.addoption("--mpi-options", action="store",
-                     help="Additional MPI options to use")
 
 
 @pytest.fixture
 def mpiexec(request):
     return request.config.getoption("--mpiexec")
-
-
-@pytest.fixture
-def mpioptions(request):
-    return request.config.getoption("--mpi-options")
 
 
 @pytest.fixture
