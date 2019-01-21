94ef41dc37f82260547bc31780e9a52b7812d04b
Garth N. Wells <gnw20@cam.ac.uk>
2019-01-21 17:02:04 +0000

    Flatten layout of demo directories.

--- python/demo/test.py
+++ python/demo/test.py
@@ -13,15 +13,13 @@ import sys
 import pytest
 
 # Get directory of this file
-dir_path = pathlib.Path(__file__).resolve().parent
+path = pathlib.Path(__file__).resolve().parent
 
 # Build list of demo programs
 demos = []
-for subdir in ['documented', 'undocumented']:
-    p = pathlib.Path(dir_path, subdir)
-    demo_files = list(p.glob('**/*.py'))
-    for f in demo_files:
-        demos.append((f.parent, f.name))
+demo_files = list(path.glob('**/*.py'))
+for f in demo_files:
+    demos.append((f.parent, f.name))
 
 
 @pytest.mark.serial
