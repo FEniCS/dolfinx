e6f81bec74fac0050aeeddc8aceec7601b640da6
Garth N. Wells <gnw20@cam.ac.uk>
2019-01-25 16:52:52 +0000

    Update poisson demo (cpp).

--- cpp/demo/poisson/main.cpp
+++ cpp/demo/poisson/main.cpp
@@ -1,89 +1,89 @@
 // Poisson equation (C++)
 // ======================
-// 
+//
 // This demo illustrates how to:
-// 
+//
 // * Solve a linear partial differential equation
 // * Create and apply Dirichlet boundary conditions
 // * Define Expressions
 // * Define a FunctionSpace
 // * Create a SubDomain
-// 
+//
 // The solution for :math:`u` in this demo will look as follows:
-// 
+//
 // .. image:: ../poisson_u.png
 //     :scale: 75 %
-// 
-// 
+//
+//
 // Equation and problem definition
 // -------------------------------
-// 
+//
 // The Poisson equation is the canonical elliptic partial differential
 // equation.  For a domain :math:`\Omega \subset \mathbb{R}^n` with
 // boundary :math:`\partial \Omega = \Gamma_{D} \cup \Gamma_{N}`, the
 // Poisson equation with particular boundary conditions reads:
-// 
+//
 // .. math::
 //    - \nabla^{2} u &= f \quad {\rm in} \ \Omega, \\
 //      u &= 0 \quad {\rm on} \ \Gamma_{D}, \\
 //      \nabla u \cdot n &= g \quad {\rm on} \ \Gamma_{N}. \\
-// 
+//
 // Here, :math:`f` and :math:`g` are input data and :math:`n` denotes the
 // outward directed boundary normal. The most standard variational form
 // of Poisson equation reads: find :math:`u \in V` such that
-// 
+//
 // .. math::
 //    a(u, v) = L(v) \quad \forall \ v \in V,
-// 
+//
 // where :math:`V` is a suitable function space and
-// 
+//
 // .. math::
 //    a(u, v) &= \int_{\Omega} \nabla u \cdot \nabla v \, {\rm d} x, \\
 //    L(v)    &= \int_{\Omega} f v \, {\rm d} x
 //    + \int_{\Gamma_{N}} g v \, {\rm d} s.
-// 
+//
 // The expression :math:`a(u, v)` is the bilinear form and :math:`L(v)`
 // is the linear form. It is assumed that all functions in :math:`V`
 // satisfy the Dirichlet boundary conditions (:math:`u = 0 \ {\rm on} \
 // \Gamma_{D}`).
-// 
+//
 // In this demo, we shall consider the following definitions of the input
 // functions, the domain, and the boundaries:
-// 
+//
 // * :math:`\Omega = [0,1] \times [0,1]` (a unit square)
 // * :math:`\Gamma_{D} = \{(0, y) \cup (1, y) \subset \partial \Omega\}` (Dirichlet boundary)
 // * :math:`\Gamma_{N} = \{(x, 0) \cup (x, 1) \subset \partial \Omega\}` (Neumann boundary)
 // * :math:`g = \sin(5x)` (normal derivative)
 // * :math:`f = 10\exp(-((x - 0.5)^2 + (y - 0.5)^2) / 0.02)` (source term)
-// 
-// 
+//
+//
 // Implementation
 // --------------
-// 
+//
 // The implementation is split in two files: a form file containing the
 // definition of the variational forms expressed in UFL and a C++ file
 // containing the actual solver.
-// 
+//
 // Running this demo requires the files: :download:`main.cpp`,
 // :download:`Poisson.ufl` and :download:`CMakeLists.txt`.
-// 
-// 
+//
+//
 // UFL form file
 // ^^^^^^^^^^^^^
-// 
+//
 // The UFL file is implemented in :download:`Poisson.ufl`, and the
 // explanation of the UFL file can be found at :doc:`here <Poisson.ufl>`.
-// 
-// 
+//
+//
 // C++ program
 // ^^^^^^^^^^^
-// 
+//
 // The main solver is implemented in the :download:`main.cpp` file.
-// 
+//
 // At the top we include the DOLFIN header file and the generated header
 // file "Poisson.h" containing the variational forms for the Poisson
 // equation.  For convenience we also include the DOLFIN namespace.
-// 
+//
 // .. code-block:: cpp
 
 #include "Poisson.h"
@@ -94,7 +94,7 @@ using namespace dolfin;
 // Then follows the definition of the coefficient functions (for
 // :math:`f` and :math:`g`), which are derived from the
 // :cpp:class:`Expression` class in DOLFIN
-// 
+//
 // .. code-block:: cpp
 
 // Source term (right-hand side)
@@ -134,7 +134,7 @@ public:
 // The ``DirichletBoundary`` is derived from the :cpp:class:`SubDomain`
 // class and defines the part of the boundary to which the Dirichlet
 // boundary condition should be applied.
-// 
+//
 // .. code-block:: cpp
 
 // Sub domain for Dirichlet boundary condition
@@ -156,7 +156,7 @@ class DirichletBoundary : public mesh::SubDomain
 // order to create a mesh consisting of 32 x 32 squares with each square
 // divided into two triangles, and the finite element space (specified in
 // the form file) defined relative to this mesh, we do as follows
-// 
+//
 // .. code-block:: cpp
 
 int main(int argc, char *argv[])
@@ -184,7 +184,7 @@ int main(int argc, char *argv[])
 // is defined by the class :cpp:class:`DirichletBoundary` listed
 // above. The definition of the Dirichlet boundary condition then looks
 // as follows:
-// 
+//
 // .. code-block:: cpp
 
   // FIXME: zero function and make sure ghosts are updated
@@ -200,7 +200,7 @@ int main(int argc, char *argv[])
 // defined :cpp:class:`FunctionSpace` ``V``.  Then we can create the
 // source and boundary flux term (:math:`f`, :math:`g`) and attach these
 // to the linear form.
-// 
+//
 // .. code-block:: cpp
 
  auto form_L = std::unique_ptr<dolfin_form>(PoissonLinearForm());
@@ -237,7 +237,7 @@ int main(int argc, char *argv[])
 // initialization, it is simply set to the zero function.) Next, we can
 // call the ``solve`` function with the arguments ``a == L``, ``u`` and
 // ``bc`` as follows:
-// 
+//
 // .. code-block:: cpp
 
   // Compute solution
@@ -254,15 +254,15 @@ int main(int argc, char *argv[])
   la::PETScOptions::set("pc_type", "lu");
   lu.set_from_options();
 
-  lu.set_operator(*A);
-  lu.solve(*u.vector(), *b);
+  lu.set_operator(A->mat());
+  lu.solve(u.vector()->vec(), b->vec());
 
 
 // The function ``u`` will be modified during the call to solve. A
 // :cpp:class:`Function` can be saved to a file. Here, we output the
 // solution to a ``VTK`` file (specified using the suffix ``.pvd``) for
 // visualisation in an external program such as Paraview.
-// 
+//
 // .. code-block:: cpp
 
   // Save solution in VTK format
