6da87e0870984794b191b693d3996ac26388056b
Garth N. Wells <gnw20@cam.ac.uk>
2019-01-27 23:50:45 +0000

    Another index compare fix.

--- cpp/dolfin/function/FunctionSpace.cpp
+++ cpp/dolfin/function/FunctionSpace.cpp
@@ -158,7 +158,7 @@ void FunctionSpace::interpolate_from_any(
         = _dofmap->cell_dofs(cell.index());
 
     // Copy dofs to vector
-    for (std::size_t i = 0; i < cell_dofs.size(); ++i)
+    for (Eigen::Index i = 0; i < cell_dofs.size(); ++i)
       coeff.x[cell_dofs[i]] = cell_coefficients[i];
   }
 }
