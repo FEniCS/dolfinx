d2e29a4c2c698853aaa132d91f2c0a407373c19c
Michal Habera <michal.habera@gmail.com>
2019-03-29 23:01:58 +0100

    Add more subdomain tests

--- python/test/unit/fem/test_assemble_domains.py
+++ python/test/unit/fem/test_assemble_domains.py
@@ -11,6 +11,7 @@ import pytest
 import dolfin
 import ufl
 
+
 @pytest.fixture
 def mesh():
     return dolfin.generation.UnitSquareMesh(dolfin.MPI.comm_world, 10, 10)
@@ -22,20 +23,26 @@ def test_assembly_dx_domains(mesh):
 
     marker = dolfin.MeshFunction("size_t", mesh, mesh.topology.dim, 0)
 
-    def bottom_half(x):
+    def bottom_fifth(x):
         return numpy.less_equal(x[:, 1], 0.2)
 
-    bottom_half_subdomain = dolfin.mesh.create_subdomain(bottom_half)
-    bottom_half_subdomain.mark(marker, 111)
+    def upper_fifth(x):
+        return numpy.greater_equal(x[:, 1], 0.8)
 
-    dx = dolfin.Measure('dx', subdomain_data=marker)
+    bottom_fifth_subdomain = dolfin.mesh.create_subdomain(bottom_fifth)
+    bottom_fifth_subdomain.mark(marker, 111)
+
+    upper_fifth_subdomain = dolfin.mesh.create_subdomain(upper_fifth)
+    upper_fifth_subdomain.mark(marker, 222)
+
+    dx = dolfin.Measure('dx', subdomain_data=marker, domain=mesh)
 
     #
     # Assemble matrix
     #
 
     # Integration just over marked cells
-    a = ufl.inner(u, v) * dx(111)
+    a = ufl.inner(u, v) * dx(111) + ufl.inner(u, v) * dx(222)
 
     A = dolfin.fem.assemble_matrix(a)
     A.assemble()
@@ -44,7 +51,7 @@ def test_assembly_dx_domains(mesh):
     # Integration over whole domain, but effectively zeroing
     # with contitional
     x = ufl.SpatialCoordinate(mesh)
-    conditional_marker = ufl.conditional(x[0] <= 0.2, 1.0, 0.0)
+    conditional_marker = ufl.conditional(ufl.Or(x[0] <= 0.2, x[0] >= 0.8), 1.0, 0.0)
 
     a2 = conditional_marker * ufl.inner(u, v) * dx
 
@@ -52,34 +59,33 @@ def test_assembly_dx_domains(mesh):
     A2.assemble()
     norm2 = A2.norm()
 
-    assert numpy.isclose(norm1, norm2)
+    assert norm1 == pytest.approx(norm2, 1.0e-12)
 
     #
     # Assemble vector
     #
 
-    L = v * dx(111)
+    L = v * dx(111) + v * dx(222)
     b = dolfin.fem.assemble_vector(L)
 
     L2 = conditional_marker * v * dx
     b2 = dolfin.fem.assemble_vector(L2)
 
-    assert numpy.isclose(b.norm(), b2.norm())
+    assert b.norm() == pytest.approx(b2.norm(), 1.0e-12)
 
     #
     # Assemble scalar
     #
 
-    L = 1.0 * dx(111, domain=mesh)
+    L = 1.0 * dx(111) + 1.0 * dx(222)
     s = dolfin.fem.assemble_scalar(L)
     s = dolfin.MPI.sum(mesh.mpi_comm(), s)
 
-    L2 = conditional_marker * 1.0 * dx(domain=mesh)
+    L2 = conditional_marker * 1.0 * dx
     s2 = dolfin.fem.assemble_scalar(L2)
     s2 = dolfin.MPI.sum(mesh.mpi_comm(), s2)
 
-    print(s, s2)
-    assert numpy.isclose(s, s2) and numpy.isclose(s, 0.5)
+    assert (s == pytest.approx(s2, 1.0e-12) and 0.4 == pytest.approx(s, 1.0e-12))
 
 
 def test_assembly_ds_domains(mesh):
@@ -89,13 +95,45 @@ def test_assembly_ds_domains(mesh):
     marker = dolfin.MeshFunction("size_t", mesh, mesh.topology.dim - 1, 0)
 
     def bottom(x):
-        return numpy.isclose(x[:, 1], 10.0)
+        return numpy.isclose(x[:, 1], 0.0)
+
+    def top(x):
+        return numpy.isclose(x[:, 1], 1.0)
 
     bottom_subdomain = dolfin.mesh.create_subdomain(bottom)
-    bottom_subdomain.mark(marker, 1)
+    bottom_subdomain.mark(marker, 111)
+
+    top_subdomain = dolfin.mesh.create_subdomain(top)
+    top_subdomain.mark(marker, 222)
 
     ds = dolfin.Measure('ds', subdomain_data=marker, domain=mesh)
 
-    L = 1.0 * ds(1)
-    s = dolfin.fem.assemble_scalar(L)
-    print(s)
+    #
+    # Assemble matrix
+    #
+
+    # Integration just over marked cells
+    a = ufl.inner(u, v) * ds(111) + ufl.inner(u, v) * ds(222)
+
+    A = dolfin.fem.assemble_matrix(a)
+    A.assemble()
+    norm1 = A.norm()
+
+    # Integration over whole domain, but effectively zeroing
+    # with contitional
+    x = ufl.SpatialCoordinate(mesh)
+    conditional_marker = ufl.conditional(ufl.Or(ufl.eq(x[0], 0.0), ufl.eq(x[0], 1.0)), 1.0, 0.0)
+
+    a2 = conditional_marker * ufl.inner(u, v) * ds
+
+    A2 = dolfin.fem.assemble_matrix(a2)
+    A2.assemble()
+    norm2 = A2.norm()
+
+    assert norm1 == pytest.approx(norm2, 1.0e-12)
+
+    # L = 1.0 * ds(1)
+    # s = dolfin.fem.assemble_scalar(L)
+    # s = dolfin.MPI.sum(mesh.mpi_comm(), s)
+
+    # assert 1.0 == pytest.approx(1.0, 1.0e-12)
