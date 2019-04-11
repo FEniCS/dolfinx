fdbf35331862b68eba0fd71ca1344876904385b4
Garth N. Wells <gnw20@cam.ac.uk>
2019-04-11 10:43:18 +0100

    Add back mesh ordering for simplex meshes (#381)
    * Add back simplex ordering.
    
    * Work on simplex ordering.
    
    * Work on ordering interface.
    
    * Simplify sort.
    
    * More ordering work.
    
    * Add ordering check.
    
    * Expose order check in Python.
    
    * Fix demo include.
    
    * Fix sort by global index.
    
    * Improve ordering checks.
    
    * Simplify mesh ordering check.
    
    * Small clean up of ordering.
    
    * Add braces for readability.
    
    * Remove some debug output.

---
+++ cpp/dolfin/mesh/Ordering.h
@@ -0,0 +1,30 @@
+// Copyright (C) 2007-2008 Anders Logg
+//
+// This file is part of DOLFIN (https://www.fenicsproject.org)
+//
+// SPDX-License-Identifier:    LGPL-3.0-or-later
+
+#pragma once
+
+namespace dolfin
+{
+namespace mesh
+{
+
+class Mesh;
+
+/// This class implements the ordering of simples mesh entities
+/// according to the UFC specification.
+
+class Ordering
+{
+public:
+  /// Order mesh
+  static void order_simplex(Mesh& mesh);
+
+  /// Check if mesh is ordered
+  static bool is_ordered_simplex(const Mesh& mesh);
+};
+
+} // namespace mesh
+} // namespace dolfin
