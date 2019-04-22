9d725312b0d83aad7df1042aaf484fff86b8dbd2
Garth N. Wells <gnw20@cam.ac.uk>
2019-04-22 19:06:05 +0100

    Tidy up MeshGeometry.

--- cpp/dolfin/mesh/MeshGeometry.h
+++ cpp/dolfin/mesh/MeshGeometry.h
@@ -51,20 +51,17 @@ public:
   MeshGeometry& operator=(MeshGeometry&&) = default;
 
   /// Return Euclidean dimension of coordinate system
-  std::size_t dim() const { return _coordinates.cols(); }
+  std::size_t dim() const;
 
   /// Return the number of local points in the geometry
-  std::size_t num_points() const { return _coordinates.rows(); }
+  std::size_t num_points() const;
 
   /// Return the number of global points in the geometry
-  std::size_t num_points_global() const { return _num_points_global; }
+  std::size_t num_points_global() const;
 
   /// Return coordinate array for point with local index n
   Eigen::Ref<const Eigen::Array<double, 1, Eigen::Dynamic>>
-  x(std::size_t n) const
-  {
-    return _coordinates.row(n);
-  }
+  x(std::size_t n) const;
 
   /// Return coordinate with local index n as a 3D point value
   geometry::Point point(std::size_t n) const;
@@ -72,35 +69,21 @@ public:
   // Should this return an Eigen::Ref?
   /// Return array of coordinates for all points
   Eigen::Array<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>&
-  points()
-  {
-    return _coordinates;
-  }
+  points();
 
   // Should this return an Eigen::Ref?
   /// Return array of coordinates for all points (const version)
   const Eigen::Array<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>&
-  points() const
-  {
-    return _coordinates;
-  }
+  points() const;
 
   /// Global indices for points (const)
-  const std::vector<std::int64_t>& global_indices() const
-  {
-    return _global_indices;
-  }
+  const std::vector<std::int64_t>& global_indices() const;
 
   /// Initialise MeshGeometry data
   void init(std::uint64_t num_points_global,
             const Eigen::Array<double, Eigen::Dynamic, Eigen::Dynamic,
                                Eigen::RowMajor>& coordinates,
-            const std::vector<std::int64_t>& global_indices)
-  {
-    _num_points_global = num_points_global;
-    _coordinates = coordinates;
-    _global_indices = global_indices;
-  }
+            const std::vector<std::int64_t>& global_indices);
 
   /// Hash of coordinate values
   ///
