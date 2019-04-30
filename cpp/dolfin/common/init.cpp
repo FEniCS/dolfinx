433a854f7766f14d15b148dc5e2ddbb774eed272
Chris Richardson <chris@bpi.cam.ac.uk>
2019-04-30 11:10:49 +0100

    Revert a few minor changes

--- cpp/dolfin/common/init.cpp
+++ cpp/dolfin/common/init.cpp
@@ -6,14 +6,13 @@
 
 #include "init.h"
 #include "SubSystemsManager.h"
-#define LOGURU_WITH_STREAMS 1
 #include <dolfin/common/loguru.hpp>
 
 //-----------------------------------------------------------------------------
 void dolfin::init(int argc, char* argv[])
 {
   common::SubSystemsManager::init_logging(argc, argv);
-  LOG_S(INFO) << "Initializing DOLFIN version" << DOLFIN_VERSION;
+  LOG(INFO) << "Initializing DOLFIN version" << DOLFIN_VERSION;
   common::SubSystemsManager::init_petsc(argc, argv);
 }
 //-----------------------------------------------------------------------------
