e5d67da293448ccbebd29037e1f40a1257335c1a
Garth N. Wells <gnw20@cam.ac.uk>
2019-05-11 14:57:44 +0100

    cffi module name fix with MPI.

--- python/test/unit/fem/test_custom_assembler.py
+++ python/test/unit/fem/test_custom_assembler.py
@@ -108,9 +108,9 @@ else:
 MatSetValues_abi = petsc_lib_cffi.MatSetValuesLocal
 
 # Make MatSetValuesLocal from PETSc available via cffi in API mode
+worker = os.getenv('PYTEST_XDIST_WORKER', None)
+module_name = "_petsc_cffi_{}".format(worker)
 if dolfin.MPI.comm_world.Get_rank() == 0:
-    worker = os.getenv('PYTEST_XDIST_WORKER', None)
-    module_name = "_petsc_cffi_{}".format(worker)
     os.environ["CC"] = "mpicc"
     petsc_dir = os.environ.get('PETSC_DIR', None)
     ffibuilder = cffi.FFI()
