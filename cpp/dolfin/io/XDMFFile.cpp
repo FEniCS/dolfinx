7b588d1aa3fe8f9563973a95cb6628ff64a5fd5e
Garth N. Wells <gnw20@cam.ac.uk>
2019-05-12 19:05:35 +0100

    Add file for xdmf read utils.

--- cpp/dolfin/io/XDMFFile.cpp
+++ cpp/dolfin/io/XDMFFile.cpp
@@ -4,6 +4,7 @@
 //
 // SPDX-License-Identifier:    LGPL-3.0-or-later
 
+#include "xdmf_read.h"
 #include "xdmf_utils.h"
 #include "xdmf_write.h"
 
@@ -68,277 +69,6 @@ std::string rank_to_string(std::size_t value_rank)
   return "";
 }
 //-----------------------------------------------------------------------------
-// Remap meshfunction data, scattering data to appropriate processes
-template <typename T>
-void remap_meshfunction_data(mesh::MeshFunction<T>& meshfunction,
-                             const std::vector<std::int64_t>& topology_data,
-                             const std::vector<T>& value_data)
-{
-  // Send the read data to each process on the basis of the first
-  // vertex of the entity, since we do not know the global_index
-  const int cell_dim = meshfunction.dim();
-  const auto mesh = meshfunction.mesh();
-  // FIXME : get vertices_per_entity properly
-  const int vertices_per_entity = meshfunction.dim() + 1;
-  const MPI_Comm comm = mesh->mpi_comm();
-  const std::size_t num_processes = MPI::size(comm);
-
-  // Wrap topology data in boost array
-  assert(topology_data.size() % vertices_per_entity == 0);
-  const std::size_t num_entities = topology_data.size() / vertices_per_entity;
-
-  // Send (sorted) entity topology and data to a post-office process
-  // determined by the lowest global vertex index of the entity
-  std::vector<std::vector<std::int64_t>> send_topology(num_processes);
-  std::vector<std::vector<T>> send_values(num_processes);
-  const std::size_t max_vertex = mesh->num_entities_global(0);
-  for (std::size_t i = 0; i < num_entities; ++i)
-  {
-    std::vector<std::int64_t> cell_topology(
-        topology_data.begin() + i * vertices_per_entity,
-        topology_data.begin() + (i + 1) * vertices_per_entity);
-    std::sort(cell_topology.begin(), cell_topology.end());
-
-    // Use first vertex to decide where to send this data
-    const std::size_t destination_process
-        = MPI::index_owner(comm, cell_topology.front(), max_vertex);
-
-    send_topology[destination_process].insert(
-        send_topology[destination_process].end(), cell_topology.begin(),
-        cell_topology.end());
-    send_values[destination_process].push_back(value_data[i]);
-  }
-
-  std::vector<std::vector<std::int64_t>> receive_topology(num_processes);
-  std::vector<std::vector<T>> receive_values(num_processes);
-  MPI::all_to_all(comm, send_topology, receive_topology);
-  MPI::all_to_all(comm, send_values, receive_values);
-
-  // Generate requests for data from remote processes, based on the
-  // first vertex of the mesh::MeshEntities which belong on this process
-  // Send our process number, and our local index, so it can come back
-  // directly to the right place
-  std::vector<std::vector<std::int64_t>> send_requests(num_processes);
-  const std::size_t rank = MPI::rank(comm);
-  for (auto& cell : mesh::MeshRange<mesh::MeshEntity>(*mesh, cell_dim,
-                                                      mesh::MeshRangeType::ALL))
-  {
-    std::vector<std::int64_t> cell_topology;
-    if (cell_dim == 0)
-      cell_topology.push_back(cell.global_index());
-    else
-    {
-      for (auto& v : mesh::EntityRange<mesh::Vertex>(cell))
-        cell_topology.push_back(v.global_index());
-    }
-
-    std::sort(cell_topology.begin(), cell_topology.end());
-
-    // Use first vertex to decide where to send this request
-    std::size_t send_to_process
-        = MPI::index_owner(comm, cell_topology.front(), max_vertex);
-    // Map to this process and local index by appending to send data
-    cell_topology.push_back(cell.index());
-    cell_topology.push_back(rank);
-    send_requests[send_to_process].insert(send_requests[send_to_process].end(),
-                                          cell_topology.begin(),
-                                          cell_topology.end());
-  }
-
-  std::vector<std::vector<std::int64_t>> receive_requests(num_processes);
-  MPI::all_to_all(comm, send_requests, receive_requests);
-
-  // At this point, the data with its associated vertices is in
-  // receive_values and receive_topology and the final destinations
-  // are stored in receive_requests as
-  // [vertices][index][process][vertices][index][process]...  Some
-  // data will have more than one destination
-
-  // Create a mapping from the topology vector to the desired data
-  std::map<std::vector<std::int64_t>, T> cell_to_data;
-
-  for (std::size_t i = 0; i < receive_values.size(); ++i)
-  {
-    assert(receive_values[i].size() * vertices_per_entity
-           == receive_topology[i].size());
-    auto p = receive_topology[i].begin();
-    for (std::size_t j = 0; j < receive_values[i].size(); ++j)
-    {
-      const std::vector<std::int64_t> cell(p, p + vertices_per_entity);
-      cell_to_data.insert({cell, receive_values[i][j]});
-      p += vertices_per_entity;
-    }
-  }
-
-  // Clear vectors for reuse - now to send values and indices to final
-  // destination
-  send_topology = std::vector<std::vector<std::int64_t>>(num_processes);
-  send_values = std::vector<std::vector<T>>(num_processes);
-
-  // Go through requests, which are stacked as [vertex, vertex, ...]
-  // [index] [proc] etc.  Use the vertices as the key for the map
-  // (above) to retrieve the data to send to proc
-  for (std::size_t i = 0; i < receive_requests.size(); ++i)
-  {
-    for (auto p = receive_requests[i].begin(); p != receive_requests[i].end();
-         p += (vertices_per_entity + 2))
-    {
-      const std::vector<std::int64_t> cell(p, p + vertices_per_entity);
-      const std::size_t remote_index = *(p + vertices_per_entity);
-      const std::size_t send_to_proc = *(p + vertices_per_entity + 1);
-
-      const auto find_cell = cell_to_data.find(cell);
-      assert(find_cell != cell_to_data.end());
-      send_values[send_to_proc].push_back(find_cell->second);
-      send_topology[send_to_proc].push_back(remote_index);
-    }
-  }
-
-  MPI::all_to_all(comm, send_topology, receive_topology);
-  MPI::all_to_all(comm, send_values, receive_values);
-
-  // At this point, receive_topology should only list the local indices
-  // and received values should have the appropriate values for each
-  for (std::size_t i = 0; i < receive_values.size(); ++i)
-  {
-    assert(receive_values[i].size() == receive_topology[i].size());
-    for (std::size_t j = 0; j < receive_values[i].size(); ++j)
-      meshfunction[receive_topology[i][j]] = receive_values[i][j];
-  }
-}
-//----------------------------------------------------------------------------
-// Return data associated with a data set node
-template <typename T>
-std::vector<T> get_dataset(MPI_Comm comm, const pugi::xml_node& dataset_node,
-                           const boost::filesystem::path& parent_path,
-                           std::array<std::int64_t, 2> range = {{0, 0}})
-{
-  // FIXME: Need to sort out datasset dimensions - can't depend on
-  // HDF5 shape, and a Topology data item is not required to have a
-  // 'Dimensions' attribute since the dimensions can be determined
-  // from the number of cells and the cell type (for topology, one
-  // must supply cell type + (number of cells or dimensions).
-  //
-  // A geometry data item must have 'Dimensions' attribute.
-
-  assert(dataset_node);
-  pugi::xml_attribute format_attr = dataset_node.attribute("Format");
-  assert(format_attr);
-
-  // Get data set shape from 'Dimensions' attribute (empty if not available)
-  const std::vector<std::int64_t> shape_xml
-      = xdmf_utils::get_dataset_shape(dataset_node);
-
-  const std::string format = format_attr.as_string();
-  std::vector<T> data_vector;
-  // Only read ASCII on process 0
-  if (format == "XML")
-  {
-    if (dolfin::MPI::rank(comm) == 0)
-    {
-      // Read data and trim any leading/trailing whitespace
-      pugi::xml_node data_node = dataset_node.first_child();
-      assert(data_node);
-      std::string data_str = data_node.value();
-
-      // Split data based on spaces and line breaks
-      std::vector<boost::iterator_range<std::string::iterator>> data_vector_str;
-      boost::split(data_vector_str, data_str, boost::is_any_of(" \n"));
-
-      // Add data to numerical vector
-      data_vector.reserve(data_vector_str.size());
-      for (auto& v : data_vector_str)
-      {
-        if (v.begin() != v.end())
-          data_vector.push_back(
-              boost::lexical_cast<T>(boost::copy_range<std::string>(v)));
-      }
-    }
-  }
-  else if (format == "HDF")
-  {
-    // Get file and data path
-    auto paths = xdmf_utils::get_hdf5_paths(dataset_node);
-
-    // Handle cases where file path is (a) absolute or (b) relative
-    boost::filesystem::path h5_filepath(paths[0]);
-    if (!h5_filepath.is_absolute())
-      h5_filepath = parent_path / h5_filepath;
-
-    // Open HDF5 for reading
-    HDF5File h5_file(comm, h5_filepath.string(), "r");
-
-    // Get data shape from HDF5 file
-    const std::vector<std::int64_t> shape_hdf5
-        = HDF5Interface::get_dataset_shape(h5_file.h5_id(), paths[1]);
-
-    // FIXME: should we support empty data sets?
-    // Check that data set is not empty
-    assert(!shape_hdf5.empty());
-    assert(shape_hdf5[0] != 0);
-
-    // Determine range of data to read from HDF5 file. This is
-    // complicated by the XML Dimension attribute and the HDF5 storage
-    // possibly having different shapes, e.g. the HDF5 storgae may be a
-    // flat array.
-
-    // If range = {0, 0} then no range is supplied
-    // and we must determine the range
-    if (range[0] == 0 and range[1] == 0)
-    {
-      if (shape_xml == shape_hdf5)
-        range = dolfin::MPI::local_range(comm, shape_hdf5[0]);
-      else if (!shape_xml.empty() and shape_hdf5.size() == 1)
-      {
-        // Size of dims > 0
-        std::int64_t d = 1;
-        for (std::size_t i = 1; i < shape_xml.size(); ++i)
-          d *= shape_xml[i];
-
-        // Check for data size consistency
-        if (d * shape_xml[0] != shape_hdf5[0])
-        {
-          throw std::runtime_error("Data size in XDMF/XML and size of HDF5 "
-                                   "dataset are inconsistent");
-        }
-
-        // Compute data range to read
-        range = dolfin::MPI::local_range(comm, shape_xml[0]);
-        range[0] *= d;
-        range[1] *= d;
-      }
-      else
-      {
-        throw std::runtime_error(
-            "This combination of array shapes in XDMF and HDF5 "
-            "is not supported");
-      }
-    }
-
-    // Retrieve data
-    data_vector
-        = HDF5Interface::read_dataset<T>(h5_file.h5_id(), paths[1], range);
-  }
-  else
-    throw std::runtime_error("Storage format \"" + format + "\" is unknown");
-
-  // Get dimensions for consistency (if available in DataItem node)
-  if (shape_xml.empty())
-  {
-    std::int64_t size = 1;
-    for (auto dim : shape_xml)
-      size *= dim;
-
-    if (size != (std::int64_t)dolfin::MPI::sum(comm, data_vector.size()))
-    {
-      throw std::runtime_error(
-          "Data sizes in attribute and size of data read are inconsistent");
-    }
-  }
-
-  return data_vector;
-}
-//----------------------------------------------------------------------------
 // Returns true for DG0 function::Functions
 bool has_cell_centred_data(const function::Function& u)
 {
@@ -1339,16 +1069,17 @@ XDMFFile::read_mesh_value_collection(std::shared_ptr<const mesh::Mesh> mesh,
   assert(topology_data_node);
   boost::filesystem::path xdmf_filename(_filename);
   const boost::filesystem::path parent_path = xdmf_filename.parent_path();
-  std::vector<std::int32_t> topology_data = get_dataset<std::int32_t>(
-      _mpi_comm.comm(), topology_data_node, parent_path);
+  std::vector<std::int32_t> topology_data
+      = xdmf_read::get_dataset<std::int32_t>(_mpi_comm.comm(),
+                                             topology_data_node, parent_path);
 
   // Read values associated with each mesh::MeshEntity described by topology
   pugi::xml_node attribute_node = grid_node.child("Attribute");
   assert(attribute_node);
   pugi::xml_node attribute_data_node = attribute_node.child("DataItem");
   assert(attribute_data_node);
-  std::vector<T> values_data
-      = get_dataset<T>(_mpi_comm.comm(), attribute_data_node, parent_path);
+  std::vector<T> values_data = xdmf_read::get_dataset<T>(
+      _mpi_comm.comm(), attribute_data_node, parent_path);
 
   // Ensure the mesh dimension is initialised
   mesh->create_entities(dim);
@@ -1661,8 +1392,8 @@ mesh::Mesh XDMFFile::read_mesh(MPI_Comm comm,
   assert(gdims[1] == gdim);
 
   // Geometry
-  const auto geometry_data
-      = get_dataset<double>(_mpi_comm.comm(), geometry_data_node, parent_path);
+  const auto geometry_data = xdmf_read::get_dataset<double>(
+      _mpi_comm.comm(), geometry_data_node, parent_path);
   const std::size_t num_local_points = geometry_data.size() / gdim;
 
   Eigen::Map<const EigenRowArrayXXd> points(geometry_data.data(),
@@ -1674,7 +1405,7 @@ mesh::Mesh XDMFFile::read_mesh(MPI_Comm comm,
   // Topology
   const std::vector<std::int64_t> tdims
       = xdmf_utils::get_dataset_shape(topology_data_node);
-  const auto topology_data = get_dataset<std::int64_t>(
+  const auto topology_data = xdmf_read::get_dataset<std::int64_t>(
       _mpi_comm.comm(), topology_data_node, parent_path);
   const std::size_t npoint_per_cell = tdims[1];
   const std::size_t num_local_cells = topology_data.size() / npoint_per_cell;
@@ -1788,8 +1519,8 @@ XDMFFile::read_checkpoint(std::shared_ptr<const function::FunctionSpace> V,
   const fem::GenericDofMap& dofmap = *V->dofmap();
 
   // Read cell ordering
-  std::vector<std::size_t> cells
-      = get_dataset<std::size_t>(_mpi_comm.comm(), cells_dataitem, parent_path);
+  std::vector<std::size_t> cells = xdmf_read::get_dataset<std::size_t>(
+      _mpi_comm.comm(), cells_dataitem, parent_path);
 
   const std::vector<std::int64_t> x_cell_dofs_shape
       = xdmf_utils::get_dataset_shape(cells_dataitem);
@@ -1799,14 +1530,14 @@ XDMFFile::read_checkpoint(std::shared_ptr<const function::FunctionSpace> V,
       = dolfin::MPI::local_range(_mpi_comm.comm(), x_cell_dofs_shape[0]);
 
   // Read number of dofs per cell
-  std::vector<std::int64_t> x_cell_dofs = get_dataset<std::int64_t>(
+  std::vector<std::int64_t> x_cell_dofs = xdmf_read::get_dataset<std::int64_t>(
       _mpi_comm.comm(), x_cell_dofs_dataitem, parent_path,
       {{cell_range[0], cell_range[1] + 1}});
 
   // Read cell dofmaps
-  std::vector<PetscInt> cell_dofs
-      = get_dataset<PetscInt>(_mpi_comm.comm(), cell_dofs_dataitem, parent_path,
-                              {{x_cell_dofs.front(), x_cell_dofs.back()}});
+  std::vector<PetscInt> cell_dofs = xdmf_read::get_dataset<PetscInt>(
+      _mpi_comm.comm(), cell_dofs_dataitem, parent_path,
+      {{x_cell_dofs.front(), x_cell_dofs.back()}});
 
   const std::vector<std::int64_t> vector_shape
       = xdmf_utils::get_dataset_shape(vector_dataitem);
@@ -1818,7 +1549,7 @@ XDMFFile::read_checkpoint(std::shared_ptr<const function::FunctionSpace> V,
 
 #ifdef PETSC_USE_COMPLEX
   // Read real component of function vector
-  std::vector<double> real_vector = get_dataset<double>(
+  std::vector<double> real_vector = xdmf_read::get_dataset<double>(
       _mpi_comm.comm(), vector_dataitem, parent_path, input_vector_range);
 
   // Find FE attribute node of the imaginary component
@@ -1837,7 +1568,7 @@ XDMFFile::read_checkpoint(std::shared_ptr<const function::FunctionSpace> V,
   assert(imag_vector_dataitem);
 
   // Read imaginary component of function vector
-  std::vector<double> imag_vector = get_dataset<double>(
+  std::vector<double> imag_vector = xdmf_read::get_dataset<double>(
       _mpi_comm.comm(), imag_vector_dataitem, parent_path, input_vector_range);
 
   assert(real_vector.size() == imag_vector.size());
@@ -1850,7 +1581,7 @@ XDMFFile::read_checkpoint(std::shared_ptr<const function::FunctionSpace> V,
                  [](double r, double i) { return r + i * PETSC_i; });
 #else
   // Read function vector
-  std::vector<double> vector = get_dataset<double>(
+  std::vector<double> vector = xdmf_read::get_dataset<double>(
       _mpi_comm.comm(), vector_dataitem, parent_path, input_vector_range);
 #endif
 
@@ -1955,19 +1686,19 @@ XDMFFile::read_mesh_function(std::shared_ptr<const mesh::Mesh> mesh,
   // Get topology dataset
   pugi::xml_node topology_data_node = topology_node.child("DataItem");
   assert(topology_data_node);
-  const auto topology_data = get_dataset<std::int64_t>(
+  const auto topology_data = xdmf_read::get_dataset<std::int64_t>(
       mesh->mpi_comm(), topology_data_node, parent_path);
   assert(topology_data.size() % num_vertices_per_cell == 0);
 
   // Get value dataset
   pugi::xml_node value_data_node = value_node.child("DataItem");
   assert(value_data_node);
-  std::vector<T> value_data
-      = get_dataset<T>(_mpi_comm.comm(), value_data_node, parent_path);
+  std::vector<T> value_data = xdmf_read::get_dataset<T>(
+      _mpi_comm.comm(), value_data_node, parent_path);
 
   // Create mesh function and scatter/gather data across processes
   mesh::MeshFunction<T> mf(mesh, dim, 0);
-  remap_meshfunction_data(mf, topology_data, value_data);
+  xdmf_read::remap_meshfunction_data(mf, topology_data, value_data);
 
   return mf;
 }
---
+++ cpp/dolfin/io/xdmf_read.cpp
@@ -0,0 +1,29 @@
+// Copyright (C) 2012-2016 Chris N. Richardson and Garth N. Wells
+//
+// This file is part of DOLFIN (https://www.fenicsproject.org)
+//
+// SPDX-License-Identifier:    LGPL-3.0-or-later
+
+#include "xdmf_read.h"
+// #include "HDF5File.h"
+// #include "pugixml.hpp"
+// #include "xdmf_utils.h"
+// #include <boost/algorithm/string.hpp>
+// #include <boost/filesystem.hpp>
+// #include <boost/lexical_cast.hpp>
+// #include <dolfin/common/MPI.h>
+// #include <dolfin/common/log.h>
+// #include <dolfin/common/utils.h>
+// #include <dolfin/fem/GenericDofMap.h>
+// #include <dolfin/function/Function.h>
+// #include <dolfin/function/FunctionSpace.h>
+// #include <dolfin/geometry/Point.h>
+// #include <dolfin/mesh/DistributedMeshTools.h>
+// #include <dolfin/mesh/Mesh.h>
+// #include <dolfin/mesh/MeshIterator.h>
+// #include <dolfin/mesh/Topology.h>
+// #include <dolfin/mesh/Vertex.h>
+
+using namespace dolfin;
+using namespace dolfin::io;
+
