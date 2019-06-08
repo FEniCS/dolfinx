d00c977aa3ad07e08857eaf8c1fbbfc54aeee4d4
Garth N. Wells <gnw20@cam.ac.uk>
2019-06-08 20:20:29 +0100

    Fix tiny comment typo [skip ci]

--- cpp/dolfin/graph/SCOTCH.h
+++ cpp/dolfin/graph/SCOTCH.h
@@ -14,7 +14,7 @@
 #include <utility>
 #include <vector>
 
-// Developer note: ptscotch.h is not part of the public interface,.
+// Developer note: ptscotch.h is not part of the public interface,
 // therefore this header (SCOTCH.h) should not be placed in the public
 // interface of DOLFIN
 extern "C"
