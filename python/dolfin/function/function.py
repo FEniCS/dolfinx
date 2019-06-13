a4e1b6145509e209b77f3a4eb5ddbe2990f88ec3
Garth N. Wells <gnw20@cam.ac.uk>
2019-06-13 23:04:57 +0100

    Import fix.

--- python/dolfin/function/function.py
+++ python/dolfin/function/function.py
@@ -13,7 +13,7 @@ from petsc4py import PETSc
 
 import ufl
 from dolfin import common, cpp, function
-from dolfin.function import functionspace
+from dolfin.function import expression, functionspace
 
 
 class Function(ufl.Coefficient):
