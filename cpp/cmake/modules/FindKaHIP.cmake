2976beda78187d4227a65bc9857ff25ad6335938
igorbaratta <igorbaratta@gmail.com>
2019-06-21 08:57:47 -0300

    add kahip test cmake

--- cpp/cmake/modules/FindKaHIP.cmake
+++ cpp/cmake/modules/FindKaHIP.cmake
@@ -95,6 +95,40 @@ check_cxx_source_runs("
 
 int main()
 {
+
+int n = 5;
+int *xadj = new int[6];
+xadj[0] = 0;
+xadj[1] = 2;
+xadj[2] = 5;
+xadj[3] = 7;
+xadj[4] = 9;
+xadj[5] = 12;
+
+int *adjncy = new int[12];
+adjncy[0] = 1;
+adjncy[1] = 4;
+adjncy[2] = 0;
+adjncy[3] = 2;
+adjncy[4] = 4;
+adjncy[5] = 1;
+adjncy[6] = 3;
+adjncy[7] = 2;
+adjncy[8] = 4;
+adjncy[9] = 0;
+adjncy[10] = 1;
+adjncy[11] = 3;
+
+double imbalance = 0.03;
+int *part = new int[n];
+int edge_cut = 0;
+int nparts = 2;
+int *vwgt = NULL;
+int *adjcwgt = NULL;
+
+kaffpa(&n, vwgt, xadj, adjcwgt, adjncy, &nparts, &imbalance, false, 0, ECO,
+       &edge_cut, part);
+
 return 0;
 }
 " KAHIP_TEST_RUNS)
