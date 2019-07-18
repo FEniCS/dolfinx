4d23deaf535458ede276de82872555c417782088
Abhinav Gupta <iitrabhi@gmail.com>
2019-07-18 12:07:23 +0530

    update test for construction of mvc from array

--- python/test/unit/mesh/test_mesh_value_collection.py
+++ python/test/unit/mesh/test_mesh_value_collection.py
@@ -158,8 +158,25 @@ def test_mvc_construction_array():
     import pygmsh
 
     geom = pygmsh.opencascade.Geometry()
-    rectangle = geom.add_rectangle([-1.0, -1.0, 0.0], 2.0, 2.0)
-    geom.add_physical(rectangle, label="rect")
+
+    mesh_ele_size = 0.5
+    p0 = geom.add_point([0, 0, 0], lcar=mesh_ele_size)
+    p1 = geom.add_point([1, 0, 0], lcar=mesh_ele_size)
+    p2 = geom.add_point([1, 1, 0], lcar=mesh_ele_size)
+    p3 = geom.add_point([0, 1, 0], lcar=mesh_ele_size)
+
+    l0 = geom.add_line(p0, p1)
+    l1 = geom.add_line(p1, p2)
+    l2 = geom.add_line(p2, p3)
+    l3 = geom.add_line(p3, p0)
+
+    ll = geom.add_line_loop(lines=[l0, l1, l2, l3])
+    ps = geom.add_plane_surface(ll)
+
+    # Tag line and surface
+    geom.add_physical(l3, label="LINE")
+    geom.add_physical(ps, label="SURFACE")
+
     pygmsh_mesh = pygmsh.generate_mesh(geom)
     points, cells, cell_data = pygmsh_mesh.points, pygmsh_mesh.cells, pygmsh_mesh.cell_data
 
@@ -170,5 +187,5 @@ def test_mvc_construction_array():
     assert mesh.geometry.dim == 3
     assert mesh.topology.dim == 2
 
-    f = MeshValueCollection("int", mesh, 2, cells["triangle"], cell_data["triangle"]['gmsh:physical'])
-    assert mesh.num_entities(2) == f.size()
+    f = MeshValueCollection("size_t", mesh, 1, cells["line"], cell_data["line"]['gmsh:physical'])
+    assert f.get_value(1, 1) == 1
