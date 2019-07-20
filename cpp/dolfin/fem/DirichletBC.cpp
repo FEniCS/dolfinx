7c62673e829b8aef7674c2f3b3ddd74d3dc695ee
Garth N. Wells <gnw20@cam.ac.uk>
2019-07-20 12:15:03 +0100

    Tidy up.

--- cpp/dolfin/fem/DirichletBC.cpp
+++ cpp/dolfin/fem/DirichletBC.cpp
@@ -140,9 +140,9 @@ std::vector<std::int32_t> marked_facets(
   // If a vertex is on the boundary, give it an index from [0, count)
   std::vector<std::int32_t> boundary_vertex(mesh.num_entities(0), -1);
   std::size_t count = 0;
-  assert(mesh.topology().connectivity(tdim - 1, tdim));
+  assert(mesh.topology().connectivity(dim, tdim));
   std::shared_ptr<const mesh::Connectivity> connectivity_facet_cell
-      = mesh.topology().connectivity(tdim - 1, tdim);
+      = mesh.topology().connectivity(dim, tdim);
   for (const auto& facet : mesh::MeshRange<mesh::Facet>(mesh))
   {
     if (connectivity_facet_cell->size_global(facet.index()) == 1)
