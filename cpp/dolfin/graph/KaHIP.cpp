48c278dd439eda560af145f4173054e4ffa46dc4
igorbaratta <igorbaratta@gmail.com>
2019-08-06 01:00:27 -0300

    change kahip parameters

--- cpp/dolfin/graph/KaHIP.cpp
+++ cpp/dolfin/graph/KaHIP.cpp
@@ -38,12 +38,12 @@ dolfin::graph::KaHIP::partition(MPI_Comm mpi_comm,
   unsigned long long* adjcwgt{nullptr};
 
   // TODO: Allow the user to set the parameters
-  int mode = ULTRAFASTMESH;
+  int mode = 4;
   int seed = 0;
   // The amount of imbalance that is allowed. (3%)
   double imbalance = 0.03;
   // Suppress output from the partitioning library.
-  bool suppress_output = true;
+  bool suppress_output = false;
 
   // Call KaHIP to partition graph
   common::Timer timer1("KaHIP: call ParHIPPartitionKWay");
