b1b70d5b4992b232a1b7b3701bb1bbb07a48f864
Chris Richardson <chris@bpi.cam.ac.uk>
2019-08-07 08:30:40 +0100

    Refinement redistribution (#494)
    * Honour redistribute flag in refinement
    
    * Fix facets and add test

--- python/test/unit/mesh/test_refinement.py
+++ python/test/unit/mesh/test_refinement.py
@@ -4,7 +4,7 @@
 #
 # SPDX-License-Identifier:    LGPL-3.0-or-later
 
-from dolfin import UnitSquareMesh, UnitCubeMesh, MPI
+from dolfin import UnitSquareMesh, UnitCubeMesh, MPI, FunctionSpace
 from dolfin.cpp.refinement import refine
 
 
@@ -16,9 +16,21 @@ def test_RefineUnitSquareMesh():
     assert mesh.num_entities_global(2) == 280
 
 
-def test_RefineUnitCubeMesh():
+def test_RefineUnitCubeMesh_repartition():
+    """Refine mesh of unit cube."""
+    mesh = UnitCubeMesh(MPI.comm_world, 5, 7, 9)
+    mesh = refine(mesh, True)
+    assert mesh.num_entities_global(0) == 3135
+    assert mesh.num_entities_global(3) == 15120
+    Q = FunctionSpace(mesh, ("CG", 1))
+    assert(Q)
+
+
+def test_RefineUnitCubeMesh_keep_partition():
     """Refine mesh of unit cube."""
     mesh = UnitCubeMesh(MPI.comm_world, 5, 7, 9)
     mesh = refine(mesh, False)
     assert mesh.num_entities_global(0) == 3135
     assert mesh.num_entities_global(3) == 15120
+    Q = FunctionSpace(mesh, ("CG", 1))
+    assert(Q)
