29dd144b89ee30bfc91bfc4f2e9257e4de2e0369
Chris Richardson <chris@bpi.cam.ac.uk>
2019-08-29 12:27:18 +0100

    Add comments and fix Eigen::Vector shape

--- cpp/dolfin/function/Constant.h
+++ cpp/dolfin/function/Constant.h
@@ -23,16 +23,22 @@ public:
   Constant(std::vector<PetscScalar> value, std::vector<int> shape)
       : value(value), shape(shape){};
 
-  /// Initialise with a scalar
+  /// Initialise with a scalar value
   Constant(PetscScalar value) : value({value}), shape({1}){};
 
-  /// Initialise with an array
+  /// Initialise with a 1D or 2D Array
   Constant(Eigen::Ref<Eigen::Array<PetscScalar, Eigen::Dynamic, Eigen::Dynamic,
                                    Eigen::RowMajor>>
                array)
       : shape({(int)array.rows(), (int)array.cols()})
   {
     value.resize(array.rows() * array.cols());
+
+    // Remove trailing 1 in shape for Eigen::Vector (1D array)
+    if (array.cols() == 1)
+      shape.pop_back();
+
+    // Copy data from Eigen::Array to flattened vector
     Eigen::Map<Eigen::Array<PetscScalar, Eigen::Dynamic, Eigen::Dynamic,
                             Eigen::RowMajor>>
         v(value.data(), array.rows(), array.cols());
