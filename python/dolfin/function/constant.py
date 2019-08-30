9ca82e0652affd550cbf39587f81e7fa2f33eb55
Michal Habera <michal.habera@gmail.com>
2019-08-30 10:16:28 +0200

    Add docstring for constant

--- python/dolfin/function/constant.py
+++ python/dolfin/function/constant.py
@@ -5,6 +5,8 @@
 #
 # SPDX-License-Identifier:    LGPL-3.0-or-later
 
+import typing
+
 import numpy
 
 import ufl
@@ -12,13 +14,26 @@ import dolfin.cpp
 
 
 class Constant(ufl.Constant):
-    def __init__(self, domain, value):
+    def __init__(self, domain, value: typing.Union[numpy.ndarray, list, int, float]):
+        """A constant wrt. domain
+
+        Parameters
+        ----------
+        domain : dolfin or UFL mesh
+        value
+            Value of the constant.
+        """
         np_value = numpy.asarray(value)
         super().__init__(domain, np_value.shape)
         self._cpp_object = dolfin.cpp.function.Constant(np_value.flatten(), np_value.shape)
 
     @property
     def value(self):
+        """Returns value of the constant.
+
+        If the value is a scalar, returns immutable scalar.
+        If the value is an array, returns ``numpy.ndarray`` by reference.
+        """
         val = self._cpp_object.value()
         # Return scalar as a scalar
         if val.shape == ():
