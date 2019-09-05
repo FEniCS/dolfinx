1350cae48de155f26f580147113d4fdb797b6999
Garth N. Wells <gnw20@cam.ac.uk>
2019-09-05 14:23:50 +0100

    Some tidy-up of Constant.

--- cpp/dolfin/function/Constant.cpp
+++ cpp/dolfin/function/Constant.cpp
@@ -1,39 +1,41 @@
-// Copyright (C) 2019 Chris Richardson, Michal Habera
+// Copyright (C) 2019 Chris Richardson abd Michal Habera
 //
 // This file is part of DOLFIN (https://www.fenicsproject.org)
 //
 // SPDX-License-Identifier:    LGPL-3.0-or-later
 
 #include "Constant.h"
+#include <Eigen/Dense>
 #include <vector>
-
 
 using namespace dolfin;
 using namespace dolfin::function;
 
-
+//-----------------------------------------------------------------------------
+Constant::Constant(PetscScalar value) : value({value}), shape({1})
+{
+  // Do nothing
+}
 //-----------------------------------------------------------------------------
 Constant::Constant(std::vector<PetscScalar> value, std::vector<int> shape)
-    : value(value), shape(shape){}
-//-----------------------------------------------------------------------------
-/// Initialise with a scalar value
-Constant::Constant(PetscScalar value) : value({value}), shape({1}){}
-//-----------------------------------------------------------------------------
-/// Initialise with a 1D or 2D Array
-Constant::Constant(Eigen::Ref<Eigen::Array<PetscScalar, Eigen::Dynamic, Eigen::Dynamic,
-                                  Eigen::RowMajor>>
-              array)
-    : shape({(int)array.rows(), (int)array.cols()})
+    : value(value), shape(shape)
 {
-  value.resize(array.rows() * array.cols());
-
+  // Do nothing
+}
+//-----------------------------------------------------------------------------
+Constant::Constant(
+    const Eigen::Ref<Eigen::Array<PetscScalar, Eigen::Dynamic, Eigen::Dynamic,
+                                  Eigen::RowMajor>>
+        c)
+    : value(c.rows() * c.cols()), shape({(int)c.rows(), (int)c.cols()})
+{
   // Remove trailing 1 in shape for Eigen::Vector (1D array)
-  if (array.cols() == 1)
+  if (c.cols() == 1)
     shape.pop_back();
 
   // Copy data from Eigen::Array to flattened vector
-  Eigen::Map<Eigen::Array<PetscScalar, Eigen::Dynamic, Eigen::Dynamic,
-                          Eigen::RowMajor>>
-      v(value.data(), array.rows(), array.cols());
-  v = array;
+  for (int i = 0; i < c.rows(); ++i)
+    for (int j = 0; j < c.cols(); ++j)
+      value[i * c.cols() + j] = c(i, j);
 }
+//-----------------------------------------------------------------------------
--- cpp/dolfin/function/Constant.h
+++ cpp/dolfin/function/Constant.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2019 Chris Richardson, Michal Habera
+// Copyright (C) 2019 Chris Richardson and Michal Habera
 //
 // This file is part of DOLFIN (https://www.fenicsproject.org)
 //
@@ -6,9 +6,9 @@
 
 #pragma once
 
-#include <vector>
+#include <Eigen/Dense>
 #include <dolfin/common/types.h>
-
+#include <vector>
 
 namespace dolfin
 {
@@ -19,18 +19,19 @@ namespace function
 class Constant
 {
 public:
+  /// Initialise with a scalar value
+  Constant(PetscScalar value);
+
+  /// Initialise with a 1D or 2D Array
+  Constant(const Eigen::Ref<Eigen::Array<PetscScalar, Eigen::Dynamic,
+                                         Eigen::Dynamic, Eigen::RowMajor>>
+               c);
+
   /// Initialise as a vector
   ///
   /// The vector is a row-major (C style) flattened value of the constant
   Constant(std::vector<PetscScalar> value, std::vector<int> shape);
 
-  /// Initialise with a scalar value
-  Constant(PetscScalar value);
-
-  /// Initialise with a 1D or 2D Array
-  Constant(Eigen::Ref<Eigen::Array<PetscScalar, Eigen::Dynamic, Eigen::Dynamic,
-                                   Eigen::RowMajor>>
-               array);
   /// Value
   std::vector<PetscScalar> value;
 
