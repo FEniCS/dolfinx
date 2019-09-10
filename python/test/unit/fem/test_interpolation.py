6a5bd75f3e6b74418a1c11dc374b91f7e18d7910
Michal Habera <michal.habera@gmail.com>
2019-09-10 14:57:20 +0200

    Add laplace interpolation

--- python/test/unit/fem/test_interpolation.py
+++ python/test/unit/fem/test_interpolation.py
@@ -1,3 +1,4 @@
+import pytest
 
 import ufl
 import dolfin
@@ -18,3 +19,22 @@ def test_interpolation():
 
     f = dolfin.Function(vP1)
     dolfin.fem.interpolation.compiled_interpolation(expr, vP1, f)
+
+
+def test_interpolation_laplace():
+    mesh = dolfin.generation.UnitSquareMesh(dolfin.MPI.comm_world, 5, 5)
+
+    P2 = dolfin.FunctionSpace(mesh, ("P", 2))
+    dP0 = dolfin.FunctionSpace(mesh, ("DP", 0))
+
+    u = dolfin.Function(P2)
+    f = dolfin.Function(dP0)
+
+    def evaluate(values, x):
+        values[:, 0] = x[:, 0] ** 2
+
+    u.interpolate(evaluate)
+    ufl_expr = ufl.div(ufl.grad(u))
+    dolfin.fem.interpolation.compiled_interpolation(ufl_expr, dP0, f)
+
+    assert pytest.approx((f.vector - 2.0).norm(), 0.0)
