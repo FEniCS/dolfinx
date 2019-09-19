5621fe2a5b73c6474bc52781e74c28d16f6afb13
dokken <dokken92@gmail.com>
2019-09-19 13:59:41 +0100

    Removed demo

--- python/demo/high-order-mesh/high-order-mesh.py
+++ python/demo/high-order-mesh/high-order-mesh.py
@@ -30,8 +30,8 @@ for order in range(1, 4):
     r = 0.5   # Small radius
     R = 1     # Big radius
     geo.add_raw_code("Mesh.ElementOrder={0};".format(order))
-    small_circle = geo.add_circle([0, 0, 0], 0.2, lcar=lcar)
-    circle = geo.add_circle([0, 0, 0], 1, lcar=lcar, holes=[small_circle.plane_surface])
+    small_circle = geo.add_circle([0, 0, 0], r, lcar=lcar)
+    circle = geo.add_circle([0, 0, 0], R, lcar=lcar, holes=[small_circle.plane_surface])
     geo.add_physical(circle.plane_surface, 1)
 
     # Generate 2D mesh
@@ -53,9 +53,37 @@ for order in range(1, 4):
     mesh = Mesh(MPI.comm_world, cpp.mesh.CellType.triangle, pygmsh.points,
                 pygmsh.cells[element], [], cpp.mesh.GhostMode.none)
 
+    # Solve Poisson problem
+    from ufl import inner, grad, dx, ds, SpatialCoordinate, sqrt
+    from dolfin import DirichletBC, Function, FunctionSpace, solve, TrialFunction, TestFunction
+    from dolfin.fem import create_coordinate_map
+    cmap = create_coordinate_map(mesh.ufl_domain())
+    mesh.geometry.coord_mapping = cmap
+    V = FunctionSpace(mesh, ("CG", 1))
+    u, v = TrialFunction(V), TestFunction(V)
+    f = Constant(mesh, 1)
+    a = inner(grad(u), grad(v))*dx
+    x, y = SpatialCoordinate(mesh)
+    g = -0.5*sqrt(x**2 + y**2)
+    L = inner(f, v)*dx + inner(g, v) * ds
+    # Boundary Condition
+    u_D = Function(V)
+    u_D.vector.set(0.0)
+    def boundary(x, only_boundary):
+        return np.sqrt(x[:,0]**2 + x[:,1]**2) > R - 1e-14
+    bc = DirichletBC(V, u_D, boundary)
+
+    uh = Function(V)
+    solve(a==L, uh, bc)
+
+    VTKFile("u{0}.pvd".format(order)).write(uh)
+    int_u = MPI.sum(mesh.mpi_comm(), assemble_scalar(uh*dx))
+    int_ex = 0.0520833
+    print(int_u, int_ex, abs(int_u-int_ex)/(int_ex)*100)
     # Compute area of mesh
-    area = assemble_scalar(Constant(mesh, 1) * dx)
-    area_ex = np.pi * (1 - 0.2**2)
+    area = MPI.sum(mesh.mpi_comm(), assemble_scalar(Constant(mesh, 1) * dx))
+
+    area_ex = np.pi * (R**2 - r**2)
     print("-" * 20 + "Mesh order {0:d}".format(order) + "-" * 20)
     print("Computed volume: {0:.2e}"
           .format(area))
