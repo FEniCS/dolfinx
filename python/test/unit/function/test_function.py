0ba0ee48acc095ed1a420ad0e156901aac77ee4f
Garth N. Wells <gnw20@cam.ac.uk>
2019-09-26 22:29:51 +0100

    Small changes.

--- python/test/unit/function/test_function.py
+++ python/test/unit/function/test_function.py
@@ -166,8 +166,8 @@ def test_eval(R, V, W, Q, mesh):
 
     x0 = (mesh.geometry.x(0) + mesh.geometry.x(1)) / 2.0
     tree = geometry.BoundingBoxTree(mesh, mesh.geometry.dim)
-    cells = geometry.compute_first_entity_collision(tree, mesh)
-    assert np.allclose(u3.eval(x0, tree)[:3], u2.eval(x0, tree), rtol=1e-15, atol=1e-15)
+    cells = geometry.compute_first_entity_collisions(tree, mesh)
+    assert np.allclose(u3.eval(x0, cells)[:3], u2.eval(x0, cells), rtol=1e-15, atol=1e-15)
 
     with pytest.raises(ValueError):
         u0.eval([0, 0, 0, 0], tree)
