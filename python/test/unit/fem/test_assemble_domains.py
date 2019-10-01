9f53b60061ea31e3662d0e18daacf6b1ee52fe61
Michal Habera <michal.habera@gmail.com>
2019-10-01 09:49:00 +0200

    Fix coefficient offsets (#552)
    * Change layout for coefficients in interior facet integrals

--- python/test/unit/fem/test_assemble_domains.py
+++ python/test/unit/fem/test_assemble_domains.py
@@ -18,6 +18,18 @@ def mesh():
     return dolfin.generation.UnitSquareMesh(dolfin.MPI.comm_world, 10, 10)
 
 
+parametrize_ghost_mode = pytest.mark.parametrize("mode", [
+    pytest.param(dolfin.cpp.mesh.GhostMode.none,
+                 marks=pytest.mark.skipif(condition=dolfin.MPI.size(dolfin.MPI.comm_world) > 1,
+                                          reason="Unghosted interior facets fail in parallel")),
+    pytest.param(dolfin.cpp.mesh.GhostMode.shared_facet,
+                 marks=pytest.mark.skipif(condition=dolfin.MPI.size(dolfin.MPI.comm_world) == 1,
+                                          reason="Shared ghost modes fail in serial")),
+    pytest.param(dolfin.cpp.mesh.GhostMode.shared_vertex,
+                 marks=pytest.mark.skipif(condition=dolfin.MPI.size(dolfin.MPI.comm_world) == 1,
+                                          reason="Shared ghost modes fail in serial"))])
+
+
 def test_assembly_dx_domains(mesh):
     V = dolfin.FunctionSpace(mesh, ("CG", 1))
     u, v = dolfin.TrialFunction(V), dolfin.TestFunction(V)
@@ -159,16 +171,7 @@ def test_assembly_ds_domains(mesh):
     assert (s == pytest.approx(s2, 1.0e-12) and 2.0 == pytest.approx(s, 1.0e-12))
 
 
-@pytest.mark.parametrize("mode",
-                         [pytest.param(dolfin.cpp.mesh.GhostMode.none,
-                                       marks=pytest.mark.skipif(condition=dolfin.MPI.size(dolfin.MPI.comm_world) > 1,
-                                                                reason="Unghosted interior facets fail in parallel")),
-                          pytest.param(dolfin.cpp.mesh.GhostMode.shared_facet,
-                                       marks=pytest.mark.skipif(condition=dolfin.MPI.size(dolfin.MPI.comm_world) == 1,
-                                                                reason="Shared ghost modes fail in serial")),
-                          pytest.param(dolfin.cpp.mesh.GhostMode.shared_vertex,
-                                       marks=pytest.mark.skipif(condition=dolfin.MPI.size(dolfin.MPI.comm_world) == 1,
-                                                                reason="Shared ghost modes fail in serial"))])
+@parametrize_ghost_mode
 def test_assembly_dS_domains(mode):
     N = 10
     mesh = dolfin.UnitSquareMesh(dolfin.MPI.comm_world, N, N, ghost_mode=mode)
@@ -176,3 +179,39 @@ def test_assembly_dS_domains(mode):
     val = dolfin.fem.assemble_scalar(one * ufl.dS)
     val = dolfin.MPI.sum(mesh.mpi_comm(), val)
     assert val == pytest.approx(2 * (N - 1) + N * numpy.sqrt(2), 1.0e-7)
+
+
+@parametrize_ghost_mode
+def test_additivity(mode):
+    mesh = dolfin.UnitSquareMesh(dolfin.MPI.comm_world, 12, 12, ghost_mode=mode)
+    V = dolfin.FunctionSpace(mesh, ("CG", 1))
+
+    f1 = dolfin.Function(V)
+    f2 = dolfin.Function(V)
+    f3 = dolfin.Function(V)
+    with f1.vector.localForm() as f1_local:
+        f1_local.set(1.0)
+    with f2.vector.localForm() as f2_local:
+        f2_local.set(2.0)
+    with f3.vector.localForm() as f3_local:
+        f3_local.set(3.0)
+    j1 = ufl.inner(f1, f1) * ufl.dx(mesh)
+    j2 = ufl.inner(f2, f2) * ufl.ds(mesh)
+    j3 = ufl.inner(ufl.avg(f3), ufl.avg(f3)) * ufl.dS(mesh)
+
+    # Assemble each scalar form separately
+    J1 = dolfin.MPI.sum(mesh.mpi_comm(), dolfin.fem.assemble_scalar(j1))
+    J2 = dolfin.MPI.sum(mesh.mpi_comm(), dolfin.fem.assemble_scalar(j2))
+    J3 = dolfin.MPI.sum(mesh.mpi_comm(), dolfin.fem.assemble_scalar(j3))
+
+    # Sum forms and assemble the result
+    J12 = dolfin.MPI.sum(mesh.mpi_comm(), dolfin.fem.assemble_scalar(j1 + j2))
+    J13 = dolfin.MPI.sum(mesh.mpi_comm(), dolfin.fem.assemble_scalar(j1 + j3))
+    J23 = dolfin.MPI.sum(mesh.mpi_comm(), dolfin.fem.assemble_scalar(j2 + j3))
+    J123 = dolfin.MPI.sum(mesh.mpi_comm(), dolfin.fem.assemble_scalar(j1 + j2 + j3))
+
+    # Compare assembled values
+    assert (J1 + J2) == pytest.approx(J12)
+    assert (J1 + J3) == pytest.approx(J13)
+    assert (J2 + J3) == pytest.approx(J23)
+    assert (J1 + J2 + J3) == pytest.approx(J123)
