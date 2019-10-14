92ad577218eb17ac278cf1bca322e0cda39e172d
Michal Habera <michal.habera@gmail.com>
2019-10-14 12:37:22 +0200

    Add interpolation test using set assembly

--- python/test/unit/fem/test_interpolation.py
+++ python/test/unit/fem/test_interpolation.py
@@ -1,24 +1,10 @@
+import cffi
+from petsc4py import PETSc
 import pytest
+import numpy as np
 
 import ufl
 import dolfin
-
-
-def test_interpolation():
-    mesh = dolfin.generation.UnitSquareMesh(dolfin.MPI.comm_world, 5, 5)
-    vP1 = dolfin.VectorFunctionSpace(mesh, ("P", 1))
-
-    u1 = dolfin.Function(vP1)
-    u1.vector.set(1.0)
-
-    u2 = dolfin.Function(vP1)
-
-    k1 = dolfin.Constant(mesh, [[1.0, 2.0], [3.0, 4.0]])
-    x = ufl.SpatialCoordinate(mesh)
-    expr = u2 * ufl.zero(()) + ufl.inner(x, u1) * ufl.dot(k1, u1)
-
-    f = dolfin.Function(vP1)
-    dolfin.fem.interpolation.compiled_interpolation(expr, vP1, f)
 
 
 def test_interpolation_laplace():
@@ -30,11 +16,27 @@ def test_interpolation_laplace():
     u = dolfin.Function(P2)
     f = dolfin.Function(dP0)
 
-    def evaluate(values, x):
-        values[:, 0] = x[:, 0] ** 2
+    def evaluate(x):
+        return x[:, 0] ** 2
 
     u.interpolate(evaluate)
     ufl_expr = ufl.div(ufl.grad(u))
-    dolfin.fem.interpolation.compiled_interpolation(ufl_expr, dP0, f)
 
-    assert pytest.approx((f.vector - 2.0).norm(), 0.0)
+    # Attach the expression with cell midpoint
+    expr = (ufl_expr, np.array([[0.25, 0.25]]))
+
+    module = dolfin.jit.ffc_jit(expr)
+    kernel = module.tabulate_expression
+
+    ffi = cffi.FFI()
+
+    I = dolfin.cpp.fem.Form([dP0._cpp_object])
+    I.set_tabulate_cell(-1, ffi.cast("uintptr_t", kernel))
+    I.set_coefficient(0, u._cpp_object)
+
+    dolfin.fem.assemble_vector(f.vector, I, mode=dolfin.cpp.fem.InsertMode.set)
+
+    # Update ghosts with the same mode as assembly
+    f.vector.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
+
+    assert np.isclose((f.vector - 2.0).norm(), 0.0)
