574b27ab6a4e9adc2c589ec4f67bad45a33e78df
Francesco Ballarin <francesco.ballarin@sissa.it>
2019-10-15 12:26:16 +0200

    Add minimal test case to reproduce CI issue encountered in PR 573
    (cherry picked from commit a9eb010f65d7a29baa09132622d944bd8ebe2edc)

---
+++ cpp/test/unit/common/CIFailure.cpp
@@ -0,0 +1,32 @@
+// Copyright (C) 2019 Francesco Ballarin
+//
+// This file is part of DOLFIN (https://www.fenicsproject.org)
+//
+// SPDX-License-Identifier:    LGPL-3.0-or-later
+//
+// Unit tests for Distributed Meshes
+
+#include <catch.hpp>
+#include <dolfin.h>
+
+using namespace dolfin;
+
+namespace
+{
+void test_ci_failure()
+{
+  int argc = 0;
+  char** argv = nullptr;
+  PetscInitialize(&argc, &argv, nullptr, nullptr);
+
+  auto mpi_comm = dolfin::MPI::Comm(MPI_COMM_WORLD);
+  int mpi_rank = dolfin::MPI::rank(mpi_comm.comm());
+
+  CHECK(mpi_rank == 0);
+}
+} // namespace
+
+TEST_CASE("CI failure", "[ci_failure]")
+{
+  CHECK_NOTHROW(test_ci_failure());
+}
