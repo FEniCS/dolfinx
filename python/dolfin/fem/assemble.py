d3a3a580e2f6578f97f1341796ae715c7b78e385
nate-sime <nsime@carnegiescience.edu>
2019-10-17 17:45:52 -0400

    remove verbosity of extracting ufl forms for dolfin forms each time. Plan to remove this in the future?

--- python/dolfin/fem/assemble.py
+++ python/dolfin/fem/assemble.py
@@ -11,50 +11,82 @@ import typing
 
 from petsc4py import PETSc
 
+import ufl
+
+import dolfin
 from dolfin import cpp
 from dolfin.fem.dirichletbc import DirichletBC
-from dolfin.fem.form import Form
 
 
-def _create_cpp_form(form: typing.Union[Form, cpp.fem.Form]) -> cpp.fem.Form:
-    """Create a compiled Form from a UFL form"""
-    if form is None:
-        return None
-    elif isinstance(form, cpp.fem.Form):
-        return form
-    else:
-        # FIXME: Attach cpp Form to UFL Form to avoid re-processing
-        form = Form(form)
-        return form._cpp_object
+def _create_cpp_form(form):
+    """Recursively look for ufl.Forms and convert to dolfin.fem.Form, otherwise
+    return form argument
+    """
+    if isinstance(form, ufl.Form):
+        return dolfin.fem.Form(form)._cpp_object
+    elif isinstance(form, (tuple, list)):
+        return list(map(lambda sub_form: _create_cpp_form(sub_form), form))
+    return form
 
+
+def convert_ufl_forms_to_dolfin_forms(f):
+    """Function decorator to wrap all ufl.Form arguments to dolfin.fem.Form.
+    This decorator prevents having to do this conversion in each of the members
+    of this module.
+    """
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        args = _create_cpp_form(args)
+        return f(*args, **kwargs)
+    return wrapper
+
+
+# # -- Vector instantiation ----------------------------------------------------
+#
+# def create_vector(L: typing.Union[Form, cpp.fem.Form]):
+#     L_cpp = _create_cpp_form(L)
+#     b = cpp.la.create_vector(L_cpp.function_space(0).dofmap.index_map)
+#     return b
+#
+#
+# def create_vector_block(L: typing.List[typing.Union[Form, cpp.fem.Form]]):
+#     L_cpp = [_create_cpp_form(form) for form in L]
+#     b = cpp.fem.create_vector(L_cpp)
+#     return b
+#
+#
+# def create_vector_nest(L: typing.List[typing.Union[Form, cpp.fem.Form]]):
+#     L_cpp = [_create_cpp_form(form) for form in L]
+#     b = cpp.fem.create_vector_nest(L_cpp)
+#     return b
 
 # -- Scalar assembly ---------------------------------------------------------
 
+@convert_ufl_forms_to_dolfin_forms
 def assemble_scalar(M: typing.Union[Form, cpp.fem.Form]) -> PETSc.ScalarType:
     """Assemble functional. The returned value is local and not accumulated
     across processes.
 
     """
-    M_cpp = _create_cpp_form(M)
-    return cpp.fem.assemble_scalar(M_cpp)
+    return cpp.fem.assemble_scalar(M)
 
 # -- Vector assembly ---------------------------------------------------------
 
-
+@convert_ufl_forms_to_dolfin_forms
 @functools.singledispatch
 def assemble_vector(L: typing.Union[Form, cpp.fem.Form], mode=cpp.fem.InsertMode.sum) -> PETSc.Vec:
     """Assemble linear form into a vector. The returned vector is not
     finalised, i.e. ghost values are not accumulated.
 
     """
-    L_cpp = _create_cpp_form(L)
-    b = cpp.la.create_vector(L_cpp.function_space(0).dofmap.index_map)
+    b = cpp.la.create_vector(L.function_space(0).dofmap.index_map)
     with b.localForm() as b_local:
         b_local.set(0.0)
-    cpp.fem.assemble_vector(b, L_cpp, mode)
+    cpp.fem.assemble_vector(b, L, mode)
     return b
 
 
+@convert_ufl_forms_to_dolfin_forms
 @assemble_vector.register(PETSc.Vec)
 def _(b: PETSc.Vec, L: typing.Union[Form, cpp.fem.Form], mode=cpp.fem.InsertMode.sum) -> PETSc.Vec:
     """Re-assemble linear form into a vector.
@@ -63,12 +95,12 @@ def _(b: PETSc.Vec, L: typing.Union[Form, cpp.fem.Form], mode=cpp.fem.InsertMode
     are not accumulated.
 
     """
-    L_cpp = _create_cpp_form(L)
-    cpp.fem.assemble_vector(b, L_cpp, mode)
+    cpp.fem.assemble_vector(b, L, mode)
     return b
 
 
 # FIXME: Revise this interface
+@convert_ufl_forms_to_dolfin_forms
 def assemble_vector_nest(L: typing.List[typing.Union[Form, cpp.fem.Form]],
                          a,
                          bcs: typing.List[DirichletBC],
@@ -79,14 +111,13 @@ def assemble_vector_nest(L: typing.List[typing.Union[Form, cpp.fem.Form]],
     accumulated.
 
     """
-    L_cpp = [_create_cpp_form(form) for form in L]
-    a_cpp = [[_create_cpp_form(form) for form in row] for row in a]
-    b = cpp.fem.create_vector_nest(L_cpp)
-    cpp.fem.assemble_vector(b, L_cpp, a_cpp, bcs, x0, scale)
+    b = cpp.fem.create_vector_nest(L)
+    cpp.fem.assemble_vector(b, L, a, bcs, x0, scale)
     return b
 
 
 # FIXME: Revise this interface
+@convert_ufl_forms_to_dolfin_forms
 def assemble_vector_block(L: typing.List[typing.Union[Form, cpp.fem.Form]],
                           a,
                           bcs: typing.List[DirichletBC],
@@ -97,14 +128,13 @@ def assemble_vector_block(L: typing.List[typing.Union[Form, cpp.fem.Form]],
     accumulated.
 
     """
-    L_cpp = [_create_cpp_form(form) for form in L]
-    a_cpp = [[_create_cpp_form(form) for form in row] for row in a]
-    b = cpp.fem.create_vector(L_cpp)
-    cpp.fem.assemble_vector(b, L_cpp, a_cpp, bcs, x0, scale)
+    b = cpp.fem.create_vector(L)
+    cpp.fem.assemble_vector(b, L, a, bcs, x0, scale)
     return b
 
 
 # FIXME: Revise this interface
+@convert_ufl_forms_to_dolfin_forms
 def reassemble_vector(b: PETSc.Vec,
                       L,
                       a=[],
@@ -115,15 +145,14 @@ def reassemble_vector(b: PETSc.Vec,
     boundary conditions
 
     """
-    L_cpp = [_create_cpp_form(form) for form in L]
-    a_cpp = [[_create_cpp_form(form) for form in row] for row in a]
-    cpp.fem.reassemble_vector(b, L_cpp, a_cpp, bcs, x0, scale)
+    cpp.fem.reassemble_vector(b, L, a, bcs, x0, scale)
     return b
 
 
 # -- Matrix assembly ---------------------------------------------------------
 
 
+@convert_ufl_forms_to_dolfin_forms
 @functools.singledispatch
 def assemble_matrix(a,
                     bcs: typing.List[DirichletBC] = [],
@@ -132,13 +161,13 @@ def assemble_matrix(a,
     finalised, i.e. ghost values are not accumulated.
 
     """
-    a_cpp = _create_cpp_form(a)
-    A = cpp.fem.create_matrix(a_cpp)
+    A = cpp.fem.create_matrix(a)
     A.zeroEntries()
-    cpp.fem.assemble_matrix(A, a_cpp, bcs, diagonal)
+    cpp.fem.assemble_matrix(A, a, bcs, diagonal)
     return A
 
 
+@convert_ufl_forms_to_dolfin_forms
 @assemble_matrix.register(PETSc.Mat)
 def _(A, a, bcs: typing.List[DirichletBC] = [],
       diagonal: float = 1.0) -> PETSc.Mat:
@@ -146,32 +175,31 @@ def _(A, a, bcs: typing.List[DirichletBC] = [],
     finalised, i.e. ghost values are not accumulated.
 
     """
-    a_cpp = _create_cpp_form(a)
-    cpp.fem.assemble_matrix(A, a_cpp, bcs, diagonal)
+    cpp.fem.assemble_matrix(A, a, bcs, diagonal)
     return A
 
 
 # FIXME: Revise this interface
+@convert_ufl_forms_to_dolfin_forms
 def assemble_matrix_nest(a,
                          bcs: typing.List[DirichletBC],
                          diagonal: float = 1.0) -> PETSc.Mat:
     """Assemble bilinear forms into matrix"""
-    a_cpp = [[_create_cpp_form(form) for form in row] for row in a]
-    A = cpp.fem.create_matrix_nest(a_cpp)
+    A = cpp.fem.create_matrix_nest(a)
     A.zeroEntries()
-    cpp.fem.assemble_blocked_matrix(A, a_cpp, bcs, diagonal)
+    cpp.fem.assemble_blocked_matrix(A, a, bcs, diagonal)
     return A
 
 
 # FIXME: Revise this interface
+@convert_ufl_forms_to_dolfin_forms
 def assemble_matrix_block(a,
                           bcs: typing.List[DirichletBC],
                           diagonal: float = 1.0) -> PETSc.Mat:
     """Assemble bilinear forms into matrix"""
-    a_cpp = [[_create_cpp_form(form) for form in row] for row in a]
-    A = cpp.fem.create_matrix_block(a_cpp)
+    A = cpp.fem.create_matrix_block(a)
     A.zeroEntries()
-    cpp.fem.assemble_blocked_matrix(A, a_cpp, bcs, diagonal)
+    cpp.fem.assemble_blocked_matrix(A, a, bcs, diagonal)
     return A
 
 
@@ -179,6 +207,7 @@ def assemble_matrix_block(a,
 
 # FIXME: Explain in docstring order of calling this function and
 # parallel udpdating w.r.t assembly of L into b.
+@convert_ufl_forms_to_dolfin_forms
 def apply_lifting(b: PETSc.Vec,
                   a: typing.List[typing.Union[Form, cpp.fem.Form]],
                   bcs: typing.List[typing.List[DirichletBC]],
@@ -187,8 +216,7 @@ def apply_lifting(b: PETSc.Vec,
     """Modify vector for lifting of Dirichlet boundary conditions.
 
     """
-    a_cpp = [_create_cpp_form(form) for form in a]
-    cpp.fem.apply_lifting(b, a_cpp, bcs, x0, scale)
+    cpp.fem.apply_lifting(b, a, bcs, x0, scale)
 
 
 def set_bc(b: PETSc.Vec,
