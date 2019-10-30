00722bc2f0a6562357bde27f9a55a4fcfe59a44e
igorbaratta <igorbaratta@gmail.com>
2019-10-30 20:07:30 +0200

    catch main test file with mpi support

--- cpp/test/unit/main.cpp
+++ cpp/test/unit/main.cpp
@@ -1,2 +1,10 @@
-#define CATCH_CONFIG_MAIN
-#include "catch/catch.hpp"
+#define CATCH_CONFIG_RUNNER
+#include "catch.hpp"
+
+int main(int argc, char* argv[])
+{
+  MPI_Init(argc, argv);
+  int result = Catch::Session().run(argc, argv);
+  MPI_Finalize();
+  return result;
+}
