1e79ada6a5d81ecc176280f84b3c6e6388e346e1
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2019-10-30 11:06:17 +0000

    Added documentation

--- cpp/dolfin/fem/entity_arrangement_types.h
+++ cpp/dolfin/fem/entity_arrangement_types.h
@@ -52,16 +52,38 @@ enum class VolumeArrangementType : int
   hexahedron = 1
 };
 
+/// Class that contains information about the dof arrangement on entities
+/// TODO: Move all this into ElementDofLayout
 class EntityArrangementTypes
 {
 public:
+  /// Construction
   EntityArrangementTypes(const ufc_dofmap& dofmap,
                          const mesh::CellType cell_type);
+
+  /// Get the arrangement type on vertices
+  /// @return The vertex arrangement type
   VertexArrangementType vertex_type() const { return _vertex_type; }
+
+  /// Get the arrangement type on edges
+  /// @return The edge arrangement type
   EdgeArrangementType edge_type() const { return _edge_type; }
+
+  /// Get the arrangement type on faces
+  /// @return The face arrangement type
   FaceArrangementType face_type() const { return _face_type; }
+
+  /// Get the arrangement type on volumes
+  /// @return The volume arrangement type
   VolumeArrangementType volume_type() const { return _volume_type; }
+
+  /// Get the vector type of the element
+  /// @return The vector type of the element
   ElementVectorType element_type() const { return _element_type; }
+
+  /// Get the blocksize on an entity of a dimension
+  /// @param[in] The dimension of the entity
+  /// @return The block size
   int get_block_size(const int dim) const;
 
 private:
