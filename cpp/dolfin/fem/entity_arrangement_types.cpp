33ba102f5e89e8d13c71376c00bb071442dd2b6e
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2019-10-31 10:37:27 +0000

    Use scalar element vector type by default

--- cpp/dolfin/fem/entity_arrangement_types.cpp
+++ cpp/dolfin/fem/entity_arrangement_types.cpp
@@ -109,9 +109,8 @@ EntityArrangementTypes::EntityArrangementTypes(const ufc_dofmap& dofmap,
   else if (dofmap.volume_arrangement_type == 4)
     _volume_type = VolumeArrangementType::hexahedron;
 
-  if (dofmap.sobolev_space_type == 0 || dofmap.sobolev_space_type == 1)
-    _element_type = ElementVectorType::scalar;
-  else if (dofmap.sobolev_space_type == 2)
+  _element_type = ElementVectorType::scalar;
+  if (dofmap.sobolev_space_type == 2)
     _element_type = ElementVectorType::curl;
   else if (dofmap.sobolev_space_type == 3)
     _element_type = ElementVectorType::div;
