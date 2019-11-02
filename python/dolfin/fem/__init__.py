9fdb6476f976a800a695e4a1235cc58a730d159a
Garth N. Wells <gnw20@cam.ac.uk>
2019-11-02 14:19:59 +0000

    Fixes in assembly for blocked nonlinear problems (#600)
    * typo fix
    
    * remove size(x0) == size(b) warning, doesn't seem to make sense. Add back size(a[i]) == size(x0) warning, it does make sense. Fix nonlinear assembly in monolithic block assembler
    
    * remove debugging statement
    
    * remove verbosity of extracting ufl forms for dolfin forms each time. Plan to remove this in the future?
    
    * flake8 fix
    
    * flake8 fix (again), and remove vector inits
    
    * fix import ...
    
    * minor python interface updates... update interface in the future
    
    * working nonlinear assembly test
    
    * fix ghost update in test
    
    * remove the insert mode from previous prmerge
    
    * restore array view of x0
    
    * functioning nonlinear block solver and nonlinear (though linear) test
    
    * use local array and not ISGeneral in subvector assignment
    
    * make the test problem actually nonlinear
    
    * functioning nonlinear block and monolithic taylor hood assembly tests
    
    * flake8 on test
    
    * assign options directly to petsc objects, otherwise solvers get confused
    
    * attempt to fix read locks on array in circleci tests
    
    * flak8
    
    * improper use of getArray()
    
    * One day spent on four lines of code
    
    * get tests working again
    
    * snes convergence assert and reduce problem size
    
    * remove old fixme and add new one
    
    * some renaming
    
    * remove unused code
    
    * fix up x0 nested assembly
    
    * get first test working
    
    * nonlinear nest solve test updates
    
    * Some const updates.
    
    * some error message updates
    
    * python assembly and test updates
    
    * test update
    
    * remove unneeded ternary operator
    
    * small updates to python interface
    
    * taylor-hood nested nonlinear
    
    * a little formatting
    
    * Add function to gather/scatter blocked vectors.
    
    * Small fix.
    
    * Fix memory management.
    
    * More fixes.
    
    * More cleaning up.
    
    * Clean out some code.
    
    * More tidy up.
    
    * Remove magic.
    
    * More cleaning up.
    
    * BC fix.
    
    * Doc updates.
    
    * Remove some unused code.
    
    * Remove some comm in assembly test.
    
    * Flake8 fix.
    
    * Small update.

--- python/dolfin/fem/__init__.py
+++ python/dolfin/fem/__init__.py
@@ -6,7 +6,9 @@
 # SPDX-License-Identifier:    LGPL-3.0-or-later
 """Tools for assembling and manipulating finite element forms"""
 
-from dolfin.fem.assemble import (assemble_scalar, assemble_vector_block,
+from dolfin.fem.assemble import (create_vector, create_vector_block, create_vector_nest,
+                                 create_matrix, create_matrix_block, create_matrix_nest,
+                                 assemble_scalar, assemble_vector_block,
                                  assemble_vector_nest, assemble_matrix,
                                  assemble_matrix_nest, assemble_matrix_block,
                                  set_bc, assemble_vector, apply_lifting)
@@ -21,6 +23,8 @@ from dolfin.fem.solving import solve
 
 
 __all__ = [
+    "create_vector", "create_vector_block", "create_vector_nest",
+    "create_matrix", "create_matrix_block", "create_matrix_nest",
     "apply_lifting", "assemble_scalar", "assemble_vector",
     "assemble_vector_block", "assemble_vector_nest",
     "assemble_matrix_block", "assemble_matrix_nest",
