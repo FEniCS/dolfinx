fd3f61e76b0a3cfd37acd2a76e47ddacc7abad43
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2019-11-06 10:01:19 +0000

    Added test for directions of vector elements

---
+++ python/test/unit/function/test_vector_function.py
@@ -0,0 +1,53 @@
+# Copyright (C) 2019 Matthew Scroggs
+#
+# This file is part of DOLFIN (https://www.fenicsproject.org)
+#
+# SPDX-License-Identifier:    LGPL-3.0-or-later
+"""Test that the vectors in vector spaces are correctly oriented"""
+
+import pytest
+from dolfin import MPI, Function
+from dolfin.cpp.mesh import CellType
+from dolfin import cpp
+from dolfin import FunctionSpace, fem
+
+import numpy as np
+
+
+@pytest.mark.parametrize(
+    'space_type', ["RT"])
+@pytest.mark.parametrize(
+    'order', [1, 2, 3, 4, 5])
+def test_div_conforming_2d(space_type, order):
+    """Checks that the vectors in div conforming spaces on a triangle are correctly oriented"""
+    # Create simple triangle mesh
+    def perform_test(points, cells):
+        mesh = cpp.mesh.Mesh(MPI.comm_world, CellType.triangle, points,
+                             np.array(cells), [], cpp.mesh.GhostMode.none)
+        mesh.geometry.coord_mapping = fem.create_coordinate_map(mesh)
+        V = FunctionSpace(mesh, (space_type, order))
+        f = Function(V)
+        output = []
+        for dof in range(len(f.vector[:])):
+            f.vector[:] = np.zeros(len(f.vector[:]))
+            f.vector[dof] = 1
+            points = np.array([[.5, .5, 0], [.5, .5, 0]])
+            cells = np.array([0, 1])
+            result = f.eval(points, cells)
+            normal = np.array([-1., 1.])
+            output.append(result.dot(normal))
+        return output
+
+    # Direction is incorrect if mesh is not ordered
+    points = np.array([[0, 0], [1, 0], [1, 1], [0, 1]])
+    cells = np.array([[0, 1, 2], [2, 3, 0]])
+    result = perform_test(points, cells)
+    for i, j in result:
+        assert np.allclose(i, -j)
+
+    # Direction is correct if mesh is ordered
+    points = np.array([[0, 0], [1, 0], [1, 1], [0, 1]])
+    cells = np.array([[0, 1, 2], [0, 2, 3]])
+    result = perform_test(points, cells)
+    for i, j in result:
+        assert np.allclose(i, j)
