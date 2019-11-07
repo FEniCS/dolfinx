63e262f8eb2fb359b03318d5261d3bd1065adde7
jorgensd <dokken92@gmail.com>
2019-11-07 17:34:24 +0000

    Interpolating solution in to this function space to compare error. This yields all errors smaller than 5e-5.

--- python/test/unit/fem/test_fem_pipeline.py
+++ python/test/unit/fem/test_fem_pipeline.py
@@ -11,7 +11,7 @@ from petsc4py import PETSc
 
 from dolfin import (MPI, DirichletBC, Function, FunctionSpace, TestFunction,
                     TrialFunction, UnitCubeMesh, UnitIntervalMesh,
-                    UnitSquareMesh, solve)
+                    UnitSquareMesh)
 from dolfin.cpp.mesh import CellType
 from dolfin.fem import (apply_lifting, assemble_matrix, assemble_scalar,
                         assemble_vector, set_bc)
@@ -25,13 +25,16 @@ def ManufacturedSolution(degree, gdim):
     xvec = sp.symbols("x[0] x[1] x[2]")
     x, y, z = xvec
     u = (x)**(degree - 1) * (1 - x)
-
     if gdim > 1:
         u *= (y)**(degree - 1) * (1 - y)
     if gdim > 2:
         u *= (z)**(degree - 1) * (1 - z)
 
-    return u
+    u_dolfin = u
+    for i in range(gdim):
+        x_d = sp.Symbol("x[:,{0:d}]".format(i))
+        u_dolfin = u_dolfin.subs(xvec[i], x_d)
+    return u, u_dolfin
 
 
 def boundary(x):
@@ -50,9 +53,6 @@ def test_manufactured_poisson(degree, cell_type, gdim):
     where p is the degree of the Lagrange function space. Solved on the
     gdim-dimensional UnitMesh, with homogeneous Dirichlet boundary
     conditions.
-
-    Compares values at each dof with the analytical solution.
-
     """
     if gdim == 1:
         mesh = UnitIntervalMesh(MPI.comm_world, 10)
@@ -64,7 +64,10 @@ def test_manufactured_poisson(degree, cell_type, gdim):
     u, v = TrialFunction(V), TestFunction(V)
 
     x = SpatialCoordinate(mesh)  # noqa: F841
-    u_exact = eval(str(ManufacturedSolution(degree, gdim)))
+    u_exact, u_dolfin = ManufacturedSolution(degree, gdim)
+    u_exact = eval(str(u_exact))
+    u_ex = Function(V)
+    u_ex.interpolate(lambda x: eval(str(u_dolfin)))
 
     f = -div(grad(u_exact))
     a = inner(grad(u), grad(v)) * dx(metadata={'quadrature_degree': 2 * degree})
@@ -95,11 +98,6 @@ def test_manufactured_poisson(degree, cell_type, gdim):
 
     uh.vector.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
 
-    error = assemble_scalar((u_exact - uh)**2 * dx(metadata={'quadrature_degree': 2 * degree}))
+    error = assemble_scalar((u_ex - uh)**2 * dx(metadata={'quadrature_degree': 2 * degree}))
     error = MPI.sum(mesh.mpi_comm(), error)
-    print(cell_type, degree, error)
-
-    # if cell_type == CellType.tetrahedron:
-    #     assert np.sqrt(error) < 2e-4
-    # else:
-    #     assert np.sqrt(error) < 5e-6
+    assert np.sqrt(error) < 5e-5
