8645cb614c4162559bd0444ac253f98dad51d8d0
Garth N. Wells <gnw20@cam.ac.uk>
2019-11-08 17:14:17 +0000

    Revert nonlinear assembly test for now.

--- python/test/unit/fem/test_nonlinear_assembler.py
+++ python/test/unit/fem/test_nonlinear_assembler.py
@@ -47,26 +47,18 @@ def test_matrix_assembly_block():
     def boundary(x):
         return numpy.logical_or(x[:, 0] < 1.0e-6, x[:, 0] > 1.0 - 1.0e-6)
 
-    def initial_guess_u(x):
-        return numpy.sin(x[:, 0]) * numpy.sin(x[:, 1])
-
-    def initial_guess_p(x):
-        return -x[:, 0]**2 - x[:, 1]**3
-
-    def bc_value(x):
-        return numpy.cos(x[:, 0]) * numpy.cos(x[:, 1])
+    initial_guess_value = 1.0
+    bc_value = 3.0
 
     u_bc = dolfin.function.Function(V1)
-    u_bc.interpolate(bc_value)
+    with u_bc.vector.localForm() as u_local:
+        u_local.set(bc_value)
     bc = dolfin.fem.dirichletbc.DirichletBC(V1, u_bc, boundary)
 
     # Define variational problem
     du, dp = dolfin.function.TrialFunction(V0), dolfin.function.TrialFunction(V1)
     u, p = dolfin.function.Function(V0), dolfin.function.Function(V1)
     v, q = dolfin.function.TestFunction(V0), dolfin.function.TestFunction(V1)
-
-    u.interpolate(initial_guess_u)
-    p.interpolate(initial_guess_p)
 
     f = 1.0
     g = -3.0
@@ -80,10 +72,18 @@ def test_matrix_assembly_block():
 
     # Monolithic blocked
     x0 = dolfin.fem.create_vector_block(L_block)
-    dolfin.cpp.la.scatter_local_vectors(
-        x0, [u.vector.array_r, p.vector.array_r],
-        [u.function_space.dofmap.index_map, p.function_space.dofmap.index_map])
-    x0.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
+    with x0.localForm() as x0_local:
+        x0_local.set(initial_guess_value)
+
+    # Copy initial guess vector x0 into FE functions
+    offset = 0
+    x_array = x0.getArray(readonly=True)
+    for var in [u, p]:
+        size_local = var.vector.getLocalSize()
+        var_array = var.vector.getArray()
+        var_array[:] = x_array[offset:offset + size_local]
+        var.vector.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
+        offset += size_local
 
     # Ghosts are updated inside assemble_vector_block
     A0 = dolfin.fem.assemble_matrix_block(a_block, [bc])
@@ -101,8 +101,6 @@ def test_matrix_assembly_block():
         x0_sub.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
         x0_sub.copy(soln_sub.vector)
         soln_sub.vector.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
-        soln_sub.vector.copy(result=x1_sub)
-        x1_sub.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
 
     A1 = dolfin.fem.assemble_matrix_nest(a_block, [bc])
     b1 = dolfin.fem.assemble.assemble_vector_nest(L_block)
@@ -124,8 +122,8 @@ def test_matrix_assembly_block():
     u0, u1 = ufl.split(U)
     v0, v1 = dolfin.function.TestFunctions(W)
 
-    U.interpolate(lambda x: numpy.column_stack(
-        (initial_guess_u(x), initial_guess_p(x))))
+    with U.vector.localForm() as Ulocal:
+        Ulocal.set(initial_guess_value)
 
     F = inner(u0, v0) * dx + inner(u1, v0) * dx \
         + inner(u0, v1) * dx + inner(u1, v1) * dx \
@@ -246,23 +244,17 @@ def test_assembly_solve_block():
     V0 = dolfin.function.functionspace.FunctionSpace(mesh, P0)
     V1 = dolfin.function.functionspace.FunctionSpace(mesh, P1)
 
-    def bc_val_0(x):
-        return x[:, 0]**2 + x[:, 1]**2
-
-    def bc_val_1(x):
-        return numpy.sin(x[:, 0]) * numpy.cos(x[:, 1])
-
-    def initial_guess_u(x):
-        return numpy.sin(x[:, 0]) * numpy.sin(x[:, 1])
-
-    def initial_guess_p(x):
-        return -x[:, 0]**2 - x[:, 1]**3
+    bc_val_0 = 1.0
+    bc_val_1 = 2.0
+    initial_guess = 1.0
 
     def boundary(x):
         return numpy.logical_or(x[:, 0] < 1.0e-6, x[:, 0] > 1.0 - 1.0e-6)
 
     u_bc0 = dolfin.function.Function(V0)
-    u_bc0.interpolate(bc_val_0)
+    u_bc0.vector.set(bc_val_0)
+    u_bc0.vector.ghostUpdate(
+        addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
     u_bc1 = dolfin.function.Function(V1)
     u_bc1.vector.set(bc_val_1)
     u_bc1.vector.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
@@ -298,15 +290,9 @@ def test_assembly_solve_block():
     snes.setFunction(problem.F_block, Fvec0)
     snes.setJacobian(problem.J_block, J=Jmat0, P=None)
 
-    u.interpolate(initial_guess_u)
-    p.interpolate(initial_guess_p)
-
     x0 = dolfin.fem.create_vector_block(F)
-    dolfin.cpp.la.scatter_local_vectors(
-        x0, [u.vector.array_r, p.vector.array_r],
-        [u.function_space.dofmap.index_map, p.function_space.dofmap.index_map])
-    x0.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
-
+    with x0.localForm() as x0l:
+        x0l.set(initial_guess)
     snes.solve(None, x0)
 
     assert snes.getKSP().getConvergedReason() > 0
@@ -342,15 +328,12 @@ def test_assembly_solve_block():
     snes.setFunction(problem.F_nest, Fvec1)
     snes.setJacobian(problem.J_nest, J=Jmat1, P=None)
 
-    u.interpolate(initial_guess_u)
-    p.interpolate(initial_guess_p)
-
     x1 = dolfin.fem.create_vector_nest(F)
-    for x1_soln_pair in zip(x1.getNestSubVecs(), (u, p)):
-        x1_sub, soln_sub = x1_soln_pair
-        soln_sub.vector.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
-        soln_sub.vector.copy(result=x1_sub)
+    x1.set(initial_guess)
+    for x1_sub in x1.getNestSubVecs():
         x1_sub.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
+    u.vector.zeroEntries()
+    p.vector.zeroEntries()
 
     snes.solve(None, x1)
 
@@ -382,9 +365,11 @@ def test_assembly_solve_block():
     J = derivative(F, U, dU)
 
     u0_bc = dolfin.function.Function(V0)
-    u0_bc.interpolate(bc_val_0)
+    u0_bc.vector.set(bc_val_0)
+    u0_bc.vector.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
     u1_bc = dolfin.function.Function(V1)
-    u1_bc.interpolate(bc_val_1)
+    u1_bc.vector.set(bc_val_1)
+    u1_bc.vector.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
 
     bcs = [dolfin.fem.dirichletbc.DirichletBC(W.sub(0), u0_bc, boundary),
            dolfin.fem.dirichletbc.DirichletBC(W.sub(1), u1_bc, boundary)]
@@ -403,11 +388,9 @@ def test_assembly_solve_block():
     snes.setFunction(problem.F_mono, Fvec2)
     snes.setJacobian(problem.J_mono, J=Jmat2, P=None)
 
-    U.interpolate(lambda x: numpy.column_stack(
-        (initial_guess_u(x), initial_guess_p(x))))
     x2 = dolfin.fem.create_vector(F)
-    x2.array = U.vector.array_r
-
+    with x2.localForm() as x2l:
+        x2l.set(initial_guess)
     snes.solve(None, x2)
 
     assert snes.getKSP().getConvergedReason() > 0
@@ -428,7 +411,6 @@ def test_assembly_solve_block():
 ])
 def test_assembly_solve_taylor_hood(mesh):
     """Assemble Stokes problem with Taylor-Hood elements and solve."""
-    gdim = mesh.geometry.dim
     P2 = dolfin.function.functionspace.VectorFunctionSpace(mesh, ("Lagrange", 2))
     P1 = dolfin.function.functionspace.FunctionSpace(mesh, ("Lagrange", 1))
 
@@ -440,28 +422,11 @@ def test_assembly_solve_taylor_hood(mesh):
         """Define boundary x = 1"""
         return x[:, 0] > (1.0 - 10 * numpy.finfo(float).eps)
 
-    def initial_guess_u(x):
-        d = x.shape[1]
-        u_init = numpy.column_stack(
-            (numpy.sin(x[:, 0]) * numpy.sin(x[:, 1]),
-             numpy.cos(x[:, 0]) * numpy.cos(x[:, 1])))
-        if d == 3:
-            u_init = numpy.column_stack((u_init, numpy.cos(x[:, 2])))
-        return u_init
-
-    def initial_guess_p(x):
-        return -x[:, 0]**2 - x[:, 1]**3
-
-    u_bc_0 = dolfin.Function(P2)
-    u_bc_0.interpolate(
-        lambda x: numpy.column_stack(tuple(x[:, j] + float(j) for j in range(gdim))))
-
-    u_bc_1 = dolfin.Function(P2)
-    u_bc_1.interpolate(
-        lambda x: numpy.column_stack(tuple(numpy.sin(x[:, j]) for j in range(gdim))))
-
-    bcs = [dolfin.DirichletBC(P2, u_bc_0, boundary0),
-           dolfin.DirichletBC(P2, u_bc_1, boundary1)]
+    u0 = dolfin.Function(P2)
+    with u0.vector.localForm() as x:
+        x.set(1.0)
+    bcs = [dolfin.DirichletBC(P2, u0, boundary0),
+           dolfin.DirichletBC(P2, u0, boundary1)]
 
     u, p = dolfin.Function(P2), dolfin.Function(P1)
     du, dp = dolfin.TrialFunction(P2), dolfin.TrialFunction(P1)
@@ -491,15 +456,9 @@ def test_assembly_solve_taylor_hood(mesh):
     snes.setFunction(problem.F_block, Fvec0)
     snes.setJacobian(problem.J_block, J=Jmat0, P=Pmat0)
 
-    u.interpolate(initial_guess_u)
-    p.interpolate(initial_guess_p)
-
     x0 = dolfin.fem.create_vector_block(F)
-    dolfin.cpp.la.scatter_local_vectors(
-        x0, [u.vector.array_r, p.vector.array_r],
-        [u.function_space.dofmap.index_map, p.function_space.dofmap.index_map])
-    x0.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
-
+    with x0.localForm() as x0l:
+        x0l.set(0.0)
     snes.solve(None, x0)
 
     assert snes.getConvergedReason() > 0
@@ -532,16 +491,7 @@ def test_assembly_solve_taylor_hood(mesh):
     snes.setFunction(problem.F_nest, Fvec1)
     snes.setJacobian(problem.J_nest, J=Jmat1, P=Pmat1)
 
-    u.interpolate(initial_guess_u)
-    p.interpolate(initial_guess_p)
-
     x1 = dolfin.fem.create_vector_nest(F)
-    for x1_soln_pair in zip(x1.getNestSubVecs(), (u, p)):
-        x1_sub, soln_sub = x1_soln_pair
-        soln_sub.vector.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
-        soln_sub.vector.copy(result=x1_sub)
-        x1_sub.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
-
     x1.zeroEntries()
     snes.solve(None, x1)
 
@@ -568,8 +518,8 @@ def test_assembly_solve_taylor_hood(mesh):
     J = derivative(F, U, dU)
     P = inner(ufl.grad(du), ufl.grad(v)) * dx + inner(dp, q) * dx
 
-    bcs = [dolfin.DirichletBC(W.sub(0), u_bc_0, boundary0),
-           dolfin.DirichletBC(W.sub(0), u_bc_1, boundary1)]
+    bcs = [dolfin.DirichletBC(W.sub(0), u0, boundary0),
+           dolfin.DirichletBC(W.sub(0), u0, boundary1)]
 
     Jmat2 = dolfin.fem.create_matrix(J)
     Pmat2 = dolfin.fem.create_matrix(P)
@@ -586,11 +536,9 @@ def test_assembly_solve_taylor_hood(mesh):
     snes.setFunction(problem.F_mono, Fvec2)
     snes.setJacobian(problem.J_mono, J=Jmat2, P=Pmat2)
 
-    U.interpolate(lambda x: numpy.column_stack(
-        (initial_guess_u(x), initial_guess_p(x))))
-
     x2 = dolfin.fem.create_vector(F)
-    x2.array = U.vector.array_r
+    with x2.localForm() as x2l:
+        x2l.set(0.0)
     snes.solve(None, x2)
 
     assert snes.getConvergedReason() > 0
