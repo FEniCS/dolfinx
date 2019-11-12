c08f750e71e266e3ff83ed6c03ef3272f1910879
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2019-11-12 14:33:00 +0000

    Remove xfailing tests for spaces where dof points cannot be generated

--- python/test/unit/fem/test_dofmap.py
+++ python/test/unit/fem/test_dofmap.py
@@ -472,18 +472,9 @@ def test_readonly_view_local_to_global_unwoned(mesh):
 @pytest.mark.parametrize('space_type', [
     ("P", 1), ("P", 2), ("P", 3), ("P", 4),
     ("N1curl", 1),
-    pytest.param(("N1curl", 2), marks=pytest.mark.xfail),
-    pytest.param(("N1curl", 3), marks=pytest.mark.xfail),
-    pytest.param(("N1curl", 4), marks=pytest.mark.xfail),
     ("RT", 1), ("RT", 2), ("RT", 3), ("RT", 4),
     ("BDM", 1),
-    pytest.param(("BDM", 2), marks=pytest.mark.xfail),
-    pytest.param(("BDM", 3), marks=pytest.mark.xfail),
-    pytest.param(("BDM", 4), marks=pytest.mark.xfail),
-    ("N2curl", 1),
-    pytest.param(("N2curl", 2), marks=pytest.mark.xfail),
-    pytest.param(("N2curl", 3), marks=pytest.mark.xfail),
-    pytest.param(("N2curl", 4), marks=pytest.mark.xfail),
+    ("N2curl", 1)
 ])
 def test_triangle_dof_ordering(space_type):
     """Checks that dofs on shared triangle edges match up"""
@@ -528,17 +519,9 @@ def test_triangle_dof_ordering(space_type):
 @pytest.mark.parametrize('space_type', [
     ("P", 1), ("P", 2), ("P", 3), ("P", 4),
     ("N1curl", 1), ("N1curl", 2),
-    pytest.param(("N1curl", 3), marks=pytest.mark.xfail),
-    pytest.param(("N1curl", 4), marks=pytest.mark.xfail),
     ("RT", 1), ("RT", 2), ("RT", 3), ("RT", 4),
     ("BDM", 1),
-    pytest.param(("BDM", 2), marks=pytest.mark.xfail),
-    pytest.param(("BDM", 3), marks=pytest.mark.xfail),
-    pytest.param(("BDM", 4), marks=pytest.mark.xfail),
     ("N2curl", 1),
-    pytest.param(("N2curl", 2), marks=pytest.mark.xfail),
-    pytest.param(("N2curl", 3), marks=pytest.mark.xfail),
-    pytest.param(("N2curl", 4), marks=pytest.mark.xfail),
 ])
 def test_tetrahedron_dof_ordering(space_type):
     """Checks that dofs on shared tetrahedron edges and faces match up"""
@@ -591,7 +574,7 @@ def test_tetrahedron_dof_ordering(space_type):
 
 @skip_in_parallel
 @pytest.mark.parametrize('space_type', [
-    ("P", 1), ("P", 2), ("P", 3), ("P", 4),
+    ("P", 1), ("P", 2), ("P", 3), ("P", 4)
 ])
 def test_quadrilateral_dof_ordering(space_type):
     """Checks that dofs on shared quadrilateral edges match up"""
@@ -635,7 +618,7 @@ def test_quadrilateral_dof_ordering(space_type):
 
 @skip_in_parallel
 @pytest.mark.parametrize('space_type', [
-    ("P", 1), ("P", 2), ("P", 3), ("P", 4),
+    ("P", 1), ("P", 2), ("P", 3), ("P", 4)
 ])
 def test_hexahedron_dof_ordering(space_type):
     """Checks that dofs on shared hexahedron edges match up"""
