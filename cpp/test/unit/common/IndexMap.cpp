d84e1beabbc46f29b20e331d5316d5f62d08b1b0
igorbaratta <igorbaratta@gmail.com>
2019-11-14 11:30:27 +0000

    test insert and add modes

--- cpp/test/unit/common/IndexMap.cpp
+++ cpp/test/unit/common/IndexMap.cpp
@@ -17,6 +17,9 @@ namespace
 {
 void test_scatter_fwd()
 {
+  // Block size
+  auto n = GENERATE(1, 5, 10);
+
   const int mpi_size = dolfin::MPI::size(MPI_COMM_WORLD);
   const int mpi_rank = dolfin::MPI::rank(MPI_COMM_WORLD);
   const int size_local = 100;
@@ -32,30 +35,22 @@ void test_scatter_fwd()
 
   // Create some data to scatter
   const std::int64_t val = 11;
-  std::vector<std::int64_t> data_local(size_local, val * mpi_rank);
-  std::vector<std::int64_t> data_ghost(num_ghosts, -1);
+  std::vector<std::int64_t> data_local(n * size_local, val * mpi_rank);
+  std::vector<std::int64_t> data_ghost(n * num_ghosts, -1);
 
   // Scatter values to ghost and check value is correctly received
-  idx_map.scatter_fwd(data_local, data_ghost, 1);
+  idx_map.scatter_fwd(data_local, data_ghost, n);
+  CHECK(data_ghost.size() == n * num_ghosts);
   CHECK(std::all_of(data_ghost.begin(), data_ghost.end(), [=](auto i) {
-    return i == val * ((mpi_rank + 1) % mpi_size);
-  }));
-
-  // Test block of values
-  const int n = 7;
-  std::vector<std::int64_t> data_local_n(n * size_local, val * mpi_rank);
-  std::vector<std::int64_t> data_ghost_n(n * num_ghosts, -1);
-
-  // Scatter values to ghost and check value is correctly received
-  idx_map.scatter_fwd(data_local_n, data_ghost_n, n);
-  CHECK(data_ghost_n.size() == n * num_ghosts);
-  CHECK(std::all_of(data_ghost_n.begin(), data_ghost_n.end(), [=](auto i) {
     return i == val * ((mpi_rank + 1) % mpi_size);
   }));
 }
 
 void test_scatter_rev()
 {
+  // Block size
+  auto n = GENERATE(1, 5, 10);
+
   const int mpi_size = dolfin::MPI::size(MPI_COMM_WORLD);
   const int mpi_rank = dolfin::MPI::rank(MPI_COMM_WORLD);
   const int size_local = 100;
@@ -71,25 +66,23 @@ void test_scatter_rev()
 
   // Create some data, setting ghost values
   std::int64_t value = 15;
-  std::vector<std::int64_t> data_local(size_local, 0);
-  std::vector<std::int64_t> data_ghost(num_ghosts, value);
+  std::vector<std::int64_t> data_local(n * size_local, 0);
+  std::vector<std::int64_t> data_ghost(n * num_ghosts, value);
+  idx_map.scatter_rev(data_local, data_ghost, n, common::IndexMap::Mode::add);
 
-  // Scatter ghost values back to owner (sum)
-  idx_map.scatter_rev(data_local, data_ghost, 1, common::IndexMap::Mode::add);
-  std::int64_t sum = std::accumulate(data_local.begin(), data_local.end(), 0);
-  CHECK(sum == value * num_ghosts);
+  std::int64_t sum;
+  CHECK(data_local.size() == n * size_local);
+  sum = std::accumulate(data_local.begin(), data_local.end(), 0);
+  CHECK(sum == n * value * num_ghosts);
 
-  // Test block of values
-  const int n = 5;
-  std::vector<std::int64_t> data_local_n(n * size_local, 0);
-  std::vector<std::int64_t> data_ghost_n(n * num_ghosts, value);
-  idx_map.scatter_rev(data_local_n, data_ghost_n, n,
-                      common::IndexMap::Mode::add);
+  idx_map.scatter_rev(data_local, data_ghost, n,
+                      common::IndexMap::Mode::insert);
+  sum = std::accumulate(data_local.begin(), data_local.end(), 0);
+  CHECK(sum == n * value * num_ghosts);
 
-  CHECK(data_local_n.size() == n * size_local);
-  std::int64_t sum_n
-      = std::accumulate(data_local_n.begin(), data_local_n.end(), 0);
-  CHECK(sum_n == n * value * num_ghosts);
+  idx_map.scatter_rev(data_local, data_ghost, n, common::IndexMap::Mode::add);
+  sum = std::accumulate(data_local.begin(), data_local.end(), 0);
+  CHECK(sum == 2 * n * value * num_ghosts);
 }
 } // namespace
 
