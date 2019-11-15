dba495bd268cf27ccfa3eb103a1d1ac62eee5630
nate-sime <nsime@carnegiescience.edu>
2019-11-15 15:01:06 -0500

    updates

--- python/demo/stokes-taylor-hood/demo_stokes-taylor-hood.py
+++ python/demo/stokes-taylor-hood/demo_stokes-taylor-hood.py
@@ -64,23 +64,23 @@
 #
 # In this demo, we shall consider the following definitions of the input functions, the domain, and the boundaries:
 #
-# * :math:`\Omega = [0,1]\times[0,1] \backslash {\rm dolphin}` (a unit cube)
-# * :math:`\Gamma_D =`
-# * :math:`\Gamma_N =`
-# * :math:`u_0 = (- \sin(\pi x_1), 0.0)` for :math:`x_0 = 1` and :math:`u_0 = (0.0, 0.0)` otherwise
-# * :math:`f = (0.0, 0.0)`
-# * :math:`g = (0.0, 0.0)`
+# * :math:`\Omega = [0,1]\times[0,1] \setminus {\rm dolphin}` (a unit square)
+# * :math:`\Gamma_N = \{ 0 \} \times [0, 1]`
+# * :math:`\Gamma_D = \partial \Omega \setminus \Gamma_N`
+# * :math:`u_0 = (- \sin(\pi x_1), 0)^\top` at :math:`x_0 = 1` and :math:`u_0 = (0, 0)^\top` otherwise
+# * :math:`f = (0, 0)^\top`
+# * :math:`g = (0, 0)^\top`
 #
 #
 # Implementation
 # --------------
 #
 # In this example, different boundary conditions are prescribed on
-# different parts of the boundaries. Each sub-regions is tagged with
-# different (integer) labels. For this purpose, DOLFIN provides
+# different parts of the domain's exterior. Each sub-region is tagged with a
+# different (integer) label. For this purpose, DOLFIN provides
 # a :py:class:`MeshFunction <dolfin.cpp.mesh.MeshFunction>` class
 # representing functions over mesh entities (such as over cells or over
-# facets). Mesh and mesh functions can be read from file in the
+# facets). Meshes and mesh functions can be read from file in the
 # following way::
 
 from petsc4py import PETSc
@@ -105,10 +105,10 @@ sub_domains = xdmf.read_mf_size_t(mesh)
 cmap = dolfin.fem.create_coordinate_map(mesh.ufl_domain())
 mesh.geometry.coord_mapping = cmap
 
-# Next, we define a :py:class:`FunctionSpace
-# <dolfin.function.FunctionSpace>` built on a mixed finite element
-# ``TH`` which consists of continuous piecewise quadratics and
-# continuous piecewise linears::
+# Next, we define two :py:class:`FunctionSpace <dolfin.functions.functionspace.FunctionSpace>`
+# instances with different finite elements. ``P2`` corresponds to piecewise quadratics for
+# the velocity field and ``P1`` to continuous piecewise
+# linears for the pressure field::
 
 # Define function spaces
 P2 = VectorElement("Lagrange", mesh.ufl_cell(), 2)
@@ -117,10 +117,9 @@ P1 = FiniteElement("Lagrange", mesh.ufl_cell(), 1)
 V = FunctionSpace(mesh, P2)
 Q = FunctionSpace(mesh, P1)
 
-# The mixed finite element space is known as Taylor–Hood.
-# It is a stable, standard element pair for the Stokes
-# equations. Now we can define boundary conditions::
-
+# The product of these finite element spaces is known as the
+# Taylor–Hood mixed element. It is a standard stable element pair
+# for the Stokes equations. Now we can define boundary conditions::
 
 # Extract subdomain facet arrays
 mf = sub_domains.values
@@ -134,10 +133,8 @@ noslip.interpolate(lambda x: np.zeros_like(x[:mesh.geometry.dim]))
 
 bc0 = DirichletBC(V, noslip, mf0[0])
 
-# Inflow boundary condition for velocity
-# x0 = 1
 
-
+# Inflow boundary condition for velocity at x0 = 1
 def inflow_eval(x):
     values = np.zeros((2, x.shape[1]))
     values[0] = - np.sin(x[1] * np.pi)
@@ -154,42 +151,38 @@ bcs = [bc0, bc1]
 # The first argument to
 # :py:class:`DirichletBC <dolfin.cpp.fem.DirichletBC>`
 # specifies the :py:class:`FunctionSpace
-# <dolfin.cpp.function.FunctionSpace>`. Since we have a
-# mixed function space, we write
-# ``W.sub(0)`` for the velocity component of the space, and
-# ``W.sub(1)`` for the pressure component of the space.
+# <dolfin.cpp.function.FunctionSpace>`.
 # The second argument specifies the value on the Dirichlet
-# boundary. The last two arguments specify the marking of the subdomains:
-# ``sub_domains`` contains the subdomain markers, and the final argument is the subdomain index.
+# boundary. The last argument specifies the marking of the subdomains:
+# ``mf0`` and ``mf1`` contain the ``0`` and ``1`` subdomain markers,
+# respectively.
 #
-# The bilinear and linear forms corresponding to the weak mixed
-# formulation of the Stokes equations are defined as follows::
+# We now define the bilinear and linear forms corresponding to the weak mixed
+# formulation of the Stokes equations. In our implementation we write
+# these formulations in a blocked structure::
 
 # Define variational problem
 (u, p) = TrialFunction(V), TrialFunction(Q)
 (v, q) = TestFunction(V), TestFunction(Q)
-f = Function(V)
-g = Function(Q)
+f = dolfin.Constant(mesh, (0, 0))
 
-a = [[inner(grad(u), grad(v)) * dx,  inner(p, div(v)) * dx],
+a = [[inner(grad(u), grad(v)) * dx, inner(p, div(v)) * dx],
      [inner(div(u), q) * dx, None]]
 
 prec = [[a[0][0], None],
         [None, p * q * dx]]
 
-L = [inner(f, v) * dx, dolfin.Constant(mesh, 0) * q * dx]
+L = [inner(f, v) * dx,
+     dolfin.Constant(mesh, 0) * q * dx]
 
-# We also need to create a :py:class:`Function
-# <dolfin.cpp.function.Function>` to store the solution(s). The (full)
-# solution will be stored in ``w``, which we initialize using the mixed
-# function space ``W``. The actual
-# computation is performed by calling solve with the arguments ``a``,
-# ``L``, ``w`` and ``bcs``. The separate components ``u`` and ``p`` of
-# the solution can be extracted by calling the :py:meth:`split
-# <dolfin.functions.function.Function.split>` function. Here we use an
-# optional argument True in the split function to specify that we want a
-# deep copy. If no argument is given we will get a shallow copy. We want
-# a deep copy for further computations on the coefficient vectors::
+# With the bilinear form ``a``, preconditioner bilinear form ``prec`` and
+# linear right hand side (RHS) ``L``, we may now assembly the finite
+# element linear system. We exploit the structure of the Stokes
+# system and assemble the finite element system into block matrices and
+# a block vector. Provision of the ``bcs`` argument to :py:func:`assemble_matrix_nest
+# <dolfin.fem.assemble_matrix_nest>` ensures the rows and columns associated with
+# the boundary conditions are zeroed and the diagonal set to the identity,
+# preserving symmetry::
 
 A = dolfin.fem.create_matrix_nest(a)
 dolfin.fem.assemble_matrix_nest(A, a, bcs)
@@ -202,6 +195,13 @@ P.assemble()
 b = dolfin.fem.create_vector_nest(L)
 b.zeroEntries()
 
+# The boundary conditions we collected in ``bcs`` may now be applied
+# to the RHS block vector ``b``. In this case we must apply the lifting
+# operator to remove the columns of ``a`` corresponding to the boundary
+# conditions from ``b`` with :py:func:`apply_lifting_nest <dolfin.fem.assemble.apply_lifting_nest>`.
+# Thereafter we prescribe the values of the boundary conditions using
+# :py:func:`set_bc_nest <dolfin.fem.assemble.set_bc_nest>`::
+
 dolfin.fem.assemble.assemble_vector_nest(b, L)
 dolfin.fem.assemble.apply_lifting_nest(b, a, bcs)
 for b_sub in b.getNestSubVecs():
@@ -211,21 +211,54 @@ dolfin.fem.assemble.set_bc_nest(b, bcs0)
 
 b.assemble()
 
-# -- Nullspace
-x_nullspace = dolfin.fem.create_vector_nest(L)
-x_nullspace.zeroEntries()
-x_nullspace.getNestSubVecs()[1].set(1.0)
-x_nullspace.getNestSubVecs()[1].array /= x_nullspace.getNestSubVecs()[1].norm()
-x_nullspace.getNestSubVecs()[1].ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
-nullspace = PETSc.NullSpace().create(vectors=(x_nullspace,), comm=mesh.mpi_comm())
-A.setNullSpace(nullspace)
-P.setNullSpace(nullspace)
+# Now we are ready to create a Krylov Subspace Solver ``ksp``. We
+# configure it for block Jacobi preconditioning using PETSc's additive
+# fieldsplit composite type.
 
-# -- Near nullspace
+ksp = PETSc.KSP().create(mesh.mpi_comm())
+ksp.setOperators(A, P)
+
+# Setup the parent KSP
+ksp.setTolerances(rtol=1e-8)
+ksp.setType("fgmres")
+ksp.getPC().setType("fieldsplit")
+ksp.getPC().setFieldSplitType(PETSc.PC.CompositeType.ADDITIVE)
+
+# Supply the KSP with the velocity and pressure matrix index sets
+nested_IS = A.getNestISs()
+ksp.getPC().setFieldSplitIS(
+    ("u", nested_IS[0][0]),
+    ("p", nested_IS[0][1]))
+
+# Configure velocity and pressure sub KSPs
+ksp_u, ksp_p = ksp.getPC().getFieldSplitSubKSP()
+ksp_u.setType("preonly")
+ksp_u.getPC().setType("gamg")
+ksp_u.getPC().setGAMGType(PETSc.PC.GAMGType.AGG)
+ksp_p.setType("preonly")
+ksp_p.getPC().setType("hypre")
+
+# Monitor the convergence of the KSP
+opts = PETSc.Options()
+opts["ksp_monitor"] = None
+ksp.setFromOptions()
+
+# Given that we use PETSc's geometric algebraic multigrid (gamg) solver
+# with smoothed aggregration on the velocity block, we can provide the
+# near nullspace of rigid body modes to encourage convergence.
+# To do this we interpolate  the rigid body modes of displacement and
+# rotation in the velocity space and collect them in a
+# :py:class:`VectorSpaceBasis <dolfin.cpp.la.VectorSpaceBasis>`.
+# The rigid body mode vectors must be orthonormalised prior to solving
+# the problem, so we simply call
+# :py:func:`orthonormalize <dolfin.cpp.la.VectorSpaceBasis.orthonormalize>`.
+# These rigid body modes are then provided to the velocity sub block of
+# the preconditioning matrix ``P``.
+
+
 rbm_functions = [lambda x: np.row_stack((np.ones_like(x[1]), np.zeros_like(x[1]))),
                  lambda x: np.row_stack((np.zeros_like(x[1]), np.ones_like(x[1]))),
                  lambda x: np.row_stack((-x[1], x[0]))]
-
 rbms = [Function(V) for _ in range(3)]
 
 for (rbm, rbm_function) in zip(rbms, rbm_functions):
@@ -234,48 +267,26 @@ for (rbm, rbm_function) in zip(rbms, rbm_functions):
 rbms = dolfin.cpp.la.VectorSpaceBasis(list(map(lambda rbm: rbm.vector, rbms)))
 rbms.orthonormalize()
 
-near_nullspace = PETSc.NullSpace().create(vectors=tuple(rbms[i] for i in range(3)), comm=mesh.mpi_comm())
-A.getNestSubMatrix(0, 0).setNearNullSpace(near_nullspace)
+near_nullspace = PETSc.NullSpace().create(vectors=[rbms[i] for i in range(3)],
+                                          comm=mesh.mpi_comm())
 P.getNestSubMatrix(0, 0).setNearNullSpace(near_nullspace)
 
-ksp = PETSc.KSP().create(mesh.mpi_comm())
 
-ksp.setOperators(A, P)
-
-ksp.setTolerances(rtol=1e-8)
-ksp.setType("fgmres")
-ksp.getPC().setType("fieldsplit")
-ksp.getPC().setFieldSplitType(PETSc.PC.CompositeType.ADDITIVE)
-
-nested_IS = A.getNestISs()
-ksp.getPC().setFieldSplitIS(
-    ("u", nested_IS[0][0]), ("p", nested_IS[0][1]))
-
-# ksp.getPC().setUp()
-ksp_u, ksp_p = ksp.getPC().getFieldSplitSubKSP()
-ksp_u.setType("preonly")
-ksp_u.setTolerances(rtol=1e-3)
-ksp_u.getPC().setType("gamg")
-ksp_u.getPC().setGAMGType(PETSc.PC.GAMGType.AGG)
-ksp_p.setType("cg")
-ksp_p.setTolerances(rtol=1e-3)
-ksp_p.getPC().setType("hypre")
-
-opts = PETSc.Options()
-opts["ksp_monitor"] = None
-opts["ksp_monitor_lg_residualnorm"] = None
-ksp.setFromOptions()
+# We also need to create a block vector,``x``, to store the (full) solution,
+# which we initialize using the block RHS form ``L``.
 
 # Compute solution
 x = dolfin.fem.create_vector_nest(L)
 ksp.solve(b, x)
 
-# ksp.view()
-# We can calculate the :math:`L^2` norms of u and p as follows::
+# From the full solution vector ``x`` we can extract the sub vectors corresponding
+# to the velocity and pressure.
 
 u, p = Function(V), Function(Q)
-(u.vector.array, p.vector.array) = \
-    map(lambda vec: vec.array_r, x.getNestSubVecs())
+u.vector.array = x.getNestSubVecs()[0].array_r
+p.vector.array = x.getNestSubVecs()[1].array_r
+
+# We can calculate the :math:`L^2` norms of u and p as follows::
 
 print("Norm of velocity coefficient vector: %.15g" % u.vector.norm())
 print("Norm of pressure coefficient vector: %.15g" % p.vector.norm())
@@ -295,8 +306,6 @@ with XDMFFile(MPI.comm_world, "pressure.xdmf") as pfile_xdmf:
 # Plot solution
 plt.figure()
 plot(u, title="velocity")
-
-# plt.figure()
 plot(p, title="pressure" + str(MPI.rank(mesh.mpi_comm())))
 
 # Display plots
