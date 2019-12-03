d24f822519bd86c71150f0484c7b63098323d1b2
Francesco Ballarin <francesco.ballarin@sissa.it>
2019-12-03 12:52:23 +0100

    Test installed header files with two custom modules built with cppimport

--- python/test/unit/common/test_mpi.py
+++ python/test/unit/common/test_mpi.py
@@ -7,7 +7,13 @@
 # SPDX-License-Identifier:    LGPL-3.0-or-later
 
 import dolfin
+import mpi4py
+import os
+import pytest
+import sys
 from mpi4py import MPI
+from dolfin.jit import dolfin_pc, mpi_jit_decorator
+from dolfin_utils.test.fixtures import tempdir  # noqa: F401
 
 
 def test_mpi_comm_wrapper():
@@ -21,3 +27,55 @@ def test_mpi_comm_wrapper():
 
     assert isinstance(w1, MPI.Comm)
     assert isinstance(w2, MPI.Comm)
+
+
+def test_mpi_comm_wrapper_cppimport(tempdir):  # noqa: F811
+    """
+    Test MPICommWrapper <-> mpi4py.MPI.Comm conversion for code compiled with cppimport
+    """
+
+    cppimport = pytest.importorskip("cppimport")
+
+    @mpi_jit_decorator
+    def compile_module():
+        cpp_code_header = f"""
+        <%
+        setup_pybind11(cfg)
+        cfg['include_dirs'] += {dolfin_pc["include_dirs"] + [mpi4py.get_include()]}
+        cfg['compiler_args'] += {["-D" + dm for dm in dolfin_pc["define_macros"]]}
+        cfg['libraries'] += {dolfin_pc["libraries"]}
+        cfg['library_dirs'] += {dolfin_pc["library_dirs"]}
+        %>
+        """
+
+        cpp_code = """
+        #include <pybind11/pybind11.h>
+
+        #include <dolfin/pybind11/caster_mpi.h>
+
+        dolfin_wrappers::MPICommWrapper
+        test_comm_passing(const dolfin_wrappers::MPICommWrapper comm)
+        {
+          MPI_Comm c = comm.get();
+          return dolfin_wrappers::MPICommWrapper(c);
+        }
+
+        PYBIND11_MODULE(test_mpi_comm_wrapper, m)
+        {
+            m.def("test_comm_passing", &test_comm_passing);
+        }
+        """
+
+        open(os.path.join(tempdir, "test_mpi_comm_wrapper.cpp"), "w").write(cpp_code_header + cpp_code)
+
+        sys.path.append(tempdir)
+        return cppimport.imp("test_mpi_comm_wrapper")
+
+    module = compile_module()
+
+    w1 = MPI.COMM_WORLD
+    w2 = module.test_comm_passing(w1)
+
+    assert isinstance(w1, MPI.Comm)
+    assert isinstance(w2, MPI.Comm)
+    assert w1 == w2
