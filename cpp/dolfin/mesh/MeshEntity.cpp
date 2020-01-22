d02538bf83f66ce7ce73e743fd68173b9009b91e
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2020-01-22 10:30:25 +0000

    Added comments to MeshEntity

--- cpp/dolfin/mesh/MeshEntity.cpp
+++ cpp/dolfin/mesh/MeshEntity.cpp
@@ -43,17 +43,23 @@ int MeshEntity::facet_permutation(const MeshEntity& entity) const
   if (_mesh != entity._mesh)
     throw std::runtime_error("Mesh entity is defined on a different mesh");
 
+  // If the entity is a point, no permutation is required
   if (entity._dim == 0)
     return 0;
 
+  // If the entity is an interval, it should be oriented pointing from the
+  // lowest numbered vertex to the highest numbered vertex
   if (entity._dim == 1)
   {
     const int* e_vertices = get_local_vertex_indices(entity);
+    // Return the number of reflections
     return e_vertices[1] < e_vertices[0];
   }
 
+  // Triangles and quadrilaterals
   if (entity._dim == 2)
   {
+    // Find the index of the lowest numbered vertex
     int num_min = -1;
     const int* e_vertices = get_local_vertex_indices(entity);
     for (int v = 0; v < entity.num_entities(0); ++v)
@@ -61,18 +67,33 @@ int MeshEntity::facet_permutation(const MeshEntity& entity) const
         num_min = v;
     if (entity.num_entities(0) == 3)
     { // triangle
+      // pre is the number of the next vertex clockwise from the lowest numbered
+      // vertex
       const int pre = num_min == 0 ? e_vertices[entity.num_entities(0) - 1]
                                    : e_vertices[num_min - 1];
+      // post is the number of the next vertex anticlockwise from the lowest
+      // numbered vertex
       const int post = num_min == entity.num_entities(0) - 1
                            ? e_vertices[0]
                            : e_vertices[num_min + 1];
+      // Orient that triangle so the the lowest numbered vertex is the origin,
+      // and the next vertex anticlockwise from the lowest has a lower number
+      // than the next vertex clockwise. Return N such that N % 2 is the number
+      // of reflections and N / 2 is the number of rotations
       return 2 * num_min + (post > pre);
     }
     if (entity.num_entities(0) == 4)
     { // quadrilateral
-      int mult = num_min;
+      // rots is the number of rotations to get the lowest numbered vertex to
+      // the origin
+      int rots = num_min;
+      // pre is the (local) number of the next vertex clockwise from the lowest
+      // numbered vertex
       int pre = 2;
+      // post is the (local) number of the next vertex anticlockwise from the
+      // lowest numbered vertex
       int post = 1;
+      // The tensor product ordering of quads must be taken into account
       if (num_min == 1)
       {
         pre = 0;
@@ -82,15 +103,19 @@ int MeshEntity::facet_permutation(const MeshEntity& entity) const
       {
         pre = 3;
         post = 0;
-        mult = 3;
+        rots = 3;
       }
       else if (num_min == 3)
       {
         pre = 1;
         post = 2;
-        mult = 2;
+        rots = 2;
       }
-      return 2 * mult + (e_vertices[post] > e_vertices[pre]);
+      // Orient that quad so the the lowest numbered vertex is the origin, and
+      // the next vertex anticlockwise from the lowest has a lower number than
+      // the next vertex clockwise. Return N such that N % 2 is the number of
+      // reflections and N / 2 is the number of rotations
+      return 2 * rots + (e_vertices[post] > e_vertices[pre]);
     }
   }
   LOG(WARNING) << "No facet permutation was found for a facet. Integrals "
