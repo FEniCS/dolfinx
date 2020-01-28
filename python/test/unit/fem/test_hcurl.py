51c8c0939c962d54a20e590ac4510a8f907d02a0
igorbaratta <igorbaratta@gmail.com>
2020-01-28 15:10:38 -0300

    fix flake8 error

--- python/test/unit/fem/test_hcurl.py
+++ python/test/unit/fem/test_hcurl.py
@@ -18,11 +18,11 @@ import ufl
 def test_assembly_hcurl(space, degree):
     mesh = dolfinx.UnitSquareMesh(dolfinx.MPI.comm_world, 10, 10)
     k = 1
-    alpha = (2*numpy.pi**2 + k)
+    alpha = (2 * numpy.pi**2 + k)
 
     def expr(x):
-        return numpy.stack([numpy.cos(numpy.pi*x[0])*numpy.sin(numpy.pi*x[1]),
-                           -numpy.sin(numpy.pi*x[0])*numpy.cos(numpy.pi*x[1])],
+        return numpy.stack([numpy.cos(numpy.pi * x[0]) * numpy.sin(numpy.pi * x[1]),
+                           -numpy.sin(numpy.pi * x[0]) * numpy.cos(numpy.pi * x[1])],
                            axis=0)
 
     V = dolfinx.FunctionSpace(mesh, (space, degree))
@@ -31,7 +31,7 @@ def test_assembly_hcurl(space, degree):
     v = ufl.TestFunction(V)
 
     dim = mesh.topology.dim
-    boundaries = numpy.where(mesh.topology.on_boundary(dim-1))[0]
+    boundaries = numpy.where(mesh.topology.on_boundary(dim - 1))[0]
     u0 = dolfinx.Function(V)
     u0.vector.set(0.0)
     bc = dolfinx.DirichletBC(V, u0, boundaries)
@@ -40,8 +40,8 @@ def test_assembly_hcurl(space, degree):
     u_ex = dolfinx.Function(W)
     u_ex.interpolate(expr)
 
-    a = ufl.inner(ufl.curl(u), ufl.curl(v))*ufl.dx + k*ufl.inner(u, v)*ufl.dx
-    L = alpha*ufl.inner(u_ex, v)*ufl.dx
+    a = ufl.inner(ufl.curl(u), ufl.curl(v)) * ufl.dx + k * ufl.inner(u, v) * ufl.dx
+    L = alpha * ufl.inner(u_ex, v) * ufl.dx
 
     # Compute solution
     u = dolfinx.Function(V)
