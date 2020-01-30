06380ea503583b13a41480e59aa8f20c154b914b
Garth N. Wells <gnw20@cam.ac.uk>
2020-01-30 15:59:44 +0000

    Simplify fem pipeline test.

--- python/test/unit/fem/test_fem_pipeline.py
+++ python/test/unit/fem/test_fem_pipeline.py
@@ -29,7 +29,7 @@ from ufl import (SpatialCoordinate, TestFunction, TrialFunction, div, dx, grad,
 ])
 @pytest.mark.parametrize("degree", [2, 3, 4])
 def test_manufactured_poisson(degree, filename, datadir):
-    """ Manufactured Poisson problem, solving u = x[i]**p, where p is the
+    """ Manufactured Poisson problem, solving u = x[1]**p, where p is the
     degree of the Lagrange function space.
 
     """
@@ -42,74 +42,71 @@ def test_manufactured_poisson(degree, filename, datadir):
 
     # Get polynomial degree for bilinear form integrand (ignores effect
     # of non-affine map)
-    q2 = ufl.algorithms.estimate_total_polynomial_degree(inner(grad(u), grad(v)) * dx)
+    a = inner(grad(u), grad(v)) * dx(metadata={"quadrature_degree": -1})
+    a.integrals()[0].metadata()["quadrature_degree"] = ufl.algorithms.estimate_total_polynomial_degree(a)
 
-    # a = inner(grad(u), grad(v)) * dx
-    for component in range(mesh.geometry.dim):
+    # Source term
+    x = SpatialCoordinate(mesh)
+    u_exact = x[1]**degree
+    f = - div(grad(u_exact))
+    L = inner(f, v) * dx(metadata={"quadrature_degree": -1})
+    L.integrals()[0].metadata()["quadrature_degree"] = ufl.algorithms.estimate_total_polynomial_degree(L)
 
-        a = inner(grad(u), grad(v)) * dx(metadata={"quadrature_degree": q2})
+    t0 = time.time()
+    L = fem.Form(L)
+    t1 = time.time()
+    print("Linear form compile time:", t1 - t0)
 
-        # Source term
-        x = SpatialCoordinate(mesh)
-        u_exact = x[component]**degree
-        f = - div(grad(u_exact))
+    # Get polynomial degree for linear form integrand (ignores effect of
+    # non-affine map)
+    q1 = ufl.algorithms.estimate_total_polynomial_degree(inner(f, v) * dx)
 
-        # Get polynomial degree for linear form integrand (ignores
-        # effect of non-affine map)
-        q1 = ufl.algorithms.estimate_total_polynomial_degree(inner(f, v) * dx)
-        L = inner(f, v) * dx(metadata={"quadrature_degree": q1})
+    u_bc = Function(V)
+    u_bc.interpolate(lambda x: x[1]**degree)
+    bc = DirichletBC(V, u_bc, lambda x: np.full(x.shape[1], True))
 
-        u_bc = Function(V)
-        u_bc.interpolate(lambda x: x[component]**degree)
-        bc = DirichletBC(V, u_bc, lambda x: np.full(x.shape[1], True))
+    t0 = time.time()
+    b = assemble_vector(L)
+    apply_lifting(b, [a], [[bc]])
+    b.ghostUpdate(addv=PETSc.InsertMode.ADD, mode=PETSc.ScatterMode.REVERSE)
+    set_bc(b, [bc])
+    t1 = time.time()
+    print("Vector assembly time:", t1 - t0)
 
-        t0 = time.time()
-        L = fem.Form(L)
-        t1 = time.time()
-        print("Linear form compile time:", t1 - t0)
+    t0 = time.time()
+    a = fem.Form(a)
+    t1 = time.time()
+    print("Bilinear form compile time:", t1 - t0)
 
-        t0 = time.time()
-        b = assemble_vector(L)
-        apply_lifting(b, [a], [[bc]])
-        b.ghostUpdate(addv=PETSc.InsertMode.ADD, mode=PETSc.ScatterMode.REVERSE)
-        set_bc(b, [bc])
-        t1 = time.time()
-        print("Vector assembly time:", t1 - t0)
+    t0 = time.time()
+    A = assemble_matrix(a, [bc])
+    A.assemble()
+    t1 = time.time()
+    print("Matrix assembly time:", t1 - t0)
 
-        t0 = time.time()
-        a = fem.Form(a)
-        t1 = time.time()
-        print("Bilinear form compile time:", t1 - t0)
+    # Create LU linear solver
+    solver = PETSc.KSP().create(MPI.comm_world)
+    solver.setType(PETSc.KSP.Type.PREONLY)
+    solver.getPC().setType(PETSc.PC.Type.LU)
+    solver.setOperators(A)
 
-        t0 = time.time()
-        A = assemble_matrix(a, [bc])
-        A.assemble()
-        t1 = time.time()
-        print("Matrix assembly time:", t1 - t0)
+    # Solve
+    t0 = time.time()
+    uh = Function(V)
+    solver.solve(b, uh.vector)
+    uh.vector.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
+    t1 = time.time()
+    print("Linear solver time:", t1 - t0)
 
-        # Create LU linear solver
-        solver = PETSc.KSP().create(MPI.comm_world)
-        solver.setType(PETSc.KSP.Type.PREONLY)
-        solver.getPC().setType(PETSc.PC.Type.LU)
-        solver.setOperators(A)
+    M = (u_exact - uh)**2 * dx
+    t0 = time.time()
+    M = fem.Form(M)
+    t1 = time.time()
+    print("Error functional compile time:", t1 - t0)
 
-        # Solve
-        t0 = time.time()
-        uh = Function(V)
-        solver.solve(b, uh.vector)
-        uh.vector.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
-        t1 = time.time()
-        print("Linear solver time:", t1 - t0)
-
-        M = (u_exact - uh)**2 * dx
-        t0 = time.time()
-        M = fem.Form(M)
-        t1 = time.time()
-        print("Error functional compile time:", t1 - t0)
-
-        t0 = time.time()
-        error = assemble_scalar(M)
-        error = MPI.sum(mesh.mpi_comm(), error)
-        t1 = time.time()
-        print("Error assembly time:", t1 - t0)
-        assert np.absolute(error) < 1.0e-14
+    t0 = time.time()
+    error = assemble_scalar(M)
+    error = MPI.sum(mesh.mpi_comm(), error)
+    t1 = time.time()
+    print("Error assembly time:", t1 - t0)
+    assert np.absolute(error) < 1.0e-14
