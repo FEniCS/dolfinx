ff2270bcd24bb0f79a668d5087695f9a713d765f
Chris Richardson <chris@bpi.cam.ac.uk>
2020-02-11 13:34:17 +0000

    Start on cell ghosts [skip ci]

--- cpp/dolfinx/mesh/Mesh.cpp
+++ cpp/dolfinx/mesh/Mesh.cpp
@@ -335,7 +335,8 @@ Mesh::Mesh(
 
   // Initialise cell topology
   Eigen::Array<std::int64_t, Eigen::Dynamic, 1> cell_ghosts(num_ghost_cells);
-  assert(num_ghost_cells == 0); // Ghost cells not enabled at the moment
+  std::cout << "cell_ghosts = " << cell_ghosts << " \n";
+
   auto cell_index_map = std::make_shared<common::IndexMap>(
       _mpi_comm.comm(), num_cells_local, cell_ghosts, 1);
   _topology->set_index_map(tdim, cell_index_map);
