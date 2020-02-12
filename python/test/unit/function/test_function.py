d7635909da530f9d175919ec053734a1aaca53f0
jorgensd <dokken92@gmail.com>
2020-02-12 12:20:50 +0000

    Simplify test

--- python/test/unit/function/test_function.py
+++ python/test/unit/function/test_function.py
@@ -188,29 +188,28 @@ def test_eval_multiple(W):
     u.eval(x[0], cells[0])
 
 
-@pytest.mark.parametrize("degree", [2, 3, 4])
-@pytest.mark.parametrize("n_el", [4, 7])
-@pytest.mark.parametrize("n_p", [6, 11])
-def test_eval_parallel(degree, n_el, n_p):
-    mesh = UnitSquareMesh(MPI.comm_world, n_el, n_el)
+@pytest.mark.parametrize("num_elements", [4, 7])
+@pytest.mark.parametrize("num_points", [6, 11])
+def test_eval_parallel(num_elements, num_points):
+    mesh = UnitSquareMesh(MPI.comm_world, num_elements, num_elements)
     cmap = fem.create_coordinate_map(mesh.ufl_domain())
     mesh.geometry.coord_mapping = cmap
 
-    P = ufl.FiniteElement("Lagrange", mesh.ufl_cell(), degree)
+    P = ufl.FiniteElement("Lagrange", mesh.ufl_cell(), 1)
     V = FunctionSpace(mesh, P)
 
     def func(x):
-        return x[0] * x[1]
+        return x[0] + x[1]
 
     u = Function(V)
     u.interpolate(func)
 
-    x_line = np.linspace(0, 1, n_p)
-    y_line = np.linspace(0, 0.9, n_p)
+    x_line = np.linspace(0, 1, num_points)
+    y_line = np.linspace(0, 0.9, num_points)
 
     tree = geometry.BoundingBoxTree(mesh, mesh.geometry.dim)
 
-    points = np.vstack((x_line, y_line, np.zeros(n_p)))
+    points = np.vstack((x_line, y_line, np.zeros(num_points)))
 
     cells = geometry.compute_first_entity_collision(tree, mesh, points.T)
     u_eval = u.eval(points.T, cells)
@@ -218,11 +217,10 @@ def test_eval_parallel(degree, n_el, n_p):
     exact = func(points)
     local_cells = np.argwhere(cells != -1).T[0]
 
-    on_proc = np.zeros(cells.shape[0])
-    on_proc[local_cells] = 1
-
     # Workaround since the cell exists on multiple processors, not respecting
     # ghosting.
+    on_proc = np.zeros(cells.shape[0])
+    on_proc[local_cells] = 1
     u_g = MPI.comm_world.allgather(u_eval)
     num_proc = MPI.comm_world.allgather(on_proc)
     u_global = sum(u_g) / sum(num_proc)
