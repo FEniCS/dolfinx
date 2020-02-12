0501c2aa4fcae9e5857e9d8154233fb566ad979f
Garth N. Wells <gnw20@cam.ac.uk>
2020-02-12 17:08:55 +0000

    Doc fix.

--- cpp/dolfin/fem/DiscreteOperators.h
+++ cpp/dolfin/fem/DiscreteOperators.h
@@ -40,9 +40,9 @@ class DiscreteOperators
 {
 public:
   /// Build the discrete gradient operator A that takes a
-  /// \f$w \in H^1\f$ (P1, nodal Lagrange) to \f$v \in H(curl)\f$
-  /// (lowest order Nedelec), i.e. v = Aw. V0 is the H(curl) space,
-  /// and V1 is the P1 Lagrange space.
+  /// \f$w \in H^1\f$ (P1, nodal Lagrange) to \f$v \in H(curl)\f$ (lowest order
+  /// Nedelec), i.e. v = Aw. V0 is the H(curl) space, and V1 is the P1
+  /// Lagrange space.
   ///
   /// @param[in] V0 H(curl) space
   /// @param[in] V1 P1 Lagrange space
