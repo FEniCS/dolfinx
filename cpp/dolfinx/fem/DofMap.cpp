1a23175ae6d794af9eb883afa79e8363c91566dc
Garth N. Wells <gnw20@cam.ac.uk>
2020-02-12 09:05:05 +0000

    Move dof re-mapping to separate function.

--- cpp/dolfinx/fem/DofMap.cpp
+++ cpp/dolfinx/fem/DofMap.cpp
@@ -19,6 +19,16 @@ using namespace dolfinx::fem;
 
 namespace
 {
+template <typename T>
+Eigen::Array<std::int32_t, Eigen::Dynamic, 1>
+remap_dofs(const std::vector<std::int32_t>& old_to_new,
+           const Eigen::Array<T, Eigen::Dynamic, 1>& dofs_old)
+{
+  Eigen::Array<std::int32_t, Eigen::Dynamic, 1> dofmap(dofs_old.rows());
+  for (Eigen::Index i = 0; i < dofmap.size(); ++i)
+    dofmap[i] = old_to_new[dofs_old[i]];
+  return dofmap;
+}
 
 // Build a collapsed DofMap from a dofmap view
 fem::DofMap build_collapsed_dofmap(MPI_Comm comm, const DofMap& dofmap_view,
@@ -121,16 +131,15 @@ fem::DofMap build_collapsed_dofmap(MPI_Comm comm, const DofMap& dofmap_view,
   // Build new dofmap
   const Eigen::Array<PetscInt, Eigen::Dynamic, 1>& dof_array_view
       = dofmap_view.dof_array();
-  Eigen::Array<std::int32_t, Eigen::Dynamic, 1> _dofmap(dof_array_view.size());
-  for (Eigen::Index i = 0; i < _dofmap.size(); ++i)
-    _dofmap[i] = old_to_new[dof_array_view[i]];
+  Eigen::Array<std::int32_t, Eigen::Dynamic, 1> dofmap
+      = remap_dofs(old_to_new, dof_array_view);
 
   // Dimension sanity checks
   assert(element_dof_layout);
-  assert(_dofmap.size()
+  assert(dofmap.rows()
          == (cells->num_nodes() * element_dof_layout->num_dofs()));
 
-  return fem::DofMap(element_dof_layout, index_map, _dofmap);
+  return fem::DofMap(element_dof_layout, index_map, dofmap);
 }
 } // namespace
 
