302f7072335019a5b327001dcbb0855a7c1c8db8
Joe <jpd62@cam.ac.uk>
2020-02-16 18:43:56 +0000

    Fix type error

--- cpp/demo/hyperelasticity/main.cpp
+++ cpp/demo/hyperelasticity/main.cpp
@@ -145,13 +145,15 @@ int main(int argc, char* argv[])
   // Create Dirichlet boundary conditions
   auto u0 = std::make_shared<function::Function>(V);
 
+  std::vector<std::reference_wrapper<function::FunctionSpace>> Vs;
+  Vs.push_back(*V);
   const Eigen::Array<std::int32_t, Eigen::Dynamic, 1> bdofs_left
       = fem::locate_dofs_geometrical(
-          *V, [](auto x) { return x.row(0) < DBL_EPSILON; });
+          Vs, [](auto x) { return x.row(0) < DBL_EPSILON; });
 
   const Eigen::Array<std::int32_t, Eigen::Dynamic, 1> bdofs_right
       = fem::locate_dofs_geometrical(
-          *V, [](auto& x) { return (x.row(0) - 1.0).abs() < DBL_EPSILON; });
+          Vs, [](auto& x) { return (x.row(0) - 1.0).abs() < DBL_EPSILON; });
 
   std::vector<std::shared_ptr<const fem::DirichletBC>> bcs
       = {std::make_shared<fem::DirichletBC>(u_clamp, bdofs_left),
