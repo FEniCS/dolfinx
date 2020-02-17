b11c164d99ae75015531e988dc57a2c8937c3bb8
Garth N. Wells <gnw20@cam.ac.uk>
2020-02-17 20:53:01 +0000

    More transation.

--- cpp/dolfinx/mesh/Topology.cpp
+++ cpp/dolfinx/mesh/Topology.cpp
@@ -105,7 +105,9 @@ std::shared_ptr<const common::IndexMap> Topology::index_map(int dim) const
 //-----------------------------------------------------------------------------
 const std::vector<std::int64_t>& Topology::global_indices(int d) const
 {
-  if (d == 0 or d == this->dim())
+  // if (d == 0 or d == this->dim())
+  //   return _global_indices[d];
+  if (d == 0)
     return _global_indices[d];
 
   auto it = _global_indices_tmp.find(d);
