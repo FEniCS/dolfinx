231fded8dfd44f729a9035972105f0f30f4a8235
Garth N. Wells <gnw20@cam.ac.uk>
2020-02-18 18:52:44 +0000

    Remove left over shared_entities (#789)
    * Remove left over shared_entities
    
    * Remove some includes.

--- cpp/dolfinx/mesh/Topology.h
+++ cpp/dolfinx/mesh/Topology.h
@@ -11,9 +11,7 @@
 #include <array>
 #include <cstdint>
 #include <dolfinx/graph/AdjacencyList.h>
-#include <map>
 #include <memory>
-#include <set>
 #include <vector>
 
 namespace dolfinx
@@ -134,11 +132,6 @@ private:
 
   // Global indices for mesh entities
   std::vector<std::vector<std::int64_t>> _global_indices;
-
-  // TODO: Could IndexMap be used here in place of std::map?
-  // For entities of a given dimension d, maps each shared entity
-  // (local index) to a list of the processes sharing the vertex
-  std::vector<std::map<std::int32_t, std::set<std::int32_t>>> _shared_entities;
 
   // IndexMap to store ghosting for each entity dimension
   std::array<std::shared_ptr<const common::IndexMap>, 4> _index_map;
