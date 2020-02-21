3ad78258b424c470d99ea02cfea005d9cc4d8080
Francesco Ballarin <francesco.ballarin@sissa.it>
2020-02-21 17:04:34 +0100

    Replace std::inclusive_scan with std::partial_sum, as the former requires gcc >= 9.2.1 (#799)

--- cpp/dolfinx/fem/DirichletBC.cpp
+++ cpp/dolfinx/fem/DirichletBC.cpp
@@ -61,8 +61,7 @@ get_remote_bcs1(const common::IndexMap& map,
 
   // Compute displacements for data to receive. Last entry has total
   // number of received items.
-  // Note: std::inclusive_scan would be better, but gcc 7.4.0 (Ubuntu
-  // 18.04) does not have full C++17 support
+  // Note: std::inclusive_scan would be better, but requires gcc >= 9.2.1
   std::vector<int> disp(num_neighbours + 1, 0);
   std::partial_sum(num_dofs_recv.begin(), num_dofs_recv.end(),
                    disp.begin() + 1);
@@ -133,8 +132,7 @@ get_remote_bcs2(const common::IndexMap& map0, const common::IndexMap& map1,
 
   // Compute displacements for data to receive. Last entry has total
   // number of received items.
-  // Note: std::inclusive_scan would be better, but gcc 7.4.0 (Ubuntu
-  // 18.04) does not have full C++17 support
+  // Note: std::inclusive_scan would be better, but requires gcc >= 9.2.1
   std::vector<int> disp(num_neighbours + 1, 0);
   std::partial_sum(num_dofs_recv.begin(), num_dofs_recv.end(),
                    disp.begin() + 1);
