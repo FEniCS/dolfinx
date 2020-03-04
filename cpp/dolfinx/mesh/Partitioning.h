c186d7c0840ff95f23aa95643e06d6828e21bdb1
Igor Baratta <igorbaratta@gmail.com>
2020-03-04 08:55:06 -0300

    Fix ghosting in KaHIP and ParMetis (#838)
    * Fixes for new partitioning output in KaHIP and ParMETIS
    
    Co-authored-by: Chris Richardson <chris@bpi.cam.ac.uk>

--- cpp/dolfinx/mesh/Partitioning.h
+++ cpp/dolfinx/mesh/Partitioning.h
@@ -123,8 +123,7 @@ public:
       const Eigen::Ref<const Eigen::Array<std::int64_t, Eigen::Dynamic,
                                           Eigen::Dynamic, Eigen::RowMajor>>&
           cell_vertices,
-      const mesh::Partitioner graph_partitioner,
-      mesh::GhostMode ghost_mode);
+      const mesh::Partitioner graph_partitioner, mesh::GhostMode ghost_mode);
 
   /// Redistribute points to the processes that need them
   /// @param[in] comm MPI Communicator
