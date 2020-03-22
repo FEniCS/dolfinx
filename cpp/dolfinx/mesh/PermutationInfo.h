4239bb85f7e8925cf560864f2e21f7904df5088b
Garth N. Wells <gnw20@cam.ac.uk>
2020-03-22 09:23:15 +0000

    Fix constructor-related build errors (#898)
    * Fix constructor compiler errors.
    
    * Small updates.

--- cpp/dolfinx/mesh/PermutationInfo.h
+++ cpp/dolfinx/mesh/PermutationInfo.h
@@ -7,12 +7,7 @@
 #pragma once
 
 #include <Eigen/Dense>
-#include <array>
 #include <cstdint>
-#include <dolfinx/graph/AdjacencyList.h>
-#include <dolfinx/graph/Partitioning.h>
-#include <memory>
-#include <vector>
 
 namespace dolfinx
 {
@@ -22,8 +17,9 @@ namespace mesh
 
 class Topology;
 
-/// Information about how the entities of each cell should be permuted to get to a
-/// low-to-high ordering
+/// Information about how the entities of each cell should be permuted
+/// to get to a low-to-high ordering
+
 class PermutationInfo
 {
 public:
@@ -39,31 +35,41 @@ public:
   /// Destructor
   ~PermutationInfo() = default;
 
-  /// Get the permutation numbers to apply to facets.
-  /// The permutations are numbered so that:
-  ///   n%2 gives the number of reflections to apply
-  ///   n//2 gives the number of rotations to apply
-  /// Each column of the returned array represents a cell, and each row a
-  /// facet of that cell.
-  /// This data is used to permute the quadrature point on facet integrals when
-  /// data from the cells on both sides of the facet is used.
+  /// Assignment
+  PermutationInfo& operator=(const PermutationInfo& info) = default;
+
+  /// Assignment
+  PermutationInfo& operator=(PermutationInfo&& info) = default;
+
+  /// Get the permutation numbers to apply to facets. The permutations
+  /// are numbered so that:
+  ///
+  ///   n % 2 gives the number of reflections to apply
+  ///
+  ///   n // 2 gives the number of rotations to apply
+  ///
+  /// Each column of the returned array represents a cell, and each row
+  /// a facet of that cell. This data is used to permute the quadrature
+  /// point on facet integrals when data from the cells on both sides of
+  /// the facet is used.
   /// @return An array of permutation numbers.
   const Eigen::Array<std::uint8_t, Eigen::Dynamic, Eigen::Dynamic>&
   get_facet_permutations() const;
 
-  /// Get the permutation information about the entities of each cell, relative
-  /// to a low-to-high ordering. This data is packed so that a 32 bit int is
-  /// used for each cell. For 2D cells, one bit is used for each edge, to
-  /// represent whether or not the edge is reversed: the least significant bit
-  /// is for edge 0, the next for edge 1, etc. For 3D cells, three bits are used
-  /// for each face, and for each edge: the least significant bit says whether
-  /// or not face 0 is reflected, the next 2 bits say how many times face 0 is
-  /// rotated; the next three bits are for face 1, then three for face 2, etc;
-  /// after all the faces, there is 1 bit for each edge to say whether or not
-  /// they are reversed.
+  /// Get the permutation information about the entities of each cell,
+  /// relative to a low-to-high ordering. This data is packed so that a
+  /// 32 bit int is used for each cell. For 2D cells, one bit is used
+  /// for each edge, to represent whether or not the edge is reversed:
+  /// the least significant bit is for edge 0, the next for edge 1, etc.
+  /// For 3D cells, three bits are used for each face, and for each
+  /// edge: the least significant bit says whether or not face 0 is
+  /// reflected, the next 2 bits say how many times face 0 is rotated;
+  /// the next three bits are for face 1, then three for face 2, etc;
+  /// after all the faces, there is 1 bit for each edge to say whether
+  /// or not they are reversed.
   ///
-  /// For example, if a quadrilateral has cell permutation info
-  /// ....0111 then (from right to left):
+  /// For example, if a quadrilateral has cell permutation info ....0111
+  /// then (from right to left):
   ///
   ///   - edge 0 is reflected (1)
   ///   - edge 1 is reflected (1)
@@ -84,12 +90,14 @@ public:
   ///   - edge 4 is reflected (1)
   ///   - edge 5 is not permuted (0)
   ///
-  /// This data is used to correct the direction of vector function on permuted
-  /// facets.
-  /// @return A vector of cell permutation info ints.
+  /// This data is used to correct the direction of vector function on
+  /// permuted facets.
+  /// @return A vector of cell permutation info ints
   const Eigen::Array<std::uint32_t, Eigen::Dynamic, 1>&
   get_cell_permutation_info() const;
 
+  /// @todo Avoid in/out argument
+  ///
   /// Compute entity permutations and reflections used in assembly
   void create_entity_permutations(Topology& topology);
 
@@ -98,9 +106,8 @@ private:
   Eigen::Array<std::uint8_t, Eigen::Dynamic, Eigen::Dynamic>
       _facet_permutations;
 
-  // Cell permutation info
-  // See the documentation for get_cell_permutation_info for documentation of
-  // how this is encoded.
+  // Cell permutation info. See the documentation for
+  // get_cell_permutation_info for documentation of how this is encoded.
   Eigen::Array<std::uint32_t, Eigen::Dynamic, 1> _cell_permutation_info;
 };
 
