5e812fce0fa52b821ce8cd601411dd25b422144f
Matthias Rambausek <matthias.rambausek@gmail.com>
2020-04-19 00:00:52 +0200

    working towards abstractions

--- cpp/dolfinx/mesh/TopologyStorage.cpp
+++ cpp/dolfinx/mesh/TopologyStorage.cpp
@@ -14,9 +14,7 @@ using namespace dolfinx;
 using namespace dolfinx::mesh;
 using namespace dolfinx::mesh::storage;
 
-//
-namespace dolfinx::mesh::storage::internal
-{
+
 // TopologyStorageLayer free functions
 // ===================================
 //------------------------------------------------------------------------------
@@ -103,133 +101,120 @@ facet_permutations(const TopologyStorageLayer& storage)
   return storage.facet_permutations;
 }
 //------------------------------------------------------------------------------
-} // namespace dolfinx::mesh::storage::internal
-//------------------------------------------------------------------------------
 //
 //
-// StorageLock
-// ========
-//------------------------------------------------------------------------------
-StorageLock::~StorageLock()
-{
-  if (!storage.second.expired() && storage.first)
-    storage.first->remove_expired_layers();
-}
-//------------------------------------------------------------------------------
-//
-//
-// TopologyStorage
-// ===============
-//------------------------------------------------------------------------------
-std::shared_ptr<const common::IndexMap> TopologyStorage::set_index_map(
-    std::shared_ptr<const common::IndexMap> index_map, int dim)
-{
-  if (layers.empty())
-    throw std::runtime_error(
-        "No storage layer present. Acquire a cache lock before writing.");
-  return internal::set_index_map(active_layer().first, index_map, dim);
-}
-//------------------------------------------------------------------------------
-std::shared_ptr<const common::IndexMap>
-TopologyStorage::index_map(int dim) const
-{
-  using func_t = std::function<std::shared_ptr<const common::IndexMap>(
-      const StorageLayer&)>;
-  func_t read = [=](const StorageLayer& storage) {
-    return internal::index_map(storage, dim);
-  };
-  return read_from_storage<func_t::result_type::element_type>(std::move(read));
-}
-//------------------------------------------------------------------------------
-std::shared_ptr<const graph::AdjacencyList<std::int32_t>>
-TopologyStorage::connectivity(int d0, int d1) const
-{
-  using func_t
-      = std::function<std::shared_ptr<const graph::AdjacencyList<std::int32_t>>(
-          const StorageLayer&)>;
-  func_t read = [=](const StorageLayer& storage) {
-    return internal::connectivity(storage, d0, d1);
-  };
-  return read_from_storage<func_t::result_type::element_type>(std::move(read));
-}
-//------------------------------------------------------------------------------
-std::shared_ptr<const graph::AdjacencyList<std::int32_t>>
-TopologyStorage::set_connectivity(
-    std::shared_ptr<const graph::AdjacencyList<std::int32_t>> c, int d0, int d1)
-{
-  if (layers.empty())
-    throw std::runtime_error(
-        "No storage layer present. Acquire a cache lock before writing.");
-  return internal::set_connectivity(active_layer().first, c, d0, d1);
-}
-//------------------------------------------------------------------------------
-std::shared_ptr<const Eigen::Array<std::uint32_t, Eigen::Dynamic, 1>>
-TopologyStorage::cell_permutations() const
-{
-  using func_t = std::function<
-      std::shared_ptr<const Eigen::Array<std::uint32_t, Eigen::Dynamic, 1>>(
-          const StorageLayer&)>;
-  func_t read = [=](const StorageLayer& storage) {
-    return internal::cell_permutations(storage);
-  };
-  return read_from_storage<func_t::result_type::element_type>(std::move(read));
-}
-//------------------------------------------------------------------------------
-std::shared_ptr<const Eigen::Array<std::uint32_t, Eigen::Dynamic, 1>>
-TopologyStorage::set_cell_permutations(
-    std::shared_ptr<const Eigen::Array<std::uint32_t, Eigen::Dynamic, 1>>
-        cell_permutations)
-{
-  if (layers.empty())
-    throw std::runtime_error(
-        "No storage layer present. Acquire a cache lock before writing.");
-  return internal::set_cell_permutations(active_layer().first,
-                                         cell_permutations);
-}
-//------------------------------------------------------------------------------
-std::shared_ptr<
-    const Eigen::Array<std::uint8_t, Eigen::Dynamic, Eigen::Dynamic>>
-TopologyStorage::facet_permutations() const
-{
-  using func_t = std::function<std::shared_ptr<const Eigen::Array<
-      std::uint8_t, Eigen::Dynamic, Eigen::Dynamic>>(const StorageLayer&)>;
-  func_t read = [=](const StorageLayer& storage) {
-    return internal::facet_permutations(storage);
-  };
-  return read_from_storage<func_t::result_type::element_type>(std::move(read));
-}
-//------------------------------------------------------------------------------
-std::shared_ptr<
-    const Eigen::Array<std::uint8_t, Eigen::Dynamic, Eigen::Dynamic>>
-TopologyStorage::set_facet_permutations(
-    std::shared_ptr<
-        const Eigen::Array<std::uint8_t, Eigen::Dynamic, Eigen::Dynamic>>
-        facet_permutations)
-{
-  if (layers.empty())
-    throw std::runtime_error(
-        "No storage layer present. Acquire a cache lock before writing.");
-  return internal::set_facet_permutations(active_layer().first,
-                                          facet_permutations);
-}
-//------------------------------------------------------------------------------
-std::shared_ptr<const std::vector<bool>>
-TopologyStorage::interior_facets() const
-{
-  using func_t = std::function<std::shared_ptr<const std::vector<bool>>(
-      const StorageLayer&)>;
-  func_t read = [=](const StorageLayer& storage) {
-    return internal::interior_facets(storage);
-  };
-  return read_from_storage<func_t::result_type::element_type>(std::move(read));
-}
-//------------------------------------------------------------------------------
-std::shared_ptr<const std::vector<bool>> TopologyStorage::set_interior_facets(
-    std::shared_ptr<const std::vector<bool>> interior_facets)
-{
-  if (layers.empty())
-    throw std::runtime_error(
-        "No storage layer present. Acquire a cache lock before writing.");
-  return internal::set_interior_facets(active_layer().first, interior_facets);
-}
-//------------------------------------------------------------------------------
+//// TopologyStorage
+//// ===============
+////------------------------------------------------------------------------------
+//std::shared_ptr<const common::IndexMap> TopologyStorage::set_index_map(
+//    std::shared_ptr<const common::IndexMap> index_map, int dim)
+//{
+//  if (layers.empty())
+//    throw std::runtime_error(
+//        "No storage layer present. Acquire a cache lock before writing.");
+//  return set_index_map(active_layer().first, index_map, dim);
+//}
+////------------------------------------------------------------------------------
+//std::shared_ptr<const common::IndexMap>
+//TopologyStorage::index_map(int dim) const
+//{
+//  using func_t = std::function<std::shared_ptr<const common::IndexMap>(
+//      const StorageLayer&)>;
+//  func_t read = [=](const StorageLayer& storage) {
+//    return index_map(storage, dim);
+//  };
+//  return read_from_storage<func_t::result_type::element_type>(std::move(read));
+//}
+////------------------------------------------------------------------------------
+//std::shared_ptr<const graph::AdjacencyList<std::int32_t>>
+//TopologyStorage::connectivity(int d0, int d1) const
+//{
+//  using func_t
+//      = std::function<std::shared_ptr<const graph::AdjacencyList<std::int32_t>>(
+//          const StorageLayer&)>;
+//  func_t read = [=](const StorageLayer& storage) {
+//    return connectivity(storage, d0, d1);
+//  };
+//  return read_from_storage<func_t::result_type::element_type>(std::move(read));
+//}
+////------------------------------------------------------------------------------
+//std::shared_ptr<const graph::AdjacencyList<std::int32_t>>
+//TopologyStorage::set_connectivity(
+//    std::shared_ptr<const graph::AdjacencyList<std::int32_t>> c, int d0, int d1)
+//{
+//  if (layers.empty())
+//    throw std::runtime_error(
+//        "No storage layer present. Acquire a cache lock before writing.");
+//  return set_connectivity(active_layer().first, c, d0, d1);
+//}
+////------------------------------------------------------------------------------
+//std::shared_ptr<const Eigen::Array<std::uint32_t, Eigen::Dynamic, 1>>
+//TopologyStorage::cell_permutations() const
+//{
+//  using func_t = std::function<
+//      std::shared_ptr<const Eigen::Array<std::uint32_t, Eigen::Dynamic, 1>>(
+//          const StorageLayer&)>;
+//  func_t read = [=](const StorageLayer& storage) {
+//    return cell_permutations(storage);
+//  };
+//  return read_from_storage<func_t::result_type::element_type>(std::move(read));
+//}
+////------------------------------------------------------------------------------
+//std::shared_ptr<const Eigen::Array<std::uint32_t, Eigen::Dynamic, 1>>
+//TopologyStorage::set_cell_permutations(
+//    std::shared_ptr<const Eigen::Array<std::uint32_t, Eigen::Dynamic, 1>>
+//        cell_permutations)
+//{
+//  if (layers.empty())
+//    throw std::runtime_error(
+//        "No storage layer present. Acquire a cache lock before writing.");
+//  return set_cell_permutations(active_layer().first,
+//                                         cell_permutations);
+//}
+////------------------------------------------------------------------------------
+//std::shared_ptr<
+//    const Eigen::Array<std::uint8_t, Eigen::Dynamic, Eigen::Dynamic>>
+//TopologyStorage::facet_permutations() const
+//{
+//  using func_t = std::function<std::shared_ptr<const Eigen::Array<
+//      std::uint8_t, Eigen::Dynamic, Eigen::Dynamic>>(const StorageLayer&)>;
+//  func_t read = [=](const StorageLayer& storage) {
+//    return facet_permutations(storage);
+//  };
+//  return read_from_storage<func_t::result_type::element_type>(std::move(read));
+//}
+////------------------------------------------------------------------------------
+//std::shared_ptr<
+//    const Eigen::Array<std::uint8_t, Eigen::Dynamic, Eigen::Dynamic>>
+//TopologyStorage::set_facet_permutations(
+//    std::shared_ptr<
+//        const Eigen::Array<std::uint8_t, Eigen::Dynamic, Eigen::Dynamic>>
+//        facet_permutations)
+//{
+//  if (layers.empty())
+//    throw std::runtime_error(
+//        "No storage layer present. Acquire a cache lock before writing.");
+//  return set_facet_permutations(active_layer().first,
+//                                          facet_permutations);
+//}
+////------------------------------------------------------------------------------
+//std::shared_ptr<const std::vector<bool>>
+//TopologyStorage::interior_facets() const
+//{
+//  using func_t = std::function<std::shared_ptr<const std::vector<bool>>(
+//      const StorageLayer&)>;
+//  func_t read = [=](const StorageLayer& storage) {
+//    return interior_facets(storage);
+//  };
+//  return read_from_storage<func_t::result_type::element_type>(std::move(read));
+//}
+////------------------------------------------------------------------------------
+//std::shared_ptr<const std::vector<bool>> TopologyStorage::set_interior_facets(
+//    std::shared_ptr<const std::vector<bool>> interior_facets)
+//{
+//  if (layers.empty())
+//    throw std::runtime_error(
+//        "No storage layer present. Acquire a cache lock before writing.");
+//  return set_interior_facets(active_layer().first, interior_facets);
+//}
+////------------------------------------------------------------------------------
