Consistency fixes for exterior facet computation (#1006)
* Revise computation on entities on the boundary.

* Check all entities when finding mesh entities by coordinate

* Allow boundary_only flag only for facets in mesh::local_entities_geometrical.

* Refactor boundary facet marker.

* Work on geometric detection.

* Doxygen fix.

* Simplifications

* Geometric search refactoring.

* Fix size check.

* Fix test.

* Remove old interface.

* Split locate function into two

* Some updates.

* Minor formatting change.

* Revert solver

* Add extra Python doc.

* Expand tests.

* Tidy up test.

* More ghost mesh fixes.

* Some refactoring

* Enable more tests.

* Some minor refactoring.

* Undo some changes in a demo

python/test/unit/la/test_krylov_solver.py
@@ -9,15 +9,14 @@
 from contextlib import ExitStack
 
 import numpy as np
-
 import pytest
 import ufl
-from dolfinx import (DirichletBC, Function, FunctionSpace,
-                     UnitSquareMesh, VectorFunctionSpace)
+from dolfinx import (DirichletBC, Function, FunctionSpace, UnitSquareMesh,
+                     VectorFunctionSpace)
 from dolfinx.fem import (apply_lifting, assemble_matrix, assemble_vector,
                          locate_dofs_topological, set_bc)
 from dolfinx.la import VectorSpaceBasis
-from dolfinx.mesh import locate_entities_geometrical
+from dolfinx.mesh import locate_entities_boundary
 from mpi4py import MPI
 from petsc4py import PETSc
 from ufl import (Identity, TestFunction, TrialFunction, dot, dx, grad, inner,
@@ -102,7 +101,7 @@ def test_krylov_samg_solver_elasticity():
             return np.full(x.shape[1], True)
 
         facetdim = mesh.topology.dim - 1
-        bndry_facets = locate_entities_geometrical(mesh, facetdim, boundary, boundary_only=True)
+        bndry_facets = locate_entities_boundary(mesh, facetdim, boundary)
 
         bdofs = locate_dofs_topological(V.sub(0), V, facetdim, bndry_facets)
         bc = DirichletBC(bc0, bdofs, V.sub(0))