bce86da738a978d6204037e3c49db49182e21cf3
Michal Habera <michal.habera@gmail.com>
2020-05-16 20:43:26 +0200

    Revert assumptions

--- python/test/unit/mesh/test_meshtags.py
+++ python/test/unit/mesh/test_meshtags.py
@@ -1,36 +1,39 @@
-from dolfinx.generation import UnitSquareMesh
-from dolfinx.mesh import locate_entities_boundary
-import dolfinx.cpp
+from dolfinx.generation import UnitCubeMesh, UnitSquareMesh
+from dolfinx.mesh import locate_entities
+from dolfinx.cpp.mesh import CellType
+from dolfinx.io import XDMFFile
+from dolfinx import cpp
+import pytest
 
 from mpi4py import MPI
 import numpy
 
+celltypes_3D = [CellType.tetrahedron, CellType.hexahedron]
 
-def test_create():
-    mesh = UnitSquareMesh(MPI.COMM_WORLD, 4, 4)
+
+@pytest.mark.parametrize("cell_type", celltypes_3D)
+@pytest.mark.parametrize("mode", [cpp.mesh.GhostMode.none,
+                                  pytest.param(cpp.mesh.GhostMode.shared_facet,
+                                               marks=pytest.mark.xfail(condition=MPI.COMM_WORLD.size == 1,
+                                                                       reason="Shared ghost modes fail in serial"))])
+def test_create(cell_type, mode):
+    comm = MPI.COMM_WORLD
+
+    mesh = UnitCubeMesh(comm, 6, 6, 6, cell_type, ghost_mode=mode)
     mesh.topology.create_connectivity_all()
 
-    bottom_facets = locate_entities_boundary(mesh, 1, lambda x: numpy.isclose(x[1], 0.0))
-    print(MPI.COMM_WORLD.rank, "bottom", bottom_facets)
-
+    marked_lines = locate_entities(mesh, 1, lambda x: numpy.isclose(x[1], 0.5))
     f_v = mesh.topology.connectivity(1, 0).array().reshape(-1, 2)
 
     # TODO: make global_indices return numpy array
     global_idx = numpy.asarray(mesh.topology.index_map(0).global_indices(True))
-    global_idx_f = numpy.asarray(mesh.topology.index_map(1).global_indices(True))
 
-    print(MPI.COMM_WORLD.rank, "global idx vertex", global_idx)
-    print(MPI.COMM_WORLD.rank, "global idx facet", global_idx_f)
-    print(MPI.COMM_WORLD.rank, "global idx entities vtx", global_idx[f_v[bottom_facets]])
+    entities = cpp.graph.AdjacencyList64(global_idx[f_v[marked_lines]])
+    values = numpy.full(marked_lines.shape[0], 2, dtype=numpy.int32)
 
-    entities = dolfinx.cpp.graph.AdjacencyList64(global_idx[f_v[bottom_facets]])
-    print(MPI.COMM_WORLD.rank, "entities")
-    values = numpy.full(bottom_facets.shape[0], 2, dtype=numpy.int32)
+    mt = cpp.mesh.create_meshtags(mesh, cpp.mesh.CellType.interval, entities, values)
 
-    mt = dolfinx.cpp.mesh.create_meshtags(mesh, dolfinx.cpp.mesh.CellType.interval, entities, values)
+    lines_local = comm.allreduce((mt.indices < mesh.topology.index_map(1).size_local).sum(), op=MPI.SUM)
+    lines_local_marked = comm.allreduce((marked_lines < mesh.topology.index_map(1).size_local).sum(), op=MPI.SUM)
 
-    print(MPI.COMM_WORLD.rank, "mt.indices", mt.indices)
-
-
-
-    assert bottom_facets.shape == mt.indices.shape
+    assert lines_local == lines_local_marked
