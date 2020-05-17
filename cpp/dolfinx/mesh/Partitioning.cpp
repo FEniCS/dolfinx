89d260b06e6f6b06aa25fbe143c04574dc727752
Garth N. Wells <gnw20@cam.ac.uk>
2020-05-17 16:14:38 +0100

    Remove old code that was targeted for removal  (#1017)
    * Remove some old code
    
    * More removals
    
    * Remove more old functions

--- cpp/dolfinx/mesh/Partitioning.cpp
+++ cpp/dolfinx/mesh/Partitioning.cpp
@@ -19,44 +19,6 @@ using namespace dolfinx;
 using namespace dolfinx::mesh;
 
 //-----------------------------------------------------------------------------
-std::vector<bool> Partitioning::compute_vertex_exterior_markers(
-    const mesh::Topology& topology_local)
-{
-  // Get list of boundary vertices
-  const int dim = topology_local.dim();
-  auto facet_cell = topology_local.connectivity(dim - 1, dim);
-  if (!facet_cell)
-  {
-    throw std::runtime_error(
-        "Need facet-cell connectivity to build distributed adjacency list.");
-  }
-
-  auto facet_vertex = topology_local.connectivity(dim - 1, 0);
-  if (!facet_vertex)
-  {
-    throw std::runtime_error(
-        "Need facet-vertex connectivity to build distributed adjacency list.");
-  }
-
-  auto map_vertex = topology_local.index_map(0);
-  if (!map_vertex)
-    throw std::runtime_error("Need vertex IndexMap from topology.");
-  assert(map_vertex->num_ghosts() == 0);
-
-  std::vector<bool> exterior_vertex(map_vertex->size_local(), false);
-  for (int f = 0; f < facet_cell->num_nodes(); ++f)
-  {
-    if (facet_cell->num_links(f) == 1)
-    {
-      auto vertices = facet_vertex->links(f);
-      for (int j = 0; j < vertices.rows(); ++j)
-        exterior_vertex[vertices[j]] = true;
-    }
-  }
-
-  return exterior_vertex;
-}
-//-------------------------------------------------------------
 graph::AdjacencyList<std::int32_t> Partitioning::partition_cells(
     MPI_Comm comm, int n, const mesh::CellType cell_type,
     const graph::AdjacencyList<std::int64_t>& cells, mesh::GhostMode ghost_mode)
