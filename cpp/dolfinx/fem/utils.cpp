e7f0a504e6ff538ad9992d8be73f74f53b630d11
Chris Richardson <chris@bpi.cam.ac.uk>
2020-06-05 08:13:13 +0100

    Get reference coordinates from basis (#1044)
    * Get coordinate element basis directly and use for geometry
    
    * Remove midpoint for now
    
    * Use replicate for repeated J,K
    
    * Fixes for tdim != gdim
    
    * Respond to some comments
    
    * Add Eigen max size template parameters

--- cpp/dolfinx/fem/utils.cpp
+++ cpp/dolfinx/fem/utils.cpp
@@ -666,8 +666,8 @@ fem::create_coordinate_map(const ufc_coordinate_mapping& ufc_cmap)
 
   return fem::CoordinateElement(
       cell_type, ufc_cmap.topological_dimension, ufc_cmap.geometric_dimension,
-      ufc_cmap.signature, dof_layout, ufc_cmap.compute_physical_coordinates,
-      ufc_cmap.compute_reference_geometry);
+      ufc_cmap.signature, dof_layout, ufc_cmap.is_affine,
+      ufc_cmap.evaluate_basis_derivatives);
 }
 //-----------------------------------------------------------------------------
 fem::CoordinateElement
