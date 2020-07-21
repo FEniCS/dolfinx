4f942d98f9077c4b0ac3b4cdca0578df79246e54
Garth N. Wells <gnw20@cam.ac.uk>
2020-07-21 05:24:22 +0100

    Add Numba parallel (threaded) assembly example (#1089)
    * Add function to compute 'transpose' of the dofmap
    
    * Add parallel numba assembler example
    
    * Fix Numba type
    
    * Minor improvement
    
    * Remove spaces
    
    * Property updates
    
    * Interface updates
    
    * Interface updates
    
    * Some assembler performance improvements
    
    * Remove unused
    
    * Matrix assembly optimisations
    
    * Fix threaded assembly
    
    * Upate test
    
    * Reduce test size
    
    * Minor updates
    
    * Add docstring
    
    * Remove comment
    
    * Compute cell_offset on the fly
    
    Co-authored-by: Chris Richardson <chris@bpi.cam.ac.uk>

--- python/dolfinx/wrappers/graph.cpp
+++ python/dolfinx/wrappers/graph.cpp
@@ -35,11 +35,12 @@ void declare_adjacency_list(py::module& m, std::string type)
           },
           "Links (edges) of a node",
           py::return_value_policy::reference_internal)
-      .def("array", &dolfinx::graph::AdjacencyList<T>::array,
-           py::return_value_policy::reference_internal)
-      .def("offsets", &dolfinx::graph::AdjacencyList<T>::offsets,
-           "Index to each node in the links array",
-           py::return_value_policy::reference_internal)
+      .def_property_readonly("array", &dolfinx::graph::AdjacencyList<T>::array,
+                             py::return_value_policy::reference_internal)
+      .def_property_readonly("offsets",
+                             &dolfinx::graph::AdjacencyList<T>::offsets,
+                             "Index to each node in the links array",
+                             py::return_value_policy::reference_internal)
       .def_property_readonly("num_nodes",
                              &dolfinx::graph::AdjacencyList<T>::num_nodes)
       .def("__eq__", &dolfinx::graph::AdjacencyList<T>::operator==,
@@ -50,6 +51,9 @@ void declare_adjacency_list(py::module& m, std::string type)
 
 void graph(py::module& m)
 {
+
+  declare_adjacency_list<std::int32_t>(m, "int32");
+  declare_adjacency_list<std::int64_t>(m, "int64");
 
   m.def("create_local_adjacency_list",
         &dolfinx::graph::Partitioning::create_local_adjacency_list);
@@ -80,8 +84,5 @@ void graph(py::module& m)
         &dolfinx::graph::Partitioning::compute_local_to_global_links);
   m.def("compute_local_to_local",
         &dolfinx::graph::Partitioning::compute_local_to_local);
-
-  declare_adjacency_list<std::int32_t>(m, "int32");
-  declare_adjacency_list<std::int64_t>(m, "int64");
 }
 } // namespace dolfinx_wrappers
--- python/test/unit/fem/test_expression_evaluation.py
+++ python/test/unit/fem/test_expression_evaluation.py
@@ -74,11 +74,11 @@ def test_rank0():
                 b[dofmap[i * 6 + j]] = b_local[j]
 
     # Prepare mesh and dofmap data
-    pos = mesh.geometry.dofmap.offsets()
-    x_dofs = mesh.geometry.dofmap.array()
+    pos = mesh.geometry.dofmap.offsets
+    x_dofs = mesh.geometry.dofmap.array
     x = mesh.geometry.x
-    coeff_dofmap = P2.dofmap.list.array()
-    dofmap = vP1.dofmap.list.array()
+    coeff_dofmap = P2.dofmap.list.array
+    dofmap = vP1.dofmap.list.array
 
     # Data structure for the result
     b = dolfinx.Function(vP1)
