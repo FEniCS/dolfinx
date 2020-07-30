ff1526639b34c1018e19f08360d7cff277c6691a
Garth N. Wells <gnw20@cam.ac.uk>
2020-07-30 20:31:14 +0100

    Move test functions

--- python/test/unit/fem/test_assembler.py
+++ python/test/unit/fem/test_assembler.py
@@ -94,71 +94,69 @@ def test_assemble_derivatives():
     assert (A1 - A2).norm() == pytest.approx(0.0, rel=1e-12, abs=1e-12)
 
 
-def compile_eigen_csr_assembler_module(tmpdir):
-    cpp_code_header = f"""
-    <%
-    setup_pybind11(cfg)
-    cfg['include_dirs'] += {dolfinx_pc["include_dirs"] + [petsc4py.get_include()] + [str(pybind_inc())]}
-    cfg['compiler_args'] += {["-D" + dm for dm in dolfinx_pc["define_macros"]]}
-    cfg['compiler_args'] = ['-std=c++17']
-    cfg['libraries'] += {dolfinx_pc["libraries"]}
-    cfg['library_dirs'] += {dolfinx_pc["library_dirs"]}
-    %>
-    """
-
-    cpp_code = """
-    #include <pybind11/pybind11.h>
-    #include <pybind11/eigen.h>
-    #include <pybind11/stl.h>
-    #include <vector>
-    #include <Eigen/Sparse>
-    #include <petscsys.h>
-    #include <dolfinx/fem/assembler.h>
-    #include <dolfinx/fem/DirichletBC.h>
-    #include <dolfinx/fem/Form.h>
-
-    template<typename T>
-    Eigen::SparseMatrix<T, Eigen::RowMajor>
-      assemble_csr(const dolfinx::fem::Form<T>& a,
-               const std::vector<std::shared_ptr<const dolfinx::fem::DirichletBC<T>>>& bcs)
-    {
-      std::vector<Eigen::Triplet<T>> triplets;
-      const auto mat_add
-         = [&triplets](std::int32_t nrow, const std::int32_t* rows,
-                       std::int32_t ncol, const std::int32_t* cols, const T* v)
-        {
-          for (int i = 0; i < nrow; ++i)
-            for (int j = 0; j < ncol; ++j)
-              triplets.emplace_back(rows[i], cols[j], v[i * ncol + j]);
-          return 0;
-        };
-
-     dolfinx::fem::assemble_matrix<T>(mat_add, a, bcs);
-
-     auto map0 = a.function_space(0)->dofmap()->index_map;
-     auto map1 = a.function_space(1)->dofmap()->index_map;
-     Eigen::SparseMatrix<T, Eigen::RowMajor> mat(
-         map0->block_size() * (map0->size_local() + map0->num_ghosts()),
-         map1->block_size() * (map1->size_local() + map1->num_ghosts()));
-      mat.setFromTriplets(triplets.begin(), triplets.end());
-      return mat;
-    }
-
-    PYBIND11_MODULE(eigen_csr, m)
-    {
-      m.def("assemble_matrix", &assemble_csr<PetscScalar>);
-    }
-    """
-
-    open(os.path.join(tmpdir, "eigen_csr.cpp"), "w").write(cpp_code_header + cpp_code)
-    sys.path.append(tmpdir)
-    return cppimport.imp("eigen_csr")
-
-
 @skip_in_parallel
-@skip_if_complex
 def test_eigen_assembly(tempdir):  # noqa: F811
     """Compare assembly into scipy.CSR matrix with PETSc assembly"""
+
+    def compile_eigen_csr_assembler_module(tmpdir):
+        cpp_code_header = f"""
+        <%
+        setup_pybind11(cfg)
+        cfg['include_dirs'] += {dolfinx_pc["include_dirs"] + [petsc4py.get_include()] + [str(pybind_inc())]}
+        cfg['compiler_args'] += {["-D" + dm for dm in dolfinx_pc["define_macros"]]}
+        cfg['compiler_args'] = ['-std=c++17']
+        cfg['libraries'] += {dolfinx_pc["libraries"]}
+        cfg['library_dirs'] += {dolfinx_pc["library_dirs"]}
+        %>
+        """
+
+        cpp_code = """
+        #include <pybind11/pybind11.h>
+        #include <pybind11/eigen.h>
+        #include <pybind11/stl.h>
+        #include <vector>
+        #include <Eigen/Sparse>
+        #include <petscsys.h>
+        #include <dolfinx/fem/assembler.h>
+        #include <dolfinx/fem/DirichletBC.h>
+        #include <dolfinx/fem/Form.h>
+
+        template<typename T>
+        Eigen::SparseMatrix<T, Eigen::RowMajor>
+        assemble_csr(const dolfinx::fem::Form<T>& a,
+                const std::vector<std::shared_ptr<const dolfinx::fem::DirichletBC<T>>>& bcs)
+        {
+        std::vector<Eigen::Triplet<T>> triplets;
+        const auto mat_add
+            = [&triplets](std::int32_t nrow, const std::int32_t* rows,
+                        std::int32_t ncol, const std::int32_t* cols, const T* v)
+            {
+            for (int i = 0; i < nrow; ++i)
+                for (int j = 0; j < ncol; ++j)
+                triplets.emplace_back(rows[i], cols[j], v[i * ncol + j]);
+            return 0;
+            };
+
+        dolfinx::fem::assemble_matrix<T>(mat_add, a, bcs);
+
+        auto map0 = a.function_space(0)->dofmap()->index_map;
+        auto map1 = a.function_space(1)->dofmap()->index_map;
+        Eigen::SparseMatrix<T, Eigen::RowMajor> mat(
+            map0->block_size() * (map0->size_local() + map0->num_ghosts()),
+            map1->block_size() * (map1->size_local() + map1->num_ghosts()));
+        mat.setFromTriplets(triplets.begin(), triplets.end());
+        return mat;
+        }
+
+        PYBIND11_MODULE(eigen_csr, m)
+        {
+        m.def("assemble_matrix", &assemble_csr<PetscScalar>);
+        }
+        """
+
+        open(os.path.join(tmpdir, "eigen_csr.cpp"), "w").write(cpp_code_header + cpp_code)
+        sys.path.append(tmpdir)
+        return cppimport.imp("eigen_csr")
 
     def assemble_csr_matrix(a: typing.Union[dolfinx.fem.Form, cpp.fem.Form],
                             bcs: typing.List[dolfinx.fem.dirichletbc.DirichletBC] = [],
@@ -180,18 +178,13 @@ def test_eigen_assembly(tempdir):  # noqa: F811
     v = ufl.TestFunction(Q)
     a = ufl.inner(ufl.grad(u), ufl.grad(v)) * ufl.dx
 
-    def boundary(x):
-        return numpy.logical_or(x[0] < 1.0e-6, x[0] > 1.0 - 1.0e-6)
-
-    bdofsQ = dolfinx.fem.locate_dofs_geometrical(Q, boundary)
+    bdofsQ = dolfinx.fem.locate_dofs_geometrical(Q, lambda x: numpy.logical_or(x[0] < 1.0e-6, x[0] > 1.0 - 1.0e-6))
     u_bc = dolfinx.function.Function(Q)
     with u_bc.vector.localForm() as u_local:
         u_local.set(1.0)
     bc = dolfinx.fem.dirichletbc.DirichletBC(u_bc, bdofsQ)
-
     A1 = dolfinx.fem.assemble_matrix(a, [bc])
     A1.assemble()
-
     A2 = assemble_csr_matrix(a, [bc])
     assert numpy.isclose(A1.norm(), scipy.sparse.linalg.norm(A2))
 
