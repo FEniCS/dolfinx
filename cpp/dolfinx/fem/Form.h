bbfbd955d69b5dbcfc1934a7fc2e5deb385c4d46
Garth N. Wells <gnw20@cam.ac.uk>
2020-08-05 14:52:58 +0100

    Doc fix

--- cpp/dolfinx/fem/Form.h
+++ cpp/dolfinx/fem/Form.h
@@ -103,6 +103,8 @@ public:
   /// using FormIntegrals::set_cell_tabulate_tensor.
   ///
   /// @param[in] function_spaces Vector of function spaces
+  /// @param[in] need_mesh_permutation_data Set to true if mesh entity
+  ///   permutation data is required
   Form(const std::vector<std::shared_ptr<const function::FunctionSpace>>&
            function_spaces,
        bool need_mesh_permutation_data)
