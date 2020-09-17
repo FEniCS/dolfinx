8346dbc2383b30f6a3aeff76954ffc02f7b2d9c8
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2020-09-17 20:03:22 +0100

    fix typo

--- python/test/unit/fem/test_dof_permuting.py
+++ python/test/unit/fem/test_dof_permuting.py
@@ -465,7 +465,7 @@ def test_hexahedron_evaluation(space_type, space_order):
     if space_type == "NCF" and space_order >= 3:
         print("Space not supported yet")
         return
-    if space_type == "NCE": and space_order >= 2:
+    if space_type == "NCE" and space_order >= 2:
         print("Space not supported yet")
         return
 
