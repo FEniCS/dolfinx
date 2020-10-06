8e46f097557d7a438bc56ece3ed83293562320e4
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2020-10-06 12:15:25 +0100

    don't import choice

--- python/test/unit/fem/test_element_integrals.py
+++ python/test/unit/fem/test_element_integrals.py
@@ -6,7 +6,7 @@
 """Unit tests for the fem interface"""
 
 from itertools import combinations, product
-from random import shuffle, choice
+from random import shuffle
 
 import numpy as np
 import pytest
