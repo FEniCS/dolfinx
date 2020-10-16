27de1e286c5cfe7ed28944f8dd00384a72dcade8
Garth N. Wells <gnw20@cam.ac.uk>
2020-10-16 11:34:52 +0100

    Update demo

--- python/demo/mixed-elasticity-sc/static-condensation-elasticity.py
+++ python/demo/mixed-elasticity-sc/static-condensation-elasticity.py
@@ -141,7 +141,7 @@ def tabulate_condensed_tensor_A(A_, w_, c_, coords_, entity_local_index, permuta
 
 
 # Prepare an empty Form and set the condensed tabulation kernel
-a_cond = dolfinx.cpp.fem.Form([U._cpp_object, U._cpp_object], False)
+a_cond = dolfinx.cpp.fem.Form([U._cpp_object, U._cpp_object], [], False)
 a_cond.set_tabulate_tensor(dolfinx.fem.IntegralType.cell, -1, tabulate_condensed_tensor_A.address)
 
 A_cond = dolfinx.fem.assemble_matrix(a_cond, [bc])
