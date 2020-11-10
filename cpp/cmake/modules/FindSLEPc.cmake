8fd31ec650f9ee797d21e3ee14164517b9ae386b
Drew Parsons <dparsons@emerall.com>
2020-11-10 21:12:09 +0800

    Update FindSLEPc.cmake
    the first name in pkg_search_module is the module prefix in cmake, the ones following it are the alternatives pkg-config module names

--- cpp/cmake/modules/FindSLEPc.cmake
+++ cpp/cmake/modules/FindSLEPc.cmake
@@ -48,7 +48,7 @@ find_package(PkgConfig REQUIRED)
 set(ENV{PKG_CONFIG_PATH} "$ENV{SLEPC_DIR}/$ENV{PETSC_ARCH}/lib/pkgconfig:$ENV{SLEPC_DIR}/lib/pkgconfig:$ENV{PKG_CONFIG_PATH}")
 set(ENV{PKG_CONFIG_PATH} "$ENV{PETSC_DIR}/$ENV{PETSC_ARCH}/lib/pkgconfig:$ENV{PETSC_DIR}/lib/pkgconfig:$ENV{PKG_CONFIG_PATH}")
 set(ENV{PKG_CONFIG_PATH} "$ENV{PETSC_DIR}/$ENV{PETSC_ARCH}:$ENV{PETSC_DIR}:$ENV{PKG_CONFIG_PATH}")
-pkg_search_module(slepc SLEPC crayslepc_real SLEPc)
+pkg_search_module(SLEPC slepc crayslepc_real SLEPc)
 
 # Extract major, minor, etc from version string
 if (SLEPC_VERSION)
