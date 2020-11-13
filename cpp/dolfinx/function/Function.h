7eccc9fda4ec88411eeb74b7ee196893e8f02772
Massimiliano Leoni <mleoni@bcamath.org>
2020-11-13 18:48:54 +0100

    Adding noexcept specification to move constructor

--- cpp/dolfinx/function/Function.h
+++ cpp/dolfinx/function/Function.h
@@ -79,7 +79,7 @@ public:
   Function(const Function& v) = delete;
 
   /// Move constructor
-  Function(Function&& v)
+  Function(Function&& v) noexcept
       : name(std::move(v.name)), _id(std::move(v._id)),
         _function_space(std::move(v._function_space)), _x(std::move(v._x)),
         _petsc_vector(std::move(v._petsc_vector))
