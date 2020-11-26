05727130bf2a191eb5ed1aafd10ddf6173903bff
Garth N. Wells <gnw20@cam.ac.uk>
2020-11-26 16:37:36 +0000

    Uodate cppimport test

--- python/test/unit/fem/test_assemble_cppimport.py
+++ python/test/unit/fem/test_assemble_cppimport.py
@@ -66,10 +66,12 @@ const auto mat_add
 dolfinx::fem::assemble_matrix<T>(mat_add, a, bcs);
 
 auto map0 = a.function_spaces().at(0)->dofmap()->index_map;
+int bs0 = a.function_spaces().at(0)->dofmap()->index_map_bs();
 auto map1 = a.function_spaces().at(1)->dofmap()->index_map;
+int bs1 = a.function_spaces().at(1)->dofmap()->index_map_bs();
 Eigen::SparseMatrix<T, Eigen::RowMajor> mat(
-    map0->block_size() * (map0->size_local() + map0->num_ghosts()),
-    map1->block_size() * (map1->size_local() + map1->num_ghosts()));
+    bs0 * (map0->size_local() + map0->num_ghosts()),
+    bs1 * (map1->size_local() + map1->num_ghosts()));
 mat.setFromTriplets(triplets.begin(), triplets.end());
 return mat;
 }
