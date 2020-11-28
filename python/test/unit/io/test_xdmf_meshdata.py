395b5f0e2b56f4815ee17cc85fa287d6cee0c172
root <dokken92@gmail.com>
2020-11-28 14:39:55 +0000

    Revert test

--- python/test/unit/io/test_xdmf_meshdata.py
+++ python/test/unit/io/test_xdmf_meshdata.py
@@ -1,15 +1,13 @@
-# Copyright (C) 2012-2020 Garth N. Wells, Jørgen S. Dokken
+# Copyright (C) 2012-2019 Garth N. Wells
 #
 # This file is part of DOLFINX (https://www.fenicsproject.org)
 #
 # SPDX-License-Identifier:    LGPL-3.0-or-later
 
 import os
-from xml.etree import ElementTree
 
-import numpy as np
 import pytest
-from dolfinx import MeshTags, UnitCubeMesh, UnitIntervalMesh, UnitSquareMesh
+from dolfinx import UnitCubeMesh, UnitIntervalMesh, UnitSquareMesh
 from dolfinx.cpp.mesh import CellType
 from dolfinx.io import XDMFFile
 from dolfinx_utils.test.fixtures import tempdir
@@ -64,44 +62,3 @@ def test_read_mesh_data(tempdir, tdim, n):
     assert cell_type[1] == 1
     assert mesh.topology.index_map(tdim).size_global == mesh.mpi_comm().allreduce(cells.shape[0], op=MPI.SUM)
     assert mesh.geometry.index_map().size_global == mesh.mpi_comm().allreduce(x.shape[0], op=MPI.SUM)
-
-
-def test_write_mesh_data(tempdir):
-    comm_world = MPI.COMM_WORLD
-    comm_self = MPI.COMM_SELF
-    filename_serial = os.path.join(tempdir, "mesh_serial.xdmf")
-    filename_parallel = os.path.join(tempdir, "mesh_parallel.xdmf")
-
-    if comm_world.rank == 0:
-        mesh = UnitCubeMesh(comm_self, 2, 2, 2)
-        mesh.name = "mesh"
-        num_cells = mesh.topology.index_map(mesh.topology.dim).size_local
-        indices = np.arange(num_cells, dtype=np.int32)
-        values = np.ones(num_cells, dtype=np.int32)
-        mt = MeshTags(mesh, mesh.topology.dim, indices, values)
-        mt.name = "cells"
-        with XDMFFile(comm_self, filename_serial, "w") as xdmf:
-            xdmf.write_mesh(mesh)
-            xdmf.write_meshtags(mt)
-    comm_world.barrier()
-
-    with XDMFFile(comm_world, filename_serial, "r") as xdmf:
-        mesh2 = xdmf.read_mesh(name="mesh")
-        mt2 = xdmf.read_meshtags(mesh2, name="cells")
-
-    with XDMFFile(comm_world, filename_parallel, "w") as xdmf:
-        xdmf.write_mesh(mesh2)
-        xdmf.write_meshtags(mt2)
-
-    parser = ElementTree.XMLParser()
-    tree = ElementTree.parse(filename_parallel, parser)
-    root = tree.getroot()
-    domain = list(root)[0]
-    meshes = list(domain)
-    num_cells = []
-    for mesh in meshes:
-        elements = list(mesh)
-        for element in elements:
-            if element.tag == "Topology":
-                num_cells.append(element.get("NumberOfElements"))
-    assert(num_cells[0] == num_cells[1])
