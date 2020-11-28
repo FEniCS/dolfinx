d36a250a8b927d85eabd6c96b9754859cdb7142e
root <dokken92@gmail.com>
2020-11-28 15:14:45 +0000

    Final update of test

--- python/test/unit/io/test_xdmf_meshtags.py
+++ python/test/unit/io/test_xdmf_meshtags.py
@@ -79,28 +79,23 @@ def test_3d(tempdir, cell_type, encoding):
     lines_local_in = comm.allreduce(
         (mt_lines_in.indices < mesh_in.topology.index_map(1).size_local).sum(), op=MPI.SUM)
 
-    imap = mesh.topology.index_map(mesh_in.topology.dim)
-    num_cells = imap.size_local + imap.num_ghosts
-    indices = np.arange(num_cells, dtype=np.int32)
-    values = np.ones(num_cells + imap.num_ghosts, dtype=np.int32)
-    mt = MeshTags(mesh_in, mesh_in.topology.dim, indices, values)
-    mt.name = "cells"
-    with XDMFFile(comm, os.path.join(tempdir, "meshtags_3d_2_out.xdmf"), "w", encoding=encoding) as file:
-        file.write_mesh(mesh_in)
-        file.write_meshtags(mt)
-
     assert lines_local == lines_local_in
 
-    MPI.COMM_WORLD.barrier()
+    # Check that only owned data is written to file
+    tags = {tag.name: tag for tag in [mt_lines_in, mt_in]}
     parser = ElementTree.XMLParser()
-    tree = ElementTree.parse(os.path.join(tempdir, "meshtags_3d_2_out.xdmf"), parser)
+    tree = ElementTree.parse(os.path.join(tempdir, "meshtags_3d_out.xdmf"), parser)
     root = tree.getroot()
     domain = list(root)[0]
     meshes = list(domain)
-    num_cells = []
     for mesh in meshes:
         elements = list(mesh)
-        for element in elements:
-            if element.tag == "Topology":
-                num_cells.append(element.get("NumberOfElements"))
-    assert(num_cells[0] == num_cells[1])
+        grid_name = mesh.get("Name")
+        if grid_name in tags.keys():
+            tag = tags[grid_name]
+            for element in elements:
+                if element.tag == "Topology":
+                    num_xdmf_indices = int(element.get("NumberOfElements"))
+                    num_global_tagged = comm.allreduce(
+                        (tag.indices < mesh_in.topology.index_map(tag.dim).size_local).sum(), op=MPI.SUM)
+                    assert(num_xdmf_indices == num_global_tagged)
