5bf0e841bc96ec581e7e991b03186033f7425d89
Michal Habera <michal.habera@gmail.com>
2020-12-02 11:29:20 +0100

    More explicitness in import to public API

--- python/dolfinx/geometry.py
+++ python/dolfinx/geometry.py
@@ -1,7 +1,9 @@
-# Copyright (C) 2018 Michal Habera and Garth N. Wells
+# Copyright (C) 2018-2020 Michal Habera and Garth N. Wells
 #
 # This file is part of DOLFINX (https://www.fenicsproject.org)
 #
 # SPDX-License-Identifier:    LGPL-3.0-or-later
 
-from dolfinx.cpp.geometry import * # noqa
+from dolfinx.cpp.geometry import (BoundingBoxTree, create_midpoint_tree, compute_closest_entity, # noqa
+                                  compute_collisions_point, compute_collisions, compute_distance_gjk,
+                                  squared_distance, select_colliding_cells)
