95ae8dde7354a5c0e5337892854a371a4b88d75c
Garth N. Wells <gnw20@cam.ac.uk>
2020-12-04 20:01:12 +0000

    Flake8 fix

--- python/demo/elasticity/demo_elasticity.py
+++ python/demo/elasticity/demo_elasticity.py
@@ -9,19 +9,17 @@
 # smoothed aggregation algebraic multigrid. ::
 
 from contextlib import ExitStack
-import os
-
-import numpy as np
-from mpi4py import MPI
-from petsc4py import PETSc
 
 import dolfinx
+import numpy as np
 from dolfinx import BoxMesh, DirichletBC, Function, VectorFunctionSpace, cpp
 from dolfinx.cpp.mesh import CellType
-from dolfinx.fem import (Form, apply_lifting, assemble_matrix, assemble_vector,
+from dolfinx.fem import (apply_lifting, assemble_matrix, assemble_vector,
                          locate_dofs_geometrical, set_bc)
 from dolfinx.io import XDMFFile
 from dolfinx.la import VectorSpaceBasis
+from mpi4py import MPI
+from petsc4py import PETSc
 from ufl import (Identity, SpatialCoordinate, TestFunction, TrialFunction,
                  as_vector, dx, grad, inner, sym, tr)
 
