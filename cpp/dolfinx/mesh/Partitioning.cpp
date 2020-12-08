b1cd0540f77840264c844e5c672d6af977737f85
igorbaratta <igorbaratta@gmail.com>
2020-12-08 17:55:07 -0300

    remove parmetis from public interface

--- cpp/dolfinx/mesh/Partitioning.cpp
+++ cpp/dolfinx/mesh/Partitioning.cpp
@@ -12,6 +12,7 @@
 #include <dolfinx/common/Timer.h>
 #include <dolfinx/common/log.h>
 #include <dolfinx/graph/AdjacencyList.h>
+#include <dolfinx/graph/ParMETIS.h>
 #include <dolfinx/graph/SCOTCH.h>
 #include <dolfinx/mesh/GraphBuilder.h>
 
@@ -41,19 +42,21 @@ graph::AdjacencyList<std::int32_t> Partitioning::partition_cells(
   const auto [dual_graph, graph_info]
       = mesh::GraphBuilder::compute_dual_graph(comm, cells, cell_type);
 
-  // Extract data from graph_info
-  const auto [num_ghost_nodes, num_local_edges, num_nonlocal_edges]
-      = graph_info;
+  // // Extract data from graph_info
+  // const auto [num_ghost_nodes, num_local_edges, num_nonlocal_edges]
+  //     = graph_info;
 
   graph::AdjacencyList<std::int64_t> adj_graph(dual_graph);
-  std::vector<std::size_t> weights;
 
   // Just flag any kind of ghosting for now
   bool ghosting = (ghost_mode != mesh::GhostMode::none);
 
   // Call partitioner
-  graph::AdjacencyList<std::int32_t> partition = graph::SCOTCH::partition(
-      comm, n, adj_graph, weights, num_ghost_nodes, ghosting);
+  // graph::AdjacencyList<std::int32_t> partition = graph::SCOTCH::partition(
+  //     comm, n, adj_graph, weights, num_ghost_nodes, ghosting);
+
+  graph::AdjacencyList<std::int32_t> partition
+      = graph::ParMETIS::partition(comm, n, adj_graph, ghosting);
 
   return partition;
 }
