155a45b7aeeac8d01e7a5d913b612e59e5ab476e
Massimiliano Leoni <mleoni@bcamath.org>
2020-12-14 11:30:20 +0100

    Improve documentation

--- cpp/dolfinx/la/PETScMatrix.h
+++ cpp/dolfinx/la/PETScMatrix.h
@@ -41,12 +41,14 @@ public:
   /// Return a function with an interface for adding values to the
   /// matrix A (calls MatSetValuesLocal)
   /// @param[in] A The matrix to set values in
+  /// @param[in] mode The PETSc insert mode (ADD_VALUES, INSERT_VALUES, ...)
   static std::function<int(std::int32_t, const std::int32_t*, std::int32_t,
                            const std::int32_t*, const PetscScalar*)>
   add_fn(Mat A, const InsertMode mode = ADD_VALUES);
 
   /// Return a function with an interface for inserting values into the
   /// matrix A
+  /// @param[in] A The matrix to set values in
   static std::function<int(std::int32_t, const std::int32_t*, std::int32_t,
                            const std::int32_t*, const PetscScalar*)>
   set_fn(Mat A);
