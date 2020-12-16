2d49c99d53f52e94d3c88963200011f762f88a51
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2020-12-16 09:44:42 +0000

    added TODO documentation

--- cpp/dolfinx/fem/libtab_wrapper.h
+++ cpp/dolfinx/fem/libtab_wrapper.h
@@ -21,51 +21,83 @@ class FiniteElement;
 
 namespace dolfinx::fem
 {
+/// TODO: document
 class LibtabElement
 {
 public:
+  /// TODO: document
   LibtabElement() {};
+
+  /// TODO: document
   virtual const Eigen::ArrayXXd& points() const;
+
+  /// TODO: document
   virtual std::vector<Eigen::ArrayXXd> tabulate(int nd, const Eigen::ArrayXXd& x) const;
+
+  /// TODO: document
   virtual int block_size() const;
 };
 
+/// TODO: document
 class WrappedLibtabElement : public LibtabElement
 {
 public:
+  /// TODO: document
   explicit WrappedLibtabElement(std::shared_ptr<const libtab::FiniteElement> libtab_element);
 
+  /// TODO: document
   const Eigen::ArrayXXd& points() const override;
+
+  /// TODO: document
   std::vector<Eigen::ArrayXXd> tabulate(int nd, const Eigen::ArrayXXd& x) const override;
 
 private:
+  /// TODO: document
   std::shared_ptr<const libtab::FiniteElement> _libtab_element;
 };
 
+/// TODO: document
 class BlockedLibtabElement : public LibtabElement
 {
 public:
+  /// TODO: document
   explicit BlockedLibtabElement(std::shared_ptr<const libtab::FiniteElement> libtab_element, int block_size);
 
+  /// TODO: document
   const Eigen::ArrayXXd& points() const override;
+
+  /// TODO: document
   std::vector<Eigen::ArrayXXd> tabulate(int nd, const Eigen::ArrayXXd& x) const override;
+
+  /// TODO: document
   int block_size() const override;
 
 private:
+  /// TODO: document
   std::shared_ptr<const libtab::FiniteElement> _libtab_element;
+
+  /// TODO: document
   int _block_size;
 };
 
+/// TODO: document
 class MixedLibtabElement : public LibtabElement
 {
 public:
+  /// TODO: document
   explicit MixedLibtabElement(std::vector<std::shared_ptr<const LibtabElement>> sub_elements);
 
+  /// TODO: document
   const Eigen::ArrayXXd& points() const override;
+
+  /// TODO: document
   std::vector<Eigen::ArrayXXd> tabulate(int nd, const Eigen::ArrayXXd& x) const override;
 
 private:
+  /// TODO: document
   std::vector<std::shared_ptr<const LibtabElement>> _sub_elements;
+
+  /// TODO: document
   Eigen::ArrayXXd _points;
 };
 
