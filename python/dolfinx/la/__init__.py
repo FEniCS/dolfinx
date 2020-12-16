bedf436cd760dc7f7f0d1c7028395dc53769296c
Chris Richardson <chris@bpi.cam.ac.uk>
2020-12-16 09:55:02 +0000

    Try removing imports (flake8)

--- python/dolfinx/la/__init__.py
+++ python/dolfinx/la/__init__.py
@@ -5,14 +5,9 @@
 # SPDX-License-Identifier:    LGPL-3.0-or-later
 """Linear algebra module for solving systems of equations"""
 
-import numpy as np
-
-from dolfinx import cpp
 from dolfinx.la.solver import solve
 
-__all__ = [
-    "solve"
-]
+__all__ = ["solve"]
 
 # Import pybind11 objects into dolfinx.la
 from dolfinx.cpp.la import VectorSpaceBasis  # noqa
