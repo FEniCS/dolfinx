0527b89589bde1c9476427a1d01cbecab69f009f
Garth N. Wells <gnw20@cam.ac.uk>
2020-12-17 16:23:56 +0000

    Some updates

--- cpp/dolfinx/fem/discreteoperators.cpp
+++ cpp/dolfinx/fem/discreteoperators.cpp
@@ -1,10 +1,10 @@
-// Copyright (C) 2015-2020 Garth N. Wells, Jørgen S. DOkken
+// Copyright (C) 2015-2020 Garth N. Wells, Jørgen S. Dokken
 //
 // This file is part of DOLFINX (https://www.fenicsproject.org)
 //
 // SPDX-License-Identifier:    LGPL-3.0-or-later
 
-#include "DiscreteOperators.h"
+#include "discreteoperators.h"
 #include <array>
 #include <dolfinx/common/IndexMap.h>
 #include <dolfinx/fem/DofMap.h>
@@ -15,29 +15,26 @@
 #include <vector>
 
 using namespace dolfinx;
-using namespace dolfinx::fem;
 
 //-----------------------------------------------------------------------------
-la::PETScMatrix DiscreteOperators::build_gradient(const fem::FunctionSpace& V0,
-                                                  const fem::FunctionSpace& V1)
+la::PETScMatrix fem::build_discrete_gradient(const fem::FunctionSpace& V0,
+                                             const fem::FunctionSpace& V1)
 {
-
   // Get mesh
-  assert(V0.mesh());
-  const mesh::Mesh& mesh = *(V0.mesh());
+  std::shared_ptr<const mesh::Mesh> mesh = V0.mesh();
+  assert(mesh);
 
   // Check that mesh is the same for both function spaces
   assert(V1.mesh());
-  if (&mesh != V1.mesh().get())
+  if (mesh != V1.mesh())
   {
     throw std::runtime_error("Compute discrete gradient operator. Function "
                              "spaces do not share the same mesh");
   }
 
   // Check that V0 is a (lowest-order) edge basis
-  mesh.topology_mutable().create_entities(1);
-  std::int64_t num_edges_global = mesh.topology().index_map(1)->size_global();
-
+  mesh->topology_mutable().create_entities(1);
+  std::int64_t num_edges_global = mesh->topology().index_map(1)->size_global();
   if (V0.dim() != num_edges_global)
   {
     throw std::runtime_error(
@@ -46,8 +43,8 @@ la::PETScMatrix DiscreteOperators::build_gradient(const fem::FunctionSpace& V0,
   }
 
   // Check that V1 is a linear nodal basis
-  std::int64_t num_vertices_global
-      = mesh.topology().index_map(0)->size_global();
+  const std::int64_t num_vertices_global
+      = mesh->topology().index_map(0)->size_global();
   if (V1.dim() != num_vertices_global)
   {
     throw std::runtime_error(
@@ -71,24 +68,26 @@ la::PETScMatrix DiscreteOperators::build_gradient(const fem::FunctionSpace& V0,
   assert(block_sizes[0] == block_sizes[1]);
 
   // Initialise sparsity pattern
-  la::SparsityPattern pattern(mesh.mpi_comm(), index_maps, block_sizes);
+  la::SparsityPattern pattern(mesh->mpi_comm(), index_maps, block_sizes);
 
   // Initialize required connectivities
-  const int tdim = mesh.topology().dim();
-  mesh.topology_mutable().create_connectivity(1, 0);
-  auto e_to_v = mesh.topology().connectivity(1, 0);
-  mesh.topology_mutable().create_connectivity(tdim, 1);
-  auto c_to_e = mesh.topology().connectivity(tdim, 1);
-  mesh.topology_mutable().create_connectivity(1, tdim);
-  auto e_to_c = mesh.topology().connectivity(1, tdim);
-  mesh.topology_mutable().create_connectivity(tdim, 0);
-  auto c_to_v = mesh.topology().connectivity(mesh.topology_mutable().dim(), 0);
+  const int tdim = mesh->topology().dim();
+  mesh->topology_mutable().create_connectivity(1, 0);
+  auto e_to_v = mesh->topology().connectivity(1, 0);
+  mesh->topology_mutable().create_connectivity(tdim, 1);
+  auto c_to_e = mesh->topology().connectivity(tdim, 1);
+  mesh->topology_mutable().create_connectivity(1, tdim);
+  auto e_to_c = mesh->topology().connectivity(1, tdim);
+  mesh->topology_mutable().create_connectivity(tdim, 0);
+  auto c_to_v = mesh->topology().connectivity(tdim, 0);
 
   // Build sparsity pattern
   std::vector<std::int32_t> rows;
   std::vector<std::int32_t> cols;
-  const std::int32_t num_edges = mesh.topology().index_map(1)->size_local()
-                                 + mesh.topology().index_map(1)->num_ghosts();
+  const std::int32_t num_edges = mesh->topology().index_map(1)->size_local()
+                                 + mesh->topology().index_map(1)->num_ghosts();
+  const std::shared_ptr<const fem::DofMap> dofmap0 = V0.dofmap();
+  assert(dofmap0);
   for (std::int32_t e = 0; e < num_edges; ++e)
   {
     // Find local index of edge in one of the cells it is part of
@@ -99,15 +98,16 @@ la::PETScMatrix DiscreteOperators::build_gradient(const fem::FunctionSpace& V0,
     const auto* it = std::find(edges.data(), edges.data() + edges.rows(), e);
     assert(it != (edges.data() + edges.rows()));
     const int local_edge = std::distance(edges.data(), it);
+
     // Find the dofs located on the edge
-    auto dofs0 = V0.dofmap()->cell_dofs(cell);
+    auto dofs0 = dofmap0->cell_dofs(cell);
     auto local_dofs = layout0->entity_dofs(1, local_edge);
     assert(local_dofs.size() == 1);
-    std::int32_t row = dofs0[local_dofs[0]];
-    rows.push_back(row);
+    rows.push_back(dofs0[local_dofs[0]]);
     auto vertices = e_to_v->links(e);
     assert(vertices.size() == 2);
     auto cell_vertices = c_to_v->links(cell);
+
     // Find local index of each of the vertices and map to local dof
     for (std::int32_t i = 0; i < 2; ++i)
     {
@@ -122,6 +122,7 @@ la::PETScMatrix DiscreteOperators::build_gradient(const fem::FunctionSpace& V0,
       cols.push_back(dofs1[local_v_dofs[0]]);
     }
   }
+
   Eigen::Map<const Eigen::Array<std::int32_t, Eigen::Dynamic, 1>> _rows(
       rows.data(), rows.size());
   Eigen::Map<const Eigen::Array<std::int32_t, Eigen::Dynamic, 1>> _cols(
@@ -130,18 +131,17 @@ la::PETScMatrix DiscreteOperators::build_gradient(const fem::FunctionSpace& V0,
   pattern.assemble();
 
   // Create matrix
-  la::PETScMatrix A(mesh.mpi_comm(), pattern);
-  auto mat_add = la::PETScMatrix::add_fn(A.mat());
+  la::PETScMatrix A(mesh->mpi_comm(), pattern);
+  Mat _A = A.mat();
 
   // Build discrete gradient operator/matrix
   const std::vector<std::int64_t>& global_indices
-      = mesh.topology().index_map(0)->global_indices();
-  Eigen::Matrix<PetscScalar, 1, 2, Eigen::RowMajor> Ae(1, 2);
-  Ae.fill(0);
-
+      = mesh->topology().index_map(0)->global_indices();
+  std::array<PetscScalar, 2> Ae;
   for (std::int32_t e = 0; e < num_edges; ++e)
   {
     std::vector<std::int32_t> cols;
+
     // Find local index of edge in one of the cells it is part of
     auto cells = e_to_c->links(e);
     assert(cells.size() > 0);
@@ -150,11 +150,17 @@ la::PETScMatrix DiscreteOperators::build_gradient(const fem::FunctionSpace& V0,
     const auto* it = std::find(edges.data(), edges.data() + edges.rows(), e);
     assert(it != (edges.data() + edges.rows()));
     const int local_edge = std::distance(edges.data(), it);
+
     // Find the dofs located on the edge
-    auto dofs0 = V0.dofmap()->cell_dofs(cell);
-    auto local_dofs = layout0->entity_dofs(1, local_edge);
+    auto dofs0 = dofmap0->cell_dofs(cell);
+
+    // FIXME: avoid this expensive call
+    const Eigen::Array<int, Eigen::Dynamic, 1> local_dofs
+        = layout0->entity_dofs(1, local_edge);
     assert(local_dofs.size() == 1);
-    std::vector<std::int32_t> rows = {dofs0[local_dofs[0]]};
+    // FIXME: avoid this expensive call
+    std::int32_t row = dofs0[local_dofs[0]];
+
     auto vertices = e_to_v->links(e);
     assert(vertices.size() == 2);
     auto cell_vertices = c_to_v->links(cell);
@@ -167,23 +173,27 @@ la::PETScMatrix DiscreteOperators::build_gradient(const fem::FunctionSpace& V0,
                       cell_vertices.data() + cell_vertices.rows(), vertices[i]);
       assert(it != (cell_vertices.data() + cell_vertices.rows()));
       const int local_vertex = std::distance(cell_vertices.data(), it);
-      auto local_v_dofs = layout1->entity_dofs(0, local_vertex);
+
+      // FIXME: avoid this expensive call
+      const Eigen::Array<int, Eigen::Dynamic, 1> local_v_dofs
+          = layout1->entity_dofs(0, local_vertex);
       assert(local_v_dofs.size() == 1);
       auto dofs1 = V1.dofmap()->cell_dofs(cell);
       cols.push_back(dofs1[local_v_dofs[0]]);
     }
+
     if (global_indices[vertices[1]] < global_indices[vertices[0]])
     {
-      Ae.row(0)[0] = 1;
-      Ae.row(0)[1] = -1;
+      Ae[0] = 1;
+      Ae[1] = -1;
     }
     else
     {
-      Ae.row(0)[0] = -1;
-      Ae.row(0)[1] = 1;
+      Ae[0] = -1;
+      Ae[1] = 1;
     }
 
-    mat_add(1, rows.data(), 2, cols.data(), Ae.data());
+    MatSetValuesLocal(_A, 1, &row, 2, cols.data(), Ae.data(), INSERT_VALUES);
   }
 
   // Finalise matrix
--- python/test/unit/fem/test_discrete_operators.py
+++ python/test/unit/fem/test_discrete_operators.py
@@ -1,60 +1,54 @@
-"""Unit tests for the DiscreteOperator class"""
-
 # Copyright (C) 2015 Garth N. Wells
 #
 # This file is part of DOLFINX (https://www.fenicsproject.org)
 #
 # SPDX-License-Identifier:    LGPL-3.0-or-later
-
+"""Unit tests for the DiscreteOperator class"""
 
 import numpy
 import pytest
 from dolfinx import FunctionSpace, UnitCubeMesh, UnitSquareMesh
-from dolfinx.cpp.fem import DiscreteOperators
+from dolfinx.cpp.fem import build_discrete_operator
 from dolfinx_utils.test.skips import skip_in_parallel
 from mpi4py import MPI
 from petsc4py import PETSc
 
 
 @skip_in_parallel
-def test_gradient():
+@pytest.mark.parametrize("mesh", [
+    UnitSquareMesh(MPI.COMM_WORLD, 11, 6, ghost_mode=dolfinx.cpp.mesh.GhostMode.none),
+    UnitSquareMesh(MPI.COMM_WORLD, 11, 6, ghost_mode=dolfinx.cpp.mesh.GhostMode.shared_facet),
+    UnitCubeMesh(MPI.COMM_WORLD, 4, 3, 7, ghost_mode=dolfinx.cpp.mesh.GhostMode.none),
+    UnitCubeMesh(MPI.COMM_WORLD, 4, 3, 7, ghost_mode=dolfinx.cpp.mesh.GhostMode.shared_facet)
+])
+def test_gradient(mesh):
     """Test discrete gradient computation (typically used for curl-curl
     AMG preconditioners"""
 
-    def compute_discrete_gradient(mesh):
-        V = FunctionSpace(mesh, ("Lagrange", 1))
-        W = FunctionSpace(mesh, ("Nedelec 1st kind H(curl)", 1))
-        G = DiscreteOperators.build_gradient(W._cpp_object, V._cpp_object)
-        assert G.getRefCount() == 1
-        num_edges = mesh.topology.index_map(1).size_global
-        m, n = G.getSize()
-        assert m == num_edges
-        assert n == mesh.topology.index_map(0). size_global
-        assert round(
-            G.norm(PETSc.NormType.FROBENIUS) - numpy.sqrt(2.0 * num_edges),
-            8) == 0.0
-
-    meshes = [
-        UnitSquareMesh(MPI.COMM_WORLD, 11, 6),
-        UnitCubeMesh(MPI.COMM_WORLD, 4, 3, 7)
-    ]
-    for mesh in meshes:
-        compute_discrete_gradient(mesh)
+    V = FunctionSpace(mesh, ("Lagrange", 1))
+    W = FunctionSpace(mesh, ("Nedelec 1st kind H(curl)", 1))
+    G = build_discrete_operator(W._cpp_object, V._cpp_object)
+    assert G.getRefCount() == 1
+    num_edges = mesh.topology.index_map(1).size_global
+    m, n = G.getSize()
+    assert m == num_edges
+    assert n == mesh.topology.index_map(0).size_global
+    assert numpy.is_close(G.norm(PETSc.NormType.FROBENIUS), numpy.sqrt(2.0 * num_edges)
 
 
 def test_incompatible_spaces():
-    "Test that error is thrown when function spaces are not compatible"
+    """Test that error is thrown when function spaces are not compatible"""
 
-    mesh = UnitSquareMesh(MPI.COMM_WORLD, 13, 7)
-    V = FunctionSpace(mesh, ("Lagrange", 1))
-    W = FunctionSpace(mesh, ("Nedelec 1st kind H(curl)", 1))
+    mesh=UnitSquareMesh(MPI.COMM_WORLD, 13, 7)
+    V=FunctionSpace(mesh, ("Lagrange", 1))
+    W=FunctionSpace(mesh, ("Nedelec 1st kind H(curl)", 1))
     with pytest.raises(RuntimeError):
-        DiscreteOperators.build_gradient(V._cpp_object, W._cpp_object)
+        build_discrete_operator(V._cpp_object, W._cpp_object)
     with pytest.raises(RuntimeError):
-        DiscreteOperators.build_gradient(V._cpp_object, V._cpp_object)
+        build_discrete_operator(V._cpp_object, V._cpp_object)
     with pytest.raises(RuntimeError):
-        DiscreteOperators.build_gradient(W._cpp_object, W._cpp_object)
+        build_discrete_operator(W._cpp_object, W._cpp_object)
 
-    V = FunctionSpace(mesh, ("Lagrange", 2))
+    V=FunctionSpace(mesh, ("Lagrange", 2))
     with pytest.raises(RuntimeError):
-        DiscreteOperators.build_gradient(W._cpp_object, V._cpp_object)
+        build_discrete_operator(W._cpp_object, V._cpp_object)
