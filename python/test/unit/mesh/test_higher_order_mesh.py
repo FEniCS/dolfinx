56486c93780747750ba7481bbf13d56d96ecebfa
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2021-01-20 19:41:24 +0000

    correct permutation direction in higher order geometry

--- python/test/unit/mesh/test_higher_order_mesh.py
+++ python/test/unit/mesh/test_higher_order_mesh.py
@@ -6,6 +6,7 @@
 """ Unit-tests for higher order meshes """
 
 import os
+import random
 
 import numpy as np
 import pytest
@@ -22,6 +23,135 @@ from dolfinx_utils.test.skips import skip_in_parallel
 from mpi4py import MPI
 from sympy.vector import CoordSys3D, matrix_to_vector
 from ufl import dx
+
+
+@skip_in_parallel
+@pytest.mark.parametrize('order', [1, 2, 3, 4])
+def test_quadrilateral_mesh(order):
+    random.seed(13)
+
+    points = []
+    points += [[i / order, 0] for i in range(order + 1)]
+    for j in range(1, order):
+        points += [[i / order + 0.1, j / order] for i in range(order + 1)]
+
+    points += [[j / order, 1] for j in range(order + 1)]
+
+    cell = [0, order, order * (order + 1), order * (order + 2)]
+    if order > 1:
+        cell += list(range(1, order))
+        cell += list(range(order + 1, order * (order + 1), order + 1))
+        cell += list(range(2 * order + 1, order * (order + 1), order + 1))
+        cell += list(range(order * (order + 1) + 1, order * (order + 2)))
+
+        for i in range(1, order):
+            cell += list(range(i * (order + 1) + 1, i * (order + 1) + order))
+
+    domain = ufl.Mesh(ufl.VectorElement(
+        "Q", ufl.Cell("quadrilateral", geometric_dimension=2), order))
+
+    point_order = list(range((order + 1) ** 2))
+    for repeat in range(5):
+        random.shuffle(point_order)
+        ordered_points = np.zeros(((order + 1) ** 2, 2))
+        for i, j in enumerate(point_order):
+            ordered_points[j] = points[i]
+        ordered_cell = [point_order[i] for i in cell]
+
+        mesh = create_mesh(MPI.COMM_WORLD, [ordered_cell], ordered_points, domain)
+        area = assemble_scalar(1 * dx(mesh))
+
+        assert np.isclose(area, 1)
+
+
+@skip_in_parallel
+@pytest.mark.parametrize('order', [1, 2, 3, 4])
+def test_hexahedron_mesh(order):
+    random.seed(13)
+
+    points = []
+    points += [[i / order, j / order, 0] for j in range(order + 1)
+               for i in range(order + 1)]
+    for k in range(1, order):
+        points += [[i / order, j / order + 0.1, k / order] for j in range(order + 1)
+                   for i in range(order + 1)]
+
+    points += [[i / order, j / order, 1] for j in range(order + 1) for i in range(order + 1)]
+
+    def coord_to_vertex(x, y, z):
+        return (order + 1) ** 2 * z + (order + 1) * y + x
+
+    cell = [coord_to_vertex(x, y, z) for x, y, z in [
+        (0, 0, 0), (order, 0, 0), (0, order, 0), (order, order, 0),
+        (0, 0, order), (order, 0, order), (0, order, order), (order, order, order)]]
+
+    if order > 1:
+        for i in range(1, order):
+            cell.append(coord_to_vertex(i, 0, 0))
+        for i in range(1, order):
+            cell.append(coord_to_vertex(0, i, 0))
+        for i in range(1, order):
+            cell.append(coord_to_vertex(0, 0, i))
+        for i in range(1, order):
+            cell.append(coord_to_vertex(order, i, 0))
+        for i in range(1, order):
+            cell.append(coord_to_vertex(order, 0, i))
+        for i in range(1, order):
+            cell.append(coord_to_vertex(i, order, 0))
+        for i in range(1, order):
+            cell.append(coord_to_vertex(0, order, i))
+        for i in range(1, order):
+            cell.append(coord_to_vertex(order, order, i))
+        for i in range(1, order):
+            cell.append(coord_to_vertex(i, 0, order))
+        for i in range(1, order):
+            cell.append(coord_to_vertex(0, i, order))
+        for i in range(1, order):
+            cell.append(coord_to_vertex(order, i, order))
+        for i in range(1, order):
+            cell.append(coord_to_vertex(i, order, order))
+
+        for j in range(1, order):
+            for i in range(1, order):
+                cell.append(coord_to_vertex(i, j, 0))
+        for j in range(1, order):
+            for i in range(1, order):
+                cell.append(coord_to_vertex(i, 0, j))
+        for j in range(1, order):
+            for i in range(1, order):
+                cell.append(coord_to_vertex(0, i, j))
+        for j in range(1, order):
+            for i in range(1, order):
+                cell.append(coord_to_vertex(order, i, j))
+        for j in range(1, order):
+            for i in range(1, order):
+                cell.append(coord_to_vertex(i, order, j))
+        for j in range(1, order):
+            for i in range(1, order):
+                cell.append(coord_to_vertex(i, j, order))
+
+        for k in range(1, order):
+            for j in range(1, order):
+                for i in range(1, order):
+                    cell.append(coord_to_vertex(i, j, k))
+
+    domain = ufl.Mesh(ufl.VectorElement(
+        "Q", ufl.Cell("hexahedron", geometric_dimension=3), order))
+
+    print(cell, len(cell))
+
+    point_order = list(range((order + 1) ** 3))
+    for repeat in range(5):
+        random.shuffle(point_order)
+        ordered_points = np.zeros(((order + 1) ** 3, 3))
+        for i, j in enumerate(point_order):
+            ordered_points[j] = points[i]
+        ordered_cell = [point_order[i] for i in cell]
+
+        mesh = create_mesh(MPI.COMM_WORLD, [ordered_cell], ordered_points, domain)
+        volume = assemble_scalar(1 * dx(mesh))
+
+        assert np.isclose(volume, 1)
 
 
 def sympy_scipy(points, nodes, L, H):
@@ -384,168 +514,6 @@ def test_xdmf_input_tri(datadir):
         mesh = xdmf.read_mesh(name="Grid")
     surface = assemble_scalar(1 * dx(mesh))
     assert mesh.mpi_comm().allreduce(surface, op=MPI.SUM) == pytest.approx(4 * np.pi, rel=1e-4)
-
-
-@skip_in_parallel
-@pytest.mark.parametrize('L', [1, 2])
-@pytest.mark.parametrize('H', [1])
-@pytest.mark.parametrize('Z', [0, 0.3])
-def test_second_order_quad(L, H, Z):
-    """ Test by comparing integration of z+x*y against sympy/scipy
-    integration of a quad element. Z>0 implies curved element.
-
-      *-----*   3--6--2
-      |     |   |     |
-      |     |   7  8  5
-      |     |   |     |
-      *-----*   0--4--1
-
-    """
-    points = np.array([[0, 0, 0], [L, 0, 0], [L, H, Z], [0, H, Z],
-                       [L / 2, 0, 0], [L, H / 2, 0],
-                       [L / 2, H, Z], [0, H / 2, 0],
-                       [L / 2, H / 2, 0],
-                       [2 * L, 0, 0], [2 * L, H, Z]])
-    cells = np.array([[0, 1, 2, 3, 4, 5, 6, 7, 8]])
-    cells = cells[:, perm_vtk(CellType.quadrilateral, cells.shape[1])]
-    cell = ufl.Cell("quadrilateral", geometric_dimension=points.shape[1])
-    domain = ufl.Mesh(ufl.VectorElement("Lagrange", cell, 2))
-    mesh = create_mesh(MPI.COMM_WORLD, cells, points, domain)
-
-    def e2(x):
-        return x[2] + x[0] * x[1]
-
-    # Interpolate function
-    V = FunctionSpace(mesh, ("CG", 2))
-    u = Function(V)
-    u.interpolate(e2)
-
-    intu = assemble_scalar(u * dx(mesh))
-    intu = mesh.mpi_comm().allreduce(intu, op=MPI.SUM)
-
-    nodes = [0, 3, 7]
-    ref = sympy_scipy(points, nodes, L, H)
-    assert ref == pytest.approx(intu, rel=1e-6)
-
-
-@skip_in_parallel
-@pytest.mark.parametrize('L', [1, 2])
-@pytest.mark.parametrize('H', [1])
-@pytest.mark.parametrize('Z', [0, 0.3])
-def test_third_order_quad(L, H, Z):
-    """Test by comparing integration of z+x*y against sympy/scipy integration
-    of a quad element. Z>0 implies curved element.
-
-      *---------*   3--8--9--2-22-23-17
-      |         |   |        |       |
-      |         |   11 14 15 7 26 27 21
-      |         |   |        |       |
-      |         |   10 12 13 6 24 25 20
-      |         |   |        |       |
-      *---------*   0--4--5--1-18-19-16
-
-    """
-    points = np.array([[0, 0, 0], [L, 0, 0], [L, H, Z], [0, H, Z],        # 0  1 2 3
-                       [L / 3, 0, 0], [2 * L / 3, 0, 0],                  # 4  5
-                       [L, H / 3, 0], [L, 2 * H / 3, 0],                  # 6  7
-                       [L / 3, H, Z], [2 * L / 3, H, Z],                  # 8  9
-                       [0, H / 3, 0], [0, 2 * H / 3, 0],                  # 10 11
-                       [L / 3, H / 3, 0], [2 * L / 3, H / 3, 0],          # 12 13
-                       [L / 3, 2 * H / 3, 0], [2 * L / 3, 2 * H / 3, 0],  # 14 15
-                       [2 * L, 0, 0], [2 * L, H, Z],                      # 16 17
-                       [4 * L / 3, 0, 0], [5 * L / 3, 0, 0],              # 18 19
-                       [2 * L, H / 3, 0], [2 * L, 2 * H / 3, 0],          # 20 21
-                       [4 * L / 3, H, Z], [5 * L / 3, H, Z],              # 22 23
-                       [4 * L / 3, H / 3, 0], [5 * L / 3, H / 3, 0],           # 24 25
-                       [4 * L / 3, 2 * H / 3, 0], [5 * L / 3, 2 * H / 3, 0]])  # 26 27
-
-    # Change to multiple cells when matthews dof-maps work for quads
-    cells = np.array([[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
-                      [1, 16, 17, 2, 18, 19, 20, 21, 22, 23, 6, 7, 24, 25, 26, 27]])
-    cells = cells[:, perm_vtk(CellType.quadrilateral, cells.shape[1])]
-    cell = ufl.Cell("quadrilateral", geometric_dimension=points.shape[1])
-    domain = ufl.Mesh(ufl.VectorElement("Lagrange", cell, 3))
-    mesh = create_mesh(MPI.COMM_WORLD, cells, points, domain)
-
-    def e2(x):
-        return x[2] + x[0] * x[1]
-
-    # Interpolate function
-    V = FunctionSpace(mesh, ("CG", 3))
-    u = Function(V)
-    u.interpolate(e2)
-
-    intu = assemble_scalar(u * dx(mesh))
-    intu = mesh.mpi_comm().allreduce(intu, op=MPI.SUM)
-
-    nodes = [0, 3, 10, 11]
-    ref = sympy_scipy(points, nodes, 2 * L, H)
-    assert ref == pytest.approx(intu, rel=1e-6)
-
-
-@skip_in_parallel
-@pytest.mark.parametrize('L', [1, 2])
-@pytest.mark.parametrize('H', [1])
-@pytest.mark.parametrize('Z', [0, 0.3])
-def test_fourth_order_quad(L, H, Z):
-    """Test by comparing integration of z+x*y against sympy/scipy integration
-    of a quad element. Z>0 implies curved element.
-
-      *---------*   20-21-22-23-24-41--42--43--44
-      |         |   |           |              |
-      |         |   15 16 17 18 19 37  38  39  40
-      |         |   |           |              |
-      |         |   10 11 12 13 14 33  34  35  36
-      |         |   |           |              |
-      |         |   5  6  7  8  9  29  30  31  32
-      |         |   |           |              |
-      *---------*   0--1--2--3--4--25--26--27--28
-
-    """
-    points = np.array([[0, 0, 0], [L / 4, 0, 0], [L / 2, 0, 0],               # 0 1 2
-                       [3 * L / 4, 0, 0], [L, 0, 0],                          # 3 4
-                       [0, H / 4, -Z / 3], [L / 4, H / 4, -Z / 3], [L / 2, H / 4, -Z / 3],   # 5 6 7
-                       [3 * L / 4, H / 4, -Z / 3], [L, H / 4, -Z / 3],                  # 8 9
-                       [0, H / 2, 0], [L / 4, H / 2, 0], [L / 2, H / 2, 0],   # 10 11 12
-                       [3 * L / 4, H / 2, 0], [L, H / 2, 0],                  # 13 14
-                       [0, (3 / 4) * H, 0], [L / 4, (3 / 4) * H, 0],          # 15 16
-                       [L / 2, (3 / 4) * H, 0], [3 * L / 4, (3 / 4) * H, 0],  # 17 18
-                       [L, (3 / 4) * H, 0], [0, H, Z], [L / 4, H, Z],         # 19 20 21
-                       [L / 2, H, Z], [3 * L / 4, H, Z], [L, H, Z],           # 22 23 24
-                       [(5 / 4) * L, 0, 0], [(6 / 4) * L, 0, 0],              # 25 26
-                       [(7 / 4) * L, 0, 0], [2 * L, 0, 0],                    # 27 28
-                       [(5 / 4) * L, H / 4, -Z / 3], [(6 / 4) * L, H / 4, -Z / 3],      # 29 30
-                       [(7 / 4) * L, H / 4, -Z / 3], [2 * L, H / 4, -Z / 3],            # 31 32
-                       [(5 / 4) * L, H / 2, 0], [(6 / 4) * L, H / 2, 0],      # 33 34
-                       [(7 / 4) * L, H / 2, 0], [2 * L, H / 2, 0],            # 35 36
-                       [(5 / 4) * L, 3 / 4 * H, 0],                           # 37
-                       [(6 / 4) * L, 3 / 4 * H, 0],                           # 38
-                       [(7 / 4) * L, 3 / 4 * H, 0], [2 * L, 3 / 4 * H, 0],    # 39 40
-                       [(5 / 4) * L, H, Z], [(6 / 4) * L, H, Z],              # 41 42
-                       [(7 / 4) * L, H, Z], [2 * L, H, Z]])                   # 43 44
-
-    # VTK ordering
-    cells = np.array([[0, 4, 24, 20, 1, 2, 3, 9, 14, 19, 21, 22, 23, 5, 10, 15, 6, 7, 8, 11, 12, 13, 16, 17, 18],
-                      [4, 28, 44, 24, 25, 26, 27, 32, 36, 40, 41, 42, 43, 9, 14, 19,
-                       29, 30, 31, 33, 34, 35, 37, 38, 39]])
-    cells = cells[:, perm_vtk(CellType.quadrilateral, cells.shape[1])]
-    cell = ufl.Cell("quadrilateral", geometric_dimension=points.shape[1])
-    domain = ufl.Mesh(ufl.VectorElement("Lagrange", cell, 4))
-    mesh = create_mesh(MPI.COMM_WORLD, cells, points, domain)
-
-    def e2(x):
-        return x[2] + x[0] * x[1]
-
-    V = FunctionSpace(mesh, ("CG", 4))
-    u = Function(V)
-    u.interpolate(e2)
-
-    intu = assemble_scalar(u * dx(mesh))
-    intu = mesh.mpi_comm().allreduce(intu, op=MPI.SUM)
-
-    nodes = [0, 5, 10, 15, 20]
-    ref = sympy_scipy(points, nodes, 2 * L, H)
-    assert ref == pytest.approx(intu, rel=1e-5)
 
 
 @skip_in_parallel
