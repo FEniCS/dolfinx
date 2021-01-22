6dde8d87982070d04e5076de60e9af2b34bbf7fa
Massimiliano Leoni <mleoni@bcamath.org>
2021-01-22 15:11:09 +0100

    Make doxygen happy

--- cpp/dolfinx/fem/DirichletBC.h
+++ cpp/dolfinx/fem/DirichletBC.h
@@ -78,7 +78,7 @@ std::array<std::vector<std::int32_t>, 2> locate_dofs_topological(
 /// with V.
 std::vector<std::int32_t>
 locate_dofs_topological(const fem::FunctionSpace& V, const int dim,
-                        const std::vector<int32_t>& entities,
+                        const std::vector<std::int32_t>& entities,
                         bool remote = true);
 
 /// Finds degrees of freedom whose geometric coordinate is true for the
