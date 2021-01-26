cb3e3272cff598bb43075818374edf0868b533d5
Jørgen Dokken <jsd55@cam.ac.uk>
2021-01-26 09:15:22 +0000

    Add pyvista yml

---
+++ python/demo/test_pyvista_demos.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2021 Garth N. Wells and Jø¶gen S. Dokken
+#
+# This file is part of DOLFINX (https://www.fenicsproject.org)
+#
+# SPDX-License-Identifier:    LGPL-3.0-or-later
+
+import os
+import pathlib
+import subprocess
+import sys
+
+import pytest
+
+
+# List of demo programs using pyvista
+demos = ["poisson/demo_poisson.py",
+         "cahn-hilliard/demo_cahn-hilliard.py",
+         "pyvista/demo_pyvista.py"]
+
+
+@pytest.mark.serial
+@pytest.mark.parametrize("path,name", demos)
+def test_demos(path, name):
+    ret = subprocess.run([sys.executable, name],
+                         cwd=str(path),
+                         env={**os.environ, 'MPLBACKEND': 'agg'},
+                         check=True)
+    assert ret.returncode == 0
+
+
+@pytest.mark.mpi
+@pytest.mark.parametrize("path,name", demos)
+def test_demos_mpi(num_proc, mpiexec, path, name):
+    cmd = [mpiexec, "-np", str(num_proc), sys.executable, name]
+    print(cmd)
+    ret = subprocess.run(cmd,
+                         cwd=str(path),
+                         env={**os.environ, 'MPLBACKEND': 'agg'},
+                         check=True)
+    assert ret.returncode == 0
