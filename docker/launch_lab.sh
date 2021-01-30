6e42414ddd902de99ff25c54f3637935a5bf35d8
dokken <dokken92@gmail.com>
2021-01-30 19:39:21 +0000

    Remove some env variables from launch lab

--- docker/launch_lab.sh
+++ docker/launch_lab.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -x
 export DISPLAY=:99.0
-export PYVISTA_OFF_SCREEN=true
-export PYVISTA_USE_IPYVTK=true
 Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
 set +x
 jupyter lab --ip 0.0.0.0 --no-browser --allow-root
