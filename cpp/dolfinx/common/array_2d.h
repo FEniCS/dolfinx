a85b2c39c0b84ef4e2449f3794b5b4e32f95675b
igorbaratta <igorbaratta@gmail.com>
2021-02-07 18:11:54 -0300

    fix docmentation

--- cpp/dolfinx/common/array_2d.h
+++ cpp/dolfinx/common/array_2d.h
@@ -25,13 +25,14 @@ struct is_2d_container<T, std::void_t<decltype(std::declval<T>().data()),
 namespace dolfinx::common
 {
 
-/// This class provides a dynamic 2-dimensional array structure.
-/// The representation is strictly local, i.e. it is not parallel
-/// aware.
+/// This class provides a dynamic 2-dimensional row-wise array
+/// data structure. The representation is strictly local, i.e.
+/// it is not parallel aware.
 template <typename T, class Allocator = std::allocator<T>>
 class array_2d
 {
 public:
+  /// \cond DO_NOT_DOCUMENT
   using value_type = T;
   using allocator_type = Allocator;
   using size_type = typename std::vector<T, Allocator>::size_type;
@@ -40,19 +41,22 @@ public:
   using pointer = typename std::vector<T, Allocator>::pointer;
   using iterator = typename std::vector<T, Allocator>::iterator;
   using const_iterator = typename std::vector<T, Allocator>::const_iterator;
+  /// \endcond
 
-  /// Construct an empty two dimensional array.
+  /// Constructs an empty two dimensional array.
   array_2d() : _rows(0), _cols(0) { _storage = std::vector<T, Allocator>(); }
 
-  array_2d(size_type rows, size_type columns, value_type value = T(),
+  /// Constructs a two dimensional array with size = rows * cols, and initialize
+  /// elements with value value.
+  array_2d(size_type rows, size_type cols, value_type value = T(),
            const Allocator& alloc = Allocator())
-      : _rows(rows), _cols(columns)
+      : _rows(rows), _cols(cols)
   {
     _storage = std::vector<T, Allocator>(_rows * _cols, value, alloc);
   }
 
-  // TODO: remvoe this construtctor.
-  // Only used for
+  /// Constructs a two dimensional array with the copy of the contents of other
+  /// two dimensional container.
   template <class Container,
             typename
             = typename std::enable_if<is_2d_container<Container>::value>::type>
@@ -60,11 +64,12 @@ public:
   {
     _rows = array.rows();
     _cols = array.cols();
-
-    std::copy(array.data(), array.data() + array.size(),
-              std::back_inserter(_storage));
+    _storage.resize(array.size());
+    std::copy(array.data(), array.data() + array.size(), _storage.begin());
   }
 
+  /// Constructs a two dimensional array with the contents of the initializer
+  /// list init.
   array_2d(size_type rows, size_type columns, std::initializer_list<T> list,
            const Allocator& alloc = Allocator())
       : _rows(rows), _cols(columns)
@@ -74,81 +79,100 @@ public:
     _storage = std::vector<T, Allocator>(list, alloc);
   }
 
-  reference operator()(size_type x, size_type y)
+  /// Returns a reference to the element at specified location (i, j).
+  /// No bounds checking is performed.
+  reference operator()(size_type i, size_type j)
   {
-    return _storage[x * _cols + y];
+    return _storage[i * _cols + j];
   }
 
-  const_reference operator()(size_type x, size_type y) const
+  /// Returns a const reference to the element at specified location (i, j).
+  /// No bounds checking is performed.
+  const_reference operator()(size_type i, size_type j) const
   {
-    return _storage[x * _cols + y];
+    return _storage[i * _cols + j];
   }
 
+  /// Returns a reference to the row at specified location (i).
   tcb::span<value_type> row(int i) const
   {
     size_type offset = i * _cols;
     return tcb::span<value_type>(pointer(&_storage[0] + offset), _cols);
   }
 
+  /// Returns a pointer to the first element of the array.
   value_type* data() noexcept { return _storage.data(); }
 
+  /// Returns a const pointer to the first element of the array.
   const value_type* data() const noexcept { return _storage.data(); };
 
+  /// Returns an iterator to the first element of the array.
   iterator begin() noexcept { return _storage.begin(); }
 
+  /// Returns an iterator to the first element of the array.
   const_iterator begin() const noexcept { return _storage.begin(); }
 
+  /// Returns an iterator to the element following the last element.
   iterator end() noexcept { return _storage.end(); }
 
+  /// Returns an iterator to the element following the last element.
   const_iterator end() const noexcept { return _storage.end(); }
 
+  /// Returns a const iterator to the first element of the array.
   const_iterator cbegin() noexcept { return _storage.cbegin(); }
 
+  /// Returns a const iterator to the element following the last element.
   const_iterator cend() noexcept { return _storage.cend(); }
 
+  /// Returns the number of elements in the array (rows * cols).
   size_type size() const noexcept { return _storage.size(); }
 
+  /// Returns the number of cols in the two-dimensional array.
   size_type cols() const noexcept { return _cols; }
 
+  /// Returns the number of rows in the two-dimensional array.
   size_type rows() const noexcept { return _rows; }
 
+  /// Returns the array dimensions {cols, rows}.
   std::pair<size_type, size_type> shape() const noexcept
   {
     return {_rows, _cols};
   }
 
+  /// Changes the number of elements stored.
   void resize(size_type rows, size_type cols, value_type val = value_type())
   {
     // TODO: check rows and cols
-    _storage.resize(rows * cols, val);
     _rows = rows;
     _cols = cols;
+    _storage.resize(rows * cols, val);
   }
 
-  /// Could be improved with ndrange
+  /// Copies a block of data from other container.
   template <class Container,
             typename
             = typename std::enable_if<is_2d_container<Container>::value>::type>
   void copy(const Container& other)
   {
-    std::int32_t rows = std::min<std::int32_t>(_rows, other.rows());
-    std::int32_t cols = std::min<std::int32_t>(_cols, other.cols());
-    for (std::int32_t i = 0; i < rows; i++)
+    size_type rows = std::min<size_type>(_rows, other.rows());
+    size_type cols = std::min<size_type>(_cols, other.cols());
+    for (size_type i = 0; i < rows; i++)
     {
-      for (std::int32_t j = 0; j < cols; j++)
+      for (size_type j = 0; j < cols; j++)
         _storage[i * _cols + j] = other(i, j);
     }
   }
 
+  /// Checks whether the container is empty.
   bool empty() const noexcept { return _storage.empty(); }
 
-  /// Returns a reference to the first element in the container.
-  /// Calling front on an empty container is undefined.
-  reference front() { return _storage.front(); }
+  // /// Returns a reference to the first element in the container.
+  // /// Calling front on an empty container is undefined.
+  // reference front() { return _storage.front(); }
 
-  reference back() { return _storage.back(); }
+  // reference back() { return _storage.back(); }
 
-protected:
+private:
   size_type _rows;
   size_type _cols;
   std::vector<T, Allocator> _storage;
