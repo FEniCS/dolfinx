7f727a8b6e24830047a614116706657233355372
Garth N. Wells <gnw20@cam.ac.uk>
2021-02-12 11:43:50 +0000

    Another fix

--- cpp/dolfinx/fem/FiniteElement.h
+++ cpp/dolfinx/fem/FiniteElement.h
@@ -164,18 +164,25 @@ public:
     assert(dofs.size() == rows);
 
     // Compute dofs = Pi * x (matrix-vector multiply)
+    tcb::span vals(values.data(), values.shape[0] * values.shape[1]);
     assert(_interpolation_matrix.size() % rows == 0);
     const std::size_t cols = _interpolation_matrix.size() / rows;
     for (std::size_t i = 0; i < rows; ++i)
     {
       dofs[i] = 0;
       for (std::size_t j = 0; j < cols; ++j)
-        dofs[i] += _interpolation_matrix[i * cols + j] * values[j];
+        dofs[i] += _interpolation_matrix[i * cols + j] * vals[j];
+
       // The doesn't work for complex values
       // dofs[r] = std::transform_reduce(
       //     std::next(_interpolation_matrix.begin(), r * cols),
       //     std::next(_interpolation_matrix.begin(), r * cols + cols),
       //     values.data(), 0.0);
+
+      // dofs[r] = std::transform_reduce(
+      //     std::next(_interpolation_matrix.begin(), r * cols),
+      //     std::next(_interpolation_matrix.begin(), r * cols + cols),
+      //     values.data(), T(0.0));
     }
 
     _apply_dof_transformation_to_scalar(dofs.data(), cell_permutation, 1);
