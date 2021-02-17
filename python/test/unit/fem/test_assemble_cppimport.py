f25eeac8ab12f96820a76bd9bda61eaf3bb0bb2d
igorbaratta <igorbaratta@gmail.com>
2021-02-17 12:19:40 +0000

    fix eigen dir

--- python/test/unit/fem/test_assemble_cppimport.py
+++ python/test/unit/fem/test_assemble_cppimport.py
@@ -33,7 +33,7 @@ def test_eigen_assembly(tempdir):  # noqa: F811
         cpp_code_header = f"""
 <%
 setup_pybind11(cfg)
-cfg['include_dirs'] = {dolfinx_pc["include_dirs"] + [petsc4py.get_include()] + [str(pybind_inc())] + [eigen_dir]}
+cfg['include_dirs'] = {dolfinx_pc["include_dirs"] + [petsc4py.get_include()] + [str(pybind_inc())] + eigen_dir}
 cfg['compiler_args'] = {["-D" + dm for dm in dolfinx_pc["define_macros"]]}
 cfg['compiler_args'] = ['-std=c++17']
 cfg['libraries'] = {dolfinx_pc["libraries"]}
