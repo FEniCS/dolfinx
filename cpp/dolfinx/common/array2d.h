38569196f6bba7b6a1a792e8503aa5256c727b33
Garth N. Wells <gnw20@cam.ac.uk>
2021-03-03 08:15:21 +0000

    Doc fix

--- cpp/dolfinx/common/array2d.h
+++ cpp/dolfinx/common/array2d.h
@@ -215,10 +215,8 @@ public:
   // }
 
   /// Construct a two dimensional span from a two dimensional array
-  /// @param[in] x A 2D span or array
   template <typename Array2d>
   span2d(Array2d& x) : shape(x.shape), _storage(x.data())
-  // span2d(const array2d<T>& x) : _storage(x.data()), shape(x.shape)
   {
     // Do nothing
   }
