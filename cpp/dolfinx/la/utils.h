bbd1ec8656099bfef42cf63d2bc31d781f0175a3
Chris Richardson <chris@bpi.cam.ac.uk>
2021-03-09 09:49:29 +0000

    Move implementation to cpp file

--- cpp/dolfinx/la/utils.h
+++ cpp/dolfinx/la/utils.h
@@ -33,35 +33,3 @@ template <typename T>
 void scatter_rev(Vector<T>& v, dolfinx::common::IndexMap::Mode op);
 
 } // namespace dolfinx::la
-
-template <typename T>
-void dolfinx::la::scatter_fwd(dolfinx::la::Vector<T>& v)
-{
-  tcb::span<const T> xlocal(v.array().data(), v.map()->size_local());
-  tcb::span<T> xremote(v.mutable_array().data() + v.map()->size_local(),
-                       v.map()->num_ghosts());
-  v.map()->scatter_fwd(xlocal, xremote, v.bs());
-}
-
-template <typename T>
-void dolfinx::la::scatter_rev(dolfinx::la::Vector<T>& v,
-                              dolfinx::common::IndexMap::Mode op)
-{
-  tcb::span<T> xlocal(v.mutable_array().data(), v.map()->size_local());
-  tcb::span<const T> xremote(v.array().data() + v.map()->size_local(),
-                             v.map()->num_ghosts());
-  v.map()->scatter_rev(xlocal, xremote, v.bs(), op);
-}
-
-// \cond turn off doxygen
-template void dolfinx::la::scatter_fwd<double>(dolfinx::la::Vector<double>& v);
-template void dolfinx::la::scatter_fwd<std::complex<double>>(
-    dolfinx::la::Vector<std::complex<double>>& v);
-
-template void
-dolfinx::la::scatter_rev<double>(dolfinx::la::Vector<double>& v,
-                                 dolfinx::common::IndexMap::Mode op);
-template void dolfinx::la::scatter_rev<std::complex<double>>(
-    dolfinx::la::Vector<std::complex<double>>& v,
-    dolfinx::common::IndexMap::Mode op);
-// \endcond
