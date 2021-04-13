f4a9d1d44f9dc9185755e995dd4b04e1211c0308
Garth N. Wells <gnw20@cam.ac.uk>
2021-04-13 18:26:07 +0100

    Remove some Eigen from mesh creation

--- cpp/dolfinx/graph/AdjacencyList.h
+++ cpp/dolfinx/graph/AdjacencyList.h
@@ -13,6 +13,7 @@
 #include <sstream>
 #include <utility>
 #include <vector>
+#include <xtensor/xtensor.hpp>
 
 namespace dolfinx::graph
 {
@@ -32,6 +33,25 @@ auto create_adjacency_data(const array2d<T>& array)
     for (std::size_t j = 0; j < array.shape[1]; ++j)
       data[i * array.shape[1] + j] = array(i, j);
     offset[i + 1] = offset[i] + array.shape[1];
+  }
+  return std::pair(std::move(data), std::move(offset));
+}
+
+/// Construct adjacency list data for a problem with a fixed number of
+/// links (edges) for each node
+/// @param [in] matrix Two-dimensional array of adjacency data where
+/// matrix(i, j) is the jth neighbor of the ith node
+/// @return Adjacency list data and offset array
+template <typename T>
+auto create_adjacency_data(const xt::xtensor<T, 2>& array)
+{
+  std::vector<T> data(array.shape(0) * array.shape(1));
+  std::vector<std::int32_t> offset(array.shape(0) + 1, 0);
+  for (std::size_t i = 0; i < array.shape(0); ++i)
+  {
+    for (std::size_t j = 0; j < array.shape(1); ++j)
+      data[i * array.shape(1) + j] = array(i, j);
+    offset[i + 1] = offset[i] + array.shape(1);
   }
   return std::pair(std::move(data), std::move(offset));
 }
