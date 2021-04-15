bcda0d342cbb6e70aa17eba726c883cc8c45a80d
Garth N. Wells <gnw20@cam.ac.uk>
2021-04-15 18:56:04 +0100

    Fix typos

--- cpp/dolfinx/fem/assemble_vector_impl.h
+++ cpp/dolfinx/fem/assemble_vector_impl.h
@@ -597,12 +597,12 @@ void assemble_cells(
 
   // FIXME: Add proper interface for num coordinate dofs
   const std::size_t num_dofs_g = x_dofmap.num_links(0);
-  const xt::xtensor<double, 3>& x_g = geometry.x();
+  const xt::xtensor<double, 2>& x_g = geometry.x();
 
   // FIXME: Add proper interface for num_dofs
   // Create data structures used in assembly
   const int num_dofs = dofmap.links(0).size();
-  xt::xtensor<double, 3> coordinate_dofs({num_dofs_g, gdim});
+  xt::xtensor<double, 2> coordinate_dofs({num_dofs_g, gdim});
   std::vector<T> be(bs * num_dofs);
 
   // Iterate over active cells
