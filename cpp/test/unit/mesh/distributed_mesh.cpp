f0774554b795ebe248062c8fa301e7cdb5e6907e
Garth N. Wells <gnw20@cam.ac.uk>
2021-04-16 16:01:37 +0100

    Fix bracket

--- cpp/test/unit/mesh/distributed_mesh.cpp
+++ cpp/test/unit/mesh/distributed_mesh.cpp
@@ -60,7 +60,8 @@ void test_distributed_mesh(mesh::CellPartitionFunction partitioner)
   // read mesh data
   xt::xtensor<double, 2> x({0, 3});
   xt::xtensor<std::int64_t, 2> cells(
-      {0, static_cast<std::size_t>(dolfinx::mesh::num_cell_vertices(mesh::CellType::triangle)}));
+      {0, static_cast<std::size_t>(
+              dolfinx::mesh::num_cell_vertices(mesh::CellType::triangle))});
   graph::AdjacencyList<std::int32_t> dest(0);
   if (subset_comm != MPI_COMM_NULL)
   {
