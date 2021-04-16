8a70917317f449ae8b1a66de918033ac63dcac6d
Jørgen Dokken <jsd55@cam.ac.uk>
2021-04-16 14:07:59 +0000

    Add docstrings to dofmap python interface

--- python/dolfinx/fem/dofmap.py
+++ python/dolfinx/fem/dofmap.py
@@ -19,24 +19,46 @@ class DofMap:
         self._cpp_object = dofmap
 
     def cell_dofs(self, cell_index: int):
+        """ Returns the Local-global dof map for the cell (using process-local indices)
+
+        Parameters
+        ----------
+        cell
+          The cell index
+        """
         return self._cpp_object.cell_dofs(cell_index)
 
     @property
     def bs(self):
+        """
+        Returns the block size of the dofmap
+        """
         return self._cpp_object.bs
 
     @property
     def dof_layout(self):
+        """
+        Returns the layout of dofs on an element
+        """
         return self._cpp_object.dof_layout
 
     @property
     def index_map(self):
+        """
+        Returns the index map that described the parallel distribution of the dofmap
+        """
         return self._cpp_object.index_map
 
     @property
     def index_map_bs(self):
+        """
+        Returns the block size of the index map
+        """
         return self._cpp_object.index_map_bs
 
     @property
     def list(self):
+        """
+        Returns the adjacency list with dof indices for each cell
+        """
         return self._cpp_object.list()
