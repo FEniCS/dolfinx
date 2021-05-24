3fd525c58babc4b3fb2399e38c87bb36990c0068
Chris Richardson <chris@bpi.cam.ac.uk>
2021-05-24 09:40:45 +0100

    Use cmake .tweak for dev version

--- python/test/unit/common/test_version.py
+++ python/test/unit/common/test_version.py
@@ -11,10 +11,8 @@ import dolfinx
 def test_version():
     """Test that installed Python version matches c++ version"""
     version = pkg_resources.get_distribution("fenics-dolfinx").version
-    # remove any final suffix like '.dev0'
-    version = '.'.join(version.split('.')[:3])
+    # Change any final '.dev0' to '.0'
+    version = version.replace('dev', '')
     cpp_version = dolfinx.__version__
-    # remove any final suffix like '-rc0'
-    cpp_version = cpp_version.split("-")[0]
     if version != cpp_version:
         raise RuntimeError("Incorrect installed version")
