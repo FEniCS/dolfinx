f2b9b493b7d362143da8fb4a05dab8a2c677a23b
Chris Richardson <chris@bpi.cam.ac.uk>
2021-05-24 09:23:28 +0100

    Add rc0/dev0 to version

--- python/test/unit/common/test_version.py
+++ python/test/unit/common/test_version.py
@@ -11,5 +11,10 @@ import dolfinx
 def test_version():
     """Test that installed Python version matches c++ version"""
     version = pkg_resources.get_distribution("fenics-dolfinx").version
-    if version != dolfinx.__version__:
+    # remove any final suffix like '.dev0'
+    version = '.'.join(version.split('.')[:3])
+    cpp_version = dolfinx.__version__
+    # remove any final suffix like '-rc0'
+    cpp_version = cpp_version.split("-")[0]
+    if version != cpp_version:
         raise RuntimeError("Incorrect installed version")
