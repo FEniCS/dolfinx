78710f1bf896334d9d788809bc7fa18a95300a79
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2021-06-09 10:09:45 +0100

    docs

--- cpp/dolfinx/fem/FiniteElement.h
+++ cpp/dolfinx/fem/FiniteElement.h
@@ -192,10 +192,6 @@ public:
   bool needs_permutation_data() const noexcept;
 
   /// Does nothing. Can be used when no permutation data is needed
-  ///
-  /// @param[in,out] data The data to be transformed
-  /// @param[in] cell_permutation Permutation data for the cell
-  /// @param[in] block_size The block_size of the input data
   template <typename T>
   void apply_no_transformation(xtl::span<T>, std::uint32_t, int) const
   {
