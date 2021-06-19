07cc38fb43541dd2e3423c3431d3e6724b1dd46c
Garth N. Wells <gnw20@cam.ac.uk>
2021-06-19 12:38:45 +0100

    Re-arrange assembly functions

--- cpp/dolfinx/fem/assemble_scalar_impl.h
+++ cpp/dolfinx/fem/assemble_scalar_impl.h
@@ -21,91 +21,7 @@
 namespace dolfinx::fem::impl
 {
 
-/// Assemble functional into an scalar
-template <typename T>
-T assemble_scalar(const fem::Form<T>& M, const xtl::span<const T>& constants,
-                  const array2d<T>& coeffs);
-
 /// Assemble functional over cells
-template <typename T>
-T assemble_cells(const mesh::Geometry& geometry,
-                 const xtl::span<const std::int32_t>& active_cells,
-                 const std::function<void(T*, const T*, const T*, const double*,
-                                          const int*, const std::uint8_t*)>& fn,
-                 const xtl::span<const T>& constants, const array2d<T>& coeffs);
-
-/// Execute kernel over exterior facets and accumulate result
-template <typename T>
-T assemble_exterior_facets(
-    const mesh::Mesh& mesh, const xtl::span<const std::int32_t>& active_cells,
-    const std::function<void(T*, const T*, const T*, const double*, const int*,
-                             const std::uint8_t*)>& fn,
-    const xtl::span<const T>& constants, const array2d<T>& coeffs,
-    const xtl::span<const std::uint8_t>& perms);
-
-/// Assemble functional over interior facets
-template <typename T>
-T assemble_interior_facets(
-    const mesh::Mesh& mesh, const xtl::span<const std::int32_t>& active_cells,
-    const std::function<void(T*, const T*, const T*, const double*, const int*,
-                             const std::uint8_t*)>& fn,
-    const xtl::span<const T>& constants, const array2d<T>& coeffs,
-    const xtl::span<const int>& offsets,
-    const xtl::span<const std::uint8_t>& perms);
-
-//-----------------------------------------------------------------------------
-template <typename T>
-T assemble_scalar(const fem::Form<T>& M, const xtl::span<const T>& constants,
-                  const array2d<T>& coeffs)
-{
-  std::shared_ptr<const mesh::Mesh> mesh = M.mesh();
-  assert(mesh);
-  const int tdim = mesh->topology().dim();
-
-  T value(0);
-  for (int i : M.integral_ids(IntegralType::cell))
-  {
-    const auto& fn = M.kernel(IntegralType::cell, i);
-    const std::vector<std::int32_t>& active_cells
-        = M.domains(IntegralType::cell, i);
-    value += impl::assemble_cells(mesh->geometry(), active_cells, fn, constants,
-                                  coeffs);
-  }
-
-  if (M.num_integrals(IntegralType::exterior_facet) > 0
-      or M.num_integrals(IntegralType::interior_facet) > 0)
-  {
-    // FIXME: cleanup these calls? Some of these happen internally again.
-    mesh->topology_mutable().create_entities(tdim - 1);
-    mesh->topology_mutable().create_connectivity(tdim - 1, tdim);
-    mesh->topology_mutable().create_entity_permutations();
-
-    const std::vector<std::uint8_t>& perms
-        = mesh->topology().get_facet_permutations();
-
-    for (int i : M.integral_ids(IntegralType::exterior_facet))
-    {
-      const auto& fn = M.kernel(IntegralType::exterior_facet, i);
-      const std::vector<std::int32_t>& active_facets
-          = M.domains(IntegralType::exterior_facet, i);
-      value += impl::assemble_exterior_facets(*mesh, active_facets, fn,
-                                              constants, coeffs, perms);
-    }
-
-    const std::vector<int> c_offsets = M.coefficient_offsets();
-    for (int i : M.integral_ids(IntegralType::interior_facet))
-    {
-      const auto& fn = M.kernel(IntegralType::interior_facet, i);
-      const std::vector<std::int32_t>& active_facets
-          = M.domains(IntegralType::interior_facet, i);
-      value += impl::assemble_interior_facets(
-          *mesh, active_facets, fn, constants, coeffs, c_offsets, perms);
-    }
-  }
-
-  return value;
-}
-//-----------------------------------------------------------------------------
 template <typename T>
 T assemble_cells(const mesh::Geometry& geometry,
                  const xtl::span<const std::int32_t>& active_cells,
@@ -142,7 +58,8 @@ T assemble_cells(const mesh::Geometry& geometry,
 
   return value;
 }
-//-----------------------------------------------------------------------------
+
+/// Execute kernel over exterior facets and accumulate result
 template <typename T>
 T assemble_exterior_facets(
     const mesh::Mesh& mesh, const xtl::span<const std::int32_t>& active_facets,
@@ -197,7 +114,8 @@ T assemble_exterior_facets(
 
   return value;
 }
-//-----------------------------------------------------------------------------
+
+/// Assemble functional over interior facets
 template <typename T>
 T assemble_interior_facets(
     const mesh::Mesh& mesh, const xtl::span<const std::int32_t>& active_facets,
@@ -284,6 +202,58 @@ T assemble_interior_facets(
 
   return value;
 }
-//-----------------------------------------------------------------------------
+
+/// Assemble functional into an scalar
+template <typename T>
+T assemble_scalar(const fem::Form<T>& M, const xtl::span<const T>& constants,
+                  const array2d<T>& coeffs)
+{
+  std::shared_ptr<const mesh::Mesh> mesh = M.mesh();
+  assert(mesh);
+  const int tdim = mesh->topology().dim();
+
+  T value(0);
+  for (int i : M.integral_ids(IntegralType::cell))
+  {
+    const auto& fn = M.kernel(IntegralType::cell, i);
+    const std::vector<std::int32_t>& active_cells
+        = M.domains(IntegralType::cell, i);
+    value += impl::assemble_cells(mesh->geometry(), active_cells, fn, constants,
+                                  coeffs);
+  }
+
+  if (M.num_integrals(IntegralType::exterior_facet) > 0
+      or M.num_integrals(IntegralType::interior_facet) > 0)
+  {
+    // FIXME: cleanup these calls? Some of these happen internally again.
+    mesh->topology_mutable().create_entities(tdim - 1);
+    mesh->topology_mutable().create_connectivity(tdim - 1, tdim);
+    mesh->topology_mutable().create_entity_permutations();
+
+    const std::vector<std::uint8_t>& perms
+        = mesh->topology().get_facet_permutations();
+
+    for (int i : M.integral_ids(IntegralType::exterior_facet))
+    {
+      const auto& fn = M.kernel(IntegralType::exterior_facet, i);
+      const std::vector<std::int32_t>& active_facets
+          = M.domains(IntegralType::exterior_facet, i);
+      value += impl::assemble_exterior_facets(*mesh, active_facets, fn,
+                                              constants, coeffs, perms);
+    }
+
+    const std::vector<int> c_offsets = M.coefficient_offsets();
+    for (int i : M.integral_ids(IntegralType::interior_facet))
+    {
+      const auto& fn = M.kernel(IntegralType::interior_facet, i);
+      const std::vector<std::int32_t>& active_facets
+          = M.domains(IntegralType::interior_facet, i);
+      value += impl::assemble_interior_facets(
+          *mesh, active_facets, fn, constants, coeffs, c_offsets, perms);
+    }
+  }
+
+  return value;
+}
 
 } // namespace dolfinx::fem::impl
