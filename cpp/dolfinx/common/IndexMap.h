b2572d1c3502ff7e4dd298166b13e02d8810dc6b
Garth N. Wells <gnw20@cam.ac.uk>
2021-06-27 08:22:13 +0100

    Doc fix

--- cpp/dolfinx/common/IndexMap.h
+++ cpp/dolfinx/common/IndexMap.h
@@ -169,11 +169,13 @@ public:
   /// be the same as n * size_local().
   ///
   /// @param[in] local_data Local data associated with each owned local
-  ///   index to be sent to process where the data is ghosted. Size must
-  ///   be n * size_local().
+  /// index to be sent to process where the data is ghosted. Size must
+  /// be n * size_local().
   /// @param[in,out] remote_data Ghost data on this process received
-  ///   from the owning process. Size will be n * num_ghosts().
+  /// from the owning process. Size will be n * num_ghosts().
   /// @param[in] n Number of data items per index
+  /// @param send_buffer
+  /// @param recv_buffer
   template <typename T>
   void scatter_fwd(const xtl::span<const T>& local_data,
                    xtl::span<T> remote_data, int n, std::vector<T>& send_buffer,
