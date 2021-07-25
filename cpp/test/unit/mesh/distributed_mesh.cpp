9fb531a4b53bd29f4465515c39710b5645075a6d
Garth N. Wells <gnw20@cam.ac.uk>
2021-07-25 12:58:49 +0100

    Test update

--- cpp/test/unit/mesh/distributed_mesh.cpp
+++ cpp/test/unit/mesh/distributed_mesh.cpp
@@ -137,11 +137,8 @@ TEST_CASE("Distributed Mesh", "[distributed_mesh]")
     {
       LOG(INFO) << "Compute partition of cells across ranks (KaHIP).";
       // Compute distributed dual graph (for the cells on this process)
-      const auto [dual_graph, graph_info]
+      const auto [dual_graph, num_ghost_edges]
           = mesh::build_dual_graph(comm, cells, tdim);
-
-      // Extract data from graph_info
-      const auto [num_ghost_edges, _] = graph_info;
 
       // Just flag any kind of ghosting for now
       bool ghosting = (ghost_mode != mesh::GhostMode::none);
