1b0e5ed457fc57735c896e1732e231e3b8cdb47c
Garth N. Wells <gnw20@cam.ac.uk>
2021-07-27 08:02:17 +0100

    Various MPI improvements (#1645)
    * Use MPI_Scan directly
    
    * Neighbourhood improvement
    
    * Fix bug
    
    * Use more MPI_Exscan
    
    * Small improvements
    
    * Use some non-blocking scan
    
    * Fix error
    
    * Non-blocking comm
    
    * More non-blocking comm.
    
    * Simplifications
    
    * More simplifications
    
    * More refactoring
    
    * Cleaning up
    
    * Tidy up
    
    * Doc improvements
    
    * More simplifications
    
    * Test update
    
    * Reduce number of MPI calls
    
    * MPI simplifications
    
    * Remove old code
    
    * More cleanup
    
    * Tidy up
    
    * Combine MPI call
    
    * CI fix
    
    * Small logic fix
    
    * Doc improvements
    
    * Improve doc
    
    * Doc improvements
    
    * Small improvement
    
    * Small improvements
    
    * Save file
    
    * Fix build errors
    
    * Remove MPI call
    
    * Simplifications
    
    * Undo some changes

--- cpp/dolfinx/refinement/utils.cpp
+++ cpp/dolfinx/refinement/utils.cpp
@@ -211,13 +211,15 @@ refinement::create_new_vertices(
       ++n;
     }
   }
-  const int num_new_vertices = n;
-  const std::size_t global_offset
-      = MPI::global_offset(mesh.mpi_comm(), num_new_vertices, true)
-        + mesh.topology().index_map(0)->local_range()[1];
 
-  for (auto& e : local_edge_to_new_vertex)
-    e.second += global_offset;
+  const std::int64_t num_local = n;
+  std::int64_t global_offset = 0;
+  MPI_Exscan(&num_local, &global_offset, 1,
+             dolfinx::MPI::mpi_type<std::int64_t>(), MPI_SUM, mesh.mpi_comm());
+  global_offset += mesh.topology().index_map(0)->local_range()[1];
+  std::for_each(local_edge_to_new_vertex.begin(),
+                local_edge_to_new_vertex.end(),
+                [global_offset](auto& e) { e.second += global_offset; });
 
   // Create actual points
   xt::xtensor<double, 2> new_vertex_coordinates
@@ -324,7 +326,7 @@ refinement::partition(const mesh::Mesh& old_mesh,
                         mesh::GhostMode)
   {
     // Find out the ghosting information
-    auto [graph, info] = mesh::build_dual_graph(comm, cell_topology, tdim);
+    auto [graph, _] = mesh::build_dual_graph(comm, cell_topology, tdim);
 
     // FIXME: much of this is reverse engineering of data that is already
     // known in the GraphBuilder
