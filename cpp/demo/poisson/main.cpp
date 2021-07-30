0ab929a08fdf513ff07cb956d0c11860768d557e
Garth N. Wells <gnw20@cam.ac.uk>
2021-07-30 16:35:30 +0100

    Remove from demo - potentially confusing

--- cpp/demo/poisson/main.cpp
+++ cpp/demo/poisson/main.cpp
@@ -235,16 +235,6 @@ int main(int argc, char* argv[])
     //
     // .. code-block:: cpp
 
-#ifdef HAS_ADIOS2
-    // Save solution in ADIOS format
-    dolfinx::io::ADIOS2File adios(MPI_COMM_WORLD, "poisson.bp", "w");
-    adios.write_mesh(*mesh);
-    f->name = "f";
-    adios.write_function({u, *f});
-    adios.write_function({u, *f});
-    adios.close();
-#endif
-
     // Save solution in VTK format
     io::VTKFile file(MPI_COMM_WORLD, "u.pvd", "w");
     file.write({u}, 0.0);
