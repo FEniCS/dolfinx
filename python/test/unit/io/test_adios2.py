74780a13bff1b9a8808e1450e0c16f65c0961d47
Jørgen Dokken <jsd55@cam.ac.uk>
2021-08-06 10:07:17 +0000

    Cleanup tests for IO

--- python/test/unit/io/test_adios2.py
+++ python/test/unit/io/test_adios2.py
@@ -18,19 +18,20 @@ assert (tempdir)
 
 
 @pytest.mark.skipif(not has_adios2, reason="Requires ADIOS2.")
-def test_save_mesh_fides(tempdir):
+def test_fides_mesh(tempdir):
     filename = os.path.join(tempdir, "mesh_fides.bp")
 
     mesh = UnitSquareMesh(MPI.COMM_WORLD, 5, 5)
     f = FidesWriter(mesh.mpi_comm(), filename, io.mode.write, mesh)
     f.write(0.0)
+    mesh.geometry.x[:, 1] += 0.1
+    f.write(0.1)
     f.close()
 
 
 @pytest.mark.skipif(not has_adios2, reason="Requires ADIOS2.")
-def test_save_mesh_vtx(tempdir):
+def test_vtx_mesh(tempdir):
     filename = os.path.join(tempdir, "mesh_vtx.bp")
-
     mesh = UnitSquareMesh(MPI.COMM_WORLD, 5, 5)
     f = VTXWriter(mesh.mpi_comm(), filename, io.mode.write, mesh)
     f.write(0.0)
@@ -57,18 +58,16 @@ def generate_mesh(dim: int, simplex: bool, N: int = 3):
 @pytest.mark.skipif(not has_adios2, reason="Requires ADIOS2.")
 @pytest.mark.parametrize("dim", [2, 3])
 @pytest.mark.parametrize("simplex", [True, False])
-def test_save_function(tempdir, dim, simplex):
+def test_fides_function_at_nodes(tempdir, dim, simplex):
+    "Test saving function values as mesh nodes"
     mesh = generate_mesh(dim, simplex)
     V = VectorFunctionSpace(mesh, ("Lagrange", 1))
     v = Function(V)
-
     Q = FunctionSpace(mesh, ("Lagrange", 1))
     q = Function(Q)
 
     filename = os.path.join(tempdir, "v.bp")
     f = FidesWriter(mesh.mpi_comm(), filename, io.mode.write, [v._cpp_object, q._cpp_object])
-    #  f  = FidesWriter(mesh.mpi_comm(), filename, io.mode.write, [q._cpp_object])
-
     for t in [0.1, 1]:
         q.interpolate(lambda x: t * (x[0] - 0.5)**2)
         mesh.geometry.x[:, :2] += 0.1
@@ -83,7 +82,8 @@ def test_save_function(tempdir, dim, simplex):
 @pytest.mark.skipif(not has_adios2, reason="Requires ADIOS2.")
 @pytest.mark.parametrize("dim", [2, 3])
 @pytest.mark.parametrize("simplex", [True, False])
-def test_save_function_vtx(tempdir, dim, simplex):
+def test_vtx_function_at_nodes(tempdir, dim, simplex):
+    "Test saving function values as mesh nodes"
     mesh = generate_mesh(dim, simplex)
     V = VectorFunctionSpace(mesh, ("Lagrange", 2))
     v = Function(V)
@@ -93,12 +93,6 @@ def test_save_function_vtx(tempdir, dim, simplex):
 
     filename = os.path.join(tempdir, "v.bp")
     f = VTXWriter(mesh.mpi_comm(), filename, io.mode.write, [v._cpp_object, q._cpp_object])
-    #  f = VTXWriter(mesh.mpi_comm(), filename, io.mode.write, [v._cpp_object])
-    # Q = FunctionSpace(mesh, ("DG", 1))
-    # q = Function(Q)
-    # q.x.array[0:10] = 1
-    #  f = VTXWriter(mesh.mpi_comm(), filename, io.mode.write, [q._cpp_object])
-
     for t in [0.1, 1, 2]:
         q.interpolate(lambda x: t * (x[0] - 0.5)**2)
         mesh.geometry.x[:, :2] += 0.1
@@ -107,5 +101,23 @@ def test_save_function_vtx(tempdir, dim, simplex):
         elif mesh.geometry.dim == 3:
             v.interpolate(lambda x: (t * x[2], x[0] + x[2] * 2j, x[1]))
         f.write(t)
+    f.close()
 
+
+@pytest.mark.skipif(not has_adios2, reason="Requires ADIOS2.")
+@pytest.mark.parametrize("dim", [2, 3])
+@pytest.mark.parametrize("simplex", [True, False])
+def test_vtx_lagrange_function(tempdir, dim, simplex):
+    "Test saving high order Lagrange functions"
+    mesh = generate_mesh(dim, simplex)
+    V = FunctionSpace(mesh, ("DG", 2))
+    v = Function(V)
+    filename = os.path.join(tempdir, "v.bp")
+    f = VTXWriter(mesh.mpi_comm(), filename, io.mode.write, [v._cpp_object])
+    v.interpolate(lambda x: x[0] + x[1])
+    for c in [0, 1]:
+        v.x.array[V.dofmap.cell_dofs(c)] = 0
+    for t in [0.1, 1]:
+        mesh.geometry.x[:, :2] += 0.1
+        f.write(t)
     f.close()
