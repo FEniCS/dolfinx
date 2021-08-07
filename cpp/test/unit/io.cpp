a330b1263aef264c2f7d8833b8d02b5a8d8be29d
Garth N. Wells <gnw20@cam.ac.uk>
2021-08-07 17:13:31 +0100

    Disable test parts

--- cpp/test/unit/io.cpp
+++ cpp/test/unit/io.cpp
@@ -20,9 +20,8 @@ void test_fides_mesh()
   auto mesh = std::make_shared<mesh::Mesh>(generation::RectangleMesh::create(
       MPI_COMM_WORLD, {{{0.0, 0.0, 0.0}, {1.0, 1.0, 0.0}}}, {22, 12},
       mesh::CellType::triangle, mesh::GhostMode::shared_facet));
-
-  io::FidesWriter writer(mesh->mpi_comm(), "test_mesh.bp", mesh);
-  writer.write(0.0);
+  // io::FidesWriter writer(mesh->mpi_comm(), "test_mesh.bp", mesh);
+  // writer.write(0.0);
 }
 
 } // namespace
