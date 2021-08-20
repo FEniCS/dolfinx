650ee5282cee9912025eba5eeaef6394a96221c6
Massimiliano Leoni <mleoni@bcamath.org>
2021-08-20 19:18:52 +0200

    Flake

--- python/dolfinx/io.py
+++ python/dolfinx/io.py
@@ -41,7 +41,7 @@ class VTKFile(cpp.io.VTKFile):
 
 class XDMFFile(cpp.io.XDMFFile):
     def write_function(self, u, t=0.0, mesh_xpath="/Xdmf/Domain/Grid[@GridType='Collection']/"
-                        "Grid[@GridType='Uniform'][1]"):
+                       "Grid[@GridType='Uniform'][1]"):
         u_cpp = getattr(u, "_cpp_object", u)
         super().write_function(u_cpp, t, mesh_xpath)
 
