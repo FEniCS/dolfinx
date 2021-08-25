40d9c775c480f97d8b90b5a415df3ffb5fdfbc21
Joe Dean <jpd62@cam.ac.uk>
2021-08-25 14:51:39 +0100

    Test 3D

--- python/test/unit/fem/test_mesh_view_spaces.py
+++ python/test/unit/fem/test_mesh_view_spaces.py
@@ -4,11 +4,9 @@ import dolfinx
 from mpi4py import MPI
 
 
-def test_mesh_view_cell_space():
-    mesh = dolfinx.UnitSquareMesh(MPI.COMM_WORLD, 4, 2)
-
-    dim = mesh.topology.dim
-    entities = dolfinx.mesh.locate_entities(mesh, dim, lambda x: x[0] <= 0.5)
+def check_dofmap(mesh, dim):
+    entities = dolfinx.mesh.locate_entities(mesh, dim,
+                                            lambda x: x[0] <= 0.5)
     mv_cpp = dolfinx.cpp.mesh.MeshView(mesh, dim, entities)
 
     V = dolfinx.FunctionSpace(mv_cpp, ("Lagrange", 1))
@@ -16,14 +14,10 @@ def test_mesh_view_cell_space():
     assert V.dofmap.list.num_nodes == len(entities)
 
 
-def test_mesh_view_facet_space():
-    mesh = dolfinx.UnitSquareMesh(MPI.COMM_WORLD, 4, 2)
-
-    dim = mesh.topology.dim - 1
-    entities = dolfinx.mesh.locate_entities_boundary(mesh, dim,
-                                                     lambda x: x[0] <= 0.5)
-    mv_cpp = dolfinx.cpp.mesh.MeshView(mesh, dim, entities)
-
-    V = dolfinx.FunctionSpace(mv_cpp, ("Lagrange", 1))
-
-    assert V.dofmap.list.num_nodes == len(entities)
+def test_mesh_view_spaces():
+    meshes = [dolfinx.UnitSquareMesh(MPI.COMM_WORLD, 4, 2),
+              dolfinx.UnitCubeMesh(MPI.COMM_WORLD, 4, 2, 2)]
+    for mesh in meshes:
+        for codim in range(0, 2):
+            dim = mesh.topology.dim - codim
+            check_dofmap(mesh, dim)
