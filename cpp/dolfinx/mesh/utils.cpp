26e586a5b4949736a271ccc37e8d4d8eb236634b
Chris Richardson <chris@bpi.cam.ac.uk>
2021-09-08 09:26:18 +0100

    Prism cell mesh (#1702)
    * Create prism BoxMesh
    * Update topologycomputation to take account of triangular and quadrilateral facets
    * Return reference cell topology as an AdjacencyList
    * Simple test
    Co-authored-by: Garth N. Wells <gnw20@cam.ac.uk>

--- cpp/dolfinx/mesh/utils.cpp
+++ cpp/dolfinx/mesh/utils.cpp
@@ -64,9 +64,12 @@ std::vector<double> mesh::h(const Mesh& mesh,
   if (dim != mesh.topology().dim())
     throw std::runtime_error("Cell size when dim ne tdim  requires updating.");
 
+  if (mesh.topology().cell_type() == mesh::CellType::prism and dim == 2)
+    throw std::runtime_error("More work needed for prism cell");
+
   // Get number of cell vertices
   const mesh::CellType type
-      = cell_entity_type(mesh.topology().cell_type(), dim);
+      = cell_entity_type(mesh.topology().cell_type(), dim, 0);
   const int num_vertices = num_cell_vertices(type);
 
   // Get geometry dofmap and dofs
@@ -106,9 +109,12 @@ xt::xtensor<double, 2>
 mesh::cell_normals(const mesh::Mesh& mesh, int dim,
                    const xtl::span<const std::int32_t>& entities)
 {
+  if (mesh.topology().cell_type() == mesh::CellType::prism and dim == 2)
+    throw std::runtime_error("More work needed for prism cell");
+
   const int gdim = mesh.geometry().dim();
   const mesh::CellType type
-      = mesh::cell_entity_type(mesh.topology().cell_type(), dim);
+      = mesh::cell_entity_type(mesh.topology().cell_type(), dim, 0);
 
   // Find geometry nodes for topology entities
   const xt::xtensor<double, 2>& xg = mesh.geometry().x();
@@ -390,9 +396,13 @@ mesh::entities_to_geometry(const mesh::Mesh& mesh, int dim,
                            const xtl::span<const std::int32_t>& entity_list,
                            bool orient)
 {
-  dolfinx::mesh::CellType cell_type = mesh.topology().cell_type();
+  mesh::CellType cell_type = mesh.topology().cell_type();
+
+  if (cell_type == mesh::CellType::prism and dim == 2)
+    throw std::runtime_error("More work needed for prism cells");
+
   const std::size_t num_entity_vertices
-      = mesh::num_cell_vertices(mesh::cell_entity_type(cell_type, dim));
+      = mesh::num_cell_vertices(mesh::cell_entity_type(cell_type, dim, 0));
   xt::xtensor<std::int32_t, 2> entity_geometry(
       {entity_list.size(), num_entity_vertices});
 
