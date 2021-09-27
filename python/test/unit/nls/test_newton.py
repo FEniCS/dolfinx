6177f7f60b58a3c1931edf8477223af407f050a8
Garth N. Wells <gnw20@cam.ac.uk>
2021-09-27 09:40:25 +0100

    Update Newton test

--- python/test/unit/nls/test_newton.py
+++ python/test/unit/nls/test_newton.py
@@ -32,7 +32,7 @@ class NonlinearPDEProblem:
         with b.localForm() as b_local:
             b_local.set(0.0)
         fem.assemble_vector(b, self.L)
-        dolfinx.fem.apply_lifting(b, [self.a], [[self.bc]], [x], -1.0)
+        dolfinx.fem.apply_lifting(b, [self.a], bcs=[[self.bc]], x0=[x], scale=-1.0)
         b.ghostUpdate(addv=PETSc.InsertMode.ADD, mode=PETSc.ScatterMode.REVERSE)
         dolfinx.fem.set_bc(b, [self.bc], x, -1.0)
 
@@ -69,7 +69,7 @@ class NonlinearPDE_SNESProblem:
         with F.localForm() as f_local:
             f_local.set(0.0)
         fem.assemble_vector(F, self.L)
-        fem.apply_lifting(F, [self.a], [[self.bc]], [x], -1.0)
+        fem.apply_lifting(F, [self.a], bcs=[[self.bc]], x0=[x], scale=-1.0)
         F.ghostUpdate(addv=PETSc.InsertMode.ADD, mode=PETSc.ScatterMode.REVERSE)
         fem.set_bc(F, [self.bc], x, -1.0)
 
