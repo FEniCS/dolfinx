7f3053c382d229e22e08d1cf40f3e508ad4a2ccc
Garth N. Wells <gnw20@cam.ac.uk>
2021-10-07 17:23:15 +0100

    Economise with tests

--- python/test/unit/io/test_adios2.py
+++ python/test/unit/io/test_adios2.py
@@ -25,9 +25,7 @@ assert (tempdir)
 def test_second_order_fides(tempdir):
     """Check that fides throws error on second order mesh"""
     filename = os.path.join(tempdir, "mesh_fides.bp")
-    points = np.array([[0, 0, 0],
-                       [1, 0, 0],
-                       [0.5, 0, 0]], dtype=np.float64)
+    points = np.array([[0, 0, 0], [1, 0, 0], [0.5, 0, 0]], dtype=np.float64)
     cells = np.array([[0, 1, 2]], dtype=np.int32)
     cell = ufl.Cell("interval", geometric_dimension=points.shape[1])
     domain = ufl.Mesh(ufl.VectorElement("Lagrange", cell, 2))
@@ -43,8 +41,8 @@ def test_functions_from_different_meshes_fides(tempdir):
     filename = os.path.join(tempdir, "mesh_fides.bp")
     mesh0 = UnitSquareMesh(MPI.COMM_WORLD, 5, 5)
     mesh1 = UnitSquareMesh(MPI.COMM_WORLD, 10, 2)
-    V0, V1 = FunctionSpace(mesh0, ("Lagrange", 1)), FunctionSpace(mesh1, ("Lagrange", 1))
-    u0, u1 = Function(V0), Function(V1)
+    u0 = Function(FunctionSpace(mesh0, ("Lagrange", 1)))
+    u1 = Function(FunctionSpace(mesh1, ("Lagrange", 1)))
     with pytest.raises(RuntimeError):
         FidesWriter(mesh0.mpi_comm(), filename, [u0._cpp_object, u1._cpp_object])
 
@@ -72,11 +70,10 @@ def test_fides_mesh(tempdir, dim, simplex):
     """ Test writing of a single Fides mesh with changing geometry"""
     filename = os.path.join(tempdir, "mesh_fides.bp")
     mesh = generate_mesh(dim, simplex)
-    f = FidesWriter(mesh.mpi_comm(), filename, mesh)
-    f.write(0.0)
-    mesh.geometry.x[:, 1] += 0.1
-    f.write(0.1)
-    f.close()
+    with FidesWriter(mesh.mpi_comm(), filename, mesh) as f:
+        f.write(0.0)
+        mesh.geometry.x[:, 1] += 0.1
+        f.write(0.1)
 
 
 @pytest.mark.skipif(not has_adios2, reason="Requires ADIOS2.")
@@ -85,10 +82,8 @@ def test_fides_mesh(tempdir, dim, simplex):
 def test_mixed_fides_functions(tempdir, dim, simplex):
     """Test saving P2 and P1 functions with Fides"""
     mesh = generate_mesh(dim, simplex)
-    V = VectorFunctionSpace(mesh, ("Lagrange", 2))
-    v = Function(V)
-    Q = FunctionSpace(mesh, ("Lagrange", 1))
-    q = Function(Q)
+    v = Function(VectorFunctionSpace(mesh, ("Lagrange", 2)))
+    q = Function(FunctionSpace(mesh, ("Lagrange", 1)))
     filename = os.path.join(tempdir, "v.bp")
     with pytest.raises(RuntimeError):
         FidesWriter(mesh.mpi_comm(), filename, [v._cpp_object, q._cpp_object])
@@ -100,23 +95,20 @@ def test_mixed_fides_functions(tempdir, dim, simplex):
 def test_two_fides_functions(tempdir, dim, simplex):
     """Test saving two functions with Fides"""
     mesh = generate_mesh(dim, simplex)
-    V = VectorFunctionSpace(mesh, ("Lagrange", 1))
-    v = Function(V)
-    Q = FunctionSpace(mesh, ("Lagrange", 1))
-    q = Function(Q)
-
+    v = Function(VectorFunctionSpace(mesh, ("Lagrange", 1)))
+    q = Function(FunctionSpace(mesh, ("Lagrange", 1)))
     filename = os.path.join(tempdir, "v.bp")
-    f = FidesWriter(mesh.mpi_comm(), filename, [v._cpp_object, q._cpp_object])
-    f.write(0)
+    with FidesWriter(mesh.mpi_comm(), filename, [v._cpp_object, q._cpp_object]) as f:
+        f.write(0)
 
-    def vel(x):
-        values = np.zeros((dim, x.shape[1]))
-        values[0] = x[1]
-        values[1] = x[0]
-        return values
-    v.interpolate(vel)
-    q.interpolate(lambda x: x[0])
-    f.write(1)
+        def vel(x):
+            values = np.zeros((dim, x.shape[1]))
+            values[0] = x[1]
+            values[1] = x[0]
+            return values
+        v.interpolate(vel)
+        q.interpolate(lambda x: x[0])
+        f.write(1)
 
 
 @pytest.mark.skipif(not has_adios2, reason="Requires ADIOS2.")
@@ -125,45 +117,35 @@ def test_two_fides_functions(tempdir, dim, simplex):
 def test_fides_function_at_nodes(tempdir, dim, simplex):
     """Test saving P1 functions with Fides (with changing geometry)"""
     mesh = generate_mesh(dim, simplex)
-    V = VectorFunctionSpace(mesh, ("Lagrange", 1))
-    v = Function(V)
-    Q = FunctionSpace(mesh, ("Lagrange", 1))
-    q = Function(Q)
+    v = Function(VectorFunctionSpace(mesh, ("Lagrange", 1)))
+    q = Function(FunctionSpace(mesh, ("Lagrange", 1)))
 
     filename = os.path.join(tempdir, "v.bp")
-    f = FidesWriter(mesh.mpi_comm(), filename, [v._cpp_object, q._cpp_object])
-    for t in [0.1, 0.5, 1]:
-        # Only change one function
-        q.interpolate(lambda x: t * (x[0] - 0.5)**2)
-        f.write(t)
+    with FidesWriter(mesh.mpi_comm(), filename, [v._cpp_object, q._cpp_object]) as f:
+        for t in [0.1, 0.5, 1]:
+            # Only change one function
+            q.interpolate(lambda x: t * (x[0] - 0.5)**2)
+            f.write(t)
 
-        mesh.geometry.x[:, :2] += 0.1
-        if mesh.geometry.dim == 2:
-            v.interpolate(lambda x: (t * x[0], x[1] + x[1] * 1j))
-        elif mesh.geometry.dim == 3:
-            v.interpolate(lambda x: (t * x[2], x[0] + x[2] * 2j, x[1]))
-        f.write(t)
-    f.close()
+            mesh.geometry.x[:, :2] += 0.1
+            if mesh.geometry.dim == 2:
+                v.interpolate(lambda x: (t * x[0], x[1] + x[1] * 1j))
+            elif mesh.geometry.dim == 3:
+                v.interpolate(lambda x: (t * x[2], x[0] + x[2] * 2j, x[1]))
+            f.write(t)
 
 
-@pytest.mark.skipif(
-    MPI.COMM_WORLD.size > 1,
-    reason="This test should only be run in serial.")
+@pytest.mark.skipif(MPI.COMM_WORLD.size > 1, reason="This test should only be run in serial.")
 @pytest.mark.skipif(not has_adios2, reason="Requires ADIOS2.")
 def test_second_order_vtx(tempdir):
     filename = os.path.join(tempdir, "mesh_fides.bp")
-
-    points = np.array([[0, 0, 0],
-                       [1, 0, 0],
-                       [0.5, 0, 0]], dtype=np.float64)
+    points = np.array([[0, 0, 0], [1, 0, 0], [0.5, 0, 0]], dtype=np.float64)
     cells = np.array([[0, 1, 2]], dtype=np.int32)
     cell = ufl.Cell("interval", geometric_dimension=points.shape[1])
     domain = ufl.Mesh(ufl.VectorElement("Lagrange", cell, 2))
     mesh = create_mesh(MPI.COMM_WORLD, cells, points, domain)
-
-    f = VTXWriter(mesh.mpi_comm(), filename, mesh)
-    f.write(0.0)
-    f.close()
+    with VTXWriter(mesh.mpi_comm(), filename, mesh) as f:
+        f.write(0.0)
 
 
 @pytest.mark.skipif(not has_adios2, reason="Requires ADIOS2.")
@@ -172,11 +154,10 @@ def test_second_order_vtx(tempdir):
 def test_vtx_mesh(tempdir, dim, simplex):
     filename = os.path.join(tempdir, "mesh_vtx.bp")
     mesh = generate_mesh(dim, simplex)
-    f = VTXWriter(mesh.mpi_comm(), filename, mesh)
-    f.write(0.0)
-    mesh.geometry.x[:, 1] += 0.1
-    f.write(0.1)
-    f.close()
+    with VTXWriter(mesh.mpi_comm(), filename, mesh) as f:
+        f.write(0.0)
+        mesh.geometry.x[:, 1] += 0.1
+        f.write(0.1)
 
 
 @pytest.mark.skipif(not has_adios2, reason="Requires ADIOS2.")
@@ -187,7 +168,6 @@ def test_vtx_functions_fail(tempdir, dim, simplex):
     mesh = generate_mesh(dim, simplex)
     v = Function(VectorFunctionSpace(mesh, ("Lagrange", 2)))
     w = Function(FunctionSpace(mesh, ("Lagrange", 1)))
-
     filename = os.path.join(tempdir, "v.bp")
     with pytest.raises(RuntimeError):
         VTXWriter(mesh.mpi_comm(), filename, [v._cpp_object, w._cpp_object])
@@ -243,4 +223,5 @@ def test_vtx_functions(tempdir, dim, simplex):
     for t in [0.1, 1]:
         mesh.geometry.x[:, :2] += 0.1
         f.write(t)
+
     f.close()
