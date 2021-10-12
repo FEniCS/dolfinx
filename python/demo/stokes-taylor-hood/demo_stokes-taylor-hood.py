bec5bbbb42f539db903188caa161272af2763f58
Garth N. Wells <gnw20@cam.ac.uk>
2021-10-12 14:48:23 +0100

    More flake8 fixes

--- python/demo/stokes-taylor-hood/demo_stokes-taylor-hood.py
+++ python/demo/stokes-taylor-hood/demo_stokes-taylor-hood.py
@@ -194,7 +194,8 @@ for b_sub in b.getNestSubVecs():
 # Set Dirichlet boundary condition values in the RHS
 # bcs0 = dolfinx.cpp.fem.bcs_rows(dolfinx.fem.assemble._create_cpp_form(L), bcs)
 # tmp = dolfinx.fem.form.extract_function_spaces(dolfinx.fem.assemble._create_cpp_form(L))
-bcs0 = dolfinx.fem.dirichletbc.bcs_by_block(dolfinx.fem.form.extract_function_spaces(dolfinx.fem.assemble._create_cpp_form(L)), bcs)
+bcs0 = dolfinx.fem.dirichletbc.bcs_by_block(
+    dolfinx.fem.form.extract_function_spaces(dolfinx.fem.assemble._create_cpp_form(L)), bcs)
 # exit(0)
 # dolfinx.fem.form.extract_function_spaces(dolfinx.fem.assemble._create_cpp_form([a[0][0], a[0][1]]))
 dolfinx.fem.assemble.set_bc_nest(b, bcs0)
