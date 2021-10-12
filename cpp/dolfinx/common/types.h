6de0d7f049a8bfec567fed51831227f019d58e22
Chris Richardson <chris@bpi.cam.ac.uk>
2021-10-12 14:06:11 +0100

    Try new ufc.h

--- cpp/dolfinx/common/types.h
+++ cpp/dolfinx/common/types.h
@@ -9,9 +9,9 @@
 #include <petscsys.h>
 
 // Typedefs for ufc_scalar
-#ifdef PETSC_USE_COMPLEX
-#include <complex>
-using ufc_scalar_t = std::complex<double>;
-#else
-using ufc_scalar_t = double;
-#endif
+// #ifdef PETSC_USE_COMPLEX
+// #include <complex>
+// using ufc_scalar_t = std::complex<double>;
+// #else
+// using ufc_scalar_t = double;
+// #endif
