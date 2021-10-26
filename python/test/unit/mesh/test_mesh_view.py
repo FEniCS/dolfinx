c1be3e0413c790e9f0cfff1c34357ff0978684cc
Joe Dean <jpd62@cam.ac.uk>
2021-10-26 17:53:34 +0100

    Test topology

--- python/test/unit/mesh/test_mesh_view.py
+++ python/test/unit/mesh/test_mesh_view.py
@@ -14,14 +14,36 @@ def boundary_0(x):
 
 
 def boundary_1(x):
-    return np.isclose(x[0], 1.0)
+    return np.logical_or(np.isclose(x[0], 1.0),
+                         np.isclose(x[1], 1.0))
 
 
 n = 1
 mesh = dolfinx.UnitSquareMesh(MPI.COMM_WORLD, n, n)
 entity_dim = mesh.topology.dim - 1
-entities = dolfinx.mesh.locate_entities_boundary(mesh, entity_dim, boundary_0)
-print(entities)
+entities = dolfinx.mesh.locate_entities_boundary(mesh, entity_dim, boundary_1)
 
 submesh = mesh.sub(entity_dim, entities)
-print(submesh)
+
+# Check topology
+print(f"entities = {entities}")
+# The vertex map that mesh.sub uses is a sorted list of unique vertices, so
+# recreate this here. TODO Could return this from mesh.sub.
+mesh.topology.create_connectivity(entity_dim, 0)
+mesh_e_to_v = mesh.topology.connectivity(entity_dim, 0)
+vertex_map = []
+for entity in entities:
+    vertices = mesh_e_to_v.links(entity)
+    vertex_map.append(vertices)
+vertex_map = np.hstack(vertex_map)
+vertex_map = np.unique(vertex_map)
+
+submesh.topology.create_connectivity(entity_dim, 0)
+submesh_e_to_v = submesh.topology.connectivity(entity_dim, 0)
+for submesh_entity in range(len(entities)):
+    submesh_entity_vertices = submesh_e_to_v.links(submesh_entity)
+    mesh_entity = entities[submesh_entity]
+    mesh_entity_vertices = mesh_e_to_v.links(mesh_entity)
+    
+    for i in range(len(submesh_entity_vertices)):
+        assert(vertex_map[submesh_entity_vertices[i]] == mesh_entity_vertices[i])
