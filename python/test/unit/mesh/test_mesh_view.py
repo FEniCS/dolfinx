3a14084bb05432ad8830426ae940d06fe89c9560
Joe Dean <jpd62@cam.ac.uk>
2021-10-27 10:51:29 +0100

    Create pytest test

--- python/test/unit/mesh/test_mesh_view.py
+++ python/test/unit/mesh/test_mesh_view.py
@@ -4,6 +4,7 @@ import dolfinx
 from mpi4py import MPI
 import numpy as np
 from dolfinx.cpp.mesh import entities_to_geometry
+import pytest
 
 
 def boundary_0(x):
@@ -19,46 +20,60 @@ def boundary_1(x):
                          np.isclose(x[1], 1.0))
 
 
-n = 1
-mesh = dolfinx.UnitSquareMesh(MPI.COMM_WORLD, n, n)
-entity_dim = mesh.topology.dim - 1
-entities = dolfinx.mesh.locate_entities_boundary(mesh, entity_dim, boundary_1)
+boundaries = [boundary_0, boundary_1]
+ns = [1, 2, 3]
 
-submesh = mesh.sub(entity_dim, entities)
 
-# Check topology
-print(f"entities = {entities}")
-# The vertex map that mesh.sub uses is a sorted list of unique vertices, so
-# recreate this here. TODO Could return this from mesh.sub or save as a property
-# etc.
-mesh.topology.create_connectivity(entity_dim, 0)
-mesh_e_to_v = mesh.topology.connectivity(entity_dim, 0)
-vertex_map = []
-for entity in entities:
-    vertices = mesh_e_to_v.links(entity)
-    vertex_map.append(vertices)
-vertex_map = np.hstack(vertex_map)
-vertex_map = np.unique(vertex_map)
+@pytest.mark.parametrize("boundary", boundaries)
+@pytest.mark.parametrize("n", ns)
+def test_topology(n, boundary):
+    mesh = dolfinx.UnitSquareMesh(MPI.COMM_WORLD, n, n)
+    entity_dim = mesh.topology.dim - 1
+    entities = dolfinx.mesh.locate_entities_boundary(mesh, entity_dim, boundary_1)
 
-submesh.topology.create_connectivity(entity_dim, 0)
-submesh_e_to_v = submesh.topology.connectivity(entity_dim, 0)
-for submesh_entity in range(len(entities)):
-    submesh_entity_vertices = submesh_e_to_v.links(submesh_entity)
-    mesh_entity = entities[submesh_entity]
-    mesh_entity_vertices = mesh_e_to_v.links(mesh_entity)
+    submesh = mesh.sub(entity_dim, entities)
+
+    # The vertex map that mesh.sub uses is a sorted list of unique vertices, so
+    # recreate this here. TODO Could return this from mesh.sub or save as a property
+    # etc.
+    mesh.topology.create_connectivity(entity_dim, 0)
+    mesh_e_to_v = mesh.topology.connectivity(entity_dim, 0)
+    vertex_map = []
+    for entity in entities:
+        vertices = mesh_e_to_v.links(entity)
+        vertex_map.append(vertices)
+    vertex_map = np.hstack(vertex_map)
+    vertex_map = np.unique(vertex_map)
+
+    submesh.topology.create_connectivity(entity_dim, 0)
+    submesh_e_to_v = submesh.topology.connectivity(entity_dim, 0)
+    for submesh_entity in range(len(entities)):
+        submesh_entity_vertices = submesh_e_to_v.links(submesh_entity)
+        mesh_entity = entities[submesh_entity]
+        mesh_entity_vertices = mesh_e_to_v.links(mesh_entity)
+        
+        for i in range(len(submesh_entity_vertices)):
+            assert(vertex_map[submesh_entity_vertices[i]] == mesh_entity_vertices[i])
+
+
+@pytest.mark.parametrize("boundary", boundaries)
+@pytest.mark.parametrize("n", ns)
+def test_geometry(n, boundary):
+    mesh = dolfinx.UnitSquareMesh(MPI.COMM_WORLD, n, n)
+    entity_dim = mesh.topology.dim - 1
+    entities = dolfinx.mesh.locate_entities_boundary(mesh, entity_dim, boundary_1)
+
+    submesh = mesh.sub(entity_dim, entities)
     
-    for i in range(len(submesh_entity_vertices)):
-        assert(vertex_map[submesh_entity_vertices[i]] == mesh_entity_vertices[i])
+    e_to_g = entities_to_geometry(mesh, entity_dim, entities, False)
 
-e_to_g = entities_to_geometry(mesh, entity_dim, entities, False)
+    for submesh_entity in range(len(entities)):
+        submesh_x_dofs = submesh.geometry.dofmap.links(submesh_entity)
 
-for submesh_entity in range(len(entities)):
-    submesh_x_dofs = submesh.geometry.dofmap.links(submesh_entity)
+        # e_to_g[i] gets the mesh x_dofs of entities[i], which should
+        # correspond to the submesh x_dofs of submesh cell i
+        mesh_x_dofs = e_to_g[submesh_entity]
 
-    # e_to_g[i] gets the mesh x_dofs of entities[i], which should
-    # correspond to the submesh x_dofs of submesh cell i
-    mesh_x_dofs = e_to_g[submesh_entity]
-
-    for i in range(len(submesh_x_dofs)):
-        assert(np.allclose(mesh.geometry.x[mesh_x_dofs[i]],
-                          submesh.geometry.x[submesh_x_dofs[i]]))
+        for i in range(len(submesh_x_dofs)):
+            assert(np.allclose(mesh.geometry.x[mesh_x_dofs[i]],
+                            submesh.geometry.x[submesh_x_dofs[i]]))
