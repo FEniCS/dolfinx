81c0637e45115252f67a84f9322a427e0e3401ef
Garth N. Wells <gnw20@cam.ac.uk>
2021-11-11 13:59:09 +0000

    Simplifications

--- python/test/unit/fem/test_dofmap.py
+++ python/test/unit/fem/test_dofmap.py
@@ -13,8 +13,7 @@ import pytest
 import ufl
 from dolfinx import (FunctionSpace, UnitCubeMesh, UnitIntervalMesh,
                      UnitSquareMesh, VectorFunctionSpace, cpp)
-from dolfinx.cpp.mesh import CellType
-from dolfinx.mesh import create_mesh
+from dolfinx.mesh import CellType, create_mesh
 from dolfinx_utils.test.skips import skip_in_parallel
 from mpi4py import MPI
 from ufl import FiniteElement, MixedElement, VectorElement
