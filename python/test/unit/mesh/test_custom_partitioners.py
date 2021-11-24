786e8c7ec686a5b1baa739196b6dc9139033dc4e
Garth N. Wells <gnw20@cam.ac.uk>
2021-11-24 16:27:46 +0000

    Test updates

--- python/test/unit/mesh/test_custom_partitioners.py
+++ python/test/unit/mesh/test_custom_partitioners.py
@@ -37,15 +37,14 @@ def test_partition_box_mesh(partitioner, Nx, cell_type):
 @pytest.mark.parametrize("Nx", [3, 10, 13])
 @pytest.mark.parametrize("cell_type", [CellType.tetrahedron, CellType.hexahedron])
 def test_custom_partitioner(tempdir, Nx, cell_type):
-    mpi_comm = MPI.COMM_WORLD
+    comm = MPI.COMM_WORLD
 
-    Lx = mpi_comm.size
+    Lx = comm.size
     points = [np.array([0, 0, 0]), np.array([Lx, Lx, Lx])]
-    mesh = dolfinx.BoxMesh(
-        mpi_comm, points, [Nx, Nx, Nx], cell_type, GhostMode.shared_facet)
+    mesh = dolfinx.BoxMesh(comm, points, [Nx, Nx, Nx], cell_type, GhostMode.shared_facet)
 
     filename = os.path.join(tempdir, "u1_.xdmf")
-    with XDMFFile(mpi_comm, filename, "w") as file:
+    with XDMFFile(comm, filename, "w") as file:
         file.write_mesh(mesh)
 
     # Read all geometry data on all processes
@@ -56,15 +55,15 @@ def test_custom_partitioner(tempdir, Nx, cell_type):
     with XDMFFile(MPI.COMM_WORLD, filename, "r") as file:
         cell_shape, cell_degree = file.read_cell_type()
         x = file.read_geometry_data()
-        topo = file.read_topology_data()
+        topology = file.read_topology_data()
 
     num_local_coor = x.shape[0]
-    all_sizes = mpi_comm.allgather(num_local_coor)
+    all_sizes = comm.allgather(num_local_coor)
     all_sizes.insert(0, 0)
     all_ranges = np.cumsum(all_sizes)
 
     # Testing the premise: coordinates are read contiguously in chunks
-    rank = mpi_comm.rank
+    rank = comm.rank
     assert (np.all(x_global[all_ranges[rank]:all_ranges[rank + 1]] == x))
 
     cell = ufl.Cell(cell_shape.name)
@@ -73,17 +72,16 @@ def test_custom_partitioner(tempdir, Nx, cell_type):
     # Partition mesh in layers, capture geometrical data and topological
     # data from outer scope
     def partitioner(*args):
-        midpoints = np.mean(x_global[topo], axis=1)
-        dest = np.floor(midpoints[:, 0] % mpi_comm.size).astype(np.int32)
+        midpoints = np.mean(x_global[topology], axis=1)
+        dest = np.floor(midpoints[:, 0] % comm.size).astype(np.int32)
         return dolfinx.cpp.graph.AdjacencyList_int32(dest)
 
-    ghost_mode = GhostMode.none
-    new_mesh = dolfinx.mesh.create_mesh(mpi_comm, topo, x, domain, ghost_mode, partitioner)
+    new_mesh = dolfinx.mesh.create_mesh(comm, topology, x, domain, GhostMode.none, partitioner)
 
     tdim = new_mesh.topology.dim
     assert mesh.topology.index_map(tdim).size_global == new_mesh.topology.index_map(tdim).size_global
     num_cells = new_mesh.topology.index_map(tdim).size_local
     cell_midpoints = dolfinx.cpp.mesh.midpoints(new_mesh, tdim, range(num_cells))
     assert num_cells > 0
-    assert np.all(cell_midpoints[:, 0] >= mpi_comm.rank)
-    assert np.all(cell_midpoints[:, 0] <= mpi_comm.rank + 1)
+    assert np.all(cell_midpoints[:, 0] >= comm.rank)
+    assert np.all(cell_midpoints[:, 0] <= comm.rank + 1)
--- python/test/unit/mesh/test_meshtags.py
+++ python/test/unit/mesh/test_meshtags.py
@@ -1,24 +1,23 @@
-import numpy
+# Copyright (C) 2019-2021 Michel Habera
+#
+# This file is part of DOLFINx (https://www.fenicsproject.org)
+#
+# SPDX-License-Identifier:    LGPL-3.0-or-later
+
+
+import numpy as np
 import pytest
-from dolfinx import cpp
 from dolfinx.generation import UnitCubeMesh
 from dolfinx.mesh import CellType, create_meshtags, locate_entities
 from mpi4py import MPI
 
-celltypes_3D = [CellType.tetrahedron, CellType.hexahedron]
 
-
-@pytest.mark.parametrize("cell_type", celltypes_3D)
+@pytest.mark.parametrize("cell_type", [CellType.tetrahedron, CellType.hexahedron])
 def test_create(cell_type):
-    comm = MPI.COMM_WORLD
-
-    mesh = UnitCubeMesh(comm, 6, 6, 6, cell_type)
-
-    marked_lines = locate_entities(mesh, 1, lambda x: numpy.isclose(x[1], 0.5))
+    mesh = UnitCubeMesh(MPI.COMM_WORLD, 6, 6, 6, cell_type)
+    marked_lines = locate_entities(mesh, 1, lambda x: np.isclose(x[1], 0.5))
     f_v = mesh.topology.connectivity(1, 0).array.reshape(-1, 2)
-
-    entities = cpp.graph.AdjacencyList_int32(f_v[marked_lines])
-    values = numpy.full(marked_lines.shape[0], 2, dtype=numpy.int32)
-
+    entities = f_v[marked_lines]
+    values = np.full(marked_lines.shape[0], 2, dtype=np.int32)
     mt = create_meshtags(mesh, 1, entities, values)
     assert mt.indices.shape == marked_lines.shape
