63ce14549786f387373ccc08c3ef87ceb5b62923
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2021-11-25 12:01:23 +0000

    Add functions to create and get full cell permutations

--- cpp/dolfinx/mesh/Topology.h
+++ cpp/dolfinx/mesh/Topology.h
@@ -102,6 +102,9 @@ public:
   /// Returns the permutation information
   const std::vector<std::uint32_t>& get_cell_permutation_info() const;
 
+  /// Returns the permutation information
+  const std::vector<std::uint8_t>& get_full_cell_permutation_info() const;
+
   /// Get the permutation number to apply to a facet. The permutations
   /// are numbered so that:
   ///
@@ -137,6 +140,9 @@ public:
   /// Compute entity permutations and reflections
   void create_entity_permutations();
 
+  /// Compute cell permutations and reflections
+  void create_full_cell_permutations();
+
   /// Mesh MPI communicator
   /// @return The communicator on which the topology is distributed
   MPI_Comm mpi_comm() const;
@@ -159,6 +165,9 @@ private:
   // [cell0_0, cell0_1, ,cell0_2, cell1_0, cell1_1, ,cell1_2, ...,
   // celln_0, celln_1, ,celln_2,]
   std::vector<std::uint8_t> _facet_permutations;
+
+  // @todo: Get Matthew to document
+  std::vector<std::uint8_t> _full_cell_permutations;
 
   // Cell permutation info. See the documentation for
   // get_cell_permutation_info for documentation of how this is encoded.
