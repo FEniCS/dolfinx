4667bb4a82870e9112bae29f930dae44524b0a61
Jørgen S. Dokken <dokken92@gmail.com>
2021-11-26 22:22:42 +0000

    Fix bc test

--- python/test/unit/fem/test_bcs.py
+++ python/test/unit/fem/test_bcs.py
@@ -7,10 +7,13 @@
 import numpy as np
 import pytest
 import ufl
-from dolfinx.fem import (DirichletBC, Function, FunctionSpace, apply_lifting,
+from dolfinx.fem import (Constant, DirichletBC, Function, FunctionSpace,
+                         VectorFunctionSpace, apply_lifting,
                          assemble_matrix, assemble_vector, create_matrix,
-                         create_vector, locate_dofs_geometrical, set_bc)
+                         create_vector, locate_dofs_geometrical,
+                         locate_dofs_topological, set_bc)
 from dolfinx.generation import UnitSquareMesh
+from dolfinx.mesh import locate_entities_boundary
 from mpi4py import MPI
 from petsc4py import PETSc
 from ufl import dx, inner
@@ -105,28 +108,28 @@ def test_constant_bc():
     Test that setting a DirichletBC with a constant yields the same result as setting it with a function.
     """
 
-    mesh = dolfinx.UnitSquareMesh(MPI.COMM_WORLD, 10, 10)
-    V = dolfinx.FunctionSpace(mesh, ("CG", 1))
-    c = dolfinx.Constant(mesh, PETSc.ScalarType(2))
+    mesh = UnitSquareMesh(MPI.COMM_WORLD, 10, 10)
+    V = FunctionSpace(mesh, ("CG", 1))
+    c = Constant(mesh, PETSc.ScalarType(2))
 
     def on_boundary(x):
         return np.ones(x.shape[1], dtype=bool)
 
     tdim = mesh.topology.dim
-    boundary_facets = dolfinx.mesh.locate_entities_boundary(mesh, tdim - 1, on_boundary)
-    boundary_dofs = dolfinx.fem.locate_dofs_topological(V, tdim - 1, boundary_facets)
+    boundary_facets = locate_entities_boundary(mesh, tdim - 1, on_boundary)
+    boundary_dofs = locate_dofs_topological(V, tdim - 1, boundary_facets)
 
-    u_bc = dolfinx.Function(V)
+    u_bc = Function(V)
     u_bc.x.array[:] = PETSc.ScalarType(c.value)
 
-    bc_f = dolfinx.DirichletBC(u_bc, boundary_dofs)
-    bc_c = dolfinx.DirichletBC(c, boundary_dofs, V)
+    bc_f = DirichletBC(u_bc, boundary_dofs)
+    bc_c = DirichletBC(c, boundary_dofs, V)
 
-    u_f = dolfinx.Function(V)
-    dolfinx.fem.set_bc(u_f.vector, [bc_f])
+    u_f = Function(V)
+    set_bc(u_f.vector, [bc_f])
 
-    u_c = dolfinx.Function(V)
-    dolfinx.fem.set_bc(u_c.vector, [bc_c])
+    u_c = Function(V)
+    set_bc(u_c.vector, [bc_c])
     assert(np.allclose(u_f.vector.array, u_c.vector.array))
 
 
@@ -135,35 +138,35 @@ def test_vector_constant_bc():
     Test that setting a DirichletBC with a vector valued constant yields the same result as setting it with a function.
     """
 
-    mesh = dolfinx.UnitSquareMesh(MPI.COMM_WORLD, 10, 10)
-    V = dolfinx.VectorFunctionSpace(mesh, ("CG", 1))
-    c = dolfinx.Constant(mesh, PETSc.ScalarType((2, 1)))
+    mesh = UnitSquareMesh(MPI.COMM_WORLD, 10, 10)
+    V = VectorFunctionSpace(mesh, ("CG", 1))
+    c = Constant(mesh, PETSc.ScalarType((2, 1)))
 
     def on_boundary(x):
         return np.ones(x.shape[1], dtype=bool)
 
     tdim = mesh.topology.dim
     Vs = [V.sub(i).collapse() for i in range(V.num_sub_spaces())]
-    boundary_facets = dolfinx.mesh.locate_entities_boundary(mesh, tdim - 1, on_boundary)
-    boundary_dofs0 = dolfinx.fem.locate_dofs_topological((V.sub(0), Vs[0]), tdim - 1, boundary_facets)
-    boundary_dofs1 = dolfinx.fem.locate_dofs_topological((V.sub(1), Vs[1]), tdim - 1, boundary_facets)
+    boundary_facets = locate_entities_boundary(mesh, tdim - 1, on_boundary)
+    boundary_dofs0 = locate_dofs_topological((V.sub(0), Vs[0]), tdim - 1, boundary_facets)
+    boundary_dofs1 = locate_dofs_topological((V.sub(1), Vs[1]), tdim - 1, boundary_facets)
 
-    u_bc0 = dolfinx.Function(Vs[0])
+    u_bc0 = Function(Vs[0])
     u_bc0.x.array[:] = PETSc.ScalarType(c.value[0])
-    u_bc1 = dolfinx.Function(Vs[1])
+    u_bc1 = Function(Vs[1])
     u_bc1.x.array[:] = PETSc.ScalarType(c.value[1])
 
-    bc_f0 = dolfinx.DirichletBC(u_bc0, boundary_dofs0, V.sub(0))
-    bc_f1 = dolfinx.DirichletBC(u_bc1, boundary_dofs1, V.sub(1))
+    bc_f0 = DirichletBC(u_bc0, boundary_dofs0, V.sub(0))
+    bc_f1 = DirichletBC(u_bc1, boundary_dofs1, V.sub(1))
 
-    boundary_dofs = dolfinx.fem.locate_dofs_topological(V, tdim - 1, boundary_facets)
-    bc_c = dolfinx.DirichletBC(c, boundary_dofs, V)
+    boundary_dofs = locate_dofs_topological(V, tdim - 1, boundary_facets)
+    bc_c = DirichletBC(c, boundary_dofs, V)
 
-    u_f = dolfinx.Function(V)
-    dolfinx.fem.set_bc(u_f.vector, [bc_f0, bc_f1])
+    u_f = Function(V)
+    set_bc(u_f.vector, [bc_f0, bc_f1])
 
-    u_c = dolfinx.Function(V)
-    dolfinx.fem.set_bc(u_c.vector, [bc_c])
+    u_c = Function(V)
+    set_bc(u_c.vector, [bc_c])
     assert(np.allclose(u_f.vector.array, u_c.vector.array))
 
 
@@ -173,31 +176,31 @@ def test_sub_constant_bc():
     yields the same result as setting it with a function.
     """
 
-    mesh = dolfinx.UnitSquareMesh(MPI.COMM_WORLD, 10, 10)
-    V = dolfinx.VectorFunctionSpace(mesh, ("CG", 1))
-    c = dolfinx.Constant(mesh, PETSc.ScalarType(3.14))
+    mesh = UnitSquareMesh(MPI.COMM_WORLD, 10, 10)
+    V = VectorFunctionSpace(mesh, ("CG", 1))
+    c = Constant(mesh, PETSc.ScalarType(3.14))
 
     def on_boundary(x):
         return np.ones(x.shape[1], dtype=bool)
 
     tdim = mesh.topology.dim
-    boundary_facets = dolfinx.mesh.locate_entities_boundary(mesh, tdim - 1, on_boundary)
+    boundary_facets = locate_entities_boundary(mesh, tdim - 1, on_boundary)
 
     for i in range(V.num_sub_spaces()):
         Vi = V.sub(i).collapse()
-        boundary_dofsi = dolfinx.fem.locate_dofs_topological((V.sub(i), Vi), tdim - 1, boundary_facets)
+        boundary_dofsi = locate_dofs_topological((V.sub(i), Vi), tdim - 1, boundary_facets)
 
-        u_bci = dolfinx.Function(Vi)
+        u_bci = Function(Vi)
         u_bci.x.array[:] = PETSc.ScalarType(c.value)
 
-        bc_fi = dolfinx.DirichletBC(u_bci, boundary_dofsi, V.sub(i))
+        bc_fi = DirichletBC(u_bci, boundary_dofsi, V.sub(i))
 
-        boundary_dofs = dolfinx.fem.locate_dofs_topological(V.sub(i), tdim - 1, boundary_facets)
-        bc_c = dolfinx.DirichletBC(c, boundary_dofs, V.sub(i))
+        boundary_dofs = locate_dofs_topological(V.sub(i), tdim - 1, boundary_facets)
+        bc_c = DirichletBC(c, boundary_dofs, V.sub(i))
 
-        u_f = dolfinx.Function(V)
-        dolfinx.fem.set_bc(u_f.vector, [bc_fi])
+        u_f = Function(V)
+        set_bc(u_f.vector, [bc_fi])
 
-        u_c = dolfinx.Function(V)
-        dolfinx.fem.set_bc(u_c.vector, [bc_c])
+        u_c = Function(V)
+        set_bc(u_c.vector, [bc_c])
         assert(np.allclose(u_f.vector.array, u_c.vector.array))
