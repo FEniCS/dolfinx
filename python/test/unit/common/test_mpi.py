9a43854825afb55f42f5e8788ade73600af574a0
Garth N. Wells <gnw20@cam.ac.uk>
2021-11-26 17:13:40 +0000

    Revert file

--- python/test/unit/common/test_mpi.py
+++ python/test/unit/common/test_mpi.py
@@ -2,20 +2,18 @@
 
 # Copyright (C) 2017 Garth N. Wells
 #
-# This file is part of DOLFINx (https://www.fenicsproject.org)
+# This file is part of DOLFINX (https://www.fenicsproject.org)
 #
 # SPDX-License-Identifier:    LGPL-3.0-or-later
 
-import pathlib
+import os
+import sys
 
-import cppimport
 import dolfinx
-import dolfinx.pkgconfig
 import mpi4py
 import pytest
 from dolfinx import wrappers
-from dolfinx.generation import UnitSquareMesh
-from dolfinx.jit import mpi_jit_decorator
+from dolfinx.jit import dolfinx_pc, mpi_jit_decorator
 from dolfinx_utils.test.fixtures import tempdir  # noqa: F401
 from mpi4py import MPI
 
@@ -23,57 +21,51 @@ from mpi4py import MPI
 def test_mpi_comm_wrapper():
     """Test MPICommWrapper <-> mpi4py.MPI.Comm conversion"""
     w1 = MPI.COMM_WORLD
-    m = UnitSquareMesh(w1, 4, 4)
-    w2 = m.comm
+    m = dolfinx.UnitSquareMesh(w1, 4, 4)
+    w2 = m.mpi_comm()
     assert isinstance(w1, MPI.Comm)
     assert isinstance(w2, MPI.Comm)
 
 
-@pytest.mark.skipif(not dolfinx.pkgconfig.exists("dolfinx"),
-                    reason="This test needs DOLFINx pkg-config.")
 def test_mpi_comm_wrapper_cppimport(tempdir):  # noqa: F811
     """Test MPICommWrapper <-> mpi4py.MPI.Comm conversion for code compiled with cppimport"""
 
-    dolfinx_pc = dolfinx.pkgconfig.parse("dolfinx")
+    cppimport = pytest.importorskip("cppimport")
 
     @mpi_jit_decorator
     def compile_module():
         cpp_code_header = f"""
-/*
-<%
-setup_pybind11(cfg)
-cfg['compiler_args'] = ['-std=c++17']
-cfg['include_dirs'] += {dolfinx_pc["include_dirs"]
-                        + [mpi4py.get_include()]
-                        + [str(wrappers.get_include_path())]}
-%>
-*/
-"""
+        <%
+        setup_pybind11(cfg)
+        cfg['include_dirs'] += {dolfinx_pc["include_dirs"]
+                                + [mpi4py.get_include()]
+                                + [str(wrappers.get_include_path())]}
+        %>
+        """
 
         cpp_code = """
-#include <pybind11/pybind11.h>
-#include <caster_mpi.h>
+        #include <pybind11/pybind11.h>
+        #include <caster_mpi.h>
 
-dolfinx_wrappers::MPICommWrapper
-test_comm_passing(const dolfinx_wrappers::MPICommWrapper comm)
-{
-    MPI_Comm c = comm.get();
-    return dolfinx_wrappers::MPICommWrapper(c);
-}
+        dolfinx_wrappers::MPICommWrapper
+        test_comm_passing(const dolfinx_wrappers::MPICommWrapper comm)
+        {
+          MPI_Comm c = comm.get();
+          return dolfinx_wrappers::MPICommWrapper(c);
+        }
 
-PYBIND11_MODULE(mpi_comm_wrapper, m)
-{
-    m.def("test_comm_passing", &test_comm_passing);
-}
-"""
+        PYBIND11_MODULE(test_mpi_comm_wrapper, m)
+        {
+            m.def("test_comm_passing", &test_comm_passing);
+        }
+        """
 
-        path = pathlib.Path(tempdir)
-        open(pathlib.Path(tempdir, "mpi_comm_wrapper.cpp"), "w").write(cpp_code + cpp_code_header)
-        rel_path = path.relative_to(pathlib.Path(__file__).parent)
-        p = str(rel_path).replace("/", ".") + ".comm_wrapper"
-        return cppimport.imp(p)
+        open(os.path.join(tempdir, "test_mpi_comm_wrapper.cpp"), "w").write(cpp_code_header + cpp_code)
 
-    module = compile_module(MPI.COMM_WORLD)
+        sys.path.append(tempdir)
+        return cppimport.imp("test_mpi_comm_wrapper")
+
+    module = compile_module()
 
     w1 = MPI.COMM_WORLD
     w2 = module.test_comm_passing(w1)
