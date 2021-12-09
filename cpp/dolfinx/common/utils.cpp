7222b4f06c98664f6ea5d2db1b3262b63955c525
Joe Dean <jpd62@cam.ac.uk>
2021-12-09 17:03:07 +0000

    Remove couts

--- cpp/dolfinx/common/utils.cpp
+++ cpp/dolfinx/common/utils.cpp
@@ -11,11 +11,6 @@ std::vector<int32_t> dolfinx::common::get_owned_indices(
     MPI_Comm comm, const xtl::span<const std::int32_t>& indices,
     std::shared_ptr<const dolfinx::common::IndexMap> index_map)
 {
-  std::stringstream ss;
-
-  int rank = dolfinx::MPI::rank(comm);
-  ss << "\n\nmesh::sub\nrank = " << rank << "\n";
-
   // Get the vertices in submesh_vertices that are owned by this process
   std::vector<std::int32_t> owned_indices; // Local numbering
   for (auto i : indices)
@@ -25,13 +20,6 @@ std::vector<int32_t> dolfinx::common::get_owned_indices(
       owned_indices.push_back(i);
     }
   }
-
-  ss << "owned_indices = ";
-  for (auto i : owned_indices)
-  {
-    ss << i << " ";
-  }
-  ss << "\n";
 
   MPI_Comm reverse_comm
       = index_map->comm(dolfinx::common::IndexMap::Direction::reverse);
@@ -49,20 +37,6 @@ std::vector<int32_t> dolfinx::common::get_owned_indices(
     }
   }
 
-  ss << "ghost_indices = ";
-  for (auto gi : ghost_indices)
-  {
-    ss << gi << " ";
-  }
-  ss << "\n";
-
-  ss << "dest_ranks = ";
-  for (auto rank : dest_ranks)
-  {
-    ss << rank << " ";
-  }
-  ss << "\n";
-
   std::vector<std::int32_t> data_per_proc(dest_ranks.size(), 0);
   auto ghost_owner_rank = index_map->ghost_owner_rank();
   for (std::size_t dest_rank_index = 0; dest_rank_index < dest_ranks.size();
@@ -77,23 +51,9 @@ std::vector<int32_t> dolfinx::common::get_owned_indices(
     }
   }
 
-  ss << "data_per_proc = ";
-  for (auto dpp : data_per_proc)
-  {
-    ss << dpp << " ";
-  }
-  ss << "\n";
-
   std::vector<int> send_disp(dest_ranks.size() + 1, 0);
   std::partial_sum(data_per_proc.begin(), data_per_proc.end(),
                    std::next(send_disp.begin(), 1));
-
-  ss << "send_disp = ";
-  for (auto disp : send_disp)
-  {
-    ss << disp << " ";
-  }
-  ss << "\n";
 
   // TODO Combine with above loop
   std::vector<std::int64_t> ghosts_to_send;
@@ -110,24 +70,10 @@ std::vector<int32_t> dolfinx::common::get_owned_indices(
     }
   }
 
-  ss << "ghosts_to_send = ";
-  for (auto g : ghosts_to_send)
-  {
-    ss << g << " ";
-  }
-  ss << "\n";
-
   const dolfinx::graph::AdjacencyList<std::int64_t> data_out(std::move(ghosts_to_send),
                                                              std::move(send_disp));
   const dolfinx::graph::AdjacencyList<std::int64_t> data_in
       = dolfinx::MPI::neighbor_all_to_all(reverse_comm, data_out);
-
-  ss << "data_in = ";
-  for (auto d : data_in.array())
-  {
-    ss << d << " ";
-  }
-  ss << "\n";
 
   // Append ghost vertices from other processes owned by this process to
   // submesh_owned_vertices. First need to get the local index
@@ -145,13 +91,6 @@ std::vector<int32_t> dolfinx::common::get_owned_indices(
   std::sort(owned_indices.begin(), owned_indices.end());
   owned_indices.erase(std::unique(owned_indices.begin(), owned_indices.end()),
                       owned_indices.end());
-
-  ss << "submesh_owned_vertices = ";
-  for (auto sv : owned_indices)
-  {
-    ss << sv << " ";
-  }
-  ss << "\n";
 
   return owned_indices;
 }
