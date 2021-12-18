78e45714291fd689079a071b652e682c4f3fbd15
Jorgen Dokken <dokken92@gmail.com>
2021-12-18 08:39:34 +0000

    Flake8

--- python/test/unit/fem/test_special_functions.py
+++ python/test/unit/fem/test_special_functions.py
@@ -32,9 +32,11 @@ def test_facet_area1D():
 
 
 @pytest.mark.parametrize('mesh_factory', [(create_unit_square, (MPI.COMM_WORLD, 3, 3), 1. / 3),
-                                          #   (create_unit_square, (MPI.COMM_WORLD, 3, 3, CellType.quadrilateral), 1. / 3),
+                                          #   (create_unit_square,
+                                          # (MPI.COMM_WORLD, 3, 3, CellType.quadrilateral), 1. / 3),
                                           (create_unit_cube, (MPI.COMM_WORLD, 3, 3, 3), 1 / 18.),
-                                          #   (create_unit_cube, (MPI.COMM_WORLD, 3, 3, 3, CellType.hexahedron), 1. / 9)
+                                          #   (create_unit_cube,
+                                          # (MPI.COMM_WORLD, 3, 3, 3, CellType.hexahedron), 1. / 9)
                                           ])
 def test_facet_area(mesh_factory):
     """
