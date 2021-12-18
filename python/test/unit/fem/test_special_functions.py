577929ebbe660842837d9355c42a7cdc65c8f5d9
Jorgen Dokken <dokken92@gmail.com>
2021-12-18 09:10:00 +0000

    Fix complex mode

--- python/test/unit/fem/test_special_functions.py
+++ python/test/unit/fem/test_special_functions.py
@@ -27,8 +27,8 @@ def test_facet_area1D():
     ds = ufl.Measure("ds", domain=mesh)
     a0 = mesh.comm.allreduce(assemble_scalar(c * ds), op=MPI.SUM)
     a = mesh.comm.allreduce(assemble_scalar(c0 * ds), op=MPI.SUM)
-    assert numpy.isclose(float(a), 2)
-    assert numpy.isclose(float(a0), 2)
+    assert numpy.isclose(a.real, 2)
+    assert numpy.isclose(a0.real, 2)
 
 
 @pytest.mark.parametrize('mesh_factory', [(create_unit_square, (MPI.COMM_WORLD, 3, 3), 1. / 3),
@@ -52,5 +52,5 @@ def test_facet_area(mesh_factory):
     ds = ufl.Measure("ds", domain=mesh)
     a = mesh.comm.allreduce(assemble_scalar(c * ds), op=MPI.SUM)
     a0 = mesh.comm.allreduce(assemble_scalar(c0 * ds), op=MPI.SUM)
-    assert numpy.isclose(float(a), num_faces)
-    assert numpy.isclose(float(a0), num_faces * exact_area)
+    assert numpy.isclose(a.real, num_faces)
+    assert numpy.isclose(a0.real, num_faces * exact_area)
