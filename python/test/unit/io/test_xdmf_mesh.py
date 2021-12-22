218281dc385b0028b9745c662ac87b0c9b81d489
Joe Dean <jpd62@cam.ac.uk>
2021-12-22 19:00:25 +0000

    Update

--- python/test/unit/io/test_xdmf_mesh.py
+++ python/test/unit/io/test_xdmf_mesh.py
@@ -157,7 +157,7 @@ def test_submesh(tempdir, d, n, codim, ghost_mode, encoding):
     mesh = mesh_factory(d, n, ghost_mode)
     edim = d - codim
     entities = locate_entities(mesh, edim, lambda x: x[0] >= 0.5)
-    submesh = mesh.sub(edim, entities)
+    submesh = mesh.create_submesh(edim, entities)[0]
 
     filename = os.path.join(tempdir, "submesh.xdmf")
     # Check writing the mesh doesn't cause a segmentation fault
