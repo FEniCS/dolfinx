5481945e1519f40b427ba1985aed97e4c87dd749
Joe Dean <jpd62@cam.ac.uk>
2021-12-23 14:13:17 +0000

    Flake8

--- python/test/unit/mesh/test_mesh.py
+++ python/test/unit/mesh/test_mesh.py
@@ -499,8 +499,7 @@ def submesh_topology_test(mesh, submesh, vertex_map, entity_dim, entities):
 
 def submesh_geometry_test(mesh, submesh, geom_map, entity_dim, entities):
     submesh_geom_index_map = submesh.geometry.index_map()
-    assert(submesh_geom_index_map.size_local
-           + submesh_geom_index_map.num_ghosts == submesh.geometry.x.shape[0])
+    assert(submesh_geom_index_map.size_local + submesh_geom_index_map.num_ghosts == submesh.geometry.x.shape[0])
 
     # Some processes might not own or ghost entities
     if len(entities) > 0:
