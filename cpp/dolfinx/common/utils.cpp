8b6717355f92f87bf09fe43a0ed6990ec73c245d
Joe Dean <jpd62@cam.ac.uk>
2021-12-29 14:14:00 +0000

    Add license

--- cpp/dolfinx/common/utils.cpp
+++ cpp/dolfinx/common/utils.cpp
@@ -1,3 +1,9 @@
+// Copyright (C) 2021 Joseph P. Dean
+//
+// This file is part of DOLFINx (https://www.fenicsproject.org)
+//
+// SPDX-License-Identifier:    LGPL-3.0-or-later
+
 #include "utils.h"
 #include <dolfinx/common/MPI.h>
 #include <dolfinx/graph/AdjacencyList.h>
