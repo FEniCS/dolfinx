d2223d939d3a2507f4b4c9ba3bca2e43cd1a2517
Garth N. Wells <gnw20@cam.ac.uk>
2022-01-10 22:23:40 +0000

    Fix CMake python interp name (#1921)

--- cpp/cmake/modules/FindUFC.cmake
+++ cpp/cmake/modules/FindUFC.cmake
@@ -56,7 +56,7 @@ if (DEFINED ENV{UFC_INCLUDE_DIR})
        MESSAGE(STATUS "Could not find UFC header.")
    endif()
 else()
-  MESSAGE(STATUS "Asking Python module FFCx for location of UFC... (${Python_EXECUTABLE})")
+  MESSAGE(STATUS "Asking Python module FFCx for location of UFC... (Python executable: ${Python3_EXECUTABLE})")
   execute_process(
     COMMAND ${Python3_EXECUTABLE} -c "import ffcx.codegeneration, sys; sys.stdout.write(ffcx.codegeneration.get_include_path())"
     OUTPUT_VARIABLE UFC_INCLUDE_DIR
