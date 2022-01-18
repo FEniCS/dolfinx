131d82ab321a116298cb3b08b4ecab37b62f13cc
Garth N. Wells <gnw20@cam.ac.uk>
2022-01-18 20:12:46 +0000

    Refector interpolation cpp demo

--- cpp/demo/interpolation-io/main.cpp
+++ cpp/demo/interpolation-io/main.cpp
@@ -27,6 +27,113 @@
 using namespace dolfinx;
 using T = PetscScalar;
 
+void interpolate_scalar(const std::shared_ptr<mesh::Mesh>& mesh)
+{
+  // Create a Basix continuous Lagrange element of degree 1
+  basix::FiniteElement e_basix = basix::element::create_lagrange(
+      mesh::cell_type_to_basix_type(mesh::CellType::triangle), 1,
+      basix::element::lagrange_variant::equispaced, true);
+
+  // Create a DOLFINx scalar Lagrange element
+  auto e = std::make_shared<fem::FiniteElement>(e_basix, 1);
+
+  // Create a dofmap
+  fem::ElementDofLayout layout(1, e_basix.entity_dofs(),
+                               e_basix.entity_closure_dofs(), {}, {});
+  auto dofmap = std::make_shared<fem::DofMap>(
+      fem::create_dofmap(mesh->comm(), layout, mesh->topology(), nullptr, e));
+
+  // Create a function space
+  auto V = std::make_shared<fem::FunctionSpace>(mesh, e, dofmap);
+
+  // Create a finite element Function
+  auto u = std::make_shared<fem::Function<T>>(V);
+
+  // Interpolate sin(2 \pi x[0]) in the scalar Lagrange finite element
+  // space
+  constexpr double PI = xt::numeric_constants<double>::PI;
+  u->interpolate([PI](auto& x) { return xt::sin(2 * PI * xt::row(x, 0)); });
+
+  // Write the function to a VTK file for visualisation, e.g. using
+  // ParaView
+  io::VTKFile file(MPI_COMM_WORLD, "u.pvd", "w");
+  file.write({*u}, 0.0);
+}
+
+void interpolate_nedelec(const std::shared_ptr<mesh::Mesh>& mesh)
+{
+  // Create a Basix Nedelec (first kind) element of degree 1 (lowest
+  // order, dim=3 on triangle)
+  basix::FiniteElement e_basix = basix::element::create_nedelec(
+      mesh::cell_type_to_basix_type(mesh::CellType::triangle), 1, false);
+
+  // Create a DOLFINx Nedelec element
+  auto e = std::make_shared<fem::FiniteElement>(e_basix, 1);
+
+  // Create a dofmap
+  fem::ElementDofLayout layout(1, e_basix.entity_dofs(),
+                               e_basix.entity_closure_dofs(), {}, {});
+  auto dofmap = std::make_shared<fem::DofMap>(
+      fem::create_dofmap(mesh->comm(), layout, mesh->topology(), nullptr, e));
+
+  // Create a function space
+  auto V = std::make_shared<fem::FunctionSpace>(mesh, e, dofmap);
+
+  // Create a finite element Function
+  auto u = std::make_shared<fem::Function<T>>(V);
+
+  // Interpolate the vector field
+  //  u = [0, 1]  if x[0] < 0.5
+  //      [1, 1]  if x[0] >= 0.5
+  // in the Nedelec space.
+  //
+  // Note that the x1 component of this field is continuous, and the x0
+  // component is discontinuous across x0 = 0.5. This function lies in
+  // the Nedelec space when there are cell edges aligned to x0 = 0.5.
+  u->interpolate(
+      [](auto& x) -> xt::xtensor<T, 2>
+      {
+        xt::xtensor<T, 2> v = xt::ones<T>({std::size_t(2), x.shape(1)});
+        auto v0 = xt::row(v, 0);
+        auto x0 = xt::row(x, 0);
+        xt::row(v, 0) = xt::where(x0 < 0.5, 0, 1);
+        return v;
+      });
+
+  // Nedelec spaces are not generally supported by visualisation tools.
+  // Simply evaluting a Nedelec function at cell vertices can
+  // mis-represent the function. However, we can represented a Nedelec
+  // function exactly in a discontinuous Lagrange space which we can
+  // then visualise. We do this here.
+
+  // First create a degree 1 vector-valued discontinuous Lagrange space:
+  basix::FiniteElement e_basix_l = basix::element::create_lagrange(
+      mesh::cell_type_to_basix_type(mesh::CellType::triangle), 1,
+      basix::element::lagrange_variant::equispaced, true);
+  auto e_l = std::make_shared<fem::FiniteElement>(e_basix_l, 2);
+  fem::ElementDofLayout layout_l(2, e_basix_l.entity_dofs(),
+                                 e_basix_l.entity_closure_dofs(), {}, {});
+  auto dofmap_l = std::make_shared<fem::DofMap>(fem::create_dofmap(
+      mesh->comm(), layout_l, mesh->topology(), nullptr, e_l));
+  auto V_l = std::make_shared<fem::FunctionSpace>(mesh, e_l, dofmap_l);
+  auto u_l = std::make_shared<fem::Function<T>>(V_l);
+
+  // Interpolate the Nedelec function into the discontinuous Lagrange
+  // space:
+  u_l->interpolate(*u);
+
+  // Outout the discontinuous Lagrange space in VTK format. When
+  // plotting the x0 component the field will appear discontinuous at x0
+  // = 0.5 (jump in the normal component between cells) and the x1
+  // component will appear continuous (continuous tangent component
+  // between cells).
+#ifdef HAS_ADIOS2
+  io::VTXWriter outfile(MPI_COMM_WORLD, "output_nedelec.bp", {u_l});
+  outfile.write(0.0);
+  outfile.close();
+#endif
+}
+
 /// This program shows how to create finite element spaces without FFCx
 /// generated code
 int main(int argc, char* argv[])
@@ -36,81 +143,13 @@ int main(int argc, char* argv[])
 
   {
     // Create mesh
-    // auto mesh = std::make_shared<mesh::Mesh>(mesh::create_rectangle(
-    //     MPI_COMM_WORLD, {{{0.0, 0.0}, {2.0, 1.0}}}, {32, 16},
-    //     mesh::CellType::triangle, mesh::GhostMode::none));
     auto mesh = std::make_shared<mesh::Mesh>(mesh::create_rectangle(
         MPI_COMM_WORLD, {{{0.0, 0.0}, {1.0, 1.0}}}, {32, 32},
         mesh::CellType::triangle, mesh::GhostMode::none));
 
-    // Create a Basix element
-    basix::FiniteElement e_basix = basix::element::create_lagrange(
-        mesh::cell_type_to_basix_type(mesh::CellType::triangle), 1,
-        basix::element::lagrange_variant::equispaced, false);
+    interpolate_scalar(mesh);
 
-    basix::FiniteElement e_basix_n = basix::element::create_nedelec(
-        mesh::cell_type_to_basix_type(mesh::CellType::triangle), 1, false);
-
-    // Create a DOLFINx element
-    auto e = std::make_shared<fem::FiniteElement>(e_basix, 1);
-    auto e_v = std::make_shared<fem::FiniteElement>(e_basix, 2);
-    auto e_n = std::make_shared<fem::FiniteElement>(e_basix_n, 1);
-
-    // Create a dofmap
-    fem::ElementDofLayout layout(1, e_basix.entity_dofs(),
-                                 e_basix.entity_closure_dofs(), {}, {});
-    fem::ElementDofLayout layout_v(2, e_basix.entity_dofs(),
-                                   e_basix.entity_closure_dofs(), {}, {});
-    fem::ElementDofLayout layout_n(1, e_basix_n.entity_dofs(),
-                                   e_basix_n.entity_closure_dofs(), {}, {});
-
-    auto dofmap = std::make_shared<fem::DofMap>(fem::create_dofmap(
-        MPI_COMM_WORLD, layout, mesh->topology(), nullptr, e));
-    auto dofmap_v = std::make_shared<fem::DofMap>(fem::create_dofmap(
-        MPI_COMM_WORLD, layout_v, mesh->topology(), nullptr, e_v));
-    auto dofmap_n = std::make_shared<fem::DofMap>(fem::create_dofmap(
-        MPI_COMM_WORLD, layout_n, mesh->topology(), nullptr, e));
-
-    // Create a function space
-    auto V = std::make_shared<fem::FunctionSpace>(mesh, e, dofmap);
-    auto V_v = std::make_shared<fem::FunctionSpace>(mesh, e_v, dofmap_v);
-    auto V_n = std::make_shared<fem::FunctionSpace>(mesh, e_n, dofmap_n);
-
-    // Create a finite elemenet Function
-    auto u = std::make_shared<fem::Function<T>>(V);
-    auto u_v = std::make_shared<fem::Function<T>>(V_v);
-    auto u_n = std::make_shared<fem::Function<T>>(V_n);
-
-    // Interpolate and expression the the finite element space and save
-    // the result to file
-    constexpr double PI = xt::numeric_constants<double>::PI;
-    u->interpolate([PI](auto& x) { return xt::sin(2 * PI * xt::row(x, 0)); });
-
-    u_n->interpolate(
-        [](auto& x) -> xt::xtensor<T, 2>
-        {
-          xt::xtensor<T, 2> v = xt::ones<T>({std::size_t(2), x.shape(1)});
-          auto v0 = xt::row(v, 0);
-          auto x0 = xt::row(x, 0);
-          xt::row(v, 0) = xt::where(x0 < 0.5, 0, 1);
-          return v;
-        });
-
-    // Save solution in VTK format
-    io::VTKFile file(MPI_COMM_WORLD, "u.pvd", "w");
-    file.write({*u}, 0.0);
-
-#ifdef HAS_ADIOS2
-    io::VTXWriter outfile(MPI_COMM_WORLD, "output.bp", {u});
-    outfile.write(0.0);
-    outfile.close();
-
-    u_v->interpolate(*u_n);
-    io::VTXWriter outfile_n(MPI_COMM_WORLD, "output_n.bp", {u_v});
-    outfile_n.write(0.0);
-    outfile_n.close();
-
-#endif
+    interpolate_nedelec(mesh);
   }
 
   common::subsystem::finalize_mpi();
