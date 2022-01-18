0426a8995a4a99ce0e944332d05519a5cc53a4d0
IgorBaratta <igorbaratta@gmail.com>
2022-01-18 18:07:16 +0000

    add more comments

--- cpp/demo/custom-spmv/main.cpp
+++ cpp/demo/custom-spmv/main.cpp
@@ -9,25 +9,9 @@
 using namespace dolfinx;
 using T = PetscScalar;
 
-// Then follows the definition of the coefficient functions (for
-// :math:`f` and :math:`g`), which are derived from the
-// :cpp:class:`Expression` class in DOLFINx
-//
-// .. code-block:: cpp
-
-// Inside the ``main`` function, we begin by defining a mesh of the
-// domain. As the unit square is a very standard domain, we can use a
-// built-in mesh provided by the :cpp:class:`UnitSquareMesh` factory. In
-// order to create a mesh consisting of 32 x 32 squares with each square
-// divided into two triangles, and the finite element space (specified in
-// the form file) defined relative to this mesh, we do as follows
-//
-// .. code-block:: cpp
-
 namespace
 {
 
-/// Compute y = Ai x
 template <typename T>
 void spmv_impl(xtl::span<const T> values,
                xtl::span<const std::int32_t> row_begin,
@@ -74,7 +58,6 @@ int main(int argc, char* argv[])
         *form_poisson_a, {V, V}, {}, {{"kappa", kappa}}, {}));
     auto L = std::make_shared<fem::Form<T>>(
         fem::create_form<T>(*form_poisson_L, {V}, {{"f", f}}, {}, {}));
-
     auto M = std::make_shared<fem::Form<T>>(fem::create_form<T>(
         *form_poisson_M, {V}, {{"ui", ui}}, {{"kappa", kappa}}, {}));
 
@@ -84,21 +67,39 @@ int main(int argc, char* argv[])
     la::SparsityPattern sp = fem::create_sparsity_pattern(*a);
     sp.assemble();
 
+    // Assemble matrix
     la::MatrixCSR<double> A(sp);
     fem::assemble_matrix(la::MatrixCSR<T>::mat_add_values(A), *a, {});
     A.finalize();
 
-    // Create compatible vectors
+    // Create vectors compatible with the sparse matrix
     auto column_map = A.index_maps()[1];
     la::Vector<T> x(column_map, 1);
-    fem::assemble_vector(x.mutable_array(), *L);
+    la::Vector<T> y0(column_map, 1);
+    la::Vector<T> y1(column_map, 1);
 
+    // Compute input vectors
+    fem::assemble_vector(x.mutable_array(), *L);
     std::copy_n(x.array().begin(), ui->x()->array().size(),
                 ui->x()->mutable_array().begin());
 
-    la::Vector<T> y(column_map, 1);
+    // The matrix A is distributed across P  processes by blocks of rows:
+    //  A = |   A_0  |
+    //      |   A_1  |
+    //      |   ...  |
+    //      |  A_P-1 |
+    //
+    // Each submatrix A_i is owned by a single process "i" and can be further
+    // decomposed into diagonal and off diagonal blocks:
+    //  Ai = |Ai_diag Ai_off|
+    //
+    // If A is square, the diagonal block Ai_diag is also square and countains
+    // only owned columns and rows.
+    //
+    // The block Ai_off contains ghost columns (unowned dofs).
+    //
 
-    // Compute diagonal
+    // Create function to compute y = A x in parallel
     auto spmv = [&A](la::Vector<T>& x, la::Vector<T>& y)
     {
       // start communication (update ghosts)
@@ -118,34 +119,46 @@ int main(int argc, char* argv[])
       xtl::span<const std::int32_t> row_end(row_ptr.data() + 1, nrows);
 
       // First stage:  spmv - diagonal
+      // yi += Ai_diag * xi
       spmv_impl<T>(values, row_begin, off_diag_offset, cols, _x, _y);
 
       // finalize ghost update
       x.scatter_fwd_end();
 
       // Second stage:  spmv - off-diagonal
+      // yi += Ai_off * xi
       spmv_impl<T>(values, off_diag_offset, row_end, cols, _x, _y);
     };
 
     // Two stage matrix vector computation
     common::Timer t0("~SPMV");
-    spmv(x, y);
+    spmv(x, y0);
     t0.stop();
 
-    la::Vector<T> y1(column_map, 1);
-
-    common::Timer t1("~Matrix Free action");
     ui->x()->scatter_fwd();
     const auto coefficients = fem::pack_coefficients(*M);
     const std::vector<T> constants = fem::pack_constants(*M);
-    fem::assemble_vector(y1.mutable_array(), *M, tcb::make_span(constants),
-                         make_coefficients_span(coefficients));
-    y1.scatter_rev(common::IndexMap::Mode::add);
+    common::Timer t1("~Matrix Free action");
+    {
+      fem::assemble_vector(y1.mutable_array(), *M, tcb::make_span(constants),
+                           make_coefficients_span(coefficients));
+      y1.scatter_rev(common::IndexMap::Mode::add);
+    }
     t1.stop();
 
-    std::cout << y.norm() << " " << y1.norm() << std::endl;
+    std::int32_t ndofs_local = column_map->size_local();
 
-    // dolfinx::list_timings(comm, {dolfinx::TimingType::wall});
+    // Check solutions
+    double err_abs = 0;
+    const auto y0_array = y0.array();
+    const auto y1_array = y1.array();
+    for (int i = 0; i < ndofs_local; i++)
+      err_abs += std::abs(y0_array[i] - y1_array[i]);
+
+    if (err_abs > 1e-15)
+      throw std::runtime_error("Solution mismatch");
+
+    dolfinx::list_timings(comm, {dolfinx::TimingType::wall});
   }
 
   common::subsystem::finalize_mpi();
