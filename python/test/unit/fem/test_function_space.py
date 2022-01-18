b46e1acafbf1187d2500233198b4ca6324eae1c1
Garth N. Wells <gnw20@cam.ac.uk>
2022-01-18 11:10:30 +0000

    property updates

--- python/test/unit/fem/test_function_space.py
+++ python/test/unit/fem/test_function_space.py
@@ -106,7 +106,7 @@ def test_sub(Q, W):
                 == len(X.dofmap.dof_layout.entity_closure_dofs(dim, i)) \
                 == len(X.dofmap.dof_layout.entity_closure_dofs(dim, 0))
 
-    assert W.dofmap.dof_layout.block_size() == X.dofmap.dof_layout.block_size()
+    assert W.dofmap.dof_layout.block_size == X.dofmap.dof_layout.block_size
     assert W.dofmap.bs * len(W.dofmap.cell_dofs(0)) == len(X.dofmap.cell_dofs(0))
 
     assert W.element.num_sub_elements == X.element.num_sub_elements
