d22f72b674de3a111b415f4258597f024b749a01
IgorBaratta <igorbaratta@gmail.com>
2022-01-19 21:28:39 +0000

    update demo

--- cpp/demo/poisson-matrix-free/main.cpp
+++ cpp/demo/poisson-matrix-free/main.cpp
@@ -9,10 +9,7 @@
 #include <cmath>
 #include <dolfinx.h>
 #include <dolfinx/fem/Constant.h>
-#include <dolfinx/fem/petsc.h>
-#include <dolfinx/la/MatrixCSR.h>
 #include <xtensor/xarray.hpp>
-#include <xtensor/xview.hpp>
 
 using namespace dolfinx;
 
