eb7e6e26777a5fd34d80188e39920c15e8aeb86a
Jack S. Hale <mail@jackhale.co.uk>
2022-01-28 08:56:26 +0100

    Can't get auto to work.

--- cpp/dolfinx/fem/Expression.h
+++ cpp/dolfinx/fem/Expression.h
@@ -137,9 +137,9 @@ public:
 
     int num_argument_dofs = 1;
     xtl::span<const std::uint32_t> cell_info;
-    auto dof_transform_to_transpose
-        = [](const xtl::span<T>&, const xtl::span<const std::uint32_t>&,
-             std::int32_t, int)
+    std::function<void(const xtl::span<T>&, const xtl::span<const std::uint32_t>&, std::int32_t, int)> dof_transform_to_transpose
+        = [](const xtl::span<T>& data, const xtl::span<const std::uint32_t>& cell_info,
+             std::int32_t cell, int block_size)
     {
       // Do nothing
     };
