13c9c580bae5879f39226f3f1f813f3f62c8e42a
Jack S. Hale <mail@jackhale.co.uk>
2022-01-31 16:55:39 +0100

    Fix.

--- cpp/dolfinx/fem/Function.h
+++ cpp/dolfinx/fem/Function.h
@@ -249,13 +249,24 @@ public:
       throw std::runtime_error(
           "Function value size not equal to Expression value size");
     }
-    if ((e.X().shape()
-         != _function_space->element()->interpolation_points().shape())
-        or !xt::allclose(e.X(),
-                         _function_space->element()->interpolation_points()))
+    
+    // Check that expression has same interpolation points as function
+    if (e.X().shape()
+         == _function_space->element()->interpolation_points().shape())
     {
-      throw std::runtime_error("Function element interpolation points not "
-                               "equal to Expression interpolation points");
+      // xt::allclose has issues on icpx in early 2022. This is why the interior
+      // if is nested.
+      if (!xt::allclose(e.X(),
+                         _function_space->element()->interpolation_points()))
+      {
+        throw std::runtime_error("Function element interpolation points not "
+                                 "equal to Expression interpolation points");
+      }
+    } 
+    else 
+    {
+      throw std::runtime_error("Function element interpolation points shape "
+                               "does not match Expression interpolation points shape");
     }
 
     // Array to hold evaluted Expression
