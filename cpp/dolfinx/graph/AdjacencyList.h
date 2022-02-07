3d90af9c82748747cc639829beb8d5e542377d07
Garth N. Wells <gnw20@cam.ac.uk>
2022-02-07 16:08:08 +0000

    Fix for cases where a rank has no part of the Mesh

--- cpp/dolfinx/graph/AdjacencyList.h
+++ cpp/dolfinx/graph/AdjacencyList.h
@@ -187,9 +187,8 @@ private:
 template <typename T, typename U>
 AdjacencyList<T> build_adjacency_list(U&& data, int degree)
 {
-  // using T = typename U::value_type;
-  assert(data.size() % degree == 0);
-  std::vector<std::int32_t> offsets((data.size() / degree) + 1, 0);
+  std::int32_t num_nodes = degree == 0 ? data.size() : data.size() / degree;
+  std::vector<std::int32_t> offsets(num_nodes + 1, 0);
   for (std::size_t i = 1; i < offsets.size(); ++i)
     offsets[i] = offsets[i - 1] + degree;
   return AdjacencyList<T>(std::forward<U>(data), std::move(offsets));
