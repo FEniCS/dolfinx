2b524a72bd47bcac768b0c88967a65a410861dd2
Garth N. Wells <gnw20@cam.ac.uk>
2022-02-08 12:10:42 +0000

    Minor updates

--- python/demo/poisson/demo_poisson.py
+++ python/demo/poisson/demo_poisson.py
@@ -95,11 +95,11 @@ from petsc4py.PETSc import ScalarType
 # -
 
 # We begin by defining a mesh of the domain and a finite element
-# function space $V$ relative to this mesh. We create a
-# rectangular mesh using built-in function provided by
-# class {py:class}`create_rectangle<dolfinx.mesh.create_rectangle>`.
-# In order to create a mesh consisting of 32 x 16 squares with each
-# square divided into two triangles, we do as follows
+# function space $V$ relative to this mesh. We create a rectangular mesh
+# using built-in function provided by class
+# {py:class}`create_rectangle<dolfinx.mesh.create_rectangle>`. In order
+# to create a mesh consisting of 32 x 16 squares with each square
+# divided into two triangles, we do as follows
 
 # +
 # Create mesh
@@ -115,27 +115,25 @@ V = fem.FunctionSpace(rect_mesh, ("Lagrange", 1))
 # -
 
 # The second argument to {py:class}`FunctionSpace
-# <dolfinx.fem.FunctionSpace>` is a tuple consisting of `(family, degree)`,
-# where `family` is the finite element family, and `degree` specifies
-# the polynomial degree. Thus, in this case,
-# our space `V` consists of first-order, continuous Lagrange finite
-# element functions (or in order words, continuous piecewise linear
-# polynomials).
+# <dolfinx.fem.FunctionSpace>` is a tuple consisting of `(family,
+# degree)`, where `family` is the finite element family, and `degree`
+# specifies the polynomial degree. Thus, in this case, our space `V`
+# consists of first-order, continuous Lagrange finite element functions
+# (or in order words, continuous piecewise linear polynomials).
 #
 # Next, we want to consider the Dirichlet boundary condition. A simple
 # Python function, returning a boolean, can be used to define the
 # boundary for the Dirichlet boundary condition ($\Gamma_D$). The
-# function should return `True` for those points inside the boundary
-# and `False` for the points outside. In our case, we want to say that
-# the points $(x, y)$ such that $x = 0$ or $x = 1$ are
-# inside on the inside of $\Gamma_D$. (Note that because of
-# rounding-off errors, it is often wise to instead specify $x <
-# \epsilon$ or $x > 1 - \epsilon$ where $\epsilon$ is a
-# small number (such as machine precision).)
+# function should return `True` for those points inside the boundary and
+# `False` for the points outside. In our case, we want to say that the
+# points $(x, y)$ such that $x = 0$ or $x = 1$ are inside on the inside
+# of $\Gamma_D$. (Note that because of rounding-off errors, it is often
+# wise to instead specify $x < \epsilon$ or $x > 1 - \epsilon$ where
+# $\epsilon$ is a small number (such as machine precision).)
 #
 # Now, the Dirichlet boundary condition can be created using the class
-# {py:class}`DirichletBC <dolfinx.fem.bcs.DirichletBC>`. A
-# {py:class}`DirichletBC <dolfinx.fem.bcs.DirichletBC>` takes three
+# {py:class}`DirichletBC <dolfinx.fem.DirichletBC>`. A
+# {py:class}`DirichletBC <dolfinx.fem.DirichletBC>` takes three
 # arguments: the value of the boundary condition, the part of the
 # boundary which the condition apply to, and the function space. This
 # boundary part is identified with degrees of freedom in the function
@@ -143,40 +141,39 @@ V = fem.FunctionSpace(rect_mesh, ("Lagrange", 1))
 #
 # To identify the degrees of freedom, we first find the facets (entities
 # of dimension 1) that likes on the boundary of the mesh, and satisfies
-# our criteria for `\Gamma_D`. Then, we use the function
+# our criteria for $\Gamma_D$. Then, we use the function
 # `locate_dofs_topological` to identify all degrees of freedom that is
 # located on the facet (including the vertices). In our example, the
 # function space is `V`, the value of the boundary condition (0.0) can
-# represented using a {py:class}`Constant
-# <dolfinx.fem.function.Constant>` and the Dirichlet boundary is defined
-# immediately above. The definition of the Dirichlet boundary condition
-# then looks as follows:
+# represented using a {py:class}`Constant <dolfinx.fem.Constant>` and
+# the Dirichlet boundary is defined immediately above. The definition of
+# the Dirichlet boundary condition then looks as follows:
 #
 # Define boundary condition on x = 0 or x = 1
 
-# Locate the facets on the boundary matching the provided marker function
+# Locate the facets on the boundary matching the provided marker
+# function
 facets = mesh.locate_entities_boundary(
     rect_mesh,
     dim=1,
     marker=lambda x: np.logical_or(np.isclose(x[0], 0.0), np.isclose(x[0], 2.0)),
 )
-# Locate the degrees of freedom for the relevant function space
-# that intersects the facets where we what to impose the boundary condition
+
+# Locate the degrees of freedom for the relevant function space that
+# intersects the facets where we what to impose the boundary condition
 dofs = fem.locate_dofs_topological(V=V, entity_dim=1, entities=facets)
 # Define the boundary condition
 bc = fem.dirichletbc(value=ScalarType(0), dofs=dofs, V=V)
 
 # Next, we want to express the variational problem.  First, we need to
-# specify the trial function $u$ and the test function $v$,
-# both living in the function space $V$. We do this by defining a
-# {py:class}`TrialFunction <dolfinx.functions.fem.TrialFunction>` and a
-# {py:class}`TestFunction <dolfinx.functions.fem.TrialFunction>` on the
-# previously defined {py:class}`FunctionSpace
-# <dolfinx.fem.FunctionSpace>` `V`.
+# specify the trial function $u$ and the test function $v$, both living
+# in the function space $V$. We do this by defining a
+# {py:class}`TrialFunction <ufl.TrialFunction>` and a
+# {py:class}`TestFunction <ufl.TrialFunction>` on the previously defined
+# {py:class}`FunctionSpace <dolfinx.fem.FunctionSpace>` `V`.
 #
-# Further, the source $f$ and the boundary normal derivative
-# $g$ are involved in the variational forms, and hence we must
-# specify these.
+# Further, the source $f$ and the boundary normal derivative $g$ are
+# involved in the variational forms, and hence we must specify these.
 #
 # With these ingredients, we can write down the bilinear form `a` and
 # the linear form `L` (using UFL operators). In summary, this reads
@@ -190,18 +187,21 @@ f = 10 * exp(-((x[0] - 0.5) ** 2 + (x[1] - 0.5) ** 2) / 0.02)
 g = sin(5 * x[0])
 a = inner(grad(u), grad(v)) * dx
 L = inner(f, v) * dx + inner(g, v) * ds
+# -
 
 # Now, we have specified the variational forms and can consider the
 # solution of the variational problem. First, we need to define a
-# :py:class:`Function <dolfinx.functions.fem.Function>` ``u`` to
-# represent the solution. (Upon initialization, it is simply set to the
-# zero function.) A :py:class:`Function
-# <dolfinx.functions.fem.Function>` represents a function living in a
-# finite element function space. Next, we initialize a solver using the
-# :py:class:`LinearProblem <dolfinx.fem.linearproblem.LinearProblem>`.
-# This class is initialized with the arguments ``a``, ``L``, and ``bc``
-# as follows: :: In this problem, we use a direct LU solver, which is
-# defined through the dictionary ``petsc_options``.
+#  {py:class}`Function <dolfinx.fem.Function>` `u` to represent the
+# solution. (Upon initialization, it is simply set to the zero
+# function.) A  {py:class}`Function <dolfinx.fem.Function>` represents a
+# function living in a finite element function space. Next, we
+# initialize a solver using the {py:class}`LinearProblem
+#  <dolfinx.fem.LinearProblem>`. This class is initialized with the
+# arguments `a`, `L`, and `bc` as follows:
+
+# +
+# In this problem, we use a direct LU solver, which is defined through
+# the dictionary `petsc_options`.
 problem = fem.LinearProblem(
     a, L, bcs=[bc], petsc_options={"ksp_type": "preonly", "pc_type": "lu"}
 )
@@ -216,18 +216,16 @@ uh = problem.solve()
 # However, the solution process can be controlled in much more detail if
 # desired.
 #
-# A {py:class}`Function <dolfinx.functions.fem.Function>` can be
-# manipulated in various ways, in particular, it can be plotted and
-# saved to file. Here, we output the solution to an `XDMF` file for
-# later visualization and also plot it using the {py:func}`plot
-# <dolfinx.common.plot.plot>` command:
+# A {py:class}`Function <dolfinx.fem.Function>` can be manipulated in
+# various ways, in particular, it can be plotted and saved to file.
+# Here, we output the solution to an XDMF file for later visualization
+# and also plot it using pyvista.
 
 # +
 # Save solution in XDMF format
 with io.XDMFFile(MPI.COMM_WORLD, "poisson.xdmf", "w") as file:
     file.write_mesh(rect_mesh)
     file.write_function(uh)
-
 
 # Plot solution
 try:
