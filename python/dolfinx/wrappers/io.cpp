3dfdf038cccdb70962865b58a63bf29c2e55ec6e
Jørgen Schartum Dokken <dokken92@gmail.com>
2022-02-08 17:26:12 +0000

    Extend pyvista plotting to higher order meshes. (#1996)
    * Extend pyvista plotting to higher order meshes.
    
    * Address feedback
    
    * Simplifications
    
    Co-authored-by: Garth N. Wells <gnw20@cam.ac.uk>

--- python/dolfinx/wrappers/io.cpp
+++ python/dolfinx/wrappers/io.cpp
@@ -14,6 +14,7 @@
 #include <dolfinx/io/VTKFile.h>
 #include <dolfinx/io/XDMFFile.h>
 #include <dolfinx/io/cells.h>
+#include <dolfinx/io/vtk_utils.h>
 #include <dolfinx/io/xdmf_utils.h>
 #include <dolfinx/mesh/Mesh.h>
 #include <dolfinx/mesh/MeshTags.h>
@@ -35,11 +36,20 @@ namespace dolfinx_wrappers
 void io(py::module& m)
 {
   // dolfinx::io::cell vtk cell type converter
-  m.def("get_vtk_cell_type", &dolfinx::io::cells::get_vtk_cell_type);
+  m.def("get_vtk_cell_type", &dolfinx::io::cells::get_vtk_cell_type,
+        "Get VTK cell identifier");
+
+  m.def(
+      "extract_vtk_connectivity",
+      [](const dolfinx::mesh::Mesh& mesh)
+      { return xt_as_pyarray(dolfinx::io::extract_vtk_connectivity(mesh)); },
+      "Extract the mesh topology with VTK ordering using geometry indices");
 
   // dolfinx::io::cell permutation functions
-  m.def("perm_vtk", &dolfinx::io::cells::perm_vtk);
-  m.def("perm_gmsh", &dolfinx::io::cells::perm_gmsh);
+  m.def("perm_vtk", &dolfinx::io::cells::perm_vtk,
+        "Permutation array to map from VTK to DOLFINx node ordering");
+  m.def("perm_gmsh", &dolfinx::io::cells::perm_gmsh,
+        "Permutation array to map from Gmsh to DOLFINx node ordering");
 
   // TODO: Template for different values dtypes
   m.def(
