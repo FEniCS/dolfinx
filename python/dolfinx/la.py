d6fea6864993cacaa74c3cb35ab9067877a48f02
Garth N. Wells <gnw20@cam.ac.uk>
2022-02-10 15:47:40 +0000

    Add meta class for MatrixCSR in Python

--- python/dolfinx/la.py
+++ python/dolfinx/la.py
@@ -5,6 +5,10 @@
 # SPDX-License-Identifier:    LGPL-3.0-or-later
 """Linear algebra functionality"""
 
+
+import numpy as np
+
+from dolfinx import cpp as _cpp
 from dolfinx.cpp.la.petsc import create_vector as create_petsc_vector
 
 __all__ = ["orthonormalize", "is_orthonormal", "create_petsc_vector"]
@@ -29,3 +33,39 @@ def is_orthonormal(basis, eps: float = 1.0e-12) -> bool:
             if abs(x.dot(y)) > eps:
                 return False
     return True
+
+
+class MatrixCSRCMetaClass:
+    def __init__(self, sp):
+        """A sparse matrix that uses compressed sparse row storage.
+
+        Args:
+            sp: The sparsity pattern that defines the nonzero structure
+            of the matrix the parallel distribution of the matrix
+        """
+        super().__init__(sp)
+
+
+def matrix_csr(sp, dtype=np.float64) -> MatrixCSRCMetaClass:
+    """Create a sparse matrix.
+
+    The matrix uses compressed sparse row storage.
+
+    Args:
+        sp: The sparsity pattern that defines the nonzero structure of
+        the matrix the parallel distribution of the matrix.
+
+    Returns:
+        A sparse matrix.
+    """
+    if dtype == np.float32:
+        ftype = _cpp.la.MatrixCSR_float32
+    elif dtype == np.float64:
+        ftype = _cpp.la.MatrixCSR_float64
+    elif dtype == np.complex128:
+        ftype = _cpp.la.MatrixCSR_complex128
+    else:
+        raise NotImplementedError(f"Type {dtype} not supported.")
+
+    matrixcls = type("MatrixCSR", (MatrixCSRCMetaClass, ftype), {})
+    return matrixcls(sp)
