e31eacb1602cf47ac9048a442bf81bfee197f4fe
Massimiliano Leoni <leoni.massimiliano1@gmail.com>
2022-02-17 12:02:05 +0100

    Adding interpolation unit test

---
+++ cpp/test/unit/interpolation.cpp
@@ -0,0 +1,89 @@
+// Copyright (C) 2022 Massimiliano Leoni
+//
+// This file is part of DOLFINx (https://www.fenicsproject.org)
+//
+// SPDX-License-Identifier:    LGPL-3.0-or-later
+
+#include <catch2/catch.hpp>
+#include <dolfinx/fem/FunctionSpace.h>
+#include <dolfinx/fem/utils.h>
+#include <dolfinx/la/petsc.h>
+#include <dolfinx/mesh/Mesh.h>
+#include <dolfinx/mesh/generation.h>
+#include <mpi.h>
+
+#include <dolfinx/common/log.h>
+
+#include "interpolation.h"
+
+using namespace dolfinx;
+
+namespace
+{
+
+void test_interpolation_different_meshes()
+{
+  auto meshL = std::make_shared<mesh::Mesh>(mesh::create_box(
+      MPI_COMM_WORLD, {{{0, 0, 0}, {1, 1, 1}}}, {11, 11, 11},
+      mesh::CellType::tetrahedron, mesh::GhostMode::shared_facet));
+
+  auto meshR = std::make_shared<mesh::Mesh>(mesh::create_box(
+      MPI_COMM_WORLD, {{{0.1, 0.1, 0.1}, {0.9, 0.9, 0.9}}}, {9, 9, 9},
+      mesh::CellType::tetrahedron, mesh::GhostMode::shared_facet));
+
+  auto VL = std::make_shared<fem::FunctionSpace>(fem::create_functionspace(
+      functionspace_form_interpolation_a, "u", meshL));
+  auto VR = std::make_shared<fem::FunctionSpace>(fem::create_functionspace(
+      functionspace_form_interpolation_a, "u", meshR));
+
+  auto uL = std::make_shared<fem::Function<PetscScalar>>(VL);
+  auto uR = std::make_shared<fem::Function<PetscScalar>>(VR);
+  auto uR_ex = std::make_shared<fem::Function<PetscScalar>>(VR);
+
+  uL->interpolate(
+      [](auto& x)
+      {
+        auto r = xt::zeros_like(x);
+        for (std::size_t i = 0; i < x.shape(1); ++i)
+        {
+          r(0, i) = std::cos(10 * x(0, i)) * std::sin(10 * x(2, i));
+          r(1, i) = std::sin(10 * x(0, i)) * std::sin(10 * x(2, i));
+          r(2, i) = std::cos(10 * x(0, i)) * std::cos(10 * x(2, i));
+        }
+        return r;
+      });
+
+  uR_ex->interpolate(
+      [](auto& x)
+      {
+        auto r = xt::zeros_like(x);
+        for (std::size_t i = 0; i < x.shape(1); ++i)
+        {
+          r(0, i) = std::cos(10 * x(0, i)) * std::sin(10 * x(2, i));
+          r(1, i) = std::sin(10 * x(0, i)) * std::sin(10 * x(2, i));
+          r(2, i) = std::cos(10 * x(0, i)) * std::cos(10 * x(2, i));
+        }
+        return r;
+      });
+
+  uR->interpolate(*uL);
+
+  la::petsc::Vector _uR(la::petsc::create_vector_wrap(*uR->x()), false);
+  la::petsc::Vector _uR_ex(la::petsc::create_vector_wrap(*uR_ex->x()), false);
+
+  VecAXPY(_uR.vec(), -1, _uR_ex.vec());
+  PetscReal diffNorm;
+  VecNorm(_uR.vec(), NORM_2, &diffNorm);
+
+  LOG(ERROR) << "diffNorm = " << diffNorm;
+}
+
+} // namespace
+
+TEST_CASE("Interpolation between different meshes",
+          "[interpolation_different_meshes]")
+{
+  CHECK_NOTHROW(test_interpolation_different_meshes());
+}
+
+#endif
