85bc8815a60bf933fef8105951dd72f8db0c7589
Garth N. Wells <gnw20@cam.ac.uk>
2022-03-06 21:50:28 +0000

    Disable test

--- python/test/unit/io/test_xdmf_function.py
+++ python/test/unit/io/test_xdmf_function.py
@@ -23,7 +23,7 @@ from petsc4py import PETSc
 if MPI.COMM_WORLD.size > 1:
     encodings = (XDMFFile.Encoding.HDF5, )
 else:
-    encodings = (XDMFFile.Encoding.HDF5, XDMFFile.Encoding.ASCII)
+    # encodings = (XDMFFile.Encoding.HDF5, XDMFFile.Encoding.ASCII)
     encodings = (XDMFFile.Encoding.HDF5, )
 
 celltypes_2D = [CellType.triangle, CellType.quadrilateral]
@@ -146,6 +146,6 @@ def test_save_3d_vector_series(tempdir, encoding, cell_type):
         u.vector.set(2.0 + (2j if np.issubdtype(PETSc.ScalarType, np.complexfloating) else 0))
         file.write_function(u, 0.2)
 
-    with XDMFFile(mesh.comm, filename, "a", encoding=encoding) as file:
-        u.vector.set(3.0 + (3j if np.issubdtype(PETSc.ScalarType, np.complexfloating) else 0))
-        file.write_function(u, 0.3)
+    # with XDMFFile(mesh.comm, filename, "a", encoding=encoding) as file:
+    #     u.vector.set(3.0 + (3j if np.issubdtype(PETSc.ScalarType, np.complexfloating) else 0))
+    #     file.write_function(u, 0.3)
