b9d620fcf9ed522d1894412b909e8cc40766a3f9
Chris Richardson <chris@bpi.cam.ac.uk>
2022-03-15 14:05:15 +0000

    Update docs

--- cpp/dolfinx/refinement/plaza.h
+++ cpp/dolfinx/refinement/plaza.h
@@ -34,6 +34,7 @@ namespace plaza
 /// @param[in] mesh Input mesh to be refined
 /// @param[in] redistribute Flag to call the mesh partitioner to
 /// redistribute after refinement
+/// @param[in] store_indices Flag to save refinement data for further use
 /// @return New mesh
 std::tuple<std::vector<std::int32_t>, std::vector<std::int64_t>, mesh::Mesh>
 refine(const mesh::Mesh& mesh, bool redistribute, bool store_indices);
@@ -45,7 +46,8 @@ refine(const mesh::Mesh& mesh, bool redistribute, bool store_indices);
 /// refinement
 /// @param[in] redistribute Flag to call the Mesh Partitioner to
 /// redistribute after refinement
-/// @return New Mesh
+/// @param[in] store_indices Flag to save refinement data for further use
+/// @return optional refeinemtn data and New Mesh
 std::tuple<std::vector<std::int32_t>, std::vector<std::int64_t>, mesh::Mesh>
 refine(const mesh::Mesh& mesh, const xtl::span<const std::int32_t>& edges,
        bool redistribute, bool store_indices);
@@ -67,6 +69,8 @@ compute_refinement_data(const mesh::Mesh& mesh, bool store_indices);
 /// @param[in] mesh Input mesh to be refined
 /// @param[in] edges Indices of the edges that should be split by this
 /// refinement
+/// @param[in] store_indices If true, returns internal data of new vertex
+/// indices associated with each original cell.
 /// @return New mesh data: cell topology, vertex coordinates and parent
 /// cell index
 std::tuple<graph::AdjacencyList<std::int64_t>, xt::xtensor<double, 2>,
