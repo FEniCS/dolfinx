7ad0863fd40df69dc9411a09d2dde0a8a1702b85
Garth N. Wells <gnw20@cam.ac.uk>
2022-03-17 12:25:43 +0000

    Revert SCOTCH only testing

--- cpp/dolfinx/graph/partition.cpp
+++ cpp/dolfinx/graph/partition.cpp
@@ -23,17 +23,16 @@ graph::partition_graph(MPI_Comm comm, int nparts,
                        const AdjacencyList<std::int64_t>& local_graph,
                        bool ghosting)
 {
-  // #if HAS_PARMETIS
-  //   return graph::parmetis::partitioner()(comm, nparts, local_graph,
-  //   ghosting);
-  // #elif HAS_PTSCOTCH
+#if HAS_PARMETIS
+  return graph::parmetis::partitioner()(comm, nparts, local_graph, ghosting);
+#elif HAS_PTSCOTCH
   return graph::scotch::partitioner()(comm, nparts, local_graph, ghosting);
-  // #elif HAS_KAHIP
-  //   return graph::kahip::partitioner()(comm, nparts, local_graph, ghosting);
-  // #else
-  // // Should never reach this point
-  // #endif
-}
+#elif HAS_KAHIP
+  return graph::kahip::partitioner()(comm, nparts, local_graph, ghosting);
+#else
+// Should never reach this point
+#endif
+}s
 //-----------------------------------------------------------------------------
 std::tuple<graph::AdjacencyList<std::int64_t>, std::vector<int>,
            std::vector<std::int64_t>, std::vector<int>>
