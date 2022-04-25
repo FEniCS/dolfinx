aa5b84de293e8577e5a5005c2fb66fb3f1c8d99b
Joe Dean <jpd62@cam.ac.uk>
2022-04-25 17:18:24 +0100

    Add test

--- python/test/unit/fem/test_assemble_submesh.py
+++ python/test/unit/fem/test_assemble_submesh.py
@@ -118,3 +118,81 @@ def test_submesh_facet_assembly(n, k, space, ghost_mode):
     assert(np.isclose(A_submesh.norm(), A_square_mesh.norm()))
     assert(np.isclose(b_submesh.norm(), b_square_mesh.norm()))
     assert(np.isclose(s_submesh, s_square_mesh))
+
+
+def assemble_mixed_forms(comm, f, g, u, v, dx, ds, domain_map={}):
+    a = fem.form(ufl.inner(f * g * u, v) * (dx + ds), domain_map=domain_map)
+    A = fem.petsc.assemble_matrix(a)
+    A.assemble()
+
+    L = fem.form(ufl.inner(f * g, v) * (dx + ds), domain_map=domain_map)
+    b = fem.petsc.assemble_vector(L)
+    b.ghostUpdate(addv=PETSc.InsertMode.ADD,
+                  mode=PETSc.ScatterMode.REVERSE)
+
+    M = fem.form(f * g * (dx + ds), domain_map=domain_map)
+    s = comm.allreduce(fem.assemble_scalar(M), op=MPI.SUM)
+
+    return A, b, s
+
+
+@pytest.mark.parametrize("d", [2, 3])
+@pytest.mark.parametrize("n", [2, 6])
+@pytest.mark.parametrize("k", [1, 4])
+@pytest.mark.parametrize("space", ["Lagrange", "Discontinuous Lagrange"])
+@pytest.mark.parametrize("ghost_mode", [GhostMode.none,
+                                        GhostMode.shared_facet])
+def test_mixed_codim_0_assembly(d, n, k, space, ghost_mode):
+    if d == 2:
+        mesh_0 = create_unit_square(
+            MPI.COMM_WORLD, n, n, ghost_mode=ghost_mode)
+        mesh_1 = create_rectangle(
+            MPI.COMM_WORLD, ((0.0, 0.0), (2.0, 1.0)), (2 * n, n),
+            ghost_mode=ghost_mode)
+    else:
+        mesh_0 = create_unit_cube(
+            MPI.COMM_WORLD, n, n, n, ghost_mode=ghost_mode)
+        mesh_1 = create_box(
+            MPI.COMM_WORLD, ((0.0, 0.0, 0.0), (2.0, 1.0, 1.0)),
+            (2 * n, n, n), ghost_mode=ghost_mode)
+
+    edim = mesh_1.topology.dim
+    entities = locate_entities(mesh_1, edim, lambda x: x[0] <= 1.0)
+    submesh, entity_map, vertex_map, geom_map = create_submesh(
+        mesh_1, edim, entities)
+
+    V_m1 = fem.FunctionSpace(mesh_1, (space, k))
+    V_sm = fem.FunctionSpace(submesh, (space, k))
+
+    u_sm = ufl.TrialFunction(V_sm)
+    v_sm = ufl.TestFunction(V_sm)
+    dx_sm = ufl.Measure("dx", domain=submesh)
+    ds_sm = ufl.Measure("ds", domain=submesh)
+
+    f_m1 = fem.Function(V_m1)
+    f_m1.interpolate(lambda x: x[0])
+
+    g_sm = fem.Function(V_sm)
+    # TODO Interpolate when issue #2126 has been resolved
+    g_sm.x.array[:] = 1.0
+
+    domain_map = {mesh_1: entity_map}
+    A_sm, b_sm, s_sm = assemble_mixed_forms(
+        submesh.comm, f_m1, g_sm, u_sm, v_sm, dx_sm, ds_sm, domain_map)
+
+    V_m0 = fem.FunctionSpace(mesh_0, (space, k))
+    f_m0 = fem.Function(V_m0)
+    f_m0.interpolate(lambda x: x[0])
+
+    g_m0 = fem.Function(V_m0)
+    g_m0.x.array[:] = 1.0
+
+    u_m0 = ufl.TrialFunction(V_m0)
+    v_m0 = ufl.TestFunction(V_m0)
+
+    A_m0, b_m0, s_m0 = assemble_mixed_forms(
+        mesh_0.comm, f_m0, g_m0, u_m0, v_m0, ufl.dx, ufl.ds)
+
+    assert(np.isclose(A_sm.norm(), A_m0.norm()))
+    assert(np.isclose(b_sm.norm(), b_m0.norm()))
+    assert(np.isclose(s_sm, s_m0))
