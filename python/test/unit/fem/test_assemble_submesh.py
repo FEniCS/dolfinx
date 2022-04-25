cc9319854dc3d68104314548ffd744df9b8fa2b6
Joe Dean <jpd62@cam.ac.uk>
2022-04-25 17:47:06 +0100

    Test additional submesh

--- python/test/unit/fem/test_assemble_submesh.py
+++ python/test/unit/fem/test_assemble_submesh.py
@@ -120,17 +120,18 @@ def test_submesh_facet_assembly(n, k, space, ghost_mode):
     assert(np.isclose(s_submesh, s_square_mesh))
 
 
-def assemble_mixed_forms(comm, f, g, u, v, dx, ds, domain_map={}):
-    a = fem.form(ufl.inner(f * g * u, v) * (dx + ds), domain_map=domain_map)
+def assemble_mixed_forms(comm, f, g, h, u, v, dx, ds, domain_map={}):
+    a = fem.form(ufl.inner(f * g * h * u, v) * (dx + ds),
+                 domain_map=domain_map)
     A = fem.petsc.assemble_matrix(a)
     A.assemble()
 
-    L = fem.form(ufl.inner(f * g, v) * (dx + ds), domain_map=domain_map)
+    L = fem.form(ufl.inner(f * g * h, v) * (dx + ds), domain_map=domain_map)
     b = fem.petsc.assemble_vector(L)
     b.ghostUpdate(addv=PETSc.InsertMode.ADD,
                   mode=PETSc.ScatterMode.REVERSE)
 
-    M = fem.form(f * g * (dx + ds), domain_map=domain_map)
+    M = fem.form(f * g * h * (dx + ds), domain_map=domain_map)
     s = comm.allreduce(fem.assemble_scalar(M), op=MPI.SUM)
 
     return A, b, s
@@ -161,8 +162,13 @@ def test_mixed_codim_0_assembly(d, n, k, space, ghost_mode):
     submesh, entity_map, vertex_map, geom_map = create_submesh(
         mesh_1, edim, entities)
 
+    entities_2 = locate_entities(mesh_1, edim, lambda x: x[0] <= 1.5)
+    submesh_2, entity_map_2, vertex_map_2, geom_map_2 = create_submesh(
+        mesh_1, edim, entities_2)
+
     V_m1 = fem.FunctionSpace(mesh_1, (space, k))
     V_sm = fem.FunctionSpace(submesh, (space, k))
+    V_sm_2 = fem.FunctionSpace(submesh_2, (space, k))
 
     u_sm = ufl.TrialFunction(V_sm)
     v_sm = ufl.TestFunction(V_sm)
@@ -176,9 +182,15 @@ def test_mixed_codim_0_assembly(d, n, k, space, ghost_mode):
     # TODO Interpolate when issue #2126 has been resolved
     g_sm.x.array[:] = 1.0
 
-    domain_map = {mesh_1: entity_map}
+    h_sm_2 = fem.Function(V_sm_2)
+    # TODO Interpolate when issue #2126 has been resolved
+    h_sm_2.x.array[:] = 2.0
+
+    domain_map = {mesh_1: entity_map,
+                  submesh_2: [entity_map_2.index(entity)
+                              for entity in entity_map]}
     A_sm, b_sm, s_sm = assemble_mixed_forms(
-        submesh.comm, f_m1, g_sm, u_sm, v_sm, dx_sm, ds_sm, domain_map)
+        submesh.comm, f_m1, g_sm, h_sm_2, u_sm, v_sm, dx_sm, ds_sm, domain_map)
 
     V_m0 = fem.FunctionSpace(mesh_0, (space, k))
     f_m0 = fem.Function(V_m0)
@@ -187,11 +199,14 @@ def test_mixed_codim_0_assembly(d, n, k, space, ghost_mode):
     g_m0 = fem.Function(V_m0)
     g_m0.x.array[:] = 1.0
 
+    h_m0 = fem.Function(V_m0)
+    h_m0.x.array[:] = 2.0
+
     u_m0 = ufl.TrialFunction(V_m0)
     v_m0 = ufl.TestFunction(V_m0)
 
     A_m0, b_m0, s_m0 = assemble_mixed_forms(
-        mesh_0.comm, f_m0, g_m0, u_m0, v_m0, ufl.dx, ufl.ds)
+        mesh_0.comm, f_m0, g_m0, h_m0, u_m0, v_m0, ufl.dx, ufl.ds)
 
     assert(np.isclose(A_sm.norm(), A_m0.norm()))
     assert(np.isclose(b_sm.norm(), b_m0.norm()))
