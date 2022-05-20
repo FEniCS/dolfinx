2411b4e5f9d2f3c26eb5245738631c1e72cb9923
Garth N. Wells <gnw20@cam.ac.uk>
2022-05-20 18:19:53 +0100

    Variable name fix

--- cpp/dolfinx/mesh/topologycomputation.cpp
+++ cpp/dolfinx/mesh/topologycomputation.cpp
@@ -179,7 +179,6 @@ get_local_indexing(MPI_Comm comm, const common::IndexMap& cell_map,
   // index
   std::vector<std::pair<std::vector<std::int64_t>, std::int32_t>>
       entity_global_to_local;
-  std::vector<std::int64_t> entity_global_to_local_new;
 
   {
     // If another rank shares all vertices of an entity, it may need the entity
@@ -216,10 +215,6 @@ get_local_indexing(MPI_Comm comm, const common::IndexMap& cell_map,
           std::sort(vglobal.begin(), vglobal.end());
           entity_global_to_local.push_back({vglobal, *entity_idx});
 
-          entity_global_to_local_new.insert(entity_global_to_local_new.end(),
-                                            vglobal.begin(), vglobal.end());
-          entity_global_to_local_new.push_back(*entity_idx);
-
           // Only send entities that are not known to be ghosts
           if (ghost_status[*entity_idx] != 2)
           {
@@ -237,29 +232,6 @@ get_local_indexing(MPI_Comm comm, const common::IndexMap& cell_map,
         it = it1;
       }
     }
-
-    std::vector<std::int32_t> perm(entity_global_to_local_new.size()
-                                   / (num_vertices_per_e + 1));
-    std::iota(perm.begin(), perm.end(), 0);
-    std::sort(perm.begin(), perm.end(),
-              [&entities = entity_global_to_local_new,
-               shape1 = num_vertices_per_e + 1](auto e0, auto e1)
-              {
-                auto it0 = std::next(entities.begin(), e0 * shape1);
-                auto it1 = std::next(entities.begin(), e1 * shape1);
-                return std::lexicographical_compare(
-                    it0, std::next(it0, shape1), it1, std::next(it1, shape1));
-              });
-    perm.erase(
-        std::unique(perm.begin(), perm.end(),
-                    [&entities = entity_global_to_local_new,
-                     shape1 = num_vertices_per_e + 1](auto e0, auto e1)
-                    {
-                      auto it0 = std::next(entities.begin(), e0 * shape1);
-                      auto it1 = std::next(entities.begin(), e1 * shape1);
-                      return std::equal(it0, std::next(it0, shape1), it1);
-                    }),
-        perm.end());
 
     std::sort(entity_global_to_local.begin(), entity_global_to_local.end());
     entity_global_to_local.erase(std::unique(entity_global_to_local.begin(),
@@ -430,7 +402,7 @@ get_local_indexing(MPI_Comm comm, const common::IndexMap& cell_map,
     }
 
     // Transform send/receive sizes and displacements for scalar send
-    for (auto x : {&send_disp, &send_disp, &recv_sizes, &recv_disp})
+    for (auto x : {&send_sizes, &send_disp, &recv_sizes, &recv_disp})
     {
       std::transform(x->begin(), x->end(), x->begin(),
                      [num_vertices_per_e](auto a)
