de5ac7c7e33c68837e7fc315b2e2f52691b65278
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-05-24 09:33:19 +0100

    size_t

--- cpp/dolfinx/fem/FiniteElement.cpp
+++ cpp/dolfinx/fem/FiniteElement.cpp
@@ -164,7 +164,7 @@ FiniteElement::FiniteElement(const ufcx_finite_element& e)
         = static_cast<basix::cell::type>(ce->cell_type);
 
     std::vector<std::size_t> value_shape(ce->value_shape_length);
-    int value_size = 1;
+    std::size_t value_size = 1;
     for (int i = 0; i < ce->value_shape_length; ++i)
     {
       value_shape[i] = ce->value_shape[i];
@@ -189,28 +189,25 @@ FiniteElement::FiniteElement(const ufcx_finite_element& e)
       int pt_n = 0;
       int p_e = 0;
       int m_e = 0;
-      const int dim = basix::cell::topological_dimension(cell_type);
-      for (int d = 0; d <= dim; ++d)
+      const std::size_t dim = static_cast<std::size_t>(
+          basix::cell::topological_dimension(cell_type));
+      for (std::size_t d = 0; d <= dim; ++d)
       {
         const int num_entities = basix::cell::num_sub_entities(cell_type, d);
         for (int entity = 0; entity < num_entities; ++entity)
         {
-          const int npts = ce->npts[pt_n];
-          const int ndofs = ce->ndofs[pt_n];
+          const std::size_t npts = static_cast<std::size_t>(ce->npts[pt_n]);
+          const std::size_t ndofs = static_cast<std::size_t>(ce->ndofs[pt_n]);
           ++pt_n;
-          xt::xtensor<double, 2> pts(
-              {static_cast<std::size_t>(npts), static_cast<std::size_t>(dim)});
-          xt::xtensor<double, 4> mat({static_cast<std::size_t>(ndofs),
-                                      static_cast<std::size_t>(value_size),
-                                      static_cast<std::size_t>(npts),
-                                      nderivs_dim});
-          for (int i = 0; i < npts; ++i)
-            for (int j = 0; j < dim; ++j)
+          xt::xtensor<double, 2> pts({npts, dim});
+          xt::xtensor<double, 4> mat({ndofs, value_size, npts, nderivs_dim});
+          for (std::size_t i = 0; i < npts; ++i)
+            for (std::size_t j = 0; j < dim; ++j)
               pts(i, j) = ce->x[p_e++];
-          for (int i = 0; i < ndofs; ++i)
-            for (int j = 0; j < value_size; ++j)
-              for (int k = 0; k < npts; ++k)
-                for (int l = 0; l < nderivs_dim; ++l)
+          for (std::size_t i = 0; i < ndofs; ++i)
+            for (std::size_t j = 0; j < value_size; ++j)
+              for (std::size_t k = 0; k < npts; ++k)
+                for (std::size_t l = 0; l < nderivs_dim; ++l)
                   mat(i, j, k, l) = ce->M[m_e++];
           x[d].push_back(pts);
           M[d].push_back(mat);
