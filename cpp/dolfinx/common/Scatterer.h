31bc17c1feb65706c4f59b45e5aacaa976473e6a
Joe Dean <jpd62@cam.ac.uk>
2022-06-06 12:07:59 +0100

    Check size

--- cpp/dolfinx/common/Scatterer.h
+++ cpp/dolfinx/common/Scatterer.h
@@ -285,8 +285,11 @@ public:
                        MPI_Request& request)
   {
     assert(local_buffer.size() == _local_inds.size());
-    assert(*std::max_element(_local_inds.begin(), _local_inds.end())
-           < local_data.size());
+    if (_local_inds.size() > 0)
+    {
+      assert(*std::max_element(_local_inds.begin(), _local_inds.end())
+             < local_data.size());
+    }
     scatter_rev_end(request);
     unpack_fn(local_buffer, _local_inds, local_data, op);
   }
