05fd77152687a7aa28cd42cd3ee8e47173b036d3
Michele Castriotta <mich.castriotta@gmail.com>
2022-08-22 11:39:42 +0200

    Add test for `RTCE` elements

--- python/test/unit/fem/test_fem_pipeline.py
+++ python/test/unit/fem/test_fem_pipeline.py
@@ -256,6 +256,68 @@ def test_curl_curl_eigenvalue(family, order):
     eigenvalues_exact = np.array([1.0, 1.0, 2.0, 4.0, 4.0, 5.0, 5.0, 8.0, 9.0])
     assert np.isclose(eigenvalues_sorted[0:eigenvalues_exact.shape[0]], eigenvalues_exact, rtol=1E-2).all()
 
+@pytest.mark.parametrize("family", ["RTCE"])
+@pytest.mark.parametrize("order", [1, 2, 3])
+def test_curl_curl_eigenvalue_RTCE(family, order):
+    """curl curl eigenvalue problem.
+
+    Solved using H(curl)-conforming finite element method.
+    See https://www-users.cse.umn.edu/~arnold/papers/icm2002.pdf for details.
+    """
+    slepc4py = pytest.importorskip("slepc4py")  # noqa: F841
+    from slepc4py import SLEPc
+
+    mesh = create_rectangle(MPI.COMM_WORLD, [np.array([0.0, 0.0]),
+                                             np.array([np.pi, np.pi])], [24, 24], CellType.quadrilateral)
+
+    element = ufl.FiniteElement(family, ufl.quadrilateral, order)
+    V = FunctionSpace(mesh, element)
+
+    u = ufl.TrialFunction(V)
+    v = ufl.TestFunction(V)
+
+    a = inner(ufl.curl(u), ufl.curl(v)) * dx
+    b = inner(u, v) * dx
+
+    boundary_facets = locate_entities_boundary(
+        mesh, mesh.topology.dim - 1, lambda x: np.full(x.shape[1], True, dtype=bool))
+    boundary_dofs = locate_dofs_topological(V, mesh.topology.dim - 1, boundary_facets)
+
+    zero_u = Function(V)
+    zero_u.x.array[:] = 0.0
+    bcs = [dirichletbc(zero_u, boundary_dofs)]
+
+    a, b = form(a), form(b)
+
+    A = assemble_matrix(a, bcs=bcs)
+    A.assemble()
+
+    B = assemble_matrix(b, bcs=bcs, diagonal=0.01)
+    B.assemble()
+
+    eps = SLEPc.EPS().create()
+    eps.setOperators(A, B)
+    PETSc.Options()["eps_type"] = "krylovschur"
+    PETSc.Options()["eps_gen_hermitian"] = ""
+    PETSc.Options()["eps_target_magnitude"] = ""
+    PETSc.Options()["eps_target"] = 5.0
+    PETSc.Options()["eps_view"] = ""
+    PETSc.Options()["eps_nev"] = 12
+    eps.setFromOptions()
+    eps.solve()
+
+    num_converged = eps.getConverged()
+    eigenvalues_unsorted = np.zeros(num_converged, dtype=np.complex128)
+
+    for i in range(0, num_converged):
+        eigenvalues_unsorted[i] = eps.getEigenvalue(i)
+
+    assert np.isclose(np.imag(eigenvalues_unsorted), 0.0).all()
+    eigenvalues_sorted = np.sort(np.real(eigenvalues_unsorted))[:-1]
+    eigenvalues_sorted = eigenvalues_sorted[np.logical_not(eigenvalues_sorted < 1E-8)]
+
+    eigenvalues_exact = np.array([1.0, 1.0, 2.0, 4.0, 4.0, 5.0, 5.0, 8.0, 9.0])
+    assert np.isclose(eigenvalues_sorted[0:eigenvalues_exact.shape[0]], eigenvalues_exact, rtol=1E-2).all()
 
 @pytest.mark.skipif(np.issubdtype(PETSc.ScalarType, np.complexfloating),
                     reason="This test does not work in complex mode.")
