FEniCS/dolfinx

commit 8b5726f9db31359ba1243d85e8a197758b3d2ef1

Eliminate ghost mode (#2337)
* Remove ghost-mode specific code

* Remove more ghost-mode stuff

* Add some debug

* more debug

* dubug scotch

* Fix in SCOTCH

* Remove debug

* Comment out some ghost-mode specific stuff

* unused var

* Remove ghost_mode from create_topology

* fix doc

* Remove ghost_mode args, but not working [skip ci]

* Fixes for Python

* flake8

* Fix cpp tests

* Fix demos

* Fix for ADIOS2

* Fix for python io

* Fixes for gmshio

* Test fixes

* Update test_mesh.py

* Update test_mesh_partitioners.py

* Remove ghost_mode from create_mesh in Python

* flake8

* gmshio correction

* Fix adios2 test

* flake8

* flake8

* reset CI

Co-authored-by: Garth N. Wells <gnw20@cam.ac.uk>

python/dolfinx/mesh.py
@@ -148,8 +148,8 @@ def refine(mesh: Mesh, edges: np.ndarray = None, redistribute: bool = True) -> M
 
 
 def create_mesh(comm: _MPI.Comm, cells: typing.Union[np.ndarray, _cpp.graph.AdjacencyList_int64],
-                x: np.ndarray, domain: ufl.Mesh, ghost_mode=GhostMode.shared_facet,
-                partitioner=_cpp.mesh.create_cell_partitioner()) -> Mesh:
+                x: np.ndarray, domain: ufl.Mesh,
+                partitioner=_cpp.mesh.create_cell_partitioner(GhostMode.none)) -> Mesh:
     """
     Create a mesh from topology and geometry arrays
 
@@ -170,10 +170,10 @@ def create_mesh(comm: _MPI.Comm, cells: typing.Union[np.ndarray, _cpp.graph.Adja
     cell_degree = ufl_element.degree()
     cmap = _cpp.fem.CoordinateElement(_uflcell_to_dolfinxcell[cell_shape], cell_degree)
     try:
-        mesh = _cpp.mesh.create_mesh(comm, cells, cmap, x, ghost_mode, partitioner)
+        mesh = _cpp.mesh.create_mesh(comm, cells, cmap, x, partitioner)
     except TypeError:
         mesh = _cpp.mesh.create_mesh(comm, _cpp.graph.AdjacencyList_int64(np.cast['int64'](cells)),
-                                     cmap, x, ghost_mode, partitioner)
+                                     cmap, x, partitioner)
     domain._ufl_cargo = mesh
     return Mesh.from_cpp(mesh, domain)
 
@@ -304,7 +304,7 @@ def meshtags_from_entities(mesh: Mesh, dim: int, entities: _cpp.graph.AdjacencyL
 
 
 def create_interval(comm: _MPI.Comm, nx: int, points: numpy.typing.ArrayLike, ghost_mode=GhostMode.shared_facet,
-                    partitioner=_cpp.mesh.create_cell_partitioner()) -> Mesh:
+                    partitioner=None) -> Mesh:
     """Create an interval mesh
 
     Args:
@@ -320,13 +320,15 @@ def create_interval(comm: _MPI.Comm, nx: int, points: numpy.typing.ArrayLike, gh
         An interval mesh
 
     """
+    if partitioner is None:
+        partitioner = _cpp.mesh.create_cell_partitioner()
     domain = ufl.Mesh(ufl.VectorElement("Lagrange", "interval", 1))
     mesh = _cpp.mesh.create_interval(comm, nx, points, ghost_mode, partitioner)
     return Mesh.from_cpp(mesh, domain)
 
 
 def create_unit_interval(comm: _MPI.Comm, nx: int, ghost_mode=GhostMode.shared_facet,
-                         partitioner=_cpp.mesh.create_cell_partitioner()) -> Mesh:
+                         partitioner=None) -> Mesh:
     """Create a mesh on the unit interval
 
     Args:
@@ -342,12 +344,14 @@ def create_unit_interval(comm: _MPI.Comm, nx: int, ghost_mode=GhostMode.shared_f
         A unit interval mesh with end points at 0 and 1
 
     """
+    if partitioner is None:
+        partitioner = _cpp.mesh.create_cell_partitioner(ghost_mode)
     return create_interval(comm, nx, [0.0, 1.0], ghost_mode, partitioner)
 
 
 def create_rectangle(comm: _MPI.Comm, points: numpy.typing.ArrayLike, n: numpy.typing.ArrayLike,
                      cell_type=CellType.triangle, ghost_mode=GhostMode.shared_facet,
-                     partitioner=_cpp.mesh.create_cell_partitioner(),
+                     partitioner=None,
                      diagonal: DiagonalType = DiagonalType.right) -> Mesh:
     """Create rectangle mesh
 
@@ -368,13 +372,15 @@ def create_rectangle(comm: _MPI.Comm, points: numpy.typing.ArrayLike, n: numpy.t
         A mesh of a rectangle
 
     """
+    if partitioner is None:
+        partitioner = _cpp.mesh.create_cell_partitioner(ghost_mode)
     domain = ufl.Mesh(ufl.VectorElement("Lagrange", cell_type.name, 1))
-    mesh = _cpp.mesh.create_rectangle(comm, points, n, cell_type, ghost_mode, partitioner, diagonal)
+    mesh = _cpp.mesh.create_rectangle(comm, points, n, cell_type, partitioner, diagonal)
     return Mesh.from_cpp(mesh, domain)
 
 
 def create_unit_square(comm: _MPI.Comm, nx: int, ny: int, cell_type=CellType.triangle,
-                       ghost_mode=GhostMode.shared_facet, partitioner=_cpp.mesh.create_cell_partitioner(),
+                       ghost_mode=GhostMode.shared_facet, partitioner=None,
                        diagonal: DiagonalType = DiagonalType.right) -> Mesh:
     """Create a mesh of a unit square
 
@@ -393,6 +399,8 @@ def create_unit_square(comm: _MPI.Comm, nx: int, ny: int, cell_type=CellType.tri
         A mesh of a square with corners at (0, 0) and (1, 1)
 
     """
+    if partitioner is None:
+        partitioner = _cpp.mesh.create_cell_partitioner(ghost_mode)
     return create_rectangle(comm, [np.array([0.0, 0.0]),
                                    np.array([1.0, 1.0])], [nx, ny], cell_type, ghost_mode,
                             partitioner, diagonal)
@@ -401,7 +409,7 @@ def create_unit_square(comm: _MPI.Comm, nx: int, ny: int, cell_type=CellType.tri
 def create_box(comm: _MPI.Comm, points: typing.List[numpy.typing.ArrayLike], n: list,
                cell_type=CellType.tetrahedron,
                ghost_mode=GhostMode.shared_facet,
-               partitioner=_cpp.mesh.create_cell_partitioner()) -> Mesh:
+               partitioner=None) -> Mesh:
     """Create box mesh
 
     Args:
@@ -418,13 +426,15 @@ def create_box(comm: _MPI.Comm, points: typing.List[numpy.typing.ArrayLike], n: 
         A mesh of a box domain
 
     """
+    if partitioner is None:
+        partitioner = _cpp.mesh.create_cell_partitioner(ghost_mode)
     domain = ufl.Mesh(ufl.VectorElement("Lagrange", cell_type.name, 1))
-    mesh = _cpp.mesh.create_box(comm, points, n, cell_type, ghost_mode, partitioner)
+    mesh = _cpp.mesh.create_box(comm, points, n, cell_type, partitioner)
     return Mesh.from_cpp(mesh, domain)
 
 
 def create_unit_cube(comm: _MPI.Comm, nx: int, ny: int, nz: int, cell_type=CellType.tetrahedron,
-                     ghost_mode=GhostMode.shared_facet, partitioner=_cpp.mesh.create_cell_partitioner()) -> Mesh:
+                     ghost_mode=GhostMode.shared_facet, partitioner=None) -> Mesh:
     """Create a mesh of a unit cube
 
     Args:
@@ -442,5 +452,7 @@ def create_unit_cube(comm: _MPI.Comm, nx: int, ny: int, nz: int, cell_type=CellT
         and (1, 1, 1)
 
     """
+    if partitioner is None:
+        partitioner = _cpp.mesh.create_cell_partitioner(ghost_mode)
     return create_box(comm, [np.array([0.0, 0.0, 0.0]), np.array(
         [1.0, 1.0, 1.0])], [nx, ny, nz], cell_type, ghost_mode, partitioner)

.github/workflows/intel.yml
@@ -47,6 +47,7 @@ jobs:
         with:
           path: ./ffcx
           repository: FEniCS/ffcx
+          ref: main
 
       - name: Install FFCx C interface
         run: |

cpp/demo/hyperelasticity/main.cpp
@@ -129,9 +129,10 @@ int main(int argc, char* argv[])
     // .. code-block:: cpp
 
     // Create mesh and define function space
-    auto mesh = std::make_shared<mesh::Mesh>(mesh::create_box(
-        MPI_COMM_WORLD, {{{0.0, 0.0, 0.0}, {1.0, 1.0, 1.0}}}, {10, 10, 10},
-        mesh::CellType::tetrahedron, mesh::GhostMode::none));
+    auto mesh = std::make_shared<mesh::Mesh>(
+        mesh::create_box(MPI_COMM_WORLD, {{{0.0, 0.0, 0.0}, {1.0, 1.0, 1.0}}},
+                         {10, 10, 10}, mesh::CellType::tetrahedron,
+                         mesh::create_cell_partitioner(mesh::GhostMode::none)));
 
     auto V = std::make_shared<fem::FunctionSpace>(fem::create_functionspace(
         functionspace_form_hyperelasticity_F_form, "u", mesh));

cpp/demo/interpolation-io/main.cpp
@@ -150,7 +150,8 @@ int main(int argc, char* argv[])
     // ensure that a boundary between cells is located at x0=0.5
     auto mesh = std::make_shared<mesh::Mesh>(mesh::create_rectangle(
         MPI_COMM_WORLD, {{{0.0, 0.0}, {1.0, 1.0}}}, {32, 4},
-        mesh::CellType::triangle, mesh::GhostMode::none));
+        mesh::CellType::triangle,
+        mesh::create_cell_partitioner(mesh::GhostMode::none)));
 
     // Interpolate a function in a scalar Lagrange space and output the
     // result to file for visualisation

cpp/demo/poisson/main.cpp
@@ -120,9 +120,10 @@ int main(int argc, char* argv[])
 
   {
     // Create mesh and function space
-    auto mesh = std::make_shared<mesh::Mesh>(mesh::create_rectangle(
-        MPI_COMM_WORLD, {{{0.0, 0.0}, {2.0, 1.0}}}, {32, 16},
-        mesh::CellType::triangle, mesh::GhostMode::shared_facet));
+    auto part = mesh::create_cell_partitioner(mesh::GhostMode::shared_facet);
+    auto mesh = std::make_shared<mesh::Mesh>(
+        mesh::create_rectangle(MPI_COMM_WORLD, {{{0.0, 0.0}, {2.0, 1.0}}},
+                               {32, 16}, mesh::CellType::triangle, part));
 
     auto V = std::make_shared<fem::FunctionSpace>(
         fem::create_functionspace(functionspace_form_poisson_a, "u", mesh));

cpp/demo/poisson_matrix_free/main.cpp
@@ -126,7 +126,7 @@ int main(int argc, char* argv[])
     // Create mesh and function space
     auto mesh = std::make_shared<mesh::Mesh>(mesh::create_rectangle(
         comm, {{{0.0, 0.0}, {1.0, 1.0}}}, {10, 10}, mesh::CellType::triangle,
-        mesh::GhostMode::none));
+        mesh::create_cell_partitioner(mesh::GhostMode::none)));
     auto V = std::make_shared<fem::FunctionSpace>(
         fem::create_functionspace(functionspace_form_poisson_M, "ui", mesh));
 

cpp/dolfinx/common/MPI.h
@@ -439,7 +439,7 @@ distribute_to_postoffice(MPI_Comm comm, const std::span<const T>& x,
                  index_local.begin(), [r0](auto idx) { return idx - r0; });
 
   return {index_local, recv_buffer_data};
-};
+}
 //---------------------------------------------------------------------------
 template <typename T>
 std::vector<T> distribute_from_postoffice(

cpp/dolfinx/common/Scatterer.cpp
@@ -167,12 +167,12 @@ void Scatterer::scatter_rev_end(MPI_Request& request) const
 std::int32_t Scatterer::local_buffer_size() const noexcept
 {
   return _local_inds.size();
-};
+}
 //-----------------------------------------------------------------------------
 std::int32_t Scatterer::remote_buffer_size() const noexcept
 {
   return _remote_inds.size();
-};
+}
 //-----------------------------------------------------------------------------
 const std::vector<std::int32_t>& Scatterer::local_indices() const noexcept
 {

commit 64e6ce32da976a15b77bb541419cf76c576d0185

Fix regression introduced in #2337 for dolfinx.mesh.create_interval

python/dolfinx/mesh.py
@@ -321,7 +321,7 @@ def create_interval(comm: _MPI.Comm, nx: int, points: numpy.typing.ArrayLike, gh
 
     """
     if partitioner is None:
-        partitioner = _cpp.mesh.create_cell_partitioner()
+        partitioner = _cpp.mesh.create_cell_partitioner(ghost_mode)
     domain = ufl.Mesh(ufl.VectorElement("Lagrange", "interval", 1))
     mesh = _cpp.mesh.create_interval(comm, nx, points, ghost_mode, partitioner)
     return Mesh.from_cpp(mesh, domain)