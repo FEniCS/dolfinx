5b6a16667251f1fa8da5e26bf1ac7d3bdc4cc8c4
Garth N. Wells <gnw20@cam.ac.uk>
2022-10-26 10:21:23 +0100

    Minor setup.py improvements

--- python/setup.py
+++ python/setup.py
@@ -3,16 +3,12 @@ import shlex
 import subprocess
 import sys
 import sysconfig
+from pathlib import Path
 
 from setuptools import Extension, setup
 from setuptools.command.build_ext import build_ext
 
-if sys.version_info < (3, 8):
-    print("Python 3.8 or higher required, please upgrade.")
-    sys.exit(1)
-
 VERSION = "0.5.2.dev0"
-
 REQUIREMENTS = [
     "cffi",
     "numpy>=1.21",
@@ -24,9 +20,9 @@ REQUIREMENTS = [
 
 
 class CMakeExtension(Extension):
-    def __init__(self, name, sourcedir=''):
-        Extension.__init__(self, name, sources=[])
-        self.sourcedir = os.path.abspath(sourcedir)
+    def __init__(self, name: str, sourcedir: str = "") -> None:
+        super().__init__(name, sources=[])
+        self.sourcedir = os.fspath(Path(sourcedir).resolve())
 
 
 class CMakeBuild(build_ext):
@@ -36,34 +32,36 @@ class CMakeBuild(build_ext):
         except OSError:
             raise RuntimeError("CMake must be installed to build the following extensions: "
                                + ", ".join(e.name for e in self.extensions))
-
         for ext in self.extensions:
             self.build_extension(ext)
 
     def build_extension(self, ext):
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
         cmake_args = shlex.split(os.environ.get("CMAKE_ARGS", ""))
-        cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                       '-DPython3_EXECUTABLE=' + sys.executable,
-                       f'-DPython3_LIBRARIES={sysconfig.get_config_var("LIBDEST")}',
+        cmake_args += [f'-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}',
+                       f'-DPython3_EXECUTABLE={sys.executable}',
                        f'-DPython3_INCLUDE_DIRS={sysconfig.get_config_var("INCLUDEPY")}']
+
+        # if "pybind11_DIR" not in os.environ:
+        import pybind11
+        cmake_args += [f'-DPYBIND11_DIR={pybind11.get_cmake_dir()}']
 
         cfg = 'Debug' if self.debug else 'Release'
         build_args = ['--config', cfg]
         cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
 
-        env = os.environ.copy()
-        # default to 3 build threads
-        if "CMAKE_BUILD_PARALLEL_LEVEL" not in env:
-            env["CMAKE_BUILD_PARALLEL_LEVEL"] = "3"
+        # Default to 3 build threads
+        if "CMAKE_BUILD_PARALLEL_LEVEL" not in os.environ:
+            build_args += ['-j3']
 
-        import pybind11
-        env['pybind11_DIR'] = pybind11.get_cmake_dir()
+        # import pybind11
+        # env = os.environ.copy()
+        # env['pybind11_DIR'] = pybind11.get_cmake_dir()
 
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
-        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
-        subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp, env=env)
+        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp)
+        subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
 
 
 setup(name='fenics-dolfinx',
@@ -81,5 +79,5 @@ setup(name='fenics-dolfinx',
       ext_modules=[CMakeExtension('dolfinx.cpp')],
       cmdclass=dict(build_ext=CMakeBuild),
       install_requires=REQUIREMENTS,
-      setup_requires=["pybind11"],
-      zip_safe=False)
+      zip_safe=False,
+      python_requires=">=3.8",)
