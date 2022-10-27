3410e01d3045386fb30b1f5bb73fe7aa1706104e
Jack S. Hale <mail@jackhale.co.uk>
2022-10-27 10:12:23 +0200

    Isolated builds causing issues.

--- python/setup.py
+++ python/setup.py
@@ -8,16 +8,6 @@ from pathlib import Path
 from setuptools import Extension, setup
 from setuptools.command.build_ext import build_ext
 
-VERSION = "0.5.2.dev0"
-REQUIREMENTS = [
-    "cffi",
-    "numpy>=1.21",
-    "mpi4py",
-    "petsc4py",
-    "fenics-ffcx>=0.5.1.dev0,<0.6.0",
-    "fenics-ufl>=2022.3.0.dev0,<2022.4.0"
-]
-
 
 class CMakeExtension(Extension):
     def __init__(self, name: str, sourcedir: str = "") -> None:
@@ -42,9 +32,14 @@ class CMakeBuild(build_ext):
                        f'-DPython3_EXECUTABLE={sys.executable}',
                        f'-DPython3_INCLUDE_DIRS={sysconfig.get_config_var("INCLUDEPY")}']
 
-        # if "pybind11_DIR" not in os.environ:
-        import pybind11
-        cmake_args += [f'-DPYBIND11_DIR={pybind11.get_cmake_dir()}']
+        # TODO: Not sure about the logic of this. cmake needs to be able to
+        # just find C++ pybind11 if its a non-Python install.
+        if "pybind11_DIR" not in os.environ:
+            try:
+                import pybind11
+                cmake_args += [f'-DPYBIND11_DIR={pybind11.get_cmake_dir()}']
+            except ImportError:
+                pass
 
         cfg = 'Debug' if self.debug else 'Release'
         build_args = ['--config', cfg]
@@ -54,30 +49,11 @@ class CMakeBuild(build_ext):
         if "CMAKE_BUILD_PARALLEL_LEVEL" not in os.environ:
             build_args += ['-j3']
 
-        # import pybind11
-        # env = os.environ.copy()
-        # env['pybind11_DIR'] = pybind11.get_cmake_dir()
-
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
         subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp)
         subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
 
 
-setup(name='fenics-dolfinx',
-      version=VERSION,
-      author='FEniCS Project',
-      description='DOLFINx Python interface',
-      long_description='',
-      packages=["dolfinx",
-                "dolfinx.fem",
-                "dolfinx.io",
-                "dolfinx.nls",
-                "dolfinx.wrappers"],
-      package_data={'dolfinx.wrappers': ['*.h'], 'dolfinx': ['py.typed'],
-                    'dolfinx.fem': ['py.typed'], 'dolfinx.nls': ['py.typed']},
-      ext_modules=[CMakeExtension('dolfinx.cpp')],
-      cmdclass=dict(build_ext=CMakeBuild),
-      install_requires=REQUIREMENTS,
-      zip_safe=False,
-      python_requires=">=3.8",)
+setup(ext_modules=[CMakeExtension('dolfinx.cpp')],
+      cmdclass=dict(build_ext=CMakeBuild))
