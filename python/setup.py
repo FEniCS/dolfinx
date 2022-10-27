f1be43f7b6bed8f06ce5a05db0b66662f0107276
Jack S. Hale <mail@jackhale.co.uk>
2022-10-27 11:47:26 +0200

    Bump version numbers

--- python/setup.py
+++ python/setup.py
@@ -11,7 +11,7 @@ if sys.version_info < (3, 8):
     print("Python 3.8 or higher required, please upgrade.")
     sys.exit(1)
 
-VERSION = "0.5.2.dev0"
+VERSION = "0.6.0.dev0"
 
 REQUIREMENTS = [
     "cffi",
