93192bcf00104ac68bee71e51ebbc2ae35ffb264
tiagovla <tiagovla@gmail.com>
2022-11-07 06:50:26 -0300

    ci: format find cmake modules

--- cpp/cmake/modules/FindKaHIP.cmake
+++ cpp/cmake/modules/FindKaHIP.cmake
@@ -1,68 +1,67 @@
-# - Try to find KaHIP
-# Once done this will define
+# * Try to find KaHIP Once done this will define
 #
-#  KAHIP_FOUND        - system has KaHIP
-#  KAHIP_INCLUDE_DIRS - include directories for KaHIP
-#  KAHIP_LIBRARIES    - libraries for KaHIP
-#  KAHIP_VERSION      - version for KaHIP
-#=============================================================================
-# Copyright (C) 2019 Igor A. Baratta
-# All rights reserved.
+# KAHIP_FOUND - system has KaHIP KAHIP_INCLUDE_DIRS - include directories for
+# KaHIP KAHIP_LIBRARIES - libraries for KaHIP KAHIP_VERSION - version for
+# KaHIP
+# =============================================================================
+# Copyright (C) 2019 Igor A. Baratta All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
+# modification, are permitted provided that the following conditions are met:
 #
-# 1. Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in
-#    the documentation and/or other materials provided with the
-#    distribution.
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
 #
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#=============================================================================
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# =============================================================================
 
 set(KAHIP_FOUND FALSE)
 
 message(STATUS "Checking for package 'KaHIP'")
 
-if (MPI_CXX_FOUND)
-  find_path(KAHIP_INCLUDE_DIRS parhip_interface.h
-    HINTS ${KAHIP_DIR}/include $ENV{KAHIP_DIR}/include PATH_SUFFIXES kahip)
-  find_library(PARHIP_LIBRARY parhip_interface HINTS ${KAHIP_DIR}/lib $ENV{KAHIP_DIR}/lib)
+if(MPI_CXX_FOUND)
+  find_path(
+    KAHIP_INCLUDE_DIRS parhip_interface.h
+    HINTS ${KAHIP_DIR}/include $ENV{KAHIP_DIR}/include
+    PATH_SUFFIXES kahip
+  )
+  find_library(
+    PARHIP_LIBRARY parhip_interface HINTS ${KAHIP_DIR}/lib $ENV{KAHIP_DIR}/lib
+  )
   find_library(KAHIP_LIBRARY kahip HINTS ${KAHIP_DIR}/lib $ENV{KAHIP_DIR}/lib)
 
   set(KAHIP_LIBRARIES ${PARHIP_LIBRARY} ${KAHIP_LIBRARY})
 
-  include (FindPackageHandleStandardArgs)
-  if (DOLFINX_SKIP_BUILD_TESTS)
-    find_package_handle_standard_args(KaHIP
-                                     "KaHIP could not be found/configured."
-                                      KAHIP_INCLUDE_DIRS
-                                      KAHIP_LIBRARIES)
+  include(FindPackageHandleStandardArgs)
+  if(DOLFINX_SKIP_BUILD_TESTS)
+    find_package_handle_standard_args(
+      KaHIP "KaHIP could not be found/configured." KAHIP_INCLUDE_DIRS
+      KAHIP_LIBRARIES
+    )
   else()
-    if (KAHIP_LIBRARIES AND KAHIP_LIBRARIES)
+    if(KAHIP_LIBRARIES AND KAHIP_LIBRARIES)
 
       # Build and run test program
       include(CheckCXXSourceRuns)
 
       # Set flags for building test program
-      set(CMAKE_REQUIRED_INCLUDES  ${KAHIP_INCLUDE_DIRS} ${MPI_CXX_INCLUDE_PATH})
+      set(CMAKE_REQUIRED_INCLUDES ${KAHIP_INCLUDE_DIRS} ${MPI_CXX_INCLUDE_PATH})
       set(CMAKE_REQUIRED_LIBRARIES ${KAHIP_LIBRARIES} ${MPI_CXX_LIBRARIES})
       set(CMAKE_REQUIRED_FLAGS ${CMAKE_REQUIRED_FLAGS} ${MPI_CXX_COMPILE_FLAGS})
-      check_cxx_source_runs("
+      check_cxx_source_runs(
+        "
         #define MPICH_IGNORE_CXX_SEEK 1
         #include <mpi.h>
         #include <vector>
@@ -83,11 +82,13 @@ if (MPI_CXX_FOUND)
                  part.data());
         return 0;
         }
-        " KAHIP_TEST_RUNS)
+        "
+        KAHIP_TEST_RUNS
+      )
     endif()
-    find_package_handle_standard_args(KaHIP "KaHIP could not be found/configured."
-                                      KAHIP_INCLUDE_DIRS
-                                      KAHIP_LIBRARIES
-                                      KAHIP_TEST_RUNS)
+    find_package_handle_standard_args(
+      KaHIP "KaHIP could not be found/configured." KAHIP_INCLUDE_DIRS
+      KAHIP_LIBRARIES KAHIP_TEST_RUNS
+    )
   endif()
 endif()
--- cpp/cmake/modules/FindUFCx.cmake
+++ cpp/cmake/modules/FindUFCx.cmake
@@ -1,66 +1,71 @@
-# - Find UFCx by interrogating the Python module FFCx
+# * Find UFCx by interrogating the Python module FFCx
 #
 # Once done this will define
 #
 # This module defines
 #
-#  UFCX_FOUND        - system has UFCx
-#  UFCX_INCLUDE_DIRS - where to find ufcx.h
-#  UFCX_VERSION      - UFC version
-#  UFCX_SIGNATURE    - UFC signature
+# UFCX_FOUND   - system has UFCx    UFCX_INCLUDE_DIRS - where to find ufcx.h
+# UFCX_VERSION - UFC version        UFCX_SIGNATURE    - UFC signature
 
-#=============================================================================
-# Copyright (C) 2010-2021 Johannes Ring and Garth N. Wells
-# All rights reserved.
+# =============================================================================
+# Copyright (C) 2010-2021 Johannes Ring and Garth N. Wells All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
+# modification, are permitted provided that the following conditions are met:
 #
-# 1. Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in
-#    the documentation and/or other materials provided with the
-#    distribution.
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
 #
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#=============================================================================
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# =============================================================================
 
-MESSAGE(STATUS "Asking Python module FFCx for location of UFC... (Python executable: ${Python3_EXECUTABLE})")
+message(
+  STATUS
+    "Asking Python module FFCx for location of UFC... (Python executable: ${Python3_EXECUTABLE})"
+)
 
 # Get include path
 execute_process(
-  COMMAND ${Python3_EXECUTABLE} -c "import ffcx.codegeneration, sys; sys.stdout.write(ffcx.codegeneration.get_include_path())"
-  OUTPUT_VARIABLE UFCX_INCLUDE_DIR)
+  COMMAND
+    ${Python3_EXECUTABLE} -c
+    "import ffcx.codegeneration, sys; sys.stdout.write(ffcx.codegeneration.get_include_path())"
+  OUTPUT_VARIABLE UFCX_INCLUDE_DIR
+)
 
 # Get ufcx.h version
-if (UFCX_INCLUDE_DIR)
-  set(UFCX_INCLUDE_DIRS ${UFCX_INCLUDE_DIR} CACHE STRING "Where to find ufcx.h")
+if(UFCX_INCLUDE_DIR)
+  set(UFCX_INCLUDE_DIRS
+      ${UFCX_INCLUDE_DIR}
+      CACHE STRING "Where to find ufcx.h"
+  )
   execute_process(
-    COMMAND ${Python3_EXECUTABLE} -c "import ffcx, sys; sys.stdout.write(ffcx.__version__)"
-    OUTPUT_VARIABLE UFCX_VERSION)
+    COMMAND ${Python3_EXECUTABLE} -c
+            "import ffcx, sys; sys.stdout.write(ffcx.__version__)"
+    OUTPUT_VARIABLE UFCX_VERSION
+  )
 endif()
 
 # Compute hash of ufcx.h
 find_file(_UFCX_HEADER "ufcx.h" ${UFCX_INCLUDE_DIR})
-if (_UFCX_HEADER)
+if(_UFCX_HEADER)
   file(SHA1 ${_UFCX_HEADER} UFCX_SIGNATURE)
 endif()
 
 mark_as_advanced(UFCX_VERSION UFCX_INCLUDE_DIRS UFCX_SIGNATURE)
-find_package_handle_standard_args(UFCx
-                                  REQUIRED_VARS UFCX_INCLUDE_DIRS UFCX_SIGNATURE UFCX_VERSION
-                                  VERSION_VAR UFCX_VERSION
-                                  REASON_FAILURE_MESSAGE "UFCx could not be found.")
+find_package_handle_standard_args(
+  UFCx
+  REQUIRED_VARS UFCX_INCLUDE_DIRS UFCX_SIGNATURE UFCX_VERSION
+  VERSION_VAR UFCX_VERSION REASON_FAILURE_MESSAGE "UFCx could not be found."
+)
