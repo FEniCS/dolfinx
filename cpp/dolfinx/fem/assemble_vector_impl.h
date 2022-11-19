2be03d6d1241037b41a341284ff42fecf90206d1
Garth N. Wells <gnw20@cam.ac.uk>
2022-11-19 21:10:56 +0000

    Doc fixes

--- cpp/dolfinx/fem/assemble_vector_impl.h
+++ cpp/dolfinx/fem/assemble_vector_impl.h
@@ -715,6 +715,7 @@ void assemble_interior_facets(
 ///
 /// @param[in,out] b The vector to be modified
 /// @param[in] a The bilinear form that generates A
+/// @param[in] geometry The mesh geometry
 /// @param[in] constants Constants that appear in `a`
 /// @param[in] coefficients Coefficients that appear in `a`
 /// @param[in] bc_values1 The boundary condition 'values'
@@ -854,6 +855,7 @@ void lift_bc(std::span<T> b, const Form<T>& a,
 /// @param[in,out] b The vector to be modified
 /// @param[in] a The bilinear forms, where a[j] is the form that
 /// generates A_j
+/// @param[in] geometry The mesh geometry
 /// @param[in] constants Constants that appear in `a`
 /// @param[in] coeffs Coefficients that appear in `a`
 /// @param[in] bcs1 List of boundary conditions for each block, i.e.
@@ -924,6 +926,7 @@ void apply_lifting(
 /// @param[in,out] b The vector to be assembled. It will not be zeroed
 /// before assembly.
 /// @param[in] L The linear forms to assemble into b
+/// @param[in] geometry The mesh geometry
 /// @param[in] constants Packed constants that appear in `L`
 /// @param[in] coefficients Packed coefficients that appear in `L`
 template <typename T>
