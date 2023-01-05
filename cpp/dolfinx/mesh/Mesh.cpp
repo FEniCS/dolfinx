46f2ed9a11afcea3a1134aec7153fc80f9fb31a8
Joe Dean <jpd62@cam.ac.uk>
2023-01-05 13:18:43 +0000

    Format

--- cpp/dolfinx/mesh/Mesh.cpp
+++ cpp/dolfinx/mesh/Mesh.cpp
@@ -327,20 +327,18 @@ mesh::create_submesh(const Mesh& mesh, int dim,
     std::vector<std::int64_t> submesh_e_to_global_v_vec(
         submesh_e_to_v_vec.size(), 0);
     submesh_map0->local_to_global(submesh_e_to_v_vec,
-                                              submesh_e_to_global_v_vec);
+                                  submesh_e_to_global_v_vec);
 
     // Scatter forward to send global vertices of owned entity vertices
     // to ghost entities
-    common::Scatterer scatterer(*submesh_map,
-                                num_vertices_per_entity);
+    common::Scatterer scatterer(*submesh_map, num_vertices_per_entity);
     std::vector<std::int64_t> ghost_vertices(
         num_vertices_per_entity * submesh_map->num_ghosts(), 0);
     scatterer.scatter_fwd(
-        std::span<const std::int64_t>(
-            submesh_e_to_global_v_vec.begin(),
-            submesh_e_to_global_v_vec.begin()
-                + num_vertices_per_entity
-                      * submesh_map->size_local()),
+        std::span<const std::int64_t>(submesh_e_to_global_v_vec.begin(),
+                                      submesh_e_to_global_v_vec.begin()
+                                          + num_vertices_per_entity
+                                                * submesh_map->size_local()),
         std::span<std::int64_t>(ghost_vertices));
 
     // Convert received global vertices back to local numbering and overwrite
@@ -349,8 +347,7 @@ mesh::create_submesh(const Mesh& mesh, int dim,
         submesh_e_to_v_vec.begin()
             + num_vertices_per_entity * submesh_map->size_local(),
         submesh_e_to_v_vec.end());
-    submesh_map0->global_to_local(ghost_vertices,
-                                              ghost_vertices_local);
+    submesh_map0->global_to_local(ghost_vertices, ghost_vertices_local);
   }
 
   auto submesh_e_to_v = std::make_shared<graph::AdjacencyList<std::int32_t>>(
@@ -501,10 +498,10 @@ mesh::create_submesh(const Mesh& mesh, int dim,
         x_dofs_per_entity * submesh_map->num_ghosts(), 0);
 
     scatterer.scatter_fwd(
-        std::span<const std::int64_t>(
-            submesh_xdofmap_global_vec.begin(),
-            submesh_xdofmap_global_vec.begin()
-                + x_dofs_per_entity * submesh_map->size_local()),
+        std::span<const std::int64_t>(submesh_xdofmap_global_vec.begin(),
+                                      submesh_xdofmap_global_vec.begin()
+                                          + x_dofs_per_entity
+                                                * submesh_map->size_local()),
         std::span<std::int64_t>(ghost_x_dofs));
 
     std::span<std::int32_t> ghost_x_dofs_local(
