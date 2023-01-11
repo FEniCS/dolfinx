e197dd601b709d35d43b43b45318430a5da9edea
Joe Dean <jpd62@cam.ac.uk>
2023-01-11 14:06:53 +0000

    Generalise test

--- cpp/demo/mixed_topology/main.cpp
+++ cpp/demo/mixed_topology/main.cpp
@@ -1,38 +1,96 @@
+#include <dolfinx/common/MPI.h>
+#include <dolfinx/fem/petsc.h>
+#include <dolfinx/graph/AdjacencyList.h>
+#include <dolfinx/mesh/Topology.h>
+#include <dolfinx/mesh/cell_types.h>
 #include <iostream>
 #include <vector>
-#include <dolfinx/mesh/Topology.h>
-#include <dolfinx/graph/AdjacencyList.h>
-#include <dolfinx/common/MPI.h>
-#include <dolfinx/mesh/cell_types.h>
-#include <dolfinx/fem/petsc.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    dolfinx::init_logging(argc, argv);
-    PetscInitialize(&argc, &argv, nullptr, nullptr);
+  dolfinx::init_logging(argc, argv);
+  PetscInitialize(&argc, &argv, nullptr, nullptr);
 
-    std::vector<std::int64_t> cells{0, 1, 2, 3,
-                                    2, 3, 4};
-    std::vector<std::int32_t> offsets{0, 4, 7};
-    dolfinx::graph::AdjacencyList<std::int64_t> cells_list(cells, offsets);
+  const int nx_s = 2;
+  const int nx_t = 2;
+  const int ny = 4;
 
-    std::vector<std::int64_t> original_global_index{0, 1};
+  const int num_s = nx_s * ny;
+  const int num_t = 2 * nx_t * ny;
 
-    std::vector<int> ghost_owners;
+  std::vector<std::int64_t> cells;
+  std::vector<std::int32_t> offsets{0};
+  for (int i = 0; i < nx_s + nx_t; ++i)
+  {
+    for (int j = 0; j < ny; ++j)
+    {
+      const int v_0 = j + i * (ny + 1);
+      const int v_1 = v_0 + 1;
+      const int v_2 = v_0 + ny + 1;
+      const int v_3 = v_0 + ny + 2;
 
-    std::vector<std::int32_t> cell_group_offsets{0, 1, 1, 1};
+      if (i < nx_s)
+      {
+        cells.push_back(v_0);
+        cells.push_back(v_1);
+        cells.push_back(v_3);
+        cells.push_back(v_2);
 
-    std::vector<std::int64_t> boundary_vertices = cells;
+        offsets.push_back(cells.size());
+      }
+      else
+      {
+        cells.push_back(v_0);
+        cells.push_back(v_1);
+        cells.push_back(v_2);
 
-    std::vector<dolfinx::mesh::CellType> cell_types{
-        dolfinx::mesh::CellType::quadrilateral,
-        dolfinx::mesh::CellType::triangle};
+        offsets.push_back(cells.size());
 
-    dolfinx::mesh::create_topology(
-        MPI_COMM_WORLD, cells_list, original_global_index, ghost_owners, cell_types,
-        cell_group_offsets, boundary_vertices);
+        cells.push_back(v_1);
+        cells.push_back(v_2);
+        cells.push_back(v_3);
 
-    PetscFinalize();
+        offsets.push_back(cells.size());
+      }
+    }
+  }
 
-    return 0;
+  dolfinx::graph::AdjacencyList<std::int64_t> cells_list(cells, offsets);
+
+  std::vector<std::int64_t> original_global_index(num_s + num_t);
+  std::iota(original_global_index.begin(), original_global_index.end(), 0);
+
+  std::vector<int> ghost_owners;
+
+  std::vector<std::int32_t> cell_group_offsets{0, num_s, num_t, num_s + num_t,
+                                               num_s + num_t};
+
+  std::vector<std::int64_t> boundary_vertices;
+  for (int j = 0; j < ny + 1; ++j)
+  {
+    boundary_vertices.push_back(j);
+    boundary_vertices.push_back(j + (nx_s + nx_t + 1) * ny);
+  }
+  for (int i = 0; i < nx_s + nx_t + 1; ++i)
+  {
+    boundary_vertices.push_back((ny + 1) * i);
+    boundary_vertices.push_back(ny + (ny + 1) * i);
+  }
+
+  std::sort(boundary_vertices.begin(), boundary_vertices.end());
+  boundary_vertices.erase(
+      std::unique(boundary_vertices.begin(), boundary_vertices.end()),
+      boundary_vertices.end());
+
+  std::vector<dolfinx::mesh::CellType> cell_types{
+      dolfinx::mesh::CellType::quadrilateral,
+      dolfinx::mesh::CellType::triangle};
+
+  dolfinx::mesh::create_topology(
+      MPI_COMM_WORLD, cells_list, original_global_index, ghost_owners,
+      cell_types, cell_group_offsets, boundary_vertices);
+
+  PetscFinalize();
+
+  return 0;
 }
