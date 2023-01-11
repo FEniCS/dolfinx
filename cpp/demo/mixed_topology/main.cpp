e0c4d7173ea1ee8d464923156023cef7efb9b8a8
IgorBaratta <igorbaratta@gmail.com>
2023-01-11 12:22:26 +0000

    remove petsc from demo

--- cpp/demo/mixed_topology/main.cpp
+++ cpp/demo/mixed_topology/main.cpp
@@ -1,38 +1,36 @@
+#include <dolfinx/common/MPI.h>
+#include <dolfinx/graph/AdjacencyList.h>
+#include <dolfinx/mesh/Topology.h>
+#include <dolfinx/mesh/cell_types.h>
 #include <iostream>
 #include <vector>
-#include <dolfinx/mesh/Topology.h>
-#include <dolfinx/graph/AdjacencyList.h>
-#include <dolfinx/common/MPI.h>
-#include <dolfinx/mesh/cell_types.h>
-#include <dolfinx/fem/petsc.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    dolfinx::init_logging(argc, argv);
-    PetscInitialize(&argc, &argv, nullptr, nullptr);
+  dolfinx::init_logging(argc, argv);
+  MPI_Init(&argc, &argv);
 
-    std::vector<std::int64_t> cells{0, 1, 2, 3,
-                                    2, 3, 4};
-    std::vector<std::int32_t> offsets{0, 4, 7};
-    dolfinx::graph::AdjacencyList<std::int64_t> cells_list(cells, offsets);
+  std::vector<std::int64_t> cells{0, 1, 2, 3, 2, 3, 4};
+  std::vector<std::int32_t> offsets{0, 4, 7};
+  dolfinx::graph::AdjacencyList<std::int64_t> cells_list(cells, offsets);
 
-    std::vector<std::int64_t> original_global_index{0, 1};
+  std::vector<std::int64_t> original_global_index{0, 1};
 
-    std::vector<int> ghost_owners;
+  std::vector<int> ghost_owners;
 
-    std::vector<std::int32_t> cell_group_offsets{0, 1, 1, 1};
+  std::vector<std::int32_t> cell_group_offsets{0, 1, 1, 1};
 
-    std::vector<std::int64_t> boundary_vertices = cells;
+  std::vector<std::int64_t> boundary_vertices = cells;
 
-    std::vector<dolfinx::mesh::CellType> cell_types{
-        dolfinx::mesh::CellType::quadrilateral,
-        dolfinx::mesh::CellType::triangle};
+  std::vector<dolfinx::mesh::CellType> cell_types{
+      dolfinx::mesh::CellType::quadrilateral,
+      dolfinx::mesh::CellType::triangle};
 
-    dolfinx::mesh::create_topology(
-        MPI_COMM_WORLD, cells_list, original_global_index, ghost_owners, cell_types,
-        cell_group_offsets, boundary_vertices);
+  dolfinx::mesh::create_topology(
+      MPI_COMM_WORLD, cells_list, original_global_index, ghost_owners,
+      cell_types, cell_group_offsets, boundary_vertices);
 
-    PetscFinalize();
+  MPI_Finalize();
 
-    return 0;
+  return 0;
 }
