641a1ce4341279d302b6f7d2ef82cabafccda743
Julius Herb <43179176+juliusgh@users.noreply.github.com>
2023-01-23 14:07:22 +0100

    Fix isort checks for some demos (#2506)
    * fix isort checks for some demos
    
    * fix flake8 checks
    
    * fix isort checks

--- python/dolfinx/io/utils.py
+++ python/dolfinx/io/utils.py
@@ -15,10 +15,8 @@ import basix
 import basix.ufl_wrapper
 import ufl
 from dolfinx import cpp as _cpp
-
-from dolfinx.cpp.io import perm_vtk as cell_perm_vtk  # noqa F401
 from dolfinx.cpp.io import perm_gmsh as cell_perm_gmsh  # noqa F401
-
+from dolfinx.cpp.io import perm_vtk as cell_perm_vtk  # noqa F401
 from dolfinx.fem import Function
 from dolfinx.mesh import GhostMode, Mesh
 
