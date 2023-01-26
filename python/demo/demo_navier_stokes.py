7f6625b4ca68eef99d851dea2a327cb1139f56e0
Garth N. Wells <gnw20@cam.ac.uk>
2023-01-26 11:10:47 +0000

    Work in docs.
    Need to figure out how to include extra latex packages

--- python/demo/demo_navier_stokes.py
+++ python/demo/demo_navier_stokes.py
@@ -8,142 +8,154 @@
 #       jupytext_version: 1.13.6
 # ---
 
-# # A divergence conforming discontinuous Galerkin method for the Navier-Stokes equations
-# This demo illustrates how to implement a divergence conforming
-# discontinuous Galerkin method for the Navier-Stokes equations in
-# FEniCSx. The method conserves mass exactly and uses upwinding. The
-# formulation is based on a combination of "A fully divergence-free
-# finite element method for magnetohydrodynamic equations" by Hiptmair
-# et al., "A Note on Discontinuous Galerkin Divergence-free Solutions
-# of the Navier-Stokes Equations" by Cockburn et al, and "On the Divergence
-# Constraint in Mixed Finite Element Methods for Incompressible Flows" by
-# John et al.
-
+# # Divergence conforming discontinuous Galerkin method for the Navier--Stokes equations
+#
+# This demo ({download}`demo_navier_stokes.py`) illustrates how to
+# implement a divergence conforming discontinuous Galerkin method for
+# the Navier-Stokes equations in FEniCSx. The method conserves mass
+# exactly and uses upwinding. The formulation is based on a combination
+# of "A fully divergence-free finite element method for
+# magnetohydrodynamic equations" by Hiptmair et al., "A Note on
+# Discontinuous Galerkin Divergence-free Solutions of the Navier-Stokes
+# Equations" by Cockburn et al, and "On the Divergence Constraint in
+# Mixed Finite Element Methods for Incompressible Flows" by John et al.
+#
+#
 # ## Governing equations
+#
 # We consider the incompressible Navier-Stokes equations in a domain
 # $\Omega \subset \mathbb{R}^d$, $d \in \{2, 3\}$, and time interval
 # $(0, \infty)$, given by
+#
 # $$
-#     \partial_t u - \nu \Delta u + (u \cdot \nabla)u + \nabla p = f
-#     \textnormal{ in } \Omega_t,
+# \partial_t u - \nu \Delta u + (u \cdot \nabla)u + \nabla p &= f \textnormal{ in } \Omega_t, \\
+# \nabla \cdot u &= 0 \textnormal{ in } \Omega_t,
 # $$
-# $$
-#     \nabla \cdot u = 0
-#     \textnormal{ in } \Omega_t,
-# $$
+#
 # where $u: \Omega_t \to \mathbb{R}^d$ is the velocity field,
 # $p: \Omega_t \to \mathbb{R}$ is the pressure field,
 # $f: \Omega_t \to \mathbb{R}^d$ is a prescribed force, $\nu \in \mathbb{R}^+$
-# is the kinematic viscosity, and
-# $\Omega_t \coloneqq \Omega \times (0, \infty)$.
-
+# is the kinematic viscosity, and $\Omega_t := \Omega \times (0, \infty)$.
+#
 # The problem is supplemented with the initial condition
+#
 # $$
 #     u(x, 0) = u_0(x) \textnormal{ in } \Omega
 # $$
+#
 # and boundary condition
+#
 # $$
 #     u = u_D \textnormal{ on } \partial \Omega \times (0, \infty),
 # $$
+#
 # where $u_0: \Omega \to \mathbb{R}^d$ is a prescribed initial velocity field
 # which satisfies the divergence free condition. The pressure field is only
 # determined up to a constant, so we seek the unique pressure field satisfying
+#
 # $$
 #     \int_\Omega p = 0.
 # $$
-
+#
+#
+#
 # ## Discrete problem
+#
 # We begin by introducing the function spaces
+#
 # $$
-#     V_h^g \coloneqq \left\{v \in H(\textnormal{div}; \Omega);
+#     V_h^g &:= \left\{v \in H(\textnormal{div}; \Omega);
 #     v|_K \in V_h(K) \; \forall K \in \mathcal{T}, v \cdot n = g \cdot n
-#     \textnormal{ on } \partial \Omega \right\}
-# $$,
-# and
-# $$
-#     Q_h \coloneqq \left\{q \in L^2_0(\Omega);
+#     \textnormal{ on } \partial \Omega \right\} \\
+#     Q_h &:= \left\{q \in L^2_0(\Omega);
 #     q|_K \in Q_h(K) \; \forall K \in \mathcal{T} \right\}.
 # $$
+#
 # The local spaces $V_h(K)$ and $Q_h(K)$ should satisfy
+#
 # $$
 #     \nabla \cdot V_h(K) \subseteq Q_h(K),
 # $$
 # in order for mass to be conserved exactly. Suitable choices on
 # affine simplex cells include
+#
 # $$
-#     V_h(K) \coloneqq \mathbb{RT}_k(K) \textnormal{ and }
-#     Q_h(K) \coloneqq \mathbb{P}_k(K),
+#     V_h(K) := \mathbb{RT}_k(K) \textnormal{ and }
+#     Q_h(K) := \mathbb{P}_k(K),
 # $$
+#
 # or
+#
 # $$
-#     V_h(K) \coloneqq \mathbb{BDM}_k(K) \textnormal{ and }
-#     Q_h(K) \coloneqq \mathbb{P}_{k-1}(K).
+#     V_h(K) := \mathbb{BDM}_k(K) \textnormal{ and }
+#     Q_h(K) := \mathbb{P}_{k-1}(K).
 # $$
-
-# Let two cells $K^+$ and $K^-$ share a facet $F$. The trace of a piecewise
-# smooth vector valued function $\phi$ on F taken approaching from inside $K^+$
-# (resp. $K^-$) is denoted $\phi^{+}$ (resp. $\phi^-$). We now introduce the
-# average
+#
+# Let two cells $K^+$ and $K^-$ share a facet $F$. The trace of a
+# piecewise smooth vector valued function $\phi$ on F taken approaching
+# from inside $K^+$ (resp. $K^-$) is denoted $\phi^{+}$ (resp.
+# $\phi^-$). We now introduce the average
 # $\renewcommand{\avg}[1]{\left\{\!\!\left\{#1\right\}\!\!\right\}}$
+#
 # $$
 #     \avg{\phi} = \frac{1}{2} \left(\phi^+ + \phi^-\right)
+#
 # $$
-# $\renewcommand{\jump}[1]{\llbracket #1 \rrbracket}$
-# and jump
+#
+# and jump $\renewcommand{\jump}[1]{\llbracket #1 \rrbracket}$
+#
 # $$
 #     \jump{\phi} = \phi^+ \otimes n^+ + \phi^- \otimes n^-,
 # $$
+#
 # operators, where $n$ denotes the outward unit normal to $\partial K$.
 # Finally, let the upwind flux of $\phi$ with respect to a vector field
 # $\psi$ be defined as
+#
 # $$
-#     \hat{\phi}^\psi \coloneqq
+#     \hat{\phi}^\psi :=
 #     \begin{cases}
 #         \lim_{\epsilon \downarrow 0} \phi(x - \epsilon \psi(x)), \;
 #         x \in \partial K \setminus \Gamma^\psi, \\
 #         0, \qquad \qquad \qquad \qquad x \in \partial K \cap \Gamma^\psi,
 #     \end{cases}
 # $$
+#
 # where $\Gamma^\psi = \left\{x \in \Gamma; \; \psi(x) \cdot n(x) < 0\right\}$.
-
+#
 # The semi-discrete version problem (in dimensionless form) is: find
 # $(u_h, p_h) \in V_h^{u_D} \times Q_h$ such that
+#
 # $$
 #     \int_\Omega \partial_t u_h \cdot v + a_h(u_h, v_h) + c_h(u_h; u_h, v_h)
-#     + b_h(v_h, p_h) = \int_\Omega f \cdot v_h + L_{a_h}(v_h) + L_{c_h}(v_h)
-#      \quad \forall v_h \in V_h^0,
+#     + b_h(v_h, p_h) &= \int_\Omega f \cdot v_h + L_{a_h}(v_h) + L_{c_h}(v_h)
+#      \quad \forall v_h \in V_h^0, \\
+#     b_h(u_h, q_h) &= 0 \quad \forall q_h \in Q_h,
 # $$
-# $$
-#     b_h(u_h, q_h) = 0 \quad \forall q_h \in Q_h,
-# $$
+#
 # where
 # $\renewcommand{\sumK}[0]{\sum_{K \in \mathcal{T}_h}}$
 # $\renewcommand{\sumF}[0]{\sum_{F \in \mathcal{F}_h}}$
+#
 # $$
-#     a_h(u, v) = Re^{-1} \left(\sumK \int_K \nabla u : \nabla v
+#     a_h(u, v) &= Re^{-1} \left(\sumK \int_K \nabla u : \nabla v
 #     - \sumF \int_F \avg{\nabla u} : \jump{v}
 #     - \sumF \int_F \avg{\nabla v} : \jump{u} \\
-#     + \sumF \int_F \frac{\alpha}{h_K} \jump{u} : \jump{v}\right),
+#     + \sumF \int_F \frac{\alpha}{h_K} \jump{u} : \jump{v}\right), \\
+#     c_h(w; u, v) &= - \sumK \int_K u \cdot \nabla \cdot (v \otimes w)
+#     + \sumK \int_{\partial_K} w \cdot n \hat{u}^{w} \cdot v, \\
+#     L_{a_h}(v_h) &= Re^{-1} \left(- \int_{\partial \Omega} u_D \otimes n :
+#     \nabla_h v_h + \frac{\alpha}{h} u_D \otimes n : v_h \otimes n \right), \\
+#     L_{c_h}(v_h) &= - \int_{\partial \Omega} u_D \cdot n \hat{u}_D \cdot v_h, \\
+#     b_h(v, q) &= - \int_K \nabla \cdot v q.
 # $$
-# $$
-#     c_h(w; u, v) = - \sumK \int_K u \cdot \nabla \cdot (v \otimes w)
-#     + \sumK \int_{\partial_K} w \cdot n \hat{u}^{w} \cdot v,
-# $$
-# $$
-# L_{a_h}(v_h) = Re^{-1} \left(- \int_{\partial \Omega} u_D \otimes n :
-#   \nabla_h v_h + \frac{\alpha}{h} u_D \otimes n : v_h \otimes n \right),
-# $$
-# $$
-#     L_{c_h}(v_h) = - \int_{\partial \Omega} u_D \cdot n \hat{u}_D \cdot v_h,
-# $$
-# and
-# $$
-#     b_h(v, q) = - \int_K \nabla \cdot v q.
-# $$
-
+#
+#
 # ## Implementation
+#
 # We begin by importing the required modules and functions
 
+# +
 import numpy as np
 from mpi4py import MPI
 from petsc4py import PETSc
@@ -155,11 +167,13 @@ from dolfinx import fem, io, mesh
 if np.issubdtype(PETSc.ScalarType, np.complexfloating):
     print("Demo should only be executed with DOLFINx real mode")
     exit(0)
+# -
 
 
 # We also define some helper functions that will be used later
 
 
+# +
 def norm_L2(comm, v):
     """Compute the L2(Ω)-norm of v"""
     return np.sqrt(comm.allreduce(
@@ -195,9 +209,10 @@ def f_expr(x):
 
 def boundary_marker(x):
     return np.isclose(x[0], 0.0) | np.isclose(x[0], 1.0) | np.isclose(x[1], 0.0) | np.isclose(x[1], 1.0)
-
+# -
 
 # We define some simulation parameters
+
 
 n = 16
 num_time_steps = 25
@@ -210,6 +225,7 @@ k = 1  # Polynomial degree
 # space, we also create a vector valued discontinuous Lagrange space to
 # interpolate into for artifact free visualisation.
 
+# +
 msh = mesh.create_unit_square(MPI.COMM_WORLD, n, n)
 
 # Function spaces for the velocity and for the pressure
@@ -233,11 +249,13 @@ n = FacetNormal(msh)
 
 def jump(phi, n):
     return outer(phi("+"), n("+")) + outer(phi("-"), n("-"))
+# -
+
+# We solve the Stokes problem for the initial condition, omitting the
+# convective term:
 
 
-# We solve the Stokes problem for the initial condition, so the
-# convective terms are omitted for now
-
+# +
 a_00 = (1 / Re) * (inner(grad(u), grad(v)) * dx
                    - inner(avg(grad(u)), jump(v, n)) * dS
                    - inner(jump(u, n), avg(grad(v))) * dS
@@ -260,37 +278,30 @@ L_1 = inner(fem.Constant(msh, 0.0), q) * dx
 L = fem.form([L_0, L_1])
 
 # Boundary conditions
-
 boundary_facets = mesh.locate_entities_boundary(msh, msh.topology.dim - 1, boundary_marker)
 boundary_vel_dofs = fem.locate_dofs_topological(V, msh.topology.dim - 1, boundary_facets)
 bc_u = fem.dirichletbc(u_D, boundary_vel_dofs)
 bcs = [bc_u]
 
 # Assemble Stokes problem
-
 A = fem.petsc.assemble_matrix_block(a, bcs=bcs)
 A.assemble()
 b = fem.petsc.assemble_vector_block(L, a, bcs=bcs)
 
 # Create and configure solver
-
 ksp = PETSc.KSP().create(msh.comm)
 ksp.setOperators(A)
 ksp.setType("preonly")
 ksp.getPC().setType("lu")
 ksp.getPC().setFactorSolverType("mumps")
 opts = PETSc.Options()
-# See https://graal.ens-lyon.fr/MUMPS/doc/userguide_5.5.1.pdf
-# TODO Check
-opts["mat_mumps_icntl_6"] = 2
-opts["mat_mumps_icntl_14"] = 100
-opts["mat_mumps_icntl_24"] = 1
-opts["mat_mumps_icntl_25"] = 0
+opts["mat_mumps_icntl_14"] = 80  # Increase MUMPS working memory
+opts["mat_mumps_icntl_24"] = 1  # Option to support solving a singular matrix (pressure nullspace)
+opts["mat_mumps_icntl_25"] = 0  # Option to support solving a singular matrix (pressure nullspace)
 opts["ksp_error_if_not_converged"] = 1
 ksp.setFromOptions()
 
 # Solve Stokes for initial condition
-
 x = A.createVecRight()
 try:
     ksp.solve(b, x)
@@ -304,7 +315,6 @@ except PETSc.Error as e:
 
 
 # Split the solution
-
 u_h = fem.Function(V)
 p_h = fem.Function(Q)
 p_h.name = "p"
@@ -322,9 +332,7 @@ u_vis.name = "u"
 u_vis.interpolate(u_h)
 
 # Write initial condition to file
-
 t = 0.0
-
 try:
     u_file = io.VTXWriter(msh.comm, "u.bp", [u_vis._cpp_object])
     p_file = io.VTXWriter(msh.comm, "p.bp", [p_h._cpp_object])
@@ -334,12 +342,13 @@ except AttributeError:
     print("File output requires ADIOS2.")
 
 # Create function to store solution and previous time step
-
 u_n = fem.Function(V)
 u_n.x.array[:] = u_h.x.array
+# -
 
 # Now we add the time stepping and convective terms
 
+# +
 lmbda = conditional(gt(dot(u_n, n), 0), 1, 0)
 u_uw = lmbda("+") * u("+") + lmbda("-") * u("-")
 a_00 += inner(u / delta_t, v) * dx - \
@@ -354,7 +363,6 @@ L_0 += inner(u_n / delta_t, v) * dx - inner(dot(u_n, n) * (1 - lmbda) * u_D, v) 
 L = fem.form([L_0, L_1])
 
 # Time stepping loop
-
 for n in range(num_time_steps):
     t += delta_t.value
 
@@ -392,9 +400,11 @@ try:
     p_file.close()
 except NameError:
     pass
+# -
 
 # Now we compare the computed solution to the exact solution
 
+# +
 # Function spaces for exact velocity and pressure
 V_e = fem.VectorFunctionSpace(msh, ("Lagrange", k + 3))
 Q_e = fem.FunctionSpace(msh, ("Lagrange", k + 2))
@@ -418,3 +428,4 @@ if msh.comm.rank == 0:
     print(f"e_u = {e_u}")
     print(f"e_div_u = {e_div_u}")
     print(f"e_p = {e_p}")
+# -
