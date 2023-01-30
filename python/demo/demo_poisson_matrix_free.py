df7e3c9081455ceb970b486ac08a1adfd2bc4bd8
Julius Herb <julius@jherb.de>
2023-01-30 23:55:47 +0100

    add documentation to matrix-free Poisson demo

--- python/demo/demo_poisson_matrix_free.py
+++ python/demo/demo_poisson_matrix_free.py
@@ -21,7 +21,7 @@
 # - Solve a linear partial differential equation using a matrix-free
 # Conjugate Gradient (CG) solver
 # - Create and apply Dirichlet boundary conditions
-# - Validate the results against the exact solution and against a
+# - Compute errors against the exact solution and against a
 # direct solver for the assembled matrix
 #
 # ## Equation and problem definition
@@ -71,18 +71,13 @@
 #
 # The modules that will be used are imported:
 
-# +
 import numpy as np
-
 import ufl
 from dolfinx import fem, mesh
-from ufl import action, dx, grad, inner
-
 from mpi4py import MPI
 from petsc4py import PETSc
 from petsc4py.PETSc import ScalarType
-
-# -
+from ufl import action, dx, grad, inner
 
 # We begin by using {py:func}`create_rectangle
 # <dolfinx.mesh.create_rectangle>` to create a rectangular
@@ -105,7 +100,7 @@ V = fem.FunctionSpace(msh, ("Lagrange", 2))
 # Next, we locate the mesh facets that lie on the
 # domain boundary $\partial\Omega$.
 # We can do this by first calling {py:func}`create_connectivity
-# <dolfinx.mesh.locate_entities_boundary>` and then retrieving all
+# <dolfinx.mesh.topology.create_connectivity>` and then retrieving all
 # facets on the boundary using
 # {py:func}`exterior_facet_indices <dolfinx.mesh.exterior_facet_indices>`.
 
@@ -121,7 +116,7 @@ dofs = fem.locate_dofs_topological(V=V, entity_dim=1, entities=facets)
 # and use {py:func}`dirichletbc <dolfinx.fem.dirichletbc>` to create a
 # {py:class}`DirichletBCMetaClass <dolfinx.fem.DirichletBCMetaClass>`
 # class that represents the boundary condition. On the boundary we prescribe
-# {py:class}`Function <dolfinx.fem.Function>` `u_D`, which is obtained by
+# the {py:class}`Function <dolfinx.fem.Function>` `u_D`, which is obtained by
 # interpolating the expression $u_{\rm D}$ onto the finite element space $V$.
 
 uD = fem.Function(V, dtype=ScalarType)
@@ -138,7 +133,7 @@ a = inner(grad(u), grad(v)) * dx
 L = inner(f, v) * dx
 
 # For the matrix-free solvers we also define a second linear form `M` as
-# the {py:class}`action <ufl.action>` of the bilinear form `a` onto an
+# the {py:class}`action <ufl.action>` of the bilinear form $a$ onto an
 # arbitrary {py:class}`Function <dolfinx.fem.Function>` `ui`.
 
 ui = fem.Function(V)
@@ -146,13 +141,17 @@ M = action(a, ui)
 
 # ### Direct solver using the assembled matrix
 #
-# For validation
+# To validate the results of the matrix free solver, we first compute the
+# solution with a direct solver using the assembled matrix.
 
 problem = fem.petsc.LinearProblem(a, L, bcs=[bc],
                                   petsc_options={"ksp_type": "preonly",
                                                  "pc_type": "lu"})
 uh_lu = problem.solve()
 
+
+# The error of the finite element solution `uh_lu` compared to the exact
+# solution $u_{\rm D}$ is calculated below in the $L_2$-norm.
 
 # +
 def L2Norm(u):
@@ -168,30 +167,45 @@ if msh.comm.rank == 0:
 # -
 
 # ### Matrix-free Conjugate Gradient solvers
+#
+# For the matrix-free solvers, the RHS vector $b$ is first assembled based
+# on the linear form $L$.  To account for the Dirichlet boundary conditions
+# in $b$, we apply lifting, i.e. set $b - A x_{\rm bc}$ as new RHS vector $b$.
+# Since we want to avoid assembling the matrix `A`, we compute the necessary
+# matrix-vector product using the linear form `M`.
 
-fem.set_bc(ui.x.array, [bc], scale=-1)
 b = fem.petsc.assemble_vector(fem.form(L))
-b += fem.petsc.assemble_vector(fem.form(M))
-b.ghostUpdate(addv=PETSc.InsertMode.ADD_VALUES, mode=PETSc.ScatterMode.REVERSE)
-fem.set_bc(b.array, [bc], scale=0.0)
+# b <- b - A * x_bc
+fem.set_bc(ui.x.array, [bc], scale=-1)
+fem.petsc.assemble_vector(b, fem.form(M))
+b.ghostUpdate(addv=PETSc.InsertMode.ADD, mode=PETSc.ScatterMode.REVERSE)
+fem.petsc.set_bc(b, [bc], scale=0.0)
 b.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
 
 
 # #### 1. Implementation using PETSc vectors
 
-def action_A(x_vec, y_vec):
-    x_vec.copy(ui.vector)
-    y = fem.petsc.assemble_vector(fem.form(M))
-    fem.set_bc(y, [bc], scale=0.0)
-    y.ghostUpdate(addv=PETSc.InsertMode.ADD_VALUES, mode=PETSc.ScatterMode.REVERSE)
-    y.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
-    y.copy(y_vec)
+# To implement the matrix-free CG solver using *PETSc* vectors, we define the
+# function `action_A` with which the matrix-vector product $y = A x$
+# is calculated.
 
+def action_A(x, y):
+    y.set(0.0)
+    x.copy(ui.vector)
+    fem.petsc.assemble_vector(y, fem.form(M))
+    fem.petsc.set_bc(y, [bc], scale=0.0)
+    y.ghostUpdate(addv=PETSc.InsertMode.ADD,
+                  mode=PETSc.ScatterMode.REVERSE)
+    y.ghostUpdate(addv=PETSc.InsertMode.INSERT,
+                  mode=PETSc.ScatterMode.FORWARD)
+
+
+# This function can be used to replace the matrix-vector product in the
+# plain Conjugate Gradient method by Hestenes and Stiefel.
 
 def cg(action_A, b, x, kmax=50, rtol=1e-8):
-    """CG method
-    """
     y = b.duplicate()
+    b.copy(y)
     action_A(x, y)
     r = b - y
     p = r.duplicate()
@@ -215,6 +229,11 @@ def cg(action_A, b, x, kmax=50, rtol=1e-8):
     return k
 
 
+# This matrix-free solver is now used to compute the finite element solution.
+# After that, the error against the exact solution in the $L_2$-norm and the
+# error of the coefficients against the solution obtained with the direct
+# solver is computed.
+
 # +
 uh_cg1 = fem.Function(V, dtype=ScalarType)
 k = cg(action_A, b, uh_cg1.vector, kmax=200, rtol=1e-6)
@@ -227,11 +246,16 @@ if msh.comm.rank == 0:
     print(f"CG iterations until convergence:  {k}")
     print(f"L2-error against exact solution:  {error_L2_cg1:.4e}")
     print(f"Coeff. error against LU solution: {error_lu_cg1:.4e}")
+assert error_L2_cg1 < 1e-6
 
 
 # -
 
 # #### 2. Implementation using NumPy arrays
+
+# It is also possible to implement a matrix-free Conjugate Gradient solver
+# using *NumPy* arrays. For that a function `action_A_np` can be defined
+# that maps an array `x` to the result of the matrix-vector product `y`.
 
 def action_A_np(x_array):
     ui.vector.setArray(x_array)
@@ -240,9 +264,10 @@ def action_A_np(x_array):
     return y.array
 
 
+# The matrix-free Conjugate Gradient solver using *NumPy* arrays is then
+# implemented analogously to the approach with *PETSc* vectors.
+
 def cg_np(action_A, b, x0=None, kmax=50, rtol=1e-8):
-    """CG method
-    """
     if x0 is None:
         x0 = np.zeros_like(b)
     r = b - action_A(x0)
@@ -267,6 +292,8 @@ def cg_np(action_A, b, x0=None, kmax=50, rtol=1e-8):
     return x, k
 
 
+# As before, we apply the solver to the Poisson problem and compute the errors.
+
 # +
 cg2_x_array, k = cg_np(action_A_np, b.array, kmax=200, rtol=1e-6)
 fem.set_bc(cg2_x_array, [bc], scale=1.0)
@@ -280,10 +307,16 @@ if msh.comm.rank == 0:
     print(f"CG iterations until convergence:  {k}")
     print(f"L2-error against exact solution:  {error_L2_cg2:.4e}")
     print(f"Coeff. error against LU solution: {error_lu_cg2:.4e}")
+assert error_L2_cg2 < 1e-6
 
 
 # -
 # ### 3. Implementation using the built-in PETSc KSP solver
+#
+# Another approach is to modify the existing KSP solver of *PETSc* to obtain
+# a matrix-free Conjugate Gradient solver. For this purpose, we create a
+# class `Poisson` to emulate the assembled matrix A of the Poisson problem
+# considered here.
 
 class Poisson:
     def create(self, A):
@@ -294,13 +327,19 @@ class Poisson:
         action_A(x, y)
 
 
+# With this we can define a virtual *PETSc* matrix, where every
+# matrix-vector product is implicitly performed matrix-free.
+
 A = PETSc.Mat()
 A.create(comm=msh.comm)
-print(b.getSize(), b.local_size, b.getLocalSize(), b.getSizes(), b.getBlockSize())
 A.setSizes([b.getSize(), b.getSize()])
 A.setType(PETSc.Mat.Type.PYTHON)
 A.setPythonContext(Poisson())
 A.setUp()
+
+# This matrix can then be passed as an operator to a predefined
+# Conjugate Gradient solver in the KSP framework, automatically making
+# that solver matrix-free.
 
 solver = PETSc.KSP().create(msh.comm)
 solver.setOperators(A)
@@ -309,10 +348,15 @@ solver.getPC().setType(PETSc.PC.Type.NONE)
 solver.setTolerances(rtol=1e-6)
 solver.setConvergenceHistory()
 
+# Again, the solver is applied and the errors are computed.
+
 # +
 uh_cg3 = fem.Function(V)
 solver.solve(b, uh_cg3.vector)
+uh_cg3.x.scatter_forward()
 fem.set_bc(uh_cg3.vector, [bc], scale=1.0)
+uh_cg3.vector.ghostUpdate(
+    addv=PETSc.InsertMode.INSERT_VALUES, mode=PETSc.ScatterMode.FORWARD)
 
 error_L2_cg3 = L2Norm(uh_cg3 - uD)
 error_lu_cg3 = np.linalg.norm(uh_cg3.x.array - uh_lu.x.array)
@@ -321,11 +365,16 @@ if msh.comm.rank == 0:
     print(f"CG iterations until convergence:  {k}")
     print(f"L2-error against exact solution:  {error_L2_cg3:.4e}")
     print(f"Coeff. error against LU solution: {error_lu_cg3:.4e}")
+assert error_L2_cg3 < 1e-6
 
 
 # -
 
 # ### 4. Implementation using a custom PETSc KSP solver
+#
+# Furthermore, it is also possible to write a custom Conjugate Gradient
+# solver in the KSP framework, which is matrix-free as before. For this
+# purpose, a base class for a custom KSP solver is created.
 
 class CustomKSP:
     def create(self, ksp):
@@ -336,12 +385,12 @@ class CustomKSP:
             v.destroy()
 
     def setUp(self, ksp):
-        self.vectors[:] = ksp.getWorkVecs(right=2, left=None)
+        self.vectors = ksp.getWorkVecs(right=2, left=None)
 
     def reset(self, ksp):
         for v in self.vectors:
             v.destroy()
-        del self.vectors[:]
+        del self.vectors
 
     def converged(self, ksp, r):
         k = ksp.getIterationNumber()
@@ -356,6 +405,9 @@ class CustomKSP:
             ksp.setConvergedReason(reason)
         return reason
 
+
+# A user-defined Conjugate Gradient procedure can then be defined based
+# on this prototype.
 
 class CG(CustomKSP):
     def setUp(self, ksp):
@@ -383,6 +435,9 @@ class CG(CustomKSP):
             p.aypx(beta, r)
 
 
+# As before, a matrix-free can be achieved by passing the emulated matrix
+# as the operator.
+
 solver = PETSc.KSP().create(msh.comm)
 solver.setOperators(A)
 solver.setType(PETSc.KSP.Type.PYTHON)
@@ -390,10 +445,15 @@ solver.setPythonContext(CG())
 solver.setTolerances(rtol=1e-6)
 solver.setConvergenceHistory()
 
+# The computed solution is again compared with the exact solution and
+# the direct solver using the assembled matrix.
+
 # +
 uh_cg4 = fem.Function(V)
 solver.solve(b, uh_cg4.vector)
 fem.set_bc(uh_cg4.vector, [bc], scale=1.0)
+uh_cg4.vector.ghostUpdate(
+    addv=PETSc.InsertMode.INSERT_VALUES, mode=PETSc.ScatterMode.FORWARD)
 
 error_L2_cg4 = L2Norm(uh_cg4 - uD)
 error_lu_cg4 = np.linalg.norm(uh_cg4.x.array - uh_lu.x.array)
@@ -402,3 +462,4 @@ if msh.comm.rank == 0:
     print(f"CG iterations until convergence:  {k}")
     print(f"L2-error against exact solution:  {error_L2_cg4:.4e}")
     print(f"Coeff. error against LU solution: {error_lu_cg4:.4e}")
+assert error_L2_cg4 < 1e-6
