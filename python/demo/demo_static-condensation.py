abecc176cc397333c386277460591eab4787502f
Garth N. Wells <gnw20@cam.ac.uk>
2023-02-06 21:04:23 +0000

    Tidy up

--- python/demo/demo_static-condensation.py
+++ python/demo/demo_static-condensation.py
@@ -137,12 +137,8 @@ def tabulate_condensed_tensor_A(A_, w_, c_, coords_, entity_local_index, permuta
 # Prepare a Form with a condensed tabulation kernel
 Form = Form_float64 if PETSc.ScalarType == np.float64 else Form_complex128
 
-tdim = msh.topology.dim
-cells = range(msh.topology.index_map(tdim).size_local)
-
-
+cells = range(msh.topology.index_map(msh.topology.dim).size_local)
 integrals = {IntegralType.cell: [(-1, tabulate_condensed_tensor_A.address, cells)]}
-# integrals = {IntegralType.cell: ([(-1, tabulate_condensed_tensor_A.address)], None)}
 a_cond = Form([U._cpp_object, U._cpp_object], integrals, [], [], False, None)
 
 A_cond = assemble_matrix(a_cond, bcs=[bc])
