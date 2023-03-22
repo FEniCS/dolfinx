e19374521a7feb0b83a85c4518d4ce20b9242700
Garth N. Wells <gnw20@cam.ac.uk>
2023-03-22 21:32:47 +0000

    Fix linkage

--- cpp/dolfinx/io/ADIOS2Writers.cpp
+++ cpp/dolfinx/io/ADIOS2Writers.cpp
@@ -427,6 +427,107 @@ void fides_initialize_function_attributes(adios2::IO& io,
 //-----------------------------------------------------------------------------
 } // namespace
 
+/// Create VTK xml scheme to be interpreted by the VTX reader
+/// https://adios2.readthedocs.io/en/latest/ecosystem/visualization.html#saving-the-vtk-xml-data-model
+std::stringstream
+io::impl::create_vtk_schema(const std::vector<std::string>& point_data,
+                            const std::vector<std::string>& cell_data)
+{
+  // Create XML
+  pugi::xml_document xml_schema;
+  pugi::xml_node vtk_node = xml_schema.append_child("VTKFile");
+  vtk_node.append_attribute("type") = "UnstructuredGrid";
+  vtk_node.append_attribute("version") = "0.1";
+  pugi::xml_node unstructured = vtk_node.append_child("UnstructuredGrid");
+
+  // -- Piece
+
+  pugi::xml_node piece = unstructured.append_child("Piece");
+
+  // Add mesh attributes
+  piece.append_attribute("NumberOfPoints") = "NumberOfNodes";
+  piece.append_attribute("NumberOfCells") = "NumberOfCells";
+
+  // -- Points
+
+  // Add point information
+  pugi::xml_node xml_geometry = piece.append_child("Points");
+  pugi::xml_node xml_vertices = xml_geometry.append_child("DataArray");
+  xml_vertices.append_attribute("Name") = "geometry";
+
+  // -- Cells
+
+  pugi::xml_node xml_topology = piece.append_child("Cells");
+  xml_topology.append_child("DataArray").append_attribute("Name")
+      = "connectivity";
+  xml_topology.append_child("DataArray").append_attribute("Name") = "types";
+
+  // -- PointData
+
+  pugi::xml_node xml_pointdata = piece.append_child("PointData");
+
+  // Stepping info for time dependency
+  pugi::xml_node item_time = xml_pointdata.append_child("DataArray");
+  item_time.append_attribute("Name") = "TIME";
+  item_time.append_child(pugi::node_pcdata).set_value("step");
+
+  pugi::xml_node item_idx = xml_pointdata.append_child("DataArray");
+  item_idx.append_attribute("Name") = "vtkOriginalPointIds";
+  pugi::xml_node item_ghost = xml_pointdata.append_child("DataArray");
+  item_ghost.append_attribute("Name") = "vtkGhostType";
+  for (auto& name : point_data)
+  {
+    pugi::xml_node item = xml_pointdata.append_child("DataArray");
+    item.append_attribute("Name") = name.c_str();
+  }
+
+  // -- CellData
+
+  if (!cell_data.empty())
+  {
+    pugi::xml_node xml_celldata = piece.append_child("CellData");
+    for (auto& name : cell_data)
+    {
+      pugi::xml_node item = xml_celldata.append_child("DataArray");
+      item.append_attribute("Name") = name.c_str();
+    }
+  }
+
+  std::stringstream ss;
+  xml_schema.save(ss, "  ");
+  return ss;
+}
+
+/// Extract name of functions and split into real and imaginary component
+template <typename T>
+std::vector<std::string>
+extract_function_names(const typename ADIOS2Writer<T>::U& u)
+{
+  std::vector<std::string> names;
+  using X = decltype(names);
+  for (auto& v : u)
+  {
+    auto n = std::visit(
+        overload{
+            [](const std::shared_ptr<const typename ADIOS2Writer<T>::Fd32>& u)
+                -> X { return {u->name}; },
+            [](const std::shared_ptr<const typename ADIOS2Writer<T>::Fd64>& u)
+                -> X { return {u->name}; },
+            [](const std::shared_ptr<const typename ADIOS2Writer<T>::Fc64>& u)
+                -> X {
+              return {u->name + field_ext[0], u->name + field_ext[1]};
+            },
+            [](const std::shared_ptr<const typename ADIOS2Writer<T>::Fc128>& u)
+                -> X {
+              return {u->name + field_ext[0], u->name + field_ext[1]};
+            }},
+        v);
+    names.insert(names.end(), n.begin(), n.end());
+  };
+
+  return names;
+}
+
 //-----------------------------------------------------------------------------
 FidesWriter::FidesWriter(MPI_Comm comm, const std::filesystem::path& filename,
                          std::shared_ptr<const mesh::Mesh<double>> mesh)
--- cpp/dolfinx/io/ADIOS2Writers.h
+++ cpp/dolfinx/io/ADIOS2Writers.h
@@ -263,72 +263,7 @@ adios2::Variable<T> define_variable(adios2::IO& io, std::string name,
 /// Create VTK xml scheme to be interpreted by the VTX reader
 /// https://adios2.readthedocs.io/en/latest/ecosystem/visualization.html#saving-the-vtk-xml-data-model
 std::stringstream create_vtk_schema(const std::vector<std::string>& point_data,
-                                    const std::vector<std::string>& cell_data)
-{
-  // Create XML
-  pugi::xml_document xml_schema;
-  pugi::xml_node vtk_node = xml_schema.append_child("VTKFile");
-  vtk_node.append_attribute("type") = "UnstructuredGrid";
-  vtk_node.append_attribute("version") = "0.1";
-  pugi::xml_node unstructured = vtk_node.append_child("UnstructuredGrid");
-
-  // -- Piece
-
-  pugi::xml_node piece = unstructured.append_child("Piece");
-
-  // Add mesh attributes
-  piece.append_attribute("NumberOfPoints") = "NumberOfNodes";
-  piece.append_attribute("NumberOfCells") = "NumberOfCells";
-
-  // -- Points
-
-  // Add point information
-  pugi::xml_node xml_geometry = piece.append_child("Points");
-  pugi::xml_node xml_vertices = xml_geometry.append_child("DataArray");
-  xml_vertices.append_attribute("Name") = "geometry";
-
-  // -- Cells
-
-  pugi::xml_node xml_topology = piece.append_child("Cells");
-  xml_topology.append_child("DataArray").append_attribute("Name")
-      = "connectivity";
-  xml_topology.append_child("DataArray").append_attribute("Name") = "types";
-
-  // -- PointData
-
-  pugi::xml_node xml_pointdata = piece.append_child("PointData");
-
-  // Stepping info for time dependency
-  pugi::xml_node item_time = xml_pointdata.append_child("DataArray");
-  item_time.append_attribute("Name") = "TIME";
-  item_time.append_child(pugi::node_pcdata).set_value("step");
-
-  pugi::xml_node item_idx = xml_pointdata.append_child("DataArray");
-  item_idx.append_attribute("Name") = "vtkOriginalPointIds";
-  pugi::xml_node item_ghost = xml_pointdata.append_child("DataArray");
-  item_ghost.append_attribute("Name") = "vtkGhostType";
-  for (auto& name : point_data)
-  {
-    pugi::xml_node item = xml_pointdata.append_child("DataArray");
-    item.append_attribute("Name") = name.c_str();
-  }
-
-  // -- CellData
-
-  if (!cell_data.empty())
-  {
-    pugi::xml_node xml_celldata = piece.append_child("CellData");
-    for (auto& name : cell_data)
-    {
-      pugi::xml_node item = xml_celldata.append_child("DataArray");
-      item.append_attribute("Name") = name.c_str();
-    }
-  }
-
-  std::stringstream ss;
-  xml_schema.save(ss, "  ");
-  return ss;
-}
+                                    const std::vector<std::string>& cell_data);
 
 /// Extract name of functions and split into real and imaginary component
 template <typename T>
