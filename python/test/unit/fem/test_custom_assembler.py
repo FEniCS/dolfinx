a12a78e906bfa343cc796c94e2176a4a53e78be7
Garth N. Wells <gnw20@cam.ac.uk>
2023-04-20 14:12:18 +0100

    flake8 fixes

--- python/test/unit/fem/test_custom_assembler.py
+++ python/test/unit/fem/test_custom_assembler.py
@@ -279,7 +279,6 @@ def test_custom_mesh_loop_rank1():
 
     # Unpack mesh and dofmap data
     num_owned_cells = mesh.topology.index_map(mesh.topology.dim).size_local
-    num_cells = num_owned_cells + mesh.topology.index_map(mesh.topology.dim).num_ghosts
     x_dofs = mesh.geometry.dofmap
     x = mesh.geometry.x
     dofmap = V.dofmap.list
@@ -361,7 +360,6 @@ def test_custom_mesh_loop_ctypes_rank2():
 
     # Extract mesh and dofmap data
     num_owned_cells = mesh.topology.index_map(mesh.topology.dim).size_local
-    num_cells = num_owned_cells + mesh.topology.index_map(mesh.topology.dim).num_ghosts
     x_dofs = mesh.geometry.dofmap
     x = mesh.geometry.x
     dofmap = V.dofmap.list.astype(np.dtype(PETSc.IntType))
@@ -419,8 +417,7 @@ def test_custom_mesh_loop_cffi_rank2(set_vals):
 
     # Unpack mesh and dofmap data
     num_owned_cells = mesh.topology.index_map(mesh.topology.dim).size_local
-    num_cells = num_owned_cells + mesh.topology.index_map(mesh.topology.dim).num_ghosts
-    x_dofs = mesh.geometry.dofmap.array.reshape(num_cells, 3)
+    x_dofs = mesh.geometry.dofmap
     x = mesh.geometry.x
     dofmap = V.dofmap.list.astype(np.dtype(PETSc.IntType))
 
