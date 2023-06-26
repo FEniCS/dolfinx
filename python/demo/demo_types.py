e47dea6e205c9a9d3ce34c9025bbaa2da437e5bd
Garth N. Wells <gnw20@cam.ac.uk>
2023-06-26 20:35:40 +0100

    Test MatrixCSR with block size

--- python/demo/demo_types.py
+++ python/demo/demo_types.py
@@ -40,94 +40,192 @@ comm = MPI.COMM_SELF
 
 msh = mesh.create_rectangle(comm=comm, points=((0.0, 0.0), (2.0, 1.0)), n=(32, 16),
                             cell_type=mesh.CellType.triangle)
-V = fem.FunctionSpace(msh, ("Lagrange", 1))
-
-# Define a variational problem.
-
-u, v = ufl.TrialFunction(V), ufl.TestFunction(V)
-x = ufl.SpatialCoordinate(msh)
-fr = 10 * ufl.exp(-((x[0] - 0.5) ** 2 + (x[1] - 0.5) ** 2) / 0.02)
-fc = ufl.sin(2 * np.pi * x[0]) + 10 * ufl.sin(4 * np.pi * x[1]) * 1j
-gr = ufl.sin(5 * x[0])
-gc = ufl.sin(5 * x[0]) * 1j
-a = ufl.inner(ufl.grad(u), ufl.grad(v)) * ufl.dx
-L = ufl.inner(fr + fc, v) * ufl.dx + ufl.inner(gr + gc, v) * ufl.ds
-
-# In preparation for constructing Dirichlet boundary conditions, locate
-# facets on the constrained boundary and the corresponding
-# degrees-of-freedom.
 
 facets = mesh.locate_entities_boundary(msh, dim=1,
                                        marker=lambda x: np.logical_or(np.isclose(x[0], 0.0),
                                                                       np.isclose(x[0], 2.0)))
-dofs = fem.locate_dofs_topological(V=V, entity_dim=1, entities=facets)
 
 
-# The below function computes the solution of the finite problem using a
-# specified scalar type.
+def poisson():
 
-def solve(dtype=np.float32):
-    """Solve the variational problem"""
+    V = fem.FunctionSpace(msh, ("Lagrange", 1))
 
-    # Process forms. This will compile the forms for the requested type.
-    a0 = fem.form(a, dtype=dtype)
-    if np.issubdtype(dtype, np.complexfloating):
-        L0 = fem.form(L, dtype=dtype)
-    else:
-        L0 = fem.form(ufl.replace(L, {fc: 0, gc: 0}), dtype=dtype)
+    # Define a variational problem.
 
-    # Create a Dirichlet boundary condition
-    bc = fem.dirichletbc(value=dtype(0), dofs=dofs, V=V)
+    u, v = ufl.TrialFunction(V), ufl.TestFunction(V)
+    x = ufl.SpatialCoordinate(msh)
+    fr = 10 * ufl.exp(-((x[0] - 0.5) ** 2 + (x[1] - 0.5) ** 2) / 0.02)
+    fc = ufl.sin(2 * np.pi * x[0]) + 10 * ufl.sin(4 * np.pi * x[1]) * 1j
+    gr = ufl.sin(5 * x[0])
+    gc = ufl.sin(5 * x[0]) * 1j
+    a = ufl.inner(ufl.grad(u), ufl.grad(v)) * ufl.dx
+    L = ufl.inner(fr + fc, v) * ufl.dx + ufl.inner(gr + gc, v) * ufl.ds
 
-    # Assemble forms
-    A = fem.assemble_matrix(a0, [bc])
-    A.finalize()
-    b = fem.assemble_vector(L0)
-    fem.apply_lifting(b.array, [a0], bcs=[[bc]])
-    b.scatter_reverse(la.InsertMode.add)
-    fem.set_bc(b.array, [bc])
+    # In preparation for constructing Dirichlet boundary conditions, locate
+    # facets on the constrained boundary and the corresponding
+    # degrees-of-freedom.
 
-    # Create a Scipy sparse matrix that shares data with A
-    As = scipy.sparse.csr_matrix((A.data, A.indices, A.indptr))
+    dofs = fem.locate_dofs_topological(V=V, entity_dim=1, entities=facets)
 
-    # Solve the variational problem and return the solution
-    uh = fem.Function(V, dtype=dtype)
-    uh.x.array[:] = scipy.sparse.linalg.spsolve(As, b.array)
-    return uh
+    # The below function computes the solution of the finite problem using a
+    # specified scalar type.
 
-# This function visualises the solution.
+    def solve(dtype=np.float32):
+        """Solve the variational problem"""
 
-
-def display(u, filter=np.real):
-    """Plot the solution using pyvista"""
-    try:
-        import pyvista
-        cells, types, x = plot.create_vtk_mesh(V)
-        grid = pyvista.UnstructuredGrid(cells, types, x)
-        grid.point_data["u"] = filter(u.x.array)
-        grid.set_active_scalars("u")
-
-        plotter = pyvista.Plotter()
-        plotter.add_mesh(grid, show_edges=True)
-        plotter.add_mesh(grid.warp_by_scalar())
-        plotter.add_title("real" if filter is np.real else "imag")
-        if pyvista.OFF_SCREEN:
-            pyvista.start_xvfb(wait=0.1)
-            plotter.screenshot(f"u_{'real' if filter is np.real else 'imag'}.png")
+        # Process forms. This will compile the forms for the requested type.
+        a0 = fem.form(a, dtype=dtype)
+        if np.issubdtype(dtype, np.complexfloating):
+            L0 = fem.form(L, dtype=dtype)
         else:
-            plotter.show()
-    except ModuleNotFoundError:
-        print("'pyvista' is required to visualise the solution")
+            L0 = fem.form(ufl.replace(L, {fc: 0, gc: 0}), dtype=dtype)
+
+        # Create a Dirichlet boundary condition
+        bc = fem.dirichletbc(value=dtype(0), dofs=dofs, V=V)
+
+        # Assemble forms
+        A = fem.assemble_matrix(a0, [bc])
+        A.finalize()
+        b = fem.assemble_vector(L0)
+        fem.apply_lifting(b.array, [a0], bcs=[[bc]])
+        b.scatter_reverse(la.InsertMode.add)
+        fem.set_bc(b.array, [bc])
+
+        # Create a Scipy sparse matrix that shares data with A
+        As = scipy.sparse.csr_matrix((A.data, A.indices, A.indptr))
+
+        # Solve the variational problem and return the solution
+        uh = fem.Function(V, dtype=dtype)
+        uh.x.array[:] = scipy.sparse.linalg.spsolve(As, b.array)
+        return uh
+
+    # This function visualises the solution.
+
+    def display(u, filter=np.real):
+        """Plot the solution using pyvista"""
+        try:
+            import pyvista
+            cells, types, x = plot.create_vtk_mesh(V)
+            grid = pyvista.UnstructuredGrid(cells, types, x)
+            grid.point_data["u"] = filter(u.x.array)
+            grid.set_active_scalars("u")
+
+            plotter = pyvista.Plotter()
+            plotter.add_mesh(grid, show_edges=True)
+            plotter.add_mesh(grid.warp_by_scalar())
+            plotter.add_title("real" if filter is np.real else "imag")
+            if pyvista.OFF_SCREEN:
+                pyvista.start_xvfb(wait=0.1)
+                plotter.screenshot(f"u_{'real' if filter is np.real else 'imag'}.png")
+            else:
+                plotter.show()
+        except ModuleNotFoundError:
+            print("'pyvista' is required to visualise the solution")
+
+    # Solve the variational problem using different scalar types
+
+    uh = solve(dtype=np.float32)
+    uh = solve(dtype=np.float64)
+    uh = solve(dtype=np.complex64)
+    uh = solve(dtype=np.complex128)
+
+    # Display the last computed solution
+
+    display(uh, np.real)
+    display(uh, np.imag)
 
 
-# Solve the variational problem using different scalar types
+def elasticity():
+    print("EEEEEE")
 
-uh = solve(dtype=np.float32)
-uh = solve(dtype=np.float64)
-uh = solve(dtype=np.complex64)
-uh = solve(dtype=np.complex128)
+    V = fem.VectorFunctionSpace(msh, ("Lagrange", 1))
 
-# Display the last computed solution
+    # Define a variational problem.
 
-display(uh, np.real)
-display(uh, np.imag)
+    ω, ρ = 300.0, 10.0
+    x = ufl.SpatialCoordinate(msh)
+    f = ufl.as_vector((ρ * ω**2 * x[0], ρ * ω**2 * x[1]))
+
+    E = 1.0e9
+    ν = 0.3
+    μ = E / (2.0 * (1.0 + ν))
+    λ = E * ν / ((1.0 + ν) * (1.0 - 2.0 * ν))
+
+    def σ(v):
+        """Return an expression for the stress σ given a displacement field"""
+        return 2.0 * μ * ufl.sym(ufl.grad(v)) + λ * ufl.tr(ufl.sym(ufl.grad(v))) * ufl.Identity(len(v))
+
+    u, v = ufl.TrialFunction(V), ufl.TestFunction(V)
+    a = ufl.inner(σ(u), ufl.grad(v)) * ufl.dx
+    L = ufl.inner(f, v) * ufl.dx
+
+    dofs = fem.locate_dofs_topological(V=V, entity_dim=1, entities=facets)
+
+    def solve(dtype=np.float32):
+        """Solve the variational problem"""
+
+        # Process forms. This will compile the forms for the requested type.
+        a0 = fem.form(a, dtype=dtype)
+        L0 = fem.form(L, dtype=dtype)
+
+        # Create a Dirichlet boundary condition
+        bc = fem.dirichletbc(np.zeros(2, dtype=dtype),
+                             fem.locate_dofs_topological(V, entity_dim=1, entities=facets), V=V)
+
+        # Assemble forms
+        A = fem.assemble_matrix(a0, [bc])
+        A.finalize()
+        b = fem.assemble_vector(L0)
+        fem.apply_lifting(b.array, [a0], bcs=[[bc]])
+        b.scatter_reverse(la.InsertMode.add)
+        fem.set_bc(b.array, [bc])
+
+        # Create a Scipy sparse matrix that shares data with A
+        As = scipy.sparse.csr_matrix((A.data, A.indices, A.indptr))
+        # As = scipy.sparse.bsr_matrix((A.data, A.indices, A.indptr))
+
+        # print(As)
+
+        # # Solve the variational problem and return the solution
+        # uh = fem.Function(V, dtype=dtype)
+        # uh.x.array[:] = scipy.sparse.linalg.spsolve(As, b.array)
+        # return uh
+
+    # This function visualises the solution.
+
+    # def display(u, filter=np.real):
+    #     """Plot the solution using pyvista"""
+    #     try:
+    #         import pyvista
+    #         cells, types, x = plot.create_vtk_mesh(V)
+    #         grid = pyvista.UnstructuredGrid(cells, types, x)
+    #         grid.point_data["u"] = filter(u.x.array)
+    #         grid.set_active_scalars("u")
+
+    #         plotter = pyvista.Plotter()
+    #         plotter.add_mesh(grid, show_edges=True)
+    #         plotter.add_mesh(grid.warp_by_scalar())
+    #         plotter.add_title("real" if filter is np.real else "imag")
+    #         if pyvista.OFF_SCREEN:
+    #             pyvista.start_xvfb(wait=0.1)
+    #             plotter.screenshot(f"u_{'real' if filter is np.real else 'imag'}.png")
+    #         else:
+    #             plotter.show()
+    #     except ModuleNotFoundError:
+    #         print("'pyvista' is required to visualise the solution")
+
+    # Solve the variational problem using different scalar types
+
+    # uh = solve(dtype=np.float32)
+    uh = solve(dtype=np.float64)
+    # uh = solve(dtype=np.complex64)
+    # uh = solve(dtype=np.complex128)
+
+    # Display the last computed solution
+
+    # display(uh, np.real)
+    # display(uh, np.imag)
+
+
+# poisson()
+elasticity()
