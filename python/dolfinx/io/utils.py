f335b47f96873bd06f1db4ba27c416886f710ea4
Garth N. Wells <gnw20@cam.ac.uk>
2023-07-05 14:52:27 +0100

    Line break fixes

--- python/dolfinx/io/utils.py
+++ python/dolfinx/io/utils.py
@@ -75,9 +75,7 @@ if _cpp.common.has_adios2:
                 try:
                     dtype = output.function_space.mesh.geometry.x.dtype  # type: ignore
                 except AttributeError:
-                    # type: ignore
-                    # type: ignore
-                    dtype = output[0].function_space.mesh.geometry.x.dtype
+                    dtype = output[0].function_space.mesh.geometry.x.dtype  # type: ignore
 
             if dtype == np.float32:
                 _vtxwriter = _cpp.io.VTXWriter_float32
