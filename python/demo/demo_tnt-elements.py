642c228ba06977335fa44d9d1e252337b0a11d39
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2023-08-07 13:54:51 +0100

    update demo

--- python/demo/demo_tnt-elements.py
+++ python/demo/demo_tnt-elements.py
@@ -82,7 +82,7 @@ for v in topology[0]:
 # matrix.
 
 # +
-pts, wts = basix.make_quadrature(basix.CellType.interval, 2)
+pts, wts = basix.quadrature.make_quadrature(basix.CellType.interval, 2)
 for e in topology[1]:
     v0 = geometry[e[0]]
     v1 = geometry[e[1]]
@@ -146,7 +146,7 @@ def create_tnt_quad(degree):
         M[0].append(np.array([[[[1.]]]]))
 
     # Edges
-    pts, wts = basix.make_quadrature(basix.CellType.interval, 2 * degree)
+    pts, wts = basix.quadrature.make_quadrature(basix.CellType.interval, 2 * degree)
     poly = basix.tabulate_polynomials(basix.PolynomialType.legendre, basix.CellType.interval, degree - 1, pts)
     edge_ndofs = poly.shape[0]
     for e in topology[1]:
@@ -165,7 +165,7 @@ def create_tnt_quad(degree):
         x[2].append(np.zeros([0, 2]))
         M[2].append(np.zeros([0, 1, 0, 1]))
     else:
-        pts, wts = basix.make_quadrature(basix.CellType.quadrilateral, 2 * degree - 1)
+        pts, wts = basix.quadrature.make_quadrature(basix.CellType.quadrilateral, 2 * degree - 1)
         poly = basix.tabulate_polynomials(basix.PolynomialType.legendre, basix.CellType.quadrilateral, degree - 2, pts)
         face_ndofs = poly.shape[0]
         x[2].append(pts)
