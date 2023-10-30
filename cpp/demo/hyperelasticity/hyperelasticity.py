20a45a99a2eab67003678286a2e4c07b0c56b487
Francesco Ballarin <francesco.ballarin@unicatt.it>
2023-10-30 18:57:50 +0100

    Add `flake8` and `isort` checks on python files in `cpp` folder (#2838)
    * Add flake8 checks on python files in cpp folder
    
    * flake8 fixes in cpp
    
    * Add isort checks on python files in cpp folder
    
    * Move flake8 marker to root directory
    
    * Remove python/.flake8

--- cpp/demo/hyperelasticity/hyperelasticity.py
+++ cpp/demo/hyperelasticity/hyperelasticity.py
@@ -37,9 +37,9 @@ u = Coefficient(V)        # Displacement from previous iteration
 
 # Kinematics
 d = len(u)
-I = Identity(d)         # Identity tensor
+I = Identity(d)         # Identity tensor  # noqa: E741
 F = variable(I + grad(u))         # Deformation gradient
-C = F.T*F               # Right Cauchy-Green tensor
+C = F.T * F               # Right Cauchy-Green tensor
 
 # Invariants of deformation tensors
 Ic = tr(C)
@@ -52,18 +52,18 @@ J = det(F)
 # Elasticity parameters
 E = 10.0
 nu = 0.3
-mu = E/(2*(1 + nu))
-lmbda = E*nu/((1 + nu)*(1 - 2*nu))
+mu = E / (2 * (1 + nu))
+lmbda = E * nu / ((1 + nu) * (1 - 2 * nu))
 
 # Both the first variation of the potential energy, and the Jacobian of
 # the variation, can be automatically computed by a call to
 # ``derivative``::
 
 # Stored strain energy density (compressible neo-Hookean model)
-psi = (mu/2)*(Ic - 3) - mu*ln(J) + (lmbda/2)*(ln(J))**2
+psi = (mu / 2) * (Ic - 3) - mu * ln(J) + (lmbda / 2) * (ln(J))**2
 
 # Total potential energy
-Pi = psi*dx  # - inner(B, u)*dx - inner(T, u)*ds
+Pi = psi * dx  # - inner(B, u) * dx - inner(T, u) * ds
 
 # First variation of Pi (directional derivative about u in the direction of v)
 F_form = derivative(Pi, u, v)
@@ -72,7 +72,7 @@ F_form = derivative(Pi, u, v)
 J_form = derivative(F_form, u, du)
 
 # Compute Cauchy stress
-sigma = (1/J)*diff(psi, F)*F.T
+sigma = (1 / J) * diff(psi, F) * F.T
 
 forms = [F_form, J_form]
 elements = [e]
