7653580f79885649542122c08e2e4571862efe90
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2023-11-06 12:04:52 +0000

    parametrize test, and add test with points and weights input

--- python/test/unit/fem/test_quadrature_elements.py
+++ python/test/unit/fem/test_quadrature_elements.py
@@ -9,13 +9,47 @@ import ufl
 import numpy as np
 from mpi4py import MPI
 import basix.ufl
+import pytest
 
 
-def test_form():
+@pytest.mark.parametrize("degree", range(1, 4))
+def test_default(degree):
     msh = dolfinx.mesh.create_unit_square(MPI.COMM_WORLD, 10, 10)
 
     CG2_vect = dolfinx.fem.functionspace(msh, ("Lagrange", 1))
-    Qe = basix.ufl.quadrature_element(msh.topology.cell_name(), value_shape=(), scheme="default", degree=1)
+    Qe = basix.ufl.quadrature_element(msh.topology.cell_name(), value_shape=(), scheme="default", degree=degree)
+    Quad = dolfinx.fem.functionspace(msh, Qe)
+
+    u = dolfinx.fem.Function(Quad)
+    v = ufl.TrialFunction(CG2_vect)
+
+    dx_m = ufl.Measure("dx", domain=msh, metadata={"quadrature_degree": 1, "quadrature_scheme": "default"})
+    ds = ufl.Measure("ds", domain=msh)
+
+    residual = u * v * dx_m
+    vol = dolfinx.fem.form(residual)
+    residual = v * ds
+    surf = dolfinx.fem.form(residual)
+
+    residual = u * v * dx_m + v * ds
+    vol_surf = dolfinx.fem.form(residual)
+
+    vol_v = dolfinx.fem.assemble_vector(vol)
+    sur_v = dolfinx.fem.assemble_vector(surf)
+
+    vol_surf = dolfinx.fem.assemble_vector(vol_surf)
+
+    assert np.allclose(vol_v.array + sur_v.array, vol_surf.array)
+
+
+def test_points_and_weights():
+    msh = dolfinx.mesh.create_unit_square(MPI.COMM_WORLD, 10, 10)
+
+    CG2_vect = dolfinx.fem.functionspace(msh, ("Lagrange", 1))
+    Qe = basix.ufl.quadrature_element(
+        msh.topology.cell_name(), value_shape=(),
+        points=np.array([[0.0, 0.0], [1.0, 0.0], [0.0, 1.0], [1/3, 1/3]]),
+        weights=np.array([0.2, 0.2, 0.2, 0.4]),)
     Quad = dolfinx.fem.functionspace(msh, Qe)
 
     u = dolfinx.fem.Function(Quad)
