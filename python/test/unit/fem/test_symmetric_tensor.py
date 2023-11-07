f50937aced681eeb9241636676b67a8017d6d21b
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2023-11-07 14:53:33 +0000

    parametrize

--- python/test/unit/fem/test_symmetric_tensor.py
+++ python/test/unit/fem/test_symmetric_tensor.py
@@ -9,15 +9,15 @@ import ufl
 
 
 @pytest.mark.parametrize("degree", range(1, 4))
-def test_transpose(degree):
+@pytest.mark.parametrize("symmetry", [True, False])
+def test_transpose(degree, symmetry):
     mesh = dolfinx.mesh.create_unit_square(MPI.COMM_WORLD, 10, 10)
-    for symmetry in [True, False]:
-        e = basix.ufl.element("Lagrange", "triangle", degree, shape=(2, 2), symmetry=symmetry)
+    e = basix.ufl.element("Lagrange", "triangle", degree, shape=(2, 2), symmetry=symmetry)
 
-        space = dolfinx.fem.functionspace(mesh, e)
+    space = dolfinx.fem.functionspace(mesh, e)
 
-        f = dolfinx.fem.Function(space)
-        f.interpolate(lambda x: (x[0], x[1], x[0] ** 3, x[0]))
+    f = dolfinx.fem.Function(space)
+    f.interpolate(lambda x: (x[0], x[1], x[0] ** 3, x[0]))
 
-        form = dolfinx.fem.form(ufl.inner(f - ufl.transpose(f), f - ufl.transpose(f)) * ufl.dx)
-        assert np.isclose(dolfinx.fem.assemble_scalar(form), 0) == symmetry
+    form = dolfinx.fem.form(ufl.inner(f - ufl.transpose(f), f - ufl.transpose(f)) * ufl.dx)
+    assert np.isclose(dolfinx.fem.assemble_scalar(form), 0) == symmetry
