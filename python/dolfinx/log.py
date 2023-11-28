b606bfefe6ae9173d8f7d2008bbadda8628cfd88
Jack S. Hale <mail@jackhale.co.uk>
2023-11-28 15:05:58 +0100

    More mypy fixes

--- python/dolfinx/log.py
+++ python/dolfinx/log.py
@@ -6,7 +6,11 @@
 
 """Logging module."""
 
-# Import nanobind wrapped code intp dolfinx.log
+# Import nanobind wrapped code into dolfinx.log
 from dolfinx.cpp.log import (
+    LogLevel,  # noqa
+    get_log_level,  # noqa
     log,  # noqa
+    set_log_level,  # noqa
+    set_output_file,  # noqa
 )
