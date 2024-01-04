55c78855b8a02ab1874672a678b1d53df6ebbb75
Garth N. Wells <gnw20@cam.ac.uk>
2024-01-04 21:29:48 +0000

    Type updates

--- python/dolfinx/io/utils.py
+++ python/dolfinx/io/utils.py
@@ -239,7 +239,6 @@ class XDMFFile(_cpp.io.XDMFFile):
         msh = _cpp.mesh.create_mesh(self.comm, _cpp.graph.AdjacencyList_int64(cells),
                                     cmap, x, _cpp.mesh.create_cell_partitioner(ghost_mode))
         msh.name = name
-
         domain = ufl.Mesh(basix.ufl.element("Lagrange", cell_shape.name, cell_degree,
                                             basix.LagrangeVariant.equispaced, shape=(x.shape[1], ), gdim=x.shape[1]))
         return Mesh(msh, domain)
