4655a7a70ec0a57ecfe4f886ec1b44eac001e7f3
Chris Richardson <chris@bpi.cam.ac.uk>
2024-01-10 13:54:45 +0000

    more tests

--- python/test/unit/mesh/test_mixed_topology.py
+++ python/test/unit/mesh/test_mixed_topology.py
@@ -85,8 +85,16 @@ def test_parallel_mixed_mesh():
     topology = create_topology(MPI.COMM_WORLD, [CellType.triangle, CellType.quadrilateral],
                                cells, orig_index, ghost_owners, boundary_vertices)
 
-    print(topology.entity_types)
+    assert topology.entity_types[2][0] == CellType.triangle
+    assert topology.entity_types[2][1] == CellType.quadrilateral
     print(topology.connectivity(2, 0, 0, 0))
     print(topology.connectivity(2, 1, 0, 0))
+
     w = topology.index_map(0)
     print(w.local_to_global(np.arange(6, dtype=int)))
+
+    size = MPI.COMM_WORLD.Get_size()
+    assert topology.index_maps(2)[0].size_global == size * 2
+    assert topology.index_maps(2)[1].size_global == size
+
+    print(topology.index_maps(2)[0].size_global)
