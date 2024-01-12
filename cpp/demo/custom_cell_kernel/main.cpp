d51bf7ec6e18350690dca4773263b0fc6603db76
Jack S. Hale <mail@jackhale.co.uk>
2024-01-12 22:21:37 +0100

    Copy and paste FFCx kernel, could do something nicer in C++.

--- cpp/demo/custom_cell_kernel/main.cpp
+++ cpp/demo/custom_cell_kernel/main.cpp
@@ -45,8 +45,53 @@ int main(int argc, char* argv[])
     std::iota(cells.begin(), cells.end(), 0);
 
     // Define element kernel
-    auto mass_cell_kernel = [](double*, const double*, const double*,
-                               const double*, const int*, const u_int8_t*) {};
+    auto mass_cell_kernel = [](double* A, const double*, const double*,
+                               const double* coordinate_dofs, const int*,
+                               const u_int8_t*) { // Quadrature rules
+      static const double weights_48e[3]
+          = {0.1666666666666667, 0.1666666666666667, 0.1666666666666667};
+      // Precomputed values of basis functions and precomputations
+      // FE* dimensions: [permutation][entities][points][dofs]
+      static const double FE0_C0_Q48e[1][1][3][3]
+          = {{{{0.6666666666666667, 0.1666666666666667, 0.1666666666666666},
+               {0.1666666666666667, 0.1666666666666666, 0.6666666666666666},
+               {0.1666666666666668, 0.6666666666666666, 0.1666666666666666}}}};
+      static const double FE1_C0_D10_Q48e[1][1][1][3] = {{{{-1.0, 1.0, 0.0}}}};
+      static const double FE1_C1_D01_Q48e[1][1][1][3] = {{{{-1.0, 0.0, 1.0}}}};
+      // Quadrature loop independent computations for quadrature rule 48e
+      double J_c0 = 0.0;
+      double J_c3 = 0.0;
+      double J_c1 = 0.0;
+      double J_c2 = 0.0;
+      for (int ic = 0; ic < 3; ++ic)
+      {
+        J_c0 += coordinate_dofs[(ic) * 3] * FE1_C0_D10_Q48e[0][0][0][ic];
+        J_c3 += coordinate_dofs[(ic) * 3 + 1] * FE1_C1_D01_Q48e[0][0][0][ic];
+        J_c1 += coordinate_dofs[(ic) * 3] * FE1_C1_D01_Q48e[0][0][0][ic];
+        J_c2 += coordinate_dofs[(ic) * 3 + 1] * FE1_C0_D10_Q48e[0][0][0][ic];
+      }
+      double sp_48e[4];
+      sp_48e[0] = J_c0 * J_c3;
+      sp_48e[1] = J_c1 * J_c2;
+      sp_48e[2] = sp_48e[0] - sp_48e[1];
+      sp_48e[3] = fabs(sp_48e[2]);
+      for (int iq = 0; iq < 3; ++iq)
+      {
+        double fw0 = sp_48e[3] * weights_48e[iq];
+        double t0[3];
+        for (int i = 0; i < 3; ++i)
+        {
+          t0[i] = fw0 * FE0_C0_Q48e[0][0][iq][i];
+        }
+        for (int i = 0; i < 3; ++i)
+        {
+          for (int j = 0; j < 3; ++j)
+          {
+            A[3 * (i) + (j)] += FE0_C0_Q48e[0][0][iq][j] * t0[i];
+          }
+        }
+      }
+    };
     // More automatic type inference?
     const std::map<
         fem::IntegralType,
