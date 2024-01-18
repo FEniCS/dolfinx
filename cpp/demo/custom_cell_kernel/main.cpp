dda707fa99a5b9022d211b6a98bf8096013e11c9
Garth N. Wells <gnw20@cam.ac.uk>
2024-01-18 19:16:24 +0000

    Modularise with functions

--- cpp/demo/custom_cell_kernel/main.cpp
+++ cpp/demo/custom_cell_kernel/main.cpp
@@ -17,15 +17,86 @@
 
 using namespace dolfinx;
 
-using T = double; // field scalar type
-using U = typename dolfinx::scalar_value_type_t<T>;
-; // geometry scalar type
+using T = double;                                   // field scalar type
+using U = typename dolfinx::scalar_value_type_t<T>; // geometry scalar type
 
 template <typename T, std::size_t ndim>
 using mdspan_t = MDSPAN_IMPL_STANDARD_NAMESPACE::mdspan<
     T, MDSPAN_IMPL_STANDARD_NAMESPACE::dextents<std::size_t, ndim>>;
 
 // .. code-block:: cpp
+
+template <typename V>
+std::pair<std::vector<V>, std::array<std::size_t, 2>>
+A_ref(mdspan_t<const V, 4> basis, std::span<const V> weights)
+{
+  std::size_t dim = basis.extent(2);
+  std::array shape = {dim, dim};
+  std::vector<T> A_b(shape[0] * shape[1]);
+  mdspan_t<T, 2> A(A_b.data(), shape);
+
+  // einsum k,ki,kj->ij on weights, basis_span, basis_span
+  for (std::size_t k = 0; k < basis.extent(1); ++k)
+    for (std::size_t i = 0; i < A.extent(0); ++i)
+      for (std::size_t j = 0; j < A.extent(1); ++j)
+        A(i, j) += weights[k] * basis(0, k, i, 0) * basis(0, k, j, 0);
+
+  return {A_b, shape};
+}
+
+template <typename T>
+void assemble0(std::shared_ptr<fem::FunctionSpace<T>> V, auto kernel,
+               const std::vector<std::int32_t>& cells)
+{
+  // Define form from integral
+  using KernelFn = std::function<void(T*, const T*, const T*, const T*,
+                                      const int*, const u_int8_t*)>;
+  std::map integrals{
+      std::pair{fem::IntegralType::cell,
+                std::vector{std::tuple{-1, KernelFn(kernel), cells}}}};
+
+  std::shared_ptr<const mesh::Mesh<U>> mesh = V->mesh();
+  fem::Form<T> a({V, V}, integrals, {}, {}, false, mesh);
+
+  auto dofmap = V->dofmap();
+  auto sparsity = la::SparsityPattern(
+      MPI_COMM_WORLD, {dofmap->index_map, dofmap->index_map},
+      {dofmap->index_map_bs(), dofmap->index_map_bs()});
+  fem::sparsitybuild::cells(sparsity, cells, {*dofmap, *dofmap});
+  sparsity.finalize();
+  auto A = la::MatrixCSR<T>(sparsity);
+  {
+    common::Timer timer("Assembler0");
+    for (int i = 0; i < 100; ++i)
+      assemble_matrix(A.mat_add_values(), a, {});
+    A.scatter_rev();
+  }
+  std::cout << "Norm (0): " << A.squared_norm() << std::endl;
+}
+
+template <typename T>
+void assemble1(const fem::FunctionSpace<T>& V, auto kernel,
+               std::span<const std::int32_t> cells)
+{
+  auto mesh = V.mesh();
+  auto dofmap = V.dofmap();
+  auto sparsity = la::SparsityPattern(
+      MPI_COMM_WORLD, {dofmap->index_map, dofmap->index_map},
+      {dofmap->index_map_bs(), dofmap->index_map_bs()});
+  fem::sparsitybuild::cells(sparsity, cells, {*dofmap, *dofmap});
+  sparsity.finalize();
+  auto A = la::MatrixCSR<T>(sparsity);
+
+  auto ident = [](auto, auto, auto, auto) {};
+  const mesh::Geometry<U>& g = mesh->geometry();
+  common::Timer timer("Assembler1");
+  for (int i = 0; i < 100; ++i)
+    fem::impl::assemble_cells(A.mat_add_values(), g.dofmap(), g.x(), cells,
+                              ident, dofmap->map(), 1, ident, dofmap->map(), 1,
+                              {}, {}, kernel, std::span<const T>(), 0, {}, {});
+  A.scatter_rev();
+  std::cout << "Norm (1): " << A.squared_norm() << std::endl;
+}
 
 int main(int argc, char* argv[])
 {
@@ -37,7 +108,7 @@ int main(int argc, char* argv[])
         mesh::create_rectangle<U>(MPI_COMM_WORLD, {{{0.0, 0.0}, {1.0, 1.0}}},
                                   {64, 64}, mesh::CellType::triangle));
 
-    // Create basix element for the field u. This will be used to construct
+    // Create Basix element for the field u. This will be used to construct
     // basis functions inside the custom cell kernel.
     constexpr int order = 1;
     basix::FiniteElement e = basix::create_element<T>(
@@ -50,11 +121,11 @@ int main(int argc, char* argv[])
     constexpr int max_degree = 2 * order;
     auto quadrature_type = basix::quadrature::get_default_rule(
         basix::cell::type::triangle, max_degree);
-    auto [points, weights] = basix::quadrature::make_quadrature<T>(
+    auto [X_b, weights] = basix::quadrature::make_quadrature<T>(
         quadrature_type, basix::cell::type::triangle,
         basix::polyset::type::standard, max_degree);
     const auto num_points = static_cast<std::size_t>(weights.size());
-    mdspan_t<const T, 2> points_span(points.data(), num_points, 2);
+    mdspan_t<const T, 2> X(X_b.data(), num_points, 2);
 
     // Create a scalar function space
     auto V = std::make_shared<fem::FunctionSpace<U>>(
@@ -70,79 +141,25 @@ int main(int argc, char* argv[])
     auto tabulate_shape = e.tabulate_shape(0, num_points);
     const std::size_t length = std::accumulate(
         tabulate_shape.begin(), tabulate_shape.end(), 1, std::multiplies<>{});
-    std::vector<T> basis_buffer(length);
-    mdspan_t<T, 4> basis(basis_buffer.data(), tabulate_shape);
-
-    e.tabulate(0, points_span, basis);
-
-    // Calculate mass matrix on reference cell
-    const int e_dim = e.dim();
-    std::vector<T> A_hat_b(e_dim * e_dim);
-    mdspan_t<T, 2> A_hat(A_hat_b.data(), e.dim(), e.dim());
-
-    // einsum k,ki,kj->ij on weights, basis_span, basis_span
-    const std::size_t extent_0 = A_hat.extent(0);
-    const std::size_t extent_1 = A_hat.extent(1);
-    for (std::size_t k = 0; k < num_points; ++k)
-      for (std::size_t i = 0; i < A_hat.extent(0); ++i)
-        for (std::size_t j = 0; j < A_hat.extent(1); ++j)
-          A_hat(i, j) += weights[k] * basis(0, k, i, 0) * basis(0, k, j, 0);
+    std::vector<T> basis_b(length);
+    mdspan_t<T, 4> basis(basis_b.data(), tabulate_shape);
+    e.tabulate(0, X, basis);
 
     // Define finite element mass kernel.
-    auto mass_cell_kernel
-        = [A_hat, extent_0, extent_1](T* A, const T*, const T*, const U* cdofs,
-                                      const int*, const u_int8_t*)
+    auto [A_hat_b, A_shape] = A_ref<U>(basis, weights);
+    mdspan_t<T, 2> A_hat(A_hat_b.data(), A_shape);
+    auto mass_cell_kernel = [A_hat](T* A, const T*, const T*, const U* x,
+                                    const int*, const u_int8_t*)
     {
-      U detJ = std::abs((cdofs[0] - cdofs[3]) * (cdofs[7] - cdofs[4])
-                        - (cdofs[1] - cdofs[4]) * (cdofs[6] - cdofs[3]));
-      for (std::size_t i = 0; i < extent_0; ++i)
-        for (std::size_t j = 0; j < extent_1; ++j)
+      U detJ = std::abs((x[0] - x[3]) * (x[7] - x[4])
+                        - (x[1] - x[4]) * (x[6] - x[3]));
+      for (std::size_t i = 0; i < A_hat.extent(0); ++i)
+        for (std::size_t j = 0; j < A_hat.extent(1); ++j)
           A[i * A_hat.extent(0) + j] = detJ * A_hat(i, j);
     };
 
-    // Construct default domain integral
-    using KernelFn = std::function<void(T*, const T*, const T*, const U*,
-                                        const int*, const u_int8_t*)>;
-    const std::map integrals{std::pair{
-        fem::IntegralType::cell,
-        std::vector{std::tuple{-1, KernelFn(mass_cell_kernel), cells}}}};
-
-    // Define form from integral
-    auto a = std::make_shared<fem::Form<T>>(
-        fem::Form<T>({V, V}, integrals, {}, {}, false, mesh));
-
-    auto dofmap = V->dofmap();
-    auto sparsity = la::SparsityPattern(
-        MPI_COMM_WORLD, {dofmap->index_map, dofmap->index_map},
-        {dofmap->index_map_bs(), dofmap->index_map_bs()});
-    fem::sparsitybuild::cells(sparsity, cells, {*dofmap, *dofmap});
-    sparsity.finalize();
-    auto A = la::MatrixCSR<T>(sparsity);
-
-    {
-      common::Timer timer("Assembler0");
-      for (int i = 0; i < 100; ++i)
-        assemble_matrix(A.mat_add_values(), *a, {});
-      A.scatter_rev();
-    }
-    std::cout << "Norm (0): " << A.squared_norm() << std::endl;
-
-    A.set(0);
-    {
-      auto ident = [](auto, auto, auto, auto) {};
-      const mesh::Geometry<U>& g = mesh->geometry();
-      auto dofmap = a->function_spaces().at(0)->dofmap()->map();
-      common::Timer timer("Assembler1");
-      for (int i = 0; i < 100; ++i)
-        fem::impl::assemble_cells(
-            A.mat_add_values(), g.dofmap(), g.x(), cells, ident, dofmap, 1,
-            ident, dofmap, 1, std::span<const std::int8_t>(),
-            std::span<const std::int8_t>(), mass_cell_kernel,
-            std::span<const T>(), 0, std::span<const T>(),
-            std::span<const std::uint32_t>());
-      A.scatter_rev();
-    }
-    std::cout << "Norm (1): " << A.squared_norm() << std::endl;
+    assemble0<T>(V, mass_cell_kernel, cells);
+    assemble1<T>(*V, mass_cell_kernel, cells);
   }
 
   list_timings(MPI_COMM_WORLD, {TimingType::wall});
