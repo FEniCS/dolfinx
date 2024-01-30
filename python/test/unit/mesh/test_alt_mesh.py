5796b886e9be227279659e8f49cf7a32c62c5476
Chris Richardson <chris@bpi.cam.ac.uk>
2024-01-30 15:47:28 +0000

    hacking

--- python/test/unit/mesh/test_alt_mesh.py
+++ python/test/unit/mesh/test_alt_mesh.py
@@ -1,12 +1,26 @@
 from mpi4py import MPI
 
+import numpy as np
+
+import basix
+from dolfinx.cpp.fem import CoordinateElement_float64
 from dolfinx.cpp.mesh import create_quad_rectangle_float64
-from dolfinx.fem import coordinate_element
-from dolfinx.mesh import CellType
+
+
+def create_tensor_product_element(cell_type, degree, variant, gdim=2, shape=None):
+    """Create tensor product element."""
+    family = basix.ElementFamily.P
+    ref = basix.create_element(family, cell_type, degree, variant)
+    factors = ref.get_tensor_product_representation()[0]
+    perm = factors[1]
+    dof_ordering = np.argsort(perm)
+    element = basix.create_element(family, cell_type, degree, variant,
+                                   dof_ordering=dof_ordering)
+    return element
 
 
 def test_quad_rect():
-
-    cmap = coordinate_element(CellType.quadrilateral, 1)
-    mesh = create_quad_rectangle_float64(MPI.COMM_SELF, [[0.0, 0.0], [1.0, 1.0]], [20, 20], cmap._cpp_object, None)
+    element = create_tensor_product_element(basix.CellType.quadrilateral, 1, basix.LagrangeVariant.gll_warped)
+    cmap = CoordinateElement_float64(element._e)
+    mesh = create_quad_rectangle_float64(MPI.COMM_SELF, [[0.0, 0.0], [1.0, 1.0]], [20, 20], cmap, None)
     assert mesh
