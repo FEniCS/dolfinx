39955fdd3b9865030d8283127f3901054f2851f1
Chris Richardson <chris@bpi.cam.ac.uk>
2024-01-30 16:12:19 +0000

    more hacking

--- python/test/unit/mesh/test_alt_mesh.py
+++ python/test/unit/mesh/test_alt_mesh.py
@@ -3,11 +3,15 @@ from mpi4py import MPI
 import numpy as np
 
 import basix
+import ufl
 from dolfinx.cpp.fem import CoordinateElement_float64
 from dolfinx.cpp.mesh import create_quad_rectangle_float64
+from dolfinx.fem import CoordinateElement
+from dolfinx.io import XDMFFile
+from dolfinx.mesh import Mesh
 
 
-def create_tensor_product_element(cell_type, degree, variant, gdim=2, shape=None):
+def create_tensor_product_element(cell_type, degree, variant):
     """Create tensor product element."""
     family = basix.ElementFamily.P
     ref = basix.create_element(family, cell_type, degree, variant)
@@ -21,6 +25,13 @@ def create_tensor_product_element(cell_type, degree, variant, gdim=2, shape=None
 
 def test_quad_rect():
     element = create_tensor_product_element(basix.CellType.quadrilateral, 1, basix.LagrangeVariant.gll_warped)
-    cmap = CoordinateElement_float64(element._e)
-    mesh = create_quad_rectangle_float64(MPI.COMM_SELF, [[0.0, 0.0], [1.0, 1.0]], [20, 20], cmap, None)
-    assert mesh
+    cmap = CoordinateElement(CoordinateElement_float64(element._e))
+    _mesh = create_quad_rectangle_float64(MPI.COMM_SELF, [[0.0, 0.0], [1.0, 1.0]], [20, 20], cmap._cpp_object, None)
+
+    e_ufl = basix.ufl._BasixElement(element)
+    e_ufl = basix.ufl.blocked_element(e_ufl, shape=(2,), gdim=2)
+
+    mesh = Mesh(_mesh, ufl.Mesh(e_ufl))
+
+    xdmf = XDMFFile(mesh.comm, "example.xdmf", "w")
+    xdmf.write_mesh(mesh)
