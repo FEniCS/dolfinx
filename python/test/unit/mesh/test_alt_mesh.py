92ea6f22d3ddacb7ed114a65c4497eb25e8e58e5
Chris Richardson <chris@bpi.cam.ac.uk>
2024-01-31 10:08:19 +0000

    Minor tweaking

--- python/test/unit/mesh/test_alt_mesh.py
+++ python/test/unit/mesh/test_alt_mesh.py
@@ -15,8 +15,8 @@ def create_tensor_product_element(cell_type, degree, variant):
     """Create tensor product element."""
     family = basix.ElementFamily.P
     ref = basix.create_element(family, cell_type, degree, variant)
-    factors = ref.get_tensor_product_representation()[0]
-    perm = factors[1]
+    factors = ref.get_tensor_product_representation()
+    perm = factors[0][1]
     dof_ordering = np.argsort(perm)
     element = basix.create_element(family, cell_type, degree, variant,
                                    dof_ordering=dof_ordering)
@@ -29,11 +29,10 @@ def test_quad_rect():
     #                            basix.LagrangeVariant.gll_warped)
     element = create_tensor_product_element(basix.CellType.quadrilateral, 1, basix.LagrangeVariant.gll_warped)
     cmap = CoordinateElement_float64(element._e)
-    _mesh = create_quad_rectangle_float64(MPI.COMM_SELF, [[0.0, 0.0], [1.0, 1.0]], [20, 20], cmap, None)
+    _mesh = create_quad_rectangle_float64(MPI.COMM_WORLD, [[0.0, 0.0], [1.0, 1.0]], [20, 20], cmap, None)
 
     e_ufl = basix.ufl._BasixElement(element)
     e_ufl = basix.ufl.blocked_element(e_ufl, shape=(2,), gdim=2)
-
     mesh = Mesh(_mesh, ufl.Mesh(e_ufl))
 
     xdmf = XDMFFile(mesh.comm, "example.xdmf", "w")
