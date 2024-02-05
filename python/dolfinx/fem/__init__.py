c896025764f46c0f70c955ad9b65eb0275ea6f43
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2024-02-05 16:30:53 +0000

    fix

--- python/dolfinx/fem/__init__.py
+++ python/dolfinx/fem/__init__.py
@@ -5,9 +5,9 @@
 # SPDX-License-Identifier:    LGPL-3.0-or-later
 """Tools for assembling and manipulating finite element forms."""
 
-from dolfinx.cpp.assembly import discrete_gradient as _discrete_gradient
 from dolfinx.cpp.fem import IntegralType, create_nonmatching_meshes_interpolation_data
 from dolfinx.cpp.fem import create_sparsity_pattern as _create_sparsity_pattern
+from dolfinx.cpp.fem import discrete_gradient as _discrete_gradient
 from dolfinx.cpp.fem import transpose_dofmap
 from dolfinx.fem.assemble import (apply_lifting, assemble_matrix, assemble_scalar, assemble_vector, create_matrix,
                                   create_vector, set_bc)
