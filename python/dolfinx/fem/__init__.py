0b2e362317b656c8b590de73d75a2ec32a2e747a
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2024-02-05 17:20:24 +0000

    wrap create_nonmatching_meshes_interpolation_data

--- python/dolfinx/fem/__init__.py
+++ python/dolfinx/fem/__init__.py
@@ -5,7 +5,8 @@
 # SPDX-License-Identifier:    LGPL-3.0-or-later
 """Tools for assembling and manipulating finite element forms."""
 
-from dolfinx.cpp.fem import IntegralType, create_nonmatching_meshes_interpolation_data
+from dolfinx.cpp.fem import IntegralType
+from dolfinx.cpp.fem import create_nonmatching_meshes_interpolation_data as _create_nonmatching_meshes_interpolation_data
 from dolfinx.cpp.fem import create_sparsity_pattern as _create_sparsity_pattern
 from dolfinx.cpp.fem import transpose_dofmap
 from dolfinx.fem.assemble import (apply_lifting, assemble_matrix, assemble_scalar, assemble_vector, create_matrix,
@@ -32,6 +33,29 @@ def create_sparsity_pattern(a: Form):
 
     """
     return _create_sparsity_pattern(a._cpp_object)
+
+
+def create_nonmatching_meshes_interpolation_data(
+    mesh_to: typing.Union(Mesh, Geometry), element: FiniteElement, mesh_from: Mesh,
+    cells: typing.Optional[npt.NDArray[np.int32]] = None, padding: float = 1e-14
+):
+    """Generate data needed to interpolate discrete functions across different meshes.
+
+    Args:
+        mesh_to: Mesh or geometry to interpolate into
+        element: Element of the space to interpolate into
+        mesh_from: Mesh of the function to interpolate from
+        cells: Indices of the cells in the destination mesh on which to interpolate.
+        padding: Absolute padding of bounding boxes of all entities on mesh_to
+    """
+    if cells is None:
+        assert isinstance(mesh_to, Mesh)
+        _create_nonmatching_meshes_interpolation_data(
+            mesh_to._cpp_object, element, mesh_from._cpp_onject, padding)
+    else:
+        assert isinstance(mesh_to, Geometry)
+        _create_nonmatching_meshes_interpolation_data(
+            mesh_to._cpp_object, element, mesh_from._cpp_object, cells, padding)
 
 
 __all__ = [
