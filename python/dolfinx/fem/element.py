8a73be4c287d9289a0e7dc3f7116eb16e2e7fe72
Jack S. Hale <mail@jackhale.co.uk>
2024-02-07 13:45:36 +0100

    Ruff format mk2 (#3044)
    * Format, still some line too long errors?
    
    * Modify CI.
    
    * Remove flake8 and isort from image.
    
    * Format C++ part.
    
    * Fix remaining check issues.
    
    Removed periodic bc test file, no longer needed.
    
    * Fix mypy
    
    * Ruff.
    
    * mypy
    
    * Fixes for jupytext import and conf.py

--- python/dolfinx/fem/element.py
+++ python/dolfinx/fem/element.py
@@ -18,10 +18,14 @@ from dolfinx import cpp as _cpp
 class CoordinateElement:
     """Coordinate element describing the geometry map for mesh cells"""
 
-    _cpp_object: typing.Union[_cpp.fem.CoordinateElement_float32, _cpp.fem.CoordinateElement_float64]
+    _cpp_object: typing.Union[
+        _cpp.fem.CoordinateElement_float32, _cpp.fem.CoordinateElement_float64
+    ]
 
-    def __init__(self, cmap: typing.Union[_cpp.fem.CoordinateElement_float32,
-                                          _cpp.fem.CoordinateElement_float64]):
+    def __init__(
+        self,
+        cmap: typing.Union[_cpp.fem.CoordinateElement_float32, _cpp.fem.CoordinateElement_float64],
+    ):
         """Create a coordinate map element.
 
         Note:
@@ -32,7 +36,9 @@ class CoordinateElement:
         Args:
             cmap: A C++ CoordinateElement.
         """
-        assert isinstance(cmap, (_cpp.fem.CoordinateElement_float32, _cpp.fem.CoordinateElement_float64))
+        assert isinstance(
+            cmap, (_cpp.fem.CoordinateElement_float32, _cpp.fem.CoordinateElement_float64)
+        )
         self._cpp_object = cmap
 
     @property
@@ -47,9 +53,12 @@ class CoordinateElement:
 
 
 @singledispatch
-def coordinate_element(celltype: _cpp.mesh.CellType, degree: int,
-                       variant=int(basix.LagrangeVariant.unset),
-                       dtype: npt.DTypeLike = np.float64):
+def coordinate_element(
+    celltype: _cpp.mesh.CellType,
+    degree: int,
+    variant=int(basix.LagrangeVariant.unset),
+    dtype: npt.DTypeLike = np.float64,
+):
     """Create a Lagrange CoordinateElement from element metadata.
 
     Coordinate elements are typically used to create meshes.
