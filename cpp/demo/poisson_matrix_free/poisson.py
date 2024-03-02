3b16374c3c0ff6ad593ed22064c3edfc9d2c914a
Garth N. Wells <gnw20@cam.ac.uk>
2024-03-02 13:45:37 +0000

    C++ demo documentation refinements (#3068)
    * Categorise C++ demos
    
    * Test demo docstring with doxygen
    
    * Simplify demo
    
    * Improve copyright notices
    
    * Doc updates

--- cpp/demo/poisson_matrix_free/poisson.py
+++ cpp/demo/poisson_matrix_free/poisson.py
@@ -25,20 +25,20 @@ V = FunctionSpace(mesh, e)
 u = TrialFunction(V)
 v = TestFunction(V)
 
-# Define a constant RHS
+# Constant RHS
 f = Constant(V)
 
-# Define the bilinear and linear forms according to the
-# variational formulation of the equations::
+# Bilinear and linear forms according to the variational
+# formulation of the equations:
 a = inner(grad(u), grad(v)) * dx
 L = inner(f, v) * dx
 
-# Define linear form representing the action of the form "a" on
-# the coefficient "ui"
+# Linear form representing the action of the form `a`` on the
+# coefficient `ui`:`
 ui = Coefficient(V)
 M = action(a, ui)
 
-# Define form to compute the L2 norm of the error
+# Form to compute the L2 norm of the error
 usol = Coefficient(V)
 uexact = Coefficient(V)
 E = inner(usol - uexact, usol - uexact) * dx
