ebbf14ee7681e104c216a5a0aa83802260fbd908
Garth N. Wells <gnw20@cam.ac.uk>
2024-03-07 12:00:57 +0000

    Doc fix

--- cpp/dolfinx/common/IndexMap.h
+++ cpp/dolfinx/common/IndexMap.h
@@ -19,6 +19,8 @@ namespace dolfinx::common
 // Forward declaration
 class IndexMap;
 
+/// Enum to control preservation of ghost index ordering in
+/// sub-IndexMaps.
 enum class IndexMapSort : bool
 {
   sort = true,
