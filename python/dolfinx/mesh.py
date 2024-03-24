c64e280827c5ec700d28a6d6d2c6a1ec2f397607
Jørgen Schartum Dokken <dokken92@gmail.com>
2024-03-24 21:32:37 +0100

    Apply suggestions from code review

--- python/dolfinx/mesh.py
+++ python/dolfinx/mesh.py
@@ -449,7 +449,7 @@ def create_submesh(msh: Mesh, dim: int, entities: npt.NDArray[np.int32]):
         entities: Indices of entities in ``mesh`` to extract
     Returns:
         A quadruplet containing the submesh and three maps from the submesh to the parent mesh.
-        The first map is the entity map, the second map the vertex map (topology) and the third map
+        The first map is the entity map, the second map the vertex map (topology), and the third map
         is the node map (geometry).
     """
     submsh, entity_map, vertex_map, geom_map = _cpp.mesh.create_submesh(
