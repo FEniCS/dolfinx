46f0118a05c0edacdda1ee852e4b6f64dd33fed5
Jorgen S. Dokken <dokken@simula.no>
2024-04-08 14:54:32 +0000

    Fix submesh C++ wrapping

--- python/dolfinx/wrappers/mesh.cpp
+++ python/dolfinx/wrappers/mesh.cpp
@@ -301,12 +301,13 @@ void declare_mesh(nb::module_& m, std::string type)
       [](const dolfinx::mesh::Mesh<T>& mesh, int dim,
          nb::ndarray<const std::int32_t, nb::ndim<1>, nb::c_contig> entities)
       {
-        auto [submesh, e_map, v_map, g_map] = dolfinx::mesh::create_submesh(
+        auto submesh = dolfinx::mesh::create_submesh(
             mesh, dim, std::span(entities.data(), entities.size()));
-        auto _e_map = as_nbarray(e_map);
-        auto _v_map = as_nbarray(v_map);
-        auto _g_map = as_nbarray(g_map);
-        return std::tuple(submesh, _e_map, _v_map, _g_map);
+        auto _e_map = as_nbarray(std::move(std::get<1>(submesh)));
+        auto _v_map = as_nbarray(std::move(std::get<2>(submesh)));
+        auto _g_map = as_nbarray(std::move(std::get<3>(submesh)));
+        return std::tuple(std::move(std::get<0>(submesh)), _e_map, _v_map,
+                          _g_map);
       },
       nb::arg("mesh"), nb::arg("dim"), nb::arg("entities"));
 
