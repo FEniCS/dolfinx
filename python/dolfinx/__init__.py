1030c28410f3e2c2fb9d72a6bcd964491f0633d6
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2024-04-16 10:18:17 +0100

    update tests

--- python/dolfinx/__init__.py
+++ python/dolfinx/__init__.py
@@ -36,6 +36,9 @@ from dolfinx.common import (
 )
 from dolfinx.cpp import __version__
 
+import basix
+basix.default_scalar_type = default_real_type
+
 _cpp.common.init_logging(sys.argv)
 del _cpp, sys
 
