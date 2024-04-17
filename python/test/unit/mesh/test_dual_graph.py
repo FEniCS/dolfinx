115877b10026541a04d67440e294a8a6c28593c6
Jack S. Hale <mail@jackhale.co.uk>
2024-04-17 12:03:29 +0200

    Only works for int64 - leave possibility open for later generalisation here.

--- python/test/unit/mesh/test_dual_graph.py
+++ python/test/unit/mesh/test_dual_graph.py
@@ -5,16 +5,15 @@ import numpy as np
 from dolfinx import graph, mesh
 
 
-def to_adj(cells):
+def to_adj(cells, dtype):
     cflat = []
     coff = [0]
     for c in cells:
         cflat += c
         cc = coff[-1] + len(c)
         coff += [cc]
-    adj = graph.adjacencylist(np.array(cflat), np.array(coff))
+    adj = graph.adjacencylist(np.array(cflat, dtype=dtype), np.array(coff, dtype=dtype))
     return adj
-
 
 def test_dgrsph_1d():
     rank = MPI.COMM_WORLD.Get_rank()
@@ -25,7 +24,7 @@ def test_dgrsph_1d():
         x = 0
     # Circular chain of interval cells
     cells = [[n0, n0 + 1], [n0 + 1, n0 + 2], [n0 + 2, x]]
-    w = mesh.build_dual_graph(MPI.COMM_WORLD, mesh.CellType.interval, to_adj(cells))
+    w = mesh.build_dual_graph(MPI.COMM_WORLD, mesh.CellType.interval, to_adj(cells, np.int64))
     assert w.num_nodes == 3
     for i in range(w.num_nodes):
         assert len(w.links(i)) == 2
