e732e51003730c944d1ed00f7448353a2e2579a1
Garth N. Wells <gnw20@cam.ac.uk>
2024-04-19 13:07:21 +0100

    Re-activate commented lines in hyperelastic demo
    Resolves #2584

--- cpp/demo/hyperelasticity/hyperelasticity.py
+++ cpp/demo/hyperelasticity/hyperelasticity.py
@@ -15,8 +15,10 @@ from ufl import (
     derivative,
     det,
     diff,
+    ds,
     dx,
     grad,
+    inner,
     ln,
     tr,
     variable,
@@ -39,8 +41,8 @@ v = TestFunction(V)  # Test function
 
 # Functions
 u = Coefficient(V)  # Displacement from previous iteration
-# B = Coefficient(element)        # Body force per unit volume
-# T = Coefficient(element)        # Traction force on the boundary
+B = Coefficient(element)  # Body force per unit volume
+T = Coefficient(element)  # Traction force on the boundary
 
 # Now, we can define the kinematic quantities involved in the model:
 
@@ -72,7 +74,7 @@ lmbda = E * nu / ((1 + nu) * (1 - 2 * nu))
 psi = (mu / 2) * (Ic - 3) - mu * ln(J) + (lmbda / 2) * (ln(J)) ** 2
 
 # Total potential energy
-Pi = psi * dx  # - inner(B, u) * dx - inner(T, u) * ds
+Pi = psi * dx - inner(B, u) * dx - inner(T, u) * ds
 
 # First variation of Pi (directional derivative about u in the direction
 # of v)
