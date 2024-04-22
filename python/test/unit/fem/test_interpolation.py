a908db1d3747833d1887f9b6aa76ba644bf9a996
Jorgen S. Dokken <dokken@simula.no>
2024-04-22 17:57:17 +0000

    Modify test again

--- python/test/unit/fem/test_interpolation.py
+++ python/test/unit/fem/test_interpolation.py
@@ -1093,7 +1093,7 @@ def test_submesh_expression_interpolation():
 
     def modified_grad(x):
         grad = grad_ref_func(x)
-        return -3 * grad[1], 2 * grad[0]
+        return -grad[1], 2 * grad[0]
 
     tdim = mesh.topology.dim
     cells = locate_entities(mesh, tdim, left_locator)
@@ -1118,7 +1118,7 @@ def test_submesh_expression_interpolation():
     np.testing.assert_allclose(u_sub_exact.x.array, u_sub.x.array, atol=atol)
 
     # Map from sub to parent
-    W = functionspace(mesh, ("DQ", 2, (mesh.geometry.dim,)))
+    W = functionspace(mesh, ("DQ", 1, (mesh.geometry.dim,)))
     w = Function(W)
 
     cell_imap = mesh.topology.index_map(tdim)
@@ -1127,7 +1127,7 @@ def test_submesh_expression_interpolation():
     parent_to_sub[sub_to_parent] = np.arange(len(sub_to_parent))
 
     # Map exact solution (based on quadrature points) back to parent mesh
-    sub_vec = ufl.as_vector((-3 * u_sub_exact[1], 2 * u_sub_exact[0]))
+    sub_vec = ufl.as_vector((-u_sub_exact[1], 2 * u_sub_exact[0]))
     sub_expr = Expression(sub_vec, W.element.interpolation_points())
 
     # Mapping back needs to be restricted to the subset of cells in the submesh
