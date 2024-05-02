bbd737bce91610d9dc97887c8520ce62aae66bea
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2024-05-02 16:04:39 +0100

    -utils

--- cpp/dolfinx/fem/FiniteElement.cpp
+++ cpp/dolfinx/fem/FiniteElement.cpp
@@ -7,7 +7,7 @@
 #include "FiniteElement.h"
 #include <algorithm>
 #include <array>
-#include <basix/finite-element.h>
+#include <basix/finite-element-utils.h>
 #include <basix/interpolation.h>
 #include <basix/polyset.h>
 #include <dolfinx/common/log.h>
