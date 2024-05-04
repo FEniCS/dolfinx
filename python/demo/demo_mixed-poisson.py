bbde5846303e4e98a82a5f6ac7a3194ecab78c50
Garth N. Wells <gnw20@cam.ac.uk>
2024-05-04 08:24:00 +0100

    Lint

--- python/demo/demo_mixed-poisson.py
+++ python/demo/demo_mixed-poisson.py
@@ -162,7 +162,11 @@ problem = LinearProblem(
     a,
     L,
     bcs=bcs,
-    petsc_options={"ksp_type": "preonly", "pc_type": "lu", "pc_factor_mat_solver_type": "superlu_dist"},
+    petsc_options={
+        "ksp_type": "preonly",
+        "pc_type": "lu",
+        "pc_factor_mat_solver_type": "superlu_dist",
+    },
 )
 try:
     w_h = problem.solve()
