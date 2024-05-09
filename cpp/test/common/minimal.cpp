2fb569b47d355787b6a58b60de4586ee1a8a6e54
Jack S. Hale <mail@jackhale.co.uk>
2024-05-09 19:54:16 +0200

    A bit less minimal as last run worked.

--- cpp/test/common/minimal.cpp
+++ cpp/test/common/minimal.cpp
@@ -7,6 +7,8 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/generators/catch_generators.hpp>
 #include <dolfinx/common/MPI.h>
+#include <dolfinx/common/IndexMap.h>
+#include <vector>
 
 using namespace dolfinx;
 
@@ -16,6 +18,19 @@ void minimal()
 {
   const int mpi_size = dolfinx::MPI::size(MPI_COMM_WORLD);
   const int mpi_rank = dolfinx::MPI::rank(MPI_COMM_WORLD);
+  const int size_local = 100;
+  
+  // Create some ghost entries on next process
+  int num_ghosts = (mpi_size - 1) * 3;
+  std::vector<std::int64_t> ghosts(num_ghosts);
+  for (int i = 0; i < num_ghosts; ++i)
+    ghosts[i] = (mpi_rank + 1) % mpi_size * size_local + i;
+
+  std::vector<int> global_ghost_owner(ghosts.size(), (mpi_rank + 1) % mpi_size);
+
+  // Create an IndexMap
+  const common::IndexMap idx_map(MPI_COMM_WORLD, size_local, ghosts,
+                                 global_ghost_owner);
 }
 
 TEST_CASE("Minimal", "[minimal]")
