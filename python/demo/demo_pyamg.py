1cc7d70108a0454bf6f74d53380e265cb29fb878
Chris Richardson <chris@bpi.cam.ac.uk>
2024-05-13 14:29:14 +0100

    Elasticity

--- python/demo/demo_pyamg.py
+++ python/demo/demo_pyamg.py
@@ -51,7 +51,7 @@ dtype = np.float64
 mesh = create_box(
     comm=MPI.COMM_WORLD,
     points=[(0.0, 0.0, 0.0), (3.0, 2.0, 1.0)],
-    n=[60, 40, 20],
+    n=[30, 20, 10],
     cell_type=CellType.tetrahedron,
     dtype=dtype,
 )
@@ -91,5 +91,79 @@ for i, q in enumerate(res):
     print(f"Iteration {i}, residual= {q}")
 
 with io.XDMFFile(mesh.comm, "out_pyamg/poisson.xdmf", "w") as file:
+    file.write_mesh(mesh)
+    file.write_function(uh)
+
+
+# Now try an elasticity problem on the same mesh
+
+
+def nullspace(Q):
+    # Nullspace
+    B = np.zeros((Q.dofmap.index_map.size_local * Q.dofmap.bs, 6))
+
+    # Get dof indices for each subspace (x, y and z dofs)
+    dofs = [Q.sub(i).dofmap.list.flatten() for i in range(3)]
+
+    # Set the three translational rigid body modes
+    for i in range(3):
+        B[dofs[i], i] = 1.0
+
+    # Set the three rotational rigid body modes
+    x = Q.tabulate_dof_coordinates()
+    dofs_block = Q.dofmap.list.flatten()
+    x0, x1, x2 = x[dofs_block, 0], x[dofs_block, 1], x[dofs_block, 2]
+    B[dofs[0], 3] = -x1
+    B[dofs[1], 3] = x0
+    B[dofs[0], 4] = x2
+    B[dofs[2], 4] = -x0
+    B[dofs[2], 5] = x1
+    B[dofs[1], 5] = -x2
+    return B
+
+
+ω, ρ = 300.0, 10.0
+x = ufl.SpatialCoordinate(mesh)
+f = ufl.as_vector((ρ * ω**2 * x[0], ρ * ω**2 * x[1], 0.0))
+
+# Define the elasticity parameters and create a function that computes
+# an expression for the stress given a displacement field.
+
+E = 1.0e9
+ν = 0.3
+μ = E / (2.0 * (1.0 + ν))
+λ = E * ν / ((1.0 + ν) * (1.0 - 2.0 * ν))
+
+
+def σ(v):
+    """Return an expression for the stress σ given a displacement field"""
+    return 2.0 * μ * ufl.sym(grad(v)) + λ * ufl.tr(ufl.sym(grad(v))) * ufl.Identity(len(v))
+
+
+V = functionspace(mesh, ("Lagrange", 1, (mesh.geometry.dim,)))
+u, v = ufl.TrialFunction(V), ufl.TestFunction(V)
+a = form(inner(σ(u), grad(v)) * dx)
+L = form(inner(f, v) * dx)
+
+bc = dirichletbc(
+    np.zeros(3, dtype=dtype), locate_dofs_topological(V, entity_dim=tdim - 1, entities=facets), V=V
+)
+
+A = assemble_matrix(a, bcs=[bc]).to_scipy()
+b = assemble_vector(L)
+apply_lifting(b.array, [a], bcs=[[bc]])
+set_bc(b.array, [bc])
+
+uh = fem.Function(V, dtype=dtype)
+B = nullspace(V)
+ml = pyamg.smoothed_aggregation_solver(A, B=B)
+print(ml)
+
+res_e: list[float] = []
+uh.x.array[:] = ml.solve(b.array, tol=1e-10, residuals=res_e, accel="cg")
+for i, q in enumerate(res_e):
+    print(f"Iteration {i}, residual= {q}")
+
+with io.XDMFFile(mesh.comm, "out_pyamg/elasticity.xdmf", "w") as file:
     file.write_mesh(mesh)
     file.write_function(uh)
