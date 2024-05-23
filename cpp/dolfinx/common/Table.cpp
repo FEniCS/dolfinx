924c98ce928856a7cf1bccd62846e03a882ce255
Garth N. Wells <gnw20@cam.ac.uk>
2024-05-23 07:32:01 +0100

    Fixes for Doxygen 1.11

--- cpp/dolfinx/common/Table.cpp
+++ cpp/dolfinx/common/Table.cpp
@@ -5,8 +5,8 @@
 // SPDX-License-Identifier:    LGPL-3.0-or-later
 
 #include "Table.h"
+#include "MPI.h"
 #include <array>
-#include <dolfinx/common/MPI.h>
 #include <functional>
 #include <iostream>
 #include <map>
