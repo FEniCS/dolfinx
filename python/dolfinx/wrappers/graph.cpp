42c43485e81ada306f0b3f1dc735d95539174cbc
Francesco Ballarin <francesco.ballarin@unicatt.it>
2024-05-24 09:02:41 +0200

    Replace `nb::any` with -1 and add argument to `nb::ndarray` in preparation of next nanobind release (#3105)
    * Replace nb::any with -1 in preparation of next nanobind release
    
    * nb::ndarray now requires to pass in the owner argument, which used to have a default value. Assign the former defaul value everywhere in nb wrappers
    
    * DROP BEFORE MERGE: install nanobind from source in workflows
    
    * CI update
    
    * Bump min nanobind version
    
    * CI udpates
    
    * System install
    
    * pip install system
    
    * Remove CI change
    
    * More updates
    
    * cmake update
    
    * Run clang-format
    
    ---------
    
    Co-authored-by: Garth N. Wells <gnw20@cam.ac.uk>

--- python/dolfinx/wrappers/graph.cpp
+++ python/dolfinx/wrappers/graph.cpp
@@ -82,7 +82,8 @@ void declare_adjacency_list(nb::module_& m, std::string type)
           [](const dolfinx::graph::AdjacencyList<T>& self, int i)
           {
             std::span<const T> link = self.links(i);
-            return nb::ndarray<const T, nb::numpy>(link.data(), {link.size()});
+            return nb::ndarray<const T, nb::numpy>(link.data(), {link.size()},
+                                                   nb::handle());
           },
           nb::rv_policy::reference_internal, nb::arg("i"),
           "Links (edges) of a node")
@@ -90,8 +91,8 @@ void declare_adjacency_list(nb::module_& m, std::string type)
           "array",
           [](const dolfinx::graph::AdjacencyList<T>& self)
           {
-            return nb::ndarray<const T, nb::numpy>(self.array().data(),
-                                                   {self.array().size()});
+            return nb::ndarray<const T, nb::numpy>(
+                self.array().data(), {self.array().size()}, nb::handle());
           },
           nb::rv_policy::reference_internal)
       .def_prop_ro(
@@ -99,7 +100,7 @@ void declare_adjacency_list(nb::module_& m, std::string type)
           [](const dolfinx::graph::AdjacencyList<T>& self)
           {
             return nb::ndarray<const std::int32_t, nb::numpy>(
-                self.offsets().data(), {self.offsets().size()});
+                self.offsets().data(), {self.offsets().size()}, nb::handle());
           },
           nb::rv_policy::reference_internal)
       .def_prop_ro("num_nodes", &dolfinx::graph::AdjacencyList<T>::num_nodes)
