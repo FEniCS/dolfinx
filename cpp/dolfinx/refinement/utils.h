99dcd248fddc053dd2c4cb535a0f5bd42d149b88
Chris Richardson <chris@bpi.cam.ac.uk>
2024-06-01 12:28:06 +0100

    Send multiple cell types to partitioner (#3237)
    * Use vector not AdjList
    
    * Use vector<vector>
    
    * Use span and ndarray
    
    * Use nb::numpy
    
    * Use back_inserter
    
    * Update documentation
    
    * Add test
    
    * Apply suggestions

--- cpp/dolfinx/refinement/utils.h
+++ cpp/dolfinx/refinement/utils.h
@@ -291,11 +291,13 @@ mesh::Mesh<T> partition(const mesh::Mesh<T>& old_mesh,
   else
   {
     auto partitioner
-        = [](MPI_Comm comm, int, mesh::CellType,
-             const graph::AdjacencyList<std::int64_t>& cell_topology)
+        = [](MPI_Comm comm, int, const std::vector<mesh::CellType>& cell_types,
+             const std::vector<std::span<const std::int64_t>>& cell_topology)
     {
-      const int mpi_rank = MPI::rank(comm);
-      const int num_cells = cell_topology.num_nodes();
+      std::int32_t mpi_rank = MPI::rank(comm);
+      std::int32_t num_cell_vertices
+          = mesh::num_cell_vertices(cell_types.front());
+      std::int32_t num_cells = cell_topology.front().size() / num_cell_vertices;
       std::vector<std::int32_t> destinations(num_cells, mpi_rank);
       std::vector<std::int32_t> dest_offsets(num_cells + 1);
       std::iota(dest_offsets.begin(), dest_offsets.end(), 0);
