FEniCS/dolfinx

commit 6f5af2066bff9e5f92124b4c03cb71d3cccea5b5

Simplify integration domain packing (#3215)
* Rewrite integration domain packing

* Simplify compute_integration_domains and fix #3213

* Apply suggestions from code review

Co-authored-by: Joe Dean <jpd62@cam.ac.uk>

* Remove assertion on lower bound

* Apply suggestions from code review

* use better naming for creating subdomains dictionary

* Sort subdomain ids

---------

Co-authored-by: Joe Dean <jpd62@cam.ac.uk>

cpp/dolfinx/fem/utils.cpp
@@ -128,11 +128,9 @@ std::vector<std::string> fem::get_constant_names(const ufcx_form& ufcx_form)
                                       + ufcx_form.num_constants);
 }
 //-----------------------------------------------------------------------------
-std::vector<std::pair<int, std::vector<std::int32_t>>>
-fem::compute_integration_domains(fem::IntegralType integral_type,
-                                 const mesh::Topology& topology,
-                                 std::span<const std::int32_t> entities,
-                                 int dim, std::span<const int> values)
+std::vector<std::int32_t> fem::compute_integration_domains(
+    fem::IntegralType integral_type, const mesh::Topology& topology,
+    std::span<const std::int32_t> entities, int dim)
 {
   const int tdim = topology.dim();
   if ((integral_type == IntegralType::cell ? tdim : tdim - 1) != dim)
@@ -147,16 +145,13 @@ fem::compute_integration_domains(fem::IntegralType integral_type,
     auto it1 = std::lower_bound(it0, entities.end(),
                                 topology.index_map(dim)->size_local());
     entities = entities.first(std::distance(it0, it1));
-    values = values.first(std::distance(it0, it1));
   }
 
   std::vector<std::int32_t> entity_data;
-  std::vector<int> values1;
   switch (integral_type)
   {
   case IntegralType::cell:
     entity_data.insert(entity_data.begin(), entities.begin(), entities.end());
-    values1.insert(values1.begin(), values.begin(), values.end());
     break;
   default:
     auto f_to_c = topology.connectivity(tdim - 1, tdim);
@@ -183,13 +178,10 @@ fem::compute_integration_domains(fem::IntegralType integral_type,
                             bfacets.end(), std::back_inserter(facets));
       for (auto f : facets)
       {
-        auto index_it = std::lower_bound(entities.begin(), entities.end(), f);
-        assert(index_it != entities.end() and *index_it == f);
-        std::size_t pos = std::distance(entities.begin(), index_it);
+        // Get the facet as a pair of (cell, local facet)
         auto facet
             = impl::get_cell_facet_pairs<1>(f, f_to_c->links(f), *c_to_f);
         entity_data.insert(entity_data.end(), facet.begin(), facet.end());
-        values1.push_back(values[pos]);
       }
     }
     break;
@@ -205,7 +197,6 @@ fem::compute_integration_domains(fem::IntegralType integral_type,
           auto facets
               = impl::get_cell_facet_pairs<2>(f, f_to_c->links(f), *c_to_f);
           entity_data.insert(entity_data.end(), facets.begin(), facets.end());
-          values1.push_back(values[j]);
         }
       }
     }
@@ -215,42 +206,6 @@ fem::compute_integration_domains(fem::IntegralType integral_type,
           "Cannot compute integration domains. Integral type not supported.");
     }
   }
-
-  // Build permutation that sorts by meshtag value
-  std::vector<std::int32_t> perm(values1.size());
-  std::iota(perm.begin(), perm.end(), 0);
-  std::stable_sort(perm.begin(), perm.end(), [&values1](auto p0, auto p1)
-                   { return values1[p0] < values1[p1]; });
-
-  std::size_t shape = 1;
-  if (integral_type == IntegralType::exterior_facet)
-    shape = 2;
-  else if (integral_type == IntegralType::interior_facet)
-    shape = 4;
-  std::vector<std::pair<int, std::vector<std::int32_t>>> integrals;
-  {
-    // Iterator to mark the start of the group
-    auto p0 = perm.begin();
-    while (p0 != perm.end())
-    {
-      auto id0 = values1[*p0];
-      auto p1 = std::find_if_not(p0, perm.end(), [id0, &values1](auto idx)
-                                 { return id0 == values1[idx]; });
-
-      std::vector<std::int32_t> data;
-      data.reserve(shape * std::distance(p0, p1));
-      for (auto it = p0; it != p1; ++it)
-      {
-        auto e_it0 = std::next(entity_data.begin(), (*it) * shape);
-        auto e_it1 = std::next(e_it0, shape);
-        data.insert(data.end(), e_it0, e_it1);
-      }
-
-      integrals.push_back({id0, std::move(data)});
-      p0 = p1;
-    }
-  }
-
-  return integrals;
+  return entity_data;
 }
 //-----------------------------------------------------------------------------

cpp/dolfinx/fem/utils.h
@@ -99,18 +99,14 @@ get_cell_facet_pairs(std::int32_t f, std::span<const std::int32_t> cells,
 /// @param[in] topology Mesh topology
 /// @param[in] entities List of tagged mesh entities
 /// @param[in] dim Topological dimension of tagged entities
-/// @param[in] values Value associated with each entity
-/// @return List of `(integral id, entities)` pairs
-/// @pre The topological dimension of the integral entity type and the
-/// topological dimension of mesh tag data must be equal.
+/// @return List of integration entities
 /// @pre For facet integrals, the topology facet-to-cell and
 /// cell-to-facet connectivity must be computed before calling this
 /// function.
-std::vector<std::pair<int, std::vector<std::int32_t>>>
+std::vector<std::int32_t>
 compute_integration_domains(IntegralType integral_type,
                             const mesh::Topology& topology,
-                            std::span<const std::int32_t> entities, int dim,
-                            std::span<const int> values);
+                            std::span<const std::int32_t> entities, int dim);
 
 /// @brief Extract test (0) and trial (1) function spaces pairs for each
 /// bilinear form for a rectangular array of forms.

python/dolfinx/fem/forms.py
@@ -8,6 +8,7 @@ from __future__ import annotations
 
 import collections
 import typing
+from itertools import chain
 
 import numpy as np
 import numpy.typing as npt
@@ -199,21 +200,43 @@ def form(
         ]
         constants = [c._cpp_object for c in form.constants()]
 
-        # NOTE Could remove this and let the user convert meshtags by
-        # calling compute_integration_domains themselves
-        def get_integration_domains(integral_type, subdomain):
+        # Make map from integral_type to subdomain id
+        subdomain_ids = {type: [] for type in sd.get(domain).keys()}
+        for integral in form.integrals():
+            if integral.subdomain_data() is not None:
+                subdomain_ids[integral.integral_type()].append(integral.subdomain_id())
+        # Chain and sort subdomain ids
+        for itg_type, marker_ids in subdomain_ids.items():
+            flattened_ids = list(chain(marker_ids))
+            flattened_ids.sort()
+            subdomain_ids[itg_type] = flattened_ids
+
+        def get_integration_domains(integral_type, subdomain, subdomain_ids):
             """Get integration domains from subdomain data"""
             if subdomain is None:
                 return []
             else:
+                domains = []
                 try:
                     if integral_type in (IntegralType.exterior_facet, IntegralType.interior_facet):
                         tdim = subdomain.topology.dim
                         subdomain._cpp_object.topology.create_connectivity(tdim - 1, tdim)
                         subdomain._cpp_object.topology.create_connectivity(tdim, tdim - 1)
-                    domains = _cpp.fem.compute_integration_domains(
-                        integral_type, subdomain._cpp_object
-                    )
+                    # Compute integration domains only for each subdomain id in the integrals
+                    # If a process has no integral entities, insert an empty array
+                    for id in subdomain_ids:
+                        try:
+                            integration_entities = _cpp.fem.compute_integration_domains(
+                                integral_type,
+                                subdomain._cpp_object.topology,
+                                subdomain.find(id),
+                                subdomain.dim,
+                            )
+                            domains.append((id, integration_entities))
+
+                        except TypeError:
+                            pass  # If subdomain id is "everywhere"
+
                     return [(s[0], np.array(s[1])) for s in domains]
                 except AttributeError:
                     return [(s[0], np.array(s[1])) for s in subdomain]
@@ -221,7 +244,7 @@ def form(
         # Subdomain markers (possibly empty list for some integral types)
         subdomains = {
             _ufl_to_dolfinx_domain[key]: get_integration_domains(
-                _ufl_to_dolfinx_domain[key], subdomain_data[0]
+                _ufl_to_dolfinx_domain[key], subdomain_data[0], subdomain_ids[key]
             )
             for (key, subdomain_data) in sd.get(domain).items()
         }

python/dolfinx/wrappers/fem.cpp
@@ -36,6 +36,7 @@
 #include <nanobind/stl/function.h>
 #include <nanobind/stl/map.h>
 #include <nanobind/stl/pair.h>
+#include <nanobind/stl/set.h>
 #include <nanobind/stl/shared_ptr.h>
 #include <nanobind/stl/string.h>
 #include <nanobind/stl/tuple.h>
@@ -706,7 +707,15 @@ void declare_form(nb::module_& m, std::string type)
       .def_prop_ro("mesh", &dolfinx::fem::Form<T, U>::mesh)
       .def_prop_ro("function_spaces",
                    &dolfinx::fem::Form<T, U>::function_spaces)
-      .def("integral_ids", &dolfinx::fem::Form<T, U>::integral_ids)
+      .def(
+          "integral_ids",
+          [](const dolfinx::fem::Form<T, U>& self,
+             dolfinx::fem::IntegralType type)
+          {
+            auto ids = self.integral_ids(type);
+            return dolfinx_wrappers::as_nbarray(std::move(ids));
+          },
+          nb::arg("type"))
       .def_prop_ro("integral_types", &dolfinx::fem::Form<T, U>::integral_types)
       .def_prop_ro("needs_facet_permutations",
                    &dolfinx::fem::Form<T, U>::needs_facet_permutations)
@@ -1114,13 +1123,17 @@ void fem(nb::module_& m)
   m.def(
       "compute_integration_domains",
       [](dolfinx::fem::IntegralType type,
-         const dolfinx::mesh::MeshTags<int>& meshtags)
+         const dolfinx::mesh::Topology& topology,
+         const nb::ndarray<const std::int32_t, nb::ndim<1>, nb::c_contig>
+             entities,
+         int dim)
       {
-        return dolfinx::fem::compute_integration_domains(
-            type, *meshtags.topology(), meshtags.indices(), meshtags.dim(),
-            meshtags.values());
+        auto integration_entities = dolfinx::fem::compute_integration_domains(
+            type, topology, std::span(entities.data(), entities.size()), dim);
+        return dolfinx_wrappers::as_nbarray(std::move(integration_entities));
       },
-      nb::arg("integral_type"), nb::arg("meshtags"));
+      nb::arg("integral_type"), nb::arg("topology"), nb::arg("entities"),
+      nb::arg("dim"));
 
   // dolfinx::fem::ElementDofLayout
   nb::class_<dolfinx::fem::ElementDofLayout>(

commit bb8c8c1e4efa0e87b95a951814c65666eb302ca4

Add test to show regression introduced in #3215

python/test/unit/fem/test_assemble_domains.py
@@ -116,6 +116,16 @@ def test_assembly_dx_domains(mode, meshtags_factory):
     s2 = mesh.comm.allreduce(s2, op=MPI.SUM)
     assert s == pytest.approx(s2, rel=1.0e-6)
 
+    # Assemble scalar, using both dx("everywhere") and dx(i), i = 1, 2, 3
+    L = form(w * (dx(1) + dx(2) + dx(3) + dx))
+    s = assemble_scalar(L)
+    s = mesh.comm.allreduce(s, op=MPI.SUM)
+    assert s == pytest.approx(0.5, rel=1.0e-6)
+    L2 = form(2 * w * dx)
+    s2 = assemble_scalar(L2)
+    s2 = mesh.comm.allreduce(s2, op=MPI.SUM)
+    assert s == pytest.approx(s2, rel=1.0e-6)
+
 
 @pytest.mark.parametrize("mode", [GhostMode.none, GhostMode.shared_facet])
 def test_assembly_ds_domains(mode):