3d8336e27bf8a6e03286d0c5d61d73a1b748b6e4
schnellerhase <56360279+schnellerhase@users.noreply.github.com>
2024-06-05 20:19:13 +0200

    Make compile time options compile time constants (#3246)
    * Remove non implemented declaration
    
    * Make compile time flags compile time constants
    
    * Fix git version macro
    
    * Add has_ptscotch
    
    * Fix include error
    
    * Add has_complex_kernels
    
    * has_complex_kernels -> has_complex_ufcx_kernels
    
    * Fix logic order
    
    * Apply docstring suggestion
    
    * Add missing docstring
    
    ---------
    
    Co-authored-by: Jørgen Schartum Dokken <dokken92@gmail.com>
    Co-authored-by: Garth N. Wells <gnw20@cam.ac.uk>

--- cpp/dolfinx/common/defines.h
+++ cpp/dolfinx/common/defines.h
@@ -23,24 +23,86 @@ std::string git_commit_hash();
 
 /// Return true if DOLFINx is compiled in debugging mode,
 /// i.e., with assertions on
-bool has_debug();
+consteval bool has_debug()
+{
+#ifndef NDEBUG
+  return true;
+#else
+  return false;
+#endif
+}
 
 /// Return true if DOLFINx is compiled with PETSc
-bool has_petsc();
+consteval bool has_petsc()
+{
+#ifdef HAS_PETSC
+  return true;
+#else
+  return false;
+#endif
+}
 
 /// Return true if DOLFINx is compiled with SLEPc
-bool has_slepc();
-
-/// Return true if DOLFINx is compiled with Scotch
-bool has_scotch();
+consteval bool has_slepc()
+{
+#ifdef HAS_SLEPC
+  return true;
+#else
+  return false;
+#endif
+}
 
 /// Return true if DOLFINx is compiled with ParMETIS
-bool has_parmetis();
+consteval bool has_parmetis()
+{
+#ifdef HAS_PARMETIS
+  return true;
+#else
+  return false;
+#endif
+}
 
 /// Return true if DOLFINx is compiled with KaHIP
-bool has_kahip();
+consteval bool has_kahip()
+{
+#ifdef HAS_KAHIP
+  return true;
+#else
+  return false;
+#endif
+}
 
 /// Return true if DOLFINX is compiled with ADIOS2
-bool has_adios2();
+consteval bool has_adios2()
+{
+#ifdef HAS_ADIOS2
+  return true;
+#else
+  return false;
+#endif
+}
+
+/// Return true if DOLFINX is compiled with PT-SCOTCH
+consteval bool has_ptscotch()
+{
+#ifdef HAS_PTSCOTCH
+  return true;
+#else
+  return false;
+#endif
+}
+
+/// Return true if DOLFINx supports UFCx kernels with arguments of type C99
+/// _Complex. When DOLFINx was built with MSVC this returns false. This
+/// returning false does not preclude using DOLFINx with kernels accepting
+/// std::complex.
+consteval bool has_complex_ufcx_kernels()
+{
+#ifdef DOLFINX_STDC_NO_COMPLEX_KERNELS
+  return false;
+#else
+  return true;
+#endif
+}
 
 } // namespace dolfinx
