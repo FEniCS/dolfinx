bf4080287727e371b4282f4cebf07807469e262d
ampdes <abdullahm225@gmail.com>
2024-06-19 02:59:55 +0200

    Add checkpointing demo

---
+++ cpp/demo/checkpointing/main.cpp
@@ -0,0 +1,25 @@
+// # Checkpointing
+//
+
+#include <dolfinx.h>
+#include <mpi.h>
+
+using namespace dolfinx;
+
+int main(int argc, char* argv[])
+{
+  dolfinx::init_logging(argc, argv);
+  MPI_Init(&argc, &argv);
+
+  // Create mesh and function space
+  auto part = mesh::create_cell_partitioner(mesh::GhostMode::shared_facet);
+  auto mesh = std::make_shared<mesh::Mesh<float>>(
+      mesh::create_rectangle<float>(MPI_COMM_WORLD, {{{0.0, 0.0}, {1.0, 1.0}}},
+                                {4, 4}, mesh::CellType::quadrilateral, part));
+
+  io::checkpointing::write(MPI_COMM_WORLD, "mesh.bp", "mesh-write", mesh);
+
+  MPI_Finalize();
+  return 0;
+
+}
