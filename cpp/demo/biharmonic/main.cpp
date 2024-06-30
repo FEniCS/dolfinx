ce727611385977c17c04aa59ddb86c976f2498a0
schnellerhase <56360279+schnellerhase@users.noreply.github.com>
2024-06-30 19:53:53 +0200

    Fix biharmonic

--- cpp/demo/biharmonic/main.cpp
+++ cpp/demo/biharmonic/main.cpp
@@ -207,7 +207,7 @@ int main(int argc, char* argv[])
     //  follows:
 
     //  Define boundary condition
-    auto facets = mesh::exterior_facet_indices(*mesh);
+    auto facets = mesh::exterior_facet_indices(*mesh->topology());
     const auto bdofs = fem::locate_dofs_topological(
         *V->mesh()->topology_mutable(), *V->dofmap(), 1, facets);
     auto bc = std::make_shared<const fem::DirichletBC<T>>(0.0, bdofs, V);
