90d5b483ec06cc86b092e6625feda294670e3e54
Massimiliano Leoni <mleoni@proximafusion.com>
2024-07-17 11:32:35 +0200

    Fix a bug and create mesh within test

--- cpp/test/mesh/read_named_meshtags.cpp
+++ cpp/test/mesh/read_named_meshtags.cpp
@@ -13,8 +13,10 @@
 #include <dolfinx/io/XDMFFile.h>
 #include <dolfinx/la/Vector.h>
 #include <dolfinx/mesh/MeshTags.h>
+#include <dolfinx/mesh/generation.h>
 #include <dolfinx/mesh/utils.h>
 
+#include <numeric>
 #include <string>
 
 using namespace dolfinx;
@@ -26,23 +28,60 @@ void test_read_named_meshtags()
 {
   const std::string mesh_file = "Domain.xdmf";
 
+  constexpr std::int32_t domain_value = 1;
+  constexpr std::int32_t material_value = 2;
+
+  // Create mesh
+  auto part = mesh::create_cell_partitioner(mesh::GhostMode::none);
+  auto mesh = std::make_shared<mesh::Mesh<double>>(
+      mesh::create_rectangle(MPI_COMM_WORLD, {{{0.0, 0.0}, {1.0, 1.0}}}, {3, 3},
+                             mesh::CellType::triangle, part));
+
+  const std::int32_t n_cells = mesh->topology()->index_map(2)->size_local();
+  std::vector<std::int32_t> indices(n_cells);
+  std::iota(std::begin(indices), std::end(indices), 0);
+
+  std::vector<std::int32_t> domain_values(n_cells);
+  std::ranges::fill(domain_values, domain_value);
+
+  std::vector<std::int32_t> material_values(n_cells);
+  std::ranges::fill(material_values, material_value);
+
+  mesh::MeshTags<std::int32_t> mt_domains(mesh->topology(), 2, indices,
+                                          domain_values);
+  mt_domains.name = "domain";
+
+  mesh::MeshTags<std::int32_t> mt_materials(mesh->topology(), 2, indices,
+                                            material_values);
+  mt_materials.name = "material";
+
+  io::XDMFFile file(mesh->comm(), mesh_file, "w",
+                    io::XDMFFile::Encoding::ASCII);
+  file.write_mesh(*mesh);
+  file.write_meshtags(mt_domains, mesh->geometry(),
+                      "/Xdmf/Domain/mesh/Geometry");
+  file.write_meshtags(mt_materials, mesh->geometry(),
+                      "/Xdmf/Domain/Grid/Geometry");
+
+  file.close();
+
   io::XDMFFile meshFile(MPI_COMM_WORLD, mesh_file, "r",
                         io::XDMFFile::Encoding::ASCII);
-  auto mesh = std::make_shared<mesh::Mesh<double>>(meshFile.read_mesh(
-      fem::CoordinateElement<double>(mesh::CellType::tetrahedron, 1),
-      mesh::GhostMode::none, "Grid"));
+  mesh = std::make_shared<mesh::Mesh<double>>(meshFile.read_mesh(
+      fem::CoordinateElement<double>(mesh::CellType::triangle, 1),
+      mesh::GhostMode::none, "mesh"));
 
   const auto mt_domain = meshFile.read_meshtags_by_label(
-      *mesh, "Grid", "domain", "/Xdmf/Domain");
+      *mesh, "domain", "domain", "/Xdmf/Domain");
 
-  CHECK(mt_domain.values().front() == 0);
+  CHECK(mt_domain.values().front() == domain_value);
 
   const auto mt_material = meshFile.read_meshtags_by_label(
-      *mesh, "Grid", "material", "/Xdmf/Domain");
+      *mesh, "material", "material", "/Xdmf/Domain");
 
-  CHECK(mt_material.values().front() == 1);
+  CHECK(mt_material.values().front() == material_value);
 
-  CHECK_THROWS(meshFile.read_meshtags_by_label(*mesh, "Grid", "missing"));
+  CHECK_THROWS(meshFile.read_meshtags_by_label(*mesh, "mesh", "missing"));
 }
 
 } // namespace
