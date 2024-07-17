2fa4a12a58e41d10c936376cf01ffbaabbbc7846
schnellerhase <56360279+schnellerhase@users.noreply.github.com>
2024-07-17 14:03:09 +0200

    Add parameter of intervals and explicit data type

--- python/test/unit/refinement/interval.py
+++ python/test/unit/refinement/interval.py
@@ -12,6 +12,7 @@ import pytest
 from dolfinx import mesh
 
 
+@pytest.mark.parametrize("n", [2, 10, 100])
 @pytest.mark.parametrize(
     "ghost_mode", [mesh.GhostMode.none, mesh.GhostMode.shared_vertex, mesh.GhostMode.shared_facet]
 )
@@ -20,22 +21,23 @@ from dolfinx import mesh
     [mesh.GhostMode.none, mesh.GhostMode.shared_vertex, mesh.GhostMode.shared_facet],
 )
 @pytest.mark.parametrize("redistribute", [True, False])
-def test_refine_interval(ghost_mode, redistribute, ghost_mode_refined):
-    msh = mesh.create_interval(MPI.COMM_WORLD, 100, [0, 1], ghost_mode=ghost_mode)
+def test_refine_interval(n, ghost_mode, redistribute, ghost_mode_refined):
+    msh = mesh.create_interval(MPI.COMM_WORLD, n, [0, 1], ghost_mode=ghost_mode)
     msh_refined, edges = mesh.refine_interval(
         msh, redistribute=redistribute, ghost_mode=ghost_mode_refined
     )
 
     # vertex count
-    assert msh_refined.topology.index_map(0).size_global == 201
+    assert msh_refined.topology.index_map(0).size_global == 2 * n + 1
 
     # edge count
     edge_count = np.array([len(edges)], dtype=np.int64)
     edge_count = MPI.COMM_WORLD.allreduce(edge_count)
 
-    assert edge_count == msh_refined.topology.index_map(1).size_global == 200
+    assert edge_count == msh_refined.topology.index_map(1).size_global == 2 * n
 
 
+@pytest.mark.parametrize("n", [50, 100])
 @pytest.mark.parametrize(
     "ghost_mode", [mesh.GhostMode.none, mesh.GhostMode.shared_vertex, mesh.GhostMode.shared_facet]
 )
@@ -44,21 +46,19 @@ def test_refine_interval(ghost_mode, redistribute, ghost_mode_refined):
     [mesh.GhostMode.none, mesh.GhostMode.shared_vertex, mesh.GhostMode.shared_facet],
 )
 @pytest.mark.parametrize("redistribute", [True, False])
-def test_refine_interval_adaptive(ghost_mode, redistribute, ghost_mode_refined):
-    msh = mesh.create_interval(MPI.COMM_WORLD, 100, [0, 1], ghost_mode=ghost_mode)
+def test_refine_interval_adaptive(n, ghost_mode, redistribute, ghost_mode_refined):
+    msh = mesh.create_interval(MPI.COMM_WORLD, n, [0, 1], ghost_mode=ghost_mode)
     msh_refined, edges = mesh.refine_interval(
-        msh, np.arange(10), redistribute=redistribute, ghost_mode=ghost_mode_refined
+        msh, np.arange(10, dtype=np.int32), redistribute=redistribute, ghost_mode=ghost_mode_refined
     )
 
     # vertex count
-    assert msh_refined.topology.index_map(0).size_global == 101 + 10 * MPI.COMM_WORLD.size
+    assert msh_refined.topology.index_map(0).size_global == n + 1 + 10 * MPI.COMM_WORLD.size
 
     # edge count
     edge_count = np.array([len(edges)], dtype=np.int64)
     edge_count = MPI.COMM_WORLD.allreduce(edge_count)
 
     assert (
-        edge_count
-        == msh_refined.topology.index_map(1).size_global
-        == 100 + 10 * MPI.COMM_WORLD.size
+        edge_count == msh_refined.topology.index_map(1).size_global == n + 10 * MPI.COMM_WORLD.size
     )
