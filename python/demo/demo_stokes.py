c535a7814bb42b7fe1e13875ada1fbbf9cbe6ec1
Garth N. Wells <gnw20@cam.ac.uk>
2024-07-19 22:07:25 +0800

    Update

--- python/demo/demo_stokes.py
+++ python/demo/demo_stokes.py
@@ -569,7 +569,7 @@ np.testing.assert_allclose(norm_u_1, norm_u_0, rtol=1e-4)
 # Solve using PETSc block matrices and an LU solver
 norm_u_2, norm_p_2 = block_direct_solver()
 np.testing.assert_allclose(norm_u_2, norm_u_0, rtol=1e-4)
-np.testing.assert_allclose(norm_p_2, norm_p_0, rtol=1e-4)
+np.testing.assert_allclose(norm_p_2, norm_p_0, rtol=1e-3)
 
 # Solve using a non-blocked matrix and an LU solver
 norm_u_3, norm_p_3 = mixed_direct()
