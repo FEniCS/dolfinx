38bb3d5693159f8a452ae7a4cef525f228a16519
ampdes <abdullahm225@gmail.com>
2024-07-31 14:44:22 +0200

    Scope into sections

--- cpp/dolfinx/io/checkpointing.cpp
+++ cpp/dolfinx/io/checkpointing.cpp
@@ -38,23 +38,42 @@ void write_mesh(adios2::IO io, adios2::Engine engine,
   const mesh::Geometry<T>& geometry = mesh.geometry();
   std::shared_ptr<const mesh::Topology> topology = mesh.topology();
 
+  // Variables/attributes to save
   std::int32_t dim = geometry.dim();
+  std::uint64_t num_vertices_global, offset, num_cells_global, cell_offset;
+  std::uint32_t num_vertices_local, num_cells_local, num_dofs_per_cell, degree;
+  std::string cell_type, lagrange_variant;
+  std::vector<std::int64_t> array_global;
+  std::vector<std::int32_t> offsets_global;
 
-  std::shared_ptr<const common::IndexMap> geom_imap
-      = mesh.geometry().index_map();
-  std::uint64_t num_vertices_global = geom_imap->size_global();
-  std::uint32_t num_vertices_local = geom_imap->size_local();
-  std::uint64_t offset = geom_imap->local_range()[0];
+  std::shared_ptr<const common::IndexMap> geom_imap;
 
-  const std::shared_ptr<const common::IndexMap> topo_imap
-      = topology->index_map(dim);
-  std::uint64_t num_cells_global = topo_imap->size_global();
-  std::uint32_t num_cells_local = topo_imap->size_local();
-  std::uint64_t cell_offset = topo_imap->local_range()[0];
+  // Vertices information
+  {
+    geom_imap = mesh.geometry().index_map();
+    num_vertices_global = geom_imap->size_global();
+    num_vertices_local = geom_imap->size_local();
+    offset = geom_imap->local_range()[0];
+  }
 
-  const fem::CoordinateElement<T>& cmap = mesh.geometry().cmap();
-  fem::ElementDofLayout geom_layout = cmap.create_dof_layout();
-  std::uint32_t num_dofs_per_cell = geom_layout.num_entity_closure_dofs(dim);
+  // Cells information
+  {
+    const std::shared_ptr<const common::IndexMap> topo_imap
+        = topology->index_map(dim);
+    num_cells_global = topo_imap->size_global();
+    num_cells_local = topo_imap->size_local();
+    cell_offset = topo_imap->local_range()[0];
+  }
+
+  // Coordinate element information
+  {
+    const fem::CoordinateElement<T>& cmap = mesh.geometry().cmap();
+    fem::ElementDofLayout geom_layout = cmap.create_dof_layout();
+    num_dofs_per_cell = geom_layout.num_entity_closure_dofs(dim);
+    cell_type = mesh::to_string(cmap.cell_shape());
+    degree = cmap.degree();
+    lagrange_variant = variant_to_string[cmap.variant()];
+  }
 
   const std::vector<int64_t> input_global_indices
       = geometry.input_global_indices();
@@ -62,68 +81,72 @@ void write_mesh(adios2::IO io, adios2::Engine engine,
       input_global_indices.begin(), num_vertices_local);
   const std::span<const T> mesh_x = geometry.x();
 
-  std::shared_ptr<const graph::AdjacencyList<std::int32_t>> connectivity
-      = topology->connectivity(dim, 0);
-  const std::vector<std::int32_t>& array = connectivity->array();
-  const std::vector<std::int32_t>& offsets = connectivity->offsets();
+  // Connectivity
+  {
+    std::shared_ptr<const graph::AdjacencyList<std::int32_t>> connectivity
+        = topology->connectivity(dim, 0);
+    const std::vector<std::int32_t>& array = connectivity->array();
+    const std::vector<std::int32_t>& offsets = connectivity->offsets();
 
-  const std::span<const int32_t> array_span(array.begin(),
-                                            offsets[num_cells_local]);
-  std::vector<std::int64_t> array_global(offsets[num_cells_local]);
+    const std::span<const int32_t> array_span(array.begin(),
+                                              offsets[num_cells_local]);
 
-  std::vector<std::int32_t> offsets_global(num_cells_local + 1);
+    array_global.resize(offsets[num_cells_local]);
+    offsets_global.resize(num_cells_local + 1);
 
-  std::iota(array_global.begin(), array_global.end(), 0);
+    std::iota(array_global.begin(), array_global.end(), 0);
 
-  geom_imap->local_to_global(array_span, array_global);
+    geom_imap->local_to_global(array_span, array_global);
 
-  for (std::size_t i = 0; i < num_cells_local + 1; ++i)
-    offsets_global[i] = offsets[i] + cell_offset * num_dofs_per_cell;
+    for (std::size_t i = 0; i < num_cells_local + 1; ++i)
+      offsets_global[i] = offsets[i] + cell_offset * num_dofs_per_cell;
+  }
 
-  io.DefineAttribute<std::string>("name", mesh.name);
-  io.DefineAttribute<std::int32_t>("dim", geometry.dim());
-  io.DefineAttribute<std::string>("cell_type",
-                                  mesh::to_string(cmap.cell_shape()));
-  io.DefineAttribute<std::int32_t>("degree", cmap.degree());
-  io.DefineAttribute<std::string>("lagrange_variant",
-                                  variant_to_string[cmap.variant()]);
+  // ADIOS2 write attributes and variables
+  {
+    io.DefineAttribute<std::string>("name", mesh.name);
+    io.DefineAttribute<std::int32_t>("dim", dim);
+    io.DefineAttribute<std::string>("cell_type", cell_type);
+    io.DefineAttribute<std::int32_t>("degree", degree);
+    io.DefineAttribute<std::string>("lagrange_variant", lagrange_variant);
 
-  adios2::Variable<std::uint64_t> var_num_vertices
-      = io.DefineVariable<std::uint64_t>("num_vertices");
-  adios2::Variable<std::uint64_t> var_num_cells
-      = io.DefineVariable<std::uint64_t>("num_cells");
-  adios2::Variable<std::uint32_t> var_num_dofs_per_cell
-      = io.DefineVariable<std::uint32_t>("num_dofs_per_cell");
+    adios2::Variable<std::uint64_t> var_num_vertices
+        = io.DefineVariable<std::uint64_t>("num_vertices");
+    adios2::Variable<std::uint64_t> var_num_cells
+        = io.DefineVariable<std::uint64_t>("num_cells");
+    adios2::Variable<std::uint32_t> var_num_dofs_per_cell
+        = io.DefineVariable<std::uint32_t>("num_dofs_per_cell");
 
-  adios2::Variable<std::int64_t> var_input_global_indices
-      = io.DefineVariable<std::int64_t>(
-          "input_global_indices", {num_vertices_global}, {offset},
-          {num_vertices_local}, adios2::ConstantDims);
+    adios2::Variable<std::int64_t> var_input_global_indices
+        = io.DefineVariable<std::int64_t>(
+            "input_global_indices", {num_vertices_global}, {offset},
+            {num_vertices_local}, adios2::ConstantDims);
 
-  adios2::Variable<T> var_x
-      = io.DefineVariable<T>("x", {num_vertices_global, 3}, {offset, 0},
-                             {num_vertices_local, 3}, adios2::ConstantDims);
+    adios2::Variable<T> var_x
+        = io.DefineVariable<T>("x", {num_vertices_global, 3}, {offset, 0},
+                               {num_vertices_local, 3}, adios2::ConstantDims);
 
-  adios2::Variable<std::int64_t> var_topology_array
-      = io.DefineVariable<std::int64_t>(
-          "topology_array", {num_cells_global * num_dofs_per_cell},
-          {cell_offset * num_dofs_per_cell},
-          {num_cells_local * num_dofs_per_cell}, adios2::ConstantDims);
+    adios2::Variable<std::int64_t> var_topology_array
+        = io.DefineVariable<std::int64_t>(
+            "topology_array", {num_cells_global * num_dofs_per_cell},
+            {cell_offset * num_dofs_per_cell},
+            {num_cells_local * num_dofs_per_cell}, adios2::ConstantDims);
 
-  adios2::Variable<std::int32_t> var_topology_offsets
-      = io.DefineVariable<std::int32_t>(
-          "topology_offsets", {num_cells_global + 1}, {cell_offset},
-          {num_cells_local + 1}, adios2::ConstantDims);
+    adios2::Variable<std::int32_t> var_topology_offsets
+        = io.DefineVariable<std::int32_t>(
+            "topology_offsets", {num_cells_global + 1}, {cell_offset},
+            {num_cells_local + 1}, adios2::ConstantDims);
 
-  engine.BeginStep();
-  engine.Put(var_num_vertices, num_vertices_global);
-  engine.Put(var_num_cells, num_cells_global);
-  engine.Put(var_num_dofs_per_cell, num_dofs_per_cell);
-  engine.Put(var_input_global_indices, input_global_indices_span.data());
-  engine.Put(var_x, mesh_x.subspan(0, num_vertices_local * 3).data());
-  engine.Put(var_topology_array, array_global.data());
-  engine.Put(var_topology_offsets, offsets_global.data());
-  engine.EndStep();
+    engine.BeginStep();
+    engine.Put(var_num_vertices, num_vertices_global);
+    engine.Put(var_num_cells, num_cells_global);
+    engine.Put(var_num_dofs_per_cell, num_dofs_per_cell);
+    engine.Put(var_input_global_indices, input_global_indices_span.data());
+    engine.Put(var_x, mesh_x.subspan(0, num_vertices_local * 3).data());
+    engine.Put(var_topology_array, array_global.data());
+    engine.Put(var_topology_offsets, offsets_global.data());
+    engine.EndStep();
+  }
 }
 
 template void write_mesh<float>(adios2::IO io, adios2::Engine engine,
