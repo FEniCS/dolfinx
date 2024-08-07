c1b57460021c80bb26a91a0913c3d8ca894c521a
schnellerhase <56360279+schnellerhase@users.noreply.github.com>
2024-08-07 10:18:21 +0200

    Extend insert doc to make clear how the block insertion works

--- cpp/dolfinx/la/SparsityPattern.h
+++ cpp/dolfinx/la/SparsityPattern.h
@@ -71,6 +71,15 @@ public:
 
   /// @brief Insert non-zero locations using local (process-wise)
   /// indices.
+  ///
+  /// This inserts for every row in rows the columns (listed in cols) to the
+  /// sparsity pattern. This allows for easy setting of dense sub-blocks, for
+  /// example sp.insert({0, 1, 2}, {1, 2, 3}) creates a 3x3 block in the
+  /// sparsity pattern with top left corner at (0, 1).
+  ///
+  /// Note: Especially this operation does not perform a pairwise insertion to
+  /// the sparsity pattern, i.e. the above call does not introduce the entries
+  /// at locations (0, 1), (1, 2) and (2, 3)!
   void insert(std::span<const std::int32_t> rows,
               std::span<const std::int32_t> cols);
 
