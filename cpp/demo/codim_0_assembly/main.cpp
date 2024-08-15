28a4748cc5bd0f039df31c68caa3139c2e7c8e08
Jørgen S. Dokken <dokken@simula.no>
2024-08-15 14:39:38 +0000

    Address Joe's comments

--- cpp/demo/codim_0_assembly/main.cpp
+++ cpp/demo/codim_0_assembly/main.cpp
@@ -29,7 +29,7 @@ int main(int argc, char* argv[])
   {
     // Create mesh and function space
     auto part = mesh::create_cell_partitioner(mesh::GhostMode::shared_facet);
-    auto domain = std::make_shared<mesh::Mesh<U>>(
+    auto mesh = std::make_shared<mesh::Mesh<U>>(
         mesh::create_rectangle<U>(MPI_COMM_WORLD, {{{0.0, 0.0}, {2.0, 1.0}}},
                                   {1, 4}, mesh::CellType::quadrilateral, part));
 
@@ -39,12 +39,12 @@ int main(int argc, char* argv[])
         basix::element::dpc_variant::unset, false);
 
     auto V = std::make_shared<fem::FunctionSpace<U>>(
-        fem::create_functionspace(domain, element, {}));
+        fem::create_functionspace(mesh, element, {}));
 
     // Next we find all cells of the mesh with y<0.5
-    const int tdim = domain->topology()->dim();
-    auto cells = mesh::locate_entities(
-        *domain, tdim,
+    const int tdim = mesh->topology()->dim();
+    auto marked_cells = mesh::locate_entities(
+        *mesh, tdim,
         [](auto x)
         {
           using U = typename decltype(x)::value_type;
@@ -58,57 +58,78 @@ int main(int argc, char* argv[])
           }
           return marker;
         });
+    // We create a MeshTags object where we mark these cells with 2, and any
+    // other cell with 1
+    auto cell_map = mesh->topology()->index_map(tdim);
+    std::size_t num_cells_local
+        = mesh->topology()->index_map(tdim)->size_local()
+          + mesh->topology()->index_map(tdim)->num_ghosts();
+    std::vector<std::int32_t> cells(num_cells_local);
+    std::iota(cells.begin(), cells.end(), 0);
+    std::vector<std::int32_t> values(cell_map->size_local(), 1);
+    std::for_each(marked_cells.begin(), marked_cells.end(),
+                  [&values](auto& c) { values[c] = 2; });
+    dolfinx::mesh::MeshTags<std::int32_t> cell_marker(mesh->topology(), tdim,
+                                                      cells, values);
 
     std::shared_ptr<mesh::Mesh<U>> submesh;
-    std::vector<std::int32_t> sub_to_parent_cells;
+    std::vector<std::int32_t> submesh_to_mesh;
     {
-      auto [_submesh, _submsh_to_msh, v_map, g_map]
-          = mesh::create_submesh(*domain, tdim, std::span<const int>(cells));
+      auto [_submesh, _submesh_to_mesh, v_map, g_map]
+          = mesh::create_submesh(*mesh, tdim, cell_marker.find(2));
       submesh = std::make_shared<mesh::Mesh<U>>(std::move(_submesh));
-      sub_to_parent_cells = std::move(_submsh_to_msh);
+      submesh_to_mesh = std::move(_submesh_to_mesh);
     }
 
     // We create the function space used for the trial space
-    auto V_sub = std::make_shared<fem::FunctionSpace<U>>(
+    auto W = std::make_shared<fem::FunctionSpace<U>>(
         fem::create_functionspace(submesh, element, {}));
 
-    // We need to invert the map from submesh cells to parent cells
-    std::size_t num_cells_local
-        = domain->topology()->index_map(tdim)->size_local()
-          + domain->topology()->index_map(tdim)->num_ghosts();
-    std::vector<std::int32_t> parent_to_sub(num_cells_local, -1);
-    for (std::size_t i = 0; i < sub_to_parent_cells.size(); ++i)
-      parent_to_sub[sub_to_parent_cells[i]] = i;
+    // A mixed-domain form has functions defined over different meshes. The mesh
+    // associated with the measure (dx, ds, etc.) is called the integration
+    // domain. To assemble mixed-domain forms, maps must be provided taking
+    // entities in the integration domain to entities on each mesh in the form.
+    // Since one of our forms has a measure defined over `mesh` and involves a
+    // function defined over `submesh`, we must provide a map from entities in
+    // `mesh` to entities in `submesh`. This is simply the "inverse" of
+    // `submesh_to_mesh`.
+    std::vector<std::int32_t> mesh_to_submesh(num_cells_local, -1);
+    for (std::size_t i = 0; i < submesh_to_mesh.size(); ++i)
+      mesh_to_submesh[submesh_to_mesh[i]] = i;
 
     std::shared_ptr<const mesh::Mesh<U>> const_ptr = submesh;
     std::map<std::shared_ptr<const mesh::Mesh<U>>,
              std::span<const std::int32_t>>
         entity_maps
-        = {{const_ptr, std::span<const std::int32_t>(parent_to_sub.data(),
-                                                     parent_to_sub.size())}};
-    // Now we need to make a restriction of the cell integrals to those cells
-    // that exist in the submesh
+        = {{const_ptr, std::span<const std::int32_t>(mesh_to_submesh.data(),
+                                                     mesh_to_submesh.size())}};
 
+    // Next we compute the integration entities on the integration domain `mesh`
     std::map<
         fem::IntegralType,
         std::vector<std::pair<std::int32_t, std::span<const std::int32_t>>>>
         subdomain_map = {};
+    auto integration_entities = fem::compute_integration_domains(
+        fem::IntegralType::cell, *mesh->topology(), cell_marker.find(2), tdim);
+
     subdomain_map[fem::IntegralType::cell].push_back(
-        {3, std::span<const std::int32_t>(sub_to_parent_cells)});
+        {3, std::span<const std::int32_t>(integration_entities.data(),
+                                          integration_entities.size())});
 
     // We can now create the bi-linear form
     auto a = std::make_shared<fem::Form<T>>(
-        fem::create_form<T>(*form_mixed_codim0_a, {V, V_sub}, {}, {},
-                            subdomain_map, entity_maps, V->mesh()));
+        fem::create_form<T>(*form_mixed_codim0_a, {V, W}, {}, {}, subdomain_map,
+                            entity_maps, V->mesh()));
 
     la::SparsityPattern sp = fem::create_sparsity_pattern(*a);
     sp.finalize();
     la::MatrixCSR<double> A(sp);
     fem::assemble_matrix(A.mat_add_values(), *a, {});
     A.scatter_rev();
+    std::cout << "here3\n" << std::endl;
 
-    auto a_sub = std::make_shared<fem::Form<T>>(fem::create_form<T>(
-        *form_mixed_codim0_a_sub, {V_sub, V_sub}, {}, {}, {}, {}));
+    auto a_sub = std::make_shared<fem::Form<T>>(
+        fem::create_form<T>(*form_mixed_codim0_a_sub, {W, W}, {}, {}, {}, {}));
     la::SparsityPattern sp_sub = fem::create_sparsity_pattern(*a);
     sp_sub.finalize();
 
@@ -122,8 +143,8 @@ int main(int argc, char* argv[])
     cc << "A:" << std::endl;
 
     std::size_t num_owned_rows = V->dofmap()->index_map->size_local();
-    std::size_t num_sub_cols = V_sub->dofmap()->index_map->size_local()
-                               + V_sub->dofmap()->index_map->num_ghosts();
+    std::size_t num_sub_cols = W->dofmap()->index_map->size_local()
+                               + W->dofmap()->index_map->num_ghosts();
     for (std::size_t i = 0; i < num_owned_rows; i++)
     {
       for (std::size_t j = 0; j < num_sub_cols; j++)
@@ -133,7 +154,7 @@ int main(int argc, char* argv[])
       cc << std::endl;
     }
 
-    std::size_t num_owned_sub_rows = V_sub->dofmap()->index_map->size_local();
+    std::size_t num_owned_sub_rows = W->dofmap()->index_map->size_local();
     std::vector<T> A_sub_flattened = A_sub.to_dense();
     cc << "A_sub" << std::endl;
     for (std::size_t i = 0; i < num_owned_sub_rows; i++)
