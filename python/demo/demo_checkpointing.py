fb88cd2a123eb3f00e5f15dc03d3fc3774a030d1
ampdes <abdullahm225@gmail.com>
2024-08-22 15:47:17 +0200

    Update checkpointing demo for meshtags

--- python/demo/demo_checkpointing.py
+++ python/demo/demo_checkpointing.py
@@ -25,6 +25,8 @@ if not dolfinx.common.has_adios2:
 
 from mpi4py import MPI
 
+import numpy as np
+
 from dolfinx import io, mesh
 
 # -
@@ -45,6 +47,22 @@ msh = mesh.create_rectangle(
 )
 # -
 
+# Create meshtags of all entity types for the above mesh
+
+# +
+tags = {}
+for dim in range(msh.topology.dim + 1):
+    msh.topology.create_connectivity(dim, msh.topology.dim)
+    imap = msh.topology.index_map(dim)
+    num_entities_local = imap.size_local
+    entities = np.arange(num_entities_local, dtype=np.int32)
+    values = imap.local_range[0] + entities
+    mt = mesh.meshtags(msh, dim, entities, values)
+    mt.name = f"entity_{dim}"
+    tags[mt.name] = mt
+
+# -
+
 # +
 config_path = os.getcwd() + "/checkpointing.yml"
 adios2 = io.ADIOS2(config_path, msh.comm, filename="mesh.bp", tag="mesh-write", mode="write")
@@ -52,6 +70,9 @@ adios2 = io.ADIOS2(config_path, msh.comm, filename="mesh.bp", tag="mesh-write", 
 
 # +
 io.write_mesh(adios2, msh)
+for mt_name, mt in tags.items():
+    io.write_meshtags(adios2, msh, mt)
+
 adios2.close()
 # -
 
@@ -63,4 +84,22 @@ adios2_read = io.ADIOS2(
 
 # +
 msh_read = io.read_mesh(adios2_read, msh.comm)
+tags_read = io.read_meshtags(adios2_read, msh_read)
+adios2_read.close()
+# -
+
+# +
+adios2_write = io.ADIOS2(
+    msh_read.comm, filename="mesh2.bp", tag="mesh-write", engine_type="BP5", mode="write"
+)
+# -
+
+# +
+io.write_mesh(adios2_write, msh_read)
+
+for mt_name, mt_read in tags_read.items():
+    io.write_meshtags(adios2_write, msh_read, mt_read)
+
+
+adios2_write.close()
 # -
