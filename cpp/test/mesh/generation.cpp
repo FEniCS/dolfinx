cf52838f5ceceb24ce092c2682c67e5ecbb7cf90
Garth N. Wells <gnw20@cam.ac.uk>
2024-09-15 19:08:54 +0100

    Test type fix

--- cpp/test/mesh/generation.cpp
+++ cpp/test/mesh/generation.cpp
@@ -263,8 +263,8 @@ TEMPLATE_TEST_CASE("Rectangle quadrilateral mesh",
 {
   using T = TestType;
 
-  const std::array<double, 2> lower = {0, 0};
-  const std::array<double, 2> upper = {1, 1};
+  const std::array<T, 2> lower = {0, 0};
+  const std::array<T, 2> upper = {1, 1};
   mesh::Mesh<T> mesh = dolfinx::mesh::create_rectangle<T>(
       MPI_COMM_SELF, {lower, upper}, {1, 1}, mesh::CellType::quadrilateral);
 
