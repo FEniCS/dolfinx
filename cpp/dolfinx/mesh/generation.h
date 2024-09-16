202b446cd4a79a1545bfd750e24fa54b6f5f6876
Paul T. Kühner <56360279+schnellerhase@users.noreply.github.com>
2024-09-16 13:24:55 +0200

    Add interval mesh generation testing (#3359)
    * Add explicit rectangle  mesh testing
    
    * Add explicit interval mesh testing
    
    * Format updates
    
    * Simplifications
    
    * Small edits
    
    * Disable parallel interval mesh checks that reply on an ownership assumption
    
    * Linting
    
    * Reactivate one
    
    * Remove duplicate test > duplicate of new one, rebase?
    
    * First showcase
    
    * Integer size fix
    
    * Readd lost in format
    
    * Finishe x check of itnerval case
    
    * showase interval parallel
    
    * Remove commented code
    
    * Test type fix
    
    * Adadpt to new templat type
    
    * Remove no longer needed type hints
    
    * missed one
    
    * Type fix
    
    * Type updates
    
    ---------
    
    Co-authored-by: Garth N. Wells <gnw20@cam.ac.uk>

--- cpp/dolfinx/mesh/generation.h
+++ cpp/dolfinx/mesh/generation.h
@@ -37,6 +37,10 @@ enum class DiagonalType
 namespace impl
 {
 template <std::floating_point T>
+std::tuple<std::vector<T>, std::vector<std::int64_t>>
+create_interval_cells(std::array<T, 2> p, std::int64_t n);
+
+template <std::floating_point T>
 Mesh<T> build_tri(MPI_Comm comm, std::array<std::array<T, 2>, 2> p,
                   std::array<std::int64_t, 2> n,
                   const CellPartitionFunction& partitioner,
@@ -48,8 +52,7 @@ Mesh<T> build_quad(MPI_Comm comm, const std::array<std::array<T, 2>, 2> p,
                    const CellPartitionFunction& partitioner);
 
 template <std::floating_point T>
-std::vector<T> create_geom(MPI_Comm comm,
-                           std::array<std::array<T, 3>, 2> p,
+std::vector<T> create_geom(MPI_Comm comm, std::array<std::array<T, 3>, 2> p,
                            std::array<std::int64_t, 3> n);
 
 template <std::floating_point T>
@@ -249,21 +252,7 @@ Mesh<T> create_interval(MPI_Comm comm, std::int64_t n, std::array<T, 2> p,
   fem::CoordinateElement<T> element(CellType::interval, 1);
   if (dolfinx::MPI::rank(comm) == 0)
   {
-    const T h = (b - a) / static_cast<T>(n);
-
-    // Create vertices
-    std::vector<T> x(n + 1);
-    std::ranges::generate(x, [i = std::int64_t(0), a, h]() mutable
-                          { return a + h * static_cast<T>(i++); });
-
-    // Create intervals -> cells=[0, 1, 1, ..., n-1, n-1, n]
-    std::vector<std::int64_t> cells(2 * n);
-    for (std::size_t ix = 0; ix < cells.size() / 2; ++ix)
-    {
-      cells[2 * ix] = ix;
-      cells[2 * ix + 1] = ix + 1;
-    }
-
+    auto [x, cells] = impl::create_interval_cells<T>(p, n);
     return create_mesh(comm, MPI_COMM_SELF, cells, element, MPI_COMM_SELF, x,
                        {x.size(), 1}, partitioner);
   }
@@ -276,9 +265,33 @@ Mesh<T> create_interval(MPI_Comm comm, std::int64_t n, std::array<T, 2> p,
 
 namespace impl
 {
+
 template <std::floating_point T>
-std::vector<T> create_geom(MPI_Comm comm,
-                           std::array<std::array<T, 3>, 2> p,
+std::tuple<std::vector<T>, std::vector<std::int64_t>>
+create_interval_cells(std::array<T, 2> p, std::int64_t n)
+{
+  const auto [a, b] = p;
+
+  const T h = (b - a) / static_cast<T>(n);
+
+  // Create vertices
+  std::vector<T> x(n + 1);
+  std::ranges::generate(x, [i = std::int64_t(0), a, h]() mutable
+                        { return a + h * static_cast<T>(i++); });
+
+  // Create intervals -> cells=[0, 1, 1, ..., n-1, n-1, n]
+  std::vector<std::int64_t> cells(2 * n);
+  for (std::size_t ix = 0; ix < cells.size() / 2; ++ix)
+  {
+    cells[2 * ix] = ix;
+    cells[2 * ix + 1] = ix + 1;
+  }
+
+  return {std::move(x), std::move(cells)};
+}
+
+template <std::floating_point T>
+std::vector<T> create_geom(MPI_Comm comm, std::array<std::array<T, 3>, 2> p,
                            std::array<std::int64_t, 3> n)
 {
   // Extract data
