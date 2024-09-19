973f16dca8705a87c23cbbbc5d4e0401520ad139
schnellerhase <56360279+schnellerhase@users.noreply.github.com>
2024-09-19 07:44:53 +0200

    Move maintain_coarse_partitioner to refine.cpp

--- cpp/dolfinx/refinement/refine.h
+++ cpp/dolfinx/refinement/refine.h
@@ -22,18 +22,9 @@ namespace dolfinx::refinement
 {
 
 /// TODO: Document function
-inline graph::AdjacencyList<std::int32_t> maintain_coarse_partitioner(
+graph::AdjacencyList<std::int32_t> maintain_coarse_partitioner(
     MPI_Comm comm, int, const std::vector<mesh::CellType>& cell_types,
-    const std::vector<std::span<const std::int64_t>>& cell_topology)
-{
-  int mpi_rank = MPI::rank(comm);
-  int num_cell_vertices = mesh::num_cell_vertices(cell_types.front());
-  std::int32_t num_cells = cell_topology.front().size() / num_cell_vertices;
-  std::vector<std::int32_t> destinations(num_cells, mpi_rank);
-  std::vector<std::int32_t> dest_offsets(num_cells + 1);
-  std::iota(dest_offsets.begin(), dest_offsets.end(), 0);
-  return graph::AdjacencyList(std::move(destinations), std::move(dest_offsets));
-}
+    const std::vector<std::span<const std::int64_t>>& cell_topology);
 
 /// @brief Refine with markers, optionally redistributing, and
 /// optionally calculating the parent-child relationships.
