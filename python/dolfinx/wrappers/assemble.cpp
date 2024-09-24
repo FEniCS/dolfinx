1bfeda58a4b71ca4477ef5cdadd62454ca6c9688
Garth N. Wells <gnw20@cam.ac.uk>
2024-09-24 15:58:03 +0100

    Add no convert

--- python/dolfinx/wrappers/assemble.cpp
+++ python/dolfinx/wrappers/assemble.cpp
@@ -434,7 +434,7 @@ void declare_assembly_functions(nb::module_& m)
                                   std::nullopt, scale);
         }
       },
-      nb::arg("b").noconvert(), nb::arg("bcs"), nb::arg("x0").none(),
+      nb::arg("b").noconvert(), nb::arg("bcs"), nb::arg("x0").noconvert().none(),
       nb::arg("scale"));
 }
 
