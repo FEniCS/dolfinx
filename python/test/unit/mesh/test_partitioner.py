b319c1b95b9a54d0f70c83cac35a55e3e3de9151
schnellerhase <56360279+schnellerhase@users.noreply.github.com>
2024-09-28 09:36:21 +0200

    Add failing test for partitioner testing

---
+++ python/test/unit/mesh/test_partitioner.py
@@ -0,0 +1,31 @@
+from mpi4py import MPI
+
+import numpy as np
+import pytest
+
+import dolfinx
+import ufl
+from basix.ufl import element
+
+avail_partitioners = []
+if dolfinx.has_parmetis:
+    avail_partitioners.append(dolfinx.graph.partitioner_parmetis)
+
+if dolfinx.has_kahip:
+    avail_partitioners.append(dolfinx.graph.partitioner_kahip)
+
+if dolfinx.has_ptscotch:
+    avail_partitioners.append(dolfinx.graph.partitioner_scotch)
+
+
+@pytest.mark.parametrize("partitioner", avail_partitioners)
+def test_partitioner(partitioner):
+    quadrilaterals = np.array([], dtype=np.int64)
+    quad_points = np.array([[0, 0], [0.3, 0]], dtype=np.float64)
+
+    ufl_quad = ufl.Mesh(element("Lagrange", "quadrilateral", 1, shape=(2,)))
+
+    cell_part = dolfinx.mesh.create_cell_partitioner(partitioner())
+    dolfinx.mesh.create_mesh(
+        MPI.COMM_WORLD, quadrilaterals, quad_points, ufl_quad, partitioner=cell_part
+    )
