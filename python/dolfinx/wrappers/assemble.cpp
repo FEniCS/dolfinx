d761e9c0528913c1f5927e03ff8a9901d01d54c3
Garth N. Wells <gnw20@cam.ac.uk>
2024-11-03 18:40:18 +0000

    Small fix

--- python/dolfinx/wrappers/assemble.cpp
+++ python/dolfinx/wrappers/assemble.cpp
@@ -423,12 +423,11 @@ void declare_assembly_functions(nb::module_& m)
         for (auto& bc1 : bcs1)
         {
           auto& _bcs0 = _bcs.emplace_back();
-          for (auto& bc : bc1)
-            for (auto& bc : bc1)
-            {
-              assert(bc);
-              _bcs0.push_back(*bc);
-            }
+          for (auto bc : bc1)
+          {
+            assert(bc);
+            _bcs0.push_back(*bc);
+          }
         }
 
         std::vector<std::span<const T>> _x0;
