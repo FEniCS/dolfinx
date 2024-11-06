81ee92c180b79012082087241246992091fd8b4b
Garth N. Wells <gnw20@cam.ac.uk>
2024-11-06 06:02:37 +0000

    Update

--- python/demo/demo_cahn-hilliard.py
+++ python/demo/demo_cahn-hilliard.py
@@ -280,9 +280,8 @@ opts[f"{option_prefix}pc_type"] = "lu"
 sys = PETSc.Sys()  # type: ignore
 # For factorisation prefer MUMPS, then superlu_dist, then default
 # if sys.hasExternalPackage("mumps") and (PETSc.IntType != np.int64 and MPI.COMM_WORLD.size > 1):
-if sys.hasExternalPackage("mumps") and (
-    PETSc.IntType != np.int64 and PETSc.ScalarType != np.complex64
-):
+use_superlu = PETSc.IntType == np.int64 or PETSc.ScalarType == np.complex64
+if sys.hasExternalPackage("mumps") and not use_superlu:
     opts[f"{option_prefix}pc_factor_mat_solver_type"] = "mumps"
 elif sys.hasExternalPackage("superlu_dist"):
     opts[f"{option_prefix}pc_factor_mat_solver_type"] = "superlu_dist"
