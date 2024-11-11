f3390d4b3e17a440ee6a470c43cf92590c852c80
schnellerhase <56360279+schnellerhase@users.noreply.github.com>
2024-11-11 20:23:51 +0100

    typos

--- cpp/dolfinx/common/MPI.h
+++ cpp/dolfinx/common/MPI.h
@@ -279,8 +279,8 @@ struct mpi_type_mapping;
 template <typename T>
 MPI_Datatype mpi_t = mpi_type_mapping<T>::type;
 
-/// @brief Registers for cpp_t the correpsonding mpi_t which can then be
-/// retrieved with mpi_t<cpp_t> form here on.
+/// @brief Registers for cpp_t the corresponding mpi_t which can then be
+/// retrieved with mpi_t<cpp_t> from here on.
 #define MAP_TO_MPI_TYPE(cpp_t, mpi_t)                                          \
   template <>                                                                  \
   struct mpi_type_mapping<cpp_t>                                               \
