fa8ba41fa23a96567dbdc00f1f4ab3a1c095425a
Jørgen Schartum Dokken <dokken92@gmail.com>
2024-12-02 16:26:38 +0100

    Unify and fix BoundingBoxtree constructor (C++) (#3545)
    * Try to unify and fix bb_tree constructors
    
    * Add permissive nanobind
    
    * Apply suggestions from code review

--- python/dolfinx/wrappers/geometry.cpp
+++ python/dolfinx/wrappers/geometry.cpp
@@ -16,8 +16,10 @@
 #include <nanobind/nanobind.h>
 #include <nanobind/ndarray.h>
 #include <nanobind/stl/array.h>
+#include <nanobind/stl/optional.h>
 #include <nanobind/stl/tuple.h>
 #include <nanobind/stl/vector.h>
+#include <optional>
 #include <span>
 
 namespace nb = nanobind;
@@ -34,16 +36,22 @@ void declare_bbtree(nb::module_& m, std::string type)
           "__init__",
           [](dolfinx::geometry::BoundingBoxTree<T>* bbt,
              const dolfinx::mesh::Mesh<T>& mesh, int dim,
-             nb::ndarray<const std::int32_t, nb::ndim<1>, nb::c_contig>
+             std::optional<
+                 nb::ndarray<const std::int32_t, nb::ndim<1>, nb::c_contig>>
                  entities,
              double padding)
           {
-            new (bbt) dolfinx::geometry::BoundingBoxTree<T>(
-                mesh, dim,
-                std::span<const std::int32_t>(entities.data(), entities.size()),
-                padding);
+            std::optional<std::span<const std::int32_t>> ents
+                = entities ? std::span<const std::int32_t>(
+                                 entities->data(),
+                                 entities->data() + entities->size())
+                           : std::optional<std::span<const std::int32_t>>(
+                                 std::nullopt);
+
+            new (bbt)
+                dolfinx::geometry::BoundingBoxTree<T>(mesh, dim, ents, padding);
           },
-          nb::arg("mesh"), nb::arg("dim"), nb::arg("entities"),
+          nb::arg("mesh"), nb::arg("dim"), nb::arg("entities").none(),
           nb::arg("padding") = 0.0)
       .def_prop_ro("num_bboxes",
                    &dolfinx::geometry::BoundingBoxTree<T>::num_bboxes)
