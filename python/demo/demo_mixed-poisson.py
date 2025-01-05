7243f2fcc6abbe9543d8d561bf23adf994160880
Garth N. Wells <gnw20@cam.ac.uk>
2025-01-05 18:36:06 +0000

    Work on block preconditioned mixed Poisson solver

--- python/demo/demo_mixed-poisson.py
+++ python/demo/demo_mixed-poisson.py
@@ -8,7 +8,7 @@
 #       jupytext_version: 1.14.1
 # ---
 
-# # Mixed formulation for the Poisson equation
+# # Mixed formulation of Poisson equation with a block-preconditioner/solver
 #
 # This demo illustrates how to solve Poisson equation using a mixed
 # (two-field) formulation and block-preconditioned iterative solver. In
@@ -17,7 +17,7 @@
 # * Use mixed and non-continuous finite element spaces.
 # * Set essential boundary conditions for subspaces and $H(\mathrm{div})$ spaces.
 # * Construct a blocked linear system.
-# * Use a block-preconditioned iterative linear solver.
+# * Construct a block-preconditioned iterative linear solver.
 #
 # ```{admonition} Download sources
 # :class: download
@@ -46,15 +46,16 @@
 #   \sigma \cdot n = g \quad {\rm on} \ \Gamma_{N}.
 # $$
 #
-# The same equations arise in connection with flow in porous media, and are
-# also referred to as Darcy flow. Here $n$ denotes the outward pointing normal
-# vector on the boundary. Looking at the variational form, we see that the
-# boundary condition for the flux ($\sigma \cdot n = g$) is now an essential
-# boundary condition (which should be enforced in the function space), while
-# the other boundary condition ($u = u_0$) is a natural boundary condition
-# (which should be applied to the variational form). Inserting the boundary
-# conditions, this variational problem can be phrased in the general form: find
-# $(\sigma, u) \in \Sigma_g \times V$ such that
+# The same equations arise in connection with flow in porous media, and
+# are also referred to as Darcy flow. Here $n$ denotes the outward
+# pointing normal vector on the boundary. Looking at the variational
+# form, we see that the boundary condition for the flux ($\sigma \cdot n
+# = g$) is now an essential boundary condition (which should be enforced
+# in the function space), while the other boundary condition ($u = u_0$)
+# is a natural boundary condition (which should be applied to the
+# variational form). Inserting the boundary conditions, this variational
+# problem can be phrased in the general form: find $(\sigma, u) \in
+# \Sigma_g \times V$ such that
 #
 # $$
 #    a((\sigma, u), (\tau, v)) = L((\tau, v))
@@ -72,14 +73,15 @@
 #   + \int_{\Gamma_D} u_0 \tau \cdot n  \ {\rm d} s,
 # \end{align}
 # $$
-# and $\Sigma_g = \{ \tau \in H({\rm div})$ such that $\tau \cdot n|_{\Gamma_N}
-# = g \}$ and $V = L^2(\Omega)$.
+# and $\Sigma_g = \{ \tau \in H({\rm div})$ such that $\tau \cdot
+# n|_{\Gamma_N} = g \}$ and $V = L^2(\Omega)$.
 #
-# To discretize the above formulation, two discrete function spaces $\Sigma_h
-# \subset \Sigma$ and $V_h \subset V$ are needed to form a mixed function space
-# $\Sigma_h \times V_h$. A stable choice of finite element spaces is to let
-# $\Sigma_h$ be the Brezzi-Douglas-Marini elements of polynomial order
-# $k$ and let $V_h$ be discontinuous elements of polynomial order $k-1$.
+# To discretize the above formulation, two discrete function spaces
+# $\Sigma_h \subset \Sigma$ and $V_h \subset V$ are needed to form a
+# mixed function space $\Sigma_h \times V_h$. A stable choice of finite
+# element spaces is to let $\Sigma_h$ be the Raviart-Thomas elements of
+# polynomial order $k$ and let $V_h$ be discontinuous elements of
+# polynomial order $k-1$.
 #
 # We will use the same definitions of functions and boundaries as in the
 # demo for {doc}`the Poisson equation <demo_poisson>`. These are:
@@ -92,9 +94,10 @@
 # * $f = 10\exp(-((x - 0.5)^2 + (y - 0.5)^2) / 0.02)$  (source term)
 #
 # ## Implementation
+#
+# Import the required modules:
 
 # +
-
 try:
     from petsc4py import PETSc
 
@@ -113,7 +116,9 @@ import numpy as np
 
 import dolfinx.fem.petsc
 from basix.ufl import element
-from dolfinx import default_real_type, default_scalar_type, fem, io, la, mesh
+from dolfinx import default_real_type, default_scalar_type, fem, la, mesh
+from dolfinx.fem.petsc import discrete_gradient, interpolation_matrix
+from dolfinx.mesh import CellType, create_unit_square
 from ufl import (
     Measure,
     SpatialCoordinate,
@@ -126,97 +131,275 @@ from ufl import (
 
 dtype = default_scalar_type
 xdtype = default_real_type
+# -
 
-# msh = mesh.create_unit_square(
-#     MPI.COMM_WORLD, 32, 32, mesh.CellType.quadrilateral, dtype=default_real_type
-# )
-msh = mesh.create_unit_square(MPI.COMM_WORLD, 32, 32, mesh.CellType.triangle, dtype=xdtype)
+# Create mesh a mesh in two dimensions. The iterative solver constructed
+# later requires special construction that is specific to two
+# dimensions. Application in three-dimensions requires a number of
+# changes.
 
+# +
+msh = create_unit_square(MPI.COMM_WORLD, 48, 48, CellType.quadrilateral, dtype=xdtype)
+# -
+#
+# Here we construct compatible function spaces for the mixed Poisson
+# problem. The `V` Raviart-Thomas ($\mathbb{RT}$) space is a vector-valued $H({\rm
+# div})$ conforming space. The `W` space is a space of discontinuous
+# Lagrange function of degree `k`.
+# ```{note}
+# The $\mathbb{RT}_{k}$ element in DOLFINx/Basix is usually denoted by
+# $\mathbb{RT}_{k-1}$ in the literature.
+# ```
+# In the lowest-order case $k=1$. It can be increased, by the
+# convergence of the iterative solver will degrade.
+
+# +
 k = 1
 V = fem.functionspace(msh, element("RT", msh.basix_cell(), k, dtype=xdtype))
-W = fem.functionspace(msh, element("DG", msh.basix_cell(), k - 1, dtype=xdtype))
+W = fem.functionspace(msh, element("Discontinuous Lagrange", msh.basix_cell(), k - 1, dtype=xdtype))
+# -
 
+# Trial functions for $\sigma$ and $u$ are declared on the space $V$ and
+# $W$, with corresponding test functions $\tau$ and $v$:
+
+# +
 (sigma, u) = TrialFunction(V), TrialFunction(W)
 (tau, v) = TestFunction(V), TestFunction(W)
+# -
 
+# The source function is set to be $f = 10\exp(-((x_{0} - 0.5)^2 +
+# (x_{1} - 0.5)^2) / 0.02)$:
+
+# +
 x = SpatialCoordinate(msh)
 f = 10.0 * exp(-((x[0] - 0.5) * (x[0] - 0.5) + (x[1] - 0.5) * (x[1] - 0.5)) / 0.02)
+# -
 
+# We now declare the bocked bilinar and linear forms. The rows of `a`
+# and `L` correspond to the $\tau$ and $v$ test functions, respectively.
+# The columns of `a` correspond to the $\sigma$ and $u$ trial functions,
+# respectively. Note that `a[1][1]` is empty, which is denoted by
+# `None`. This zero block is typical of a saddle-point problem. In the
+# `L[0]` block, the test function $\tau$ is multiplied by a zero
+# `Constant`, which is evaluated at runtime. We do this to preserve
+# knowledge of the test space in the block.
+
+# +
 dx = Measure("dx", msh)
 a = [[inner(sigma, tau) * dx, inner(u, div(tau)) * dx], [inner(div(sigma), v) * dx, None]]
 L = [
     inner(fem.Constant(msh, np.zeros(2, dtype=dtype)), tau) * dx,
     -inner(f, v) * dx,
 ]
+# -
 
+# We now compile the abstract/symbolic forms in `a` and `L` into
+# concrete instanced that can be assembled into matrix operators and
+# vectors, respectively.
+
+# +
 a, L = fem.form(a, dtype=dtype), fem.form(L, dtype=dtype)
+# -
 
+# In preparation for Dirichlet boundary conditions, we use the function
+# `locate_entities_boundary` to locate mesh entities (facets) with which
+# degree-of-freedoms to be constrained are associated with, and then use
+# `locate_dofs_topological` to get the  degree-of-freedom indices. Below
+# we identify the degree-of-freedom in `V` on the (i) top ($x_{1} = 1$)
+# and (ii) bottom ($x_{1} = 0$) of the mesh/domain.
 
+# +
 fdim = msh.topology.dim - 1
 dofs_top = fem.locate_dofs_topological(
     V, fdim, mesh.locate_entities_boundary(msh, fdim, lambda x: np.isclose(x[1], 1.0))
 )
+dofs_bottom = fem.locate_dofs_topological(
+    V, fdim, mesh.locate_entities_boundary(msh, fdim, lambda x: np.isclose(x[1], 0.0))
+)
+# -
+
+# Now, we create Dirichlet boundary objects for the condition $\sigma
+# \cdot n = \sin(5 x_(0)$ on the top and bottom boundaries:
+
+# +
 f_h1 = fem.Function(V, dtype=dtype)
 f_h1.interpolate(lambda x: np.vstack((np.zeros_like(x[0]), np.sin(5 * x[0]))))
 bc_top = fem.dirichletbc(f_h1, dofs_top)
 
-
-dofs_bottom = fem.locate_dofs_topological(
-    V, fdim, mesh.locate_entities_boundary(msh, fdim, lambda x: np.isclose(x[1], 0.0))
-)
 f_h2 = fem.Function(V, dtype=dtype)
 f_h2.interpolate(lambda x: np.vstack((np.zeros_like(x[0]), -np.sin(5 * x[0]))))
 bc_bottom = fem.dirichletbc(f_h2, dofs_bottom)
 
 bcs = [bc_top, bc_bottom]
+# -
 
-# Assemble the matrix operator as a 'nested' matrix
+# Assemble the matrix operator `A` into a PETSc 'nested matrix', zero
+# rows and columns associated with a Dirichlet boundary condition and
+# placing 1 on the diagonal for Dirichlet constrained
+# degrees-of-freedom:
+
+# +
 A = fem.petsc.assemble_matrix_nest(a, bcs=bcs)
 A.assemble()
-
-# Define preconditioner
-a_p00 = inner(sigma, tau) * dx + inner(div(sigma), div(tau)) * dx
-a_p11 = inner(u, v) * dx
-a_p = fem.form([[a_p00, None], [None, a_p11]], dtype=dtype)
-P = fem.petsc.assemble_matrix_nest(a_p, bcs=bcs)
-P.assemble()
+# -
 
 # Assemble the RHS vector as a 'nested' vector and modify (apply
-# lifting) to account for non-zero Dirichlet boundary conditions
+# lifting) to account for the effect non-zero Dirichlet boundary
+# conditions. Then set Dirichlet boundary values in the RHS vector `b`:
+
+# +
 b = fem.petsc.assemble_vector_nest(L)
 fem.petsc.apply_lifting_nest(b, a, bcs=bcs)
 for b_sub in b.getNestSubVecs():
     b_sub.ghostUpdate(addv=PETSc.InsertMode.ADD, mode=PETSc.ScatterMode.REVERSE)
 
-# Set Dirichlet boundary condition values in the RHS vector
 bcs0 = fem.bcs_by_block(fem.extract_function_spaces(L), bcs)
 fem.petsc.set_bc_nest(b, bcs0)
+# -
 
-# Create PETSc Krylov solver
+# Rather than solving the linear system $A x = b$, we will solve the
+# preconditioned problem $P^{-1} A x = P^{-1} b$. Commonly $P = A$, but this
+# does not lead to efficient solvers for saddle point problems.
+#
+# For this problem, we introduce the preconditioner
+# $$
+# a_p((\sigma, u), (\tau, v))
+# = \begin{bmatrix} \int_{\Omega} \sigma \cdot \tau + (\nabla \cdot
+# \sigma) (\nabla \cdot \tau) \ {\rm d} x  & 0 \\ 0 &
+# \int_{\Omega} u \cdot v \ {\rm d} x \end{bmatrix}
+# $$
+# and assemble it into the matrix `P`:
+
+# +
+a_p00 = inner(sigma, tau) * dx + inner(div(sigma), div(tau)) * dx
+a_p11 = inner(u, v) * dx
+a_p = fem.form([[a_p00, None], [None, a_p11]], dtype=dtype)
+P = fem.petsc.assemble_matrix_nest(a_p, bcs=bcs)
+P.assemble()
+# -
+
+# We now create a PETSc Krylov solver and set the preconditioner (`P`)
+# and operator (`A`) matrices:
+
+# +
 ksp = PETSc.KSP().create(msh.comm)
 ksp.setOperators(A, P)
-ksp.setMonitor(lambda _, its, rnorm: print(f"Iteration: {its}, rel. residual: {rnorm}"))
-ksp.setType("minres")
+ksp.setMonitor(lambda ksp, its, rnorm: print(f"Iteration: {its}, residual: {rnorm}"))
+ksp.setType("gmres")
 ksp.setTolerances(rtol=1e-8)
+ksp.setGMRESRestart(100)
+# -
 
-# Set a field-split (block) preconditioner
+# To apply different solvers/preconditioners to the blocks of `P`, we
+# set the preconditioer to be a `fieldsplit` (block) type and set the
+# 'splits' between the $\sigma$ and $u$ fields.
+
+# +
 ksp.getPC().setType("fieldsplit")
 ksp.getPC().setFieldSplitType(PETSc.PC.CompositeType.ADDITIVE)
 nested_IS = P.getNestISs()
 ksp.getPC().setFieldSplitIS(("sigma", nested_IS[0][0]), ("u", nested_IS[0][1]))
 ksp_sigma, ksp_u = ksp.getPC().getFieldSplitSubKSP()
-ksp_sigma.setType("preonly")
-ksp_sigma.getPC().setType("lu")
-# if PETSc.Sys().hasExternalPackage("superlu_dist"):
-#     ksp_sigma.getPC().setFactorSolverType("superlu_dist")
-# ksp_u.setType("preonly")
-# ksp_u.getPC().setType("bjacobi")
+# -
 
-# Solve
+# For the $P_{11}$ block, which is the discontinuous Lagrange mass
+# matrix, we let the preconditoner be the default, which is incomplete
+# LU factorisation and which can solve the block exactly in one
+# iteration. The $P_{00}$ requires careful handling as $H({\rm div})$
+# problems require special preconditioners to be efficient.
+#
+# If PETSc has been configured with Hypre, we use the Hypre `Auxiliary
+# Maxwell Space` (AMS) algebraic multigrid preconditioner. We can use
+# AMS for this $H({\rm div})$-type problem in two-dimensions because
+# $H({\rm div})$ and $H({\rm curl})$ spaces are effectively the same in
+# two-dimensions, just rotated by $\pi/2.
+#
+# +
+pc_sigma = ksp_sigma.getPC()
+if PETSc.Sys().hasExternalPackage("hypre"):
+    pc_sigma.setType("hypre")
+    pc_sigma.setHYPREType("ams")
+
+    opts = PETSc.Options()
+    opts[f"{ksp_sigma.prefix}pc_hypre_ams_cycle_type"] = 7
+    opts[f"{ksp_sigma.prefix}pc_hypre_ams_relax_times"] = 2
+
+    # Construct at set the 'discrete gradient' operator, which maps
+    # grad H1 -> H(curl), i.e. the gradient of a scalar Lagrange space
+    # to a H(curl) space
+    V_H1 = fem.functionspace(msh, element("Lagrange", msh.basix_cell(), k, dtype=xdtype))
+    V_curl = fem.functionspace(msh, element("N1curl", msh.basix_cell(), k, dtype=xdtype))
+    G = discrete_gradient(V_H1, V_curl)  #
+    G.assemble()
+    pc_sigma.setHYPREDiscreteGradient(G)
+
+    assert k > 0, "Element degree must be at least 1."
+    if k == 1:
+        # For the lowest order base (k=1), we can supply interpolation
+        # of the '1' vectors in the space V. Hypre can then construct
+        # the required operators from G and the '1' vectors.
+        cvec0, cvec1 = fem.Function(V), fem.Function(V)
+        cvec0.interpolate(lambda x: np.vstack((np.ones_like(x[0]), np.zeros_like(x[1]))))
+        cvec1.interpolate(lambda x: np.vstack((np.zeros_like(x[0]), np.ones_like(x[1]))))
+        pc_sigma.setHYPRESetEdgeConstantVectors(cvec0.x.petsc_vec, cvec1.x.petsc_vec, None)
+    else:
+        # For high-order spaces, we must provide the (H1)^d -> H(div)
+        # interpolation operator/matrix
+        V_H1d = fem.functionspace(msh, ("Lagrange", k, (msh.geometry.dim,)))
+        Pi = interpolation_matrix(V_H1d, V)  # (H1)^d -> H(div)
+        Pi.assemble()
+        pc_sigma.setHYPRESetInterpolations(msh.geometry.dim, None, None, Pi, None)
+
+        # High-order elements generally converge less well than the
+        # lowest-order case with algebraic multigrid, so we perform
+        # extra work at the multigrid stage
+        opts[f"{ksp_sigma.prefix}pc_hypre_ams_tol"] = 1e-12
+        opts[f"{ksp_sigma.prefix}pc_hypre_ams_max_iter"] = 3
+
+    ksp_sigma.setFromOptions()
+else:
+    # If Hypre is not available, use LU factorisation on the $P_{00}$
+    # block
+    pc_sigma.setType("lu")
+# -
+
+# Create finite element functions that will hold the solutions
+
+# +
 sigma, u = fem.Function(V, dtype=dtype), fem.Function(W, dtype=dtype)
+# -
+
+# Create a PETSc 'nested' vector that holds reference to the `sigma` and
+# `u` solution (degree-of-freedom) vectors and solve.
+
+# +
 x = PETSc.Vec().createNest([la.create_petsc_vector_wrap(sigma.x), la.create_petsc_vector_wrap(u.x)])
 ksp.solve(b, x)
+reason = ksp.getConvergedReason()
+assert reason > 0, f"Krylov solver has not converged {reason}."
+ksp.view()
+# -
 
-with io.XDMFFile(msh.comm, "out_mixed_poisson/u.xdmf", "w") as file:
-    file.write_mesh(msh)
-    file.write_function(u)
+# We save the solution `u` in VTX format. DOLFINx VTX output support
+# visualisation of discontinuous Lagrange element, hence the finte
+# element field can be visualised exactly. However, VTX does not support
+# cell-wise data, so for the lowest-order `u` case we interpolate to a
+# degree 1 discotintious space.
+
+# +
+try:
+    from dolfinx.io import VTXWriter
+
+    if k == 1:
+        gdim = msh.geometry.dim
+        V0 = fem.functionspace(msh, ("Discontinuous Lagrange", k))
+        u0 = fem.Function(V0, dtype=default_scalar_type)
+        u0.interpolate(u)
+        with VTXWriter(msh.comm, "output_mixed_poisson.bp", u0, "bp4") as f:
+            f.write(0.0)
+    else:
+        with VTXWriter(msh.comm, "output_mixed_poisson.bp", u, "bp4") as f:
+            f.write(0.0)
+except ImportError:
+    print("ADIOS2 required for VTX output.")
+# -
