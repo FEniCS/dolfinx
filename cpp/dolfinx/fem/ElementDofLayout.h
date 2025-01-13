b4f6451f1a166baacb40c872c71ef154cd5a6223
Garth N. Wells <gnw20@cam.ac.uk>
2025-01-13 08:01:02 +0000

    Doc update

--- cpp/dolfinx/fem/ElementDofLayout.h
+++ cpp/dolfinx/fem/ElementDofLayout.h
@@ -23,21 +23,24 @@ namespace dolfinx::fem
 //
 // TODO: Handle block dofmaps properly
 
-/// The class represents the degree-of-freedom (dofs) for an element.
-/// Dofs are associated with a mesh entity. This class also handles
-/// sub-space dofs, which are views into the parent dofs.
+/// Representation of the degree-of-freedom (dofs) for an element. Dofs
+/// are associated with a mesh entity. This class also handles sub-space
+/// dofs, which are views into the parent dofs.
 class ElementDofLayout
 {
 public:
-  /// Constructor
-  /// @param[in] block_size The number of dofs co-located at each point.
-  /// @param[in] entity_dofs The dofs on each entity, in the format:
-  /// entity_dofs[entity_dim][entity_number] = [dof0, dof1, ...]
-  /// @param[in] entity_closure_dofs The dofs on the closure of each
-  /// entity, in the format:
-  ///   entity_closure_dofs[entity_dim][entity_number] = [dof0, dof1, ...]
+  /// @brief Constructor
+  ///
+  /// @param[in] block_size Number of times each degree-of-freedom is
+  /// 'replicated', e.g. number of DOFs 'co-located' at each 'point'.
+  /// @param[in] entity_dofs Dofs on each entity, in the format
+  /// `entity_dofs[entity_dim][entity_number]=[dof0, dof1, ...]`.
+  /// @param[in] entity_closure_dofs Dofs on the closure of each entity,
+  /// in the format
+  /// `entity_closure_dofs[entity_dim][entity_number]=[dof0, dof1,
+  /// ...]`.
   /// @param[in] parent_map TODO
-  /// @param[in] sub_layouts (`std::vector<ElementDofLayout>`)TODO
+  /// @param[in] sub_layouts TODO
   ElementDofLayout(
       int block_size,
       const std::vector<std::vector<std::vector<int>>>& entity_dofs,
@@ -45,81 +48,93 @@ public:
       const std::vector<int>& parent_map,
       const std::vector<ElementDofLayout>& sub_layouts);
 
-  /// Copy the DOF layout, discarding any parent information
+  /// @brief Copy the DOF layout, discarding any parent information.
+  ///
+  /// @return Copy of the layout.
   ElementDofLayout copy() const;
 
-  /// Copy constructor
+  /// @brief Copy constructor
   ElementDofLayout(const ElementDofLayout& dofmap) = default;
 
-  /// Move constructor
+  /// @brief Move constructor
   ElementDofLayout(ElementDofLayout&& dofmap) = default;
 
-  /// Destructor
+  /// @brief Destructor
   ~ElementDofLayout() = default;
 
-  /// Copy assignment
+  /// @brief Copy assignment
   ElementDofLayout& operator=(const ElementDofLayout& dofmap) = default;
 
-  /// Move assignment
+  /// @brief Move assignment
   ElementDofLayout& operator=(ElementDofLayout&& dofmap) = default;
 
-  /// Equality operator
-  /// @return Returns true if the layout data is the same. Sub- and
-  /// parent dofmap data is not compared.
-  /// @note The block sizes of the layouts are not compared
+  /// @brief Equality operator
+  ///
+  /// Sub- and parent dof layout data is not compared. The block sizes
+  /// of the layouts are not compared.
+  ///
+  /// @return True if the layout data is the same, false otherwise.
   bool operator==(const ElementDofLayout& layout) const;
 
-  /// Return the dimension of the local finite element function space on
-  /// a cell (number of dofs on element)
+  /// @brief Dimension of the local finite element function space on a
+  /// cell (number of dofs on element).
   /// @return Dimension of the local finite element function space.
   int num_dofs() const;
 
-  /// Return the number of dofs for a given entity dimension
-  /// @param[in] dim Entity dimension
-  /// @return Number of dofs associated with given entity dimension
+  /// @brief Return the number of dofs for a given entity dimension.
+  /// @param[in] dim Entity dimension.
+  /// @return Number of dofs associated with given entity dimension.
   int num_entity_dofs(int dim) const;
 
-  /// Return the number of closure dofs for a given entity dimension
-  /// @param[in] dim Entity dimension
+  /// @brief Return the number of closure dofs for a given entity
+  /// dimension.
+  /// @param[in] dim Entity dimension.
   /// @return Number of dofs associated with closure of given entity
-  /// dimension
+  /// dimension.
   int num_entity_closure_dofs(int dim) const;
 
-  /// Local-local mapping of dofs on entity of cell
-  /// @param[in] dim The entity dimension
-  /// @param[in] entity_index The local entity index on the cell
-  /// @return Cell-local degree-of-freedom indices
+  /// @brief Local-local mapping of dofs on entity of cell.
+  /// @param[in] dim Entity dimension.
+  /// @param[in] entity_index Local entity index on the cell.
+  /// @return Cell-local degree-of-freedom indices.
   const std::vector<int>& entity_dofs(int dim, int entity_index) const;
 
-  /// Local-local closure dofs on entity of cell
-  /// @param[in] dim The entity dimension
-  /// @param[in] entity_index The local entity index on the cell
-  /// @return Cell-local degree-of-freedom indices
+  /// @brief Local-local closure dofs on entity of cell.
+  /// @param[in] dim Entity dimension.
+  /// @param[in] entity_index Local entity index on the cell.
+  /// @return Cell-local degree-of-freedom indices.
   const std::vector<int>& entity_closure_dofs(int dim, int entity_index) const;
 
-  /// Direct access to all entity dofs (dof = _entity_dofs[dim][entity][i])
+  /// @brief Direct access to all entity dofs.
+  ///
+  /// Storage is `dof=_entity_dofs[dim][entity][i]).
+  /// @return Entity dofs data structure.
   const std::vector<std::vector<std::vector<int>>>& entity_dofs_all() const;
 
-  /// Direct access to all entity closure dofs (dof =
-  /// _entity_dofs[dim][entity][i])
+  /// @brief Direct access to all entity closure dofs.
+  ///
+  /// Storage is  `dof=_entity_dofs[dim][entity][i])`.
+  /// @return Entity dofs data structure for closure dofs.
   const std::vector<std::vector<std::vector<int>>>&
   entity_closure_dofs_all() const;
 
-  /// Get number of sub-dofmaps
+  /// @brief  Get number of sub-layouts.
+  /// @return Number of sub-maps.
   int num_sub_dofmaps() const;
 
-  /// Get sub-dofmap given by list of components, one for each level
+  /// @brief Get sub-dofmap given by list of components, one for each
+  /// level.
   const ElementDofLayout& sub_layout(std::span<const int> component) const;
 
-  /// Get view for a sub-layout, defined by the component list (as for
-  /// sub_layour()), into this dofmap. I.e., the dofs in this dofmap
-  /// that are the sub-dofs.
+  /// @brief Get view for a sub-layout, defined by the component list
+  /// (as for ::sub_layout), into this dofmap. I.e., the dofs in this
+  /// dofmap that are the sub-dofs.
   std::vector<int> sub_view(std::span<const int> component) const;
 
-  /// Block size
+  /// @brief Block size.
   int block_size() const;
 
-  /// True iff dof map is a view into another map
+  /// @brief True iff dof map is a view into another map.
   /// @returns bool True if the dof map is a sub-dof map (a view into
   /// another map).
   bool is_view() const;
