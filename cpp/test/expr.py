b60c7b2d63bd326eb8d744fed6c781614c105c2a
Garth N. Wells <gnw20@cam.ac.uk>
2025-02-14 18:13:54 +0000

    Re-format

--- cpp/test/expr.py
+++ cpp/test/expr.py
@@ -1,6 +1,6 @@
 import ufl
+from basix import CellType, make_quadrature
 from basix.ufl import element
-from basix import make_quadrature, CellType
 
 cell = CellType.hexahedron
 mesh1 = ufl.Mesh(element("Lagrange", CellType.hexahedron, degree=1, shape=(3,)))
@@ -16,5 +16,5 @@ quadrature_rule = "default"
 u1, u2 = ufl.Coefficient(V1), ufl.Coefficient(V2)
 pts, _ = make_quadrature(CellType.hexahedron, quadrature_degree)
 Q6_P1, Q6_P2 = ufl.grad(u1), ufl.grad(u2)
-#expressions = [(Q6_P1, pts), (Q6_P2, pts)]
+# expressions = [(Q6_P1, pts), (Q6_P2, pts)]
 expressions = [(Q6_P2, pts)]
