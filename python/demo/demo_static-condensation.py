78040f9080d527df9c090870652a8882e1fabe43
Garth N. Wells <gnw20@cam.ac.uk>
2025-02-16 12:21:15 +0000

    Add coment

--- python/demo/demo_static-condensation.py
+++ python/demo/demo_static-condensation.py
@@ -133,7 +133,9 @@ kernel10 = getattr(ufcx10.form_integrals[0], f"tabulate_tensor_{np.dtype(PETSc.S
 ffi = cffi.FFI()
 if np.issubdtype(PETSc.ScalarType, np.complexfloating):
     if cffi.__version_info__ > (1, 16, 99) and cffi.__version_info__ <= (1, 17, 1):
-        print("CFFI 1.17.0 and 1.17.1 has a bug for complex type. Exiting.")
+        print(
+            "CFFI 1.17.0 and 1.17.1 has a bug for complex type. See https://github.com/FEniCS/dolfinx/pull/3635. Exiting."
+        )
         exit(0)
     cffi_support.register_type(ffi.typeof("double _Complex"), numba.types.complex128)
 
