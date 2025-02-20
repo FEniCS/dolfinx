f7639179ce0af5068dd3c8a3daf71c2fcceacc1f
Jørgen S. Dokken <dokken@simula.no>
2025-02-20 10:14:29 +0000

    Add snes solver

--- python/dolfinx/nls/petsc.py
+++ python/dolfinx/nls/petsc.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2021 Jørgen S. Dokken
+# Copyright (C) 2021-2025 Jørgen S. Dokken
 #
 # This file is part of DOLFINx (https://www.fenicsproject.org)
 #
@@ -25,7 +25,7 @@ from dolfinx import cpp as _cpp
 from dolfinx import fem
 from dolfinx.fem.petsc import create_matrix, create_vector
 
-__all__ = ["NewtonSolver"]
+__all__ = ["NewtonSolver", "SNESSolver"]
 
 
 class NewtonSolver(_cpp.nls.petsc.NewtonSolver):
@@ -72,3 +72,64 @@ class NewtonSolver(_cpp.nls.petsc.NewtonSolver):
 
         """
         super().setP(P, Pmat)
+
+class SNESSolver:
+    def __init__(self, problem: dolfinx.fem.petsc.SNESProblem, options: dict|None = None):
+        """Initialize a PETSc-SNES solver
+
+        Args:
+            problem: A problem instance for PETSc SNES
+            options: Solver options. Can include any options for sub objects such as KSP and PC
+        """
+        self.problem = problem
+        self.options = options if options is not None else {}
+        self.create_solver()
+        self.create_data_structures()
+        self.error_if_not_converged = True
+
+    def create_solver(self):
+        """Create the PETSc SNES object and set solver options"""
+        self._snes = PETSc.SNES().create(comm=self.problem.u.function_space.mesh.comm)
+        self._snes.setOptionsPrefix("snes_solve")
+        option_prefix = self._snes.getOptionsPrefix()
+        opts = PETSc.Options()
+        opts.prefixPush(option_prefix)
+        for key, v in self.options.items():
+            opts[key] = v
+        opts.prefixPop()
+        self._snes.setFromOptions()
+
+    def create_data_structures(self):
+        """
+        Create PETSc objects for the matrix, residual and solution
+        """
+        self._A = dolfinx.fem.petsc.create_matrix(self.problem.a)
+        self._b = dolfinx.fem.Function(self.problem.u.function_space, name="Residual")
+        self._x = dolfinx.fem.Function(self.problem.u.function_space, name="work-array")
+
+    @property
+    def _b_petsc(self):
+        return self._b.x.petsc_vec
+    
+    def solve(self):
+        # Set function and Jacobian (in case the change in the SNES problem)
+        self._snes.setFunction(self.problem.F, self._b_petsc)
+        self._snes.setJacobian(self.problem.J, self._A)
+
+        # Move current iterate into the work array.
+        self._x.interpolate(self.problem.u)
+        self._snes.solve(None, self._x.x.petsc_vec)
+        self._x.x.scatter_forward()
+
+        # Check for convergence
+        converged_reason = self._snes.getConvergedReason()
+        if self.error_if_not_converged and converged_reason < 0:
+            raise RuntimeError(f"Solver did not converge. Reason: {converged_reason}")
+
+        self.problem.u.x.array[:] = self._x.x.array 
+        return converged_reason, self._snes.getIterationNumber()
+
+    def __del__(self):
+        self._snes.destroy()
+        self._A.destroy()
+
