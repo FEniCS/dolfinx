bc24cf5ef225eace6b0ce0cb1c6546e22ac4876e
Claus Susanne <sclaus@Clauss-MacBook-Pro.local>
2025-02-21 16:41:40 +0100

    create intrinsic numba function to get empty void pointer for static condensation demo

--- python/demo/demo_static-condensation.py
+++ python/demo/demo_static-condensation.py
@@ -145,8 +145,20 @@ Ssize = S.element.space_dimension
 Usize = U.element.space_dimension
 
 
+@numba.extending.intrinsic
+def empty_void_pointer(typingctx):
+    """Custom intrinsic to return an empty void* pointer."""
+
+    def codegen(context, builder, signature, args):
+        null_ptr = context.get_constant(numba.types.voidptr, 0)
+        return null_ptr
+
+    sig = numba.types.voidptr()
+    return sig, codegen
+
+
 @numba.cfunc(ufcx_signature(PETSc.ScalarType, PETSc.RealType), nopython=True)  # type: ignore
-def tabulate_A(A_, w_, c_, coords_, entity_local_index, permutation=ffi.NULL, custom_data=ffi.NULL):
+def tabulate_A(A_, w_, c_, coords_, entity_local_index, permutation=ffi.NULL, custom_data=None):
     """Element kernel that applies static condensation."""
 
     # Prepare target condensed local element tensor
@@ -154,13 +166,37 @@ def tabulate_A(A_, w_, c_, coords_, entity_local_index, permutation=ffi.NULL, cu
 
     # Tabulate all sub blocks locally
     A00 = np.zeros((Ssize, Ssize), dtype=PETSc.ScalarType)
-    kernel00(ffi.from_buffer(A00), w_, c_, coords_, entity_local_index, permutation, custom_data)
+    kernel00(
+        ffi.from_buffer(A00),
+        w_,
+        c_,
+        coords_,
+        entity_local_index,
+        permutation,
+        empty_void_pointer(),
+    )
 
     A01 = np.zeros((Ssize, Usize), dtype=PETSc.ScalarType)
-    kernel01(ffi.from_buffer(A01), w_, c_, coords_, entity_local_index, permutation, custom_data)
+    kernel01(
+        ffi.from_buffer(A01),
+        w_,
+        c_,
+        coords_,
+        entity_local_index,
+        permutation,
+        empty_void_pointer(),
+    )
 
     A10 = np.zeros((Usize, Ssize), dtype=PETSc.ScalarType)
-    kernel10(ffi.from_buffer(A10), w_, c_, coords_, entity_local_index, permutation, custom_data)
+    kernel10(
+        ffi.from_buffer(A10),
+        w_,
+        c_,
+        coords_,
+        entity_local_index,
+        permutation,
+        empty_void_pointer(),
+    )
 
     # A = - A10 * A00^{-1} * A01
     A[:, :] = -A10 @ np.linalg.solve(A00, A01)
