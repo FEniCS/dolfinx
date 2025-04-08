7273d2b83f6a69fb74d76c2446f8a7f4b2f9cb1c
Garth N. Wells <gnw20@cam.ac.uk>
2025-04-08 14:12:54 +0100

    Fixes

--- python/test/unit/fem/test_petsc_solver_wrappers.py
+++ python/test/unit/fem/test_petsc_solver_wrappers.py
@@ -11,15 +11,22 @@ import numpy as np
 import pytest
 
 import basix.ufl
-import dolfinx
 import ufl
+from dolfinx.fem import (
+    Expression,
+    Function,
+    assemble_scalar,
+    dirichletbc,
+    form,
+    functionspace,
+    locate_dofs_topological,
+)
+from dolfinx.mesh import GhostMode, create_unit_square, locate_entities_boundary
 
 
 @pytest.mark.petsc4py
 class TestPETScSolverWrappers:
-    @pytest.mark.parametrize(
-        "mode", [dolfinx.mesh.GhostMode.none, dolfinx.mesh.GhostMode.shared_facet]
-    )
+    @pytest.mark.parametrize("mode", [GhostMode.none, GhostMode.shared_facet])
     def test_compare_solvers(self, mode):
         """Test that the wrapper for Linear problem and NonlinearProblem give the same result"""
         from petsc4py import PETSc
@@ -27,9 +34,9 @@ class TestPETScSolverWrappers:
         import dolfinx.nls.petsc
         from dolfinx.fem.petsc import LinearProblem, NonlinearProblem
 
-        msh = dolfinx.mesh.create_unit_square(MPI.COMM_WORLD, 12, 12, ghost_mode=mode)
-        V = dolfinx.fem.functionspace(msh, ("Lagrange", 1))
-        uh = dolfinx.fem.Function(V)
+        msh = create_unit_square(MPI.COMM_WORLD, 12, 12, ghost_mode=mode)
+        V = functionspace(msh, ("Lagrange", 1))
+        uh = Function(V)
         v = ufl.TestFunction(V)
         x = ufl.SpatialCoordinate(msh)
         f = x[0] * ufl.sin(x[1])
@@ -70,9 +77,7 @@ class TestPETScSolverWrappers:
         solver.solve(uh)
         assert np.allclose(u_lin.x.array, uh.x.array, atol=eps, rtol=eps)
 
-    @pytest.mark.parametrize(
-        "mode", [dolfinx.mesh.GhostMode.none, dolfinx.mesh.GhostMode.shared_facet]
-    )
+    @pytest.mark.parametrize("mode", [GhostMode.none, GhostMode.shared_facet])
     @pytest.mark.parametrize(
         "kind",
         [
@@ -87,27 +92,25 @@ class TestPETScSolverWrappers:
 
         from dolfinx.fem.petsc import LinearProblem
 
-        msh = dolfinx.mesh.create_unit_square(
-            MPI.COMM_WORLD, 12, 12, ghost_mode=mode, dtype=PETSc.RealType
-        )
+        msh = create_unit_square(MPI.COMM_WORLD, 12, 12, ghost_mode=mode, dtype=PETSc.RealType)
 
         def top_bc(x):
             return np.isclose(x[1], 1.0)
 
         msh.topology.create_connectivity(msh.topology.dim - 1, msh.topology.dim)
-        bndry_facets = dolfinx.mesh.locate_entities_boundary(msh, msh.topology.dim - 1, top_bc)
+        bndry_facets = locate_entities_boundary(msh, msh.topology.dim - 1, top_bc)
 
         el_0 = basix.ufl.element("Lagrange", msh.basix_cell(), 1, dtype=PETSc.RealType)
         el_1 = basix.ufl.element("Lagrange", msh.basix_cell(), 2, dtype=PETSc.RealType)
 
         if kind is None:
             me = basix.ufl.mixed_element([el_0, el_1])
-            W = dolfinx.fem.functionspace(msh, me)
+            W = functionspace(msh, me)
             V, _ = W.sub(0).collapse()
             Q, _ = W.sub(1).collapse()
         else:
-            V = dolfinx.fem.functionspace(msh, el_0)
-            Q = dolfinx.fem.functionspace(msh, el_1)
+            V = functionspace(msh, el_0)
+            Q = functionspace(msh, el_1)
             W = ufl.MixedFunctionSpace(V, Q)
 
         u, p = ufl.TrialFunctions(W)
@@ -121,42 +124,32 @@ class TestPETScSolverWrappers:
         L0 = ufl.inner(f, v) * ufl.dx
         L1 = ufl.inner(g, q) * ufl.dx
 
-        f_expr = dolfinx.fem.Expression(f, V.element.interpolation_points)
-        g_expr = dolfinx.fem.Expression(g, Q.element.interpolation_points)
-        u_bc = dolfinx.fem.Function(V)
+        f_expr = Expression(f, V.element.interpolation_points)
+        g_expr = Expression(g, Q.element.interpolation_points)
+        u_bc = Function(V)
         u_bc.interpolate(f_expr)
-        p_bc = dolfinx.fem.Function(Q)
+        p_bc = Function(Q)
         p_bc.interpolate(g_expr)
 
         if kind is None:
             a = a00 + a11
             L = L0 + L1
-            dofs_V = dolfinx.fem.locate_dofs_topological(
-                (W.sub(0), V), msh.topology.dim - 1, bndry_facets
-            )
-            dofs_Q = dolfinx.fem.locate_dofs_topological(
-                (W.sub(1), Q), msh.topology.dim - 1, bndry_facets
-            )
-            bcs = [
-                dolfinx.fem.dirichletbc(u_bc, dofs_V, W.sub(0)),
-                dolfinx.fem.dirichletbc(p_bc, dofs_Q, W.sub(1)),
-            ]
+            dofs_V = locate_dofs_topological((W.sub(0), V), msh.topology.dim - 1, bndry_facets)
+            dofs_Q = locate_dofs_topological((W.sub(1), Q), msh.topology.dim - 1, bndry_facets)
+            bcs = [dirichletbc(u_bc, dofs_V, W.sub(0)), dirichletbc(p_bc, dofs_Q, W.sub(1))]
         else:
             a = [[a00, None], [None, a11]]
             L = [L0, L1]
-            dofs_V = dolfinx.fem.locate_dofs_topological(V, msh.topology.dim - 1, bndry_facets)
-            dofs_Q = dolfinx.fem.locate_dofs_topological(Q, msh.topology.dim - 1, bndry_facets)
-            bcs = [
-                dolfinx.fem.dirichletbc(u_bc, dofs_V),
-                dolfinx.fem.dirichletbc(p_bc, dofs_Q),
-            ]
+            dofs_V = locate_dofs_topological(V, msh.topology.dim - 1, bndry_facets)
+            dofs_Q = locate_dofs_topological(Q, msh.topology.dim - 1, bndry_facets)
+            bcs = [dirichletbc(u_bc, dofs_V), dirichletbc(p_bc, dofs_Q)]
 
-        eps = 1e2 * np.finfo(PETSc.RealType()).eps
+        # eps = 1e2 * np.finfo(PETSc.RealType()).eps
         petsc_options = {
-            # "ksp_type": "preonly",
-            # "pc_type": "lu",
-            # "pc_factor_mat_solver_type": "mumps",
-            "ksp_rtol": eps,
+            "ksp_type": "preonly",
+            "pc_type": "lu",
+            "pc_factor_mat_solver_type": "mumps",
+            # "ksp_rtol": eps,
             "ksp_error_if_not_converged": True,
         }
 
@@ -166,10 +159,10 @@ class TestPETScSolverWrappers:
             uh, ph = wh.split()
         else:
             uh, ph = wh
-        error_uh = dolfinx.fem.form(ufl.inner(uh - f, uh - f) * ufl.dx)
-        error_ph = dolfinx.fem.form(ufl.inner(ph - g, ph - g) * ufl.dx)
-        local_uh_L2 = dolfinx.fem.assemble_scalar(error_uh)
-        local_ph_L2 = dolfinx.fem.assemble_scalar(error_ph)
+        error_uh = form(ufl.inner(uh - f, uh - f) * ufl.dx)
+        error_ph = form(ufl.inner(ph - g, ph - g) * ufl.dx)
+        local_uh_L2 = assemble_scalar(error_uh)
+        local_ph_L2 = assemble_scalar(error_ph)
         global_uh_L2 = np.sqrt(msh.comm.allreduce(local_uh_L2, op=MPI.SUM))
         global_ph_L2 = np.sqrt(msh.comm.allreduce(local_ph_L2, op=MPI.SUM))
         tol = 1e5 * np.finfo(PETSc.RealType()).eps
