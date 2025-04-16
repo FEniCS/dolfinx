49a67d612c0e537de1b2db8d0e3fdf71e1cc65d8
Garth N. Wells <gnw20@cam.ac.uk>
2025-04-16 21:42:42 +0100

    Doc update

--- cpp/demo/mixed_poisson/main.cpp
+++ cpp/demo/mixed_poisson/main.cpp
@@ -203,7 +203,7 @@ int main(int argc, char* argv[])
 
     // We'd like to represent `u_0` using a function space defined only
     // on the facets in dfacets. To do so, we begin by calling
-    // `create_submesh` to get a submesh of those facets. It also
+    // `create_submesh` to get a `submesh` of those facets. It also
     // returns a map `submesh_to_mesh` whose `i`th entry is the facet in
     // mesh corresponding to cell `i` in submesh.
     int tdim = mesh->topology()->dim();
