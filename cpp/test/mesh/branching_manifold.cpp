f29cad8d27f94f662897fa635ce5ba943a87823b
schnellerhase <56360279+schnellerhase@users.noreply.github.com>
2025-05-15 09:21:11 +0200

    Tidy up

--- cpp/test/mesh/branching_manifold.cpp
+++ cpp/test/mesh/branching_manifold.cpp
@@ -1,9 +1,10 @@
-// Copyright (C) 2024 Paul T. Kühner
+// Copyright (C) 2025 Paul T. Kühner
 //
 // This file is part of DOLFINx (https://www.fenicsproject.org)
 //
 // SPDX-License-Identifier:    LGPL-3.0-or-later
 
+#include "catch2/matchers/catch_matchers.hpp"
 #include <algorithm>
 #include <basix/finite-element.h>
 #include <catch2/catch_template_test_macros.hpp>
@@ -23,7 +24,7 @@
 
 using namespace dolfinx;
 
-// branching graph G
+// branching manifold graph G
 //
 //          (3)
 //           |
@@ -35,23 +36,27 @@ using namespace dolfinx;
 // its dual G'
 //
 //          [2]
-//         /   \
-//        /     \
+//         /   ＼
+//        /     ＼
 //      [0] --- [1] --- [3]
 //
 
-TEST_CASE("dual_graph_branching_mesh")
+TEST_CASE("dual_graph_branching")
 {
   std::vector<mesh::CellType> celltypes{mesh::CellType::interval};
   std::vector<std::int64_t> cells{{0, 1, 0, 2, 0, 3, 2, 4}};
-  auto [dual_graph, facets, facet_data, cell_data]
+  auto [dual_graph, unmatched_facets, max_vertices_per_facet, cell_data]
       = mesh::build_local_dual_graph(celltypes, {cells});
 
   CHECK(dual_graph.num_nodes() == 4);
+
   CHECK(dual_graph.num_links(0) == 2);
   CHECK_THAT(dual_graph.links(0),
              Catch::Matchers::RangeEquals(std::array{1, 2}));
+
   CHECK(dual_graph.num_links(1) == 3);
+  CHECK_THAT(dual_graph.links(3), Catch::Matchers::RangeEquals(std::array{1}));
+
   CHECK_THAT(dual_graph.links(1),
              Catch::Matchers::RangeEquals(std::array{0, 2, 3}));
 
@@ -61,6 +66,13 @@ TEST_CASE("dual_graph_branching_mesh")
 
   CHECK(dual_graph.num_links(3) == 1);
   CHECK_THAT(dual_graph.links(3), Catch::Matchers::RangeEquals(std::array{1}));
+
+  CHECK_THAT(unmatched_facets,
+             Catch::Matchers::RangeEquals(std::array{1, 3, 4}));
+
+  CHECK(max_vertices_per_facet == 1);
+
+  CHECK_THAT(cell_data, Catch::Matchers::RangeEquals(std::array{0, 2, 3}));
 }
 
 // TEST_CASE("debug")
