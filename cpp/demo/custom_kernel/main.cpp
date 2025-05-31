4775760fa5f36c483c3c2f31cfe5b9e712425c5e
Garth N. Wells <gnw20@cam.ac.uk>
2025-05-31 14:06:03 +0100

    Update demo

--- cpp/demo/custom_kernel/main.cpp
+++ cpp/demo/custom_kernel/main.cpp
@@ -141,9 +141,13 @@ double assemble_matrix1(const mesh::Geometry<T>& g, const fem::DofMap& dofmap,
   common::Timer timer("Assembler1 lambda (matrix)");
   md::mdspan<const T, md::extents<std::size_t, md::dynamic_extent, 3>> x(
       g.x().data(), g.x().size() / 3, 3);
-  fem::impl::assemble_cells<T>(
-      A.mat_add_values(), g.dofmap(), x, cells, {dofmap.map(), 1, cells}, ident,
-      {dofmap.map(), 1, cells}, ident, {}, {}, kernel, {}, {}, {}, {});
+
+  std::vector<T> Ae_b(dofmap.map().extent(1) * dofmap.map().extent(1));
+  std::vector<scalar_value_t<T>> cdofs_b(3 * g.dofmap().extent(1));
+  fem::impl::assemble_cells<T>(A.mat_add_values(), g.dofmap(), x, cells,
+                               {dofmap.map(), 1, cells}, ident,
+                               {dofmap.map(), 1, cells}, ident, {}, {}, kernel,
+                               {}, {}, {}, {}, Ae_b, cdofs_b);
   A.scatter_rev();
   return A.squared_norm();
 }
