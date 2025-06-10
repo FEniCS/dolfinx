85daff5941e4e631aff5fa27c5d594a5d8a52f1a
Joe Dean <jpd62@cam.ac.uk>
2025-06-10 17:16:27 +0100

    Add a demo showing how submeshes can be used to represent boundary data (#3711)
    * Start modifying demo
    
    * Change order
    
    * Tidy
    
    * Tidy
    
    * Document
    
    * Tidy
    
    * Docs
    
    * Doc updates
    
    * Work on docs
    
    * Doc update
    
    * Update cpp/demo/mixed_poisson/main.cpp
    
    Co-authored-by: Jørgen Schartum Dokken <dokken92@gmail.com>
    
    * Update cpp/demo/mixed_poisson/main.cpp
    
    Co-authored-by: Jørgen Schartum Dokken <dokken92@gmail.com>
    
    * Update cpp/demo/mixed_poisson/main.cpp
    
    Co-authored-by: Jørgen Schartum Dokken <dokken92@gmail.com>
    
    * Don't hardcode celltype
    
    * Docs
    
    * Ruff
    
    * Use continous Lagrange space
    
    * Write with VTXWriter too
    
    * Add comment
    
    * Add comment
    
    ---------
    
    Co-authored-by: Garth N. Wells <gnw20@cam.ac.uk>
    Co-authored-by: Jørgen Schartum Dokken <dokken92@gmail.com>

--- cpp/demo/mixed_poisson/mixed_poisson.py
+++ cpp/demo/mixed_poisson/mixed_poisson.py
@@ -3,6 +3,8 @@
 # {download}`demo_mixed_poisson/mixed_poisson.py`.  We begin by defining the
 # finite element:
 
+from basix import CellType
+from basix.cell import sub_entity_type
 from basix.ufl import element, mixed_element
 from ufl import (
     Coefficient,
@@ -16,12 +18,23 @@ from ufl import (
     inner,
 )
 
-shape = "triangle"
-RT = element("RT", shape, 1)
-P = element("DP", shape, 0)
+# Cell type for the mesh
+msh_cell = CellType.triangle
+
+# Weakly enforced boundary data will be represented using a function space
+# defined over a submesh of the boundary. We get the submesh cell type from
+# then mesh cell type.
+submsh_cell = sub_entity_type(msh_cell, dim=1, index=0)
+
+# Define finite elements for the problem
+RT = element("RT", msh_cell, 1)
+P = element("DP", msh_cell, 0)
 ME = mixed_element([RT, P])
 
-msh = Mesh(element("Lagrange", shape, 1, shape=(2,)))
+# Define UFL mesh and submesh
+msh = Mesh(element("Lagrange", msh_cell, 1, shape=(2,)))
+submsh = Mesh(element("Lagrange", submsh_cell, 1, shape=(2,)))
+
 n = FacetNormal(msh)
 V = FunctionSpace(msh, ME)
 
@@ -30,8 +43,13 @@ V = FunctionSpace(msh, ME)
 
 V0 = FunctionSpace(msh, P)
 f = Coefficient(V0)
-u0 = Coefficient(V0)
 
+# We represent boundary data using a first-degree Lagrange space
+# defined over a submesh of the boundary
+Q = FunctionSpace(submsh, element("Lagrange", submsh_cell, 1))
+u0 = Coefficient(Q)
+
+# Specify the weak form of the problem
 dx = Measure("dx", msh)
 ds = Measure("ds", msh)
 a = inner(sigma, tau) * dx + inner(u, div(tau)) * dx + inner(div(sigma), v) * dx
