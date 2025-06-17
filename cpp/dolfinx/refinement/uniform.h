d7047aed3cef098f9b1a795271ab600d60429eb8
Chris Richardson <chris@bpi.cam.ac.uk>
2025-06-17 14:32:10 +0100

    clang-format

--- cpp/dolfinx/refinement/uniform.h
+++ cpp/dolfinx/refinement/uniform.h
@@ -5,7 +5,8 @@
 namespace dolfinx::refinement
 {
 
-/// @brief Uniform refinement of a 3D mesh containing hex, tet, prism or pyramids.
+/// @brief Uniform refinement of a 3D mesh containing hex, tet, prism or
+/// pyramids.
 /// @param mesh Input mesh
 mesh::Mesh<double> uniform_refine(const mesh::Mesh<double>& mesh);
 
