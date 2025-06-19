8eb109cebf2c1f01d66337cec683a2228d8c6649
Chris Richardson <chris@bpi.cam.ac.uk>
2025-06-19 14:45:11 +0200

    working for 2D too

--- cpp/dolfinx/refinement/uniform.cpp
+++ cpp/dolfinx/refinement/uniform.cpp
@@ -12,12 +12,17 @@ using namespace dolfinx;
 
 mesh::Mesh<double> refinement::uniform_refine(const mesh::Mesh<double>& mesh)
 {
-  // Requires edges and facets to be built already
+  // Requires edges (and facets for some 3D meshes) to be built already
   auto topology = mesh.topology();
+  int tdim = topology->dim();
+  if (tdim < 2)
+    throw std::runtime_error("Uniform refinement only for 2D and 3D meshes");
+
+  spdlog::info("Topology dim = {}", tdim);
 
   // Collect up entity types on each dimension
   std::vector<std::vector<mesh::CellType>> entity_types;
-  for (int i = 0; i < 4; ++i)
+  for (int i = 0; i < tdim + 1; ++i)
     entity_types.push_back(topology->entity_types(i));
 
   // Get index maps for vertices and edges and create maps for new vertices
@@ -26,17 +31,21 @@ mesh::Mesh<double> refinement::uniform_refine(const mesh::Mesh<double>& mesh)
 
   // Indices for vertices and edges on dim 0 and dim 1.
   std::vector<int> e_index = {0, 0};
+
   // Check for quadrilateral faces and get index, if any.
   if (auto it = std::find(entity_types[2].begin(), entity_types[2].end(),
                           mesh::CellType::quadrilateral);
       it != entity_types[2].end())
     e_index.push_back(std::distance(entity_types[2].begin(), it));
 
-  // Check for hexahedral cells, and get index, if any.
-  if (auto it = std::find(entity_types[3].begin(), entity_types[3].end(),
-                          mesh::CellType::hexahedron);
-      it != entity_types[3].end())
-    e_index.push_back(std::distance(entity_types[3].begin(), it));
+  if (tdim == 3)
+  {
+    // In 3D, check for hexahedral cells, and get index, if any.
+    if (auto it = std::find(entity_types[3].begin(), entity_types[3].end(),
+                            mesh::CellType::hexahedron);
+        it != entity_types[3].end())
+      e_index.push_back(std::distance(entity_types[3].begin(), it));
+  }
 
   // Add up all local vertices, edges, quad facets and hex cells.
   std::int64_t nlocal = 0;
@@ -59,19 +68,20 @@ mesh::Mesh<double> refinement::uniform_refine(const mesh::Mesh<double>& mesh)
                                         + index_maps[0]->num_ghosts());
 
   // Iterate over cells
-  for (int j = 0; j < static_cast<int>(entity_types[3].size()); ++j)
+  for (int j = 0; j < static_cast<int>(entity_types.back().size()); ++j)
   {
     // Get geometry for each cell type
     auto x_dofmap = mesh.geometry().dofmap(j);
-    auto c_to_v = topology->connectivity({3, j}, {0, 0});
+    auto c_to_v = topology->connectivity({tdim, j}, {0, 0});
     auto dof_layout = mesh.geometry().cmaps().at(j).create_dof_layout();
     std::vector<int> entity_dofs(dof_layout.num_dofs());
     for (int k = 0; k < dof_layout.num_dofs(); ++k)
       entity_dofs[k] = dof_layout.entity_dofs(0, k)[0];
 
     // Iterate over cells of this type
-    for (std::int32_t c = 0; c < topology->index_maps(3)[j]->size_local()
-                                     + topology->index_maps(3)[j]->num_ghosts();
+    for (std::int32_t c = 0;
+         c < topology->index_maps(tdim)[j]->size_local()
+                 + topology->index_maps(tdim)[j]->num_ghosts();
          ++c)
     {
       auto vertices = c_to_v->links(c);
@@ -125,58 +135,75 @@ mesh::Mesh<double> refinement::uniform_refine(const mesh::Mesh<double>& mesh)
 
   // Create new topology...
 
-  std::vector<std::vector<std::int64_t>> mixed_topology(entity_types[3].size());
+  std::vector<std::vector<std::int64_t>> mixed_topology(
+      entity_types.back().size());
 
   // Find index of tets in topology list, if any
   int ktet = -1;
-  auto it = std::find(entity_types[3].begin(), entity_types[3].end(),
+  auto it = std::find(entity_types.back().begin(), entity_types.back().end(),
                       mesh::CellType::tetrahedron);
-  if (it != entity_types[3].end())
-    ktet = std::distance(entity_types[3].begin(), it);
+  if (it != entity_types.back().end())
+    ktet = std::distance(entity_types.back().begin(), it);
   // Topology for tetrahedra which arise from pyramid subdivision
   std::vector<int> pyr_to_tet_list
       = {5, 13, 7, 9, 6, 13, 11, 7, 10, 13, 12, 11, 8, 13, 9, 12};
 
   std::vector<int> refined_cell_list;
-  for (int k = 0; k < static_cast<int>(entity_types[3].size()); ++k)
+  for (int k = 0; k < static_cast<int>(entity_types.back().size()); ++k)
   {
     // Reserve an estimate of space for the topology of each type
-    mixed_topology[k].reserve(mesh.topology()->index_maps(3)[k]->size_local()
+    mixed_topology[k].reserve(mesh.topology()->index_maps(tdim)[k]->size_local()
                               * 8 * 6);
 
     // Select correct subdivision for celltype
     // Hex -> 8 hex, Prism -> 8 prism, Tet -> 8 tet, Pyr -> 5 pyr + 4 tet
-    if (entity_types[3][k] == mesh::CellType::hexahedron)
+    if (entity_types.back()[k] == mesh::CellType::hexahedron)
+    {
+      spdlog::info("Hex subdivision");
       refined_cell_list
           = {0, 9,  8,  20, 10, 22, 21, 26, 1, 11, 8,  20, 12, 23, 21, 26,
              2, 13, 9,  20, 14, 24, 22, 26, 3, 13, 11, 20, 15, 24, 23, 26,
              4, 16, 10, 21, 17, 25, 22, 26, 5, 16, 12, 21, 18, 25, 23, 26,
              6, 17, 14, 22, 19, 25, 24, 26, 7, 18, 15, 23, 19, 25, 24, 26};
-    else if (entity_types[3][k] == mesh::CellType::tetrahedron)
+    }
+    else if (entity_types.back()[k] == mesh::CellType::tetrahedron)
     {
+      spdlog::info("Tet subdivision");
       refined_cell_list = {0, 7, 8, 9, 1, 5, 6, 9, 2, 4, 6, 8, 3, 4, 5, 7,
                            9, 4, 6, 8, 9, 4, 8, 7, 9, 4, 7, 5, 9, 4, 5, 6};
     }
-    else if (entity_types[3][k] == mesh::CellType::prism)
+    else if (entity_types.back()[k] == mesh::CellType::prism)
     {
+      spdlog::info("Prism subdivision");
       refined_cell_list
           = {0,  6,  7,  8,  15, 16, 6,  1,  9,  15, 10, 17, 7,  9,  2,  16,
              17, 11, 6,  9,  7,  15, 17, 16, 15, 17, 16, 12, 14, 13, 8,  15,
              16, 3,  12, 13, 11, 17, 16, 5,  14, 13, 10, 15, 17, 4,  12, 14};
     }
-    else if (entity_types[3][k] == mesh::CellType::pyramid)
+    else if (entity_types.back()[k] == mesh::CellType::pyramid)
     {
+      spdlog::info("Pyramid subdivision");
       refined_cell_list = {0,  5,  6, 13, 7,  1,  8,  5, 13, 9,  3,  10, 8,
                            13, 12, 2, 6,  10, 13, 11, 7, 9,  11, 12, 4};
       if (ktet == -1)
         throw std::runtime_error("Cannot refine mesh with pyramids and no "
                                  "tetrahedra.");
     }
+    else if (entity_types.back()[k] == mesh::CellType::triangle)
+    {
+      spdlog::info("Triangle subdivision");
+      refined_cell_list = {0, 4, 5, 1, 5, 3, 2, 3, 4, 3, 4, 5};
+    }
+    else if (entity_types.back()[k] == mesh::CellType::quadrilateral)
+    {
+      spdlog::info("Quad subdivision");
+      refined_cell_list = {0, 4, 5, 8, 1, 6, 4, 8, 2, 7, 5, 8, 3, 7, 6, 8};
+    }
 
-    auto c_to_v = topology->connectivity({3, k}, {0, 0});
-    auto c_to_e = topology->connectivity({3, k}, {1, 0});
+    auto c_to_v = topology->connectivity({tdim, k}, {0, 0});
+    auto c_to_e = topology->connectivity({tdim, k}, {1, 0});
 
-    for (int c = 0; c < topology->index_maps(3)[k]->size_local(); ++c)
+    for (int c = 0; c < topology->index_maps(tdim)[k]->size_local(); ++c)
     {
       std::vector<std::int64_t> entities;
       for (std::int32_t i : c_to_v->links(c))
@@ -185,22 +212,29 @@ mesh::Mesh<double> refinement::uniform_refine(const mesh::Mesh<double>& mesh)
         entities.push_back(new_v[1][i]);
       if (e_index.size() > 2)
       {
-        auto conn = topology->connectivity({3, k}, {2, e_index[2]});
-        if (conn)
+        if (tdim == 3)
         {
-          spdlog::debug("Get connectivity from (3,{})->(2,{})", k, e_index[2]);
-          for (std::int32_t i :
-               topology->connectivity({3, k}, {2, e_index[2]})->links(c))
-            entities.push_back(new_v[2][i]);
+          auto conn = topology->connectivity({3, k}, {2, e_index[2]});
+          if (conn)
+          {
+            spdlog::debug("Get connectivity from (3,{})->(2,{})", k,
+                          e_index[2]);
+            for (std::int32_t i :
+                 topology->connectivity({3, k}, {2, e_index[2]})->links(c))
+              entities.push_back(new_v[2][i]);
+          }
         }
+        else
+          entities.push_back(new_v[2][c]);
       }
+
       if (e_index.size() > 3)
         entities.push_back(new_v[3][c]);
 
       for (int i : refined_cell_list)
         mixed_topology[k].push_back(entities[i]);
 
-      if (entity_types[3][k] == mesh::CellType::pyramid)
+      if (entity_types.back()[k] == mesh::CellType::pyramid)
       {
         for (int i : pyr_to_tet_list)
           mixed_topology[ktet].push_back(entities[i]);
--- python/test/unit/refinement/test_uniform.py
+++ python/test/unit/refinement/test_uniform.py
@@ -3,11 +3,11 @@ from mpi4py import MPI
 import pytest
 
 import dolfinx
-from dolfinx.mesh import CellType, Mesh, create_unit_cube
+from dolfinx.mesh import CellType, Mesh, create_unit_cube, create_unit_square
 
 
 @pytest.mark.parametrize("ctype", [CellType.hexahedron, CellType.tetrahedron, CellType.prism])
-def test_uniform_refinement(ctype):
+def test_uniform_refinement_3d(ctype):
     mesh = create_unit_cube(MPI.COMM_WORLD, 3, 2, 1, cell_type=ctype)
 
     ncells0 = mesh.topology.index_map(3).size_local
@@ -17,4 +17,17 @@ def test_uniform_refinement(ctype):
     m2 = Mesh(dolfinx.cpp.refinement.uniform_refine(mesh._cpp_object), None)
     ncells1 = m2.topology.index_map(3).size_local
 
-    assert ncells0 * 8 == ncells1
+    assert mesh.comm.allreduce(ncells0) * 8 == mesh.comm.allreduce(ncells1)
+
+
+@pytest.mark.parametrize("ctype", [CellType.triangle, CellType.quadrilateral])
+def test_uniform_refinement_2d(ctype):
+    mesh = create_unit_square(MPI.COMM_WORLD, 12, 11, cell_type=ctype)
+
+    ncells0 = mesh.topology.index_map(2).size_local
+    mesh.topology.create_entities(1)
+
+    m2 = Mesh(dolfinx.cpp.refinement.uniform_refine(mesh._cpp_object), None)
+    ncells1 = m2.topology.index_map(2).size_local
+
+    assert mesh.comm.allreduce(ncells0) * 4 == mesh.comm.allreduce(ncells1)
