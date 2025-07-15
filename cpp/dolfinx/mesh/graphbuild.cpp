b674fb45033a2ddf5b4a391f2f7dca10960afc30
Garth N. Wells <gnw20@cam.ac.uk>
2025-07-15 20:24:26 +0100

    Simplify

--- cpp/dolfinx/mesh/graphbuild.cpp
+++ cpp/dolfinx/mesh/graphbuild.cpp
@@ -241,14 +241,10 @@ graph::AdjacencyList<std::int64_t> compute_nonlocal_dual_graph(
       // Copy facet data into buffer
       auto fdata = facets.subspan(f * local_max_vertices_per_facet,
                                   local_max_vertices_per_facet);
-      std::ranges::copy(fdata,
-                        std::next(send_buffer.begin(), buffer_shape1 * pos));
-      // std::copy_n(std::next(facets.begin(), f *
-      // local_max_vertices_per_facet),
-      //             local_max_vertices_per_facet,
-      //             std::next(send_buffer.begin(), buffer_shape1 * pos));
-      send_buffer[buffer_shape1 * pos + max_vertices_per_facet]
-          = cells[f] + cell_offset;
+      std::span send_buffer_f(send_buffer.data() + buffer_shape1 * pos,
+                              max_vertices_per_facet + 1);
+      std::ranges::copy(fdata, send_buffer_f.begin());
+      send_buffer_f.back() = cells[f] + cell_offset;
       ++send_offsets[neigh_dest];
     }
   }
