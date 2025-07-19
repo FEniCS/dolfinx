02e8d772127b18d8e0b989289582871cb7cf1584
Garth N. Wells <gnw20@cam.ac.uk>
2025-07-19 19:19:39 +0100

    Simplify imports and testing

--- python/demo/test.py
+++ python/demo/test.py
@@ -4,6 +4,7 @@
 #
 # SPDX-License-Identifier:    LGPL-3.0-or-later
 
+import importlib.util
 import pathlib
 import subprocess
 import sys
@@ -13,16 +14,19 @@ import pytest
 # Get directory of this file
 path = pathlib.Path(__file__).resolve().parent
 
-# Build list of demo programs
-demos = []
-demo_files = list(path.glob("**/*.py"))
-for f in demo_files:
+
+def imports_petsc4py(f):
     with open(f, encoding="utf-8") as file:
         read_data = file.read()
-        if "petsc4py" in read_data:
-            print("petsc4oy demo")
-    # continue
-    demos.append((f.parent, f.name))
+        return "petsc4py" in read_data
+
+
+# Build list of demo programs
+demo_files = list(path.glob("**/*.py"))
+if importlib.util.find_spec("petsc4py") is not None:
+    demos = [(f.parent, f.name) for f in demo_files]
+else:
+    demos = [(f.parent, f.name) for f in demo_files if not imports_petsc4py(f)]
 
 
 @pytest.mark.serial
