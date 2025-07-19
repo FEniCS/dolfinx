11ada30874060a8c6e471dbb62685065f6520738
Francesco Ballarin <francesco.ballarin@unicatt.it>
2025-07-19 07:53:35 +0200

    Update python/test/unit/fem/test_assembler.py

--- python/test/unit/fem/test_assembler.py
+++ python/test/unit/fem/test_assembler.py
@@ -1217,7 +1217,7 @@ class TestPETScAssemblers:
         # Apply lifting with input coefficient
         coeffs = pack_coefficients(J)
         b = petsc_create_vector(L, kind=kind)
-        assert b.equal(create_vector([V, Q]))
+        assert b.equal(petsc_create_vector([V, Q]))
         petsc_apply_lifting(b, J, bcs=bcs1, coeffs=coeffs)
         b.assemble()
 
