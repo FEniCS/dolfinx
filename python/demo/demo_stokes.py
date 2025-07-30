9af1fa1af8d9dced6e23731090304d09cb1c097e
jorgensd <dokken92@gmail.com>
2025-07-30 09:28:29 +0000

    Ruff

--- python/demo/demo_stokes.py
+++ python/demo/demo_stokes.py
@@ -74,10 +74,10 @@
 # ## Implementation
 #
 # The Stokes problem using Taylor-Hood elements is solved using:
-# 1. [Block preconditioner using PETSc Nest data
-#    structures using the high-level interface](#high-level-nested-matrix-solver)
-# 1. [Block preconditioner using PETSc Nest data
-#    structures using the low-level interface](#low-level-nested-matrix-solver)
+# 1. [Block preconditioner using PETSc Nest data structures using
+#    the high-level interface](#high-level-nested-matrix-solver)
+# 1. [Block preconditioner using PETSc Nest data structures using
+#    the low-level interface](#low-level-nested-matrix-solver)
 # 1. [Block preconditioner with the `u` and `p` fields stored block-wise
 #    in a single matrix](#monolithic-block-iterative-solver)
 # 1. [Direct solver with the `u` and `p` fields stored block-wise in a
