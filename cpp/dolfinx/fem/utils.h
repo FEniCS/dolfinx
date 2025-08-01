d90075d2970e022368bd5f03417cf344f81ee924
schnellerhase <56360279+schnellerhase@users.noreply.github.com>
2025-08-01 18:12:37 +0200

    Move kernel extraction to own header

--- cpp/dolfinx/fem/utils.h
+++ cpp/dolfinx/fem/utils.h
@@ -15,6 +15,7 @@
 #include "Form.h"
 #include "Function.h"
 #include "FunctionSpace.h"
+#include "kernel.h"
 #include "sparsitybuild.h"
 #include <algorithm>
 #include <array>
@@ -91,47 +92,6 @@ get_cell_facet_pairs(std::int32_t f, std::span<const std::int32_t> cells,
   return cell_local_facet_pairs;
 }
 
-/// @brief Kernel C-pointer type.
-/// @tparam T scalar type.
-/// @tparam U geometry type.
-template <dolfinx::scalar T, std::floating_point U = scalar_value_t<T>>
-using kern_c_t = void (*)(T*, const T*, const T*, const U*, const int*,
-                          const std::uint8_t*, void*);
-
-/// @brief Kernel callback type.
-/// @tparam T scalar type.
-/// @tparam U geometry type.
-template <dolfinx::scalar T, std::floating_point U = scalar_value_t<T>>
-using kern_t = std::function<void(T*, const T*, const T*, const U*, const int*,
-                                  const std::uint8_t*, void*)>;
-
-/// @brief Extract correct kernel by type from UFCx integral.
-/// @tparam T scalar type of kernel to extract.s
-/// @tparam U geometry type of kernel to extract.
-/// @param integral UFCx integral to retrieve the kernel from.
-/// @return Kernel callback.
-template <dolfinx::scalar T, std::floating_point U = scalar_value_t<T>>
-constexpr kern_t<T, U> extract_kernel(const ufcx_integral* integral)
-{
-  if constexpr (std::is_same_v<T, float>)
-    return integral->tabulate_tensor_float32;
-  else if constexpr (std::is_same_v<T, std::complex<float>>
-                     && has_complex_ufcx_kernels())
-  {
-    return reinterpret_cast<kern_c_t<T, U>>(
-        integral->tabulate_tensor_complex64);
-  }
-  else if constexpr (std::is_same_v<T, double>)
-    return integral->tabulate_tensor_float64;
-  else if constexpr (std::is_same_v<T, std::complex<double>>
-                     && has_complex_ufcx_kernels())
-  {
-    return reinterpret_cast<kern_c_t<T, U>>(
-        integral->tabulate_tensor_complex128);
-  }
-  else
-    throw std::runtime_error("Could not extract kernel from ufcx integral.");
-}
 } // namespace impl
 
 /// @brief Given an integral type and a set of entities, computes and
