d5595f70e7be4893a45c9fdf57fed543df43c88a
schnellerhase <56360279+schnellerhase@users.noreply.github.com>
2025-08-02 12:07:10 +0200

    document

--- cpp/dolfinx/multigrid/inclusion.h
+++ cpp/dolfinx/multigrid/inclusion.h
@@ -21,6 +21,22 @@
 namespace dolfinx::multigrid
 {
 
+/**
+ * @brief Computes an inclusion map: a map between vertex indices from one mesh
+ * to another.
+ *
+ * @param mesh_from Domain of the map
+ * @param mesh_to Range of the map
+ *
+ * @return Inclusion map, the `i`-th component is the vertex index of the vertex
+ * with the same coordinates in `mesh_to` and `-1` if it can not be found
+ * (locally!) in `mesh_to`. If `map[i] != -1` it holds
+ * `mesh_from.geometry.x()[i:i+3] == mesh_to.geometry.x()[map[i]:map[i]+3]`.
+ *
+ * @note Invoking `inclusion_map` on a `(mesh_coarse, mesh_fine)` tuple, where
+ * `mesh_fine` is produced by refinement with `IdentityPartitionerPlaceholder()`
+ * option, the returned `map` is guaranteed to match all vertices.
+ */
 template <std::floating_point T>
 std::vector<std::int32_t>
 inclusion_mapping(const dolfinx::mesh::Mesh<T>& mesh_from,
