30bb8de1f9b238819fa4fe1c909809766e124121
schnellerhase <56360279+schnellerhase@users.noreply.github.com>
2025-08-07 14:20:27 +0200

    format

--- cpp/dolfinx/fem/utils.h
+++ cpp/dolfinx/fem/utils.h
@@ -115,12 +115,14 @@ get_cell_facet_pairs(std::int32_t f, std::span<const std::int32_t> cells,
 ///
 /// @param[in] integral_type Integral type.
 /// @param[in] topology Mesh topology.
-/// @param[in] entities List of mesh entities. Depending on the `IntegralType` these are associated with different entities:
-///     `IntegralType::cell`:             cells 
+/// @param[in] entities List of mesh entities. Depending on the `IntegralType`
+/// these are associated with different entities:
+///     `IntegralType::cell`:             cells
 ///     `IntegralType::exterior_facet`:   facets
 ///     `IntegralType::interior_facet`:   facets
 ///     `IntegralType::vertex`:           vertices
-/// @return List of integration entity data, depending on the `IntegralType` the data per entity has different layouts
+/// @return List of integration entity data, depending on the `IntegralType` the
+/// data per entity has different layouts
 ///     `IntegralType::cell`:             cell
 ///     `IntegralType::exterior_facet`:   (cell, local_facet)
 ///     `IntegralType::interior_facet`:   (cell, local_facet)
