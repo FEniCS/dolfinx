4f423a8b972f0ee105fed9643136842654368487
schnellerhase <56360279+schnellerhase@users.noreply.github.com>
2025-08-27 19:48:20 +0200

    typos

--- python/dolfinx/io/gmshio.py
+++ python/dolfinx/io/gmshio.py
@@ -2,4 +2,4 @@ import warnings
 
 from dolfinx.io.gmsh import *  # noqa: F403
 
-warnings.deprecated("dolfinx.io.gmshio is deprected, use dolfinx.io.gmsh.")
+warnings.deprecated("dolfinx.io.gmshio is deprecated, use dolfinx.io.gmsh.")
