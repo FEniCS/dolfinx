882cf231914be0581ec6e4e061e020685c24109c
Jørgen S. Dokken <dokken@simula.no>
2025-09-15 08:39:40 +0000

    Minor fix in test

--- python/test/unit/mesh/test_manifold_point_search.py
+++ python/test/unit/mesh/test_manifold_point_search.py
@@ -17,7 +17,7 @@ def test_manifold_point_search():
     vertices = np.array([[0.0, 0.0, 1.0], [1.0, 1.0, 1.0], [1.0, 0.0, 0.0], [0.0, 1.0, 0.0]])
     cells = np.array([[0, 1, 2], [0, 1, 3]], dtype=np.int64)
     domain = ufl.Mesh(element("Lagrange", "triangle", 1, shape=(2,)))
-    mesh = create_mesh(MPI.COMM_WORLD, cells, vertices, domain)
+    mesh = create_mesh(MPI.COMM_WORLD, cells=cells, x=vertices, e=domain)
     bb = bb_tree(mesh, mesh.topology.dim, padding=0.0)
 
     # Find cell colliding with point
