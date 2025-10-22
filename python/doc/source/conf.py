0055beee3a4c977faea8b761f59074bee5d4a459
Jack S. Hale <mail@jackhale.co.uk>
2025-10-22 11:48:02 +0200

    Backport documentation fixes.

--- python/doc/source/conf.py
+++ python/doc/source/conf.py
@@ -6,7 +6,14 @@ import datetime
 import os
 import sys
 
+import mpi4py
+
+import numpy as np
+
+import basix
 import dolfinx
+import ffcx
+import ufl
 
 sys.path.insert(0, os.path.abspath("."))
 import jupytext_process  # isort:skip
@@ -152,14 +159,47 @@ autoclass_content = "both"
 
 codeautolink_concat_default = True
 
+dolfinx_version = "main" if "dev0" in dolfinx.__version__ else "v" + dolfinx.__version__
+basix_version = "main" if "dev0" in basix.__version__ else "v" + basix.__version__
+ffcx_version = "main" if "dev0" in ffcx.__version__ else "v" + ffcx.__version__
+ufl_version = "main" if "dev0" in ufl.__version__ else ufl.__version__
+numpy_version = "".join(np.__version__.split(".")[:-1]) # major.minor
+
+# Note that as of late 2025 pyvista and petsc4py only have docs for the latest
+# releases.
 intersphinx_mapping = {
-    "basix": ("https://docs.fenicsproject.org/basix/main/python/", None),
-    "ffcx": ("https://docs.fenicsproject.org/ffcx/main/", None),
-    "ufl": ("https://docs.fenicsproject.org/ufl/main/", None),
-    "mpi4py": ("https://mpi4py.readthedocs.io/en/stable", None),
-    "petsc4py": ("https://petsc.org/release/petsc4py", None),
-    "numpy": ("https://numpy.org/doc/stable/", None),
-    "pyvista": ("https://docs.pyvista.org/", None),
+    "petsc4py": (
+        "https://petsc.org/release/petsc4py",
+        None,
+    ),
+    "numpy": (
+        f"https://numpy.org/doc/{numpy_version}",
+        None,
+    ),
+    "pyvista": (
+        "https://docs.pyvista.org",
+        None,
+    ),
+    "mpi4py": (
+        f"https://mpi4py.readthedocs.io/en/{mpi4py.__version__}",
+        None,
+    ),
+    "dolfinx": (
+        f"https://docs.fenicsproject.org/dolfinx/{dolfinx_version}/python",
+        None,
+    ),
+    "basix": (
+        f"https://docs.fenicsproject.org/basix/{basix_version}/python",
+        None,
+    ),
+    "ffcx": (
+        f"https://docs.fenicsproject.org/ffcx/{ffcx_version}",
+        None,
+    ),
+    "ufl": (
+        f"https://docs.fenicsproject.org/ufl/{ufl_version}",
+        None,
+    ),
 }
 
 napoleon_google_docstring = True
