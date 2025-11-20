c5727978340e5ff84f0cad754b5208193c8c1bb1
Jack S. Hale <mail@jackhale.co.uk>
2025-11-20 10:40:23 +0100

    Does this trigger a runtime assertion anywhere?

--- cpp/test/common/sort.cpp
+++ cpp/test/common/sort.cpp
@@ -29,8 +29,7 @@ TEMPLATE_TEST_CASE("Test radix sort", "[vector][template]", std::int16_t,
   vec.reserve(vec_size);
 
   // Generate a vector of ints with a Uniform Int distribution
-  constexpr TestType lower_bound = std::is_signed_v<TestType> ? -10000 : 0;
-  std::uniform_int_distribution<TestType> distribution(lower_bound, 10000);
+  std::uniform_int_distribution<TestType> distribution(-10000, 10000);
   std::mt19937 engine;
   auto generator = std::bind(distribution, engine);
   std::generate_n(std::back_inserter(vec), vec_size, generator);
