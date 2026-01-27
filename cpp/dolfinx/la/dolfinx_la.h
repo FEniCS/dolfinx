75bf3d196a7ba83cbd25a2f35555b9ad29998395
Jack S. Hale <mail@jackhale.co.uk>
2026-01-27 16:50:16 +0100

    Remove ifdefs

--- cpp/dolfinx/la/dolfinx_la.h
+++ cpp/dolfinx/la/dolfinx_la.h
@@ -10,11 +10,7 @@ namespace dolfinx::la
 // DOLFINx la interface
 
 #include <dolfinx/la/SparsityPattern.h>
-#ifdef HAS_PETSC
 #include <dolfinx/la/petsc.h>
-#endif
 #include <dolfinx/la/slepc.h>
-#ifdef HAS_SUPERLU_DIST
 #include <dolfinx/la/superlu.h>
-#endif
 #include <dolfinx/la/utils.h>
