db9e2cfdf9b476e0f05ce5cf01a318e21528b8cf
Chris Richardson <chris@bpi.cam.ac.uk>
2026-01-27 10:37:58 +0000

    Add cmake finder

---
+++ cpp/cmake/modules/FindSuperLUDist.cmake
@@ -0,0 +1,25 @@
+# - Try to find SuperLU-dist
+# Once done this will define
+#  SuperLUDist_FOUND - System has Mumps
+#  SuperLUDist_INCLUDE_DIRS - The Mumps include directories
+#  SuperLUDist_LIBRARY_DIRS - The library directories needed to use Mumps
+#  SuperLUDist_LIBRARIES    - The libraries needed to use Mumps
+
+find_path(SuperLUDist_INCLUDE_DIR NAMES superlu_dist_config.h
+     PATHS "${PETSC_INCLUDE_DIRS}/"
+           "/usr/include/"
+          )
+
+find_library(SuperLUDist_LIBRARY
+              superlu_dist
+              PATHS "${PETSC_LIBRARY_DIRS}}/lib/"
+                    "/usr/lib")
+
+
+include(FindPackageHandleStandardArgs)
+# handle the QUIETLY and REQUIRED arguments and set FOUND to TRUE
+# if all listed variables are TRUE
+find_package_handle_standard_args(SuperLUDist  DEFAULT_MSG
+                                  SuperLUDist_LIBRARY SuperLUDist_INCLUDE_DIR)
+
+mark_as_advanced(SuperLUDist_INCLUDE_DIR SuperLUDist_LIBRARY)
