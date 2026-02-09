276fa119ae84e2f58b599620d50a87f6791bdcba
Jack S. Hale <mail@jackhale.co.uk>
2026-02-09 12:06:00 +0100

    Finish tests.

--- python/test/unit/la/test_superlu_dist.py
+++ python/test/unit/la/test_superlu_dist.py
@@ -23,7 +23,6 @@ from dolfinx.fem import (
     locate_dofs_topological,
 )
 from dolfinx.la import InsertMode
-from dolfinx.la.superlu_dist import superlu_dist_matrix
 from dolfinx.mesh import create_unit_square, exterior_facet_indices
 from ufl import SpatialCoordinate, TestFunction, TrialFunction, div, dx, grad, inner
 
@@ -31,8 +30,12 @@ from ufl import SpatialCoordinate, TestFunction, TrialFunction, div, dx, grad, i
 @pytest.mark.parametrize("dtype", [np.float32, np.float64, np.complex128])
 @pytest.mark.skipif(not dolfinx.has_superlu_dist, reason="No SuperLU_DIST")
 def test_superlu_solver(dtype):
-    """Manufactured Poisson problem with exact solution u = x[1]**3."""
-    from dolfinx.la.superlu_dist import superlu_dist_solver
+    """Manufactured Poisson and screened problem with exact solution u = x[1]**3.
+
+    Test includes various checks that SuperLU_DIST Factor option works correctly
+    for efficient re-use of permutations and factors.
+    """
+    from dolfinx.la.superlu_dist import superlu_dist_matrix, superlu_dist_solver
 
     mesh_dtype = dtype().real.dtype
     mesh = create_unit_square(MPI.COMM_WORLD, 5, 5, dtype=mesh_dtype)
@@ -70,67 +73,69 @@ def test_superlu_solver(dtype):
     A = assemble_matrix(a, bcs=[bc])
     A.scatter_reverse()
 
+    def check_error(u_ex, uh):
+        M = (u_ex(x) - uh) ** 2 * dx
+        M = form(M, dtype=dtype)
+        error = mesh.comm.allreduce(assemble_scalar(M), op=MPI.SUM)
+        eps = np.sqrt(np.finfo(dtype).eps)
+        assert np.isclose(error, 0.0, atol=eps)
+
+    def solve_and_check(solver, b):
+        uh = Function(V, dtype=dtype)
+        error_code = solver.solve(b, uh.x)
+        assert error_code == 0
+        uh.x.scatter_forward()
+        return uh
+
     # Standard solve
     A_superlu = superlu_dist_matrix(A)
-    solver = superlu_dist_solver(A_superlu)
-    solver.set_option("SymmetricMode", "YES")
+    solver_1 = superlu_dist_solver(A_superlu)
+    solver_1.set_option("SymmetricMode", "YES")
+    uh_1 = solve_and_check(solver_1, b)
+    check_error(u_ex, uh_1)
 
-    uh = Function(V, dtype=dtype)
-    error_code = solver.solve(b, uh.x)
-    assert error_code == 0
-    uh.x.scatter_forward()
-
-    M = (u_ex(x) - uh) ** 2 * dx
-    M = form(M, dtype=dtype)
-    error = mesh.comm.allreduce(assemble_scalar(M), op=MPI.SUM)
-    eps = np.sqrt(np.finfo(dtype).eps)
-    assert np.isclose(error, 0.0, atol=eps)
-
-    # Check second solve using same factors as previous solve
-    solver.set_option("Fact", "FACTORED")
-
-    uh = Function(V, dtype=dtype)
-    error_code = solver.solve(b, uh.x)
-    assert error_code == 0
-    uh.x.scatter_forward()
-
-    M = (u_ex(x) - uh) ** 2 * dx
-    M = form(M, dtype=dtype)
-    error = mesh.comm.allreduce(assemble_scalar(M), op=MPI.SUM)
-    eps = np.sqrt(np.finfo(dtype).eps)
-    assert np.isclose(error, 0.0, atol=eps)
+    # Check second solve re-using permutations from previous solve
+    solver_1.set_option("Fact", "FACTORED")
+    solve_and_check(solver_1, b)
+    uh_2 = solve_and_check(solver_1, b)
+    check_error(u_ex, uh_2)
 
     # Check can do same solve with MatrixCSR A again.
     A_superlu_2 = superlu_dist_matrix(A)
-    solver = superlu_dist_solver(A_superlu_2)
-    solver.set_option("SymmetricMode", "YES")
+    solver_2 = superlu_dist_solver(A_superlu_2)
+    solver_2.set_option("SymmetricMode", "YES")
+    uh_3 = solve_and_check(solver_2, b)
+    check_error(u_ex, uh_3)
 
-    uh = Function(V, dtype=dtype)
-    error_code = solver.solve(b, uh.x)
-    assert error_code == 0
-    uh.x.scatter_forward()
+    # Assemble a new operator and re-use row permutation from solve with old
+    # operator.
+    gamma = 1e-1
+    a_2 = (inner(grad(u), grad(v)) + gamma * inner(u, v)) * dx
+    a_2 = form(a_2, dtype=dtype)
 
-    M = (u_ex(x) - uh) ** 2 * dx
-    M = form(M, dtype=dtype)
-    error = mesh.comm.allreduce(assemble_scalar(M), op=MPI.SUM)
-    eps = np.sqrt(np.finfo(dtype).eps)
-    assert np.isclose(error, 0.0, atol=eps)
+    # Exact solution
+    def u_ex_2(x):
+        return x[1] ** 2
 
-    # Advanced usage where we solved with A_1, replace the solver operator with
-    # A_2 (with same sparsity pattern and similar values as A_1), and then
-    # solve with A_2 reusing column permutations.
-    # TODO: Change this so A_superlu_3 is different to A_superlu_2.
-    A_superlu_3 = superlu_dist_matrix(A)
-    solver.set_A(A_superlu_3)
-    solver.set_option("Fact", "SamePattern")
+    f_2 = -div(grad(u_ex_2(x))) + gamma * u_ex_2(x)
+    L_2 = inner(f_2, v) * dx
+    L_2 = form(L_2, dtype=dtype)
 
-    uh = Function(V, dtype=dtype)
-    error_code = solver.solve(b, uh.x)
-    assert error_code == 0
-    uh.x.scatter_forward()
+    # Create Dirichlet boundary condition
+    u_bc_2 = Function(V, dtype=dtype)
+    u_bc_2.interpolate(u_ex_2)
+    bc_2 = dirichletbc(u_bc_2, bdofs)
 
-    M = (u_ex(x) - uh) ** 2 * dx
-    M = form(M, dtype=dtype)
-    error = mesh.comm.allreduce(assemble_scalar(M), op=MPI.SUM)
-    eps = np.sqrt(np.finfo(dtype).eps)
-    assert np.isclose(error, 0.0, atol=eps)
+    b_2 = assemble_vector(L_2)
+    apply_lifting(b_2.array, [a_2], bcs=[[bc_2]])
+    b_2.scatter_reverse(InsertMode.add)
+    bc_2.set(b_2.array)
+
+    A_3 = assemble_matrix(a_2, bcs=[bc])
+    A_3.scatter_reverse()
+
+    A_superlu_3 = superlu_dist_matrix(A_3)
+    solver_2.set_A(A_superlu_3)
+    solver_2.set_option("Fact", "SamePattern")
+    uh_2 = solve_and_check(solver_2, b_2)
+    check_error(u_ex_2, uh_2)
