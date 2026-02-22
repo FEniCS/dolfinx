2b7389e32d3c1112a29a2ffdc9fb682427bfca39
jorgensd <dokken92@gmail.com>
2026-02-22 13:41:30 +0100

    Fix import order

--- cpp/doc/source/conf.py
+++ cpp/doc/source/conf.py
@@ -10,13 +10,14 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 
+import datetime
 import os
 import sys
-import dolfinx
+
 import basix
-import ufl
-import datetime
+import dolfinx
 import ffcx
+import ufl
 
 sys.path.insert(0, os.path.abspath("."))
 
