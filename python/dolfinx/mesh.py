7abf419e160b3227612d3b537290c71edf1282d3
Garth N. Wells <gnw20@cam.ac.uk>
2026-03-01 21:54:36 +0000

    Updates

--- python/dolfinx/mesh.py
+++ python/dolfinx/mesh.py
@@ -1056,7 +1056,7 @@ def create_rectangle(
         A mesh of a rectangle.
     """
     if partitioner is None and comm.size > 1:
-        partitioner = _cpp.mesh.create_cell_partitioner(ghost_mode, 2, 0)
+        partitioner = create_cell_partitioner(ghost_mode, 2)
     domain = ufl.Mesh(
         basix.ufl.element(
             "Lagrange",
@@ -1148,7 +1148,7 @@ def create_box(
         A mesh of a box domain.
     """
     if partitioner is None and comm.size > 1:
-        partitioner = _cpp.mesh.create_cell_partitioner(ghost_mode, 2, num_threads)
+        partitioner = create_cell_partitioner(ghost_mode, 2, num_threads)
     domain = ufl.Mesh(
         basix.ufl.element(
             "Lagrange",
