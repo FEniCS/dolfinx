4487a75447f3c73f570ad4d6f370442fb1ede067
jorgensd <dokken92@gmail.com>
2026-04-01 14:07:14 +0000

    Fix import for docs

--- python/dolfinx/fem/function.py
+++ python/dolfinx/fem/function.py
@@ -26,7 +26,8 @@ from dolfinx.geometry import PointOwnershipData
 if typing.TYPE_CHECKING:
     from mpi4py import MPI as _MPI
 
-    from dolfinx.mesh import EntityMap, Mesh
+    from dolfinx.mesh import EntityMap as _EntityMap
+    from dolfinx.mesh import Mesh
 
 
 class Constant(ufl.Constant):
@@ -117,7 +118,7 @@ class Expression:
         form_compiler_options: dict | None = None,
         jit_options: dict | None = None,
         dtype: npt.DTypeLike | None = None,
-        entity_maps: list[EntityMap] | None = None,
+        entity_maps: list[_EntityMap] | None = None,
     ):
         """Create an Expression.
 
