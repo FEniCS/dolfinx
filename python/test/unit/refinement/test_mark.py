c4a2bdb6e8292f94782f08cadfe912fbb1b1e63f
schnellerhase <56360279+schnellerhase@users.noreply.github.com>
2026-04-27 22:38:28 +0200

    Add equidistribution marking (py)

--- python/test/unit/refinement/test_mark.py
+++ python/test/unit/refinement/test_mark.py
@@ -31,3 +31,26 @@ def test_mark_maximum(theta: float, dtype: np.dtype) -> None:
     msh.topology.create_entities(1)
     marked_edges = mesh.compute_incident_entities(msh.topology, marked_cells, tdim, 1)
     mesh.refine(msh, marked_edges)
+
+
+@pytest.mark.parametrize("theta", [0.2, 0.4, 0.6, 0.8])
+@pytest.mark.parametrize("dtype", [np.float32, np.float64])
+def test_mark_equidistribution(theta: float, dtype: np.dtype) -> None:
+    msh = mesh.create_unit_square(comm := MPI.COMM_WORLD, n := 10, n, dtype=dtype)
+
+    tdim = msh.topology.dim
+    cell_count = (cell_im := msh.topology.index_map(tdim)).size_local + cell_im.num_ghosts
+    marker = np.random.default_rng(0).random(cell_count)
+
+    marked_cells = mesh.mark_equidistribution(marker, theta, comm)
+
+    norm = np.sqrt(comm.allreduce(np.sum(marker**2), MPI.SUM))
+    count = comm.allreduce(marker.size)
+    assert np.allclose(
+        marked_cells,
+        np.argwhere(marker > theta * norm / np.sqrt(count)).flatten(),
+    )
+
+    msh.topology.create_entities(1)
+    marked_edges = mesh.compute_incident_entities(msh.topology, marked_cells, tdim, 1)
+    mesh.refine(msh, marked_edges)
