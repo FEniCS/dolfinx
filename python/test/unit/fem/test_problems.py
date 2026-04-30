e9ccb8df9f61045359c4119109cdf2e3a755228e
Jack S. Hale <mail@jackhale.co.uk>
2026-04-30 17:58:41 +0200

    Add file

---
+++ python/test/unit/fem/test_problems.py
@@ -0,0 +1,67 @@
+# Copyright (C) 2026 Jack S. Hale, Chris Richardson
+#
+# This file is part of DOLFINx (https://www.fenicsproject.org)
+#
+# SPDX-License-Identifier:    LGPL-3.0-or-later
+"""Unit tests for native variational problem classes."""
+
+from mpi4py import MPI
+
+import numpy as np
+import pytest
+
+import dolfinx
+from dolfinx.fem import (
+    Function,
+    assemble_scalar,
+    dirichletbc,
+    form,
+    functionspace,
+    locate_dofs_topological,
+)
+from dolfinx.mesh import create_unit_square, exterior_facet_indices
+from ufl import SpatialCoordinate, TestFunction, TrialFunction, div, dx, grad, inner
+
+
+@pytest.mark.parametrize("dtype", [np.float32, np.float64, np.complex128])
+@pytest.mark.skipif(not dolfinx.has_superlu_dist, reason="No SuperLU_DIST")
+def test_superlu_solver(dtype):
+    """Manufactured Poisson and screened problem with exact solution u = x[1]**3."""
+    from dolfinx.fem.problems import LinearProblem
+
+    mesh_dtype = dtype().real.dtype
+    mesh = create_unit_square(MPI.COMM_WORLD, 5, 5, dtype=mesh_dtype)
+    V = functionspace(mesh, ("Lagrange", 4))
+    u, v = TrialFunction(V), TestFunction(V)
+
+    a = inner(grad(u), grad(v)) * dx
+
+    # Exact solution
+    def u_ex(x):
+        return x[1] ** 3
+
+    x = SpatialCoordinate(mesh)
+    f = -div(grad(u_ex(x)))
+    L = inner(f, v) * dx
+
+    u_bc = Function(V, dtype=dtype)
+    u_bc.interpolate(u_ex)
+
+    # Create Dirichlet boundary condition
+    facetdim = mesh.topology.dim - 1
+    mesh.topology.create_connectivity(facetdim, mesh.topology.dim)
+    bndry_facets = exterior_facet_indices(mesh.topology)
+    bdofs = locate_dofs_topological(V, facetdim, bndry_facets)
+    bc = dirichletbc(u_bc, bdofs)
+
+    def check_error(u_ex, uh):
+        M = (u_ex(x) - uh) ** 2 * dx
+        M = form(M, dtype=dtype)
+        error = mesh.comm.allreduce(assemble_scalar(M), op=MPI.SUM)
+        eps = np.sqrt(np.finfo(dtype).eps)
+        assert np.isclose(error, 0.0, atol=eps)
+
+    problem = LinearProblem(a, L, [bc], dtype=dtype)
+    uh = problem.solve()
+
+    check_error(u_ex, uh)
