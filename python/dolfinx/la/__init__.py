d9513e12fb2b4468164fe696e1d39e58f27d462d
Paul T. Kühner <56360279+schnellerhase@users.noreply.github.com>
2026-05-04 14:48:51 +0200

    Add typing generics (#4131)
    * Add typing generics to vector
    
    * Add typing generics to matrix
    
    * Add typing generics to superludist{matrix, solver}
    
    * Add typing generics to CoordinateElement
    
    * Add typing generics to finiteelement
    
    * Add typing generics to constant
    
    * Add typing generics dirichletbc
    
    * Add typing generics Expression
    
    * Add typing generics Function
    
    * Fix: demo_lagrange
    
    * Add typing generics Geometry
    
    * Add typing generics Mesh
    
    * Add typing generics PointOwnershipData
    
    * Add typing generics BoundingBoxTree
    
    * Add typing generics FunctionSpace
    
    * Add typing generics Form
    
    * Centralise common names
    
    * Document SuperLU_DIST type limitations
    
    Add a comment about SuperLU_DIST type support.
    
    * Fix comment
    
    ---------
    
    Co-authored-by: Jack S. Hale <mail@jackhale.co.uk>

--- python/dolfinx/la/__init__.py
+++ python/dolfinx/la/__init__.py
@@ -5,6 +5,8 @@
 # SPDX-License-Identifier:    LGPL-3.0-or-later
 """Linear algebra functionality."""
 
+from typing import Generic, TypeVar
+
 import numpy as np
 import numpy.typing as npt
 
@@ -12,6 +14,7 @@ import dolfinx
 from dolfinx import cpp as _cpp
 from dolfinx.cpp.common import IndexMap
 from dolfinx.cpp.la import BlockMode, InsertMode, Norm
+from dolfinx.typing import Scalar
 
 __all__ = [
     "InsertMode",
@@ -26,7 +29,10 @@ __all__ = [
 ]
 
 
-class Vector:
+_T = TypeVar("_T", np.float32, np.float64, np.complex64, np.complex128, np.int8, np.int32, np.int64)
+
+
+class Vector(Generic[_T]):
     """Distributed vector object."""
 
     _cpp_object: (
@@ -79,7 +85,7 @@ class Vector:
         return self._cpp_object.bs
 
     @property
-    def array(self) -> np.ndarray:
+    def array(self) -> npt.NDArray[_T]:
         """Local representation of the vector."""
         return self._cpp_object.array
 
@@ -117,7 +123,7 @@ class Vector:
         self._cpp_object.scatter_reverse(mode)
 
 
-class MatrixCSR:
+class MatrixCSR(Generic[Scalar]):
     """Distributed compressed sparse row matrix."""
 
     _cpp_object: (
@@ -155,7 +161,7 @@ class MatrixCSR:
         """
         return self._cpp_object.index_map(i)
 
-    def mult(self, x: Vector, y: Vector, transpose: bool = False) -> None:
+    def mult(self, x: Vector[Scalar], y: Vector[Scalar], transpose: bool = False) -> None:
         """Compute ``y += Ax`` or ``y += A^T x``.
 
         Args:
@@ -200,7 +206,7 @@ class MatrixCSR:
 
     def add(
         self,
-        x: npt.NDArray[np.floating],
+        x: npt.NDArray[Scalar],
         rows: npt.NDArray[np.int32],
         cols: npt.NDArray[np.int32],
         bs: int = 1,
@@ -210,7 +216,7 @@ class MatrixCSR:
 
     def set(
         self,
-        x: npt.NDArray[np.floating],
+        x: npt.NDArray[Scalar],
         rows: npt.NDArray[np.int32],
         cols: npt.NDArray[np.int32],
         bs: int = 1,
@@ -218,7 +224,7 @@ class MatrixCSR:
         """Set a block of values in the matrix."""
         self._cpp_object.set(x, rows, cols, bs)
 
-    def set_value(self, x: np.floating) -> None:
+    def set_value(self, x: Scalar) -> None:
         """Set all non-zero entries to a value.
 
         Args:
@@ -239,7 +245,7 @@ class MatrixCSR:
         return self._cpp_object.squared_norm()
 
     @property
-    def data(self) -> npt.NDArray[np.floating]:
+    def data(self) -> npt.NDArray[Scalar]:
         """Underlying matrix entry data."""
         return self._cpp_object.data
 
@@ -253,7 +259,7 @@ class MatrixCSR:
         """Local row pointers."""
         return self._cpp_object.indptr
 
-    def to_dense(self) -> npt.NDArray[np.floating]:
+    def to_dense(self) -> npt.NDArray[Scalar]:
         """Copy to a dense 2D array.
 
         Note:
@@ -364,17 +370,17 @@ def vector(map, bs=1, dtype: npt.DTypeLike = np.float64) -> Vector:
     return Vector(vtype(map, bs))
 
 
-def orthonormalize(basis: list[Vector]):
+def orthonormalize(basis: list[Vector[_T]]) -> None:
     """Orthogonalise set of vectors in-place."""
     _cpp.la.orthonormalize([x._cpp_object for x in basis])
 
 
-def is_orthonormal(basis: list[Vector], eps: float = 1.0e-12) -> bool:
+def is_orthonormal(basis: list[Vector[_T]], eps: float = 1.0e-12) -> bool:
     """Check that list of vectors are orthonormal."""
     return _cpp.la.is_orthonormal([x._cpp_object for x in basis], eps)
 
 
-def norm(x: Vector, type: _cpp.la.Norm = _cpp.la.Norm.l2) -> np.floating:
+def norm(x: Vector[_T], type: _cpp.la.Norm = _cpp.la.Norm.l2) -> np.floating:
     """Compute a norm of the vector.
 
     Args:
