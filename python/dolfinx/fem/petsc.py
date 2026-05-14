2c1a592f190b319cc998f67215302c764a82aaa2
Massimiliano Leoni <leoni.massimiliano1@gmail.com>
2026-05-14 14:48:17 +0200

    Doc typos (#4209)

--- python/dolfinx/fem/petsc.py
+++ python/dolfinx/fem/petsc.py
@@ -364,7 +364,7 @@ def assemble_matrix(
 
        a. If ``kind`` is a ``PETSc.Mat.Type`` (other than
           ``PETSc.Mat.Type.NEST``) or is ``None``, the matrix type is
-          ``kind`` of the default type (if ``kind`` is ``None``).
+          ``kind`` or the default type (if ``kind`` is ``None``).
        #. If ``kind`` is ``PETSc.Mat.Type.NEST`` or a rectangular array
           of PETSc matrix types, the returned matrix has type
           ``PETSc.Mat.Type.NEST``.
@@ -372,7 +372,7 @@ def assemble_matrix(
     Rows/columns that are constrained by a Dirichlet boundary condition
     are zeroed, with the diagonal to set to ``diag``.
 
-    Constant and coefficient data that appear in the forms(s) can be
+    Constant and coefficient data that appear in the form(s) can be
     packed outside of this function to avoid re-packing by this
     function. The functions :func:`dolfinx.fem.pack_constants` and
     :func:`dolfinx.fem.pack_coefficients` can be used to 'pre-pack' the
@@ -388,8 +388,8 @@ def assemble_matrix(
         diag: Value to set on the matrix diagonal for Dirichlet
             boundary condition constrained degrees-of-freedom belonging
             to the same trial and test space.
-        constants: Constants appearing the in the form.
-        coeffs: Coefficients appearing the in the form.
+        constants: Constants appearing in the form.
+        coeffs: Coefficients appearing in the form.
         kind: PETSc matrix type (``MatType``).
 
     Returns:
