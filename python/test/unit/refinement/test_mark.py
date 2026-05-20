52b75786be698cf82989e556c740bcfaf85fbe08
Jack S. Hale <mail@jackhale.co.uk>
2026-05-20 13:53:17 +0200

    Needs checking

--- python/test/unit/refinement/test_mark.py
+++ python/test/unit/refinement/test_mark.py
@@ -9,7 +9,8 @@ from mpi4py import MPI
 import numpy as np
 import pytest
 
-from dolfinx import mesh
+import ufl
+from dolfinx import default_real_type, fem, la, mesh
 
 
 @pytest.mark.parametrize("theta", [0.2, 0.4, 0.6, 0.8])
@@ -60,3 +61,61 @@ def test_mark_equidistribution(theta: float, dtype: np.dtype) -> None:
     mesh.refine(msh, marked_edges)
 
     assert np.all(marked_cells == mesh.mark_equidistribution_squared(comm, indicators**2, theta))
+
+
+@pytest.mark.parametrize("theta", [0.2, 0.4, 0.6, 0.8])
+def test_mark_poisson_residual_estimator(theta: float, cg_solver) -> None:
+    r"""Solve a Poisson problem, compute the standard residual a posteriori
+    error estimator, and use it to drive equidistribution marking and refinement.
+    """
+    comm = MPI.COMM_WORLD
+    dtype = default_real_type
+    msh = mesh.create_unit_square(comm, 8, 8, dtype=default_real_type)
+    tdim = msh.topology.dim
+
+    # Solve Poisson with Gaussian bump.
+    V = fem.functionspace(msh, ("Lagrange", 1))
+    u, v = ufl.TrialFunction(V), ufl.TestFunction(V)
+    x = ufl.SpatialCoordinate(msh)
+    f = ufl.exp(-((x[0] - 0.25) ** 2 + (x[1] - 0.25) ** 2) / 0.02)
+    a = fem.form(ufl.inner(ufl.grad(u), ufl.grad(v)) * ufl.dx, dtype=dtype)
+    L = fem.form(ufl.inner(f, v) * ufl.dx, dtype=dtype)
+
+    fdim = tdim - 1
+    msh.topology.create_connectivity(fdim, tdim)
+    bdofs = fem.locate_dofs_topological(V, fdim, mesh.exterior_facet_indices(msh.topology))
+    bc = fem.dirichletbc(dtype(0), bdofs, V)
+
+    A = fem.assemble_matrix(a, bcs=[bc])
+    A.scatter_reverse()
+    b = fem.assemble_vector(L)
+    fem.apply_lifting(b.array, [a], bcs=[[bc]])
+    b.scatter_reverse(la.InsertMode.add)
+    bc.set(b.array)
+
+    uh = fem.Function(V, dtype=dtype)
+    cg_solver(msh.comm, A, b, uh.x)
+    uh.x.scatter_forward()
+
+    # Assemble squared cell indicators eta_K^2 into a DG0 vector.
+    V0 = fem.functionspace(msh, ("DG", 0))
+    eta_T = ufl.TestFunction(V0)
+    h = ufl.CellDiameter(msh)
+    n = ufl.FacetNormal(msh)
+    j = ufl.jump(ufl.grad(uh), n)
+    eta_form = fem.form(
+        h**2 * ufl.inner(f, f) * eta_T * ufl.dx
+        + ufl.avg(h) * ufl.inner(j, j) * ufl.avg(eta_T) * ufl.dS,
+        dtype=dtype,
+    )
+    eta_vec = fem.assemble_vector(eta_form)
+    eta_vec.scatter_reverse(la.InsertMode.add)
+
+    indicators_squared = eta_vec.array[: V0.dofmap.index_map.size_local]
+    assert np.all(indicators_squared >= 0)
+
+    marked_cells = mesh.mark_equidistribution_squared(comm, indicators_squared, theta)
+
+    msh.topology.create_entities(1)
+    marked_edges = mesh.compute_incident_entities(msh.topology, marked_cells, tdim, 1)
+    mesh.refine(msh, marked_edges)
