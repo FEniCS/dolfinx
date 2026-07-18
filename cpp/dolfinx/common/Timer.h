c9b113a684a2283eb40f63bd6575dfaab351e3fc
Garth N. Wells <gnw20@cam.ac.uk>
2026-07-18 15:37:18 +0100

    Simplify

--- cpp/dolfinx/common/Timer.h
+++ cpp/dolfinx/common/Timer.h
@@ -45,11 +45,8 @@ public:
   /// destructor is called.
   ///
   /// @param[in] task Name used to registered the elapsed time in the
-  /// logger. If no name is set, the elapsed time is not registered in
-  /// the logger.
-  Timer(std::optional<std::string> task = std::nullopt) : _task(std::move(task))
-  {
-  }
+  /// logger.
+  Timer(std::string task) : _task(std::move(task)) {}
 
   /// If timer is still running, it is stopped. Elapsed time is
   /// registered in the logger.
