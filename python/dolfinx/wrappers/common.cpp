5a4bce77c79060d73c2ea2f26af761d6de935c40
Garth N. Wells <gnw20@cam.ac.uk>
2026-07-18 18:00:44 +0100

    Another fix

--- python/dolfinx/wrappers/common.cpp
+++ python/dolfinx/wrappers/common.cpp
@@ -173,7 +173,7 @@ void common(nb::module_& m)
   // dolfinx::common::Timer
   nb::class_<dolfinx::common::Timer<std::chrono::high_resolution_clock>>(
       m, "Timer", "Timer class")
-      .def(nb::init<std::string>(), nb::arg("task"),
+      .def(nb::init<std::optional<std::string>>(), nb::arg("task").none(),
            "Create and start timer. Elapsed time is optionally registered in "
            "the logger when the Timer destructor is called.")
       .def("start",
