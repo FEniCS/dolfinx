e9ecb73bacd50b904787021f9942e95aaca42368
schnellerhase <56360279+schnellerhase@users.noreply.github.com>
2026-08-01 18:51:23 +0200

    Use PETSc build in rigid body mode null space construction

--- python/demo/demo_elasticity.py
+++ python/demo/demo_elasticity.py
@@ -61,39 +61,13 @@ dtype = PETSc.ScalarType
 # modes.
 
 
-def build_nullspace(V: FunctionSpace):
+def build_nullspace(V: FunctionSpace) -> PETSc.NullSpace:
     """Build PETSc nullspace for 3D elasticity."""
-    # Create vectors that will span the nullspace
-    bs = V.dofmap.index_map_bs
-    length0 = V.dofmap.index_map.size_local
-    basis = [la.vector(V.dofmap.index_map, bs=bs, dtype=dtype) for i in range(6)]
-    b = [b.array for b in basis]
-
-    # Get dof indices for each subspace (x, y and z dofs)
-    dofs = [V.sub(i).dofmap.list.flatten() for i in range(3)]
-
-    # Set the three translational rigid body modes
-    for i in range(3):
-        b[i][dofs[i]] = 1.0
-
-    # Set the three rotational rigid body modes
-    x = V.tabulate_dof_coordinates()
-    dofs_block = V.dofmap.list.flatten()
-    x0, x1, x2 = x[dofs_block, 0], x[dofs_block, 1], x[dofs_block, 2]
-    b[3][dofs[0]] = -x1
-    b[3][dofs[1]] = x0
-    b[4][dofs[0]] = x2
-    b[4][dofs[2]] = -x0
-    b[5][dofs[2]] = x1
-    b[5][dofs[1]] = -x2
-
-    la.orthonormalize(basis)
-
-    basis_petsc = [
-        PETSc.Vec().createWithArray(x[: bs * length0], bsize=3, comm=V.mesh.comm) for x in b
-    ]
-    return PETSc.NullSpace().create(vectors=basis_petsc)
-
+    x = V.tabulate_dof_coordinates().flatten()
+    gdim = V.mesh.geometry.dim
+    comm = V.mesh.comm
+    coords = PETSc.Vec().createWithArray(x, comm=comm, bsize=gdim)
+    return PETSc.NullSpace().createRigidBody(coords)
 
 # ## Problem definition
 
