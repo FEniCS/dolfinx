0eedc473dc905cb223c48cd838865fd7116f009c
Garth N. Wells <gnw20@cam.ac.uk>
2026-08-02 22:21:45 +0100

    Updates

--- cpp/dolfinx/fem/Function.h
+++ cpp/dolfinx/fem/Function.h
@@ -52,21 +52,6 @@ public:
   /// Geometry type of the Mesh that the Function is defined on.
   using geometry_type = U;
 
-  /// @brief Create function on given function space.
-  /// @param[in] V The function space
-  explicit Function(std::shared_ptr<const FunctionSpace<geometry_type>> V)
-      : _function_space(V),
-        _x(std::make_shared<la::Vector<value_type>>(V->dofmap()->index_map,
-                                                    V->dofmap()->index_map_bs(),
-                                                    V->scatterer()))
-  {
-    if (!V->component().empty())
-    {
-      throw std::runtime_error("Cannot create Function from subspace. Consider "
-                               "collapsing the function space");
-    }
-  }
-
   /// @brief Create function on given function space with a given
   /// vector.
   ///
@@ -86,6 +71,21 @@ public:
     assert(V->dofmap());
     assert(V->dofmap()->index_map->size_global() * V->dofmap()->index_map_bs()
            <= _x->bs() * _x->index_map()->size_global());
+  }
+
+  /// @brief Create function on given function space.
+  ///
+  /// @param[in] V The function space.
+  explicit Function(std::shared_ptr<const FunctionSpace<geometry_type>> V)
+      : Function(V, std::make_shared<la::Vector<value_type>>(
+                        V->dofmap()->index_map, V->dofmap()->index_map_bs(),
+                        V->scatterer()))
+  {
+    if (!V->component().empty())
+    {
+      throw std::runtime_error("Cannot create Function from subspace. Consider "
+                               "collapsing the function space");
+    }
   }
 
   // Copy constructor
