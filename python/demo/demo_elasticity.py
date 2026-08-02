bca97d142c3f36c8095d7f4cee0011048c701db5
schnellerhase <56360279+schnellerhase@users.noreply.github.com>
2026-08-02 12:41:17 +0200

    Remove name collision

--- python/demo/demo_elasticity.py
+++ python/demo/demo_elasticity.py
@@ -144,9 +144,9 @@ bc.set(b.array_w)
 #
 # Create the near-nullspace and attach it to the PETSc matrix:
 
-x = V.tabulate_dof_coordinates()[: V.dofmap.index_map.size_local, :].ravel()
-coords = PETSc.Vec().createWithArray(x, bsize=gdim, comm=comm)
-ns = PETSc.NullSpace().createRigidBody(coords)
+coords = V.tabulate_dof_coordinates()[: V.dofmap.index_map.size_local, :].ravel()
+coords_vec = PETSc.Vec().createWithArray(x, bsize=gdim, comm=comm)
+ns = PETSc.NullSpace().createRigidBody(coords_vec)
 A.setNearNullSpace(ns)
 A.setOption(PETSc.Mat.Option.SPD, True)
 
