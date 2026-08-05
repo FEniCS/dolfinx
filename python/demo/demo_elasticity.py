37c468c1399f96911d15c174c9d65446aefdae21
schnellerhase <56360279+schnellerhase@users.noreply.github.com>
2026-08-05 16:49:14 +0200

    One more comms

--- python/demo/demo_elasticity.py
+++ python/demo/demo_elasticity.py
@@ -49,7 +49,6 @@ from dolfinx.io import XDMFFile
 from dolfinx.mesh import CellType, GhostMode, create_box, locate_entities_boundary
 
 dtype = PETSc.ScalarType
-comm = MPI.COMM_WORLD
 # -
 
 # ## Create the operator near-nullspace
@@ -65,7 +64,8 @@ comm = MPI.COMM_WORLD
 # shortened to
 # ```python
 # coords = V.tabulate_dof_coordinates()[:V.dofmap.index_map.size_local,:]
-# vec = PETSc.Vec().createWithArray(coords.ravel(), bsize=gdim, comm=comm)
+# coords = coords.ravel()
+# vec = PETSc.Vec().createWithArray(coords, bsize=gdim, comm=mesh.comm)
 # ns = PETSc.NullSpace().createRigidBody(vec)
 # ```
 
@@ -110,7 +110,7 @@ def build_nullspace(V: FunctionSpace):
 
 
 msh = create_box(
-    comm,
+    MPI.COMM_WORLD,
     [np.array([0.0, 0.0, 0.0]), np.array([2.0, 1.0, 1.0])],
     (16, 16, 16),
     CellType.tetrahedron,
@@ -211,7 +211,7 @@ opts["mg_levels_pc_type"] = "jacobi"
 opts["mg_levels_ksp_chebyshev_esteig_steps"] = 10
 
 # Create PETSc Krylov solver and turn convergence monitoring on
-solver = PETSc.KSP().create(comm)
+solver = PETSc.KSP().create(msh.comm)
 solver.setFromOptions()
 
 # Set matrix operator
@@ -260,12 +260,12 @@ sigma_vm_h.interpolate(sigma_vm_expr)
 # XDMF format files.
 
 # +
-with XDMFFile(comm, "out_elasticity/displacements.xdmf", "w") as file:
+with XDMFFile(msh.comm, "out_elasticity/displacements.xdmf", "w") as file:
     file.write_mesh(msh)
     file.write_function(uh)
 
 # Save solution to XDMF format
-with XDMFFile(comm, "out_elasticity/von_mises_stress.xdmf", "w") as file:
+with XDMFFile(msh.comm, "out_elasticity/von_mises_stress.xdmf", "w") as file:
     file.write_mesh(msh)
     file.write_function(sigma_vm_h)
 # -
@@ -277,7 +277,7 @@ with XDMFFile(comm, "out_elasticity/von_mises_stress.xdmf", "w") as file:
 
 # +
 unorm = la.norm(uh.x)
-if comm.rank == 0:
+if msh.comm.rank == 0:
     print("Solution vector norm:", unorm)
 # -
 
