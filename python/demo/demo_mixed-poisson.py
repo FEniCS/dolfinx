10872be93198c8bf9e56d782838156165a478f6b
Garth N. Wells <gnw20@cam.ac.uk>
2026-08-30 05:00:04 +0100

    Simplify mixed Poisson output setup

--- python/demo/demo_mixed-poisson.py
+++ python/demo/demo_mixed-poisson.py
@@ -273,21 +273,13 @@ if has_adios2:
     from dolfinx.io import VTXWriter
 
 
+use_hypre = has_hypre and hypre_ams_compatible
 for k in (1, 2):
-    sigma, u = solve(k, has_hypre and hypre_ams_compatible)
+    sigma, u = solve(k, use_hypre)
     if has_adios2:
         # VTX supports (discontinuous) Lagrange functions, so interpolate
         # the flux.
-        V_sigma = fem.functionspace(
-            msh,
-            element(
-                "Discontinuous Lagrange",
-                msh.basix_cell(),
-                k,
-                shape=(msh.geometry.dim,),
-                dtype=xdtype,
-            ),
-        )
+        V_sigma = fem.functionspace(msh, ("Discontinuous Lagrange", k, (msh.geometry.dim,)))
         sigma_output = fem.Function(V_sigma, name="sigma", dtype=dtype)
         sigma_output.interpolate(sigma)
         with VTXWriter(msh.comm, f"output_mixed_poisson_sigma_{k}.bp", sigma_output) as f:
