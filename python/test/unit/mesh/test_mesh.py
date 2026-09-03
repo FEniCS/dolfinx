ff61dc6eb8f21f86c371e1d95689597401fa3f17
Garth N. Wells <gnw20@cam.ac.uk>
2026-09-03 08:19:15 +0000

    Add mesh reordering Python test

--- python/test/unit/mesh/test_mesh.py
+++ python/test/unit/mesh/test_mesh.py
@@ -670,6 +670,21 @@ def test_create_mesh_cell_reordering():
 
 
 @pytest.mark.skip_in_parallel
+def test_create_mesh_default_cell_reordering():
+    """Test default reverse Cuthill-McKee cell reordering."""
+    cells = np.array([[0, 1, 2], [1, 3, 2]], dtype=np.int64)
+    x = np.array([[0.0, 0.0], [1.0, 0.0], [0.0, 1.0], [1.0, 1.0]])
+    domain = ufl.Mesh(element("Lagrange", "triangle", 1, shape=(2,)))
+
+    msh_default = _mesh.create_mesh(MPI.COMM_SELF, cells, domain, x)
+    msh_rcm = _mesh.create_mesh(MPI.COMM_SELF, cells, domain, x, reorder_fn=_cpp.graph.reorder_rcm)
+
+    assert np.array_equal(
+        msh_default.topology.original_cell_index, msh_rcm.topology.original_cell_index
+    )
+
+
+@pytest.mark.skip_in_parallel
 def test_create_mesh_sfc_reordering():
     """Test a built-in space-filling-curve cell reordering."""
     cells = np.array([[1, 3, 2], [0, 1, 2]], dtype=np.int64)
