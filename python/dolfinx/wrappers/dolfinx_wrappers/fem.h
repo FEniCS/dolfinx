7f02decfe604cc33c3c628cb6b6bbc65e3a70911
Joergen Schartum Dokken <dokken@simula.no>
2026-09-18 15:19:33 +0000

    Expose is_discontinuous in coordinatelement

--- python/dolfinx/wrappers/dolfinx_wrappers/fem.h
+++ python/dolfinx/wrappers/dolfinx_wrappers/fem.h
@@ -1,4 +1,5 @@
-// Copyright (C) 2017-2026 Chris Richardson and Garth N. Wells
+// Copyright (C) 2017-2026 Chris Richardson, Garth N. Wells and Jørgen S.
+// Dokken
 //
 // This file is part of DOLFINx (https://www.fenicsproject.org)
 //
@@ -1113,6 +1114,8 @@ void declare_coordinate_element(nb::module_& m, const std::string& type)
            &dolfinx::fem::CoordinateElement<T>::create_dof_layout)
       .def_prop_ro("degree", &dolfinx::fem::CoordinateElement<T>::degree)
       .def_prop_ro("dim", &dolfinx::fem::CoordinateElement<T>::dim)
+      .def_prop_ro("is_discontinuous",
+                   &dolfinx::fem::CoordinateElement<T>::is_discontinuous)
       .def_prop_ro("variant", [](const dolfinx::fem::CoordinateElement<T>& self)
                    { return static_cast<int>(self.variant()); })
       .def("hash", &dolfinx::fem::CoordinateElement<T>::hash)
